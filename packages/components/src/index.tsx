4215b97b30f4f3d1c520a87189fac341c3561ec2
kwoktung <onlyif2009@gmail.com>
2021-11-25 22:32:21 +0800

    add token component

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -16,5 +16,6 @@ export { default as Switch } from './Switch';
 export { default as Address } from './Address';
 export { default as Jazzicon } from './Jazzicon';
 export { default as Icon } from './Icon';
+export { default as Token } from './Token';
 
 export * from './Provider/hooks';
--- packages/components/src/utils/index.ts
+++ packages/components/src/utils/index.ts
@@ -4,3 +4,5 @@ export const shortenAddress = (address: string, chars = 4) => {
   }
   return `${address.slice(0, chars + 2)}...${address.slice(-chars)}`;
 };
+
+export const CDN_PREFIX = 'https://onekey-asset.com/';
