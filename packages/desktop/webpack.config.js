ff20bbc8e6140237a627526e96ca5e3b1a78bb2a
zuozhuo <zuoyizhuo@live.com>
2021-12-08 18:29:41 +0800

    fix build config

--- packages/desktop/webpack.config.js
+++ packages/desktop/webpack.config.js
@@ -1,6 +1,14 @@
+const webpack = require('webpack');
 const { createWebpackConfigAsync } = require('expo-yarn-workspaces/webpack');
+
+console.log('============ webpack.version ', webpack.version);
 
 module.exports = async function (env, argv) {
   const config = await createWebpackConfigAsync(env, argv);
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.ONEKEY_BUILD_TYPE': JSON.stringify('desktop'),
+    }),
+  );
   return config;
 };
