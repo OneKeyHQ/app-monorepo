066e5aa0796352ff184c19ca51c26dedfa1d60e6
zuozhuo <zuoyizhuo@live.com>
2021-12-15 18:36:43 +0800

    fix ext popup size (#39)

--- packages/ext/src/entry/ui-popup.tsx
+++ packages/ext/src/entry/ui-popup.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import inpageProviderUi from '@onekeyhq/inpage-provider/src/extension/ui';
 import App from '../App';
 import hotReload from '../ui/hotReload';
+import popupSizeFix from '../ui/popupSizeFix';
 
+popupSizeFix();
 window.extJsBridgeUiToBg = inpageProviderUi.createUiJsBridge();
 
 ReactDOM.render(<App />, window.document.querySelector('#root'));
