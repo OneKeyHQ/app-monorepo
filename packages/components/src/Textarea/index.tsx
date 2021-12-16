d4c4583b7ba1f41a9e7e905d3e25ac11af60e4a4
kwoktung <onlyif2009@gmail.com>
2021-12-16 15:16:52 +0800

    add form component example

--- packages/components/src/Textarea/index.tsx
+++ packages/components/src/Textarea/index.tsx
@@ -1,34 +1,38 @@
-import React, { ComponentProps, FC } from 'react';
+import React, { ComponentProps } from 'react';
 
 import { TextArea as NativeBaseTextArea } from 'native-base';
 
-export default React.forwardRef<
+type TextAreaProps = { isInvalid?: boolean };
+
+const TextArea = React.forwardRef<
   typeof NativeBaseTextArea,
-  ComponentProps<typeof NativeBaseTextArea>
->(({ isInvalid, ...props }, ref) => {
-  return (
-    <NativeBaseTextArea
-      ref={ref}
-      isInvalid={isInvalid}
-      borderColor="border-default"
-      bg="action-secondary-default"
-      color="text-default"
-      _focus={{
-        borderColor: isInvalid ? 'border-critical-default' : 'focused-default',
-        bg: 'action-secondary-default',
-      }}
-      _hover={{
-        borderColor: isInvalid ? 'border-critical-default' : 'focused-default',
-        bg: 'action-secondary-default',
-      }}
-      _disabled={{
-        borderColor: 'border-disabled',
-        bg: 'action-secondary-disabled',
-      }}
-      _invalid={{
-        borderColor: 'border-critical-default',
-      }}
-      {...props}
-    />
-  );
-});
+  ComponentProps<typeof NativeBaseTextArea> & TextAreaProps
+>(({ isInvalid, ...props }, ref) => (
+  <NativeBaseTextArea
+    ref={ref}
+    isInvalid={isInvalid}
+    borderColor="border-default"
+    bg="action-secondary-default"
+    color="text-default"
+    _focus={{
+      borderColor: isInvalid ? 'border-critical-default' : 'focused-default',
+      bg: 'action-secondary-default',
+    }}
+    _hover={{
+      borderColor: isInvalid ? 'border-critical-default' : 'focused-default',
+      bg: 'action-secondary-default',
+    }}
+    _disabled={{
+      borderColor: 'border-disabled',
+      bg: 'action-secondary-disabled',
+    }}
+    _invalid={{
+      borderColor: 'border-critical-default',
+    }}
+    {...props}
+  />
+));
+
+TextArea.displayName = 'TextArea';
+
+export default TextArea;
