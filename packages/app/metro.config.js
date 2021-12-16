c2dc6eb6b5d6651eb3eec2db1e3c945d665af645
zuozhuo <zuoyizhuo@live.com>
2021-12-16 14:53:41 +0800

    add .d.ts file support for native building

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -3,6 +3,6 @@ const { createMetroConfiguration } = require('expo-yarn-workspaces');
 const config = createMetroConfiguration(__dirname);
 
 // hot-reload file type
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'text-js'];
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'text-js', 'd.ts'];
 
 module.exports = config;
