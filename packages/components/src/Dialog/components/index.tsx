57959257afafb009866cf09c48254b71b8cf2be9
ByteZhang1024 <ByteZhang@protonmail.com>
2021-12-16 12:15:47 +0800

    component: Dialog

---
+++ packages/components/src/Dialog/components/index.tsx
@@ -0,0 +1,9 @@
+import Content from './Content';
+import FooterButton from './FooterButton';
+
+const DialogDefault = {
+  Content,
+  FooterButton,
+};
+
+export default DialogDefault;
--- packages/components/src/Icon/Icons.tsx
+++ packages/components/src/Icon/Icons.tsx
@@ -325,6 +325,8 @@ import DatabaseSolid from "./react/solid/Database";
 import DesktopComputerSolid from "./react/solid/DesktopComputer";
 import DeviceMobileSolid from "./react/solid/DeviceMobile";
 import DeviceTabletSolid from "./react/solid/DeviceTablet";
+import DialogIconTypeDangerSolid from "./react/solid/DialogIconTypeDanger";
+import DialogIconTypeInfoSolid from "./react/solid/DialogIconTypeInfo";
 import DocumentSolid from "./react/solid/Document";
 import DocumentAddSolid from "./react/solid/DocumentAdd";
 import DocumentDownloadSolid from "./react/solid/DocumentDownload";
@@ -801,6 +803,8 @@ export type ICON_NAMES =
   | "DesktopComputerSolid"
   | "DeviceMobileSolid"
   | "DeviceTabletSolid"
+  | "DialogIconTypeDangerSolid"
+  | "DialogIconTypeInfoSolid"
   | "DocumentSolid"
   | "DocumentAddSolid"
   | "DocumentDownloadSolid"
@@ -1277,6 +1281,8 @@ export default {
   DesktopComputerSolid: DesktopComputerSolid,
   DeviceMobileSolid: DeviceMobileSolid,
   DeviceTabletSolid: DeviceTabletSolid,
+  DialogIconTypeDangerSolid: DialogIconTypeDangerSolid,
+  DialogIconTypeInfoSolid: DialogIconTypeInfoSolid,
   DocumentSolid: DocumentSolid,
   DocumentAddSolid: DocumentAddSolid,
   DocumentDownloadSolid: DocumentDownloadSolid,
