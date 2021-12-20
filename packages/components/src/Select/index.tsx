72075fe7784c6d65da21730b3185569c217cd17a
zhao.zhang <loatheb.zhao@gmail.com>
2021-12-20 17:44:14 +0800

    feat: add <Select /> components.

--- packages/components/src/Select/index.tsx
+++ packages/components/src/Select/index.tsx
@@ -1,1 +1,197 @@
-export { Select as default } from 'native-base';
+import React, {
+  ComponentProps,
+  ReactNode,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
+
+import { flatten } from 'lodash';
+
+import Box from '../Box';
+import Icon, { ICON_NAMES } from '../Icon';
+import Pressable from '../Pressable';
+import { useUserDevice } from '../Provider/hooks';
+import Token from '../Token';
+import Typography from '../Typography';
+
+import Desktop from './Container/Desktop';
+import Mobile from './Container/Mobile';
+
+export type SelectItem<T = string> = {
+  label: string;
+  value: T;
+  tokenProps?: ComponentProps<typeof Token>;
+};
+
+export type SelectGroupItem<T = string> = {
+  title: string;
+  options: SelectItem<T>[];
+};
+
+export type SelectProps<T = string> = {
+  title?: string;
+  options: (SelectItem<T> | SelectGroupItem<T>)[];
+  value?: T;
+  defaultValue?: T;
+  containerProps?: ComponentProps<typeof Box>;
+  triggerProps?: ComponentProps<typeof Pressable>;
+  dropdownProps?: ComponentProps<typeof Box>;
+  renderTrigger?: (activeItem: SelectItem<T>) => ReactNode;
+  renderItem?: (item: SelectItem<T>, isActive: boolean) => ReactNode;
+  dropdownPosition?: 'center' | 'left' | 'right';
+  onChange?: (v: T, item: SelectItem<T>) => void;
+  footer?: ReactNode;
+  footerText?: string;
+  footerIcon?: ICON_NAMES;
+  onPressFooter?: () => void;
+};
+
+export type ChildProps<T> = Pick<
+  SelectProps<T>,
+  | 'dropdownProps'
+  | 'options'
+  | 'onChange'
+  | 'title'
+  | 'footer'
+  | 'footerText'
+  | 'footerIcon'
+  | 'onPressFooter'
+  | 'renderItem'
+> & {
+  toggleVisible: () => void;
+  visible: boolean;
+  activeOption: SelectItem<T>;
+};
+
+function getTriggerAlignSelf(
+  dropdownPosition: SelectProps['dropdownPosition'],
+) {
+  if (dropdownPosition === 'left') return 'flex-end';
+  if (dropdownPosition === 'right') return 'flex-start';
+  return 'center';
+}
+
+function Select<T = string>({
+  options,
+  value,
+  containerProps,
+  triggerProps,
+  dropdownProps,
+  renderTrigger,
+  renderItem,
+  onChange,
+  defaultValue,
+  title,
+  footer,
+  footerText,
+  footerIcon,
+  onPressFooter,
+  dropdownPosition = 'center',
+}: SelectProps<T>) {
+  const [visible, setVisible] = useState(false);
+  const { size } = useUserDevice();
+  const toggleVisible = useCallback(() => {
+    setVisible((v) => !v);
+  }, []);
+
+  const [innerValue, setInnerValue] = useState<T | undefined>(defaultValue);
+  const currentActiveValue = value ?? innerValue;
+
+  const activeOption = useMemo(() => {
+    if (
+      (options as SelectGroupItem<T>[]).every((option) =>
+        Array.isArray(option.options),
+      )
+    ) {
+      const groupOptions = options as SelectGroupItem<T>[];
+      const flattenedOptions = flatten(
+        groupOptions.map((option) => option.options),
+      );
+      return (flattenedOptions.find(
+        (option) => option.value === currentActiveValue,
+      ) ?? {}) as SelectItem<T>;
+    }
+
+    return ((options as SelectItem<T>[]).find(
+      (option) => option.value === currentActiveValue,
+    ) ?? {}) as SelectItem<T>;
+  }, [currentActiveValue, options]);
+
+  const handleChange = useCallback(
+    (v: SelectItem<T>['value'], option: SelectItem<T>) => {
+      setInnerValue(v);
+      onChange?.(v, option);
+      toggleVisible();
+    },
+    [onChange, toggleVisible],
+  );
+
+  const container = useMemo(() => {
+    const childContainerProps = {
+      visible,
+      options,
+      toggleVisible,
+      dropdownProps,
+      title,
+      footer,
+      footerText,
+      footerIcon,
+      onPressFooter,
+      activeOption,
+      renderItem,
+      onChange: handleChange,
+    };
+
+    if (['SMALL', 'NORMAL'].includes(size)) {
+      return <Mobile<T> {...childContainerProps} />;
+    }
+    return <Desktop<T> {...childContainerProps} />;
+  }, [
+    visible,
+    size,
+    toggleVisible,
+    options,
+    handleChange,
+    dropdownProps,
+    title,
+    footer,
+    footerText,
+    footerIcon,
+    onPressFooter,
+    activeOption,
+    renderItem,
+  ]);
+
+  return (
+    <Box width="100%" position="relative" zIndex={-1} {...containerProps}>
+      <Pressable
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        borderRadius="12px"
+        borderColor="border-default"
+        bg="action-secondary-default"
+        py="2"
+        px="3"
+        width="100%"
+        onPress={toggleVisible}
+        alignSelf={getTriggerAlignSelf(dropdownPosition)}
+        {...triggerProps}
+      >
+        {renderTrigger?.(activeOption) ?? (
+          <>
+            <Typography.Body2 numberOfLines={1} flex="1" mr="1">
+              {activeOption.label ?? '-'}
+            </Typography.Body2>
+            <Icon name="ChevronDownOutline" size={12} />
+          </>
+        )}
+      </Pressable>
+      {container}
+    </Box>
+  );
+}
+
+export default Select;
--- packages/kit/src/views/Components/stories/Select.tsx
+++ packages/kit/src/views/Components/stories/Select.tsx
@@ -1,16 +1,174 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Center, Select } from '@onekeyhq/components';
+import { Center, Select, Stack } from '@onekeyhq/components';
+
+const Select1 = () => {
+  const [value, setValue] = useState('https://rpc.onekey.so/eth');
+  return (
+    <Select
+      onChange={(v) => setValue(v)}
+      value={value}
+      footer={null}
+      containerProps={{
+        width: '280px',
+        zIndex: 999,
+      }}
+      defaultValue="https://rpc.onekey.so/eth"
+      options={[
+        {
+          label: 'https://rpc.onekey.so/eth',
+          value: 'https://rpc.onekey.so/eth',
+        },
+        {
+          label: 'https://google.com',
+          value: 'https://google.com',
+        },
+        {
+          label: 'https://baidu.com',
+          value: 'https://baidu.com',
+        },
+      ]}
+    />
+  );
+};
 
 const SelectGallery = () => (
-  <Center flex="1" bg="background-hovered">
-    <Select>
-      <Select.Item label="UX Research" value="ux" />
-      <Select.Item label="Web Development" value="web" />
-      <Select.Item label="Cross Platform Development" value="cross" />
-      <Select.Item label="UI Designing" value="ui" />
-      <Select.Item label="Backend Development" value="backend" />
-    </Select>
+  <Center flex="1" bg="background-default">
+    <Stack direction="column" space="2" mb="2" alignItems="center">
+      <Select1 />
+      <Select
+        footer={null}
+        containerProps={{
+          width: '280px',
+          zIndex: 999,
+        }}
+        defaultValue="https://rpc.onekey.so/eth"
+        options={[
+          {
+            label: 'https://rpc.onekey.so/eth',
+            value: 'https://rpc.onekey.so/eth',
+          },
+          {
+            label: 'https://google.com',
+            value: 'https://google.com',
+          },
+          {
+            label: 'https://baidu.com',
+            value: 'https://baidu.com',
+          },
+        ]}
+      />
+      <Select
+        containerProps={{
+          width: '280px',
+          zIndex: 998,
+        }}
+        triggerProps={{
+          width: '105px',
+        }}
+        dropdownPosition="left"
+        options={[
+          {
+            label: 'https://rpc.onekey.so/eth',
+            value: 'https://rpc.onekey.so/eth',
+          },
+          {
+            label: 'https://google.com',
+            value: 'https://google.com',
+          },
+          {
+            label: 'https://baidu.com',
+            value: 'https://baidu.com',
+          },
+        ]}
+        footerText="Customize"
+        footerIcon="PencilOutline"
+      />
+      <Select
+        containerProps={{
+          width: '280px',
+        }}
+        triggerProps={{
+          width: '105px',
+        }}
+        dropdownPosition="left"
+        options={[
+          {
+            title: 'EVM',
+            options: [
+              {
+                label: 'Ethereum',
+                value: 'ethereum',
+                tokenProps: {
+                  chain: 'eth',
+                },
+              },
+              {
+                label: 'BSC',
+                value: 'bsc',
+                tokenProps: {
+                  chain: 'bsc',
+                },
+              },
+              {
+                label: 'HECO',
+                value: 'heco',
+                tokenProps: {
+                  chain: 'heco',
+                },
+              },
+              {
+                label: 'OKExChain',
+                value: 'okex',
+                tokenProps: {
+                  chain: 'okex',
+                },
+              },
+              {
+                label: 'Polygon',
+                value: 'polygon',
+                tokenProps: {
+                  chain: 'polygon',
+                },
+              },
+              {
+                label: 'Fantom',
+                value: 'fantom',
+                tokenProps: {
+                  chain: 'fantom',
+                },
+              },
+            ],
+          },
+          {
+            title: 'SOLANA',
+            options: [
+              {
+                label: 'Solana',
+                value: 'solana',
+                tokenProps: {
+                  chain: 'solana',
+                },
+              },
+            ],
+          },
+          {
+            title: 'CFX',
+            options: [
+              {
+                label: 'Conflux',
+                value: 'conflux',
+                tokenProps: {
+                  chain: 'conflux',
+                },
+              },
+            ],
+          },
+        ]}
+        footerText="Customize"
+        footerIcon="PencilOutline"
+      />
+    </Stack>
   </Center>
 );
 
