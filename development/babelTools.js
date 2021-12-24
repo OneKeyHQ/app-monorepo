10b7cf2835be303bf02f46c3e62651faf841d9ed
zuozhuo <zuoyizhuo@live.com>
2021-12-24 16:19:36 +0800

    fix babel error

--- development/babelTools.js
+++ development/babelTools.js
@@ -1,12 +1,25 @@
 function normalizeConfig(config) {
   config.plugins = [
     ...(config.plugins || []),
+    /*
+    support lodash import in Ext background like this:
+      import { isFunction } from 'lodash';
+
+    error in ui:
+       Unchecked runtime.lastError: Could not establish connection. Receiving end does not exist.
+    and background code will never be executed.
+     */
+    ['babel-plugin-lodash'],
     [
       'babel-plugin-inline-import',
       {
         'extensions': ['.text-js'],
       },
     ],
+    /* FIX:
+       TypeError: undefiend is not an object (evaluating 'this._callListeners.bind')
+     */
+    ['@babel/plugin-transform-flow-strip-types'],
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     ['@babel/plugin-proposal-private-methods', { 'loose': true }],
