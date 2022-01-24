763fb477d42f7f71df2034d9f1c9dffc93ce76f4
kwoktung <onlyif2009@gmail.com>
2022-01-24 14:45:18 +0800

    delete useless redux snippet

--- packages/kit/src/store/reducers/settings.ts
+++ packages/kit/src/store/reducers/settings.ts
@@ -6,7 +6,6 @@ import { ThemeVariant } from '@onekeyhq/components/src/Provider/theme';
 
 type SettingsState = {
   theme: ThemeVariant | 'system';
-  systemTheme: ThemeVariant;
   locale: LocaleSymbol;
   version: string;
   instanceId: string;
@@ -14,7 +13,6 @@ type SettingsState = {
 
 const initialState: SettingsState = {
   theme: 'dark',
-  systemTheme: 'dark',
   locale: 'zh-CN',
   version: '1.0.0',
   instanceId: uuid.v4() as string,
@@ -27,15 +25,12 @@ export const settingsSlice = createSlice({
     setTheme: (state, action: PayloadAction<ThemeVariant | 'system'>) => {
       state.theme = action.payload;
     },
-    setSystemTheme: (state, action: PayloadAction<ThemeVariant>) => {
-      state.systemTheme = action.payload;
-    },
     setLocale: (state, action: PayloadAction<LocaleSymbol>) => {
       state.locale = action.payload;
     },
   },
 });
 
-export const { setTheme, setLocale, setSystemTheme } = settingsSlice.actions;
+export const { setTheme, setLocale } = settingsSlice.actions;
 
 export default settingsSlice.reducer;
