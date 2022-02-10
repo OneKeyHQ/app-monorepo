a55dbddb25d6911665609cc89a464161e5886172
jiaji-wei <jiajiwei76@gmail.com>
2022-02-10 12:02:43 +0800

    [refactor] format code use eslint and update dependencies

--- packages/engine/src/managers/covalent.ts
+++ packages/engine/src/managers/covalent.ts
@@ -1,60 +1,52 @@
 import axios from 'axios';
-import { HistoryQuery, TxQuery, HistoryDetilList, TxDetail } from '../types/covalent';
+
+import {
+  HistoryDetilList,
+  HistoryQuery,
+  TxDetail,
+  TxQuery,
+} from '../types/covalent';
 
 const COVALENT_API_KEY = 'YOUR_KEY_HERE';
 
 function getHistoris(query: HistoryQuery): Promise<HistoryDetilList> {
+  let request = `https://api.covalenthq.com/v1/${query.chainId}/address/${query.address}/transactions_v2/?key=${COVALENT_API_KEY}&`;
 
-    if (query.chainId === 'undefined' || query.address === "undefined") {
-        return Promise.reject("Invalid query");
-    }
+  if (query.pageNumber) {
+    request += `page-number=${query.pageNumber}&`;
+  }
+  if (query.pageSize) {
+    request += `page-size=${query.pageSize}&`;
+  }
+  if (query.quoteCurrency) {
+    request += `quote-currency=${query.quoteCurrency}&`;
+  }
+  if (query.format) {
+    request += `format=${query.format}&`;
+  }
+  if (query.blockSignedAtAsc) {
+    request += `block-signed-at-asc=${query.blockSignedAtAsc.toString()}&`;
+  }
+  if (query.noLogs) {
+    request += `no-logs=${query.noLogs.toString()}&`;
+  }
 
-    var request = `https://api.covalenthq.com/v1/${query.chainId}/address/${query.address}/transactions_v2/?key=${COVALENT_API_KEY}&`
-
-    if (query.pageNumber) {
-        request += `page-number=${query.pageNumber}&`
-    }
-    if (query.pageSize) {
-        request += `page-size=${query.pageSize}&`
-    }
-    if (query.quoteCurrency) {
-        request += `quote-currency=${query.quoteCurrency}&`
-    }
-    if (query.format) {
-        request += `format=${query.format}&`
-    }
-    if (query.blockSignedAtAsc) {
-        request += `block-signed-at-asc=${query.blockSignedAtAsc}&`
-    }
-    if (query.noLogs) {
-        request += `no-logs=${query.noLogs}&`
-    }
-
-    return axios.get<HistoryDetilList>(request)
-        .then((response) => {
-            return response.data;
-        });
+  return axios.get<HistoryDetilList>(request).then((response) => response.data);
 }
 
-function getTxDetail(query: TxQuery) : Promise<TxDetail> {
-    if (query.chainId === 'undefined' || query.txHash === "undefined") {
-        return Promise.reject("Invalid query");
-    }
-    var request = `https://api.covalenthq.com/v1/${query.chainId}/transaction_v2/${query.txHash}/?key=${COVALENT_API_KEY}&`
-    if (query.quoteCurrency) {
-        request += `quote-currency=${query.quoteCurrency}&`
-    }
-    if (query.format) {
-        request += `format=${query.format}&`
-    }
-    if (query.noLogs) {
-        request += `no-logs=${query.noLogs}&`
-    }
+function getTxDetail(query: TxQuery): Promise<TxDetail> {
+  let request = `https://api.covalenthq.com/v1/${query.chainId}/transaction_v2/${query.txHash}/?key=${COVALENT_API_KEY}&`;
+  if (query.quoteCurrency) {
+    request += `quote-currency=${query.quoteCurrency}&`;
+  }
+  if (query.format) {
+    request += `format=${query.format}&`;
+  }
+  if (query.noLogs) {
+    request += `no-logs=${query.noLogs.toString()}&`;
+  }
 
-    return axios.get<TxDetail>(request)
-        .then((response) => {
-            return response.data;
-        });
+  return axios.get<TxDetail>(request).then((response) => response.data);
 }
 
-export { getHistoris, getTxDetail};
+export { getHistoris, getTxDetail };
--- packages/engine/src/managers/opensea.ts
+++ packages/engine/src/managers/opensea.ts
@@ -4,46 +4,40 @@ import { OpenSeaAsset } from 'opensea-js/lib/types';
 const OPENSEA_API_KEY = 'YOUR_KEY_HERE';
 
 const config = {
-    headers: { "X-API-KEY": OPENSEA_API_KEY, Accept: 'application/json' }
+  headers: { 'X-API-KEY': OPENSEA_API_KEY, Accept: 'application/json' },
 };
 
-type AssetList = { assets: OpenSeaAsset[]; estimatedCount: number; }
-
+type AssetList = { assets: OpenSeaAsset[]; estimatedCount: number };
 
 function getAssets(
-    owner: string,
-    offset: number = 0,
-    limit: number = 50,
-    assetContractAddress?: string,
+  owner: string,
+  offset = 0,
+  limit = 50,
+  assetContractAddress?: string,
 ): Promise<AssetList> {
+  let request = `https://api.opensea.io/api/v1/assets?offset=${offset}&limit=${limit}&`;
 
-    var request = `https://api.opensea.io/api/v1/assets?offset=${offset}&limit=${limit}&`
+  request += `owner=${owner}`;
 
-    request += `owner=${owner}`
+  if (assetContractAddress) {
+    request += `asset_contract_address=${assetContractAddress}&`;
+  }
 
-    if (assetContractAddress) {
-        request += `asset_contract_address=${assetContractAddress}&`
-    }
-
-    return axios.get<AssetList>(request, config)
-        .then((response) => {
-            return response.data;
-        });
+  return axios
+    .get<AssetList>(request, config)
+    .then((response) => response.data);
 }
-
 
 function getAssetDetail(
-    assetContractAddress: string,
-    tokenId: string,
+  assetContractAddress: string,
+  tokenId: string,
 ): Promise<OpenSeaAsset> {
+  const request = `https://api.opensea.io/api/v1/asset/${assetContractAddress}/${tokenId}`;
 
-    var request = `https://api.opensea.io/api/v1/asset/${assetContractAddress}/${tokenId}`
-
-    return axios.get<OpenSeaAsset>(request, config)
-        .then((response) => {
-            return response.data;
-        });
+  return axios
+    .get<OpenSeaAsset>(request, config)
+    .then((response) => response.data);
 }
 
-
+export { getAssets, getAssetDetail };
 export type { OpenSeaAsset as asset } from 'opensea-js/lib/types';
