baf839ad8c81d6728c435395524d799c4bf7d0c8
jiaji-wei <jiajiwei76@gmail.com>
2022-02-21 15:18:36 +0800

    [feat] add swap and erc721 type

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -761,14 +761,15 @@ class Engine {
     ]);
 
     if (network.impl !== IMPL_EVM) {
-      throw new OneKeyInternalError('Network not support.');
+      return [];
     }
 
     if (typeof dbAccount === 'undefined') {
-      throw new OneKeyInternalError('Account not found.');
+      return [];
     }
+
     if (dbAccount.type !== ACCOUNT_TYPE_SIMPLE) {
-      throw new NotImplemented();
+      return [];
     }
     const chainId = network.id.split(SEPERATOR)[1];
 
@@ -793,15 +794,17 @@ class Engine {
     ]);
 
     if (network.impl !== IMPL_EVM) {
-      throw new OneKeyInternalError('Network not support.');
+      return [];
     }
 
     if (typeof dbAccount === 'undefined') {
-      throw new OneKeyInternalError('Account not found.');
+      return [];
     }
+
     if (dbAccount.type !== ACCOUNT_TYPE_SIMPLE) {
-      throw new NotImplemented();
+      return [];
     }
+
     const chainId = network.id.split(SEPERATOR)[1];
     return getErc20TransferHistories(
       chainId,
