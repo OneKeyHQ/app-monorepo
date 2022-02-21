0157f4b27fe2ef2a6af8ab7c0b870c5cc1e3d51b
zuozhuo <zuoyizhuo@live.com>
2022-02-21 17:04:05 +0800

    account/chain changed to dapp

--- packages/kit/src/background/WalletApi.ts
+++ packages/kit/src/background/WalletApi.ts
@@ -1,5 +1,17 @@
 // TODO rename to BackgroundService, WalletService, ProviderService?
+import type { SimpleAccount } from '@onekeyhq/engine/src/types/account';
+
+import { appSelector } from '../store';
+import { GeneralInitialState } from '../store/reducers/general';
+
 class WalletApi {
+  // TODO remove
+  selectedAddress = '0x76f3f64cb3cd19debee51436df630a342b736c24';
+
+  // TODO remove
+  chainId = '0x1';
+
+  // TODO remove
   accounts = [
     '0x76f3f64cb3cd19debee51436df630a342b736c24',
     '0x99f825d80cadd21d77d13b7e13d25960b40a6299',
@@ -8,13 +20,33 @@ class WalletApi {
     '0x76b4a2de2e67ef5ee4a5050352aec077208fc7f1',
   ];
 
-  chainId = '0x1'; // 0x3 Ropsten
-
-  networkVersion = '1';
-
-  selectedAddress = this.accounts[0];
-
   isConnected = true; // current dapp isConnected
+
+  getCurrentAccounts() {
+    if (!this.isConnected) {
+      return [];
+    }
+    const { activeAccount } = appSelector(
+      (s) => s.general,
+    ) as GeneralInitialState;
+    const account = activeAccount as SimpleAccount;
+    if (account && account.address) {
+      return [account.address];
+    }
+    return [];
+  }
+
+  getCurrentNetwork() {
+    const { activeNetwork } = appSelector(
+      (s) => s.general,
+    ) as GeneralInitialState;
+    // TODO chainId, networkVersion needs in activeNetwork
+    return {
+      chainId: '0x1',
+      networkVersion: '1',
+      activeNetwork,
+    };
+  }
 }
 
 export default WalletApi;
