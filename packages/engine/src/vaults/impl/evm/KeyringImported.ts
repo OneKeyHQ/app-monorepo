954fd4205da45aa9783f2b169fc5758e7fead517
LiuZJ <hust_lzj@icloud.com>
2022-04-02 19:09:54 +0800

    feat: vaults support imported private key.

--- packages/engine/src/vaults/impl/evm/KeyringImported.ts
+++ packages/engine/src/vaults/impl/evm/KeyringImported.ts
@@ -1,7 +1,32 @@
+import {
+  SignedTx,
+  UnsignedTx,
+} from '@onekeyfe/blockchain-libs/dist/types/provider';
+
+import debugLogger from '@onekeyhq/shared/src/logger/debugLogger';
+
+import { ISignCredentialOptions } from '../../../types/vault';
 import { KeyringImportedBase } from '../../keyring/KeyringImportedBase';
 
 export class KeyringImported extends KeyringImportedBase {
-  signTransaction(): Promise<any> {
-    return Promise.resolve(undefined);
+  async signTransaction(
+    unsignedTx: UnsignedTx,
+    options: ISignCredentialOptions,
+  ): Promise<SignedTx> {
+    // return Promise.resolve(undefined);
+    const { networkId } = this;
+    const dbAccount = await this.getDbAccount();
+    const credential = await this.getCredential(options);
+    const signers = this.engine.providerManager.getSigners(
+      networkId,
+      credential,
+      dbAccount,
+    );
+    debugLogger.engine('EVM signTransaction', unsignedTx, signers);
+    return this.engine.providerManager.signTransaction(
+      networkId,
+      unsignedTx,
+      signers,
+    );
   }
 }
