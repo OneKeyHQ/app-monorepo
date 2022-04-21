0f11ef9f4cc9df56b94b855eee7c388284603750
KaidonWong <1162327524@qq.com>
2022-04-21 13:20:58 +0800

    fix: fix 2 bugs of toast component

--- packages/kit/src/provider/NavigationProvider.tsx
+++ packages/kit/src/provider/NavigationProvider.tsx
@@ -50,7 +50,7 @@ const NavigationApp = () => {
       >
         <Navigator />
       </NavigationContainer>
-      <Box pointerEvents='none' style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, overflow: 'hidden'}}>
+      <Box overflow='hidden' pointerEvents='none' position='absolute' top={0} bottom={0} left={0} right={0}>
         <Toast bottomOffset={60} />
       </Box>
     </>
