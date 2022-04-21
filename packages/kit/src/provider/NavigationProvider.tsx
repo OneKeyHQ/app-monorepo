4a971252142e3e1a326df585258be4acddb1bd36
KaidonWong <41094868+KaidonWong@users.noreply.github.com>
2022-04-21 19:43:41 +0800

    fix: 2 problems related to Toast Component. One is its usage in Modal… (#579)
    * fix: 2 problems related to Toast Component. One is its usage in Modal component will cause memory leak. The other is a display issue about abnormal offset of the Modal component when toast was popped up before.
    
    * fix: fix 2 bugs of toast component
    
    * fix: my lint problem
    
    * fix: my lint problem.
    
    Co-authored-by: ll__ <loatheb.zhao@gmail.com>

--- packages/kit/src/provider/NavigationProvider.tsx
+++ packages/kit/src/provider/NavigationProvider.tsx
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createURL } from 'expo-linking';
 
-import { useThemeValue } from '@onekeyhq/components';
+import { Box, useThemeValue } from '@onekeyhq/components';
 import Toast from '@onekeyhq/components/src/Toast/Custom';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
@@ -50,7 +50,17 @@ const NavigationApp = () => {
       >
         <Navigator />
       </NavigationContainer>
-      <Toast bottomOffset={60} />
+      <Box
+        overflow="hidden"
+        pointerEvents="none"
+        position="absolute"
+        top={0}
+        bottom={0}
+        left={0}
+        right={0}
+      >
+        <Toast bottomOffset={60} />
+      </Box>
     </>
   );
 };
