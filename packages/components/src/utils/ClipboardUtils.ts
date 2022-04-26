90173d6a2d0562cd9af7b2dc4a15bb345c21b211
zuozhuo <zuoyizhuo@live.com>
2022-04-27 07:33:02 +0800

    fix: can get clipboard check

--- packages/components/src/utils/ClipboardUtils.ts
+++ packages/components/src/utils/ClipboardUtils.ts
@@ -1,7 +1,14 @@
 import * as Clipboard from 'expo-clipboard';
 
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
 export const copyToClipboard = (msg: string) => {
   Clipboard.setString(msg);
 };
 
-export const getClipboard = () => Clipboard.getStringAsync();
+export const getClipboard = () => {
+  if (!platformEnv.canGetClipboard) {
+    throw new Error('getClipboard is not allowed in Web and Extension');
+  }
+  return Clipboard.getStringAsync();
+};
