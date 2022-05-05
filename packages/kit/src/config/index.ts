f802091b8f68cc44b55fb40c516d3e85963fb5bf
zhao.zhang <loatheb.zhao@gmail.com>
2022-05-05 21:52:33 +0800

    fix: device connection at mobile

--- packages/kit/src/config/index.ts
+++ packages/kit/src/config/index.ts
@@ -3,5 +3,5 @@ export const MAX_PAGE_CONTAINER_WIDTH = 768;
 /**
  * Tokens will injected at build process. These are client token.
  */
-export const COVALENT_API_KEY = process.env.COVALENT_KEY as string;
-export const OPENSEA_API_KEY = process.env.OPENSEA_KEY as string;
+export const COVALENT_API_KEY = process.env.COVALENT_KEY!;
+export const OPENSEA_API_KEY = process.env.OPENSEA_KEY!;
