f9b811f3e7c5062dd14780165eb9d88536084763
kwoktung <onlyif2009@gmail.com>
2022-05-08 21:56:38 +0800

    feat: adjust <Protected/> autoFocus behaviour (#674)
    * feat: adjust <Protected/> autoFocus behaviour
    
    * OK-7640: tweat AddExistingWallet view

---
+++ packages/kit/src/components/LocalAuthenticationButton/LocalAuthenticationButton.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import React, { FC, useCallback, useEffect, useLayoutEffect } from 'react';
+
+import { useIntl } from 'react-intl';
+import { AppState, AppStateStatus } from 'react-native';
+
+import { IconButton } from '@onekeyhq/components';
+
+import backgroundApiProxy from '../../background/instance/backgroundApiProxy';
+import { useLocalAuthentication, useToast } from '../../hooks';
+import { useSettings, useStatus } from '../../hooks/redux';
+import { ValidationFields } from '../Protected/types';
+
+type LocalAuthenticationButtonProps = {
+  field?: ValidationFields;
+  onOk?: (password: string) => void;
+  onNg?: () => void;
+};
+
+const LocalAuthenticationButton: FC<LocalAuthenticationButtonProps> = ({
+  onOk,
+  onNg,
+  field,
+}) => {
+  const intl = useIntl();
+  const toast = useToast();
+  const { enableLocalAuthentication, validationState = {} } = useSettings();
+  const { authenticationType } = useStatus();
+  const { localAuthenticate, getPassword } = useLocalAuthentication();
+
+  const onLocalAuthenticate = useCallback(async () => {
+    const localAuthenticateResult = await localAuthenticate();
+    if (localAuthenticateResult.success) {
+      const password = await getPassword();
+      if (password) {
+        const result = await backgroundApiProxy.serviceApp.verifyPassword(
+          password,
+        );
+        if (result) {
+          onOk?.(password);
+          return;
+        }
+      }
+    }
+    toast.show({
+      title: intl.formatMessage({ id: 'msg__verification_failure' }),
+    });
+    onNg?.();
+  }, [onOk, onNg, localAuthenticate, getPassword, toast, intl]);
+
+  const onChange = useCallback(
+    (state: AppStateStatus) => {
+      if (!field || state !== 'active' || field !== ValidationFields.Unlock) {
+        return;
+      }
+      if (
+        validationState[field] === true ||
+        validationState[field] === undefined
+      ) {
+        onLocalAuthenticate();
+      }
+    },
+    [onLocalAuthenticate, field, validationState],
+  );
+
+  // for app unlock
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onChange);
+    return () => {
+      // @ts-ignore
+      if (subscription) {
+        // @ts-ignore
+        subscription?.remove();
+      } else {
+        AppState.removeEventListener('change', onChange);
+      }
+    };
+  }, [onChange]);
+
+  useLayoutEffect(() => {
+    if (
+      !field ||
+      !enableLocalAuthentication ||
+      field === ValidationFields.Unlock
+    ) {
+      return;
+    }
+    if (
+      validationState[field] === true ||
+      validationState[field] === undefined
+    ) {
+      onLocalAuthenticate();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <IconButton
+      size="xl"
+      name={
+        authenticationType === 'FACIAL' ? 'FaceIdIllus' : 'FingerPrintIllus'
+      }
+      onPress={onLocalAuthenticate}
+    />
+  );
+};
+
+export default LocalAuthenticationButton;
