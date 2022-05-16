9875e555e02a87d2152adfdea29ea7eb717d9716
kwoktung <onlyif2009@gmail.com>
2022-05-16 22:59:38 +0800

    OK-8873: fix enableAppLock

--- packages/kit/src/components/AppLock/AppLock.tsx
+++ packages/kit/src/components/AppLock/AppLock.tsx
@@ -1,10 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { Box, OverlayContainer } from '@onekeyhq/components';
-import { useData } from '@onekeyhq/kit/src/hooks/redux';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
-import { useAppSelector } from '../../hooks/redux';
+import { useAppSelector, useDebounce } from '../../hooks';
 
 import { AppStateHeartbeat } from './AppStateHeartbeat';
 import { AppStateUnlock } from './AppStateUnlock';
@@ -14,10 +13,19 @@ type AppLockProps = { children: JSX.Element };
 
 export const AppLockOverlayMode: FC<AppLockProps> = ({ children }) => {
   const enableAppLock = useAppSelector((s) => s.settings.enableAppLock);
+  const isPasswordSet = useAppSelector((s) => s.data.isPasswordSet);
   const isStatusUnlock = useAppSelector((s) => s.status.isUnlock);
-  const { isPasswordSet, isUnlock: isDataUnlock } = useData();
-  const prerequisites = isPasswordSet && enableAppLock;
-  const isUnlock = isDataUnlock && isStatusUnlock;
+  const isDataUnlock = useAppSelector((s) => s.data.isUnlock);
+
+  const memo = useMemo(
+    () => ({ enableAppLock, isPasswordSet, isStatusUnlock, isDataUnlock }),
+    [enableAppLock, isPasswordSet, isStatusUnlock, isDataUnlock],
+  );
+  const data = useDebounce(memo, 300);
+
+  const prerequisites = data.isPasswordSet && data.enableAppLock;
+  const isUnlock = data.isDataUnlock && data.isStatusUnlock;
+
   return (
     <Box w="full" h="full">
       {prerequisites && !isUnlock ? (
@@ -34,23 +42,29 @@ export const AppLockOverlayMode: FC<AppLockProps> = ({ children }) => {
 
 export const AppLockNormalMode: FC<AppLockProps> = ({ children }) => {
   const enableAppLock = useAppSelector((s) => s.settings.enableAppLock);
+  const isPasswordSet = useAppSelector((s) => s.data.isPasswordSet);
   const isStatusUnlock = useAppSelector((s) => s.status.isUnlock);
-  const { isPasswordSet, isUnlock: isDataUnlock } = useData();
-  const prerequisites = isPasswordSet && enableAppLock;
-  const isUnlock = isDataUnlock && isStatusUnlock;
-  if (!prerequisites) {
-    return children;
+  const isDataUnlock = useAppSelector((s) => s.data.isUnlock);
+
+  const memo = useMemo(
+    () => ({ enableAppLock, isPasswordSet, isStatusUnlock, isDataUnlock }),
+    [enableAppLock, isPasswordSet, isStatusUnlock, isDataUnlock],
+  );
+  const data = useDebounce(memo, 300);
+
+  const prerequisites = data.isPasswordSet && data.enableAppLock;
+  const isUnlock = data.isDataUnlock && data.isStatusUnlock;
+
+  if (prerequisites && !isUnlock) {
+    return <AppStateUnlock />;
   }
-  if (isUnlock) {
-    return (
-      <Box w="full" h="full">
-        <AppStateUpdator />
-        <AppStateHeartbeat />
-        {children}
-      </Box>
-    );
-  }
-  return <AppStateUnlock />;
+  return (
+    <Box w="full" h="full">
+      {prerequisites && isUnlock ? <AppStateUpdator /> : null}
+      {prerequisites && isUnlock ? <AppStateHeartbeat /> : null}
+      {children}
+    </Box>
+  );
 };
 
 export const AppLock: FC<AppLockProps> = ({ children }) => {
