f72385fe120904e854674f803fd1a523c68567fc
taimanhui <taimanhui@protonmail.com>
2022-05-25 18:52:38 +0800

    feat: show token management by settings
    Fixes: OK-9062

--- packages/engine/src/vaults/impl/near/settings.ts
+++ packages/engine/src/vaults/impl/near/settings.ts
@@ -3,6 +3,7 @@ import { IVaultSettings } from '../../types';
 const settings: IVaultSettings = {
   feeInfoEditable: false,
   privateKeyExportEnabled: true,
+  tokenEnabled: true,
 
   importedAccountEnabled: true,
   hardwareAccountEnabled: false,
--- packages/engine/src/vaults/types.ts
+++ packages/engine/src/vaults/types.ts
@@ -1,6 +1,7 @@
 export type IVaultSettings = {
   feeInfoEditable: boolean;
   privateKeyExportEnabled: boolean;
+  tokenEnabled: boolean;
 
   importedAccountEnabled: boolean;
   watchingAccountEnabled: boolean;
