58a5053de1ff0655086bbe72120b1ef367079af1
ByteZhang1024 <ByteZhang@protonmail.com>
2022-05-25 17:34:02 +0800

    chore: partial icon color

--- packages/components/src/Icon/react/outline/Scan.tsx
+++ packages/components/src/Icon/react/outline/Scan.tsx
@@ -3,11 +3,8 @@ import Svg, { SvgProps, Path } from 'react-native-svg';
 
 function SvgScan(props: SvgProps) {
   return (
-    <Svg viewBox="0 0 24 24" fill="none" {...props}>
-      <Path
-        d="M10 9h4V7h-4v2zm5 1v4h2v-4h-2zm-1 5h-4v2h4v-2zm-5-1v-4H7v4h2zm1 1a1 1 0 01-1-1H7a3 3 0 003 3v-2zm5-1a1 1 0 01-1 1v2a3 3 0 003-3h-2zm-1-5a1 1 0 011 1h2a3 3 0 00-3-3v2zm-4-2a3 3 0 00-3 3h2a1 1 0 011-1V7zM5 9V6H3v3h2zm0 9v-3H3v3h2zm14-3v3h2v-3h-2zm0-9v3h2V6h-2zM6 5h3V3H6v2zm9 0h3V3h-3v2zM9 19H6v2h3v-2zm9 0h-3v2h3v-2zM3 18a3 3 0 003 3v-2a1 1 0 01-1-1H3zm16 0a1 1 0 01-1 1v2a3 3 0 003-3h-2zm2-12a3 3 0 00-3-3v2a1 1 0 011 1h2zM5 6a1 1 0 011-1V3a3 3 0 00-3 3h2z"
-        fill="#8C8CA1"
-      />
+    <Svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <Path d="M10 9h4V7h-4v2zm5 1v4h2v-4h-2zm-1 5h-4v2h4v-2zm-5-1v-4H7v4h2zm1 1a1 1 0 01-1-1H7a3 3 0 003 3v-2zm5-1a1 1 0 01-1 1v2a3 3 0 003-3h-2zm-1-5a1 1 0 011 1h2a3 3 0 00-3-3v2zm-4-2a3 3 0 00-3 3h2a1 1 0 011-1V7zM5 9V6H3v3h2zm0 9v-3H3v3h2zm14-3v3h2v-3h-2zm0-9v3h2V6h-2zM6 5h3V3H6v2zm9 0h3V3h-3v2zM9 19H6v2h3v-2zm9 0h-3v2h3v-2zM3 18a3 3 0 003 3v-2a1 1 0 01-1-1H3zm16 0a1 1 0 01-1 1v2a3 3 0 003-3h-2zm2-12a3 3 0 00-3-3v2a1 1 0 011 1h2zM5 6a1 1 0 011-1V3a3 3 0 00-3 3h2z" />
     </Svg>
   );
 }
--- packages/components/src/Icon/react/solid/Scan.tsx
+++ packages/components/src/Icon/react/solid/Scan.tsx
@@ -3,14 +3,13 @@ import Svg, { SvgProps, Path } from 'react-native-svg';
 
 function SvgScan(props: SvgProps) {
   return (
-    <Svg viewBox="0 0 20 20" fill="none" {...props}>
+    <Svg viewBox="0 0 20 20" fill="currentColor" {...props}>
       <Path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M3 5a2 2 0 012-2h3a1 1 0 010 2H5v3a1 1 0 01-2 0V5zm8-1a1 1 0 011-1h3a2 2 0 012 2v3a1 1 0 11-2 0V5h-3a1 1 0 01-1-1zm-7 7a1 1 0 011 1v3h3a1 1 0 110 2H5a2 2 0 01-2-2v-3a1 1 0 011-1zm12 0a1 1 0 011 1v3a2 2 0 01-2 2h-3a1 1 0 110-2h3v-3a1 1 0 011-1z"
-        fill="#8C8CA1"
       />
-      <Path d="M7 7h6v6H7V7z" fill="#8C8CA1" />
+      <Path d="M7 7h6v6H7V7z" />
     </Svg>
   );
 }
