5d10c5e2353169ce8a4d6daac265f645b43d3a13
Sunny Luo <sunnylqm@gmail.com>
2022-05-30 22:47:35 +0800

    chore: update expo 45 and remove next (#795)
    * feat: v8
    
    * feat: update expo 45
    
    * chore: remove next-adapter except ext
    
    * fix: ios build
    
    * chore: cleanup
    
    * fix: ext build

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ const babelTools = require('./development/babelTools');
 module.exports = babelTools.normalizeConfig({
   platform: babelTools.developmentConsts.platforms.all,
   config: {
-    presets: ['@expo/next-adapter/babel'],
+    presets: ['babel-preset-expo'],
     plugins: [],
   },
 });
--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -3,7 +3,7 @@ const babelTools = require('../../development/babelTools');
 module.exports = babelTools.normalizeConfig({
   platform: babelTools.developmentConsts.platforms.web,
   config: {
-    presets: ['@expo/next-adapter/babel'],
+    presets: ['babel-preset-expo'],
     plugins: ['react-native-reanimated/plugin'],
   },
 });
