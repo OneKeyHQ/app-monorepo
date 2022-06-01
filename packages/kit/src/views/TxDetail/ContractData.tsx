b592ac8e63c39c92be84e9cd4958c980f843f0fa
liuzhijie <hust_lzj@icloud.com>
2022-06-01 13:23:02 +0800

    fix: swap histroy & tx detail ui.

---
+++ packages/kit/src/views/TxDetail/ContractData.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+import { useIntl } from 'react-intl';
+
+import { Container } from '@onekeyhq/components';
+import { ContentItemProps } from '@onekeyhq/components/src/ContentBox/ContentBasisItem';
+import { EVMDecodedItem } from '@onekeyhq/engine/src/vaults/impl/evm/decoder/types';
+
+export type HashProps = {
+  tx: EVMDecodedItem;
+} & ContentItemProps;
+
+const ContractData: FC<HashProps> = (props) => {
+  const { tx } = props;
+  const intl = useIntl();
+
+  return (
+    <Container.Item
+      {...props}
+      title={intl.formatMessage({ id: 'form__contract_data' })}
+      describe={tx.data.slice(0, 200)}
+    />
+  );
+};
+
+export default ContractData;
