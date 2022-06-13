fb558ce90120dc439c9259a75ff7eab3363b6422
zuozhuo <zuoyizhuo@live.com>
2022-06-13 16:14:44 +0800

    fix: send bugs

--- packages/components/src/Form/FormItem.tsx
+++ packages/components/src/Form/FormItem.tsx
@@ -6,14 +6,17 @@ import React, {
 } from 'react';
 
 import { Controller, ControllerProps, FieldValues } from 'react-hook-form';
+import { useIntl } from 'react-intl';
 
 import { gotoScanQrcode } from '@onekeyhq/kit/src/utils/gotoScanQrcode';
 
 import Box from '../Box';
 import FormControl from '../FormControl';
+import { ICON_NAMES } from '../Icon';
 import IconButton from '../IconButton';
 import { useIsVerticalLayout } from '../Provider/hooks';
 import Stack from '../Stack';
+import { TextAreaAction } from '../Textarea';
 import Typography from '../Typography';
 import { getClipboard } from '../utils/ClipboardUtils';
 
@@ -24,6 +27,7 @@ type InternalActionList = 'paste' | 'scan';
 type FormItemProps = {
   label?: string;
   labelAddon?: ReactElement | InternalActionList[];
+  isLabelAddonActions?: boolean;
   helpText?: string | ((v: any) => string) | ReactElement;
   onLabelAddonPress?: () => void;
   children?: ReactElement<any>;
@@ -39,6 +43,7 @@ export function FormItem<TFieldValues extends FieldValues = FieldValues>({
   defaultValue,
   formControlProps,
   labelAddon,
+  isLabelAddonActions,
   onLabelAddonPress,
   ...props
 }: Omit<ControllerProps<TFieldValues>, 'render'> & FormItemProps) {
@@ -51,6 +56,7 @@ export function FormItem<TFieldValues extends FieldValues = FieldValues>({
     }
   }, []);
   const small = useIsVerticalLayout();
+  const intl = useIntl();
 
   return (
     <Controller
@@ -61,22 +67,23 @@ export function FormItem<TFieldValues extends FieldValues = FieldValues>({
       render={({
         field: { onChange, onBlur, value },
         fieldState: { error },
-      }) => (
-        <FormControl isInvalid={!!error} width="full" {...formControlProps}>
-          <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-            mb={1}
-          >
-            {!!label && (
-              <FormControl.Label mb={0}>
-                <Typography.Body2Strong>{label}</Typography.Body2Strong>
-              </FormControl.Label>
-            )}
-
-            {Array.isArray(labelAddon) ? (
+      }) => {
+        const onPasteClick = async () => {
+          await handleCopied(onChange);
+          onLabelAddonPress?.();
+        };
+        const onScanClick = () => {
+          gotoScanQrcode((result) => {
+            onChange(result);
+            onLabelAddonPress?.();
+          });
+        };
+        const renderLabelAddon = () => {
+          if (isLabelAddonActions) {
+            return null;
+          }
+          if (Array.isArray(labelAddon)) {
+            return (
               <Stack direction="row" space="2">
                 {labelAddon.map((item, i) => {
                   if (item === 'paste') {
@@ -87,10 +94,7 @@ export function FormItem<TFieldValues extends FieldValues = FieldValues>({
                         size="xs"
                         circle
                         name="ClipboardSolid"
-                        onPress={async () => {
-                          await handleCopied(onChange);
-                          onLabelAddonPress?.();
-                        }}
+                        onPress={onPasteClick}
                       />
                     );
                   }
@@ -102,37 +106,75 @@ export function FormItem<TFieldValues extends FieldValues = FieldValues>({
                         size="xs"
                         circle
                         name={small ? 'ScanOutline' : 'ScanSolid'}
-                        onPress={() => {
-                          gotoScanQrcode((result) => {
-                            onChange(result);
-                            onLabelAddonPress?.();
-                          });
-                        }}
+                        onPress={onScanClick}
                       />
                     );
                   }
                   return null;
                 })}
               </Stack>
-            ) : (
-              labelAddon
+            );
+          }
+          return labelAddon;
+        };
+        let actions: Array<TextAreaAction | null> | undefined;
+        if (isLabelAddonActions && Array.isArray(labelAddon)) {
+          actions = labelAddon
+            .map((item) => {
+              if (item === 'paste') {
+                return {
+                  icon: 'ClipboardSolid' as ICON_NAMES,
+                  text: intl.formatMessage({ id: 'action__paste' }),
+                  onPress: onPasteClick,
+                };
+              }
+              if (item === 'scan') {
+                return {
+                  icon: 'ScanSolid' as ICON_NAMES,
+                  text: intl.formatMessage({ id: 'title__scan_qr_code' }),
+                  onPress: onScanClick,
+                };
+              }
+              return null;
+            })
+            .filter(Boolean);
+        }
+        return (
+          <FormControl isInvalid={!!error} width="full" {...formControlProps}>
+            {(!!label || !isLabelAddonActions) && (
+              <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+                mb={1}
+              >
+                {!!label && (
+                  <FormControl.Label mb={0}>
+                    <Typography.Body2Strong>{label}</Typography.Body2Strong>
+                  </FormControl.Label>
+                )}
+
+                {renderLabelAddon()}
+              </Box>
             )}
-          </Box>
-          {children
-            ? cloneElement(children, { onChange, onBlur, value })
-            : null}
-          {helpText ? (
-            <FormControl.HelperText>
-              <Typography.Body2 color="text-subdued">
-                {typeof helpText === 'function' ? helpText(value) : helpText}
-              </Typography.Body2>
-            </FormControl.HelperText>
-          ) : null}
-          {error && error?.message ? (
-            <FormErrorMessage message={error?.message} />
-          ) : null}
-        </FormControl>
-      )}
+
+            {children
+              ? cloneElement(children, { onChange, onBlur, value, actions })
+              : null}
+            {helpText ? (
+              <FormControl.HelperText>
+                <Typography.Body2 color="text-subdued">
+                  {typeof helpText === 'function' ? helpText(value) : helpText}
+                </Typography.Body2>
+              </FormControl.HelperText>
+            ) : null}
+            {error && error?.message ? (
+              <FormErrorMessage message={error?.message} />
+            ) : null}
+          </FormControl>
+        );
+      }}
     />
   );
 }
