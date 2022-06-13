b36585a2edc61bc09a4e557d315025c93950da29
sunnylqm <sunnylqm@gmail.com>
2022-06-13 20:23:02 +0800

    feat: backdrop press to close modal

--- packages/components/src/Modal/Container/Desktop.tsx
+++ packages/components/src/Modal/Container/Desktop.tsx
@@ -8,6 +8,7 @@ import Box from '../../Box';
 import Button from '../../Button';
 import HStack from '../../HStack';
 import IconButton from '../../IconButton';
+import Pressable from '../../Pressable';
 import Typography from '../../Typography';
 
 import useModalClose from './useModalClose';
@@ -72,6 +73,18 @@ const DesktopModal: FC<ModalProps> = ({
       alignItems="center"
       bg="rgba(0, 0, 0, 0.6)"
     >
+      <Pressable
+        _web={{
+          // @ts-ignore
+          cursor: 'default',
+        }}
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        onPress={close}
+      />
       <Box
         width={modalSizing(size)}
         height={height}
@@ -79,6 +92,7 @@ const DesktopModal: FC<ModalProps> = ({
         alignSelf="center"
         borderRadius="24px"
         bg="surface-subdued"
+        zIndex={1}
       >
         {!!headerShown && (
           <Box
