33c2f6e30ed0c2cf98b732d97456dbeb6bbbdfa1
linleiqin <278438611@qq.com>
2022-06-17 12:13:28 +0800

    OK-9950: add AutoSizeText placeholder (#905)

--- packages/kit/src/views/FiatPay/AmountInput/AutoSizeText.web.tsx
+++ packages/kit/src/views/FiatPay/AmountInput/AutoSizeText.web.tsx
@@ -1,29 +1,25 @@
 import React, { FC } from 'react';
 
-import { useIntl } from 'react-intl';
-
 import { Input } from '@onekeyhq/components';
 
 export const AutoSizeText: FC<{
   text: string;
+  placeholder?: string;
   onChangeText?: (text: string) => void;
-}> = ({ text, onChangeText }) => {
-  const intl = useIntl();
-  return (
-    <Input
-      flex={1}
-      height="112"
-      size="xl"
-      textAlign="center"
-      borderWidth="0"
-      fontSize="40px"
-      placeholder={intl.formatMessage({ id: 'content__amount' })}
-      placeholderTextColor="text-disabled"
-      lineHeight="64px"
-      fontWeight="bold"
-      bgColor="surface-subdued"
-      onChangeText={onChangeText}
-      value={text}
-    />
-  );
-};
+}> = ({ text, onChangeText, placeholder }) => (
+  <Input
+    flex={1}
+    height="112"
+    size="xl"
+    textAlign="center"
+    borderWidth="0"
+    fontSize="40px"
+    placeholder={placeholder}
+    placeholderTextColor="text-disabled"
+    lineHeight="64px"
+    fontWeight="bold"
+    bgColor="surface-subdued"
+    onChangeText={onChangeText}
+    value={text}
+  />
+);
