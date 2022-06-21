5cd8573c917641bcf98c7d5c4f6b90ec9a88a3e9
sunnylqm <sunnylqm@gmail.com>
2022-06-21 21:51:32 +0800

    chore: remove remote-console

--- packages/kit/src/provider/index.tsx
+++ packages/kit/src/provider/index.tsx
@@ -7,39 +7,35 @@ import { SWRConfig } from 'swr';
 
 import { ErrorBoundary } from '@onekeyhq/kit/src/components/ErrorBoundary';
 import store from '@onekeyhq/kit/src/store';
-import useRemoteConsole from '@onekeyhq/remote-console/src/useRemoteConsole';
 
 import AppLoading from './AppLoading';
 import NavigationApp from './NavigationProvider';
 import ThemeApp from './ThemeProvider';
 
 // TODO: detect network change & APP in background mode
-const KitProvider: FC = () => {
-  useRemoteConsole();
-  return (
-    <SWRConfig
-      value={{
-        refreshInterval: 0,
-        fetcher: async (resource, init) => {
-          const result = await axios(resource, init);
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-          return result.data;
-        },
-      }}
-    >
-      <ReduxProvider store={store}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <ThemeApp>
-            <AppLoading>
-              <ErrorBoundary>
-                <NavigationApp />
-              </ErrorBoundary>
-            </AppLoading>
-          </ThemeApp>
-        </GestureHandlerRootView>
-      </ReduxProvider>
-    </SWRConfig>
-  );
-};
+const KitProvider: FC = () => (
+  <SWRConfig
+    value={{
+      refreshInterval: 0,
+      fetcher: async (resource, init) => {
+        const result = await axios(resource, init);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return result.data;
+      },
+    }}
+  >
+    <ReduxProvider store={store}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <ThemeApp>
+          <AppLoading>
+            <ErrorBoundary>
+              <NavigationApp />
+            </ErrorBoundary>
+          </AppLoading>
+        </ThemeApp>
+      </GestureHandlerRootView>
+    </ReduxProvider>
+  </SWRConfig>
+);
 
 export default KitProvider;
