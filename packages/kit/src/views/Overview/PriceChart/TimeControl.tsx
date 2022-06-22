02fd9a084d2967bf358c70e21fa45687418974df
sunnylqm <sunnylqm@gmail.com>
2022-06-22 18:01:53 +0800

    fix: lint error

--- packages/kit/src/views/Overview/PriceChart/TimeControl.tsx
+++ packages/kit/src/views/Overview/PriceChart/TimeControl.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { useIntl } from 'react-intl';
-
 import { Box, SegmentedControl } from '@onekeyhq/components';
 
 export type TimeOptions = '1D' | '1W' | '1M' | '1Y' | 'All';
@@ -11,38 +9,35 @@ type TimeControlProps = {
   onTimeChange(time: string): void;
 };
 
-const TimeControl: React.FC<TimeControlProps> = ({ time, onTimeChange }) => {
-  const intl = useIntl();
-  return (
-    <Box>
-      <SegmentedControl
-        options={[
-          {
-            label: '1D',
-            value: '1D',
-          },
-          {
-            label: '1W',
-            value: '1W',
-          },
-          {
-            label: '1M',
-            value: '1M',
-          },
-          {
-            label: '1Y',
-            value: '1Y',
-          },
-          {
-            label: 'All',
-            value: 'All',
-          },
-        ]}
-        onChangeValue={onTimeChange}
-        defaultValue={time}
-      />
-    </Box>
-  );
-};
+const TimeControl: React.FC<TimeControlProps> = ({ time, onTimeChange }) => (
+  <Box>
+    <SegmentedControl
+      options={[
+        {
+          label: '1D',
+          value: '1D',
+        },
+        {
+          label: '1W',
+          value: '1W',
+        },
+        {
+          label: '1M',
+          value: '1M',
+        },
+        {
+          label: '1Y',
+          value: '1Y',
+        },
+        {
+          label: 'All',
+          value: 'All',
+        },
+      ]}
+      onChangeValue={onTimeChange}
+      defaultValue={time}
+    />
+  </Box>
+);
 TimeControl.displayName = 'TimeControl';
 export default TimeControl;
