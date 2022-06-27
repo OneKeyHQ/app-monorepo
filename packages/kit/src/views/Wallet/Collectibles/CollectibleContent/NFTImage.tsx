f91889001ea730509df5ce119ff5936778e24402
linleiqin <278438611@qq.com>
2022-06-27 20:33:16 +0800

    OK-10253: NFT SVG animation support. (#984)

--- packages/kit/src/views/Wallet/Collectibles/CollectibleContent/NFTImage.tsx
+++ packages/kit/src/views/Wallet/Collectibles/CollectibleContent/NFTImage.tsx
@@ -1,35 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { Box, Center, Icon, Image } from '@onekeyhq/components';
 
 import { NFTProps } from './type';
 
-const NFTImage: FC<NFTProps> = ({ asset, width, height }) => (
-  <Box size={width}>
-    <Image
-      flex="1"
-      alt={`image of ${typeof asset.name === 'string' ? asset.name : 'nft'}`}
-      height={width}
-      width={height}
+const NFTImage: FC<NFTProps> = ({ asset, width, height }) => {
+  const uri = useMemo(
+    () => asset.imageUrl ?? asset.imagePreviewUrl ?? asset.imageOriginalUrl,
+    [asset.imageOriginalUrl, asset.imagePreviewUrl, asset.imageUrl],
+  );
+
+  const fallbackElement = (
+    <Center
+      width={width}
+      height="333px"
+      bgColor="surface-default"
       borderRadius="20px"
-      src={
-        asset.imageUrl ??
-        asset.imagePreviewUrl ??
-        asset.imageOriginalUrl ??
-        undefined
-      }
-      fallbackElement={
-        <Center
-          width={width}
-          height="333px"
-          bgColor="surface-default"
+    >
+      <Icon name="QuestionMarkCircleOutline" size={166} />
+    </Center>
+  );
+  return (
+    <Box size={width}>
+      {uri ? (
+        <Image
+          flex="1"
+          alt={`image of ${
+            typeof asset.name === 'string' ? asset.name : 'nft'
+          }`}
+          height={width}
+          width={height}
           borderRadius="20px"
-        >
-          <Icon name="QuestionMarkCircleOutline" size={166} />
-        </Center>
-      }
-    />
-  </Box>
-);
+          src={uri}
+          fallbackElement={fallbackElement}
+        />
+      ) : (
+        fallbackElement
+      )}
+    </Box>
+  );
+};
 
 export default NFTImage;
