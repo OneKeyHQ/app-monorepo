109d2d92bd8bc46533f1bb5894bdb79fe3fbaa6c
qwang1113 <1432613064@qq.com>
2022-06-27 21:17:50 +0800

    fix: revert escape to close modal on desktop

--- packages/components/src/Modal/Container/Desktop.tsx
+++ packages/components/src/Modal/Container/Desktop.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import React, { isValidElement } from 'react';
+import React, { isValidElement, useEffect } from 'react';
 
 import { useNavigation, useNavigationState } from '@react-navigation/core';
 import { useIntl } from 'react-intl';
+
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import Box from '../../Box';
 import Button from '../../Button';
@@ -64,6 +66,20 @@ const DesktopModal = ({
   const defaultClose = useModalClose({ onClose });
 
   const close = closeAction || defaultClose;
+
+  useEffect(() => {
+    if (platformEnv.isRuntimeBrowser) {
+      const closeOnEsc = (e: KeyboardEvent) => {
+        if (e.code === 'Escape') {
+          close();
+        }
+      };
+      document.addEventListener('keydown', closeOnEsc);
+      return () => {
+        document.removeEventListener('keydown', closeOnEsc);
+      };
+    }
+  }, [close]);
 
   return (
     <Box
