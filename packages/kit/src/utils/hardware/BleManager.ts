758c1ac7c0cc46d052354333957c470d4f3386aa
Leon <lixiao.dev@gmail.com>
2022-06-28 18:12:45 +0800

    fix: Error initializing bleManager on desktop

--- packages/kit/src/utils/hardware/BleManager.ts
+++ packages/kit/src/utils/hardware/BleManager.ts
@@ -1,6 +1,10 @@
 import BleManager from 'react-native-ble-manager';
 
-BleManager.start({ showAlert: false });
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
+if (platformEnv.isNativeAndroid || platformEnv.isNativeIOS) {
+  BleManager.start({ showAlert: false });
+}
 
 export const getBondedDevices = async () => {
   const peripherals = await BleManager.getBondedPeripherals();
