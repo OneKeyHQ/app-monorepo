81b0a9e8b6d4c83b6b24446d39af5b394b4780c2
wk3368 <wk3368@163.com>
2022-07-20 21:26:14 +0800

    fix: /app-monorepo/node_modules/keytar/build/Release/keytar.node (mach-o file, but is an incompatible architecture (have 'x86_64', need 'arm64e')

--- packages/desktop/src-electron/preload.ts
+++ packages/desktop/src-electron/preload.ts
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 import logger from 'electron-log';
-import keytar from 'keytar';
+// import keytar from 'keytar';
 
 export type PrefType = 'camera' | 'bluetooth' | 'location';
 export type DesktopAPI = {
@@ -67,10 +67,12 @@ const desktopApi = {
       ipcRenderer.send('app/promptTouchID', msg);
     }),
   secureSetItemAsync(key: string, value: string) {
-    return keytar.setPassword('OneKey', key, value);
+    return Promise.resolve();
+    // return keytar.setPassword('OneKey', key, value);
   },
   secureGetItemAsync(key: string) {
-    return keytar.getPassword('OneKey', key);
+    return Promise.resolve(null);
+    // return keytar.getPassword('OneKey', key);
   },
   reloadBridgeProcess: () => {
     ipcRenderer.send('app/reloadBridgeProcess');
