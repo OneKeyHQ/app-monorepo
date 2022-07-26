e0464ebe96efbbe50952ef13c16f7c53038d0ec9
sunnylqm <sunnylqm@gmail.com>
2022-07-26 20:46:29 +0800

    fix: lint error

--- packages/kit/src/views/Wallet/index.tsx
+++ packages/kit/src/views/Wallet/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 
 import { useNavigation } from '@react-navigation/core';
 import { useIntl } from 'react-intl';
