24b5983dde82e0f386edcbbab3c390c5c1d2dda6
Franco <chenhuifan.gg@gmail.com>
2022-08-08 19:21:59 +0800

    style: setup password in onboarding

--- packages/kit/src/components/Protected/Setup.tsx
+++ packages/kit/src/components/Protected/Setup.tsx
@@ -80,7 +80,10 @@ const Setup: FC<SetupProps> = ({ onOk, skipSavePassword, hideTitle }) => {
       : intl.formatMessage({ id: 'content__touch_id' });
 
   return (
-    <KeyboardDismissView px={{ base: hideTitle ? 0 : 4, md: 0 }}>
+    <KeyboardDismissView
+      h={{ base: 'full', sm: 'auto' }}
+      px={{ base: hideTitle ? 0 : 4, md: 0 }}
+    >
       {!hideTitle ? (
         <Box mb="8">
           <Typography.DisplayLarge textAlign="center" mb={2}>
