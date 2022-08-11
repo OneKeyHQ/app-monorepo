1975be80e327128005cafa204a268ccb0bcade1e
linleiqin <278438611@qq.com>
2022-08-11 18:45:03 +0800

    OK-11606:fix NFT issue

--- packages/kit/src/views/Wallet/Collectibles/index.tsx
+++ packages/kit/src/views/Wallet/Collectibles/index.tsx
@@ -1,13 +1,10 @@
 import React, { useCallback } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
-import { useIntl } from 'react-intl';
 
-import { Box, Center, Empty, Spinner } from '@onekeyhq/components';
 import { isCollectibleSupportedChainId } from '@onekeyhq/engine/src/managers/nft';
 import { Network } from '@onekeyhq/engine/src/types/network';
 import type { Collection, NFTAsset } from '@onekeyhq/engine/src/types/nft';
-import IconNFT from '@onekeyhq/kit/assets/3d_nft.png';
 import {
   CollectiblesModalRoutes,
   CollectiblesRoutesParams,
@@ -19,7 +16,6 @@ import {
 } from '@onekeyhq/kit/src/routes/types';
 
 import CollectibleGallery from './CollectibleGallery';
-// import { collectibles } from './mockData';
 import { useCollectiblesData } from './useCollectiblesData';
 
 type NavigationProps = ModalScreenProps<CollectiblesRoutesParams>;
@@ -32,14 +28,12 @@ export type CollectiblesProps = {
 function CollectibleListView({ address, network }: CollectiblesProps) {
   const navigation = useNavigation<NavigationProps['navigation']>();
   const isCollectibleSupported = isCollectibleSupportedChainId(network?.id);
-  const intl = useIntl();
 
   const { collectibles, isLoading, fetchData } = useCollectiblesData({
     network,
     address,
     isCollectibleSupported,
   });
-  // console.log('collectibles = ', collectibles);
 
   const handleSelectAsset = useCallback(
     (asset: NFTAsset) => {
@@ -77,48 +71,14 @@ function CollectibleListView({ address, network }: CollectiblesProps) {
     [address, navigation, network],
   );
 
-  if (!isCollectibleSupported) {
-    return (
-      <Box py={4}>
-        <Empty
-          imageUrl={IconNFT}
-          title={intl.formatMessage({ id: 'empty__not_supported' })}
-          subTitle={intl.formatMessage({ id: 'empty__not_supported_desc' })}
-        />
-      </Box>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <Center pb={8} pt={8}>
-        <Spinner size="lg" />
-      </Center>
-    );
-  }
-  if (collectibles.length === 0) {
-    return (
-      <Box py={4}>
-        <Empty
-          imageUrl={IconNFT}
-          title={intl.formatMessage({
-            id: 'asset__collectibles_empty_title',
-          })}
-          subTitle={intl.formatMessage({
-            id: 'asset__collectibles_empty_desc',
-          })}
-          actionTitle={intl.formatMessage({ id: 'action__refresh' })}
-          handleAction={fetchData}
-        />
-      </Box>
-    );
-  }
-
   return (
     <CollectibleGallery
       collectibles={collectibles}
       onSelectCollection={handleSelectCollectible}
       onSelectAsset={handleSelectAsset}
+      fetchData={fetchData}
+      isCollectibleSupported={isCollectibleSupported}
+      isLoading={isLoading}
     />
   );
 }
--- packages/kit/src/views/Wallet/Collectibles/types.ts
+++ packages/kit/src/views/Wallet/Collectibles/types.ts
@@ -5,4 +5,7 @@ export type CollectibleGalleryProps = {
   collectibles: Collection[];
   onSelectCollection?: (cols: Collection) => void;
   onSelectAsset?: (asset: NFTAsset) => void;
+  fetchData?: () => void;
+  isCollectibleSupported?: boolean;
+  isLoading?: boolean;
 };
