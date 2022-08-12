344ab113252d10fe34ec259a42f3dfa17e276ca1
taimanhui <73929028+taimanhui@users.noreply.github.com>
2022-08-12 17:29:51 +0800

    feat: second confirmation uption deleting HD wallet (#1301)
    Fixes: OK-11437

--- packages/kit/src/views/ManagerWallet/DeleteWallet/index.tsx
+++ packages/kit/src/views/ManagerWallet/DeleteWallet/index.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 import { useIntl } from 'react-intl';
 
 import backgroundApiProxy from '@onekeyhq//kit/src/background/instance/backgroundApiProxy';
-import { Dialog, useToast } from '@onekeyhq/components';
+import { CheckBox, Dialog, useToast } from '@onekeyhq/components';
 import { OnCloseCallback } from '@onekeyhq/components/src/Dialog/components/FooterButton';
 
 export type DeleteWalletProp = {
@@ -29,9 +29,12 @@ const ManagerWalletDeleteDialog: FC<ManagerWalletDeleteDialogProps> = ({
 
   const { walletId, password, hardware } = deleteWallet ?? {};
   const [isLoading, setIsLoading] = React.useState(false);
+  // If a hardware wallet is being deleted, no second confirmation is required.
+  const [confirmed, setConfirmed] = React.useState(hardware ?? false);
 
   return (
     <Dialog
+      hasFormInsideDialog
       visible={visible}
       canceledOnTouchOutside={false}
       onClose={() => onDialogClose?.()}
@@ -45,12 +48,31 @@ const ManagerWalletDeleteDialog: FC<ManagerWalletDeleteDialogProps> = ({
             ? 'dialog__delete_hardware_wallet_desc'
             : 'dialog__delete_wallet_desc',
         }),
+        input: hardware ? undefined : (
+          <CheckBox
+            w="full"
+            mt="4"
+            isChecked={confirmed}
+            defaultIsChecked={false}
+            onChange={(checked) => {
+              setConfirmed(checked);
+            }}
+            title={intl.formatMessage({
+              id: 'checkbox__i_have_written_down_phrase',
+            })}
+            description={intl.formatMessage({
+              id: 'checkbox__i_have_written_down_phrase_desc',
+            })}
+          />
+        ),
       }}
       footerButtonProps={{
         primaryActionProps: {
           type: 'destructive',
           children: intl.formatMessage({ id: 'action__remove' }),
           isLoading,
+          disabled: !hardware && !confirmed,
+          isDisabled: !hardware && !confirmed,
         },
         onPrimaryActionPress: ({ onClose }: OnCloseCallback) => {
           if (!walletId) return;
