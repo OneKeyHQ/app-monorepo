d5ed9a1dce3ab6646509edc7b525048eae942ba5
sunnylqm <sunnylqm@gmail.com>
2022-08-12 23:01:48 +0800

    fix: toast not show

--- packages/kit/src/routes/PortalElementsContainer.tsx
+++ packages/kit/src/routes/PortalElementsContainer.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 import { RootSiblingParent } from 'react-native-root-siblings';
 
-import Toast from '@onekeyhq/components/src/Toast/Custom';
-
 import HardwarePopup from '../views/Hardware/PopupHandle';
 
 export const PortalElementsContainer = () => (
   <RootSiblingParent>
-    <Toast bottomOffset={60} />
     <HardwarePopup />
   </RootSiblingParent>
 );
