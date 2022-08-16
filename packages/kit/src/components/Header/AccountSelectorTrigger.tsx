738a63d90fc17a8420f4fb9cf4c4601613769415
ByteZhang1024 <ByteZhang@protonmail.com>
2022-08-16 22:35:29 +0800

    fix: Switch to software wallet marker status error

--- packages/kit/src/components/Header/AccountSelectorTrigger.tsx
+++ packages/kit/src/components/Header/AccountSelectorTrigger.tsx
@@ -62,6 +62,9 @@ const AccountSelectorTrigger: FC<Props> = ({
           setNotificationColor('icon-warning');
         }
       });
+    } else {
+      setHasNotification(false);
+      setNotificationColor(undefined);
     }
   }, [connected, deviceUpdates, engine, wallet]);
 
