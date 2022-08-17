b3c068b5918ecd14146a602879be9ba245a45391
sunnylqm <sunnylqm@gmail.com>
2022-08-17 13:27:39 +0800

    fix: OK-11763

--- packages/kit/src/views/FullTokenList/FullTokenList.tsx
+++ packages/kit/src/views/FullTokenList/FullTokenList.tsx
@@ -2,6 +2,8 @@ import { FC, useEffect } from 'react';
 
 import { useIntl } from 'react-intl';
 
+import { Box, useSafeAreaInsets } from '@onekeyhq/components';
+
 import { useNavigation } from '../../hooks';
 import { HomeRoutes } from '../../routes/types';
 import AssetsList from '../Wallet/AssetsList';
@@ -20,6 +22,7 @@ export type FullTokenListProps = NativeStackScreenProps<
 const FullTokenList: FC<FullTokenListProps> = () => {
   const navigation = useNavigation();
   const intl = useIntl();
+  const { bottom } = useSafeAreaInsets();
   useEffect(() => {
     const title = intl.formatMessage({ id: 'asset__tokens' });
     navigation.setOptions({
@@ -27,6 +30,6 @@ const FullTokenList: FC<FullTokenListProps> = () => {
     });
   }, [navigation, intl]);
 
-  return <AssetsList flatStyle />;
+  return <AssetsList flatStyle ListFooterComponent={<Box h={12 + bottom} />} />;
 };
 export default FullTokenList;
