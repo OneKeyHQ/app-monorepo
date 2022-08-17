0423e116b95f122a1798b8421bce2a7495a9d7d2
sunnylqm <sunnylqm@gmail.com>
2022-08-17 17:02:53 +0800

    feat: add apiproxyutils

---
+++ packages/engine/src/vaults/utils/apiProxyUtils.ts
@@ -0,0 +1,25 @@
+export const balanceSupprtedNetwork = {
+  // alchemy
+  // arbitrum: 'arb',
+  // eth: 'eth',
+  // optimism: 'opt',
+  // polygon: 'polygon',
+  'evm--42161': 'arbitrum',
+  'evm--1': 'eth',
+  'evm--10': 'optimism',
+  'evm--137': 'polygon',
+
+  // moralis
+  // avalanche: 'avalanche',
+  // eth: 'eth',
+  // polygon: 'polygon',
+  // cronos: 'cronos',
+  // fantom: 'fantom',
+  // bsc: 'bsc',
+  // tbsc: 'bsc testnet',
+  'evm--43114': 'avalanche',
+  'evm--25': 'cronos',
+  'evm--250': 'fantom',
+  'evm--56': 'bsc',
+  'evm--97': 'tbsc',
+};
