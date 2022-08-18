3c3fbef4cbb09c0d83adbff01e7fb9c2bc195905
linleiqin <278438611@qq.com>
2022-08-18 23:44:55 +0800

    OK-11547 OK-11531 feat:show balance in recover account list (#1354)

--- packages/kit/src/views/Account/AddNewAccount/RecoverAccounts.tsx
+++ packages/kit/src/views/Account/AddNewAccount/RecoverAccounts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React, {
   ComponentProps,
   FC,
@@ -11,18 +12,24 @@ import React, {
 import { HardwareErrorCode } from '@onekeyfe/hd-shared';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { useIntl } from 'react-intl';
-import { ListRenderItem } from 'react-native';
 
 import {
   Box,
   Center,
   CheckBox,
+  DataProvider,
   Divider,
   Empty,
+  LayoutProvider,
   Modal,
+  Pressable,
+  RecyclerListView,
   Spinner,
+  ToastManager,
   Typography,
+  useThemeValue,
 } from '@onekeyhq/components';
+import { shortenAddress } from '@onekeyhq/components/src/utils';
 import type {
   Account,
   ImportableHDAccount,
@@ -35,7 +42,9 @@ import {
   CreateAccountRoutesParams,
 } from '@onekeyhq/kit/src/routes';
 import { ModalScreenProps } from '@onekeyhq/kit/src/routes/types';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
+import { FormatBalance } from '../../../components/Format';
 import { deviceUtils } from '../../../utils/hardware';
 
 type NavigationProps = ModalScreenProps<CreateAccountRoutesParams>;
@@ -53,65 +62,86 @@ type FlatDataType = ImportableHDAccount & {
 type CellProps = {
   item: FlatDataType;
   index: number;
+  decimal: number;
+  symbol: string;
   onChange: (v: boolean, callback: (success: boolean) => void) => void;
 } & ComponentProps<typeof Box>;
 
-const CustomCell: FC<CellProps> = ({ item, index, onChange }) => {
+const AccountCell: FC<CellProps> = ({
+  item,
+  index,
+  onChange,
+  decimal,
+  symbol,
+}) => {
   const [isChecked, setIsCheck] = useState(item.selected);
+
+  const onPress = useCallback(() => {
+    if (!item.isDisabled && onChange) {
+      onChange(!isChecked, (success) => {
+        if (success) {
+          setIsCheck(!isChecked);
+        }
+      });
+    }
+  }, [isChecked, item.isDisabled, onChange]);
+
   return (
-    <Box
-      key={item.path}
+    <Pressable
+      onPress={platformEnv.isNative ? onPress : undefined}
       flexDirection="row"
-      height="100px"
+      height="72px"
       bgColor="surface-default"
-      justifyContent="space-between"
       borderTopRadius={index === 0 ? '12px' : 0}
+      padding="16px"
+      flex={1}
     >
-      <Box
-        flexDirection="column"
-        flex={1}
-        justifyContent="center"
-        paddingLeft="16px"
-      >
-        <Typography.Body1Strong>{item.displayAddress}</Typography.Body1Strong>
-        <Typography.Body2 color="text-subdued">{item.path}</Typography.Body2>
-      </Box>
-      <Box width="60px" height="100%" justifyContent="center" pl="24px">
-        <CheckBox
-          isChecked={isChecked}
-          isDisabled={item.isDisabled}
-          onChange={(isSelected) => {
-            if (!item.isDisabled && onChange) {
-              onChange(isSelected, (success) => {
-                if (success) {
-                  setIsCheck(isSelected);
-                }
-              });
-            }
+      <CheckBox
+        isChecked={isChecked}
+        isDisabled={item.isDisabled}
+        onChange={onPress}
+      />
+      <Box flexDirection="row" justifyContent="space-between" flex={1}>
+        <Box flexDirection="column" justifyContent="center" paddingX="16px">
+          <Typography.Body2>
+            {shortenAddress(item.displayAddress)}
+          </Typography.Body2>
+          <Typography.Body2 color="text-subdued">{item.path}</Typography.Body2>
+        </Box>
+        <FormatBalance
+          balance={item.mainBalance}
+          suffix={symbol}
+          formatOptions={{
+            fixed: decimal ?? 4,
           }}
+          render={(ele) => <Typography.Body2>{ele}</Typography.Body2>}
         />
       </Box>
-      <Box />
-    </Box>
+    </Pressable>
   );
 };
 
-type PageStatusType = 'loading' | 'empty' | 'data';
 const RecoverAccounts: FC = () => {
   const intl = useIntl();
   const route = useRoute<RouteProps>();
   const { password, walletId, network, purpose, onLoadingAccount } =
     route.params;
+  const bgColor = useThemeValue('surface-default');
+
   const [currentPage, setCurrentPage] = useState(0);
   const [searchEnded, setSearchEnded] = useState(false);
-  const [pageStatus, setPageStatus] = useState<PageStatusType>('loading');
   const navigation = useNavigation<NavigationProps['navigation']>();
 
   const [flatListData, updateFlatListData] = useState<FlatDataType[]>([]);
-  const { wallets } = useRuntime();
+  const { wallets, networks } = useRuntime();
+  const [pageWidth, setPageWidth] = useState<number>(0);
+  const selectedNetWork = networks.filter((n) => n.id === network)[0];
+  const decimal = selectedNetWork?.nativeDisplayDecimals;
+  const symbol = selectedNetWork?.symbol;
 
   const wallet = wallets.find((w) => w.id === walletId) ?? null;
 
+  const [isLoading, setLoading] = useState(true);
   const [isValid, setIsValid] = useState(false);
   const isFetchingData = useRef(false);
   const activeAccounts = useRef<Account[]>([]);
@@ -144,7 +174,7 @@ const RecoverAccounts: FC = () => {
         .searchHDAccounts(walletId, network, password, start, limit, purpose)
         .then((accounts) => {
           if (currentPage === 0) {
-            setPageStatus(accounts.length > 0 ? 'data' : 'empty');
+            setLoading(false);
           }
           // For BIP-44 compliance, if number of accounts is less than that we
           // required, stop searching for more accounts.
@@ -193,18 +223,41 @@ const RecoverAccounts: FC = () => {
     [flatListData],
   );
 
-  const renderItem: ListRenderItem<FlatDataType> = useCallback(
-    ({ item, index }) => (
-      <CustomCell
-        item={item}
-        index={index}
-        borderBottomRadius={index === flatListData.length - 1 ? '12px' : 0}
-        onChange={(isSelected, callback) => {
-          callback(checkBoxOnChange(isSelected, item));
-        }}
-      />
+  const dataProvider = useMemo(
+    () => new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(flatListData),
+    [flatListData],
+  );
+
+  const layoutProvider = useMemo(
+    () =>
+      new LayoutProvider(
+        () => 'item',
+        (_, dim) => {
+          dim.width = pageWidth - 32;
+          dim.height = 73;
+        },
+      ),
+    [pageWidth],
+  );
+
+  const rowRenderer = useCallback(
+    (type, item, index: number) => (
+      <Box flex={1}>
+        <AccountCell
+          symbol={symbol}
+          decimal={decimal}
+          flex={1}
+          item={item}
+          index={index}
+          onChange={(isSelected, callback) => {
+            callback(checkBoxOnChange(isSelected, item));
+          }}
+        />
+        <Divider />
+      </Box>
     ),
-    [checkBoxOnChange, flatListData.length],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [checkBoxOnChange],
   );
 
   const pageSize = 10;
@@ -243,35 +296,6 @@ const RecoverAccounts: FC = () => {
     [],
   );
 
-  const flatlistProps = useMemo(
-    () =>
-      pageStatus === 'data'
-        ? {
-            height: '640px',
-            data: flatListData,
-            // @ts-ignore
-            renderItem,
-            ItemSeparatorComponent: () => <Divider />,
-            keyExtractor: (item: ImportableHDAccount) => item.path,
-            ListFooterComponent: searchEnded
-              ? undefined
-              : () => (
-                  <Box pt="20px">
-                    <Spinner size="sm" />
-                  </Box>
-                ),
-            onEndReached: () => {
-              /**
-               * Prevent duplicate loading to cause hardware error
-               */
-              if (isFetchingData.current) return;
-              setCurrentPage((p) => p + 1);
-            },
-          }
-        : undefined,
-    [flatListData, pageStatus, renderItem, searchEnded],
-  );
-
   return (
     <Modal
       height="640px"
@@ -279,11 +303,6 @@ const RecoverAccounts: FC = () => {
       headerDescription={`${intl.formatMessage({
         id: 'account__recover_Step_1_of_2',
       })}`}
-      onBackActionPress={() => {
-        navigation.navigate(CreateAccountModalRoutes.CreateAccountForm, {
-          walletId,
-        });
-      }}
       primaryActionTranslationId="action__next"
       onPrimaryActionPress={() => {
         hardwareCancel();
@@ -299,27 +318,58 @@ const RecoverAccounts: FC = () => {
         isDisabled: !isValid,
       }}
       hideSecondaryAction
-      // @ts-ignore
-      flatListProps={flatlistProps}
-      mt="10px"
+      staticChildrenProps={{
+        flex: 1,
+        padding: '16px',
+        onLayout: (e) => {
+          if (pageWidth === 0) {
+            setPageWidth(e.nativeEvent.layout.width);
+          }
+        },
+      }}
     >
-      {pageStatus !== 'data' ? (
-        <Center h="full" w="full">
-          {pageStatus === 'empty' ? (
-            <Empty
-              imageUrl={IconAccount}
-              title={intl.formatMessage({
-                id: 'empty__no_account_title',
-              })}
-              subTitle={intl.formatMessage({
-                id: 'empty__no_recoverable_account_desc',
-              })}
-            />
-          ) : (
-            <Spinner size="lg" />
-          )}
+      {isLoading ? (
+        <Center flex={1}>
+          <Spinner size="lg" />
         </Center>
-      ) : null}
+      ) : flatListData.length === 0 ? (
+        <Center flex={1}>
+          <Empty
+            imageUrl={IconAccount}
+            title={intl.formatMessage({
+              id: 'empty__no_account_title',
+            })}
+            subTitle={intl.formatMessage({
+              id: 'empty__no_recoverable_account_desc',
+            })}
+          />
+        </Center>
+      ) : (
+        <RecyclerListView
+          flex={1}
+          style={{ borderRadius: 12, backgroundColor: bgColor }}
+          dataProvider={dataProvider}
+          layoutProvider={layoutProvider}
+          rowRenderer={rowRenderer}
+          disableRecycling
+          renderFooter={
+            searchEnded || currentPage === 0
+              ? undefined
+              : () => (
+                  <Box my="24px">
+                    <Spinner size="sm" />
+                  </Box>
+                )
+          }
+          onEndReached={() => {
+            /**
+             * Prevent duplicate loading to cause hardware error
+             */
+            if (isFetchingData.current) return;
+            setCurrentPage((p) => p + 1);
+          }}
+        />
+      )}
     </Modal>
   );
 };
---
+++ packages/kit/src/views/Account/AddNewAccount/SelectChain.tsx
@@ -0,0 +1,135 @@
+import React, { FC, useCallback, useMemo } from 'react';
+
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { useIntl } from 'react-intl';
+import { ListRenderItem } from 'react-native';
+
+import {
+  Badge,
+  Box,
+  Modal,
+  Pressable,
+  Token,
+  Typography,
+} from '@onekeyhq/components';
+import { Network } from '@onekeyhq/engine/src/types/network';
+import { useManageNetworks } from '@onekeyhq/kit/src/hooks';
+import { useRuntime } from '@onekeyhq/kit/src/hooks/redux';
+import {
+  CreateAccountModalRoutes,
+  CreateAccountRoutesParams,
+} from '@onekeyhq/kit/src/routes';
+import { ModalScreenProps } from '@onekeyhq/kit/src/routes/types';
+
+type NavigationProps = ModalScreenProps<CreateAccountRoutesParams>;
+
+type RouteProps = RouteProp<
+  CreateAccountRoutesParams,
+  CreateAccountModalRoutes.RecoverySelectChainList
+>;
+
+const RecoverSelectChainModal: FC = () => {
+  const intl = useIntl();
+
+  const route = useRoute<RouteProps>();
+  const { wallets } = useRuntime();
+  const { enabledNetworks: networks } = useManageNetworks();
+  const navigation = useNavigation<NavigationProps['navigation']>();
+
+  const { walletId: selectedWalletId, onLoadingAccount } = route.params;
+
+  const wallet = useMemo(
+    () => wallets.find((w) => w.id === selectedWalletId),
+    [selectedWalletId, wallets],
+  );
+
+  const selectableNetworks = useMemo(
+    () =>
+      networks.filter((network) => {
+        if (wallet?.type === 'hw') {
+          return network.settings.hardwareAccountEnabled;
+        }
+        if (wallet?.type === 'imported') {
+          return network.settings.importedAccountEnabled;
+        }
+        if (wallet?.type === 'watching') {
+          return network.settings.watchingAccountEnabled;
+        }
+        return true;
+      }),
+    [networks, wallet],
+  );
+
+  function getPurpose(network: Network) {
+    const { category } = network.accountNameInfo.default;
+    const purpose = parseInt(category.split("'/")[0]);
+    return purpose;
+  }
+
+  const onSelectChain = useCallback(
+    (network: Network) => {
+      navigation.navigate(
+        CreateAccountModalRoutes.CreateAccountAuthentication,
+        {
+          walletId: selectedWalletId,
+          onDone: (password) => {
+            const purpose = getPurpose(network);
+            navigation.replace(CreateAccountModalRoutes.RecoverAccountsList, {
+              purpose,
+              walletId: selectedWalletId,
+              network: network.id,
+              password,
+              onLoadingAccount,
+            });
+          },
+        },
+      );
+    },
+    [navigation, onLoadingAccount, selectedWalletId],
+  );
+
+  const renderItem: ListRenderItem<Network> = useCallback(
+    ({ item }) => (
+      <Pressable
+        height="56px"
+        alignItems="center"
+        flexDirection="row"
+        borderRadius="12px"
+        paddingX="16px"
+        _pressed={{ bg: 'surface-pressed' }}
+        _hover={{ bg: 'surface-hovered' }}
+        onPress={() => {
+          onSelectChain(item);
+        }}
+      >
+        <Token size="32px" src={item.logoURI} />
+        <Typography.Body1Strong ml="16px" mr="12px">
+          {item.shortName}
+        </Typography.Body1Strong>
+        {item.impl === 'evm' ? (
+          <Badge title="EVM" size="sm" type="default" />
+        ) : null}
+      </Pressable>
+    ),
+    [onSelectChain],
+  );
+
+  return (
+    <Modal
+      height="640px"
+      header={intl.formatMessage({ id: 'modal__select_chain' })}
+      footer={null}
+      flatListProps={{
+        style: { paddingLeft: 0, paddingRight: 0 },
+        data: selectableNetworks,
+        // @ts-ignore
+        renderItem,
+        ItemSeparatorComponent: () => <Box h="8px" />,
+        keyExtractor: (item) => (item as Network).id,
+        showsVerticalScrollIndicator: false,
+      }}
+    />
+  );
+};
+
+export default RecoverSelectChainModal;
