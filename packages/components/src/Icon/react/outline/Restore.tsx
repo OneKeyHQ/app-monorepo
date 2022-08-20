a825b71d7742a14978970e27c3020b092c0b84dc
Franco <chenhuifan.gg@gmail.com>
2022-08-20 22:43:22 +0800

    style: crunk of UI details (#1372)

--- packages/components/src/Icon/react/outline/Restore.tsx
+++ packages/components/src/Icon/react/outline/Restore.tsx
@@ -5,7 +5,7 @@ function SvgRestore(props: SvgProps) {
   return (
     <Svg viewBox="0 0 24 24" fill="none" {...props}>
       <Path
-        d="M4 8c1.473-2.963 4.466-5 8-5a9 9 0 110 18 9.003 9.003 0 01-8.777-7M4 8V3m0 5h5"
+        d="M4 13.04a8 8 0 10.5-4m-.5-5v5h5"
         stroke="currentColor"
         strokeWidth={2}
         strokeLinecap="round"
