7ff5b0e06b2df81aeb349d12665e4d9e63810ced
Franco <chenhuifan.gg@gmail.com>
2022-08-25 15:22:12 +0800

    style: fix icon color (#1409)

--- packages/components/src/Icon/react/outline/Umbrella.tsx
+++ packages/components/src/Icon/react/outline/Umbrella.tsx
@@ -3,10 +3,9 @@ import Svg, { SvgProps, Path } from 'react-native-svg';
 
 function SvgUmbrella(props: SvgProps) {
   return (
-    <Svg viewBox="0 0 20 23" fill="none" {...props}>
+    <Svg viewBox="0 0 20 23" fill="none" stroke="currentColor" {...props}>
       <Path
         d="M15 18V19.1c0 1.42-1.008 2.571-2.428 2.571A2.57 2.57 0 0110 19.098v-4.142m0-13.054a9 9 0 00-9 9h18a9 9 0 00-9-9z"
-        stroke="#8C8CA1"
         strokeWidth={2}
         strokeMiterlimit={10}
         strokeLinecap="round"
