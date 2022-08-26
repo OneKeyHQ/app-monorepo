a04a03d55020e3fe2034a04b550c450d74c0ec9e
ByteZhang <ByteZhang@protonmail.com>
2022-08-26 17:04:17 +0800

    fix: CheckBox color problem (#1424)

--- packages/components/src/Icon/react/solid/CheckBoxIconChecked.tsx
+++ packages/components/src/Icon/react/solid/CheckBoxIconChecked.tsx
@@ -6,7 +6,7 @@ function SvgCheckBoxIconChecked(props: SvgProps) {
     <Svg viewBox="0 0 16 16" fill="none" {...props}>
       <Path
         d="M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z"
-        fill="#fff"
+        fill="currentColor"
       />
     </Svg>
   );
--- packages/components/src/Icon/react/solid/CheckBoxIconDefaultDisable.tsx
+++ packages/components/src/Icon/react/solid/CheckBoxIconDefaultDisable.tsx
@@ -4,7 +4,7 @@ import Svg, { SvgProps, Rect } from 'react-native-svg';
 function SvgCheckBoxIconDefaultDisable(props: SvgProps) {
   return (
     <Svg viewBox="0 0 16 16" fill="none" {...props}>
-      <Rect x={4} y={7} width={8} height={2} rx={1} fill="#48485B" />
+      <Rect x={4} y={7} width={8} height={2} rx={1} fill="currentColor" />
     </Svg>
   );
 }
