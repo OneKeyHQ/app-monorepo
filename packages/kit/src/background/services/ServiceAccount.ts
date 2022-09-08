63d7f9ca1b401822b93cea4b80456f49dda6ef50
qwang1113 (王强) <15064266+qwang1113@users.noreply.github.com>
2022-09-08 22:44:12 +0800

    fix: remove account dynamic subscibe when remove wallets or accounts (#1513)

--- packages/kit/src/background/services/ServiceAccount.ts
+++ packages/kit/src/background/services/ServiceAccount.ts
@@ -714,8 +714,20 @@ class ServiceAccount extends ServiceBase {
 
   @backgroundMethod()
   async removeWallet(walletId: string, password: string | undefined) {
-    const { appSelector, engine, dispatch, serviceCloudBackup } =
-      this.backgroundApi;
+    const {
+      appSelector,
+      serviceNotification,
+      engine,
+      dispatch,
+      serviceCloudBackup,
+    } = this.backgroundApi;
+    const wallet = await engine.getWallet(walletId);
+    const accounts = await engine.getAccounts(wallet.accounts);
+    for (const account of accounts) {
+      await serviceNotification.removeAccountDynamic({
+        address: account.address,
+      });
+    }
     const activeWalletId = appSelector((s) => s.general.activeWalletId);
     await engine.removeWallet(walletId, password ?? '');
 
@@ -737,8 +749,22 @@ class ServiceAccount extends ServiceBase {
     accountId: string,
     password: string | undefined,
   ) {
-    const { appSelector, engine, dispatch, serviceCloudBackup } =
-      this.backgroundApi;
+    const {
+      appSelector,
+      engine,
+      dispatch,
+      serviceCloudBackup,
+      serviceNotification,
+    } = this.backgroundApi;
+    const account = await this.getAccount({
+      walletId,
+      accountId,
+    });
+    if (account) {
+      await serviceNotification.removeAccountDynamic({
+        address: account.address,
+      });
+    }
     const activeAccountId = appSelector((s) => s.general.activeAccountId);
     await engine.removeAccount(accountId, password ?? '');
     await simpleDb.walletConnect.removeAccount({ accountId });
@@ -823,6 +849,20 @@ class ServiceAccount extends ServiceBase {
         return target;
       }
     }
+  }
+
+  @backgroundMethod()
+  async getAccount({
+    walletId,
+    accountId,
+  }: {
+    walletId: string;
+    accountId: string;
+  }) {
+    const { engine } = this.backgroundApi;
+    const wallet = await engine.getWallet(walletId);
+    const accounts = await engine.getAccounts(wallet.accounts);
+    return accounts.find((item) => item.id === accountId);
   }
 }
 
