a9df68fa71bf719558ac945a2dab572c8ed22b42
morizon <sidmorizon@outlook.com>
2022-09-16 08:44:12 +0800

    fix: check is mounted in usePromiseResult

--- packages/kit/src/hooks/usePromiseResult.ts
+++ packages/kit/src/hooks/usePromiseResult.ts
@@ -2,26 +2,36 @@ import { useEffect, useRef, useState } from 'react';
 
 import { wait } from '../utils/helper';
 
+import { useIsMounted } from './useIsMounted';
+
 // TODO useSWR instead
 export function usePromiseResult<T>(
   method: (...args: any[]) => Promise<T>,
   deps: any[] = [],
-  { loadingDelay = 0 }: { loadingDelay?: number } = {},
+  {
+    loadingDelay = 0,
+    checkIsMounted,
+  }: { loadingDelay?: number; checkIsMounted?: boolean } = {},
 ) {
   const [result, setResult] = useState<T | undefined>();
   const [isLoading, setIsLoading] = useState(false);
   const methodRef = useRef<typeof method>();
+  const isMountedRef = useIsMounted();
   methodRef.current = method;
 
   useEffect(() => {
     (async function () {
+      const shouldSetState = () =>
+        (checkIsMounted && isMountedRef.current) || !checkIsMounted;
       try {
-        setIsLoading(true);
-        const r = await methodRef?.current?.();
-        setResult(r);
+        if (shouldSetState()) {
+          setIsLoading(true);
+          const r = await methodRef?.current?.();
+          if (shouldSetState()) setResult(r);
+        }
       } finally {
         await wait(loadingDelay);
-        setIsLoading(false);
+        if (shouldSetState()) setIsLoading(false);
       }
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
