61079f57ecadeb7c55747bdd2293af457661d3a6
qwang1113 (王强) <15064266+qwang1113@users.noreply.github.com>
2022-09-19 18:25:33 +0800

    fix: remove passphrase account subscriptions after restart (#1586)

--- packages/kit/src/background/services/serviceNotification.ts
+++ packages/kit/src/background/services/serviceNotification.ts
@@ -28,7 +28,6 @@ export default class ServiceNotification extends ServiceBase {
     if (!platformEnv.isNative) {
       return;
     }
-    this.syncLocalEnabledAccounts();
     setInterval(() => {
       this.syncLocalEnabledAccounts();
     }, 5 * 60 * 1000);
