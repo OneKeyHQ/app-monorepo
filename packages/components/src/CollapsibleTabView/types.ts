f8d96f496ea54de980202f3c0b4d0ac801c25c57
linleiqin <278438611@qq.com>
2022-09-26 11:45:33 +0800

    feat:ios home page setPage method (#1631)

--- packages/components/src/CollapsibleTabView/types.ts
+++ packages/components/src/CollapsibleTabView/types.ts
@@ -1,6 +1,9 @@
+import React from 'react';
+
 import { StyleProp, ViewStyle } from 'react-native';
 
 export type ContainerProps = {
+  ref?: React.RefObject<any>;
   disableRefresh?: boolean;
   refreshing?: boolean;
   headerHeight: number;
