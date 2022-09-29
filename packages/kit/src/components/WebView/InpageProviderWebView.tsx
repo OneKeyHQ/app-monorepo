86d80391e39d8dd01c76594d12579d2002f7c07e
morizon <sidmorizon@outlook.com>
2022-09-29 13:58:10 +0800

    fix: mobile ua for desktop webview

--- packages/kit/src/components/WebView/InpageProviderWebView.tsx
+++ packages/kit/src/components/WebView/InpageProviderWebView.tsx
@@ -232,10 +232,11 @@ const InpageProviderWebView: FC<InpageProviderWebViewProps> = forwardRef(
                 // @ts-expect-error
                 allowpopups={allowpopups ? 'true' : false}
                 useragent={
+                  // TODO move it to Developer Settings
                   // we can resize desktop to vertical only in DEV env currently
                   platformEnv.isDev && isVertical
-                    // sim mobile app UA
-                    ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+                    ? // sim mobile app UA
+                      'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
                     : undefined
                 }
               />
