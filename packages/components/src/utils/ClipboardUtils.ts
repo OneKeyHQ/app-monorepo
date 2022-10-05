ddf7a3601f8ed77ed85892d412952e046e51af74
sunnylqm <sunnylqm@gmail.com>
2022-10-05 22:53:46 +0800

    feat: allow android 6

--- packages/components/src/utils/ClipboardUtils.ts
+++ packages/components/src/utils/ClipboardUtils.ts
@@ -2,9 +2,7 @@ import * as Clipboard from 'expo-clipboard';
 
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
-export const copyToClipboard = (msg: string) => {
-  Clipboard.setString(msg);
-};
+export const copyToClipboard = Clipboard.setStringAsync;
 
 export const getClipboard = async () => {
   if (!platformEnv.canGetClipboard) {
