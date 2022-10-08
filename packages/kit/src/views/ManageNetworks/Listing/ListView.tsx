f240ce193de86f6c23a6f5a45b66dc104a2399dc
taimanhui <taimanhui@protonmail.com>
2022-10-08 14:49:42 +0800

    fix: wrap network name in network management views
    Fixes: OK-13147

--- packages/kit/src/views/ManageNetworks/Listing/ListView.tsx
+++ packages/kit/src/views/ManageNetworks/Listing/ListView.tsx
@@ -90,10 +90,10 @@ export const ListView: FC<ListViewProps> = ({ onPress }) => {
             >
               <NetworkIcon network={network} />
               <Text
+                flexShrink={1}
                 mr="3"
                 typography={{ sm: 'Body1Strong', md: 'Body2Strong' }}
-                numberOfLines={1}
-                maxW="48"
+                numberOfLines={2}
                 isTruncated
               >
                 {network.name}
