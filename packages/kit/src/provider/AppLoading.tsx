bbe690d807572c854a6bee7e20b69dbdbf4d099e
morizon <sidmorizon@outlook.com>
2022-10-17 18:59:16 +0800

    fix: lint

--- packages/kit/src/provider/AppLoading.tsx
+++ packages/kit/src/provider/AppLoading.tsx
@@ -14,6 +14,7 @@ import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import { fetchCurrencies } from '../views/FiatPay/Service';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { serviceApp, serviceCronJob } = backgroundApiProxy;
 
 const AppLoading: FC = ({ children }) => {
