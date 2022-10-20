98f1d1c5d82816dd65ccb147ce2b33a89734c191
morizon <sidmorizon@outlook.com>
2022-10-20 19:06:21 +0800

    fix: webview bugs & modal full size OK-13495 (#1805)
    * fix: lint
    
    * fix: modal container size to exact full screen

--- packages/kit/src/views/Discover/Explorer/Content/WebContent.tsx
+++ packages/kit/src/views/Discover/Explorer/Content/WebContent.tsx
@@ -15,16 +15,15 @@ import { useGotoSite } from '../Controller/useGotoSite';
 import { useWebController } from '../Controller/useWebController';
 import { webHandler, webviewRefs } from '../explorerUtils';
 
-// const aboutBlankUrl = '';
 const aboutBlankUrl = 'about:blank';
 
 const WebContent = React.memo((tab: WebTab) => {
   const { id } = tab;
+  const initialUrl = tab.url;
   const [navigationStateChangeEvent, setNavigationStateChangeEvent] =
     useState<WebViewNavigation>();
   const [localUrl, setLocalUrl] = useState(
-    aboutBlankUrl,
-    // platformEnv.isDesktop ? 'about:blank' : url,
+    platformEnv.isDesktop ? aboutBlankUrl : initialUrl,
   );
   const onSrcChange = useCallback((src: string) => {
     if (platformEnv.isDesktop) {
@@ -36,7 +35,6 @@ const WebContent = React.memo((tab: WebTab) => {
     }
   }, []);
 
-  const initialUrl = tab.url;
   const isInitUrlLoaded = useRef(false);
   const showHome = useMemo(() => {
     if (id === 'home' && webHandler === 'tabbedWebview') {
@@ -70,7 +68,7 @@ const WebContent = React.memo((tab: WebTab) => {
       const { dispatch } = backgroundApiProxy;
       if (ref && ref.innerRef) {
         if (webviewRefs[id] === ref) {
-          return;
+          // return;
         }
         webviewRefs[id] = ref;
         // TODO check if equal before dispatch
@@ -78,7 +76,7 @@ const WebContent = React.memo((tab: WebTab) => {
         loadInitialUrl();
       } else {
         if (!webviewRefs[id]) {
-          return;
+          // return;
         }
         delete webviewRefs[id];
         // TODO check if equal before dispatch
