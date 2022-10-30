c70dd91bf6071a99b0939e537761f4a6cae85781
Franco <chenhuifan.gg@gmail.com>
2022-10-30 10:28:29 +0800

    style: repleace logo (#1865)

--- packages/components/src/Icon/react/illus/BrandLogo.tsx
+++ packages/components/src/Icon/react/illus/BrandLogo.tsx
@@ -1,16 +1,27 @@
-import Svg, { SvgProps, Path } from 'react-native-svg';
+import Svg, { SvgProps, G, Path, Defs, ClipPath } from 'react-native-svg';
 const SvgBrandLogo = (props: SvgProps) => (
-  <Svg viewBox="0 0 27 27" fill="none" accessibilityRole="image" {...props}>
-    <Path
-      d="M13.459 19.272a2.331 2.331 0 1 0 0-4.663 2.331 2.331 0 0 0 0 4.663Z"
-      fill="#00B812"
-    />
-    <Path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M13.459 26.918c9.291 0 13.459-4.168 13.459-13.46C26.918 4.169 22.75 0 13.458 0 4.169 0 0 4.167 0 13.459c0 9.291 4.167 13.459 13.459 13.459ZM10.93 5.707h3.744v6.17h-2.322V7.693h-2.08l.658-1.986Zm2.528 15.504a4.27 4.27 0 1 0 0-8.54 4.27 4.27 0 0 0 0 8.54Z"
-      fill="#00B812"
-    />
+  <Svg fill="none" viewBox="0 0 27 27" accessibilityRole="image" {...props}>
+    <G clipPath="url(#a)">
+      <Path
+        d="M26.918 13.459c0 9.291-4.168 13.459-13.46 13.459C4.169 26.918 0 22.75 0 13.458 0 4.169 4.167 0 13.459 0c9.291 0 13.459 4.167 13.459 13.459Z"
+        fill="#44D62C"
+      />
+      <Path
+        d="M14.675 5.707H10.93l-.657 1.986h2.08v4.184h2.32v-6.17Z"
+        fill="#000"
+      />
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M17.729 16.94a4.27 4.27 0 1 1-8.54 0 4.27 4.27 0 0 1 8.54 0Zm-1.939 0a2.332 2.332 0 1 1-4.663 0 2.332 2.332 0 0 1 4.663 0Z"
+        fill="#000"
+      />
+    </G>
+    <Defs>
+      <ClipPath id="a">
+        <Path fill="#fff" d="M0 0h27v27H0z" />
+      </ClipPath>
+    </Defs>
   </Svg>
 );
 export default SvgBrandLogo;
