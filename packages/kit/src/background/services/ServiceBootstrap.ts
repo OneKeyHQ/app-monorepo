3075c40cec0d6e95a5c96ffc20dbfcc00ea35487
qwang1113 (王强) <15064266+qwang1113@users.noreply.github.com>
2022-10-31 18:42:50 +0800

    fix: switch rpc may faild (#1882)
    * fix: switch rpc may faild
    
    * fix: clean code

--- packages/kit/src/background/services/ServiceBootstrap.ts
+++ packages/kit/src/background/services/ServiceBootstrap.ts
@@ -9,7 +9,7 @@ import {
 import { updateAutoSwitchDefaultRpcAtVersion } from '../../store/reducers/status';
 import { backgroundClass, backgroundMethod } from '../decorators';
 
-import ServiceBase, { IServiceBaseProps } from './ServiceBase';
+import ServiceBase from './ServiceBase';
 
 const defaultNetworkRpcs: Record<string, string> = {
   'aptos--1': 'https://fullnode.mainnet.aptoslabs.com',
@@ -40,10 +40,6 @@ const defaultNetworkRpcs: Record<string, string> = {
 
 @backgroundClass()
 export default class ServiceBootstrap extends ServiceBase {
-  constructor(props: IServiceBaseProps) {
-    super(props);
-    this.switchDefaultRpcToOnekeyRpcNode();
-  }
   // eslint-disable-next-line
   @backgroundMethod()
   async checkShouldShowNotificationGuide(): Promise<boolean> {
@@ -76,9 +72,7 @@ export default class ServiceBootstrap extends ServiceBase {
 
   @backgroundMethod()
   async switchDefaultRpcToOnekeyRpcNode() {
-    const { serviceApp, appSelector, dispatch, serviceNetwork } =
-      this.backgroundApi;
-    await serviceApp.initApp();
+    const { appSelector, dispatch, serviceNetwork } = this.backgroundApi;
     const { networks } = appSelector((s) => s.runtime);
     const { autoSwitchDefaultRpcAtVersion, userSwitchedNetworkRpcFlag } =
       appSelector((s) => s.status);
