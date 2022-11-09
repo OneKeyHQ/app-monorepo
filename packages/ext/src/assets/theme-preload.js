81867fcf28b5c1d8f9229df332291eb32627f61a
morizon <sidmorizon@outlook.com>
2022-11-09 10:25:15 +0800

    fix: preload-theme closure

--- packages/ext/src/assets/theme-preload.js
+++ packages/ext/src/assets/theme-preload.js
@@ -1,7 +1,9 @@
-const theme = localStorage.getItem('ONEKEY_THEME_PRELOAD');
-if (theme === 'dark') {
-  document.documentElement.style.backgroundColor = 'rgb(19, 19, 27)';
-}
-if (theme === 'light' || theme === 'system') {
-  document.documentElement.style.backgroundColor = 'white';
-}
+(function () {
+  const theme = localStorage.getItem('ONEKEY_THEME_PRELOAD');
+  if (theme === 'dark') {
+    document.documentElement.style.backgroundColor = 'rgb(19, 19, 27)';
+  }
+  if (theme === 'light' || theme === 'system') {
+    document.documentElement.style.backgroundColor = 'white';
+  }
+})();
