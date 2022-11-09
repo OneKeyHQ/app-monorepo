091eaab0587bafd6fede872dcfb0ecc9ffc2254b
morizon <sidmorizon@outlook.com>
2022-11-09 10:31:34 +0800

    Fix/theme preload desktop (#1950)
    * fix: default bg color in dark theme
    
    * fix: solana retry times

--- packages/ext/src/assets/theme-preload.js
+++ packages/ext/src/assets/theme-preload.js
@@ -1,6 +1,6 @@
 const theme = localStorage.getItem('ONEKEY_THEME_PRELOAD');
 if (theme === 'dark') {
-  document.documentElement.style.backgroundColor = 'black';
+  document.documentElement.style.backgroundColor = 'rgb(19, 19, 27)';
 }
 if (theme === 'light' || theme === 'system') {
   document.documentElement.style.backgroundColor = 'white';
