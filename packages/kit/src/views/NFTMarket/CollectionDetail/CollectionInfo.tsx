d5c161d19ff7b8ff2e12977450b058d7c171d83d
Franco <chenhuifan.gg@gmail.com>
2022-11-14 14:54:33 +0800

    fix: collection details ui

--- packages/kit/src/views/NFTMarket/CollectionDetail/CollectionInfo.tsx
+++ packages/kit/src/views/NFTMarket/CollectionDetail/CollectionInfo.tsx
@@ -86,7 +86,7 @@ const CollectionInfo: FC<ComponentProps<typeof Box>> = ({ ...props }) => {
     <Box {...props}>
       <Box flexDirection="row">
         <CollectionLogo src={collection?.logoUrl} width="64px" height="64px" />
-        <Box ml={{ base: '16px', md: '24px' }} maxW="576px">
+        <Box ml={{ base: '16px', md: '24px' }} maxW="576px" flex={1}>
           <Text typography="PageHeading" mb="4px" numberOfLines={1}>
             {name || '–'}
           </Text>
