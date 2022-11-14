7d74df100aecabe23c87995335a94d6a8d6acaa2
morizon <sidmorizon@outlook.com>
2022-11-14 17:26:35 +0800

    fix: double check tx speed up

--- packages/engine/src/vaults/types.ts
+++ packages/engine/src/vaults/types.ts
@@ -419,6 +419,7 @@ export type IDecodedTx = {
   extraInfo: IDecodedTxExtraNear | IDecodedTxExtraBtc | null;
 
   encodedTx?: IEncodedTx;
+  encodedTxEncrypted?: string;
   payload?: any;
 
   tokenIdOnNetwork?: string; // indicates this tx belongs to which token
