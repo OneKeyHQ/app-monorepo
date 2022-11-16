eac1122fc0c2cbead4a02ca4364dd770d292749f
sunnylqm <sunnylqm@gmail.com>
2022-11-16 11:00:32 +0800

    fix: sui.js transpile

---
+++ development/webpackTranspiles.js
@@ -0,0 +1,13 @@
+const webModuleTranspile = ['moti', '@gorhom', '@mysten/sui.js'];
+
+const extModuleTranspile = [
+  'react-native-animated-splash-screen',
+  'moti',
+  'popmotion',
+  '@mysten/sui.js',
+];
+
+module.exports = {
+  webModuleTranspile,
+  extModuleTranspile,
+};
