4560bca969268a8101a081b133b87ddf9eff175c
morizon <sidmorizon@outlook.com>
2022-11-18 22:45:32 +0800

    fix: ext theme bug

--- packages/kit/src/provider/ThemeProvider.tsx
+++ packages/kit/src/provider/ThemeProvider.tsx
@@ -1,12 +1,17 @@
-import React, { ComponentProps, FC, memo, useEffect } from 'react';
+import React, { ComponentProps, FC, memo, useEffect, useState } from 'react';
 
 import { Provider } from '@onekeyhq/components';
-import { useSettings } from '@onekeyhq/kit/src/hooks/redux';
+import { useAppSelector, useSettings } from '@onekeyhq/kit/src/hooks/redux';
 import { useColorScheme } from '@onekeyhq/kit/src/hooks/useColorScheme';
 import { setThemePreloadToLocalStorage } from '@onekeyhq/kit/src/store/reducers/settings';
 import { defaultHapticStatus } from '@onekeyhq/shared/src/haptics';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
+import backgroundApiProxy from '../background/instance/backgroundApiProxy';
+import { waitForDataLoaded } from '../background/utils';
 import { useSystemLocale } from '../hooks/useSystemLocale';
+import store from '../store';
+import { setIsReduxReady } from '../store/reducers/data';
 
 export function useThemeProviderVariant() {
   const { theme, locale, enableHaptics = defaultHapticStatus } = useSettings();
@@ -23,13 +28,57 @@ export function useThemeProviderVariant() {
   };
 }
 
+function useReduxSyncReady() {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    (async () => {
+      await waitForDataLoaded({
+        logName: 'WaitBackgroundReady @ ThemeApp',
+        wait: 300,
+        data: async () => {
+          const result = await backgroundApiProxy.getState();
+          if (result && result.bootstrapped) {
+            if (platformEnv.isExtensionUi) {
+              store.dispatch({
+                // TODO use consts
+                type: 'REPLACE_WHOLE_STATE',
+                payload: result.state,
+                $isDispatchFromBackground: true,
+              });
+            }
+            setTimeout(() => {
+              store.dispatch({
+                ...setIsReduxReady(),
+                $isDispatchFromBackground: true,
+              });
+            });
+            return true;
+          }
+          return false;
+        },
+      });
+      setIsReady(true);
+    })();
+  }, []);
+  return {
+    isReady,
+  };
+}
+
 const ThemeApp: FC = ({ children }) => {
   const { themeVariant, localeVariant, enableHaptics } =
     useThemeProviderVariant();
-
+  const isReduxReady = useAppSelector((s) => s.data.isReduxReady);
+  const { isReady } = useReduxSyncReady();
   useEffect(() => {
-    setThemePreloadToLocalStorage(themeVariant);
-  }, [themeVariant]);
+    if (isReduxReady && isReady) {
+      setThemePreloadToLocalStorage(themeVariant);
+    }
+  }, [isReady, isReduxReady, themeVariant]);
+
+  if (!isReduxReady || !isReady) {
+    return null;
+  }
 
   return (
     <Provider
