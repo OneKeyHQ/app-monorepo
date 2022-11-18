5c4a85fe54d71864e6eba24af8b262dbe1515905
Franco <chenhuifan.gg@gmail.com>
2022-11-18 16:40:32 +0800

    fix: restore onboarding welcome entry

--- packages/kit/src/components/WalletSelector/WalletSelectorChildren/Header.tsx
+++ packages/kit/src/components/WalletSelector/WalletSelectorChildren/Header.tsx
@@ -1,31 +1,50 @@
 import React, { FC } from 'react';
 
-import { Box, Text } from '@onekeyhq/components';
+import { Box, IconButton, Text } from '@onekeyhq/components';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
+import { useNavigationActions } from '../../../hooks';
+import useAppNavigation from '../../../hooks/useAppNavigation';
+import { RootRoutes } from '../../../routes/routesEnum';
 
 type HeaderProps = {
   title?: string;
 };
 
-const Header: FC<HeaderProps> = ({ title }) => (
-  <>
-    <Box
-      flexDirection="row"
-      alignItems="center"
-      py={platformEnv.isNative ? 4 : 3}
-      pr="15px"
-      pl={4}
-    >
-      <Text
-        flex={1}
-        typography={platformEnv.isNative ? 'PageHeading' : 'Heading'}
-        mr={3}
-        isTruncated
+const Header: FC<HeaderProps> = ({ title }) => {
+  const navigation = useAppNavigation();
+  const { closeWalletSelector } = useNavigationActions();
+
+  return (
+    <>
+      <Box
+        flexDirection="row"
+        alignItems="center"
+        py={platformEnv.isNative ? 4 : 3}
+        pr="15px"
+        pl={4}
       >
-        {title}
-      </Text>
-    </Box>
-  </>
-);
+        <Text
+          flex={1}
+          typography={platformEnv.isNative ? 'PageHeading' : 'Heading'}
+          mr={3}
+          isTruncated
+        >
+          {title}
+        </Text>
+        <IconButton
+          circle
+          name="PlusSolid"
+          onPress={() => {
+            navigation.navigate(RootRoutes.Onboarding);
+            setTimeout(() => {
+              closeWalletSelector();
+            }, 300);
+          }}
+        />
+      </Box>
+    </>
+  );
+};
 
 export default Header;
