c594db347894199c0c620a953eaecbfcaaab84d6
kwoktung <onlyif2009@gmail.com>
2022-11-21 17:28:13 +0800

    feat: limit discover search dapps

--- packages/kit/src/background/services/ServiceSetting.ts
+++ packages/kit/src/background/services/ServiceSetting.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/require-await  */
 import axios from 'axios';
+import semver from 'semver';
 
 import simpleDb from '@onekeyhq/engine/src/dbs/simple/simpleDb';
 import { getFiatEndpoint } from '@onekeyhq/engine/src/endpoint';
 
+import { setEnableIOSDappSearch } from '../../store/reducers/discover';
 import { setSwapMaintain } from '../../store/reducers/swapTransactions';
 import { backgroundClass, backgroundMethod } from '../decorators';
 
@@ -12,6 +14,7 @@ import ServiceBase from './ServiceBase';
 type RemoteSetting = {
   enableAppRatings: boolean;
   swapMaintain: boolean;
+  helloVersion: string;
 };
 
 @backgroundClass()
@@ -24,13 +27,20 @@ export default class ServiceSetting extends ServiceBase {
 
   @backgroundMethod()
   async updateRemoteSetting() {
+    const { appSelector, dispatch } = this.backgroundApi;
     const baseUrl = this.getFiatEndpoint();
     const url = `${baseUrl}/setting/list`;
     const res = await this.client.get(url);
     const data = res.data as RemoteSetting;
     await simpleDb.setting.setEnableAppRatings(data.enableAppRatings);
     await simpleDb.setting.setSwapMaintain(data.swapMaintain);
-    this.backgroundApi.dispatch(setSwapMaintain(data.swapMaintain));
+    dispatch(setSwapMaintain(data.swapMaintain));
+    if (data.helloVersion && semver.valid(data.helloVersion)) {
+      const version = appSelector((s) => s.settings.version);
+      if (semver.lte(version, data.helloVersion)) {
+        dispatch(setEnableIOSDappSearch(true));
+      }
+    }
   }
 
   @backgroundMethod()
