109c6f0ddc140359c7a6b7cd660e205c5e15c1d6
王山栋 <ezail.shandong@gmail.com>
2022-11-23 16:47:26 +0800

    fix: toggleButton icon text spac & canvas devicePixelRatio
    Signed-off-by: 王山栋 <ezail.shandong@gmail.com>

--- packages/kit/src/views/Market/Components/MarketList/SparklineChart.tsx
+++ packages/kit/src/views/Market/Components/MarketList/SparklineChart.tsx
@@ -6,6 +6,8 @@ import Canvas, { CanvasRenderingContext2D } from 'react-native-canvas';
 import { Box } from '@onekeyhq/components/src';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
+import { useDevicePixelRatio } from '../../hooks/useMarketLayout';
+
 type SparkLineChartProps = {
   data?: number[];
   lineColor?: string;
@@ -17,7 +19,7 @@ type SparkLineChartProps = {
   linearGradientColor?: string;
 };
 
-const offsetHeight = 10;
+const offsetY = 5;
 
 const SparkLineChart: React.FC<SparkLineChartProps> = ({
   data,
@@ -29,71 +31,81 @@ const SparkLineChart: React.FC<SparkLineChartProps> = ({
   smooth = true,
   scale = 0.1,
 }) => {
+  const devicePixelRatio = useDevicePixelRatio();
   const draw = useCallback(
-    (canvas: Canvas | HTMLCanvasElement | null) => {
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async (canvas: Canvas | HTMLCanvasElement | null) => {
       if (canvas) {
-        canvas.width = width;
-        canvas.height = height + offsetHeight;
+        canvas.width = width * devicePixelRatio;
+        canvas.height = height * devicePixelRatio;
+        const calculateHeight = height - offsetY;
         const showData = data || [];
         const maxValue = Math.max(...showData);
         const minValue = Math.min(...showData);
-        const yStep = height / (maxValue - minValue);
+        const yStep = calculateHeight / (maxValue - minValue);
         const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
         if (ctx) {
+          ctx.scale(devicePixelRatio, devicePixelRatio);
           ctx.beginPath();
           ctx.lineWidth = lineWidth;
           ctx.strokeStyle = lineColor;
           const xSpath = width / (showData.length - 1);
           let xPoint = 0;
+          let lastY = 0;
           showData.forEach((v, i) => {
-            const yPoint = height - (v - minValue) * yStep;
+            const yPoint = calculateHeight - (v - minValue) * yStep;
             if (smooth && i >= 2 && i < showData.length - 1) {
               // draw bezier
               const pre1X = xSpath * (i - 1);
-              const pre1Y = height - (showData[i - 1] - minValue) * yStep;
+              const pre1Y =
+                calculateHeight - (showData[i - 1] - minValue) * yStep;
               const pre2X = xSpath * (i - 2);
-              const pre2Y = height - (showData[i - 2] - minValue) * yStep;
+              const pre2Y =
+                calculateHeight - (showData[i - 2] - minValue) * yStep;
               const nextX = xSpath * (i + 1);
-              const nextY = height - (showData[i + 1] - minValue) * yStep;
+              const nextY =
+                calculateHeight - (showData[i + 1] - minValue) * yStep;
               const cp1x = pre1X + (xPoint - pre2X) * scale;
-              const cp1y = pre1Y + (yPoint - pre2Y) * scale;
+              let cp1y = pre1Y + (yPoint - pre2Y) * scale;
               const cp2x = xPoint - (nextX - pre1X) * scale;
-              const cp2y = yPoint - (nextY - pre1Y) * scale;
+              let cp2y = yPoint - (nextY - pre1Y) * scale;
+              if (cp1y < 0) cp1y = 0;
+              if (cp2y < 0) cp2y = 0;
               ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, xPoint, yPoint);
             } else {
               ctx.lineTo(xPoint, yPoint);
             }
+            if (i === showData.length - 1) lastY = yPoint;
             xPoint += xSpath;
           });
-          ctx.lineTo(xPoint, height + offsetHeight);
-          ctx.lineTo(0, height + offsetHeight);
+          ctx.lineTo(xPoint + xSpath, lastY);
+          ctx.lineTo(xPoint, height);
+          ctx.lineTo(0, height);
           ctx.stroke();
           ctx.closePath();
-          if (!platformEnv.isNative) {
-            const grad = ctx.createLinearGradient(0, 0, 0, height);
-            grad.addColorStop(0, linearGradientColor);
-            grad.addColorStop(1, 'rgba(255,255,255,0)');
-            ctx.fillStyle = grad;
-            ctx.fill();
-          }
-          xPoint -= xSpath;
+          const grad = platformEnv.isNative
+            ? // eslint-disable-next-line @typescript-eslint/await-thenable
+              await ctx.createLinearGradient(0, 0, 0, calculateHeight)
+            : ctx.createLinearGradient(0, 0, 0, calculateHeight);
+          grad.addColorStop(0, linearGradientColor);
+          grad.addColorStop(1, 'rgba(255,255,255,0)');
+          ctx.fillStyle = grad;
+          ctx.fill();
+
+          xPoint -= lineWidth;
           // clear last 2 line
-          ctx.clearRect(xPoint - xSpath, 0, xSpath, height + offsetHeight);
-          ctx.clearRect(
-            0,
-            height + offsetHeight - lineWidth,
-            width + xSpath,
-            lineWidth,
-          );
+          ctx.clearRect(xPoint, 0, xSpath, height);
+          ctx.clearRect(0, height - lineWidth, width, lineWidth);
         }
       }
     },
     [
-      data,
-      height,
-      lineColor,
-      lineWidth,
       width,
+      devicePixelRatio,
+      height,
+      data,
+      lineWidth,
+      lineColor,
       smooth,
       scale,
       linearGradientColor,
@@ -101,12 +113,24 @@ const SparkLineChart: React.FC<SparkLineChartProps> = ({
   );
 
   return !platformEnv.isNative ? (
-    <div style={{ width, height: height + offsetHeight }}>
-      <canvas ref={draw} />
+    <div style={{ width, height }}>
+      <canvas
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+        }}
+        ref={draw}
+      />
     </div>
   ) : (
-    <Box w={width} h={height + offsetHeight}>
-      <Canvas ref={draw} />
+    <Box>
+      <Canvas
+        style={{
+          width,
+          height,
+        }}
+        ref={draw}
+      />
     </Box>
   );
 };
