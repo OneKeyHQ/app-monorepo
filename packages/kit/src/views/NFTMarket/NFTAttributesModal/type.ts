e2647baf6933446957f25ed215fdb4e2490dfa84
linleiqin <278438611@qq.com>
2022-11-23 10:03:55 +0800

    fix:lint

--- packages/kit/src/views/NFTMarket/NFTAttributesModal/type.ts
+++ packages/kit/src/views/NFTMarket/NFTAttributesModal/type.ts
@@ -7,6 +7,10 @@ export enum NFTAttributeFilterRoutes {
 export type NFTAttributeFilterRoutesParams = {
   [NFTAttributeFilterRoutes.FilterModal]: {
     collection: Collection;
+    attributes: {
+      attribute_name: string;
+      attribute_values: string[];
+    }[];
     onAttributeSelected: (
       attributes: { attribute_name: string; attribute_values: string[] }[],
     ) => void;
