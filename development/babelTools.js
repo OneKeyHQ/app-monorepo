6d475c32ccd7d556a7335b32c1a423ebec54cff3
sunnylqm <sunnylqm@gmail.com>
2022-11-29 13:39:39 +0800

    chore: add comments for targets

--- development/babelTools.js
+++ development/babelTools.js
@@ -121,8 +121,10 @@ function normalizeConfig({ platform, config }) {
 
   // console.log('babelToolsConfig > moduleResolver: ', moduleResolver);
 
-  // https://babeljs.io/docs/en/assumptions
+  // https://babeljs.io/docs/en/options#no-targets
   config.targets = 'defaults';
+
+  // https://babeljs.io/docs/en/assumptions
   config.assumptions = {
     noDocumentAll: true,
     noClassCalls: true,
