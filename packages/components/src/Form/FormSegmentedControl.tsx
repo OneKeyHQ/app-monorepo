8396d0ba84695e0dbf7bebbc5db4b8a6d0a8fc0e
ByteZhang1024 <ByteZhang@protonmail.com>
2022-11-29 21:26:29 +0800

    fix: import watching error

---
+++ packages/components/src/Form/FormSegmentedControl.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+import SegmentedControl, {
+  type SegmentedControlProps,
+} from '../SegmentedControl';
+
+type FormSegmentedControlProps = Omit<
+  SegmentedControlProps,
+  'selectedIndex'
+> & {
+  value?: number;
+};
+
+export const FormSegmentedControl: FC<FormSegmentedControlProps> = ({
+  value,
+  ...props
+}) => <SegmentedControl selectedIndex={value ?? 0} {...props} />;
