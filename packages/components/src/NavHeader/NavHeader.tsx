bf75528bcdc143c2f21c7ba67a133f94b539d4f1
sunnylqm <sunnylqm@gmail.com>
2022-12-08 21:11:48 +0800

    feat: add navheadergallery

--- packages/components/src/NavHeader/NavHeader.tsx
+++ packages/components/src/NavHeader/NavHeader.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable no-nested-ternary */
-import { ReactNode } from 'react';
+import { FC, ReactNode } from 'react';
 
 import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -10,30 +10,32 @@ import { useIsVerticalLayout } from '../Provider/hooks';
 import HeaderBackButton from './HeaderBackButton';
 import HeaderTitle from './HeaderTitle';
 
-interface PageHeaderProps {
+interface HeaderProps {
   title?: string;
   subtitle?: string;
   headerTitle?: () => ReactNode;
   headerLeft?: () => ReactNode;
   headerRight?: () => ReactNode;
+  enableBackButton?: boolean;
+  safeTop?: number;
 }
 
 const defaultMobileHeight = 56;
 const defaultDesktopHeight = 64;
 
-export default function PageHeader({
+const NavHeader: FC<HeaderProps> = ({
   title,
   subtitle,
   headerLeft,
   headerRight,
   headerTitle,
-}: PageHeaderProps) {
+  safeTop,
+  enableBackButton = true,
+}) => {
   const insets = useSafeAreaInsets();
   const isVertical = useIsVerticalLayout();
 
   const height = isVertical ? defaultMobileHeight : defaultDesktopHeight;
-
-  const leftButton = headerLeft ? headerLeft() : <HeaderBackButton />;
 
   const titleComponent = headerTitle ? (
     headerTitle()
@@ -46,19 +48,21 @@ export default function PageHeader({
       pointerEvents="box-none"
       style={{
         height,
-        paddingTop: insets.top,
+        paddingTop: safeTop ?? insets.top,
       }}
     >
-      <View pointerEvents="box-none" style={styles.content}>
+      <View
+        pointerEvents="box-none"
+        style={[styles.content, { marginHorizontal: isVertical ? 16 : 32 }]}
+      >
         <View
           pointerEvents="box-none"
           style={[styles.left, { marginStart: insets.left }]}
         >
-          {leftButton}
+          {enableBackButton && <HeaderBackButton />}
+          {headerLeft?.()}
         </View>
-        <View pointerEvents="box-none" style={styles.title}>
-          {titleComponent}
-        </View>
+        {titleComponent}
         <View
           pointerEvents="box-none"
           style={[styles.right, styles.expand, { marginEnd: insets.right }]}
@@ -68,17 +72,13 @@ export default function PageHeader({
       </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   content: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'stretch',
-  },
-  title: {
-    marginHorizontal: 16,
-    justifyContent: 'center',
   },
   left: {
     justifyContent: 'center',
@@ -93,3 +93,4 @@ const styles = StyleSheet.create({
     flexBasis: 0,
   },
 });
+export default NavHeader;
---
+++ packages/kit/src/views/Components/stories/NavHeader.tsx
@@ -0,0 +1,37 @@
+import { Box, IconButton } from '@onekeyhq/components';
+import NavHeader from '@onekeyhq/components/src/NavHeader/NavHeader';
+
+const NavHeaderGallery = () => (
+  <Box p="20px">
+    <Box bg="surface-selected">
+      <NavHeader title="Title" safeTop={0} />
+    </Box>
+    <Box h="20px" />
+    <Box bg="surface-selected">
+      <NavHeader
+        safeTop={0}
+        title="TitleWithOutBackButton"
+        subtitle="subtitle"
+        enableBackButton={false}
+      />
+    </Box>
+    <Box h="20px" />
+    <Box bg="surface-selected">
+      <NavHeader
+        safeTop={0}
+        title="Title"
+        subtitle="subtitle"
+        headerRight={() => (
+          <IconButton
+            name="EllipsisVerticalOutline"
+            size="lg"
+            type="plain"
+            circle
+          />
+        )}
+      />
+    </Box>
+  </Box>
+);
+
+export default NavHeaderGallery;
