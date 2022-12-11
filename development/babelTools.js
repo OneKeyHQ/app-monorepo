d275ac86f2a3491cc2098e7053338bc81c75ea98
sunnylqm <sunnylqm@gmail.com>
2022-12-11 10:05:22 +0800

    chore: cleanup

--- development/babelTools.js
+++ development/babelTools.js
@@ -119,15 +119,6 @@ function normalizeConfig({ platform, config }) {
     moduleResolver && ['module-resolver', moduleResolver],
   ].filter(Boolean);
 
-  if (platform !== developmentConsts.platforms.app) {
-    config.plugins.push([
-      'react-native-web',
-      // {
-      //   commonjs: true,
-      // },
-    ]);
-  }
-
   // console.log('babelToolsConfig > moduleResolver: ', moduleResolver);
 
   // https://babeljs.io/docs/en/options#no-targets
