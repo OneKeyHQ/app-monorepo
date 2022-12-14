a8a10c0773d707dd0dc5b862c83b77b9db002fde
kwoktung <onlyif2009@gmail.com>
2022-12-14 14:08:50 +0800

    fix: show swap features modal when the view is focused

--- packages/kit/src/views/Swap/SwapObserver.tsx
+++ packages/kit/src/views/Swap/SwapObserver.tsx
@@ -5,12 +5,14 @@ import {
   AppUIEventBusNames,
   appUIEventBus,
 } from '@onekeyhq/shared/src/eventBus/appUIEventBus';
+import { useIsFocused } from '@react-navigation/native';
 
 import backgroundApiProxy from '../../background/instance/backgroundApiProxy';
 import { useAppSelector, useNavigation } from '../../hooks';
 import { ModalRoutes, RootRoutes } from '../../routes/types';
 
 import { SwapRoutes } from './typings';
+
 
 const NetworkObserver = () => {
   useEffect(() => {
@@ -45,11 +47,12 @@ const TokenUpdater = () => {
 
 const WelcomeObserver = () => {
   const navigation = useNavigation();
+  const isFocused = useIsFocused();
   useEffect(() => {
     async function main() {
       const swapWelcomeShown =
         await backgroundApiProxy.serviceSwap.getSwapWelcomeShown();
-      if (swapWelcomeShown) {
+      if (swapWelcomeShown || !isFocused) {
         return;
       }
       navigation.navigate(RootRoutes.Modal, {
@@ -59,9 +62,9 @@ const WelcomeObserver = () => {
         },
       });
     }
-    const timer = setTimeout(main, 2000);
+    const timer = setTimeout(main, 1000);
     return () => clearTimeout(timer);
-  }, [navigation]);
+  }, [navigation, isFocused]);
   return null;
 };
 
