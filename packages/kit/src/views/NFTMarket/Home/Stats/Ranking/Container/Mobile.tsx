8819a5a4201ef335d28c65352d2855a93cf00f08
linleiqin <278438611@qq.com>
2022-12-14 22:01:52 +0800

    fix:lint

--- packages/kit/src/views/NFTMarket/Home/Stats/Ranking/Container/Mobile.tsx
+++ packages/kit/src/views/NFTMarket/Home/Stats/Ranking/Container/Mobile.tsx
@@ -6,9 +6,11 @@ import { useIntl } from 'react-intl';
 import { ListRenderItem } from 'react-native';
 
 import { Box, List, ListItem, Text } from '@onekeyhq/components';
-
 import type { Network } from '@onekeyhq/engine/src/types/network';
-import type { NFTMarketRanking,Collection } from '@onekeyhq/engine/src/types/nft';
+import type {
+  Collection,
+  NFTMarketRanking,
+} from '@onekeyhq/engine/src/types/nft';
 
 import CollectionLogo from '../../../../CollectionLogo';
 import { PriceString } from '../../../../PriceText';
@@ -142,7 +144,13 @@ const Mobile: FC<Props> = ({
         </ListItem>
       );
     },
-    [intl, network?.id, context?.selectedNetwork?.id, goToCollectionDetail],
+    [
+      intl,
+      network?.id,
+      context?.selectedNetwork?.id,
+      onSelectCollection,
+      goToCollectionDetail,
+    ],
   );
 
   if (listData === undefined || listData?.length === 0 || context?.loading) {
