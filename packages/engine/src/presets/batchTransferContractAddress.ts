998a25e9b334e04c9e1d63f1e78eeb321058b69a
weatherstar <weather@onekey.so>
2022-12-15 11:05:59 +0800

    feat: Call the new batch contract & fix bugs OK-15361 OK-15062 (#2189)
    * chore: change disperseNFT function hash
    
    * fix: unlimited query pending tx status
    
    * fix: approvel is still required after approvel success
    
    * chore: update batch transfer contract address

--- packages/engine/src/presets/batchTransferContractAddress.ts
+++ packages/engine/src/presets/batchTransferContractAddress.ts
@@ -19,16 +19,14 @@ export const batchTransferContractAddress: {
 } = {
   // https://etherscan.io/address/0x94214d492f1c94c977378e0884e1dc3f32ee6b99
   [OnekeyNetwork.eth]: '0x94214d492f1c94c977378e0884e1dc3f32ee6b99',
-  // https://goerli.etherscan.io/address/0x6bbe9c913e6ae7448b8cc975bac5364a557c724a
-  [OnekeyNetwork.goerli]: '0x6bbe9c913e6ae7448b8cc975bac5364a557c724a',
-  // https://polygonscan.com/address/0x8db8e4a1351e333823c016e931672dd10779e7f5
-  [OnekeyNetwork.polygon]: '0x8db8e4a1351e333823c016e931672dd10779e7f5',
-  // https://bscscan.com/address/0xbeaa4219f495611ccccc441c1f19ab0b3708dc67
-  [OnekeyNetwork.bsc]: '0xbeaa4219f495611ccccc441c1f19ab0b3708dc67',
-  // https://snowtrace.io/address/0xe786ad03a6a35da315436a7e30be731047972091
-  [OnekeyNetwork.avalanche]: '0xe786ad03a6a35da315436a7e30be731047972091',
-  // https://arbiscan.io/address/0x9801d11c0ed5bdadcc30b37bd1a99e3d06022503
-  [OnekeyNetwork.arbitrum]: '0x9801d11c0ed5bdadcc30b37bd1a99e3d06022503',
-  // https://optimistic.etherscan.io/address/0x8e17ed9f91ddab1f2d7c2075abf7c293d3a28cd3
-  [OnekeyNetwork.optimism]: '0x8e17ed9f91ddab1f2d7c2075abf7c293d3a28cd3',
+  // https://polygonscan.com/address/0xbb4b1ce29767346cf5d51b4b7557774b1ebf7db0
+  [OnekeyNetwork.polygon]: '0xbb4b1ce29767346cf5d51b4b7557774b1ebf7db0',
+  // https://bscscan.com/address/0x4fa8257d636b4b017cf06c164e35e981c23925d5
+  [OnekeyNetwork.bsc]: '0x4fa8257d636b4b017cf06c164e35e981c23925d5',
+  // https://snowtrace.io/address/0x5687422a1b780d80e46473878d3e410cdc98064d
+  [OnekeyNetwork.avalanche]: '0x5687422a1b780d80e46473878d3e410cdc98064d',
+  // https://arbiscan.io/address/0xe5206106ae1e21c347ab1aee0e1c5f9d84be21ac
+  [OnekeyNetwork.arbitrum]: '0xe5206106ae1e21c347ab1aee0e1c5f9d84be21ac',
+  // https://optimistic.etherscan.io/address/0x9874000287152a4e0a114ed6bb0c7ded57697e98
+  [OnekeyNetwork.optimism]: '0x9874000287152a4e0a114ed6bb0c7ded57697e98',
 } as const;
