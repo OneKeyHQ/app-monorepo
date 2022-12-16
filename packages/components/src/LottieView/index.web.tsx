9c6ee7f48814724c93ee832289cb6987b07e3090
sunnylqm <sunnylqm@gmail.com>
2022-12-16 21:12:03 +0800

    feat: split

---
+++ packages/components/src/LottieView/index.web.tsx
@@ -0,0 +1,24 @@
+import LottieViewWeb, {
+  LottieComponentProps as LottieWebProps,
+} from 'lottie-react';
+
+// Stick props the same as LottieNative by now
+type LottieViewProps = Omit<LottieWebProps, 'animationData'> & {
+  source: LottieWebProps['animationData'];
+  autoPlay?: boolean;
+};
+
+const LottieView = ({
+  source,
+  autoPlay = false,
+  loop,
+  ...props
+}: LottieViewProps) => (
+  <LottieViewWeb
+    animationData={source}
+    autoPlay={autoPlay}
+    loop={loop}
+    {...props}
+  />
+);
+export default LottieView;
