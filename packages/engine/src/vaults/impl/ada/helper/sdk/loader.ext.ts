e02ed3ee921d0b7ca7804dd585de91ecc77ed9c7
Leon <lixiao.dev@gmail.com>
2022-12-20 09:39:48 +0800

    fix: firefox disable ada

--- packages/engine/src/vaults/impl/ada/helper/sdk/loader.ext.ts
+++ packages/engine/src/vaults/impl/ada/helper/sdk/loader.ext.ts
@@ -1,1 +1,8 @@
-export const LibLoader = () => null;
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
+export const LibLoader = async () => {
+  if (platformEnv.isExtFirefox) {
+    return null;
+  }
+  return import('cardano-coin-selection-asmjs');
+};
