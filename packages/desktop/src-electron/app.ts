382ac7ca65d599539d231ffad3c65596064367eb
王山栋 <ezail.shandong@gmail.com>
2022-12-28 11:58:02 +0800

    fix: macos menu add open window
    Signed-off-by: 王山栋 <ezail.shandong@gmail.com>

--- packages/desktop/src-electron/app.ts
+++ packages/desktop/src-electron/app.ts
@@ -6,6 +6,7 @@ import { format as formatUrl } from 'url';
 
 import {
   BrowserWindow,
+  Menu,
   app,
   ipcMain,
   screen,
@@ -72,6 +73,107 @@ function showMainWindow() {
   mainWindow.show();
   mainWindow.focus();
 }
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' },
+          ],
+        },
+      ]
+    : []),
+  // { role: 'fileMenu' }
+  // {
+  //   label: 'File',
+  //   submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
+  // },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac
+        ? [
+            { role: 'pasteAndMatchStyle' },
+            { role: 'delete' },
+            { role: 'selectAll' },
+            { type: 'separator' },
+            {
+              label: 'Speech',
+              submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }],
+            },
+          ]
+        : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
+    ],
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
+    ],
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac
+        ? [
+            { type: 'separator' },
+            { role: 'front' },
+            { type: 'separator' },
+            { role: 'window' },
+            {
+              label: 'Onekey',
+              click: showMainWindow,
+              accelerator: 'CmdOrCtrl+O',
+            },
+          ]
+        : [{ role: 'close' }]),
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          await shell.openExternal('https://onekey.so');
+        },
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(template as any);
+Menu.setApplicationMenu(menu);
 
 const emitter = new EventEmitter();
 let isAppReady = false;
