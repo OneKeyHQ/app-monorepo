ec8833b40b09ac50a1fef88fed3e2b203d427488
王山栋 <ezail.shandong@gmail.com>
2022-12-28 13:02:43 +0800

    fix: use default role
    Signed-off-by: 王山栋 <ezail.shandong@gmail.com>

--- packages/desktop/src-electron/app.ts
+++ packages/desktop/src-electron/app.ts
@@ -75,70 +75,9 @@ function showMainWindow() {
 }
 
 const template = [
-  // { role: 'appMenu' }
-  ...(isMac
-    ? [
-        {
-          label: app.name,
-          submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'services' },
-            { type: 'separator' },
-            { role: 'hide' },
-            { role: 'hideOthers' },
-            { role: 'unhide' },
-            { type: 'separator' },
-            { role: 'quit' },
-          ],
-        },
-      ]
-    : []),
-  // { role: 'fileMenu' }
-  // {
-  //   label: 'File',
-  //   submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
-  // },
-  // { role: 'editMenu' }
-  {
-    label: 'Edit',
-    submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      ...(isMac
-        ? [
-            { role: 'pasteAndMatchStyle' },
-            { role: 'delete' },
-            { role: 'selectAll' },
-            { type: 'separator' },
-            {
-              label: 'Speech',
-              submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }],
-            },
-          ]
-        : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
-    ],
-  },
-  // { role: 'viewMenu' }
-  {
-    label: 'View',
-    submenu: [
-      { role: 'reload' },
-      { role: 'forceReload' },
-      { role: 'toggleDevTools' },
-      { type: 'separator' },
-      { role: 'resetZoom' },
-      { role: 'zoomIn' },
-      { role: 'zoomOut' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' },
-    ],
-  },
-  // { role: 'windowMenu' }
+  { role: 'appMenu' },
+  { role: 'editMenu' },
+  { role: 'viewMenu' },
   {
     label: 'Window',
     submenu: [
