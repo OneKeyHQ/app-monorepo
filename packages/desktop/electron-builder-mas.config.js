48e4a908b818182a80a93022d7a63297e4e2ca94
Leon <lixiao.dev@gmail.com>
2023-01-05 15:52:20 +0800

    chore: mas local build

--- packages/desktop/electron-builder-mas.config.js
+++ packages/desktop/electron-builder-mas.config.js
@@ -4,7 +4,7 @@ require('../../development/env');
 module.exports = {
   'extraMetadata': {
     'main': 'dist/app.js',
-    'version': '3.25.0',
+    'version': '3.25.1',
   },
   'appId': 'so.onekey.wallet',
   'productName': 'OneKey',
