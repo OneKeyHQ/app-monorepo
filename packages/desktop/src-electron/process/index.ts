fa2dc06907bcb130b825e14c9ef0b1a7f59c660b
Leon <lixiao.dev@gmail.com>
2023-01-06 09:42:06 +0800

    fix: launch bridge process on MAS

--- packages/desktop/src-electron/process/index.ts
+++ packages/desktop/src-electron/process/index.ts
@@ -39,6 +39,7 @@ export const restartBridge = async () => {
 };
 
 const init = async ({ mainWindow, store }: Dependencies) => {
+  logger.info('Electron main process log path: ', logger.transports.file.file);
   await launchBridge();
   if (!process.mas) {
     autoUpdateInit({ mainWindow, store });
