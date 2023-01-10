29920ccdb401890d42dfb8cea1559ed9cfa9569a
sunnylqm <sunnylqm@gmail.com>
2023-01-10 20:28:42 +0800

    fix: authentication failure

--- packages/kit/src/utils/localAuthentication.native.tsx
+++ packages/kit/src/utils/localAuthentication.native.tsx
@@ -1,29 +1,27 @@
-import * as LocalAuthentication from 'expo-local-authentication';
-import * as SecureStore from 'expo-secure-store';
+import {
+  authenticateAsync,
+  hasHardwareAsync,
+  isEnrolledAsync,
+} from 'expo-local-authentication';
+import { getItemAsync, setItemAsync } from 'expo-secure-store';
 
 export const hasHardwareSupported = () =>
-  new Promise<boolean>((resolve) => {
-    const p = LocalAuthentication.hasHardwareAsync().then((supported) =>
-      LocalAuthentication.isEnrolledAsync().then(
-        (isEnrolled) => supported && isEnrolled,
-      ),
-    );
-    resolve(p);
+  hasHardwareAsync().then((supported) =>
+    isEnrolledAsync().then((isEnrolled) => supported && isEnrolled),
+  );
+
+export const localAuthenticate = async () => {
+  if (!(await hasHardwareSupported())) {
+    return { success: false, error: 'no supported' };
+  }
+
+  return authenticateAsync({
+    cancelLabel: 'Cancel',
+    disableDeviceFallback: true,
   });
-
-export const localAuthenticate: () => Promise<LocalAuthentication.LocalAuthenticationResult> =
-  async () => {
-    const supported = await hasHardwareSupported();
-    if (!supported) {
-      return { success: false, error: 'no supported' };
-    }
-
-    return LocalAuthentication.authenticateAsync({
-      cancelLabel: 'Cancel',
-    });
-  };
+};
 
 export const savePassword = (password: string) =>
-  SecureStore.setItemAsync('password', password);
+  setItemAsync('password', password);
 
-export const getPassword = () => SecureStore.getItemAsync('password');
+export const getPassword = () => getItemAsync('password');
