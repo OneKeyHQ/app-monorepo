b7f92264824a05199f96a39f9f38dee1eef037b9
Leon <lixiao.dev@gmail.com>
2023-01-12 21:03:58 +0800

    fix: skip resource check (#2402)

--- packages/kit/src/views/Hardware/UpdateFirmware/UpdateInfo/index.tsx
+++ packages/kit/src/views/Hardware/UpdateFirmware/UpdateInfo/index.tsx
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import { useNavigation, useRoute } from '@react-navigation/core';
 import { get } from 'lodash';
@@ -207,6 +207,11 @@ const UpdateInfoModal: FC = () => {
     isSmallScreen,
   ]);
 
+  const buttonEnable = useMemo(() => {
+    if (device?.deviceType !== 'touch') return true;
+    return !!features;
+  }, [device, features]);
+
   return (
     <Modal
       maxHeight={560}
@@ -222,6 +227,10 @@ const UpdateInfoModal: FC = () => {
             resourceUpdateInfo,
           },
         );
+      }}
+      primaryActionProps={{
+        isDisabled: !buttonEnable,
+        isLoading: !buttonEnable,
       }}
       scrollViewProps={{
         children: (
