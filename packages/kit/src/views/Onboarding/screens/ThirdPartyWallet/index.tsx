dcfd34453489fdb9b8d729050c357714a47783d6
Leon <lixiao.dev@gmail.com>
2023-01-16 13:08:53 +0800

    feat: onboarding done

--- packages/kit/src/views/Onboarding/screens/ThirdPartyWallet/index.tsx
+++ packages/kit/src/views/Onboarding/screens/ThirdPartyWallet/index.tsx
@@ -1,16 +1,33 @@
+import { useRoute } from '@react-navigation/native';
 import { useIntl } from 'react-intl';
 
-import { Center, Text } from '@onekeyhq/components';
+import { Center, Hidden, Text, ToastManager } from '@onekeyhq/components';
 
 import { ConnectWalletListView } from '../../../../components/WalletConnect/WalletConnectQrcodeModal';
 import useAppNavigation from '../../../../hooks/useAppNavigation';
+import { useOnboardingDone } from '../../../../hooks/useOnboardingRequired';
+import { wait } from '../../../../utils/helper';
 import { useAddExternalAccount } from '../../../ExternalAccount/useAddExternalAccount';
 import Layout from '../../Layout';
+
+import type { EOnboardingRoutes } from '../../routes/enums';
+import type { IOnboardingRoutesParams } from '../../routes/types';
+import type { RouteProp } from '@react-navigation/native';
+
+type RouteProps = RouteProp<
+  IOnboardingRoutesParams,
+  EOnboardingRoutes.ThirdPartyWallet
+>;
 
 const ThirdPartyWallet = () => {
   const intl = useIntl();
 
   const { addExternalAccount } = useAddExternalAccount();
+  const onboardingDone = useOnboardingDone();
+
+  const route = useRoute<RouteProps>();
+  const disableOnboardingDone = route.params?.disableOnboardingDone;
+  const onSuccess = route.params?.onSuccess;
 
   const navigation = useAppNavigation();
 
@@ -26,26 +43,48 @@ const ThirdPartyWallet = () => {
         <ConnectWalletListView
           onConnectResult={async (result) => {
             await addExternalAccount(result);
-            navigation?.goBack?.();
+            if (!disableOnboardingDone) {
+              await onboardingDone();
+              await wait(600);
+              ToastManager.show({
+                title: intl.formatMessage({ id: 'msg__account_imported' }),
+              });
+            } else {
+              navigation?.goBack?.();
+            }
+            if (onSuccess) {
+              onSuccess();
+            }
           }}
         />
       </Center>
-      <Text>INSTITUTIONAL WALLETS</Text>
-      <Center
-        justifyContent="flex-start"
-        flexDir={{ sm: 'row' }}
-        flexWrap={{ sm: 'wrap' }}
-        alignSelf="stretch"
-        mx={-1}
-      >
-        <ConnectWalletListView
-          onConnectResult={async (result) => {
-            await addExternalAccount(result);
-            navigation?.goBack?.();
-          }}
-          isInstitutionWallet
-        />
-      </Center>
+      <Hidden till="sm">
+        <>
+          <Text
+            mt={8}
+            mb={3}
+            typography={{ sm: 'Subheading', md: 'Subheading' }}
+            color="text-subdued"
+          >
+            INSTITUTIONAL WALLETS
+          </Text>
+          <Center
+            justifyContent="flex-start"
+            flexDir={{ sm: 'row' }}
+            flexWrap={{ sm: 'wrap' }}
+            alignSelf="stretch"
+            mx={-1}
+          >
+            <ConnectWalletListView
+              onConnectResult={async (result) => {
+                await addExternalAccount(result);
+                navigation?.goBack?.();
+              }}
+              isInstitutionWallet
+            />
+          </Center>
+        </>
+      </Hidden>
     </Layout>
   );
 };
