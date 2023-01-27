b0e7d8aff85c83919e9b691b2dc6c96bf50c42a9
JellyWang <38491708+ezailWang@users.noreply.github.com>
2023-01-27 20:45:37 +0800

    Fix/wallet switch ext (#2389)
    * fix:wallet switch enable for ext
    
    Signed-off-by: 王山栋 <ezail.shandong@gmail.com>
    
    * fix: update cross-inpage version
    
    Signed-off-by: 王山栋 <ezail.shandong@gmail.com>
    
    Signed-off-by: 王山栋 <ezail.shandong@gmail.com>
    Co-authored-by: morizon <sidmorizon@outlook.com>
    Co-authored-by: Sunny Luo <sunnylqm@gmail.com>

---
+++ packages/kit/src/views/Me/UtilSection/WalletSwitch/index.tsx
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useMemo } from 'react';
+
+import { useIntl } from 'react-intl';
+
+import { Box, Icon, SectionList, Typography } from '@onekeyhq/components';
+
+import { useNavigation } from '../../../../hooks';
+
+import { WalletSwitchCell } from './component/WalletSwitchCell';
+import { useWalletSwitch } from './hooks/useWalletSwitch';
+
+import type { SectionListRenderItem } from 'react-native';
+
+export type WalletGroup = { title: string; data: string[] };
+
+const WalletSwitch = () => {
+  const navigation = useNavigation();
+  const intl = useIntl();
+  useEffect(() => {
+    navigation.setOptions({
+      title: intl.formatMessage({ id: 'form__wallet_switch' }),
+    });
+  }, [intl, navigation]);
+  const { walletSwitchSectionData } = useWalletSwitch();
+  const renderItem: SectionListRenderItem<string, WalletGroup> = useCallback(
+    ({ item }) => (
+      <WalletSwitchCell
+        toggleWalletSwitch={(walletId, enable) => {}}
+        walletId={item}
+      />
+    ),
+    [],
+  );
+  const renderSectionHeader = useCallback(
+    ({ section: { title } }) => (
+      <Typography.Subheading mt={4} color="text-subdued">
+        {title}
+      </Typography.Subheading>
+    ),
+    [],
+  );
+  const headerComponent = useMemo(
+    () => (
+      <Box
+        borderRadius="12px"
+        bgColor="surface-neutral-subdued"
+        p={4}
+        flexDirection="row"
+        mb={2}
+      >
+        <Icon name="InformationCircleMini" />
+        <Typography.Body2Strong ml={2}>
+          {intl.formatMessage({
+            id: 'content__when_the_option_is_turned_on_the_inject_cnonection_method_for_that_wallet_will_be_overridden_by_onekey',
+          })}
+        </Typography.Body2Strong>
+        <Box />
+      </Box>
+    ),
+    [intl],
+  );
+  return (
+    <Box flex={1} p={4}>
+      <SectionList
+        sections={walletSwitchSectionData}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        ListHeaderComponent={headerComponent}
+      />
+    </Box>
+  );
+};
+
+export default WalletSwitch;
