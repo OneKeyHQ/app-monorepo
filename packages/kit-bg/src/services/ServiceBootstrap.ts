f8d9f518cb6481cad78a6f7216eda806bbbf5df8
王山栋 <jelly@onekey.so>
2023-02-01 17:32:23 +0800

    fix: move fiat-money trigger move to init app after endpoint change & ui bug
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/kit-bg/src/services/ServiceBootstrap.ts
+++ packages/kit-bg/src/services/ServiceBootstrap.ts
@@ -58,6 +58,8 @@ const defaultNetworkRpcs: Record<string, string> = {
 
 @backgroundClass()
 export default class ServiceBootstrap extends ServiceBase {
+  static fetchFiatTimer: NodeJS.Timeout | null = null;
+
   constructor(props: IServiceBaseProps) {
     super(props);
 
@@ -133,5 +135,15 @@ export default class ServiceBootstrap extends ServiceBase {
     dispatch(
       updateAutoSwitchDefaultRpcAtVersion(AUTO_SWITCH_DEFAULT_RPC_AT_VERSION),
     );
+  }
+
+  @backgroundMethod()
+  fetchFiatMoneyRate() {
+    this.backgroundApi.serviceCronJob.getFiatMoney();
+    if (!ServiceBootstrap.fetchFiatTimer) {
+      ServiceBootstrap.fetchFiatTimer = setInterval(() => {
+        this.backgroundApi.serviceCronJob.getFiatMoney();
+      }, 5 * 60 * 1000);
+    }
   }
 }
