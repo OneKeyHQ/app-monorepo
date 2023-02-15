d8c031962d90547e61bd735d714c04a7c5cd7666
Leon <lixiao@onekey.so>
2023-02-15 16:42:04 +0800

    fix: hw wallet can not auto creata account

--- packages/engine/src/vaults/VaultBase.ts
+++ packages/engine/src/vaults/VaultBase.ts
@@ -183,6 +183,10 @@ export abstract class VaultBaseChainOnly extends VaultContext {
   async isContractAddress(address: string): Promise<boolean> {
     return false;
   }
+
+  async canAutoCreateNextAccount(password: string): Promise<boolean> {
+    return Promise.resolve(true);
+  }
 }
 
 /*
