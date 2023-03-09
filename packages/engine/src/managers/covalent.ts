112584d6b1622cad0eeb0e98b4e90fba4a4082e0
morizon <sidmorizon@outlook.com>
2023-03-09 16:37:42 +0800

    fix: covalent api url

--- packages/engine/src/managers/covalent.ts
+++ packages/engine/src/managers/covalent.ts
@@ -32,6 +32,7 @@ import type { Token } from '../types/token';
 import type { IEncodedTxEvm } from '../vaults/impl/evm/Vault';
 import type { IDecodedTx, IDecodedTxAction } from '../vaults/types';
 import type { VaultBase } from '../vaults/VaultBase';
+import { getCovalentApiEndpoint } from '../endpoint';
 
 const NOBODY = '0x0000000000000000000000000000000000000000';
 
@@ -149,7 +150,7 @@ function getNftDetail(
   contractAddress: string,
   tokenId: string,
 ): Promise<NftMetadata | null> {
-  const request = `https://api.covalenthq.com/v1/${chainId}/tokens/${contractAddress}/nft_metadata/${tokenId}/`;
+  const request = `${getCovalentApiEndpoint()}/v1/${chainId}/tokens/${contractAddress}/nft_metadata/${tokenId}/`;
 
   return axios
     .get<NftDetail>(request, {
@@ -400,7 +401,7 @@ function getTxHistories(
   pageNumber = 0,
   pageSize = 50,
 ): Promise<HistoryDetailList | null> {
-  const request = `https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/`;
+  const request = `${getCovalentApiEndpoint()}/v1/${chainId}/address/${address}/transactions_v2/`;
   return axios
     .get<HistoryDetailList>(request, {
       params: {
@@ -867,9 +868,9 @@ async function fetchCovalentHistoryRaw({
   pageSize = pageSize ?? HISTORY_CONSTS.FETCH_ON_CHAIN_LIMIT;
 
   // https://www.covalenthq.com/docs/api/#/0/Get%20ERC20%20token%20transfers%20for%20address/USD/1
-  const tokenRequestUrl = `https://api.covalenthq.com/v1/${chainId}/address/${address}/transfers_v2/`;
+  const tokenRequestUrl = `${getCovalentApiEndpoint()}/v1/${chainId}/address/${address}/transfers_v2/`;
   // https://www.covalenthq.com/docs/api/#/0/Get%20transactions%20for%20address/USD/1
-  const url = `https://api.covalenthq.com/v1/${chainId}/address/${address}/transactions_v2/`;
+  const url = `${getCovalentApiEndpoint()}/v1/${chainId}/address/${address}/transactions_v2/`;
 
   const response = await axios.get<ICovalentHistoryList>(
     contract ? tokenRequestUrl : url,
@@ -892,7 +893,7 @@ function getErc20TransferHistories(
   pageNumber: number,
   pageSize: number,
 ): Promise<HistoryDetailList | null> {
-  const request = `https://api.covalenthq.com/v1/${chainId}/address/${address}/transfers_v2/`;
+  const request = `${getCovalentApiEndpoint()}/v1/${chainId}/address/${address}/transfers_v2/`;
   return axios
     .get<HistoryDetailList>(request, {
       params: {
