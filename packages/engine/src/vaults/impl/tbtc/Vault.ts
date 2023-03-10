1b8f9339e2013387b3aa3286bbb0bb887b85b95a
Leon <lixiao.dev@gmail.com>
2023-03-10 17:25:46 +0800

    OK-17133: Support Bitcoin Taproot address (#2657)
    * refactor: change btc vault to BTCForkVault
    
    * refactor: remove bitcoin provider on blockchain-libs
    
    # Conflicts:
    #	packages/blockchain-libs/src/provider/chains/btc/provider.ts
    
    * feat: support bitcoin taproot derivation type
    
    * feat: validate taproot address
    
    * feat: add xpubSegwit for taproot account
    
    * chore: migrate realm
    
    * feat: support Taproot on Bitcoin testnet
    
    * feat: taproot sign schnorr
    
    * feat: ecpair patch
    
    * feat: hardware account support taproot
    
    * chore: hide taproot option for Classic and Mini
    
    * chore: upgrade sdk version
    
    * fix: appConfig
    
    * feat: support taproot watching account
    
    * feat: support taproot imported account
    
    * chore: upgrade sdk

--- packages/engine/src/vaults/impl/tbtc/Vault.ts
+++ packages/engine/src/vaults/impl/tbtc/Vault.ts
@@ -1,5 +1,13 @@
+import { omit } from 'lodash';
+
 import VaultBtcFork from '@onekeyhq/engine/src/vaults/utils/btcForkChain/VaultBtcFork';
-import { COINTYPE_TBTC } from '@onekeyhq/shared/src/engine/engineConsts';
+import {
+  COINTYPE_TBTC,
+  IMPL_TBTC,
+} from '@onekeyhq/shared/src/engine/engineConsts';
+
+import { getAccountNameInfoByImpl } from '../../../managers/impl';
+import Provider from '../btc/provider';
 
 import { KeyringHardware } from './KeyringHardware';
 import { KeyringHd } from './KeyringHd';
@@ -7,7 +15,12 @@ import { KeyringImported } from './KeyringImported';
 import { KeyringWatching } from './KeyringWatching';
 import settings from './settings';
 
+import type { DBUTXOAccount } from '../../../types/account';
+import type { AccountNameInfo } from '../../../types/network';
+
 export default class Vault extends VaultBtcFork {
+  override providerClass = Provider;
+
   override keyringMap = {
     hd: KeyringHd,
     hw: KeyringHardware,
@@ -44,5 +57,38 @@ export default class Vault extends VaultBtcFork {
 
   override getDefaultBlockTime(): number {
     return 600;
+  }
+
+  override getAccountXpub(account: DBUTXOAccount): string {
+    return account.xpubSegwit || account.xpub;
+  }
+
+  override async getAccountNameInfoMap(): Promise<
+    Record<string, AccountNameInfo>
+  > {
+    const isHwWallet = this.walletId.startsWith('hw');
+    const network = await this.getNetwork();
+    const accountNameInfo = getAccountNameInfoByImpl(network.impl);
+    if (!isHwWallet || !this.walletId) {
+      return accountNameInfo;
+    }
+    const wallet = await this.engine.getWallet(this.walletId);
+    if (
+      isHwWallet &&
+      (wallet.deviceType === 'classic' || wallet.deviceType === 'mini')
+    ) {
+      return omit(accountNameInfo, 'BIP86');
+    }
+    return accountNameInfo;
+  }
+
+  override async getAccountNameInfosByImportedOrWatchingCredential(
+    input: string,
+  ): Promise<AccountNameInfo[]> {
+    if (input.startsWith('tpub') || input.startsWith('tprv')) {
+      const accountNameInfo = getAccountNameInfoByImpl(IMPL_TBTC);
+      return Promise.resolve([accountNameInfo.BIP86, accountNameInfo.BIP44]);
+    }
+    return Promise.resolve([]);
   }
 }
