a4799c355e551a88626e1194c3e4641bd2f744a2
sunnylqm <sunnylqm@onekey.so>
2023-03-22 20:19:33 +0800

    chore: optimise haptics

--- packages/components/src/Pressable/Pressable.tsx
+++ packages/components/src/Pressable/Pressable.tsx
@@ -1,27 +1,15 @@
 import type { ComponentProps } from 'react';
-import { forwardRef, memo, useCallback } from 'react';
+import { forwardRef, memo } from 'react';
 
 import { Pressable as NBPressable } from 'native-base';
 
-import { enableHaptics } from '@onekeyhq/shared/src/haptics';
-
-import useProviderValue from '../Provider/hooks/useProviderValue';
-import { beforeOnPress } from '../utils/beforeOnPress';
+import { useBeforeOnPress } from '../utils/useBeforeOnPress';
 
 export type PressableItemProps = ComponentProps<typeof NBPressable>;
 
 const PressableCapture = forwardRef<typeof NBPressable, PressableItemProps>(
   ({ onPress, ...props }: PressableItemProps, ref) => {
-    const { hapticsEnabled } = useProviderValue();
-    const onPressOverride = useCallback(
-      (e) => {
-        if (hapticsEnabled && onPress) {
-          enableHaptics();
-        }
-        beforeOnPress(e, onPress);
-      },
-      [onPress, hapticsEnabled],
-    );
+    const onPressOverride = useBeforeOnPress(onPress);
 
     return (
       <NBPressable
