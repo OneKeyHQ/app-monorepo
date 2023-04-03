a6ce47ddbd7350520050d960e3d12dc68079bdfe
sunnylqm <sunnylqm@onekey.so>
2023-04-03 17:33:00 +0800

    fix: overlay 2

--- packages/components/src/OverlayContainer/index.tsx
+++ packages/components/src/OverlayContainer/index.tsx
@@ -4,25 +4,24 @@ import { StyleSheet, View } from 'react-native';
 import { RootSiblingPortal } from 'react-native-root-siblings';
 import { FullWindowOverlay } from 'react-native-screens';
 
+import { PortalEntry } from '@onekeyhq/kit/src/views/Overlay/RootPortal';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 const OverlayContainer: FC<{ useFullWindowForIOS?: boolean }> = ({
   useFullWindowForIOS,
   ...props
 }) => {
-  const Container =
-    useFullWindowForIOS && platformEnv.isNativeIOS
-      ? FullWindowOverlay
-      : RootSiblingPortal;
-  return (
-    <Container>
-      <View
-        testID="OverlayContainer-View"
-        pointerEvents="box-none"
-        style={StyleSheet.absoluteFill}
-        {...props}
-      />
-    </Container>
+  const content = (
+    <View
+      // testID="OverlayContainer-View"
+      pointerEvents="box-none"
+      style={StyleSheet.absoluteFill}
+      {...props}
+    />
   );
+  if (useFullWindowForIOS && platformEnv.isNativeIOS) {
+    return <PortalEntry target="Root-FullWindowOverlay">{content}</PortalEntry>;
+  }
+  return <RootSiblingPortal>{content}</RootSiblingPortal>;
 };
 export default OverlayContainer;
