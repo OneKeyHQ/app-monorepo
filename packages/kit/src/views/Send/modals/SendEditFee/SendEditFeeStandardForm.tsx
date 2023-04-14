5fc2f2f17417ad37e8e58e521b894a067d75fd85
weatherstar <weather@onekey.so>
2023-04-14 12:42:19 +0800

    chore: update style

--- packages/kit/src/views/Send/modals/SendEditFee/SendEditFeeStandardForm.tsx
+++ packages/kit/src/views/Send/modals/SendEditFee/SendEditFeeStandardForm.tsx
@@ -19,8 +19,8 @@ const presetItemStyle = {
   marginTop: 2,
   paddingLeft: '16px',
   paddingRight: '16px',
-  paddingTop: '8px',
-  paddingBottom: '8px',
+  paddingTop: '4px',
+  paddingBottom: '4px',
   alignItems: 'center',
 };
 
