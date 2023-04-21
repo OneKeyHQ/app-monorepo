8ab41338434f10bb6e5caa82efa247b5f2dbbabe
sunnylqm <sunnylqm@onekey.so>
2023-04-21 13:13:09 +0800

    feat: onhover close

--- packages/components/src/InAppNotification/index.tsx
+++ packages/components/src/InAppNotification/index.tsx
@@ -2,12 +2,17 @@
 import type { FC, ReactNode } from 'react';
 import { useEffect, useState } from 'react';
 
-import { AnimatePresence, MotiView } from 'moti';
+import { AnimatePresence, MotiView, motify, useDynamicAnimation } from 'moti';
+import { MotiPressable } from 'moti/interactions';
 import { useWindowDimensions } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 import Box from '../Box';
 import Button from '../Button';
-import IconButton from '../IconButton';
 import NetImage from '../NetImage';
 import Pressable from '../Pressable';
 import useIsVerticalLayout from '../Provider/hooks/useIsVerticalLayout';
@@ -16,6 +21,8 @@ import { Body2, Body2Strong } from '../Typography';
 
 const DISMISS_TIMEOUT = 3000;
 const TRANSITION_DURATION = 300;
+
+const AnimatedButton = Animated.createAnimatedComponent(Button);
 export interface InAppNotificationProps {
   title: string;
   subtitle?: string;
@@ -44,6 +51,14 @@ const InAppNotification: FC<InAppNotificationProps> = ({
   const { width } = useWindowDimensions();
   const [visible, setVisible] = useState(true);
   const isVerticalLayout = useIsVerticalLayout();
+  const isHovered = useSharedValue(false);
+  const showCloseButtonStyle = useAnimatedStyle(
+    () => ({
+      display: isHovered.value ? 'flex' : 'none',
+      opacity: isHovered.value ? 1 : 0,
+    }),
+    [],
+  );
   const startTransY = isVerticalLayout ? 100 + bottom : -(100 + top);
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -67,58 +82,59 @@ const InAppNotification: FC<InAppNotificationProps> = ({
             translateY: startTransY,
           }}
         >
-          <Pressable
-            w={width - 16}
-            onPress={onBodyPress}
-            maxW="374px"
-            px="16px"
-            py="12px"
-            borderRadius="12px"
-            borderColor="border-default"
-            borderWidth="1px"
-            flexDirection="row"
-            position="absolute"
-            right={isVerticalLayout ? '8px' : '32px'}
-            top={isVerticalLayout ? undefined : '32px'}
-            bottom={isVerticalLayout ? `${8 + bottom}px` : undefined}
-            bg="surface-default"
-          >
-            {({ isHovered, isPressed }) => (
-              <>
-                <Box flex={1}>
-                  <Body2Strong mb="4px">{title}</Body2Strong>
-                  {!!subtitle && <Body2>{subtitle}</Body2>}
-                  {!!footer && <Body2>{footer}</Body2>}
-                </Box>
-                <Box ml="auto" justifyContent="center">
-                  {rightContent ? (
-                    rightContent()
-                  ) : cover ? (
-                    <NetImage
-                      width="40px"
-                      height="40px"
-                      // resizeMode="cover"
-                      src={cover}
-                    />
-                  ) : (
-                    <Button onPress={onActionPress}>{actionText}</Button>
-                  )}
-                </Box>
-                {/* {isHovered && (
-                  <IconButton
-                    size="sm"
-                    rounded="full"
-                    position="absolute"
-                    zIndex={1}
-                    right="4px"
-                    top="-4px"
-                    name="XCircleMini"
-                    onPress={onClose}
+          <MotiPressable hoveredValue={isHovered}>
+            <Pressable
+              w={width - 16}
+              onPress={onBodyPress}
+              maxW="374px"
+              px="16px"
+              py="12px"
+              borderRadius="12px"
+              borderColor="border-default"
+              borderWidth="1px"
+              flexDirection="row"
+              position="absolute"
+              right={isVerticalLayout ? '8px' : '32px'}
+              top={isVerticalLayout ? undefined : '32px'}
+              bottom={isVerticalLayout ? `${8 + bottom}px` : undefined}
+              bg="surface-default"
+            >
+              <Box flex={1}>
+                <Body2Strong mb="4px">{title}</Body2Strong>
+                {!!subtitle && <Body2>{subtitle}</Body2>}
+                {!!footer && <Body2>{footer}</Body2>}
+              </Box>
+              <Box ml="auto" justifyContent="center">
+                {rightContent ? (
+                  rightContent()
+                ) : cover ? (
+                  <NetImage
+                    width="40px"
+                    height="40px"
+                    // resizeMode="cover"
+                    src={cover}
                   />
-                )} */}
-              </>
-            )}
-          </Pressable>
+                ) : (
+                  <Button onPress={onActionPress}>{actionText}</Button>
+                )}
+              </Box>
+              <AnimatedButton
+                style={showCloseButtonStyle}
+                rounded="full"
+                position="absolute"
+                zIndex={1}
+                right="4px"
+                top="-4px"
+                leftIconName="XCircleMini"
+                iconSize={16}
+                pt={0}
+                pr={0}
+                pb={0}
+                pl={0}
+                onPress={onClose}
+              />
+            </Pressable>
+          </MotiPressable>
         </MotiView>
       )}
     </AnimatePresence>
