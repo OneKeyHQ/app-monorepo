59086ead06ce12b694c75e71690bed73bd3fa901
Leon <lixiao@onekey.so>
2023-04-23 16:27:33 +0800

    feat: add advanced section

---
+++ packages/kit/src/views/Me/AdvancedSection/index.tsx
@@ -0,0 +1,72 @@
+import { useNavigation } from '@react-navigation/core';
+import { useIntl } from 'react-intl';
+
+import {
+  Box,
+  Icon,
+  Pressable,
+  Text,
+  Typography,
+  useTheme,
+} from '@onekeyhq/components';
+import { HomeRoutes } from '@onekeyhq/kit/src/routes/routesEnum';
+import type { HomeRoutesParams } from '@onekeyhq/kit/src/routes/types';
+
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type NavigationProps = NativeStackNavigationProp<
+  HomeRoutesParams,
+  HomeRoutes.PushNotification
+>;
+
+export const AdvancedSection = () => {
+  const intl = useIntl();
+  const navigation = useNavigation<NavigationProps>();
+  const { themeVariant } = useTheme();
+
+  return (
+    <Box w="full" mb="6">
+      <Box pb="2">
+        <Typography.Subheading color="text-subdued">
+          {intl.formatMessage({
+            id: 'form__advanced__uppercase',
+            defaultMessage: 'ADVANCED',
+          })}
+        </Typography.Subheading>
+      </Box>
+      <Box
+        borderRadius="12"
+        bg="surface-default"
+        borderWidth={themeVariant === 'light' ? 1 : undefined}
+        borderColor="border-subdued"
+      >
+        <Pressable
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          py={4}
+          px={{ base: 4, md: 6 }}
+          onPress={() => {
+            navigation.navigate(HomeRoutes.PushNotification);
+          }}
+        >
+          <Icon name="WrenchScrewdriverOutline" />
+          <Text
+            typography={{ sm: 'Body1Strong', md: 'Body2Strong' }}
+            flex="1"
+            numberOfLines={1}
+            mx={3}
+          >
+            {intl.formatMessage({
+              id: 'form__advanced',
+            })}
+          </Text>
+          <Box>
+            <Icon name="ChevronRightMini" color="icon-subdued" size={20} />
+          </Box>
+        </Pressable>
+      </Box>
+    </Box>
+  );
+};
