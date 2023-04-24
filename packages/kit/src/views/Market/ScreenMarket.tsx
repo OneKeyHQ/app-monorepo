ba4acaa74f5dedb9dc1ba823ff80b90c15db6c89
sunnylqm <sunnylqm@onekey.so>
2023-04-24 15:07:43 +0800

    fix: Revert "feat: market tab swipe OK-16256 (#2768)"
    This reverts commit cf799298a13fbf14c064caa7a496a63617ed6629.

--- packages/kit/src/views/Market/ScreenMarket.tsx
+++ packages/kit/src/views/Market/ScreenMarket.tsx
@@ -3,5 +3,5 @@ import { TabRoutes } from '../../routes/routesEnum';
 import { ScreenMarketOrSwap } from './ScreenMarketOrSwap';
 
 export function ScreenMarket() {
-  return <ScreenMarketOrSwap desktopTabName={TabRoutes.Market} />;
+  return <ScreenMarketOrSwap marketTopTabName={TabRoutes.Market} />;
 }
