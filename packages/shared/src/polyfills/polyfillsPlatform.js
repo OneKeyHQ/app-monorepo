f04c2e4cfc087096eb2c5aa59dc199622d08f7ba
Sunny Luo <sunnylqm@gmail.com>
2023-05-09 18:02:50 +0800

    feat: optimize polyfill and change stc maxGasAmout  (#2956)
    * fix: optimize polyfill
    
    * fix: update stc gas

--- packages/shared/src/polyfills/polyfillsPlatform.js
+++ packages/shared/src/polyfills/polyfillsPlatform.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable global-require, no-restricted-syntax, import/no-unresolved,  @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 require('./intlShim');
+
+const shimsLog = (str) => console.log(`Shims Injected: ${str}`);
 
 if (typeof __dirname === 'undefined') global.__dirname = '/';
 if (typeof __filename === 'undefined') global.__filename = '';
 if (typeof process === 'undefined') {
+  shimsLog('process');
   global.process = require('process');
 } else {
   const bProcess = require('process');
@@ -17,15 +21,18 @@ if (typeof process === 'undefined') {
 
 // TextEncoder and TextDecoder polyfill for starcoin
 if (typeof TextDecoder === 'undefined') {
+  shimsLog('TextDecoder');
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   global.TextDecoder = require('text-encoding').TextDecoder;
 }
 if (typeof TextEncoder === 'undefined') {
+  shimsLog('TextEncoder');
   global.TextEncoder = require('text-encoding').TextEncoder;
 }
 
 // Buffer polyfill
 if (typeof Buffer === 'undefined') {
+  shimsLog('Buffer');
   // global.Buffer = require('@craftzdog/react-native-buffer').Buffer;
   global.Buffer = require('buffer').Buffer;
 }
@@ -34,6 +41,7 @@ if (typeof Buffer === 'undefined') {
 
 if (typeof crypto === 'undefined') {
   try {
+    shimsLog('crypto');
     // eslint-disable-next-line no-const-assign
     global.crypto = require('crypto');
   } catch (error) {
@@ -52,7 +60,66 @@ Shims Injected:
   - nextTick
   - FileReader.prototype.readAsArrayBuffer
  */
-require('@ethersproject/shims');
+// Shim atob and btoa
+const { Base64 } = require('js-base64');
+
+if (!global.atob) {
+  shimsLog('atob');
+  global.atob = Base64.atob;
+}
+if (!global.btoa) {
+  shimsLog('btoa');
+  global.btoa = Base64.btoa;
+}
+
+// Shim ArrayBuffer.isView
+if (!ArrayBuffer.isView) {
+  shimsLog('ArrayBuffer.isView');
+  ArrayBuffer.isView = function (obj) {
+    // @TODO: This should probably check various instanceof aswell
+    return !!obj.buffer;
+  };
+}
+
+// Shim nextTick
+if (!global.nextTick) {
+  shimsLog('nextTick');
+  global.nextTick = function (callback) {
+    setTimeout(callback, 0);
+  };
+}
+
+// Shim FileReader.readAsArrayBuffer
+// https://github.com/facebook/react-native/issues/21209
+try {
+  const fr = new FileReader();
+  try {
+    fr.readAsArrayBuffer(new Blob(['hello'], { type: 'text/plain' }));
+  } catch (error) {
+    shimsLog('FileReader.prototype.readAsArrayBuffer');
+    FileReader.prototype.readAsArrayBuffer = function (blob) {
+      if (this.readyState === this.LOADING) {
+        throw new Error('InvalidStateError');
+      }
+      this._setReadyState(this.LOADING);
+      this._result = null;
+      this._error = null;
+      const fr = new FileReader();
+      fr.onloadend = () => {
+        const content = atob(fr.result.split(',').pop().trim());
+        const buffer = new ArrayBuffer(content.length);
+        const view = new Uint8Array(buffer);
+        view.set(Array.from(content).map((c) => c.charCodeAt(0)));
+        this._result = buffer;
+        this._setReadyState(this.DONE);
+      };
+      fr.readAsDataURL(blob);
+    };
+  }
+} catch (error) {
+  console.log('Missing FileReader; unsupported platform');
+}
+
 const platformEnv = require('@onekeyhq/shared/src/platformEnv');
 
 if (platformEnv.isNativeAndroid) {
