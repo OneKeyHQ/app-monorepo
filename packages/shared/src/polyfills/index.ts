379763cbb17c90103be1388b310617c5d50649f4
sunnylqm <sunnylqm@onekey.so>
2023-05-09 11:44:37 +0800

    fix: optimize polyfill

--- packages/shared/src/polyfills/index.ts
+++ packages/shared/src/polyfills/index.ts
@@ -1,14 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access, import/order, @typescript-eslint/no-unsafe-call */
-import platformEnv from '../platformEnv';
-
-if (platformEnv.isExtension) {
-  require('./polyfillsPlatform.ext');
-} else if (platformEnv.isNative) {
-  require('./polyfillsPlatform.native');
-} else {
-  // auto select platform extension file not working, so we use explicit require here
-  require('./polyfillsPlatform');
-}
+import './polyfillsPlatform';
 
 const { normalizeRequestLibs } = require('../request/normalize');
 const timerUtils = require('../utils/timerUtils').default;
