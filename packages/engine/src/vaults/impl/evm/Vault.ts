121b37ed5d7e80f246342998f3805dfbe75f9b67
qwang1113 <qwang@onekey.so>
2023-05-18 21:47:29 +0800

    feat: add checkRpcBatchSupport for rpcBatch check & rpc status check

--- packages/engine/src/vaults/impl/evm/Vault.ts
+++ packages/engine/src/vaults/impl/evm/Vault.ts
@@ -1977,7 +1977,7 @@ export default class Vault extends VaultBase {
           [
             'eth_getBalance',
             // fake address
-            ['0xf44371ccc370662734cfc4b78b1beadf7012bc5d', 'latest'],
+            ['0x0000000000000000000000000000000000000000', 'latest'],
           ],
           ['eth_gasPrice', []],
         ],
