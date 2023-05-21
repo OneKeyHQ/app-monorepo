312e2870c04c335e0f1bce46e8c4f8aeb0e22cdb
sunnylqm <sunnylqm@onekey.so>
2023-05-21 20:59:31 +0800

    fix: lint

--- packages/kit/src/components/NetworkAccountSelector/triggers/BaseSelectorTrigger.tsx
+++ packages/kit/src/components/NetworkAccountSelector/triggers/BaseSelectorTrigger.tsx
@@ -29,7 +29,7 @@ export interface INetworkAccountSelectorTriggerProps
 interface IBaseSelectorTriggerProps extends ISelectorTriggerSharedProps {
   onPress?: () => void;
   icon: any;
-  label: string;
+  label: string | boolean;
   labelTypography?: TypographyStyle;
   description?: string | boolean;
   subDescription?: string;
