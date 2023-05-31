b2d8a56db0bcb907986c8bcd764f3b63346344c8
huhuanming <huanming@onekey.so>
2023-05-31 17:22:35 +0800

    fix: fix near unitest

--- packages/engine/src/vaults/impl/near/keyring/KeyringWatching.test.ts
+++ packages/engine/src/vaults/impl/near/keyring/KeyringWatching.test.ts
@@ -24,54 +24,54 @@ describe('Near KeyringImported Tests', () => {
       },
     );
 
-    // await testPrepareAccounts(
-    //   {
-    //     dbNetwork: network,
-    //     dbAccount: watchingAccount2.account,
-    //     privateKey: watchingAccount2.privateKey,
-    //     password: watchingAccount2.password,
-    //     accountIdPrefix: 'external',
-    //   },
-    //   {
-    //     keyring({ vault }) {
-    //       return new KeyringWatching(vault);
-    //     },
-    //   },
-    // );
+    await testPrepareAccounts(
+      {
+        dbNetwork: network,
+        dbAccount: watchingAccount2.account,
+        privateKey: watchingAccount2.privateKey,
+        password: watchingAccount2.password,
+        accountIdPrefix: 'external',
+      },
+      {
+        keyring({ vault }) {
+          return new KeyringWatching(vault);
+        },
+      },
+    );
 
-    // await testPrepareAccounts(
-    //   {
-    //     dbNetwork: network,
-    //     dbAccount: watchingAccount3.account,
-    //     privateKey: watchingAccount3.privateKey,
-    //     password: watchingAccount3.password,
-    //     accountIdPrefix: 'external',
-    //   },
-    //   {
-    //     keyring({ vault }) {
-    //       return new KeyringWatching(vault);
-    //     },
-    //   },
-    // );
+    await testPrepareAccounts(
+      {
+        dbNetwork: network,
+        dbAccount: watchingAccount3.account,
+        privateKey: watchingAccount3.privateKey,
+        password: watchingAccount3.password,
+        accountIdPrefix: 'external',
+      },
+      {
+        keyring({ vault }) {
+          return new KeyringWatching(vault);
+        },
+      },
+    );
 
-    // try {
-    //   await testPrepareAccounts(
-    //     {
-    //       dbNetwork: network,
-    //       dbAccount: {
-    //         ...watchingAccount3.account,
-    //         address: '&^%$#891',
-    //       },
-    //     },
-    //     {
-    //       keyring({ vault }) {
-    //         return new KeyringWatching(vault);
-    //       },
-    //     },
-    //   );
-    // } catch (error: any) {
-    //   expect(error.message).toBe('InvalidAddress.');
-    // }
+    try {
+      await testPrepareAccounts(
+        {
+          dbNetwork: network,
+          dbAccount: {
+            ...watchingAccount3.account,
+            address: '&^%$#891',
+          },
+        },
+        {
+          keyring({ vault }) {
+            return new KeyringWatching(vault);
+          },
+        },
+      );
+    } catch (error: any) {
+      expect(error.message).toBe('InvalidAddress.');
+    }
   });
 });
 
