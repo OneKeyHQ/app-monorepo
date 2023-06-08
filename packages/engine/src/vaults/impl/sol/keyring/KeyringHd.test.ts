c599431db02ba92d8b2ee8ad934367d0cdca1f49
huhuanming <huanming@onekey.so>
2023-06-08 15:56:15 +0800

    test: add ledgerLive type

--- packages/engine/src/vaults/impl/sol/keyring/KeyringHd.test.ts
+++ packages/engine/src/vaults/impl/sol/keyring/KeyringHd.test.ts
@@ -1,10 +1,11 @@
+import { IMPL_SOL } from '@onekeyhq/shared/src/engine/engineConsts';
+
+import { getAccountNameInfoByImpl } from '../../../../managers/impl';
 import solMockData from '../@tests/solMockData';
 
 import { KeyringHd } from './KeyringHd';
 
 import type { SOLPresetCaseType } from '../@tests/solPresetCase';
-import { getAccountNameInfoByImpl } from '../../../../managers/impl';
-import { IMPL_SOL } from '@onekeyhq/shared/src/engine/engineConsts';
 
 jest.setTimeout(3 * 60 * 1000);
 
@@ -14,7 +15,7 @@ describe('Solana KeyringHd Tests', () => {
   beforeAll(() => {
     jest.mock('../../../../managers/nft.ts', () => ({}));
   });
-  it('solana prepareAccounts with default coinType', async () => {
+  it('solana prepareAccounts with default coinType BIP44', async () => {
     const { testPrepareAccounts } =
       require('../@tests/solPresetCase') as SOLPresetCaseType;
     const { network, hdAccount1 } = solMockData;
@@ -34,6 +35,31 @@ describe('Solana KeyringHd Tests', () => {
         accounts: hdAccount1.accounts || [],
         coinType: solAccountNameInfo.default.coinType,
         template: solAccountNameInfo.default.template,
+        indexes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      },
+    );
+  });
+
+  it('solana prepareAccounts with ledgerLive coinType', async () => {
+    const { testPrepareAccounts } =
+      require('../@tests/solPresetCase') as SOLPresetCaseType;
+    const { network, hdAccount2 } = solMockData;
+    await testPrepareAccounts(
+      {
+        dbNetwork: network,
+        dbAccount: hdAccount2.account,
+        mnemonic: hdAccount2.mnemonic,
+        password: hdAccount2.password,
+      },
+      {
+        keyring({ vault }) {
+          return new KeyringHd(vault);
+        },
+      },
+      {
+        accounts: hdAccount2.accounts || [],
+        coinType: solAccountNameInfo.ledgerLive.coinType,
+        template: solAccountNameInfo.ledgerLive.template,
         indexes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
       },
     );
