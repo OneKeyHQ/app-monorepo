cbde29145bfb43141bb994e20baaa2217ef42963
huhuanming <huanming@onekey.so>
2023-06-22 00:47:57 +0800

    fix: fix provider

--- packages/engine/src/vaults/impl/nexa/KeyringHd.ts
+++ packages/engine/src/vaults/impl/nexa/KeyringHd.ts
@@ -24,70 +24,70 @@ import type { IPrepareSoftwareAccountsParams } from '../../types';
 import type BTCForkVault from '../../utils/btcForkChain/VaultBtcFork';
 
 export class KeyringHd extends KeyringHdBtcFork {
-  override async prepareAccounts(
-    params: IPrepareSoftwareAccountsParams,
-  ): Promise<DBUTXOAccount[]> {
-    const curve: CurveName = 'secp256k1';
-    const accountNamePrefix = 'NEAR';
-    const hardened = true;
+  // override async prepareAccounts(
+  //   params: IPrepareSoftwareAccountsParams,
+  // ): Promise<DBUTXOAccount[]> {
+  //   const curve: CurveName = 'secp256k1';
+  //   const accountNamePrefix = 'NEAR';
+  //   const hardened = true;
 
-    const { password, indexes, names, template } = params;
-    const addressIndex = 0;
-    const { seed } = (await this.engine.dbApi.getCredential(
-      this.walletId,
-      password,
-    )) as ExportedSeedCredential;
+  //   const { password, indexes, names, template } = params;
+  //   const addressIndex = 0;
+  //   const { seed } = (await this.engine.dbApi.getCredential(
+  //     this.walletId,
+  //     password,
+  //   )) as ExportedSeedCredential;
 
-    const vault = this.vault as unknown as BTCForkVault;
-    const defaultPurpose = vault.getDefaultPurpose();
-    const coinName = vault.getCoinName();
-    const impl = await this.getNetworkImpl();
-    const { prefix: namePrefix } = getAccountNameInfoByTemplate(impl, template);
-    const { pathPrefix } = slicePathTemplate(template);
-    const pubkeyInfos = batchGetPublicKeys(
-      curve,
-      seed,
-      password,
-      pathPrefix,
-      indexes.map((index) => `${index}${hardened ? "'" : ''}`),
-    );
+  //   const vault = this.vault as unknown as BTCForkVault;
+  //   const defaultPurpose = vault.getDefaultPurpose();
+  //   const coinName = vault.getCoinName();
+  //   const impl = await this.getNetworkImpl();
+  //   const { prefix: namePrefix } = getAccountNameInfoByTemplate(impl, template);
+  //   const { pathPrefix } = slicePathTemplate(template);
+  //   const pubkeyInfos = batchGetPublicKeys(
+  //     curve,
+  //     seed,
+  //     password,
+  //     pathPrefix,
+  //     indexes.map((index) => `${index}${hardened ? "'" : ''}`),
+  //   );
 
-    if (pubkeyInfos.length !== indexes.length) {
-      throw new OneKeyInternalError('Unable to get publick key.');
-    }
-    const prefix = coinName;
-    const ret = [];
-    let index = 0;
-    const isChange = false;
-    for (const info of pubkeyInfos) {
-      const {
-        path,
-        extendedKey: { key: pubkey },
-      } = info;
-      const address = pubkey.toString('hex');
-      console.log(address);
-      const name =
-        (names || [])[index] || `${accountNamePrefix} #${indexes[index] + 1}`;
-      const addressRelPath = `${isChange ? '1' : '0'}/${addressIndex}`;
-      const encodeAddress = new PublicKey(address, {
-        network: 'nexatest',
-      })
-        .toAddress()
-        .toNexaAddress();
-      ret.push({
-        id: `${this.walletId}--${path}`,
-        name,
-        type: AccountType.UTXO,
-        path,
-        coinType: COIN_TYPE,
-        xpub: '',
-        address: encodeAddress,
-        addresses: { [addressRelPath]: encodeAddress },
-        customAddresses: { [addressRelPath]: encodeAddress },
-        template,
-      });
-      index += 1;
-    }
-    return ret;
-  }
+  //   if (pubkeyInfos.length !== indexes.length) {
+  //     throw new OneKeyInternalError('Unable to get publick key.');
+  //   }
+  //   const prefix = coinName;
+  //   const ret = [];
+  //   let index = 0;
+  //   const isChange = false;
+  //   for (const info of pubkeyInfos) {
+  //     const {
+  //       path,
+  //       extendedKey: { key: pubkey },
+  //     } = info;
+  //     const address = pubkey.toString('hex');
+  //     console.log(address);
+  //     const name =
+  //       (names || [])[index] || `${accountNamePrefix} #${indexes[index] + 1}`;
+  //     const addressRelPath = `${isChange ? '1' : '0'}/${addressIndex}`;
+      // const encodeAddress = new PublicKey(address, {
+      //   network: 'nexatest',
+      // })
+      //   .toAddress()
+      //   .toNexaAddress();
+  //     ret.push({
+  //       id: `${this.walletId}--${path}`,
+  //       name,
+  //       type: AccountType.UTXO,
+  //       path,
+  //       coinType: COIN_TYPE,
+  //       xpub: '',
+  //       address: encodeAddress,
+  //       addresses: { [addressRelPath]: encodeAddress },
+  //       customAddresses: { [addressRelPath]: encodeAddress },
+  //       template,
+  //     });
+  //     index += 1;
+  //   }
+  //   return ret;
+  // }
 }
--- packages/engine/src/vaults/impl/nexa/provider.ts
+++ packages/engine/src/vaults/impl/nexa/provider.ts
@@ -1,12 +1,11 @@
 import * as bchaddrjs from 'bchaddrjs';
 import * as BitcoinForkJS from 'bitcoinforkjs';
-import { encode } from 'nexaaddrjs';
+import { PublicKey } from 'nexcore-lib';
 
 import { Provider as BaseProvider } from '../../utils/btcForkChain/provider';
+import { AddressEncodings } from '../../utils/btcForkChain/types';
 
-import { decodeBase58Address } from './utils';
-
-import type { Psbt } from 'bitcoinjs-lib';
+import type { Payment, Psbt } from 'bitcoinjs-lib';
 
 export default class Provider extends BaseProvider {
   override decodeAddress(address: string): string {
@@ -23,19 +22,34 @@ export default class Provider extends BaseProvider {
     return address;
   }
 
-  override encodeAddress(address: string): string {
-    if (!bchaddrjs.isValidAddress(address)) {
-      throw new Error(`Invalid address: ${address}`);
+  override pubkeyToPayment(
+    pubkey: Buffer,
+    encoding: AddressEncodings,
+  ): Payment {
+    const payment: Payment = {
+      pubkey,
+      network: this.network,
+    };
+    console.log(this.network);
+    switch (encoding) {
+      case AddressEncodings.P2PKH:
+        const address = new PublicKey(Buffer.from(pubkey).toString('hex'), {
+          network: 'nexatest',
+        })
+          .toAddress()
+          .toNexaAddress();
+        return {
+          pubkey,
+          address,
+        };
+
+      default:
+        throw new Error(`Invalid encoding: ${encoding as string}`);
     }
-    // if (!bchaddrjs.isCashAddress(address)) {
-    //   return bchaddrjs.toCashAddress(address);
-    // }
-    const { hash, network, type } = decodeBase58Address(address);
-    return encode(
-      network === 'testnet' ? 'nexatest' : 'nexa',
-      type?.toUpperCase(),
-      Buffer.from(hash),
-    );
+  }
+
+  override encodeAddress(address: string): string {
+    return address;
   }
 
   override getPsbt(): Psbt {
