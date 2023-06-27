da609f5bc805f6c3f70f9df194187d98698cbd93
loatheb <zhangzhao@onekey.so>
2023-06-27 19:42:26 +0800

    fix: android background to foreground issue

--- packages/kit/src/components/AppStatusActiveListener/index.native.tsx
+++ packages/kit/src/components/AppStatusActiveListener/index.native.tsx
@@ -1,30 +1,12 @@
 import type { FC } from 'react';
-import { useCallback, useEffect, useRef } from 'react';
 
-import { AppState } from 'react-native';
-
-import type { AppStateStatus } from 'react-native';
+import { useAppStateChange } from '@onekeyhq/kit/src/hooks/useAppStateChange';
 
 type AppStatusActiveListenerProps = { onActive: () => void };
 
 export const AppStatusActiveListener: FC<AppStatusActiveListenerProps> = ({
   onActive,
 }) => {
-  const appState = useRef(AppState.currentState);
-  const onChange = useCallback(
-    (nextState: AppStateStatus) => {
-      if (appState.current === 'background' && nextState === 'active') {
-        onActive?.();
-      }
-      appState.current = nextState;
-    },
-    [onActive],
-  );
-  useEffect(() => {
-    const subscription = AppState.addEventListener('change', onChange);
-    return () => {
-      subscription.remove();
-    };
-  }, [onChange]);
+  useAppStateChange(onActive);
   return null;
 };
