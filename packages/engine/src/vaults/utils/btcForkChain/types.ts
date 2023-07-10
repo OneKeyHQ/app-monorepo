0ff81959d44b7d2681995430f625dc75167ed43f
Leon <lixiao@onekey.so>
2023-07-10 11:51:46 +0800

    fix: support multi sign address

--- packages/engine/src/vaults/utils/btcForkChain/types.ts
+++ packages/engine/src/vaults/utils/btcForkChain/types.ts
@@ -16,6 +16,7 @@ export enum AddressEncodings {
   P2PKH = 'P2PKH', // Legacy BIP-44
   P2SH_P2WPKH = 'P2SH_P2WPKH', // BIP-49 P2WPKH nested in P2SH
   P2WPKH = 'P2WPKH', // BIP-84 P2WPKH
+  P2WSH = 'P2WSH', // BIP-84 P2WSH
   P2TR = 'P2TR', // BIP-86 P2TR
 }
 
