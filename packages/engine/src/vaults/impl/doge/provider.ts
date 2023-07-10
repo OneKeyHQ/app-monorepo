797eac62e5322dc658cf4f86acdc380cc2b4201a
Leon <lixiao@onekey.so>
2023-07-10 14:54:13 +0800

    fix: change doge maximum fee rate

---
+++ packages/engine/src/vaults/impl/doge/provider.ts
@@ -0,0 +1,14 @@
+import * as BitcoinJS from 'bitcoinjs-lib';
+
+import { Provider as BaseProvider } from '../../utils/btcForkChain/provider';
+
+import type { Psbt } from 'bitcoinjs-lib';
+
+export default class Provider extends BaseProvider {
+  override getPsbt(): Psbt {
+    return new BitcoinJS.Psbt({
+      network: this.network,
+      maximumFeeRate: 10000,
+    });
+  }
+}
