cf2e3be58d8a7ab6a109557692fde2a6ddac8398
huhuanming <huanming@onekey.so>
2023-07-11 02:44:29 +0800

    refactor: add nexa_ws proxy

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -33,14 +33,6 @@ module.exports = async function (env, argv) {
     config.devtool = false;
   }
 
-  if (config.devServer) {
-    config.devServer.proxy = config.devServer.proxy || {};
-    config.devServer.proxy['/nexa_ws'] = {
-      target: 'wss://testnet-explorer.nexa.org:30004',
-      changeOrigin: true,
-      ws: true,
-    };
-  }
   devUtils.writePreviewWebpackConfigJson(config, 'webpack.config.preview.json');
   return config;
 };
