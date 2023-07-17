24a024574c25b160b4ecb6af1458ea9b079a1cea
weatherstar <weather@onekey.so>
2023-07-17 11:38:39 +0800

    feat: add DApp security info tooltip

--- packages/kit/src/views/Send/components/DappSecurityView.tsx
+++ packages/kit/src/views/Send/components/DappSecurityView.tsx
@@ -3,7 +3,16 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { useIntl } from 'react-intl';
 
-import { Box, HStack, Icon, Typography, VStack } from '@onekeyhq/components';
+import {
+  Box,
+  HStack,
+  Icon,
+  Pressable,
+  RichTooltip,
+  Text,
+  Typography,
+  VStack,
+} from '@onekeyhq/components';
 import type { LocaleIds } from '@onekeyhq/components/src/locale';
 import type {
   GoPlusDappContract,
@@ -11,6 +20,9 @@ import type {
 } from '@onekeyhq/engine/src/types/goplus';
 
 import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
+import { openUrlExternal } from '../../../utils/openUrl';
+
+const GOPLUS_FEEDBACK_URL = 'https://feedback.gopluslabs.io/';
 
 // @ts-ignore
 const localeMaps: Record<
@@ -43,17 +55,67 @@ export const DappSecurityView: FC<{
     fetchSecurityInfo();
   }, [fetchSecurityInfo]);
 
+  const GoplusFeedbackLink = useCallback(
+    (text) => (
+      <Text
+        color="text-subdued"
+        onPress={() => openUrlExternal(GOPLUS_FEEDBACK_URL)}
+        fontSize="10px"
+        typography="Body2Underline"
+      >
+        {text}
+      </Text>
+    ),
+    [],
+  );
+
   const dappIcon = useMemo(() => {
     if (typeof securityItems === 'undefined') {
       return <Icon name="QuestionMarkCircleMini" size={32} />;
     }
-    if (securityItems?.length === 0) {
-      return <Icon name="BadgeCheckMini" size={32} color="icon-success" />;
-    }
+
     return (
-      <Icon name="ShieldExclamationMini" size={32} color="icon-critical" />
+      <RichTooltip
+        // eslint-disable-next-line react/no-unstable-nested-components
+        trigger={({ ...props }) => (
+          <Pressable {...props}>
+            {securityItems.length === 0 ? (
+              <Icon name="BadgeCheckMini" size={32} color="icon-success" />
+            ) : (
+              <Icon
+                name="ShieldExclamationMini"
+                size={32}
+                color="icon-critical"
+              />
+            )}
+          </Pressable>
+        )}
+        bodyProps={{
+          children: (
+            <>
+              {securityItems.length === 0 ? (
+                <Text typography="Caption" fontSize="14px" mb={1}>
+                  {intl.formatMessage({ id: 'title__verified_site' })}
+                </Text>
+              ) : null}
+              <HStack space={1} alignItems="center">
+                <Text color="text-subdued" fontSize="10px">
+                  {intl.formatMessage(
+                    { id: 'content__provided_by_str' },
+                    { source: 'GoPlus' },
+                  )}
+                  .
+                </Text>
+                {GoplusFeedbackLink(
+                  intl.formatMessage({ id: 'action__report' }),
+                )}
+              </HStack>
+            </>
+          ),
+        }}
+      />
     );
-  }, [securityItems]);
+  }, [GoplusFeedbackLink, intl, securityItems]);
 
   return (
     <>
