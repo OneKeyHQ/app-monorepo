ccef56645a464d135c48e4554ccf30b41e099fab
huhuanming <huanming@onekey.so>
2023-07-18 16:22:12 +0800

    fix: fix lint

--- packages/shared/src/logger/autoLogger.ts
+++ packages/shared/src/logger/autoLogger.ts
@@ -3,7 +3,7 @@ import { NotAutoPrintError } from '../errors/common-errors';
 let prevErrorStack: string | undefined;
 
 const autoLogger = {
-  error: (error: Error, ...messages) => {
+  error: (error: Error, ...messages: unknown[]) => {
     if (
       error.stack !== prevErrorStack &&
       !(error instanceof NotAutoPrintError)
