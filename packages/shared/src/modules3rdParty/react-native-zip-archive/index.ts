153f5076b7d9f325851f474c2dd60068face70d9
huhuanming <huanming@onekey.so>
2023-07-18 23:24:46 +0800

    fix: fix type

--- packages/shared/src/modules3rdParty/react-native-zip-archive/index.ts
+++ packages/shared/src/modules3rdParty/react-native-zip-archive/index.ts
@@ -1,8 +1,8 @@
 import type { zip as ZipType } from 'react-native-zip-archive';
 
-export const zip = (() => {
+export const zip: typeof ZipType = () => {
   if (process.env.NODE_ENV !== 'production') {
     console.warn('The zip method is not supported on the this platform.');
   }
   return Promise.resolve('');
-}) as typeof ZipType;
+};
