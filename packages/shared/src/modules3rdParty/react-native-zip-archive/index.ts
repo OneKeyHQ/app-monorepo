db7bca9afd953eec2592ed7e4a48083a457e4133
huhuanming <huanming@onekey.so>
2023-07-18 23:01:07 +0800

    refactor: to 0

--- packages/shared/src/modules3rdParty/react-native-zip-archive/index.ts
+++ packages/shared/src/modules3rdParty/react-native-zip-archive/index.ts
@@ -1,3 +1,3 @@
 import type { zip as ZipType } from 'react-native-zip-archive';
 
-export const zip = (() => undefined) as unknown as typeof ZipType;
+export const zip = 0 as unknown as typeof ZipType;
