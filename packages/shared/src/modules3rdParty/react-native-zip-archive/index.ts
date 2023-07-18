f4c400cb4b6eed78d362b73db0f796eb5c33fb02
huhuanming <huanming@onekey.so>
2023-07-18 23:22:01 +0800

    docs: add warning

--- packages/shared/src/modules3rdParty/react-native-zip-archive/index.ts
+++ packages/shared/src/modules3rdParty/react-native-zip-archive/index.ts
@@ -1,3 +1,8 @@
 import type { zip as ZipType } from 'react-native-zip-archive';
 
-export const zip = 0 as unknown as typeof ZipType;
+export const zip = (() => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn('The zip method is not supported on the this platform.');
+  }
+  return Promise.resolve('');
+}) as typeof ZipType;
