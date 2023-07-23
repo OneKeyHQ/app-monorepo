173c330e937d38a39550dd13842aa8065b71ab37
qwang1113 (王强) <15064266+qwang1113@users.noreply.github.com>
2023-07-23 23:32:23 +0800

    Fix/all networks issues, OK-21724 (#3282)
    * fix: allNetworks token details ui
    
    * fix: use appSelector issues
    
    * fix: OK-21724
    
    * fix: lint

--- packages/kit/src/views/Swap/ScreenSwap.tsx
+++ packages/kit/src/views/Swap/ScreenSwap.tsx
@@ -1,6 +1,7 @@
+import { memo } from 'react';
+
 import { TabRoutes } from '../../routes/routesEnum';
 import { ScreenMarketOrSwap } from '../Market/ScreenMarketOrSwap';
-import { memo } from 'react';
 
 const ScreenSwap = () => <ScreenMarketOrSwap routeName={TabRoutes.Swap} />;
 
