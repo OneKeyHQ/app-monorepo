c1dbfe05574745fe0aa0fa51b5cd04b6f0260547
qwang1113 <qwang@onekey.so>
2023-07-26 20:28:54 +0800

    fix: OK-21843

--- packages/kit/src/hooks/useOverview.ts
+++ packages/kit/src/hooks/useOverview.ts
@@ -211,14 +211,8 @@ export function useAccountTokensOnChain(
   accountId = '',
   useFilter = false,
 ) {
-  const {
-    hideRiskTokens,
-    hideSmallBalance,
-    putMainTokenOnTop,
-    selectedFiatMoneySymbol,
-  } = useAppSelector((s) => s.settings);
-  const fiatMap = useAppSelector((s) => s.fiatMoney.map);
-  const fiat = fiatMap[selectedFiatMoneySymbol]?.value || 0;
+  const { hideRiskTokens, hideSmallBalance, putMainTokenOnTop } =
+    useAppSelector((s) => s.settings);
   const tokens =
     useAppSelector((s) => s.tokens.accountTokens?.[networkId]?.[accountId]) ??
     [];
@@ -231,16 +225,16 @@ export function useAccountTokensOnChain(
   const valueTokens = tokens.map((t) => {
     const priceInfo =
       prices[`${networkId}${t.address ? '-' : ''}${t.address ?? ''}`];
-    const price = priceInfo?.[selectedFiatMoneySymbol] ?? 0;
-    const price24h = priceInfo?.[`${selectedFiatMoneySymbol}_24h_change`] ?? 0;
+    const price = priceInfo?.usd ?? 0;
+    const price24h = priceInfo?.usd_24h_change ?? 0;
     const balance = balances[getBalanceKey(t)]?.balance ?? '0';
     const value = new B(price).multipliedBy(balance);
-    const usdValue = fiat === 0 ? 0 : value.div(fiat);
+    const usdValue = value;
     const value24h = new B(balance).multipliedBy(
       getPreBaseValue({
         priceInfo,
-        vsCurrency: selectedFiatMoneySymbol,
-      })[selectedFiatMoneySymbol] ?? 0,
+        vsCurrency: 'usd',
+      }).usd ?? 0,
     );
     const info = {
       ...t,
@@ -371,6 +365,8 @@ export function useAccountTokens({
     accountId,
     false,
   );
+
+  console.log(accountTokensOnChain);
 
   const accountTokensLoading = useAccountTokenLoading(networkId, accountId);
 
