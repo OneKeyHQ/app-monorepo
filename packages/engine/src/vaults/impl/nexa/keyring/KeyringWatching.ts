6a85e380da4116b60e3fd141427850f812752d6e
huhuanming <huanming@onekey.so>
2023-07-28 11:58:26 +0800

    fix: importing non-identical network addresses is not allowed.(OK-21922)

--- packages/engine/src/vaults/impl/nexa/keyring/KeyringWatching.ts
+++ packages/engine/src/vaults/impl/nexa/keyring/KeyringWatching.ts
@@ -3,7 +3,7 @@ import { COINTYPE_NEXA as COIN_TYPE } from '@onekeyhq/shared/src/engine/engineCo
 import { InvalidAddress } from '../../../../errors';
 import { AccountType } from '../../../../types/account';
 import { KeyringWatchingBase } from '../../../keyring/KeyringWatchingBase';
-import { verifyNexaAddress } from '../utils';
+import { getNexaNetworkInfo, verifyNexaAddress } from '../utils';
 
 import type { DBSimpleAccount } from '../../../../types/account';
 import type { IPrepareWatchingAccountsParams } from '../../../types';
@@ -15,7 +15,14 @@ export class KeyringWatching extends KeyringWatchingBase {
     const { name, target: address, accountIdPrefix } = params;
     let normalizedAddress = '';
     let accountType = AccountType.SIMPLE;
+
     if (address.startsWith('nexa')) {
+      const addressPrefix = address.split(':')[0];
+      const chainId = await this.vault.getNetworkChainId();
+      const network = getNexaNetworkInfo(chainId);
+      if (network.prefix !== addressPrefix) {
+        throw new InvalidAddress();
+      }
       const { normalizedAddress: nexaAddress, isValid } =
         verifyNexaAddress(address);
       normalizedAddress = nexaAddress || '';
