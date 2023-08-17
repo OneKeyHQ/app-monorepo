a28e675d4239f742c0fa5e9a866295dd67ef5736
morizon <sidmorizon@outlook.com>
2023-08-17 09:53:33 +0800

    fix: deepfresh demo

--- packages/kit/src/views/Components/stories/DemoDeepFresh.tsx
+++ packages/kit/src/views/Components/stories/DemoDeepFresh.tsx
@@ -1,16 +1,14 @@
-import { memo, useCallback, useEffect, useRef } from 'react';
+import { memo, useEffect, useMemo } from 'react';
 
 import BigNumber from 'bignumber.js';
-import { isEqual, random, shuffle } from 'lodash';
-import { boolean } from 'superstruct';
+import { isEqual, random, sample, shuffle } from 'lodash';
 
 import {
   Box,
   Button,
-  Center,
   ScrollView,
+  Stack,
   Typography,
-  useThemeValue,
 } from '@onekeyhq/components';
 import { DebugRenderTracker } from '@onekeyhq/components/src/DebugRenderTracker';
 
@@ -20,15 +18,31 @@ import {
 } from '../../../store/jotai/createJotaiContext';
 import { wait } from '../../../utils/helper';
 
+const OWNED_TOKENS = [
+  [
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    '0x1f068a896560632a4d2e05044bd7f03834f1a465',
+  ],
+  [
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    '0x4d224452801aced8b2f0aebe155379bb5d594381',
+  ],
+  [
+    '0x630fe3adb53f3d2e0c594bc180309fdfdd0a854d',
+    '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0',
+  ],
+];
+
 async function mockBackgroundTokensFetch({
   shouldShuffle,
 }: {
   shouldShuffle: boolean;
 }) {
   console.log('mockBackgroundTokensFetch ------------===========');
-  let items: DemoDeepFreshToken[] = [
+  let items: IDemoDeepFreshToken[] = [
     {
       networkId: 'evm--1',
+      accountId: "hd-1--m/44'/60'/0'/0/0",
       address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
       name: 'BTC',
       balance: random(1, 2).toString(),
@@ -36,6 +50,7 @@ async function mockBackgroundTokensFetch({
     },
     {
       networkId: 'evm--2',
+      accountId: "hd-2--m/44'/60'/0'/0/0",
       address: '0x4d224452801aced8b2f0aebe155379bb5d594381',
       name: 'USDT',
       balance: random(50, 51).toString(),
@@ -43,6 +58,7 @@ async function mockBackgroundTokensFetch({
     },
     {
       networkId: 'evm--3',
+      accountId: "hd-1--m/44'/60'/0'/0/0",
       address: '0x1f068a896560632a4d2e05044bd7f03834f1a465',
       name: 'ADA',
       balance: random(1, 100).toString(),
@@ -50,6 +66,7 @@ async function mockBackgroundTokensFetch({
     },
     {
       networkId: 'evm--4',
+      accountId: "hd-1--m/44'/60'/0'/0/0",
       address: '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0',
       name: 'MATIC',
       balance: random(1, 100).toString(),
@@ -57,6 +74,7 @@ async function mockBackgroundTokensFetch({
     },
     {
       networkId: 'evm--5',
+      accountId: "hd-1--m/44'/60'/0'/0/0",
       address: '0x630fe3adb53f3d2e0c594bc180309fdfdd0a854d',
       name: 'SOL',
       balance: random(1, 100).toString(),
@@ -68,33 +86,54 @@ async function mockBackgroundTokensFetch({
   }
   const keys: string[] = [];
   const map: {
-    [key: string]: DemoDeepFreshToken;
+    [key: string]: IDemoDeepFreshTokenValuesMap;
   } = {};
+  const ownedTokens = sample(OWNED_TOKENS);
   items.forEach((item) => {
-    const key = `${item.address}__${item.networkId}`;
+    const key = `${item.networkId}__${item.accountId}__${item.address}__${item.name}`;
     item.$key = key;
+    item.owned = ownedTokens?.includes(item.address);
     keys.push(key);
-    map[key] = item;
+    map[key] = {
+      price: item.price,
+      balance: item.balance,
+      owned: item.owned,
+    };
   });
-  await wait(1500);
+  await wait(1000);
+  const overview: IDemoDeepFreshOverview = {
+    totalCounts: random(10, 11).toString(),
+    totalValues: random(399, 500).toString(),
+  };
   return {
     items,
     keys,
     map,
+    overview,
   };
 }
-
-interface DemoDeepFreshToken {
+interface IDemoDeepFreshOverview {
+  totalCounts: string;
+  totalValues: string;
+}
+interface IDemoDeepFreshToken {
   networkId: string;
+  accountId: string;
   address: string;
   name: string;
   balance: string;
   price: string;
   $key?: string;
+  owned?: boolean;
+}
+interface IDemoDeepFreshTokenValuesMap {
+  balance: string;
+  price: string;
+  owned?: boolean;
 }
 
 const atomDemoDeepFreshTokens = atom<{
-  items: DemoDeepFreshToken[];
+  items: IDemoDeepFreshToken[];
   keys: string[];
 }>({
   items: [],
@@ -102,8 +141,13 @@ const atomDemoDeepFreshTokens = atom<{
 });
 
 const atomDemoDeepFreshTokensMap = atom<{
-  [key: string]: DemoDeepFreshToken;
+  [key: string]: IDemoDeepFreshTokenValuesMap;
 }>({});
+
+const atomDemoDeepFreshOverview = atom<IDemoDeepFreshOverview>({
+  totalCounts: '0',
+  totalValues: '0',
+});
 
 const atomDemoDeepFreshTokensLoading = atom<boolean>(false);
 
@@ -120,11 +164,13 @@ const atomDemoDeepFreshReload = atom(
   ) => {
     try {
       set(atomDemoDeepFreshTokensLoading, true);
+      // TODO usePromiseResult
       const result = await mockBackgroundTokensFetch({ shouldShuffle });
       if (!isEqual(get(atomDemoDeepFreshTokens).keys, result.keys)) {
         set(atomDemoDeepFreshTokens, result);
       }
       set(atomDemoDeepFreshTokensMap, result.map);
+      set(atomDemoDeepFreshOverview, result.overview);
     } finally {
       set(atomDemoDeepFreshTokensLoading, false);
     }
@@ -137,20 +183,20 @@ const {
   useContextAtom: useAtomDemoDeepFresh,
 } = createJotaiContext();
 
-function BalanceView({ name, balance }: { name: string; balance: string }) {
+function BalanceView({ balance }: { balance: string }) {
   return (
     <DebugRenderTracker>
-      <Typography.Body1Strong>
-        {balance} {name}
-      </Typography.Body1Strong>
+      <Box pr={2}>
+        <Typography.Body1Strong>{balance}</Typography.Body1Strong>
+      </Box>
     </DebugRenderTracker>
   );
 }
 const BalanceViewMemo = memo(BalanceView);
-function BalanceViewDeepFresh({ $key }: DemoDeepFreshToken) {
+function BalanceViewDeepFresh({ $key }: IDemoDeepFreshToken) {
   const [map] = useAtomDemoDeepFresh(atomDemoDeepFreshTokensMap);
   const item = map[$key || ''];
-  return <BalanceViewMemo balance={item?.balance} name={item?.name} />;
+  return <BalanceViewMemo balance={item?.balance} />;
 }
 
 function PriceView({ price }: { price: string }) {
@@ -163,20 +209,44 @@ function PriceView({ price }: { price: string }) {
   );
 }
 const PriceViewMemo = memo(PriceView);
-function PriceViewDeepFresh({ $key }: DemoDeepFreshToken) {
+function PriceViewDeepFresh({ $key }: IDemoDeepFreshToken) {
   const [map] = useAtomDemoDeepFresh(atomDemoDeepFreshTokensMap);
   const item = map[$key || ''];
   return <PriceViewMemo price={item?.price} />;
 }
 
-function DemoDeepFreshTokenCell({ item }: { item: DemoDeepFreshToken }) {
+function OwnedViewDeepFresh({ $key }: IDemoDeepFreshToken) {
+  const [map] = useAtomDemoDeepFresh(atomDemoDeepFreshTokensMap);
+  const item = map[$key || ''];
+  const content = useMemo(
+    () => (
+      <DebugRenderTracker>
+        <Typography.Body1Strong>
+          {item.owned ? '   ✅' : '   '}
+        </Typography.Body1Strong>
+      </DebugRenderTracker>
+    ),
+    [item.owned],
+  );
+  return content;
+}
+
+function DemoDeepFreshTokenCell({ item }: { item: IDemoDeepFreshToken }) {
   return (
     <DebugRenderTracker>
-      <Box borderTopColor="divider" borderTopWidth={1} p={4}>
+      <Box borderTopColor="divider" borderTopWidth={1} py={4}>
         <Typography.Body1>{item.address}</Typography.Body1>
-        <BalanceViewDeepFresh {...item} />
+        <Stack direction="row">
+          <BalanceViewDeepFresh {...item} />
+          <Typography.Body1Strong>{item?.name}</Typography.Body1Strong>
+          <Box flex={1} />
+          <PriceViewDeepFresh {...item} />
+        </Stack>
         <Typography.Body1>{item.networkId}</Typography.Body1>
-        <PriceViewDeepFresh {...item} />
+        <Stack direction="row">
+          <Typography.Body1>{item.accountId}</Typography.Body1>
+          <OwnedViewDeepFresh {...item} />
+        </Stack>
       </Box>
     </DebugRenderTracker>
   );
@@ -186,11 +256,51 @@ function DemoDeepFreshTokenList() {
 
   return (
     <DebugRenderTracker>
-      <Box mt={8}>
+      <Box mt={4}>
         {tokensInfo.items.map((item) => (
           <DemoDeepFreshTokenCell key={item.$key} item={item} />
         ))}
       </Box>
+    </DebugRenderTracker>
+  );
+}
+
+function DemoDeepFreshOverviewTotalCounts() {
+  const [val] = useAtomDemoDeepFresh(atomDemoDeepFreshOverview);
+  const content = useMemo(
+    () => (
+      <DebugRenderTracker>
+        <Typography.Body1Strong>{val.totalCounts}</Typography.Body1Strong>
+      </DebugRenderTracker>
+    ),
+    [val.totalCounts],
+  );
+  return content;
+}
+
+function DemoDeepFreshOverviewTotalValues() {
+  const [val] = useAtomDemoDeepFresh(atomDemoDeepFreshOverview);
+  const content = useMemo(
+    () => (
+      <DebugRenderTracker>
+        <Typography.Body1Strong>{val.totalValues}</Typography.Body1Strong>
+      </DebugRenderTracker>
+    ),
+    [val.totalValues],
+  );
+  return content;
+}
+
+function DemoDeepFreshOverview() {
+  return (
+    <DebugRenderTracker>
+      <Stack direction="row" py={2}>
+        <Typography.Body1Strong>Total: </Typography.Body1Strong>
+        <DemoDeepFreshOverviewTotalCounts />
+        <Box mr={4} />
+        <Typography.Body1Strong> Values: $</Typography.Body1Strong>
+        <DemoDeepFreshOverviewTotalValues />
+      </Stack>
     </DebugRenderTracker>
   );
 }
@@ -202,10 +312,11 @@ function ReloadButtons() {
   return (
     <DebugRenderTracker>
       <Box>
-        <Button onPress={() => refresh()} isLoading={loading}>
+        <Button size="lg" onPress={() => refresh()} isLoading={loading}>
           Refresh
         </Button>
         <Button
+          size="lg"
           mt={4}
           onPress={() => refresh({ shouldShuffle: true })}
           isLoading={loading}
@@ -235,6 +346,7 @@ function DemoDeepFresh() {
           <Box>
             <ReloadButtons />
             <ReloadOnMount />
+            <DemoDeepFreshOverview />
             <DemoDeepFreshTokenList />
           </Box>
         </DebugRenderTracker>
