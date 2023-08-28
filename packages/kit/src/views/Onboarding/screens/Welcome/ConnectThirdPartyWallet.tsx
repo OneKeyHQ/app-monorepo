f5ba6e01466d2aa9013c60b405d6a5bcfd7e75a6
ByteZhang <quincysx@onekey.so>
2023-08-28 11:06:08 +0800

    feat: add connect walletconnect loading (#3448)

--- packages/kit/src/views/Onboarding/screens/Welcome/ConnectThirdPartyWallet.tsx
+++ packages/kit/src/views/Onboarding/screens/Welcome/ConnectThirdPartyWallet.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import { useIntl } from 'react-intl';
 import { StyleSheet } from 'react-native';
@@ -8,6 +8,7 @@ import {
   Icon,
   Image,
   Pressable,
+  Spinner,
   Text,
   ToastManager,
 } from '@onekeyhq/components';
@@ -18,6 +19,7 @@ import LogoWalletconnect from '@onekeyhq/kit/assets/onboarding/logo_walletconnec
 import debugLogger from '@onekeyhq/shared/src/logger/debugLogger';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
+import { useAppStateChange } from '../../../../hooks/useAppStateChange';
 import { useOnboardingDone } from '../../../../hooks/useOnboardingRequired';
 import { wait } from '../../../../utils/helper';
 import { useAddExternalAccount } from '../../../ExternalAccount/useAddExternalAccount';
@@ -27,9 +29,11 @@ export function ConnectThirdPartyWallet({ onPress }: { onPress: () => void }) {
   const intl = useIntl();
   const { addExternalAccount } = useAddExternalAccount();
   const onboardingDone = useOnboardingDone();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const onConnectResult = useCallback(
     async (result) => {
+      setLoading(false);
       await addExternalAccount(result);
       await onboardingDone();
       await wait(600);
@@ -52,6 +56,11 @@ export function ConnectThirdPartyWallet({ onPress }: { onPress: () => void }) {
     }
   }, [connectExternalWallet]);
 
+  const onAppStateChange = useCallback(() => {
+    setLoading(false);
+  }, []);
+  useAppStateChange(onAppStateChange);
+
   const logos = [LogoMetaMask, LogoTrustWallet, LogoRainbow, LogoWalletconnect];
 
   return (
@@ -68,6 +77,7 @@ export function ConnectThirdPartyWallet({ onPress }: { onPress: () => void }) {
       _pressed={{ bgColor: 'surface-pressed' }}
       onPress={() => {
         if (platformEnv.isNativeAndroid) {
+          setLoading(true);
           onWalletConnectDirectly();
         } else {
           onPress();
@@ -102,7 +112,11 @@ export function ConnectThirdPartyWallet({ onPress }: { onPress: () => void }) {
           </Box>
         )}
       </Box>
-      <Icon name="ChevronRightMini" size={20} color="icon-subdued" />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Icon name="ChevronRightMini" size={20} color="icon-subdued" />
+      )}
     </Pressable>
   );
 }
