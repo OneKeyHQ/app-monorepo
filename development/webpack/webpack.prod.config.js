a9ef998acdc316f41a053f0e1cf9874f36e0d0a8
huhuanming <huanming@onekey.so>
2023-09-03 21:25:26 +0800

    feat: clean build folder

--- development/webpack/webpack.prod.config.js
+++ development/webpack/webpack.prod.config.js
@@ -2,6 +2,9 @@ const { EsbuildPlugin } = require('esbuild-loader');
 
 module.exports = {
   mode: 'production',
+  output: {
+    clean: true,
+  },
   optimization: {
     minimizer: [
       new EsbuildPlugin({
