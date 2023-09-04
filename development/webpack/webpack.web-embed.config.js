8171fc755f6d9be2d4a2c704b7168e536b2ef46d
huhuanming <huanming@onekey.so>
2023-09-04 10:25:42 +0800

    fix: fix web-embed

---
+++ development/webpack/webpack.web-embed.config.js
@@ -0,0 +1,31 @@
+const { merge } = require('webpack-merge');
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+
+const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
+const baseConfig = require('./webpack.base.config');
+const developmentConfig = require('./webpack.development.config');
+const productionConfig = require('./webpack.prod.config');
+
+const { NODE_ENV = 'development' } = process.env;
+
+module.exports = ({ basePath, platform = 'web-embed' }) => {
+  switch (NODE_ENV) {
+    case 'production':
+      return merge(baseConfig({ platform, basePath }), productionConfig, {
+        output: {
+          publicPath: '/',
+        },
+      });
+    case 'development':
+    default:
+      return merge(
+        baseConfig({ platform, basePath }),
+        developmentConfig({ platform, basePath }),
+        {
+          output: {
+            publicPath: '',
+          },
+        },
+      );
+  }
+};
