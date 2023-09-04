ff415a9a4e5907a5efd42c716f18bc765d700f45
huhuanming <huanming@onekey.so>
2023-09-04 15:01:23 +0800

    fix: fix web-embed index

--- packages/web-embed/index.js
+++ packages/web-embed/index.js
@@ -1,22 +1,8 @@
-// import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo';
 
-// import App from './App';
+import App from './App';
 
-// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// // It also ensures that whether you load the app in Expo Go or in a native build,
-// // the environment is set up appropriately
-// registerRootComponent(App);
-const LibLoader = async () => import('@onekeyfe/cardano-coin-selection-asmjs');
-
-const getCardanoApi = async () => {
-    const Loader = await LibLoader();
-    return {
-      composeTxPlan: Loader.onekeyUtils.composeTxPlan,
-      signTransaction: Loader.onekeyUtils.signTransaction,
-      hwSignTransaction: Loader.trezorUtils.signTransaction,
-      txToOneKey: Loader.onekeyUtils.txToOneKey,
-      dAppUtils: Loader.dAppUtils,
-    };
-  };
-
-  getCardanoApi()
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
