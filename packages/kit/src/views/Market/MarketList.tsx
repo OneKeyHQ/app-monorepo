4529a5027f8ef829e81477c331659598e0b9f767
oidsix <oidsix@sina.com>
2023-09-04 13:45:39 +0800

    fix:marketlist-ui

--- packages/kit/src/views/Market/MarketList.tsx
+++ packages/kit/src/views/Market/MarketList.tsx
@@ -152,16 +152,18 @@ const MarketList: FC = () => {
 
   return (
     <>
-      <Box flex={1} px={isVerticalLayout ? 2 : 3}>
-        <MarketCategoryToggles categories={categories} />
-        <FlatList
-          flex={1}
+       <Box flex={1} px={isVerticalLayout ? 2 : 3}
           style={{
             maxWidth: MAX_PAGE_CONTAINER_WIDTH,
             width: '100%',
             marginHorizontal: 'auto',
-            alignSelf: 'center',
-          }}
+              alignSelf: 'center',
+            paddingTop: 32,
+              }}
+          >
+        <MarketCategoryToggles categories={categories} />
+        <FlatList
+          flex={1}
           refreshing={refreshing}
           onRefresh={onRefresh}
           ref={scrollRef}
