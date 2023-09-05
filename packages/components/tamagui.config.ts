a11fafa451e4f16ebd668421db77c0533d024910
huhuanming <huanming@onekey.so>
2023-09-05 18:20:19 +0800

    fix: fix Duplicate identifier 'TamaguiCustomConfig'.

--- packages/components/tamagui.config.ts
+++ packages/components/tamagui.config.ts
@@ -88,12 +88,5 @@ const config = createTamagui({
     pointerCoarse: { pointer: 'coarse' },
   }),
 });
-export type AppConfig = typeof config;
-declare module 'tamagui' {
-  // overrides TamaguiCustomConfig so your custom types
 
-  // work everywhere you import `tamagui`
-
-  type TamaguiCustomConfig = AppConfig;
-}
 export default config;
