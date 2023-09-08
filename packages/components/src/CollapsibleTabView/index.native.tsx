54d5795fe49c46a89b4c3ec8f33872a208f56e3f
ByteZhang <quincysx@onekey.so>
2023-09-08 10:21:04 +0800

    chore: Optimize tabview usage (#3517)

--- packages/components/src/CollapsibleTabView/index.native.tsx
+++ packages/components/src/CollapsibleTabView/index.native.tsx
@@ -1,10 +1,5 @@
-// ios
+// iOS and Android
 import { Fragment, forwardRef } from 'react';
-
-import FlatList from '../FlatList';
-import { FlatListPlain } from '../FlatListPlain';
-import ScrollView from '../ScrollView';
-import SectionList from '../SectionList';
 
 import { TabContainerNative } from './TabContainerNative';
 
@@ -12,8 +7,4 @@ export const Tabs = {
   Container: forwardRef(TabContainerNative),
   // @ts-ignore to stop the warning about Fragment under development
   Tab: __DEV__ ? ({ children }) => <>{children}</> : Fragment,
-  FlatList,
-  FlatListPlain,
-  ScrollView,
-  SectionList,
 };
