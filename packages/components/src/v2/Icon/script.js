6535a9146ba6740ee3db8f43ad572e237579a239
huhuanming <huanming@onekey.so>
2023-09-17 16:58:14 +0800

    fix: fix scripts

--- packages/components/src/v2/Icon/script.js
+++ packages/components/src/v2/Icon/script.js
@@ -12,17 +12,21 @@ const pascalCase = (str) =>
 const items = [];
 
 dirs.forEach((dir) => {
-  const files = fs.readdirSync(path.resolve(base, `${dir}`));
-  files
-    .filter((item) => !item.includes('index'))
-    .forEach((file) => {
-      const basicName = path.basename(file, path.extname(file));
-      items.push({
-        symbol: pascalCase(`${basicName}${dir.toUpperCase()}`),
-        path: `${dir}/${basicName}`,
-        name: pascalCase(`${basicName}${dir.toUpperCase()}`),
+  if (dir !== '.DS_Store') {
+    const files = fs.readdirSync(path.resolve(base, `${dir}`));
+    files
+      .filter((item) => !item.includes('index'))
+      .forEach((file) => {
+        const basicName = path.basename(file, path.extname(file));
+        if (!basicName.toLowerCase().includes('.ds_store')) {
+          items.push({
+            symbol: pascalCase(`${basicName}${dir.toUpperCase()}`),
+            path: `${dir}/${basicName}`,
+            name: pascalCase(`${basicName}${dir.toUpperCase()}`),
+          });
+        }
       });
-    });
+  }
 });
 
 const typesTemplate = `
@@ -39,6 +43,5 @@ const typesTemplate = `
 fs.writeFileSync(
   path.resolve(__dirname, `./Icons.tsx`),
   prettier.format(typesTemplate, { parser: 'typescript' }),
-  typesTemplate,
   'utf8',
 );
