932baef6f29886446b0673bee235978ba256d619
huhuanming <huanming@onekey.so>
2023-09-19 18:45:06 +0800

    fix: fix typescript suggestions

--- development/generate-tamagui-json.js
+++
@@ -1,7 +0,0 @@
-const { loadTamagui } = require('@tamagui/static');
-const path = require('path');
-
-loadTamagui({
-  config: path.resolve(__dirname, '../packages/components/tamagui.config.ts'),
-  components: ['tamagui'],
-});
