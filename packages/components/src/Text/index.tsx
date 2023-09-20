f6b9e11a1ef202687fba2fa1dac4485d4808e04f
Franco <chenhuifan.gg@gmail.com>
2023-09-20 10:28:15 +0800

    feat: add Button Component

--- packages/components/src/Text/index.tsx
+++ packages/components/src/Text/index.tsx
@@ -1,6 +1,10 @@
-import { styled, Text as TextComponent } from 'tamagui';
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { GetProps, styled } from '@tamagui/web';
+import { Text as OriginText } from 'tamagui';
 
-export const Text = styled(TextComponent, {
+export type TextProps = GetProps<typeof OriginText>;
+
+export const Text = styled(OriginText, {
   variants: {
     variant: {
       '...size': (size, { font }) => ({
