fadfd9ed7be5199f75af8854a024882b50bb20ad
王山栋 <jelly@onekey.so>
2023-09-21 12:07:54 +0800

    fix: disable lint
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/kit/src/hooks/useAppStatus.ts
+++ packages/kit/src/hooks/useAppStatus.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useMemo } from 'react';
 
 import backgroundApiProxy from '../background/instance/backgroundApiProxy';
--- packages/kit/src/hooks/useProtectedVerify.tsx
+++ packages/kit/src/hooks/useProtectedVerify.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useCallback, useEffect } from 'react';
 
 import {
