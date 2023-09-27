fd57cf97c5673287ec58be743872f3c93b73859f
Franco <chenhuifan.gg@gmail.com>
2023-09-27 16:17:27 +0800

    Toast (WIP)

--- packages/components/src/Toast/index.tsx
+++ packages/components/src/Toast/index.tsx
@@ -10,6 +10,8 @@ import {
 import { YStack } from 'tamagui';
 
 import { removePortalComponent, setPortalComponent } from '../Portal';
+import useSafeAreaInsets from '../Provider/hooks/useSafeAreaInsets';
+import { Text } from '../Text';
 
 import type { CreateNativeToastOptions } from '@tamagui/toast/src/types';
 
@@ -20,16 +22,20 @@ function ToastInstance() {
     <Toast
       key={currentToast.id}
       duration={currentToast.duration}
-      enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
-      exitStyle={{ opacity: 0, scale: 1, y: -20 }}
+      enterStyle={{ opacity: 0, scale: 0.9, y: -16 }}
+      exitStyle={{ opacity: 0, scale: 0.9, y: -16 }}
       y={0}
       opacity={1}
       scale={1}
-      animation="100ms"
+      animation="quick"
       viewportName={currentToast.viewportName}
     >
       <YStack>
-        <Toast.Title>{currentToast.title}</Toast.Title>
+        <Toast.Title asChild>
+          <YStack>
+            <Text variant="$bodyLg">{currentToast.title}</Text>
+          </YStack>
+        </Toast.Title>
         {!!currentToast.message && (
           <Toast.Description>{currentToast.message}</Toast.Description>
         )}
@@ -39,15 +45,17 @@ function ToastInstance() {
 }
 
 export function ToastProvider({ children }: PropsWithChildren<unknown>) {
+  // const { top } = useSafeAreaInsets();
+
   return (
-    <TMToastProvider native={false}>
+    <TMToastProvider>
       {children}
       <ToastViewport
-        multipleToasts
-        marginTop="$16"
-        width="100%"
-        justifyContent="center"
-        alignContent="center"
+        flexDirection="column-reverse"
+        // top={top || '$5'}
+        top="$16"
+        right={0}
+        left={0}
       />
       <ToastInstance />
     </TMToastProvider>
--- packages/components/tamagui.config.ts
+++ packages/components/tamagui.config.ts
@@ -103,6 +103,7 @@ const font = createFont({
   },
 });
 
+// https://docs.swmansion.com/react-native-reanimated/docs/2.x/api/animations/withSpring/
 const animations = createAnimations({
   '150ms': {
     type: 'timing',
@@ -116,18 +117,15 @@ const animations = createAnimations({
     type: 'spring',
     damping: 10,
     mass: 0.9,
-    stiffness: 100,
   },
   lazy: {
     type: 'spring',
     damping: 20,
-    stiffness: 60,
   },
   quick: {
     type: 'spring',
     damping: 20,
-    mass: 1.2,
-    stiffness: 250,
+    mass: 0.1,
   },
 });
 
