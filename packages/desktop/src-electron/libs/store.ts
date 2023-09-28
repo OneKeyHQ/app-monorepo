4e1c92ce6463281c867e0381bcf2763efc189b3d
kwoktung <onlyif2009@gmail.com>
2023-09-28 11:46:25 +0800

    feat: remove node-keytar lib

--- packages/desktop/src-electron/libs/store.ts
+++ packages/desktop/src-electron/libs/store.ts
@@ -1,3 +1,4 @@
+import { safeStorage } from 'electron';
 import Store from 'electron-store';
 
 const store = new Store();
@@ -5,7 +6,7 @@ const store = new Store();
 export type LocalStore = {
   getUpdateSettings(): UpdateSettings;
   setUpdateSettings(updateSettings: UpdateSettings): void;
-  clear(): void;
+  clearUpdateSettings(): void;
 };
 
 export type UpdateSettings = {
@@ -19,6 +20,27 @@ export const setUpdateSettings = (updateSettings: UpdateSettings): void => {
   store.set('updateSettings', updateSettings);
 };
 
-export const clear = () => {
-  store.clear();
+export const clearUpdateSettings = () => {
+  store.delete('updateSettings');
+};
+
+export const getSecureItem = (key: string) => {
+  const item = store.get('SecureDB', {}) as Record<string, string>;
+  const value = item[key];
+  if (value) {
+    const result = safeStorage.decryptString(Buffer.from(value, 'hex'));
+    return result;
+  }
+};
+
+export const setSecureItem = (key: string, value: string): void => {
+  const items = store.get('SecureDB', {}) as Record<string, string>;
+  items[key] = safeStorage.encryptString(value).toString('hex');
+  store.set('SecureDB', items);
+};
+
+export const clearSecureItem = (key: string) => {
+  const items = store.get('SecureDB', {}) as Record<string, string>;
+  delete items[key];
+  store.set('SecureDB', items);
 };
