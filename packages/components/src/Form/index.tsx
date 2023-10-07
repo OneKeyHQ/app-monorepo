641e8b3aba8fc16338ab7a5613edd1b73693319c
huhuanming <huanming@onekey.so>
2023-10-08 00:25:58 +0800

    fix: fix form component

--- packages/components/src/Form/index.tsx
+++ packages/components/src/Form/index.tsx
@@ -1,21 +1,20 @@
-import {
-  createContext,
-  forwardRef,
-  useContext,
-  useMemo,
-  useState,
-} from 'react';
+import type { PropsWithChildren } from 'react';
+import { useState } from 'react';
 
 import { useHeaderHeight as useHeaderHeightOG } from '@react-navigation/elements';
+import { FormProvider } from 'react-hook-form';
 import { KeyboardAvoidingView, Platform } from 'react-native';
 import {
+  Fieldset,
   ScrollView,
+  Form as TMForm,
   YStack,
   useWindowDimensions,
   withStaticProperties,
 } from 'tamagui';
 
-import type { TamaguiElement, YStackProps } from 'tamagui';
+import type { UseFormReturn } from 'react-hook-form';
+import type { JsxElement } from 'typescript';
 
 const useHeaderHeight = () => {
   try {
@@ -25,72 +24,64 @@ const useHeaderHeight = () => {
   }
 };
 
-const FormWrapperContext = createContext<{ height: number } | null>(null);
+export interface FormProps {
+  onSubmit: () => void;
+  form: UseFormReturn;
+  header: JsxElement;
+  footer: JsxElement;
+}
 
-// eslint-disable-next-line react/display-name
-const Wrapper = forwardRef<TamaguiElement, YStackProps>((props, ref) => {
+export function FormWrapper({
+  onSubmit,
+  form: formContext,
+  header,
+  children,
+  footer,
+}: PropsWithChildren<FormProps>) {
   const [height, setHeight] = useState(0);
-  const value = useMemo(() => ({ height }), [height]);
-  return (
-    <FormWrapperContext.Provider value={value}>
-      <YStack
-        onLayout={(event) => {
-          setHeight(event.nativeEvent.layout.height);
-        }}
-        ref={ref}
-        gap="$4"
-        flex={1}
-        jc="center"
-        $gtSm={{
-          width: '100%',
-          maxWidth: 600,
-          als: 'center',
-        }}
-        // $gtSm={{ width: 500, mx: 'auto' }}
-        $sm={{ jc: 'space-between' }}
-        {...props}
-      />
-    </FormWrapperContext.Provider>
-  );
-});
-
-// eslint-disable-next-line react/display-name
-const Body = forwardRef<TamaguiElement, YStackProps>((props, ref) => (
-  <ScrollView>
-    <YStack p="$4" ref={ref} gap="$2" pb="$8" {...props} />
-  </ScrollView>
-));
-
-/**
- * on native, this will be pushed to the bottom of the screen
- */
-// eslint-disable-next-line react/display-name
-const Footer = forwardRef<TamaguiElement, YStackProps>((props, ref) => {
   const dimensions = useWindowDimensions();
   const headerHeight = useHeaderHeight();
-  const formWrapperContext = useContext(FormWrapperContext);
-  const modalOffsetFromTop = formWrapperContext
-    ? dimensions.height - formWrapperContext.height
-    : headerHeight;
-
+  const modalOffsetFromTop = height ? dimensions.height - height : headerHeight;
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={modalOffsetFromTop}
-    >
-      <YStack
-        ref={ref}
-        pb="$4"
-        px="$4"
-        gap="$4"
-        flexDirection="column-reverse"
-        {...props}
-      />
-    </KeyboardAvoidingView>
+    <FormProvider {...formContext}>
+      <TMForm onSubmit={onSubmit}>
+        <YStack
+          onLayout={(event) => {
+            setHeight(event.nativeEvent.layout.height);
+          }}
+          gap="$4"
+          flex={1}
+          jc="center"
+          $gtSm={{
+            width: '100%',
+            maxWidth: 600,
+            als: 'center',
+          }}
+          // $gtSm={{ width: 500, mx: 'auto' }}
+          $sm={{ jc: 'space-between' }}
+        >
+          {header}
+          <ScrollView>
+            <YStack p="$4" gap="$2" pb="$8">
+              {children}
+            </YStack>
+          </ScrollView>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={modalOffsetFromTop}
+          >
+            <YStack pb="$4" px="$4" gap="$4" flexDirection="column-reverse">
+              {footer}
+            </YStack>
+          </KeyboardAvoidingView>
+        </YStack>
+      </TMForm>
+    </FormProvider>
   );
+}
+
+export const Form = withStaticProperties(FormWrapper, {
+  Field: Fieldset,
 });
 
-export const FormWrapper = withStaticProperties(Wrapper, {
-  Body,
-  Footer,
-});
+export { useForm } from 'react-hook-form';
