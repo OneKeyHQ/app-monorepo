6e27209f11ad614e3cc9b9c4b554fd037e510412
huhuanming <huanming@onekey.so>
2023-10-10 23:45:57 +0800

    fix: export

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,26 +1,22 @@
 import Provider from './Provider';
 
-export { Text } from './Text';
-export { Button } from './Button';
-export { IconButton } from './IconButton';
-export { Icon } from './Icon';
-export { Stack, XStack, YStack, ZStack } from './Stack';
-export { Badge } from './Badge';
-export { Alert } from './Alert';
-export { Divider } from './Divider';
-export { Spinner } from './Spinner';
-export { Dialog } from './Dialog';
-export { ActionList } from './ActionList';
+export * from './Text';
+export * from './Button';
+export * from './IconButton';
+export * from './Icon';
+export * from './Stack';
+export * from './Badge';
+export * from './Alert';
+export * from './Divider';
+export * from './Spinner';
+export * from './Dialog';
+export * from './ActionList';
 export * from './Form';
 export * from './Portal';
-
-export type { LocaleSymbol, LocaleIds } from './locale';
-export { LOCALES_OPTION } from './locale';
-
-export { ToastProvider, ToastController } from './Toast';
-
-export { useThemeValue } from './Provider/hooks/useThemeValue';
-export { useIsMounted } from './Provider/hooks/useIsMounted';
+export * from './locale';
+export * from './Toast';
+export * from './Provider/hooks/useThemeValue';
+export * from './Provider/hooks/useIsMounted';
 
 // Navigation
 export * from './Navigation/StackNavigator';
