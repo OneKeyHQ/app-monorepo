5aa27e714f3b304bbc32d7224e70f1bc88bf2628
hublot <hublot@aliyun.com>
2023-10-18 10:40:11 +0800

    feat: Remove the unused method

--- packages/components/src/hooks/useKeyboardHeight.ts
+++ packages/components/src/hooks/useKeyboardHeight.ts
@@ -7,18 +7,18 @@ export default function useKeyboardHeight() {
   const handleKeyboardWillShow: KeyboardEventListener = useCallback((e) => {
     setKeyboardHeight(e.endCoordinates.height);
   });
-  const handleKeyboardDidShow: KeyboardEventListener = useCallback((e) => {});
+  // const handleKeyboardDidShow: KeyboardEventListener = useCallback((e) => {});
   const handleKeyboardWillHide: KeyboardEventListener = useCallback((e) => {
     setKeyboardHeight(0);
   });
-  const handleKeyboardDidHide: KeyboardEventListener = useCallback((e) => {});
+  // const handleKeyboardDidHide: KeyboardEventListener = useCallback((e) => {});
 
   useEffect(() => {
     const subscriptions = [
       Keyboard.addListener('keyboardWillShow', handleKeyboardWillShow),
-      Keyboard.addListener('keyboardDidShow', handleKeyboardDidShow),
+      // Keyboard.addListener('keyboardDidShow', handleKeyboardDidShow),
       Keyboard.addListener('keyboardWillHide', handleKeyboardWillHide),
-      Keyboard.addListener('keyboardDidHide', handleKeyboardDidHide),
+      // Keyboard.addListener('keyboardDidHide', handleKeyboardDidHide),
     ];
 
     return () => {
