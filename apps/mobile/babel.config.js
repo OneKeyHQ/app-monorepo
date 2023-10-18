525ed7d22815ec55a69aa77f86fd394d00e41e1e
huhuanming <workboring@gmail.com>
2023-10-18 15:44:14 +0800

    fix: fix app build

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function(api) {
+const path = require('path');
+const babelTools = require('../../development/babelTools');
+
+module.exports = function (api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+  return babelTools.normalizeConfig({
+    platform: babelTools.developmentConsts.platforms.app,
+    config: {
+      presets: ['babel-preset-expo'],
+      plugins: [
+        [
+          require('@tamagui/babel-plugin/dist/cjs/index.native'),
+          {
+            components: ['tamagui'],
+            config: path.join(__dirname, '../../packages/components/tamagui.config.ts'),
+            importsWhitelist: [],
+            logTimings: true,
+            disableExtraction: process.env.NODE_ENV === 'development',
+          },
+        ],
+        [
+          'react-native-reanimated/plugin',
+          {
+            globals: ['__scanCodes'],
+          },
+        ],
+      ],
+    },
+  });
 };
