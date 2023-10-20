89e5a8126b28d47de625383499e0f95d3f8473ea
Leon <lixiao.dev@gmail.com>
2023-10-20 13:51:17 +0800

    feat: add example page

---
+++ packages/kit/src/views/Components/stories/Webview.tsx
@@ -0,0 +1,13 @@
+import WebView from '@onekeyhq/kit/src/components/WebView';
+
+const WebviewGallery = () => (
+  <WebView
+    src="https://dapp-example.onekeytest.com"
+    containerProps={{
+      w: '100%',
+      h: '100%',
+    }}
+  />
+);
+
+export default WebviewGallery;
