51318f1a7810a9b734ab63c52e072400df18ed1e
Franco <chenhuifan.gg@gmail.com>
2023-10-20 11:06:37 +0800

    togglegroup (wip)

--- packages/components/src/ToggleGroup/index.tsx
+++ packages/components/src/ToggleGroup/index.tsx
@@ -1,1 +1,50 @@
-export { ToggleGroup } from 'tamagui';
+import { ToggleGroup as TMToggleGroup } from 'tamagui';
+
+import { Icon } from '../Icon';
+import { Stack } from '../Stack';
+import { Text } from '../Text';
+
+import type { ICON_NAMES } from '../Icon';
+import type { GetProps } from 'tamagui';
+
+export function ToggleGroup(props: GetProps<typeof TMToggleGroup>) {
+  return <TMToggleGroup unstyled {...props} />;
+}
+
+function ToggleGroupItem(props: GetProps<typeof TMToggleGroup.Item>) {
+  return (
+    <TMToggleGroup.Item
+      unstyled
+      m="$-px"
+      p="$2"
+      bg="$bgSubdued"
+      borderWidth="$px"
+      borderColor="$borderStrong"
+      hoverStyle={{
+        bg: '$bgHover',
+      }}
+      focusStyle={{
+        outlineColor: '$focusRing',
+        outlineWidth: 2,
+        outlineStyle: 'solid',
+      }}
+      {...props}
+    />
+  );
+}
+
+function ToggleGroupText(props: GetProps<typeof Text>) {
+  return <Text variant="$bodyMdMedium" px="$1" {...props} />;
+}
+
+function ToggleGroupIcon({ name }: { name: ICON_NAMES }) {
+  return (
+    <Stack>
+      <Icon size="$5" color="$iconSubdued" name={name} />
+    </Stack>
+  );
+}
+
+ToggleGroup.Item = ToggleGroupItem;
+ToggleGroup.Icon = ToggleGroupIcon;
+ToggleGroup.Text = ToggleGroupText;
--- packages/kit/src/views/Components/stories/ToggleGroup.tsx
+++ packages/kit/src/views/Components/stories/ToggleGroup.tsx
@@ -1,6 +1,4 @@
-import { ToggleGroup } from 'tamagui';
-
-import { Stack } from '@onekeyhq/components';
+import { Icon, Stack, ToggleGroup } from '@onekeyhq/components';
 
 import { Layout } from './utils/Layout';
 
@@ -13,11 +11,28 @@ const ToggleGroupGallery = () => (
       {
         title: 'Default',
         element: (
-          <Stack space="$2">
+          <Stack space="$4">
             <ToggleGroup type="single">
-              <ToggleGroup.Item value="1">Item</ToggleGroup.Item>
-              <ToggleGroup.Item value="2">Item</ToggleGroup.Item>
-              <ToggleGroup.Item value="3">Item</ToggleGroup.Item>
+              <ToggleGroup.Item value="1">
+                <ToggleGroup.Text>Item</ToggleGroup.Text>
+              </ToggleGroup.Item>
+              <ToggleGroup.Item value="2">
+                <ToggleGroup.Text>Item</ToggleGroup.Text>
+              </ToggleGroup.Item>
+              <ToggleGroup.Item value="3">
+                <ToggleGroup.Text>Item</ToggleGroup.Text>
+              </ToggleGroup.Item>
+            </ToggleGroup>
+            <ToggleGroup type="single">
+              <ToggleGroup.Item value="4">
+                <ToggleGroup.Icon name="AlignLeftOutline" />
+              </ToggleGroup.Item>
+              <ToggleGroup.Item value="5">
+                <ToggleGroup.Icon name="AlignmentCenterOutline" />
+              </ToggleGroup.Item>
+              <ToggleGroup.Item value="6">
+                <ToggleGroup.Icon name="AlignRightOutline" />
+              </ToggleGroup.Item>
             </ToggleGroup>
           </Stack>
         ),
