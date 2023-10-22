929defc4058b537ce5bc905f381624a7f16f2b9b
huhuanming <huanming@onekey.so>
2023-10-22 17:01:02 +0800

    chore: ext version works

--- apps/ext/src/ui/renderApp.tsx
+++ apps/ext/src/ui/renderApp.tsx
@@ -1,12 +1,16 @@
+// fix missing setimmediate on react-dom
+// eslint-disable-next-line import/order
+import 'setimmediate';
+
 // eslint-disable-next-line import/order
 import { createRoot } from 'react-dom/client';
 
-// import App from '../App';
+import App from '../App';
 
 function renderApp() {
   const root = window.document.querySelector('#root');
   if (!root) throw new Error('No root element found!');
-  createRoot(root).render(<div>hello, onekey</div>);
+  createRoot(root).render(<App />);
 }
 
 export default renderApp;
