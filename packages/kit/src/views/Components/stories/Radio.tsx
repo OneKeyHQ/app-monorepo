f7fb2933cee41ed6f09a754b2ecc2ceb6067f8e1
huhuanming <huanming@onekey.so>
2023-10-23 18:38:46 +0800

    fix: fix radio story event

--- packages/kit/src/views/Components/stories/Radio.tsx
+++ packages/kit/src/views/Components/stories/Radio.tsx
@@ -1,6 +1,23 @@
+import { useState } from 'react';
+
 import { Radio } from '@onekeyhq/components';
 
 import { Layout } from './utils/Layout';
+
+const RadioExample = () => {
+  const [radioValue, setRadioValue] = useState();
+  return (
+    <Radio
+      value={radioValue}
+      onChange={setRadioValue}
+      options={[
+        { label: 'Option 1', value: '1' },
+        { label: 'Option 2', value: '2' },
+        { label: 'Option 3', value: '3' },
+      ]}
+    />
+  );
+};
 
 const RadioGallery = () => (
   <Layout
@@ -10,15 +27,7 @@ const RadioGallery = () => (
     elements={[
       {
         title: 'Default',
-        element: (
-          <Radio
-            options={[
-              { label: 'Option 1', value: '1' },
-              { label: 'Option 2', value: '2' },
-              { label: 'Option 3', value: '3' },
-            ]}
-          />
-        ),
+        element: <RadioExample />,
       },
     ]}
   />
