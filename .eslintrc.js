ff2820dc521543ebd5689d07e11e44c0d40a8503
huhuanming <huanming@onekey.so>
2023-10-23 08:54:35 +0800

    fix: fix github alert (#3684)
    * fix: fix github alert
    
    * fix: fix branch
    
    * fix: remove react-native-webview

--- .eslintrc.js
+++ .eslintrc.js
@@ -98,16 +98,12 @@ module.exports = {
     'packages/components/src/Icon/*',
     'packages/desktop/public/static/js-sdk/*',
     // 临时忽略以下目录的检查，迭代后会逐步开启
-    'packages/app',
     'packages/blockchain-libs',
     'packages/kit/src/store',
-    'packages/desktop',
+    'packages/core',
     'packages/engine',
-    'packages/ext',
     'packages/kit-bg',
     'packages/shared',
-    'packages/web',
-    'packages/web-embed',
   ],
   env: {
     browser: true,
