f8dbcda2dbd004652d90fdef87a9c7aa4375e309
hublot <hublot@aliyun.com>
2023-10-23 10:39:46 +0800

    fix: Remove the Trigger and Close from the Popover because of gesture conflict (#3683)

--- packages/kit/src/views/Components/stories/Popover.tsx
+++ packages/kit/src/views/Components/stories/Popover.tsx
@@ -1,35 +1,45 @@
+import { useState } from 'react';
+
 import { Button, Popover, Stack, Text } from '@onekeyhq/components';
 
 import { Layout } from './utils/Layout';
 
-const PopoverGallery = () => (
-  <Layout
-    description=".."
-    suggestions={['...']}
-    boundaryConditions={['...']}
-    elements={[
-      {
-        title: 'Default',
-        element: (
-          <Popover
-            title="Popover Demo"
-            renderTrigger={<Button>Open</Button>}
-            renderContent={
-              <Stack space="$4" p="$5">
-                <Text>
-                  Non exercitation ea laborum cupidatat sunt amet aute
-                  exercitation occaecat minim incididunt non est est voluptate.
-                </Text>
-                <Popover.Close>
-                  <Button variant="primary">Button</Button>
-                </Popover.Close>
-              </Stack>
-            }
-          />
-        ),
-      },
-    ]}
-  />
-);
+const PopoverGallery = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Layout
+      description=".."
+      suggestions={['...']}
+      boundaryConditions={['...']}
+      elements={[
+        {
+          title: 'Default',
+          element: (
+            <Popover
+              title="Popover Demo"
+              open={isOpen}
+              onOpenChange={setIsOpen}
+              renderTrigger={
+                <Button onPress={() => setIsOpen(true)}>Open</Button>
+              }
+              renderContent={
+                <Stack space="$4" p="$5">
+                  <Text>
+                    Non exercitation ea laborum cupidatat sunt amet aute
+                    exercitation occaecat minim incididunt non est est
+                    voluptate.
+                  </Text>
+                  <Button variant="primary" onPress={() => setIsOpen(false)}>
+                    Button
+                  </Button>
+                </Stack>
+              }
+            />
+          ),
+        },
+      ]}
+    />
+  );
+};
 
 export default PopoverGallery;
