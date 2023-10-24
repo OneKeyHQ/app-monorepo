c66696b80ae226397c1db76e9f5a86aba98ebf45
kwoktung <onlyif2009@gmail.com>
2023-10-24 10:07:34 +0800

    feat: support native props (#3697)

--- packages/kit/src/views/Components/stories/Select.tsx
+++ packages/kit/src/views/Components/stories/Select.tsx
@@ -58,6 +58,22 @@ const SelectDefaultItem = () => {
   );
 };
 
+const SelectDefaultNativeItem = () => {
+  const [val, setVal] = useState('Apple');
+
+  return (
+    <Select
+      native
+      items={items}
+      value={val}
+      onValueChange={setVal}
+      triggerProps={{ width: '100%' }}
+      disablePreventBodyScroll
+      title="Demo Title"
+    />
+  );
+};
+
 const SelectCustomTriggerItem = () => {
   const [val, setVal] = useState('Apple');
 
@@ -113,7 +129,7 @@ const sections: ISelectSection[] = [
       {
         label: 'Peach',
         value: 'Peach',
-        leading: <Icon name="AcademicCapMini" size="$5" />,
+        leading: <Icon name="AccessibilityEyeOutline" size="$5" />,
       },
     ],
   },
@@ -149,24 +165,23 @@ const SelectSectionsItemDemo = () => {
 
 const SelectGallery = () => (
   <Layout
-    description="对操作结果的反馈，无需用户操作即可自行消失"
-    suggestions={[
-      '使用 Toast 显示简约明确的信息反馈',
-      '用户点击或触摸 Toast 内容时，浮层将会停留在页面上',
-      'Toast 显示的文本应少于 20 字',
-      '不建议使用 Toast 显示过长的报错信息',
-    ]}
-    boundaryConditions={[
-      'Toast 永远拥有最高层级的浮层',
-      'Toast 组件能显示的最长文本内容为三排，超出三排将会缩略',
-      '界面中只会存在一个 Toast 示例，后触发的 Toast 信息会覆盖前一条 Toast 信息',
-    ]}
+    description="****"
+    suggestions={['****']}
+    boundaryConditions={['****']}
     elements={[
       {
         title: '默认状态',
         element: (
           <Stack space="$1">
             <SelectDefaultItem />
+          </Stack>
+        ),
+      },
+      {
+        title: '默认状态 native prop',
+        element: (
+          <Stack space="$1">
+            <SelectDefaultNativeItem />
           </Stack>
         ),
       },
