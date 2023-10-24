dc601ba44c1057b10c1a54bd65091a5066b2d20b
hublot <hublot@aliyun.com>
2023-10-24 12:11:56 +0800

    feat: Add tooltip to iconButton

--- packages/components/src/IconButton/index.tsx
+++ packages/components/src/IconButton/index.tsx
@@ -6,12 +6,14 @@ import {
 import { Icon } from '../Icon';
 import { Spinner } from '../Spinner';
 import { Stack } from '../Stack';
+import { Tooltip } from '../Tooltip';
 
 import type { ICON_NAMES } from '../Icon';
 
 interface IconButtonProps
   extends Omit<ButtonProps, 'iconAfter' | 'children' | 'icon'> {
   icon: ICON_NAMES;
+  title?: string;
 }
 
 const getSizeStyles = (size: ButtonProps['size']) => {
@@ -37,6 +39,7 @@ export const IconButton = (props: IconButtonProps) => {
   const {
     disabled,
     loading,
+    title,
     icon,
     size,
     variant = 'secondary',
@@ -51,32 +54,40 @@ export const IconButton = (props: IconButtonProps) => {
     variant,
   });
 
-  return (
-    <ButtonFrame
-      p={p}
-      borderRadius="$full"
-      disabled={disabled || loading}
-      {...(variant === 'tertiary' && {
-        m: negativeMargin,
-      })}
-      {...sharedFrameStyles}
-      {...rest}
-    >
-      {loading ? (
-        <Stack
-          {...(size !== 'small' && {
-            p: '$0.5',
-          })}
-        >
-          <Spinner color={iconColor} size="small" />
-        </Stack>
-      ) : (
-        <Icon
-          color={iconColor}
-          name={icon}
-          size={size === 'small' ? '$5' : '$6'}
-        />
-      )}
-    </ButtonFrame>
-  );
+  const renderIconButton = () => {
+    return (
+      <ButtonFrame
+        p={p}
+        borderRadius="$full"
+        disabled={disabled || loading}
+        {...(variant === 'tertiary' && {
+          m: negativeMargin,
+        })}
+        {...sharedFrameStyles}
+        {...rest}
+      >
+        {loading ? (
+          <Stack
+            {...(size !== 'small' && {
+              m: '$0.5',
+            })}
+          >
+            <Spinner color={iconColor} size="small" />
+          </Stack>
+        ) : (
+          <Icon
+            color={iconColor}
+            name={icon}
+            size={size === 'small' ? '$5' : '$6'}
+          />
+        )}
+      </ButtonFrame>
+    );
+  };
+
+  if (title) {
+    return <Tooltip renderTrigger={renderIconButton()} renderContent={title} />;
+  }
+
+  return renderIconButton();
 };
