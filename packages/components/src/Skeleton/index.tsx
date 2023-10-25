325ddf2213ac1690f1fb1e350c8a8ad44367005a
huhuanming <huanming@onekey.so>
2023-10-25 22:27:40 +0800

    fix: fix lint

--- packages/components/src/Skeleton/index.tsx
+++ packages/components/src/Skeleton/index.tsx
@@ -2,16 +2,17 @@ import { Skeleton as MotiSkeleton } from 'moti/skeleton';
 import { styled, withStaticProperties } from 'tamagui';
 
 import useTheme from '../Provider/hooks/useTheme';
+import { useThemeValue } from '../Provider/hooks/useThemeValue';
 
 import type { MotiSkeletonProps } from 'moti/build/skeleton/types';
-import { useThemeValue } from '../Provider/hooks/useThemeValue';
 
 export type SkeletonProps = Omit<MotiSkeletonProps, 'Gradient'>;
 
 function BasicSkeleton({ children, ...restProps }: SkeletonProps) {
   const { themeVariant } = useTheme();
   const primaryColor: any = useThemeValue('bgStrong');
-  const secondaryColor = themeVariant === 'light' ? 'rgb(205, 205, 205)' : 'rgb(51, 51, 51)';
+  const secondaryColor =
+    themeVariant === 'light' ? 'rgb(205, 205, 205)' : 'rgb(51, 51, 51)';
   return (
     <MotiSkeleton
       colors={[
