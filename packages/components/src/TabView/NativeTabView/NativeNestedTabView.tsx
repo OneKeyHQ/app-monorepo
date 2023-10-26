f0dd2bac5e859c41f6608c31025dc4974fdaf107
hublot <hublot@aliyun.com>
2023-10-26 16:40:52 +0800

    Feat: Move NativeNestedTabView native code to the react-native-tab-page-view library

--- packages/components/src/TabView/NativeTabView/NativeNestedTabView.tsx
+++ packages/components/src/TabView/NativeTabView/NativeNestedTabView.tsx
@@ -1,14 +1,8 @@
-import { requireNativeComponent } from 'react-native';
-
 import type { NativeNestedTabViewProps } from './types';
 import type { HostComponent } from 'react-native';
 
-const VIEW_MANAGER_NAME = 'NestedTabView';
+import NestedTabView from 'react-native-tab-page-view';
 
 export type PagerViewViewManagerType = HostComponent<NativeNestedTabViewProps>;
 
-const NativeNestedTabView = requireNativeComponent(
-  VIEW_MANAGER_NAME,
-) as PagerViewViewManagerType;
-
-export default NativeNestedTabView;
+export default NestedTabView as PagerViewViewManagerType;
