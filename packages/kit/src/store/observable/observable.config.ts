c80288455d8d036cf8dc6df74070bdc95b4c8aa9
Leon <lixiao.dev@gmail.com>
2023-10-30 16:56:19 +0800

    Feat/x web3 browser (#3736)
    * chore: add react-render-tracker
    
    * feat: discover explorer
    
    * feat: export store for jotai
    
    * feat: create discover dashboard
    
    * fix: load web home container
    
    * feat: add webview container
    
    * feat: add webview hooks
    
    * refactor: move electron webviewTag event listerner
    
    * fix: desktop controller
    
    * fix: delete unused log
    
    * refactor: webTabsActions api
    
    * feat: mobile browser
    
    * feat: tab list
    
    * fix: progress bar
    
    * feat: search modal
    
    * fix: native webview navigation
    
    * feat: add new event for native webview
    
    * chore: mock simple db
    
    * feat: Inject provider for DApp
    
    * feat: inject logic
    
    * chore: fix lint

--- packages/kit/src/store/observable/observable.config.ts
+++
@@ -1,9 +0,0 @@
-import { configureObservablePersistence } from '@legendapp/state/persist';
-// Web
-import { ObservablePersistLocalStorage } from '@legendapp/state/persist-plugins/local-storage';
-
-// Global configuration
-configureObservablePersistence({
-  // Use Local Storage on web
-  persistLocal: ObservablePersistLocalStorage,
-});
