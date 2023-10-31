f084527c6e85bf9d96785deb866d83bae9438d65
huhuanming <huanming@onekey.so>
2023-10-31 11:24:40 +0800

    fix: fix progress without errors

--- development/lintCheck.js
+++ development/lintCheck.js
@@ -4,9 +4,20 @@ const { exit } = require('process');
 const MAX_WARNINGS_COUNT = 180;
 
 try {
-  execSync(
+  const result = execSync(
     `sh -c 'npx eslint . --ext .ts,.tsx --fix --cache --cache-location \"$(yarn config get cacheFolder)\"'`,
-  );
+  ).toString('utf-8');
+
+  console.log(result);
+  const warningsCount = result.match(/, (\d+) warnings\)/)?.[1];
+  if (Number(warningsCount) > MAX_WARNINGS_COUNT) {
+    console.log(`Warnings Counts: ${warningsCount}`);
+    console.log(
+      `Please do not add more ESLint warnings than ${MAX_WARNINGS_COUNT}`,
+    );
+    console.log('Hope you can fix the ts warings introduced after this merge.');
+    exit(1);
+  }
 } catch (error) {
   const result = error.stdout.toString('utf-8');
   console.log(result);
@@ -14,16 +25,6 @@ try {
   const errorCount = Number(result.match(/(\d+) errors/)?.[1]);
   if (errorCount > 0) {
     console.error(`${errorCount} errors must be resolved`);
-    exit(1);
-  }
-
-  const WarningsCount = result.match(/, (\d+) warnings\)/)?.[1];
-  if (Number(WarningsCount) > MAX_WARNINGS_COUNT) {
-    console.log(`Warnings Counts: ${WarningsCount}`);
-    console.log(
-      `Please do not add more ESLint warnings than ${MAX_WARNINGS_COUNT}`,
-    );
-    console.log('Hope you can fix the ts warings introduced after this merge.');
     exit(1);
   }
 }
