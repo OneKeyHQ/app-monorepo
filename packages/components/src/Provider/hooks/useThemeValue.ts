bfd696cd40f607a72d97905ade354f451a7161fd
hublot <hublot@aliyun.com>
2023-11-07 09:56:58 +0800

    fix: set tab page style and content height (#3765)

--- packages/components/src/Provider/hooks/useThemeValue.ts
+++ packages/components/src/Provider/hooks/useThemeValue.ts
@@ -10,12 +10,14 @@ const getValue = (
   theme: UseThemeResult,
   key: ThemeKeys,
   fallback?: VariableVal,
+  isRawValue?: boolean,
 ): VariableVal => {
   // avoid re-renders
   // https://tamagui.dev/docs/core/use-theme
-  const value = platformEnv.isNative
-    ? theme?.[key]?.val
-    : (theme?.[key]?.get() as VariableVal);
+  const value =
+    platformEnv.isNative || isRawValue
+      ? theme?.[key]?.val
+      : (theme?.[key]?.get() as VariableVal);
   return value || fallback || '';
 };
 
@@ -24,15 +26,19 @@ export const getThemeTokens = coreGetTokens;
 export function useThemeValue<T extends ThemeKeys[] | ThemeKeys>(
   colorSymbol: T,
   fallback?: VariableVal,
+  isRawValue?: boolean,
 ): T extends ThemeKeys ? string : string[];
 
 export function useThemeValue(
   colorSymbol: ThemeKeys | ThemeKeys[],
   fallback?: VariableVal,
+  isRawValue?: boolean,
 ): VariableVal | VariableVal[] {
   const theme = useTheme();
   if (Array.isArray(colorSymbol)) {
-    return colorSymbol.map((c) => getValue(theme, c, fallback)) as string[];
+    return colorSymbol.map((c) =>
+      getValue(theme, c, fallback, isRawValue),
+    ) as string[];
   }
-  return getValue(theme, colorSymbol, fallback) as string;
+  return getValue(theme, colorSymbol, fallback, isRawValue) as string;
 }
