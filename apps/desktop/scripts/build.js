38ef89e3895fe3c92e5a019f165b6bbdbbb5efd3
huhuanming <huanming@onekey.so>
2023-11-08 10:54:24 +0800

    fix: help esbuild locate missing dependencies. (#3758)
    * fix: fix esbuild script in missing deps
    
    * docs: add comments

--- apps/desktop/scripts/build.js
+++ apps/desktop/scripts/build.js
@@ -18,6 +18,19 @@ build({
   platform: 'node',
   bundle: true,
   target: 'node16',
+  // Help esbuild locate missing dependencies.
+  alias: {
+    '@onekeyhq/shared': path.join(__dirname, '../../../packages/shared'),
+    'react-native-uuid': path.join(
+      __dirname,
+      '../../../node_modules/react-native-uuid/dist',
+    ),
+    'axios': path.join(
+      __dirname,
+      '../../../node_modules/axios/dist/esm/axios.js',
+    ),
+  },
+  // Avoid introducing dependencies that lead to script bloat.
   external: Object.keys({
     ...pkg.dependencies,
     ...pkg.devDependencies,
