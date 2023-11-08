0351b540a1e94eff814c2aea72f0cef1755396d5
JellyWang <38491708+ezailWang@users.noreply.github.com>
2023-11-08 17:14:57 +0800

    Feat/password component (#3753)
    * fix: add passwordverify component
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix:icon update
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: add password setup
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * feat: add selectTextOnFocus
    
    * fix: service password & db updatePassword &  password update
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: lint
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: add bioloyg auth switch status
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code & add passwordSet status
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: lint
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: remove console
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: verify oldpassword empty
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: add promp verify password
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: remove useLoacalAuthentication
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt atom
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    * fix: opt code
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    
    ---------
    
    Signed-off-by: 王山栋 <jelly@onekey.so>
    Co-authored-by: huhuanming <huanming@onekey.so>

--- packages/kit-bg/src/dbs/local/LocalDbBase.ts
+++ packages/kit-bg/src/dbs/local/LocalDbBase.ts
@@ -36,8 +36,7 @@ export abstract class LocalDbBase {
   abstract confirmHDWalletBackuped(walletId: string): Promise<Wallet>;
 
   // ---------------------------------------------- credential
-  async checkPassword(password: string): Promise<boolean> {
-    const context = await this.getContext({ verifyPassword: password });
+  checkPassword(context: OneKeyContext, password: string): boolean {
     if (!context) {
       console.error('Unable to get main context.');
       return false;
@@ -57,11 +56,20 @@ export abstract class LocalDbBase {
     }
   }
 
-  async verifyPassword(password: string): Promise<void> {
-    const r = await this.checkPassword(password);
-    if (!r) {
-      throw new WrongPassword();
+  async verifyPassword(password: string): Promise<boolean> {
+    const ctx = await this.getContext();
+    if (ctx && ctx.verifyString !== DEFAULT_VERIFY_STRING) {
+      return this.checkPassword(ctx, password);
     }
+    throw new WrongPassword();
+  }
+
+  async checkPasswordSet(): Promise<boolean> {
+    const ctx = await this.getContext();
+    if (ctx && ctx.verifyString !== DEFAULT_VERIFY_STRING) {
+      return true;
+    }
+    return false;
   }
 
   abstract updatePassword(
---
+++ packages/kit/src/components/BiologyAuthComponent/components/BiologyAuthSwitch.tsx
@@ -0,0 +1,18 @@
+import { memo } from 'react';
+
+import { Switch } from '@onekeyhq/components';
+
+interface IBiologyAuthSwitchProps {
+  isSupport: boolean;
+  isBiologyAuthEnable: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+const BiologyAuthSwitch = ({
+  isSupport,
+  isBiologyAuthEnable,
+  onChange,
+}: IBiologyAuthSwitchProps) =>
+  isSupport ? <Switch value={isBiologyAuthEnable} onChange={onChange} /> : null;
+
+export default memo(BiologyAuthSwitch);
