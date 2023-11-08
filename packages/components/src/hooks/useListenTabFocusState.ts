1a8b92161e2a4d5822792350c997becf8771e192
huhuanming <huanming@onekey.so>
2023-11-08 17:32:15 +0800

    docs: add docs

--- packages/components/src/hooks/useListenTabFocusState.ts
+++ packages/components/src/hooks/useListenTabFocusState.ts
@@ -2,8 +2,11 @@ import { useEffect } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
-// Note that this method is only effective in the current routing structure.
-// If the hierarchical relationship between the root route and the tab route changes, it may be necessary to recheck its availability.
+/**
+ * Note that this method is only effective in the current routing structure.
+ * If the hierarchical relationship between the root route and the tab route changes, it may be necessary to recheck its availability.
+ */
+
 export default function useListenTabFocusState(
   tabName: string,
   callback: (isFocus: boolean) => void,
