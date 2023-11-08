caea3c7d56e98ee7d019f6970d236dfa9c41f916
huhuanming <huanming@onekey.so>
2023-11-08 14:23:01 +0800

    chore: Add ESLint check for naming of enum types (#3774)

--- development/lintCheck.js
+++ development/lintCheck.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const { exit } = require('process');
 
-const MAX_WARNINGS_COUNT = 307;
+const MAX_WARNINGS_COUNT = 352;
 
 function handleWarnings(result) {
   const warningsCount = result.match(/, (\d+) warnings\)/)?.[1];
