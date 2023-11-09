f0d62c4abec3a820065d340bdec3554d82b7ac6e
huhuanming <huanming@onekey.so>
2023-11-09 11:24:58 +0800

    fix: remove KeyboardManager

--- packages/kit/src/views/Components/stories/NavigatorRoute/DemoRootApp.tsx
+++ packages/kit/src/views/Components/stories/NavigatorRoute/DemoRootApp.tsx
@@ -1,8 +1,3 @@
-import { useEffect } from 'react';
-
-import { Platform } from 'react-native';
-import KeyboardManager from 'react-native-keyboard-manager';
-
 import type { RootStackNavigatorConfig } from '@onekeyhq/components/src/Navigation/Navigator';
 import { RootStackNavigator } from '@onekeyhq/components/src/Navigation/Navigator';
 
@@ -23,22 +18,6 @@ const rootConfig: RootStackNavigatorConfig<DemoRootRoutes, any>[] = [
   },
 ];
 
-export const DemoRootApp = () => {
-  useEffect(() => {
-    if (Platform.OS === 'ios') {
-      KeyboardManager.setEnable(true);
-      KeyboardManager.setEnableDebugging(false);
-      KeyboardManager.setKeyboardDistanceFromTextField(10);
-      KeyboardManager.setLayoutIfNeededOnUpdate(true);
-      KeyboardManager.setEnableAutoToolbar(true);
-      // KeyboardManager.setToolbarDoneBarButtonItemText(
-      //   intl.formatMessage({ id: 'action__done' }),
-      // );
-      KeyboardManager.setToolbarPreviousNextButtonEnable(false);
-      KeyboardManager.setKeyboardAppearance('default');
-      KeyboardManager.setShouldPlayInputClicks(true);
-    }
-  }, []);
-
-  return <RootStackNavigator<DemoRootRoutes, any> config={rootConfig} />;
-};
+export const DemoRootApp = () => (
+  <RootStackNavigator<DemoRootRoutes, any> config={rootConfig} />
+);
