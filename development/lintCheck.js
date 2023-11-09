49a6660851e38f77f1a07acbc9ee15161318eebc
huhuanming <huanming@onekey.so>
2023-11-09 15:24:04 +0800

    Update MAX_WARNINGS_COUNT to 197 in lintCheck.js.

--- development/lintCheck.js
+++ development/lintCheck.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const { exit } = require('process');
 
-const MAX_WARNINGS_COUNT = 247;
+const MAX_WARNINGS_COUNT = 197;
 
 function handleWarnings(result) {
   const warningsCount = result.match(/, (\d+) warnings\)/)?.[1];
