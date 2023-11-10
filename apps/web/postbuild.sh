2e8f13fb5310e7c182c4f30e232f353417f384ae
huhuanming <huanming@onekey.so>
2023-11-10 18:17:24 +0800

    chore: add web build

---
+++ apps/web/postbuild.sh
@@ -0,0 +1,3 @@
+mkdir -p ./web-build/.well-known
+cp ./validation/deeplink.android.json ./web-build/.well-known/assetlinks.json
+cp ./validation/deeplink.ios.json ./web-build/.well-known/apple-app-site-association
