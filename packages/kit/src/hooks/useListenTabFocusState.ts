753b72cadb77caf01e3be5c3edb44e3351704133
huhuanming <huanming@onekey.so>
2023-11-13 15:34:08 +0800

    fix: remove atoms

--- packages/kit/src/hooks/useListenTabFocusState.ts
+++ packages/kit/src/hooks/useListenTabFocusState.ts
@@ -1,18 +1,16 @@
 import { useEffect } from 'react';
-import { useNavigationState } from '@react-navigation/native';
 
-/**
- * Note that this method is only effective in the current routing structure.
- * If the hierarchical relationship between the root route and the tab route changes, it may be necessary to recheck its availability.
- */
+import { useNavigationState } from '@react-navigation/native';
 
 export default function useListenTabFocusState(
   tabName: string,
   callback: (isFocus: boolean) => void,
 ) {
-  const routes = useNavigationState((state) => state.routes);
-  console.log(routes)
-  // useEffect(() => {
-  //   callback(route.currentTab === tabName);
-  // }, [callback, route.currentTab, tabName]);
+  const currentTabName = useNavigationState((state) => {
+    const rootState = state.routes.find(({ name }) => name === 'main')?.state;
+    return rootState?.routeNames?.[rootState?.index || 0] || '';
+  });
+  useEffect(() => {
+    callback(currentTabName === tabName);
+  }, [callback, currentTabName, tabName]);
 }
