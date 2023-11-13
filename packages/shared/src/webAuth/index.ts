d8348bdddc299fed393f76f32ece2e6e94ef01b5
王山栋 <jelly@onekey.so>
2023-11-13 15:32:25 +0800

    fix: opt base64 decode base64Data
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/shared/src/webAuth/index.ts
+++ packages/shared/src/webAuth/index.ts
@@ -1,55 +1,16 @@
-/* eslint-disable no-plusplus, no-bitwise */
+import { Base64 } from 'js-base64';
+
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
-const chars =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+export const base64Encode = function (arraybuffer: ArrayBuffer): string {
+  const uint8Array = new Uint8Array(arraybuffer);
+  const base64Data = Base64.fromUint8Array(uint8Array);
+  return base64Data;
+};
 
-// Use a lookup table to find the index.
-const lookup = new Uint8Array(256);
-for (let i = 0; i < chars.length; i++) {
-  lookup[chars.charCodeAt(i)] = i;
-}
-
-// export const encode = function (arraybuffer: ArrayBuffer): string {
-//   const bytes = new Uint8Array(arraybuffer);
-//   const len = bytes.length;
-//   let base64 = '';
-
-//   for (let i = 0; i < len; i += 3) {
-//     base64 += chars[bytes[i] >> 2];
-//     base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
-//     base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
-//     base64 += chars[bytes[i + 2] & 63];
-//   }
-
-//   if (len % 3 === 2) {
-//     base64 = base64.substring(0, base64.length - 1);
-//   } else if (len % 3 === 1) {
-//     base64 = base64.substring(0, base64.length - 2);
-//   }
-
-//   return base64;
-// };
-
-export const decode = function (base64: string): ArrayBuffer {
-  const len = base64.length;
-  const bufferLength = base64.length * 0.75;
-  const arraybuffer = new ArrayBuffer(bufferLength);
-  const bytes = new Uint8Array(arraybuffer);
-
-  let p = 0;
-  for (let i = 0; i < len; i += 4) {
-    const encoded1 = lookup[base64.charCodeAt(i)];
-    const encoded2 = lookup[base64.charCodeAt(i + 1)];
-    const encoded3 = lookup[base64.charCodeAt(i + 2)];
-    const encoded4 = lookup[base64.charCodeAt(i + 3)];
-
-    bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
-    bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
-    bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
-  }
-
-  return arraybuffer;
+export const base64Decode = function (base64: string): ArrayBuffer {
+  const uint8Array = Base64.toUint8Array(base64);
+  return uint8Array.buffer;
 };
 
 const isContextSupportWebAuth = Boolean(
@@ -88,7 +49,6 @@ export const registerWebAuth = async () => {
         name: 'OneKey',
         displayName: 'OneKey',
       },
-      // eslint-disable-next-line spellcheck/spell-checker
       pubKeyCredParams: [{ type: 'public-key', alg: -7 }],
       timeout: 60000,
       attestation: 'direct',
@@ -114,7 +74,7 @@ export const verifiedWebAuth = async (credId: string) => {
       allowCredentials: [
         {
           type: 'public-key',
-          id: decode(credId),
+          id: base64Decode(credId),
         },
       ],
       challenge: challenge.buffer,
