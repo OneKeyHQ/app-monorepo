1e43cd5ff160ca20766725569d68b50155ed476b
huhuanming <huanming@onekey.so>
2023-11-15 23:48:20 +0800

    refactor: rename to  index.native file

--- packages/components/src/View/index.tsx
+++ packages/components/src/View/index.tsx
@@ -1,11 +1,7 @@
-// The View component is not exposed externally, it is only used for component layer optimization.
-import type { ElementType } from 'react';
+import { Stack } from '../Stack';
 
 import type { IViewType } from './type';
 
-const ViewNativeComponent: {
-  default: ElementType<IViewType>;
-} = require('react-native/Libraries/Components/View/ViewNativeComponent');
-
-export * from './type';
-export const View = ViewNativeComponent.default;
+export function View(props: IViewType) {
+  return <Stack {...props} />;
+}
