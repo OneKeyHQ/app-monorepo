0359f9820b582bdf95d3c62891ca5aa25e12d81c
kwoktung <onlyif2009@gmail.com>
2023-11-16 22:22:07 +0800

    feat: add locale setting

--- packages/kit/src/views/Setting/Language/index.tsx
+++ packages/kit/src/views/Setting/Language/index.tsx
@@ -1,17 +1,29 @@
-import { ListItem, ModalContainer, Stack } from '@onekeyhq/components';
+import { useCallback } from 'react';
+
+import { ModalContainer, ScrollView } from '@onekeyhq/components';
+import type { ILocaleSymbol } from '@onekeyhq/components/src/locale';
+import { useSettingsPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+
+import { ListItemSelect } from '../Components/ListItemSelect';
+import { useLocaleOptions } from '../hooks';
 
 export default function SettingLanguageModal() {
+  const [settings, setSettings] = useSettingsPersistAtom();
+  const options = useLocaleOptions();
+  const onChange = useCallback(
+    (text: string) =>
+      setSettings({ ...settings, 'locale': text as ILocaleSymbol }),
+    [settings, setSettings],
+  );
   return (
     <ModalContainer>
-      <Stack>
-        <ListItem title="English">
-          <ListItem.IconButton
-            iconProps={{ 'color': '$iconActive' }}
-            icon="CheckRadioSolid"
-          />
-        </ListItem>
-        <ListItem title="简体中文" />
-      </Stack>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ListItemSelect
+          onChange={onChange}
+          value={settings.locale}
+          options={options.map((o) => ({ title: o.label, value: o.value }))}
+        />
+      </ScrollView>
     </ModalContainer>
   );
 }
--- packages/kit/src/views/Setting/List/PreferenceSection.tsx
+++ packages/kit/src/views/Setting/List/PreferenceSection.tsx
@@ -1,6 +1,9 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
+
+import { useIntl } from 'react-intl';
 
 import { ListItem } from '@onekeyhq/components';
+import { LOCALES_OPTION } from '@onekeyhq/components/src/locale';
 import type { IPageNavigationProp } from '@onekeyhq/components/src/Navigation';
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { EModalRoutes } from '@onekeyhq/kit/src/routes/Root/Modal/Routes';
@@ -9,6 +12,8 @@ import {
   type ISettingsPersistAtom,
   useSettingsPersistAtom,
 } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+
+import { useLocaleOptions } from '../hooks';
 
 import { Section } from './Section';
 
@@ -42,18 +47,49 @@ const ThemeListItem = () => {
   );
 };
 
+const LocaleListItem = () => {
+  const navigation =
+    useAppNavigation<IPageNavigationProp<IModalSettingParamList>>();
+  const onLanguage = useCallback(() => {
+    navigation.pushModal(EModalRoutes.SettingModal, {
+      screen: EModalSettingRoutes.SettingLanguageModal,
+    });
+  }, [navigation]);
+  const locales = useLocaleOptions();
+  const labels = useMemo(
+    () =>
+      locales.reduce((a, b) => {
+        a[b.value] = b.label;
+        return a;
+      }, {} as Record<string, string>),
+    [locales],
+  );
+
+  const [{ locale }] = useSettingsPersistAtom();
+  return (
+    <ListItem
+      onPress={onLanguage}
+      icon="GlobusOutline"
+      title="Language"
+      drillIn
+    >
+      <ListItem.Text
+        primary={labels[locale]}
+        align="right"
+        primaryTextProps={{
+          tone: 'subdued',
+        }}
+      />
+    </ListItem>
+  );
+};
+
 export const PreferenceSection = () => {
   const navigation =
     useAppNavigation<IPageNavigationProp<IModalSettingParamList>>();
   const onPress = useCallback(() => {
     navigation.pushModal(EModalRoutes.SettingModal, {
       screen: EModalSettingRoutes.SettingCurrencyModal,
-    });
-  }, [navigation]);
-
-  const onLanguage = useCallback(() => {
-    navigation.pushModal(EModalRoutes.SettingModal, {
-      screen: EModalSettingRoutes.SettingLanguageModal,
     });
   }, [navigation]);
   return (
@@ -72,20 +108,7 @@ export const PreferenceSection = () => {
           }}
         />
       </ListItem>
-      <ListItem
-        onPress={onLanguage}
-        icon="GlobusOutline"
-        title="Language"
-        drillIn
-      >
-        <ListItem.Text
-          primary="English"
-          align="right"
-          primaryTextProps={{
-            tone: 'subdued',
-          }}
-        />
-      </ListItem>
+      <LocaleListItem />
       <ThemeListItem />
     </Section>
   );
