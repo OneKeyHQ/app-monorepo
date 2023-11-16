d395fb28859817ab08c2a2cdfd2e73e7f9b10c4a
huhuanming <huanming@onekey.so>
2023-11-16 20:18:58 +0800

    refactor: change MAX_WARNINGS_COUNT

--- development/lintCheck.js
+++ development/lintCheck.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 const { exit } = require('process');
 
-const MAX_WARNINGS_COUNT = 300;
+const MAX_WARNINGS_COUNT = 199;
 
 function handleWarnings(result) {
   const warningsCount = result.match(/, (\d+) warnings\)/)?.[1];
