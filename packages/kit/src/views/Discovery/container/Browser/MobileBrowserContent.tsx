f2f0593bd1d45b167fa2f7b74c738b0da118a785
Leon <lixiao.dev@gmail.com>
2023-11-18 11:14:00 +0800

    chore: lint

--- packages/kit/src/views/Discovery/container/Browser/MobileBrowserContent.tsx
+++ packages/kit/src/views/Discovery/container/Browser/MobileBrowserContent.tsx
@@ -1,33 +1,14 @@
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { Freeze } from 'react-freeze';
-import { captureRef } from 'react-native-view-shot';
 import { Stack } from 'tamagui';
 
-import type { IPageNavigationProp } from '@onekeyhq/components/src/Navigation';
-import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
-import { EModalRoutes } from '@onekeyhq/kit/src/routes/Root/Modal/Routes';
-
-import MobileBrowserBottomBar from '../../components/MobileBrowser/MobileBrowserBottomBar-1';
 import WebContent from '../../components/WebContent/WebContent';
-import { THUMB_HEIGHT, THUMB_WIDTH } from '../../config/TabList.constants';
-import useBrowserBookmarkAction from '../../hooks/useBrowserBookmarkAction';
 import useBrowserHistoryAction from '../../hooks/useBrowserHistoryAction';
-import useWebTabAction from '../../hooks/useWebTabAction';
-import {
-  useActiveTabId,
-  useWebTabData,
-  useWebTabs,
-} from '../../hooks/useWebTabs';
-import { EDiscoveryModalRoutes } from '../../router/Routes';
+import { useActiveTabId, useWebTabData } from '../../hooks/useWebTabs';
 import { homeTab } from '../../store/contextWebTabs';
-import { captureViewRefs, webviewRefs } from '../../utils/explorerUtils';
-import { getScreenshotPath, saveScreenshot } from '../../utils/screenshot';
+import { captureViewRefs } from '../../utils/explorerUtils';
 import DiscoveryDashboard from '../Dashboard';
-
-import type { IDiscoveryModalParamList } from '../../router/Routes';
-import type WebView from 'react-native-webview';
-import type { TamaguiElement } from 'tamagui';
 
 function MobileBrowserContent({ id }: { id: string }) {
   const { tab } = useWebTabData(id);
@@ -35,7 +16,6 @@ function MobileBrowserContent({ id }: { id: string }) {
   const { activeTabId } = useActiveTabId();
   const [backEnabled, setBackEnabled] = useState(false);
   const [forwardEnabled, setForwardEnabled] = useState(false);
-  const captureViewRef = useRef<TamaguiElement | null>();
 
   const isActive = useMemo(
     () => activeTabId === tab?.id,
@@ -48,7 +28,6 @@ function MobileBrowserContent({ id }: { id: string }) {
 
   const initCaptureViewRef = useCallback(
     ($ref: any) => {
-      captureViewRef.current = $ref;
       captureViewRefs[id] = $ref;
     },
     [id],
