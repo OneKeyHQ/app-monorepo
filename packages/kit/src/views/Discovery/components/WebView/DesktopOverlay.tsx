00e2e079904df55bd991677379428c5914f91c2d
huhuanming <huanming@onekey.so>
2023-11-21 12:28:32 +0800

    fix: fix overlay event

--- packages/kit/src/views/Discovery/components/WebView/DesktopOverlay.tsx
+++ packages/kit/src/views/Discovery/components/WebView/DesktopOverlay.tsx
@@ -2,29 +2,47 @@ import { memo, useCallback, useEffect, useState } from 'react';
 
 import { Stack } from '@onekeyhq/components';
 
-const BASIC_DESKTOP_OVERLAY_EVENT_NAME = 'root_overlay';
+class DesktopMenuOverlayStatus {
+  #isShowOverlay = false;
+
+  #statusChangeListeners: ((isShowOverlay: boolean) => void)[] = [];
+
+  set isShowOverlay(status: boolean) {
+    this.#isShowOverlay = status;
+    this.#statusChangeListeners.forEach((callback) => {
+      callback(status);
+    });
+  }
+
+  get isShowOverlay() {
+    return this.#isShowOverlay;
+  }
+
+  onStatusChange(callback: (isShowOverlay: boolean) => void) {
+    this.#statusChangeListeners.push(callback);
+    callback(this.#isShowOverlay);
+    return () => this.removeSubscription(callback);
+  }
+
+  removeSubscription(callback: (isShowOverlay: boolean) => void) {
+    this.#statusChangeListeners = this.#statusChangeListeners.filter(
+      (c) => c === callback,
+    );
+  }
+}
+
+const desktopMenuOverlayStatus = new DesktopMenuOverlayStatus();
 
 function BasicDesktopOverlay() {
   const [isShowOverlay, setOverlayStatus] = useState(false);
 
-  const changeOverlayStatus = useCallback(
-    (event: CustomEvent<{ isOpen: boolean }>) => {
-      setOverlayStatus(event.detail.isOpen);
-    },
-    [],
-  ) as EventListener;
-
+  const changeOverlayStatus = useCallback((isOpen: boolean) => {
+    setOverlayStatus(isOpen);
+  }, []);
   useEffect(() => {
-    document.addEventListener(
-      BASIC_DESKTOP_OVERLAY_EVENT_NAME,
-      changeOverlayStatus,
-    );
-    return () => {
-      document.removeEventListener(
-        BASIC_DESKTOP_OVERLAY_EVENT_NAME,
-        changeOverlayStatus,
-      );
-    };
+    const subscription =
+      desktopMenuOverlayStatus.onStatusChange(changeOverlayStatus);
+    return subscription;
   }, [changeOverlayStatus]);
   return isShowOverlay ? (
     <Stack position="absolute" top={0} bottom={0} left={0} right={0} />
@@ -32,11 +50,7 @@ function BasicDesktopOverlay() {
 }
 
 export const dispatchOverlayEvent = (isOpen: boolean) => {
-  document.dispatchEvent(
-    new CustomEvent<{ isOpen: boolean }>(BASIC_DESKTOP_OVERLAY_EVENT_NAME, {
-      detail: { isOpen },
-    }),
-  );
+  desktopMenuOverlayStatus.isShowOverlay = isOpen;
 };
 
 export const DesktopOverlay = memo(BasicDesktopOverlay);
