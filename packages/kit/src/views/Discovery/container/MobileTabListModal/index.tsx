d0c32e17ea6783de3c97de9b35079aa8b01b1724
Leon <lixiao.dev@gmail.com>
2023-11-21 18:07:35 +0800

    chore: lint

--- packages/kit/src/views/Discovery/container/MobileTabListModal/index.tsx
+++ packages/kit/src/views/Discovery/container/MobileTabListModal/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 
 import { FlatList, StyleSheet } from 'react-native';
 
