e713a4998c8800b3454fb33508af6a5a733acea5
王山栋 <jelly@onekey.so>
2023-11-23 11:20:13 +0800

    fix: lint
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/kit/src/views/Swap/index.tsx
+++ packages/kit/src/views/Swap/index.tsx
@@ -1,24 +1,6 @@
-import { Suspense, memo } from 'react';
+import { memo } from 'react';
 
-import {
-  Button,
-  Dialog,
-  Page,
-  Spinner,
-  Text,
-  Toast,
-  XStack,
-} from '@onekeyhq/components';
-import {
-  EPasswordResStatus,
-  type IPasswordRes,
-} from '@onekeyhq/kit-bg/src/services/ServicePassword';
-
-import backgroundApiProxy from '../../background/instance/backgroundApiProxy';
-import BiologyAuthSwitchContainer from '../../components/BiologyAuthComponent/container/BiologyAuthSwitchContainer';
-import WebAuthSwitchContainer from '../../components/BiologyAuthComponent/container/WebAuthSwitchContainer';
-import PasswordSetupContainer from '../../components/Password/container/PasswordSetupContainer';
-import PasswordUpdateContainer from '../../components/Password/container/PasswordUpdateContainer';
+import { Page, Text } from '@onekeyhq/components';
 
 const Swap = () => {
   console.log('swap');
