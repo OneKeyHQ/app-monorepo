74eea6024c325bdbe56e9a908b1a25d4c52db639
kwoktung <onlyif2009@gmail.com>
2023-11-24 18:01:56 +0800

    feat: update service password

--- packages/kit-bg/src/services/ServicePassword/index.ts
+++ packages/kit-bg/src/services/ServicePassword/index.ts
@@ -233,10 +233,10 @@ export default class ServicePassword extends ServiceBase {
   async setPassword(password: string): Promise<string> {
     this.validatePasswordStrength(password);
     try {
+      await this.unLockApp();
       await this.biologyAuthSavePassword(password);
       await this.setCachedPassword(password);
       await this.setPasswordSetStatus(true);
-      await this.unLockApp();
       await localDb.createPassword({ password });
       return password;
     } catch (e) {
