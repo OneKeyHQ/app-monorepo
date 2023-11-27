7a5a897b0f63806d3633f1b75f62eefd4d83ee82
huhuanming <huanming@onekey.so>
2023-11-27 21:15:11 +0800

    fix: remove logo when ui is ready

--- packages/components/src/Splash/SplashView.tsx
+++ packages/components/src/Splash/SplashView.tsx
@@ -6,12 +6,18 @@ import { Stack } from '../Stack';
 import type { ISplashViewProps } from './SplashView.type';
 import type { LayoutChangeEvent } from 'react-native';
 
+const removeWebLogo = () => {
+  const img = document.querySelector('.onekey-index-html-preload-image');
+  img?.remove();
+};
+
 export function SplashView({ onReady }: ISplashViewProps) {
   const handleLayout = useCallback(
     (e: LayoutChangeEvent) => {
       const { height, width } = e.nativeEvent.layout;
       if (height && width) {
         setTimeout(() => {
+          removeWebLogo();
           onReady();
         }, 0);
       }
