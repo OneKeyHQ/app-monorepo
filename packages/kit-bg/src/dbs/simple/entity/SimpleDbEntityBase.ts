12326de1ab9d0bfdb4ff049f77eca7b556427157
Leon <lixiao.dev@gmail.com>
2023-12-04 22:09:09 +0800

    fix: Remove unnecessary code and update updatedAt assignment

--- packages/kit-bg/src/dbs/simple/entity/SimpleDbEntityBase.ts
+++ packages/kit-bg/src/dbs/simple/entity/SimpleDbEntityBase.ts
@@ -65,13 +65,8 @@ abstract class SimpleDbEntityBase<T> {
       data,
       updatedAt,
     };
-    if (platformEnv.isDev && platformEnv.isDesktop) {
-      Object.assign(savedData, {
-        _tmpUpdatedAtText: new Date(updatedAt).toLocaleString(),
-      });
-    }
     await appStorage.setItem(this.entityKey, JSON.stringify(savedData));
-    this.updatedAt = updatedAt ?? 0;
+    this.updatedAt = updatedAt;
     return data;
   }
 
