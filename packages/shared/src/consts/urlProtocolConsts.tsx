a52194d0b99bb27a2521a997e5f15403caabe247
Leon <lixiao.dev@gmail.com>
2023-12-04 21:59:54 +0800

    fix: only https

--- packages/shared/src/consts/urlProtocolConsts.tsx
+++ packages/shared/src/consts/urlProtocolConsts.tsx
@@ -8,13 +8,13 @@ export const PROTOCOLS_SUPPORTED_TO_OPEN = [
   'https:' as const,
   // 'file:' as const,
 
-  // OneKey legacy
-  `${ONEKEY_APP_DEEP_LINK_NAME}:` as const,
+  // // OneKey legacy
+  // `${ONEKEY_APP_DEEP_LINK_NAME}:` as const,
 
-  // wallet connect
-  `${WALLET_CONNECT_DEEP_LINK_NAME}:` as const,
-  'ethereum:' as const,
+  // // wallet connect
+  // `${WALLET_CONNECT_DEEP_LINK_NAME}:` as const,
+  // 'ethereum:' as const,
 
   // lightning network
-  'lightning:' as const,
+  // 'lightning:' as const,
 ];
