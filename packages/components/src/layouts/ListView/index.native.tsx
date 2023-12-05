cfe762399ff39eac8325f40570da9f2986f12410
hublot <hublot@aliyun.com>
2023-12-05 18:29:15 +0800

    chore: refactor some search options of Page.Header and useNavigation and useDemoAppNavigation

--- packages/components/src/layouts/ListView/index.native.tsx
+++ packages/components/src/layouts/ListView/index.native.tsx
@@ -38,11 +38,6 @@ type IListViewProps<T> = Omit<
       See https://shopify.github.io/flash-list/docs/estimated-item-size/#how-to-calculate
     */
     estimatedItemSize: number | `$${keyof Tokens['size']}`;
-    onBlankArea?: (blankAreaEvent: {
-      offsetStart: number;
-      offsetEnd: number;
-      blankArea: number;
-    }) => void;
   };
 
 function BaseListView<T>(
