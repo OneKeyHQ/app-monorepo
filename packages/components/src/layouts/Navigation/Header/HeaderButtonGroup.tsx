775d11dcee18ce56bcd6f5cf4752c80e87bd3535
joe <joewong7321@gmail.com>
2023-12-07 17:19:15 +0800

    fix: 🐛 roll back unchange file

--- packages/components/src/layouts/Navigation/Header/HeaderButtonGroup.tsx
+++ packages/components/src/layouts/Navigation/Header/HeaderButtonGroup.tsx
@@ -6,7 +6,12 @@ export default function HeaderButtonGroup(props: GetProps<typeof XStack>) {
   const { children, ...rest } = props;
 
   return (
-    <XStack space="$6" alignItems="center" {...rest}>
+    <XStack
+      space="$6"
+      alignItems="center"
+      testID="Navigation-HeaderView-ButtonGroup"
+      {...rest}
+    >
       {children}
     </XStack>
   );
