0d357e3cfcd0757743039444929dc7b5d9e0db7d
huhuanming <huanming@onekey.so>
2023-12-08 18:12:12 +0800

    Update index.ts

--- packages/components/src/actions/index.ts
+++ packages/components/src/actions/index.ts
@@ -6,5 +6,6 @@ export * from './ListItem';
 export * from './Popover';
 export * from './SegmentControl';
 export * from './Shortcut';
+export * from './Toast';
 export * from './Tooltip';
 export * from './Trigger';
