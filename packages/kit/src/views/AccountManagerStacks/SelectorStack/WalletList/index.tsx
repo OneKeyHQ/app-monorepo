e491215c23ffe4b3713ccad97e6e9013b7c15fe5
huhuanming <huanming@onekey.so>
2023-12-12 23:05:10 +0800

    fix: fix pushModal

--- packages/kit/src/views/AccountManagerStacks/SelectorStack/WalletList/index.tsx
+++ packages/kit/src/views/AccountManagerStacks/SelectorStack/WalletList/index.tsx
@@ -43,7 +43,7 @@ export function WalletList({
   const navigation = useAppNavigation();
 
   const handleCreateWalletPress = () => {
-    navigation.push(EModalRoutes.OnboardingModal, {
+    navigation.pushModal(EModalRoutes.OnboardingModal, {
       screen: EModalRoutes.OnboardingModal,
     });
   };
