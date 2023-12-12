c903f4d1336f8a3990a9343262ac3de428b1c518
hublot <hublot@aliyun.com>
2023-12-12 10:49:42 +0800

    feat: add sortableCellContainer

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/SortableListView.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/SortableListView.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 
-import { Pressable } from 'react-native';
-
-import { SortableListView, Text } from '@onekeyhq/components';
+import { SortableListView, Stack, Text } from '@onekeyhq/components';
 
 export const mapIndexToData = (_d: any, index: number, array: any[]) => {
   const getColor = (i: number, numItems = 25) => {
@@ -26,25 +24,16 @@ const SortableListViewGallery = () => {
       bg="$bgApp"
       data={data}
       keyExtractor={(item) => `${item.index}`}
-      renderItem={({ item, drag, isActive }) => (
-        // Don't use `Stack.onLongPress` as it will only be called after `onPressOut`
-        <SortableListView.ShadowDecorator>
-          <SortableListView.ScaleDecorator activeScale={0.9}>
-            <Pressable
-              onLongPress={drag}
-              disabled={isActive}
-              style={{
-                backgroundColor: item.backgroundColor,
-                width: '100%',
-                height: CELL_HEIGHT,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Text color="white">{item.index}</Text>
-            </Pressable>
-          </SortableListView.ScaleDecorator>
-        </SortableListView.ShadowDecorator>
+      renderItem={({ item }) => (
+        <Stack
+          w="100%"
+          h={CELL_HEIGHT}
+          alignItems="center"
+          justifyContent="center"
+          bg={item.backgroundColor}
+        >
+          <Text color="white">{item.index}</Text>
+        </Stack>
       )}
       getItemLayout={(_, index) => ({
         length: CELL_HEIGHT,
