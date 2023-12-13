ed9d2f479c4f5dcb17fdd78aff0ffc5605bbe750
huhuanming <huanming@onekey.so>
2023-12-13 17:05:46 +0800

    fix: fix lint

--- packages/components/src/composite/Dialog/context.ts
+++ packages/components/src/composite/Dialog/context.ts
@@ -2,4 +2,6 @@ import { createContext } from 'react';
 
 import type { IDialogContextType } from './type';
 
-export const DialogContext = createContext<IDialogContextType>({});
+export const DialogContext = createContext<IDialogContextType>(
+  {} as IDialogContextType,
+);
