ab2695ee7a8a02cc06fddc5c33257b336272121c
huhuanming <huanming@onekey.so>
2023-12-15 11:00:17 +0800

    feat: support color tokens in LinearGradient

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/LinearGradient.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/LinearGradient.tsx
@@ -1,3 +1,5 @@
+import { Stack } from 'tamagui';
+
 import { LinearGradient, SizableText } from '@onekeyhq/components';
 
 import { Layout } from './utils/Layout';
@@ -11,17 +13,30 @@ const LinearGradientGallery = () => (
       {
         title: 'default',
         element: (
-          <LinearGradient
-            // Button Linear Gradient
-            colors={['#4c669f', '#3b5998', '#192f6a']}
-            height="$24"
-            width="100%"
-            justifyContent="center"
-          >
-            <SizableText size="$bodyMd" color="#fff" textAlign="center">
-              Sign in
-            </SizableText>
-          </LinearGradient>
+          <Stack space="$8">
+            <LinearGradient
+              // Button Linear Gradient
+              colors={['#4c669f', '#3b5998', '#192f6a']}
+              height="$24"
+              width="100%"
+              justifyContent="center"
+            >
+              <SizableText size="$bodyMd" color="#fff" textAlign="center">
+                Sign in
+              </SizableText>
+            </LinearGradient>
+            <LinearGradient
+              // Button Linear Gradient
+              colors={['bgActive', 'backgroundHover', 'bgPrimary']}
+              height="$24"
+              width="100%"
+              justifyContent="center"
+            >
+              <SizableText size="$bodyMd" color="#fff" textAlign="center">
+                Sign in
+              </SizableText>
+            </LinearGradient>
+          </Stack>
         ),
       },
     ]}
