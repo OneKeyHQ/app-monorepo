dc5ed564a0be8b6521d54cedddb12e97f8a8f1d2
huhuanming <huanming@onekey.so>
2023-12-15 21:55:05 +0800

    Update index.tsx

--- packages/components/src/actions/Trigger/index.tsx
+++ packages/components/src/actions/Trigger/index.tsx
@@ -3,6 +3,8 @@ import { Children, cloneElement, forwardRef, isValidElement } from 'react';
 
 import { debounce } from 'lodash';
 
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
 import { Stack } from '../../primitives';
 
 import type { IButtonProps } from '../../primitives/Button';
@@ -23,6 +25,18 @@ const composeEventHandlers =
 type ITrigger = PropsWithChildren<{ onPress?: () => void; disabled?: boolean }>;
 const noop = () => undefined;
 
+const stopPropagationPress = (onPress: (...params: any[]) => void) =>
+  platformEnv.isRuntimeBrowser
+    ? (...params: any[]) => {
+        const event = params[0];
+        if (event && 'stopPropagation' in event) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+          event.stopPropagation();
+        }
+        return onPress(...params);
+      }
+    : onPress;
+
 function BasicTrigger(
   { onPress: onPressInTrigger, disabled, children }: ITrigger,
   ref: ForwardedRef<IView>,
@@ -34,8 +48,11 @@ function BasicTrigger(
       const handleOpen = onPress
         ? composeEventHandlers(onPress, onPressInTrigger)
         : onPressInTrigger;
-      const debounceHandlePress = debounce(handleOpen as () => void, 10);
+      const debounceHandlePress = stopPropagationPress(
+        debounce(handleOpen as () => void, 10),
+      );
       const handlePressWithStatus = disabled ? noop : debounceHandlePress;
+
       return (
         <Stack ref={ref} onPress={handlePressWithStatus}>
           {cloneElement(child, {
