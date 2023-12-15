bc464e1bd93a9eb23a1ba9060c41a43c36cf35a1
huhuanming <huanming@onekey.so>
2023-12-15 23:30:33 +0800

    Update metro.config.js

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,6 @@
 /* eslint-disable spellcheck/spell-checker */
 // Learn more https://docs.expo.dev/guides/monorepos
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-const fs = require('fs-extra');
-const connect = require('connect');
-const dynamicImports = require('./plugins/dynamicImports');
-const { fileToIdMap } = require('./plugins/map');
 // Find the project and workspace directories
 const projectRoot = __dirname;
 // This can be replaced with `find-yarn-workspace-root`
@@ -14,14 +9,6 @@ const projectRoot = __dirname;
 const config = getDefaultConfig(projectRoot);
 
 config.projectRoot = projectRoot;
-
-const fileMapCacheDirectoryPath = path.resolve(
-  __dirname,
-  'node_modules',
-  '.cache/file-map-cache',
-);
-fs.ensureDirSync(fileMapCacheDirectoryPath);
-config.fileMapCacheDirectory = fileMapCacheDirectoryPath;
 
 // hot-reload file type
 // cjs is needed for superstruct: https://github.com/ianstormtaylor/superstruct/issues/404#issuecomment-800182972
@@ -48,126 +35,148 @@ config.resolver.extraNodeModules = {
   zlib: require.resolve('browserify-zlib'),
 };
 
-const workspaceRoot = path.resolve(projectRoot, '../..');
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-// config.resolver.disableHierarchicalLookup = true;
-// config.transformer.getTransformOptions = async () => ({
-//   transform: {
-//     experimentalImportSupport: false,
-//     inlineRequires: true,
-//     unstable_disableES6Transforms: false,
-//   },
-//   preloadedModules: false,
-//   ramGroups: [],
-// });
+if (process.env.NODE_ENV !== 'production') {
+  const path = require('path');
+  const fs = require('fs-extra');
+  const connect = require('connect');
+  const dynamicImports = require('./plugins/dynamicImports');
+  const { fileToIdMap } = require('./plugins/map');
 
-// `Dynamic imports` is a feature that allows you to load modules on demand.
-config.transformer.asyncRequireModulePath = path.resolve(
-  __dirname,
-  `./plugins/asyncRequire.js`,
-);
+  const fileMapCacheDirectoryPath = path.resolve(
+    __dirname,
+    'node_modules',
+    '.cache/file-map-cache',
+  );
+  fs.ensureDirSync(fileMapCacheDirectoryPath);
 
-// config.serializer.processModuleFilter = (() => {
-//   const dllArr = [];
-//   const busineArr = [];
-//   const timeId = null;
+  const workspaceRoot = path.resolve(projectRoot, '../..');
+  // 1. Watch all files within the monorepo
+  config.watchFolders = [workspaceRoot];
+  // 2. Let Metro know where to resolve packages and in what order
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+  ];
+  config.fileMapCacheDirectory = fileMapCacheDirectoryPath;
 
-//   return (arg) =>
-// const relativePath = replacePath(arg.path);
-// const arr =
-//   this.options.dll.entry.length !== 0 && isBaseDllPath(relativePath)
-//     ? dllArr
-//     : busineArr;
-// arr.push(relativePath);
+  // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+  // config.resolver.disableHierarchicalLookup = true;
+  // config.transformer.getTransformOptions = async () => ({
+  //   transform: {
+  //     experimentalImportSupport: false,
+  //     inlineRequires: true,
+  //     unstable_disableES6Transforms: false,
+  //   },
+  //   preloadedModules: false,
+  //   ramGroups: [],
+  // });
 
-// timeId && clearTimeout(timeId);
-// timeId = setTimeout(async () => {
-//   try {
-//     const dllOutputPath = path.resolve(paths.outputDir, dllJsonName);
-//     const dllContent = JSON.stringify([...new Set(dllArr)], null, 2);
-//     await fse.writeFile(dllOutputPath, dllContent);
-//     console.log(
-//       `info Writing json output to: ${replacePath(dllOutputPath)}`,
-//     );
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }, 1500);
+  // `Dynamic imports` is a feature that allows you to load modules on demand.
+  config.transformer.asyncRequireModulePath = path.resolve(
+    __dirname,
+    `./plugins/asyncRequire.js`,
+  );
 
-//     true;
-// })();
+  // config.serializer.processModuleFilter = (() => {
+  //   const dllArr = [];
+  //   const busineArr = [];
+  //   const timeId = null;
 
-config.serializer.createModuleIdFactory = () => {
-  let nextId = 0;
-  return (path) => {
-    let id = fileToIdMap.get(path);
-    if (typeof id !== 'number') {
-      // eslint-disable-next-line no-plusplus
-      nextId += 1;
-      id = nextId;
-      fileToIdMap.set(path, id);
-    }
-    return id;
-  };
-};
+  //   return (arg) =>
+  // const relativePath = replacePath(arg.path);
+  // const arr =
+  //   this.options.dll.entry.length !== 0 && isBaseDllPath(relativePath)
+  //     ? dllArr
+  //     : busineArr;
+  // arr.push(relativePath);
 
-const beforeCustomSerializer = (entryPoint, prepend, graph, bundleOptions) => {
-  for (const [key, value] of graph.dependencies) {
-    // to entry file injection of global variables __APP__
-    if (entryPoint === key) {
-      for (const { data } of value.output) {
-        data.code = `__APP__= {}\n${data.code}`;
+  // timeId && clearTimeout(timeId);
+  // timeId = setTimeout(async () => {
+  //   try {
+  //     const dllOutputPath = path.resolve(paths.outputDir, dllJsonName);
+  //     const dllContent = JSON.stringify([...new Set(dllArr)], null, 2);
+  //     await fse.writeFile(dllOutputPath, dllContent);
+  //     console.log(
+  //       `info Writing json output to: ${replacePath(dllOutputPath)}`,
+  //     );
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }, 1500);
+
+  //     true;
+  // })();
+
+  config.serializer.createModuleIdFactory = () => {
+    let nextId = 0;
+    return (path) => {
+      let id = fileToIdMap.get(path);
+      if (typeof id !== 'number') {
+        // eslint-disable-next-line no-plusplus
+        nextId += 1;
+        id = nextId;
+        fileToIdMap.set(path, id);
       }
-      break;
-    }
-  }
-};
+      return id;
+    };
+  };
 
-config.serializer.customSerializer = async (
-  entryPoint,
-  prepend,
-  graph,
-  bundleOptions,
-  ...args
-) => {
-  beforeCustomSerializer(entryPoint, prepend, graph, bundleOptions);
-  const bundle = await dynamicImports(
+  const beforeCustomSerializer = (
     entryPoint,
     prepend,
     graph,
     bundleOptions,
-  );
-
-  return bundle;
-};
-
-const outputChunkDir = path.resolve(__dirname, 'dist/chunks');
-config.server.enhanceMiddleware = (metroMiddleware, metroServer) =>
-  connect()
-    .use(metroMiddleware)
-    .use('/async-thunks', (req, res, next) => {
-      const { url } = req;
-      const query = url.split('?').pop();
-      console.log(url, query);
-      const params = new URLSearchParams(query);
-      const hash = params.get('hash');
-      console.log('async-thunks hash:', hash);
-      const content = fs.readFileSync(
-        path.resolve(outputChunkDir, `${hash}.bundle`),
-        'utf8',
-      );
-      if (hash) {
-        res.end(content);
-      } else {
-        next();
+  ) => {
+    for (const [key, value] of graph.dependencies) {
+      // to entry file injection of global variables __APP__
+      if (entryPoint === key) {
+        for (const { data } of value.output) {
+          data.code = `__APP__= {}\n${data.code}`;
+        }
+        break;
       }
-    });
+    }
+  };
+
+  config.serializer.customSerializer = async (
+    entryPoint,
+    prepend,
+    graph,
+    bundleOptions,
+    ...args
+  ) => {
+    beforeCustomSerializer(entryPoint, prepend, graph, bundleOptions);
+    const bundle = await dynamicImports(
+      entryPoint,
+      prepend,
+      graph,
+      bundleOptions,
+    );
+
+    return bundle;
+  };
+
+  const outputChunkDir = path.resolve(__dirname, 'dist/chunks');
+  config.server.enhanceMiddleware = (metroMiddleware, metroServer) =>
+    connect()
+      .use(metroMiddleware)
+      .use('/async-thunks', (req, res, next) => {
+        const { url } = req;
+        const query = url.split('?').pop();
+        console.log(url, query);
+        const params = new URLSearchParams(query);
+        const hash = params.get('hash');
+        console.log('async-thunks hash:', hash);
+        const content = fs.readFileSync(
+          path.resolve(outputChunkDir, `${hash}.bundle`),
+          'utf8',
+        );
+        if (hash) {
+          res.end(content);
+        } else {
+          next();
+        }
+      });
+}
 
 module.exports = config;
