581827be5f7511ce6598d3afbc64337407d45732
huhuanming <huanming@onekey.so>
2023-12-17 14:41:44 +0800

    fix: fix md scroll

--- packages/components/src/layouts/Page/BasicPage.tsx
+++ packages/components/src/layouts/Page/BasicPage.tsx
@@ -1,10 +1,18 @@
+import { useHeaderHeight } from '@react-navigation/elements';
+import { useWindowDimensions } from 'react-native';
+
 import { Stack } from '../../primitives';
 
 import type { IBasicPageProps } from './type';
 
 export function BasicPage({ children }: IBasicPageProps) {
+  const headerHeight = useHeaderHeight();
+  const windowHeight = useWindowDimensions().height;
+  const height = windowHeight - headerHeight;
+
+  console.log(height);
   return (
-    <Stack bg="$bgApp" flex={1}>
+    <Stack bg="$bgApp" flex={1} maxHeight={height}>
       {children}
     </Stack>
   );
