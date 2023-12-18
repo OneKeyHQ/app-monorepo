d3eec4a867af54364ded18ff38e5f2225a893772
huhuanming <huanming@onekey.so>
2023-12-19 00:05:45 +0800

    Update hooks.ts

--- packages/kit/src/views/Onboarding/pages/ImportWallet/hooks.ts
+++ packages/kit/src/views/Onboarding/pages/ImportWallet/hooks.ts
@@ -84,7 +84,7 @@ export const useSuggestion = (
       }
       const text = value.toLowerCase().trim();
       const words = fetchSuggestions(text);
-      openStatusRef.current = words.length > 1;
+      openStatusRef.current = words.length > 0;
       if (words.length === 1 && text === words[0]) {
         return text.slice(0, value.length - 1);
       }
