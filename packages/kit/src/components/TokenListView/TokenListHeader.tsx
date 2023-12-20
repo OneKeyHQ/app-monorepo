1ec47373c94ee78ceb87a71c4b83440f774d86a9
Franco <franco@onekey.so>
2023-12-20 17:40:02 +0800

    merge

--- packages/kit/src/components/TokenListView/TokenListHeader.tsx
+++ packages/kit/src/components/TokenListView/TokenListHeader.tsx
@@ -1,14 +1,13 @@
 import { useIntl } from 'react-intl';
 
-import { Text, XStack } from '@onekeyhq/components';
+import { Button, SearchBar, Text, XStack } from '@onekeyhq/components';
 
 function TokenListHeader() {
   const intl = useIntl();
   return (
-    <XStack px="$2">
-      <Text variant="$headingLg">
-        {intl.formatMessage({ id: 'title__assets' })}
-      </Text>
+    <XStack>
+      <SearchBar placeholder="Search token" />
+      <Button>3 hiddens</Button>
     </XStack>
   );
 }
