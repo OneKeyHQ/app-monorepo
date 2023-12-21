f5f10bb7f663944d205398b346758c2564fab382
huhuanming <huanming@onekey.so>
2023-12-21 18:36:08 +0800

    feat: remove setThemePreloadToLocalStorage

--- apps/ext/src/assets/preload-html-head.js
+++ apps/ext/src/assets/preload-html-head.js
@@ -32,13 +32,7 @@
   // $$onekeyPerfTrace end ----------------------------------------------
 
   // themePreload start ----------------------------------------------
-  const theme = localStorage.getItem('ONEKEY_THEME_PRELOAD');
-  if (theme === 'dark') {
-    document.documentElement.style.backgroundColor = 'rgb(19, 19, 27)';
-  }
-  if (theme === 'light' || theme === 'system') {
-    document.documentElement.style.backgroundColor = 'white';
-  }
+  document.documentElement.style.backgroundColor = '#0f0f0f';
   // themePreload end ----------------------------------------------
 
   // optimizeResize start ----------------------------------------------
