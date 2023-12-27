a2789920f66afd5363bd3909cdc6bc2754fdb092
huhuanming <huanming@onekey.so>
2023-12-27 21:48:07 +0800

    chore: remove unused plugins

--- apps/web/babel.config.js
+++ apps/web/babel.config.js
@@ -3,7 +3,12 @@ const babelTools = require('../../development/babelTools');
 module.exports = babelTools.normalizeConfig({
   platform: babelTools.developmentConsts.platforms.web,
   config: {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        { web: { unstable_transformProfile: 'hermes-stable' } },
+      ],
+    ],
     plugins: ['react-native-reanimated/plugin'],
   },
 });
