6e08c93a215bcd93d644700fc50a771b91ae7f4a
huhuanming <huanming@onekey.so>
2023-12-29 17:22:43 +0800

    fix: fix re-rendering lazyLoad

--- packages/shared/src/lazyLoad/index.tsx
+++ packages/shared/src/lazyLoad/index.tsx
@@ -2,14 +2,14 @@ import { Suspense, lazy, memo } from 'react';
 
 const LazyLoad = (factory: () => Promise<{ default: any }>) => {
   const Component = lazy(factory);
-  function Component1(props: any) {
+  function LazyLoadContainer(props: any) {
     return (
       <Suspense>
         <Component {...props} />
       </Suspense>
     );
   }
-  return memo(Component1);
+  return memo(LazyLoadContainer);
 };
 
 export default LazyLoad;
