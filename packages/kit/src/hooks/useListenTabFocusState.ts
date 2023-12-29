d0ed040589e8dc317a14d18e582cacccbd4d2423
huhuanming <huanming@onekey.so>
2023-12-29 17:48:55 +0800

    fix: get rootState from rootNavigationRef

--- packages/kit/src/hooks/useListenTabFocusState.ts
+++ packages/kit/src/hooks/useListenTabFocusState.ts
@@ -2,12 +2,16 @@ import { useEffect } from 'react';
 
 import { useNavigationState } from '@react-navigation/native';
 
+import { rootNavigationRef } from '@onekeyhq/components';
+
 export default function useListenTabFocusState(
   tabName: string,
   callback: (isFocus: boolean) => void,
 ) {
-  const currentTabName = useNavigationState((state) => {
-    const rootState = state.routes.find(({ name }) => name === 'main')?.state;
+  const currentTabName = useNavigationState(() => {
+    const rootState = rootNavigationRef.current
+      ?.getState()
+      .routes.find(({ name }) => name === 'main')?.state;
     return rootState?.routeNames?.[rootState?.index || 0] || '';
   });
   useEffect(() => {
