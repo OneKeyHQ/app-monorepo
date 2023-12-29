a918d2c58551a96358b90b8f51a3e58462493332
huhuanming <huanming@onekey.so>
2023-12-29 12:23:02 +0800

    Update babelTools.js

--- development/babelTools.js
+++ development/babelTools.js
@@ -141,8 +141,8 @@ function normalizeConfig({ platform, config }) {
     /* FIX:
        TypeError: undefined is not an object (evaluating 'this._callListeners.bind')
      */
-    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
     ['@babel/plugin-proposal-private-methods', { 'loose': true }],
     ['@babel/plugin-proposal-private-property-in-object', { 'loose': true }],
     ['@babel/plugin-proposal-export-namespace-from'],
