c2a5e6518b43d1ab4dfa70731df0048384a96d00
huhuanming <huanming@onekey.so>
2024-01-02 15:54:20 +0800

    fix: fix useListenTabFocusState

--- packages/components/src/layouts/Navigation/Navigator/NavigationContainer.tsx
+++ packages/components/src/layouts/Navigation/Navigator/NavigationContainer.tsx
@@ -1,4 +1,5 @@
-import { createRef } from 'react';
+import type { MutableRefObject } from 'react';
+import { createContext, createRef, useContext, useEffect, useRef } from 'react';
 
 import { NavigationContainer as RNNavigationContainer } from '@react-navigation/native';
 
@@ -8,6 +9,28 @@ import type { GetProps } from 'tamagui';
 type IBasicNavigationContainerProps = GetProps<typeof RNNavigationContainer>;
 export type INavigationContainerProps = Partial<IBasicNavigationContainerProps>;
 export const rootNavigationRef = createRef<NavigationContainerRef<any>>();
+
+export type IRouterChangeEvent = INavigationContainerProps['onStateChange'];
+const RouterEventContext = createContext<
+  MutableRefObject<IRouterChangeEvent[]>
+>({
+  current: [],
+});
+
+export const useRouterEventsRef = () => useContext(RouterEventContext);
+export const RouterEventProvider = RouterEventContext.Provider;
+
+export const useOnRouterChange = (callback: IRouterChangeEvent) => {
+  const routerRef = useContext(RouterEventContext);
+  useEffect(() => {
+    routerRef.current.push(callback);
+    callback?.(rootNavigationRef.current?.getState());
+    return () => {
+      routerRef.current = routerRef.current.filter((i) => i !== callback);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
 
 export function NavigationContainer(props: IBasicNavigationContainerProps) {
   return <RNNavigationContainer {...props} ref={rootNavigationRef} />;
