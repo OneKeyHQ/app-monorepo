0c4a277196d7d1563c05e1d06d30cbd808e71741
huhuanming <huanming@onekey.so>
2024-01-03 17:22:18 +0800

    chore: deprecate plugins based on JS compatibility (#3997)

--- apps/web-embed/babel.config.js
+++ apps/web-embed/babel.config.js
@@ -3,7 +3,12 @@ const babelTools = require('../../development/babelTools');
 module.exports = babelTools.normalizeConfig({
   platform: babelTools.developmentConsts.platforms.webEmbed,
   config: {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        { web: { unstable_transformProfile: 'hermes-canary' } },
+      ],
+    ],
     plugins: ['react-native-reanimated/plugin'],
   },
 });
