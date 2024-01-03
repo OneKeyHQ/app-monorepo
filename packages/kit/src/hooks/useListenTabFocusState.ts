a53c1e601ddf9609010ff19337a0ea3f8bc84c29
huhuanming <huanming@onekey.so>
2024-01-03 20:21:02 +0800

    feat: support ETabRoutes array

--- packages/kit/src/hooks/useListenTabFocusState.ts
+++ packages/kit/src/hooks/useListenTabFocusState.ts
@@ -2,17 +2,20 @@ import { rootNavigationRef, useOnRouterChange } from '@onekeyhq/components';
 
 import { ERootRoutes } from '../routes/enum';
 
+import type { ETabRoutes } from '../routes/Tab/type';
+
 export default function useListenTabFocusState(
-  tabName: string,
+  tabName: ETabRoutes | ETabRoutes[],
   callback: (isFocus: boolean) => void,
 ) {
+  const tabNames = Array.isArray(tabName) ? tabName : [tabName];
   useOnRouterChange(() => {
     const rootState = rootNavigationRef.current
       ?.getState()
       .routes.find(({ name }) => name === ERootRoutes.Main)?.state;
     const currentTabName = rootState?.routeNames
-      ? rootState?.routeNames?.[rootState?.index || 0]
-      : rootState?.routes[0].name;
-    callback(currentTabName === tabName);
+      ? (rootState?.routeNames?.[rootState?.index || 0] as ETabRoutes)
+      : (rootState?.routes[0].name as ETabRoutes);
+    callback(tabNames.includes(currentTabName));
   });
 }
