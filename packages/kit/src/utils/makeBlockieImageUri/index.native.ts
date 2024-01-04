b280ab61e5fde384baaedbefd26b28004bf82e88
hublot <hublot@aliyun.com>
2024-01-04 15:30:55 +0800

    feat: replace the CachesDirectoryPath  to DocumentDirectoryPath

--- packages/kit/src/utils/makeBlockieImageUri/index.native.ts
+++ packages/kit/src/utils/makeBlockieImageUri/index.native.ts
@@ -1,19 +1,25 @@
 import makeBlockie from 'ethereum-blockies-base64';
 import RNFS from 'react-native-fs';
 
+// eslint-disable-next-line spellcheck/spell-checker
+const BLOCKIE_IMAGE_CACHE_DIR = `file://${RNFS.DocumentDirectoryPath}/blockies_avatar_cache`;
+
 export default function makeBlockieImageUri(id: string) {
   return new Promise<string>((resolve) => {
-    const filepath = `file://${RNFS.CachesDirectoryPath}/eth_base64_cache_${id}.png`;
+    const filepath = `${BLOCKIE_IMAGE_CACHE_DIR}/${id.replace('/', '')}.png`;
 
     const createNewEthBase64 = () => {
       const data = makeBlockie(id);
-      void RNFS.write(
-        filepath,
-        // Although it's a bit complicated, we only decode base64 once to improve performance.
-        data.replace('data:image/png;base64,', ''),
-        0,
-        'base64',
-      );
+      // Don't need check, same as `mkdir -p`
+      void RNFS.mkdir(BLOCKIE_IMAGE_CACHE_DIR).then(() => {
+        void RNFS.write(
+          filepath,
+          // Although it's a bit complicated, we only decode base64 once to improve performance.
+          data.replace('data:image/png;base64,', ''),
+          0,
+          'base64',
+        );
+      });
       resolve(data);
     };
     RNFS.exists(filepath)
