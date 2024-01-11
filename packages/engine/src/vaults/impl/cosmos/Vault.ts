408848645a2256ec072c3eca7aeaf93476a31eee
ByteZhang <quincysx@onekey.so>
2024-01-11 09:58:58 +0800

    v4 fix: support EIP6963 & fix cosmos signMessage (#4018)
    * feat: support petra wallet switch
    
    * feat: support cosmos signMessage
    
    * feat: support EIP6963
    
    * chore: fix lint

--- packages/engine/src/vaults/impl/cosmos/Vault.ts
+++ packages/engine/src/vaults/impl/cosmos/Vault.ts
@@ -72,7 +72,6 @@ import { TxAminoBuilder } from './sdk/amino/TxAminoBuilder';
 import { defaultAminoMsgOpts } from './sdk/amino/types';
 import { MessageType } from './sdk/message';
 import { queryRegistry } from './sdk/query/IQuery';
-import { Type } from './sdk/query/mintScanTypes';
 import { OneKeyQuery } from './sdk/query/OneKeyQuery';
 import { serializeSignedTx } from './sdk/txBuilder';
 import { TxMsgBuilder } from './sdk/txMsgBuilder';
