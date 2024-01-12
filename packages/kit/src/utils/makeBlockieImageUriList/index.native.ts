0d5183d7a2afe596bf401c44dd81ce0aa8338fdc
hublot <hublot@aliyun.com>
2024-01-12 16:39:27 +0800

    feat: add web worker to improve account selector performance (#4040)

---
+++ packages/kit/src/utils/makeBlockieImageUriList/index.native.ts
@@ -0,0 +1,48 @@
+import makeBlockie from 'ethereum-blockies-base64';
+import RNFS from 'react-native-fs';
+
+import {
+  BLOCKIE_IMAGE_BASE64_PREFIX,
+  BLOCKIE_IMAGE_CACHE_DIR,
+} from './BlockieImageCache.const';
+
+export default function makeBlockieImageUriList(idList: string[]) {
+  return RNFS.mkdir(BLOCKIE_IMAGE_CACHE_DIR).then(() =>
+    Promise.all(
+      idList.map(
+        (id) =>
+          new Promise<string>((resolve) => {
+            const filepath = `${BLOCKIE_IMAGE_CACHE_DIR}/${encodeURIComponent(
+              id,
+            )}.png`;
+
+            const createNewEthBase64 = () => {
+              const data = makeBlockie(id);
+              const writeCacheData = () => {
+                void RNFS.write(
+                  filepath,
+                  // Although it's a bit complicated, we only decode base64 once to improve performance.
+                  data.replace(BLOCKIE_IMAGE_BASE64_PREFIX, ''),
+                  0,
+                  'base64',
+                );
+              };
+              writeCacheData();
+              resolve(data);
+            };
+            RNFS.exists(filepath)
+              .then((isExists) => {
+                if (isExists) {
+                  resolve(filepath);
+                } else {
+                  createNewEthBase64();
+                }
+              })
+              .catch(() => {
+                createNewEthBase64();
+              });
+          }),
+      ),
+    ),
+  );
+}
