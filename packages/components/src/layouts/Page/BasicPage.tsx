8924d84790c7328152bbd89dc8491998bf06d17b
huhuanming <huanming@onekey.so>
2024-01-15 09:39:22 +0800

    fix: fix re-execute issues in Lazy Component via commit phrase (#4043)

--- packages/components/src/layouts/Page/BasicPage.tsx
+++ packages/components/src/layouts/Page/BasicPage.tsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect, useState } from 'react';
+
 import { useHeaderHeight } from '@react-navigation/elements';
 import { useWindowDimensions } from 'react-native';
 import { useMedia } from 'tamagui';
@@ -19,9 +21,15 @@ const usePageHeight = () => {
 export function BasicPage({ children }: IBasicPageProps) {
   // fix scrolling issues on md Web
   const height = usePageHeight();
-  return (
+
+  // fix re-execute issues in Lazy Component via render phrase
+  const [isLayoutMount, setIsLayoutMount] = useState(false);
+  useLayoutEffect(() => {
+    setIsLayoutMount(true);
+  }, []);
+  return isLayoutMount ? (
     <Stack bg="$bgApp" flex={1} maxHeight={height}>
       {children}
     </Stack>
-  );
+  ) : null;
 }
