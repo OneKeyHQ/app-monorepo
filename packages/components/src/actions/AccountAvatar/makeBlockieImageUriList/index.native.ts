2cac892b431d8cd1f1e9441edf9b693e14e4580c
huhuanming <huanming@onekey.so>
2024-01-17 23:34:00 +0800

    Update index.native.ts

--- packages/components/src/actions/AccountAvatar/makeBlockieImageUriList/index.native.ts
+++ packages/components/src/actions/AccountAvatar/makeBlockieImageUriList/index.native.ts
@@ -6,39 +6,28 @@ import {
   BLOCKIE_IMAGE_CACHE_DIR,
 } from './BlockieImageCache.const';
 
-export default function makeBlockieImageUri(id: string) {
-  return RNFS.mkdir(BLOCKIE_IMAGE_CACHE_DIR).then(
-    () =>
-      new Promise<string>((resolve) => {
-        const filepath = `${BLOCKIE_IMAGE_CACHE_DIR}/${encodeURIComponent(
-          id,
-        )}.png`;
+const caches: Record<string, string> = {};
 
-        const createNewEthBase64 = () => {
-          const data = makeBlockie(id);
-          const writeCacheData = () => {
-            void RNFS.write(
-              filepath,
-              // Although it's a bit complicated, we only decode base64 once to improve performance.
-              data.replace(BLOCKIE_IMAGE_BASE64_PREFIX, ''),
-              0,
-              'base64',
-            );
-          };
-          writeCacheData();
-          resolve(data);
-        };
-        RNFS.exists(filepath)
-          .then((isExists) => {
-            if (isExists) {
-              resolve(filepath);
-            } else {
-              createNewEthBase64();
-            }
-          })
-          .catch(() => {
-            createNewEthBase64();
-          });
-      }),
+const writeBlockieImage = async (id: string, filepath: string) => {
+  const data = makeBlockie(id);
+  await RNFS.write(
+    filepath,
+    // Although it's a bit complicated, we only decode base64 once to improve performance.
+    data.replace(BLOCKIE_IMAGE_BASE64_PREFIX, ''),
+    0,
+    'base64',
   );
+};
+export default async function makeBlockieImageUri(id: string) {
+  if (caches[id]) {
+    return caches[id];
+  }
+  await RNFS.mkdir(BLOCKIE_IMAGE_CACHE_DIR);
+  const filepath = `${BLOCKIE_IMAGE_CACHE_DIR}/${encodeURIComponent(id)}.png`;
+  const isExists = await RNFS.exists(filepath);
+  if (!isExists) {
+    await writeBlockieImage(id, filepath);
+  }
+  caches[id] = filepath;
+  return filepath;
 }
