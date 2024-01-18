c7b1ee26bd12e9584773ee6abcca52ee031494fa
huhuanming <huanming@onekey.so>
2024-01-18 11:13:47 +0800

    Update index.tsx

--- packages/kit/src/views/Discovery/components/CustomHeaderTitle/index.tsx
+++ packages/kit/src/views/Discovery/components/CustomHeaderTitle/index.tsx
@@ -14,38 +14,48 @@ export function CustomHeaderTitle() {
   const screenWidth = useWindowDimensions().width;
   return (
     <XStack
-      role="button"
+      width="100%"
+      justifyContent="center"
       $md={{
-        width: screenWidth - 40,
-      }}
-      minWidth="$64"
-      px="$2"
-      py="$1.5"
-      alignItems="center"
-      bg="$bgStrong"
-      borderRadius="$3"
-      hoverStyle={{
-        bg: '$bgHover',
-      }}
-      pressStyle={{
-        bg: '$bgActive',
-      }}
-      onPress={() => {
-        console.log('onPress');
-      }}
-      style={{
-        borderCurve: 'continuous',
+        ml: -22,
+        px: '$5',
+        width: screenWidth,
       }}
     >
-      <Icon name="SearchOutline" size="$5" color="$iconSubdued" />
-      <SizableText pl="$2" size="$bodyLg" color="$textSubdued" flex={1}>
-        Search
-      </SizableText>
-      {media.gtMd && (
-        <Shortcut>
-          <Shortcut.Key>/</Shortcut.Key>
-        </Shortcut>
-      )}
+      <XStack
+        role="button"
+        minWidth="$64"
+        $md={{
+          width: '100%',
+        }}
+        px="$2"
+        py="$1.5"
+        alignItems="center"
+        bg="$bgStrong"
+        borderRadius="$3"
+        hoverStyle={{
+          bg: '$bgHover',
+        }}
+        pressStyle={{
+          bg: '$bgActive',
+        }}
+        onPress={() => {
+          console.log('onPress');
+        }}
+        style={{
+          borderCurve: 'continuous',
+        }}
+      >
+        <Icon name="SearchOutline" size="$5" color="$iconSubdued" />
+        <SizableText pl="$2" size="$bodyLg" color="$textSubdued" flex={1}>
+          Search
+        </SizableText>
+        {media.gtMd && (
+          <Shortcut>
+            <Shortcut.Key>/</Shortcut.Key>
+          </Shortcut>
+        )}
+      </XStack>
     </XStack>
   );
 }
