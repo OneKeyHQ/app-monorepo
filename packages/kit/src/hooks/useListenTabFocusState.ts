604c0b3b4a9e6708775221e48194809ac624d655
huhuanming <huanming@onekey.so>
2024-01-31 12:24:27 +0800

    Update useListenTabFocusState.ts

--- packages/kit/src/hooks/useListenTabFocusState.ts
+++ packages/kit/src/hooks/useListenTabFocusState.ts
@@ -6,16 +6,25 @@ import type { ETabRoutes } from '../routes/Tab/type';
 
 export default function useListenTabFocusState(
   tabName: ETabRoutes | ETabRoutes[],
-  callback: (isFocus: boolean) => void,
+  callback: (isFocus: boolean, isHideByModal: boolean) => void,
 ) {
   const tabNames = Array.isArray(tabName) ? tabName : [tabName];
   useOnRouterChange((state) => {
     const rootState = state?.routes.find(
       ({ name }) => name === ERootRoutes.Main,
     )?.state;
+    const modalRoutes = state?.routes.find(
+      ({ name }) => name === ERootRoutes.Modal,
+    )?.state;
+    const fullModalRoutes = state?.routes.find(
+      ({ name }) => name === ERootRoutes.iOSFullScreen,
+    )?.state;
     const currentTabName = rootState?.routeNames
       ? (rootState?.routeNames?.[rootState?.index || 0] as ETabRoutes)
       : (rootState?.routes[0].name as ETabRoutes);
-    callback(tabNames.includes(currentTabName));
+    callback(
+      tabNames.includes(currentTabName),
+      !!(modalRoutes || fullModalRoutes),
+    );
   });
 }
