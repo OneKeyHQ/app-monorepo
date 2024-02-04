978530561553e836450c6b13ee59cbd673b57100
huhuanming <huanming@onekey.so>
2024-02-04 21:16:11 +0800

    fix: fix types

--- packages/kit-bg/src/states/jotai/atoms/password.ts
+++ packages/kit-bg/src/states/jotai/atoms/password.ts
@@ -1,3 +1,5 @@
+// TODO: move consts to shared
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { ELockDuration } from '@onekeyhq/kit/src/views/Setting/pages/AppAutoLock/const';
 import biologyAuth from '@onekeyhq/shared/src/biologyAuth';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
