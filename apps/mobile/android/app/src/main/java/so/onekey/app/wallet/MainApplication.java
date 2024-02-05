349e841d7e306520fc5b46967568ab0e23917cfc
huhuanming <huanming@onekey.so>
2024-02-05 22:52:03 +0800

    Revert "Update MainApplication.java"
    This reverts commit db5d6543fa1074a228801c3f44458190c8fd7daf.

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainApplication.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainApplication.java
@@ -85,9 +85,7 @@ public class MainApplication extends Application implements ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    if (BuildConfig.DEBUG) {
-      ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    }
+    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
 
