e917830de34dc340f4ceb92a40cd712c902219d8
kwoktung <onlyif2009@gmail.com>
2024-02-19 20:01:35 +0800

    feat: address book component (#4094)
    * feat: address book
    
    * feat: add address book tip
    
    * feat: add sectionList keyExtractor
    
    * feat: 🎸 add testID
    
    * feat: improve address book
    
    * chore: merge x branch and fix bug
    
    * feat: add pick addres book item
    
    * fix: address book update
    
    * fix: 🐛 change testID
    
    * fix: 🐛 add testID
    
    * fix: 🐛 add delete confirm testID
    
    * fix: 🐛 add testID
    
    * fix: 🐛 add form message testid
    
    * feat: update address item entity
    
    * feat: verify hash when user copy address book data
    
    * fix: 🐛 add testID
    
    * fix: 🐛 testID confic
    
    * feat: update address book service
    
    ---------
    
    Co-authored-by: hublot <hublot@aliyun.com>
    Co-authored-by: joe <joewong7321@gmail.com>

--- packages/kit-bg/src/apis/IBackgroundApi.ts
+++ packages/kit-bg/src/apis/IBackgroundApi.ts
@@ -10,6 +10,7 @@ import type ProviderApiBase from '../providers/ProviderApiBase';
 import type { ProviderApiWalletConnect } from '../providers/ProviderApiWalletConnect';
 import type ServiceAccount from '../services/ServiceAccount';
 import type ServiceAccountProfile from '../services/ServiceAccountProfile';
+import type ServiceAddressBook from '../services/ServiceAddressBook';
 import type ServiceApp from '../services/ServiceApp';
 import type ServiceBootstrap from '../services/ServiceBootstrap';
 import type ServiceDApp from '../services/ServiceDApp';
@@ -98,4 +99,5 @@ export interface IBackgroundApi extends IBackgroundApiBridge {
   serviceHardware: ServiceHardware;
 
   serviceOnboarding: ServiceOnboarding;
+  serviceAddressBook: ServiceAddressBook;
 }
--- packages/kit/src/views/Send/pages/SendDataInput/SendDataInputContainer.tsx
+++ packages/kit/src/views/Send/pages/SendDataInput/SendDataInputContainer.tsx
@@ -18,6 +18,7 @@ import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/background
 import {
   AddressInput,
   type IAddressInputValue,
+  allAddressInputPlugins,
 } from '@onekeyhq/kit/src/common/components/AddressInput';
 import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
 import { useAccountData } from '@onekeyhq/kit/src/hooks/useAccountData';
@@ -428,7 +429,11 @@ function SendDataInputContainer() {
               },
             }}
           >
-            <AddressInput networkId={networkId} />
+            <AddressInput
+              networkId={networkId}
+              enableAddressBook
+              plugins={allAddressInputPlugins}
+            />
           </Form.Field>
           {isNFT ? renderNFTDataInputForm() : renderTokenDataInputForm()}
         </Form>
