301bb2bf98f149f74911df7ca16842ad4405d845
huhuanming <huanming@onekey.so>
2024-02-27 14:46:45 +0800

    Update AmountInput.tsx

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/AmountInput.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/AmountInput.tsx
@@ -2,10 +2,156 @@
 /* eslint-disable react/no-unstable-nested-components */
 import { useState } from 'react';
 
-import { Button, Form, Stack, useForm } from '@onekeyhq/components';
+import {
+  Button,
+  Form,
+  Image,
+  ListView,
+  SizableText,
+  Stack,
+  Tooltip,
+  XStack,
+  useForm,
+} from '@onekeyhq/components';
+import type { IXStackProps } from '@onekeyhq/components';
 import { AmountInput } from '@onekeyhq/kit/src/components/AmountInput';
+import type { IListItemProps } from '@onekeyhq/kit/src/components/ListItem';
+import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
 
 import { Layout } from './utils/Layout';
+// TokenListItem in TokenSearchModal
+type ITokenListItemProps = {
+  tokenImageSrc?: string;
+  networkImageSrc?: string;
+  tokenName?: string;
+  tokenSymbol?: string;
+  tokenContrastAddress?: string;
+  balance?: string;
+  value?: string;
+} & IListItemProps;
+
+function TokenListItem({
+  tokenImageSrc,
+  networkImageSrc,
+  tokenName,
+  tokenSymbol,
+  tokenContrastAddress,
+  balance,
+  value,
+  ...rest
+}: ITokenListItemProps) {
+  return (
+    <ListItem userSelect="none" {...rest}>
+      <ListItem.Avatar
+        src={tokenImageSrc}
+        cornerImageProps={{
+          src: networkImageSrc,
+        }}
+      />
+      <ListItem.Text
+        flex={1}
+        primary={tokenName}
+        secondary={
+          <XStack>
+            <SizableText size="$bodyMd" color="$textSubdued" pr="$1.5">
+              {tokenSymbol}
+            </SizableText>
+            {tokenContrastAddress && (
+              <SizableText size="$bodyMd" color="$textDisabled">
+                {tokenContrastAddress}
+              </SizableText>
+            )}
+          </XStack>
+        }
+      />
+      <ListItem.Text align="right" primary={balance} secondary={value} />
+    </ListItem>
+  );
+}
+
+// NetworksFilterItem
+type INetworksFilterItemProps = {
+  networkImageUri?: string;
+  networkName?: string;
+  isSelected?: boolean;
+  tooltipContent?: string;
+} & IXStackProps;
+
+function NetworksFilterItem({
+  networkImageUri,
+  networkName,
+  isSelected,
+  tooltipContent,
+  ...rest
+}: INetworksFilterItemProps) {
+  const BaseComponent = (
+    <XStack
+      justifyContent="center"
+      px="$3"
+      py="$1.5"
+      bg={isSelected ? '$bgPrimary' : '$bgStrong'}
+      borderRadius="$2"
+      userSelect="none"
+      style={{
+        borderCurve: 'continuous',
+      }}
+      {...(!isSelected && {
+        focusable: true,
+        hoverStyle: {
+          bg: '$bgStrongHover',
+        },
+        pressStyle: {
+          bg: '$bgStrongActive',
+        },
+        focusStyle: {
+          outlineWidth: 2,
+          outlineStyle: 'solid',
+          outlineColor: '$focusRing',
+        },
+      })}
+      {...rest}
+    >
+      {networkImageUri && (
+        <Image
+          height="$6"
+          width="$6"
+          borderRadius="$full"
+          $gtMd={{
+            height: '$5',
+            width: '$5',
+          }}
+        >
+          <Image.Source
+            source={{
+              uri: networkImageUri,
+            }}
+          />
+        </Image>
+      )}
+      {networkName && (
+        <SizableText
+          color={isSelected ? '$textOnColor' : '$textSubdued'}
+          size="$bodyLgMedium"
+          $gtMd={{
+            size: '$bodyMdMedium',
+          }}
+        >
+          {networkName}
+        </SizableText>
+      )}
+    </XStack>
+  );
+
+  if (!tooltipContent) return BaseComponent;
+
+  return (
+    <Tooltip
+      renderContent={tooltipContent}
+      placement="top"
+      renderTrigger={BaseComponent}
+    />
+  );
+}
 
 const GalleryLayout = () => (
   <Layout
@@ -120,6 +266,74 @@ const GalleryLayout = () => (
             </Stack>
           );
         },
+      },
+      {
+        title: 'TokenListItem in TokenSearchModal',
+        element: (
+          <Stack>
+            {/* Networks filter */}
+            <Stack>
+              <XStack px="$5" pt="$1" pb="$3" space="$2">
+                <NetworksFilterItem
+                  networkName="All"
+                  tooltipContent="All Networks"
+                />
+                <NetworksFilterItem
+                  networkImageUri="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/eth.png"
+                  isSelected
+                  tooltipContent="Ethereum"
+                />
+                <NetworksFilterItem
+                  networkImageUri="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/btc.png"
+                  tooltipContent="Bitcoin"
+                />
+                <NetworksFilterItem
+                  networkImageUri="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/eth.png"
+                  tooltipContent="Ethereum"
+                />
+                <NetworksFilterItem
+                  networkImageUri="https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/btc.png"
+                  tooltipContent="Bitcoin"
+                />
+                <NetworksFilterItem networkName="12+" flex={1} />
+              </XStack>
+              <XStack px="$5" py="$2">
+                <SizableText size="$headingSm" pr="$2">
+                  Network:
+                </SizableText>
+                <XStack>
+                  <Image height="$5" width="$5" borderRadius="$full">
+                    <Image.Source
+                      source={{
+                        uri: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/eth.png',
+                      }}
+                    />
+                  </Image>
+                  <SizableText size="$bodyMd" pl="$2">
+                    Ethereum
+                  </SizableText>
+                </XStack>
+              </XStack>
+            </Stack>
+            {/* List – Scroll area */}
+            <ListView
+              estimatedItemSize={60}
+              data={new Array(10).fill({
+                tokenImageSrc:
+                  'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/usdc.png',
+                networkImageSrc:
+                  'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/color/matic.png',
+                tokenName: 'USD Coin',
+                tokenSymbol: 'USDC',
+                tokenContrastAddress: '0x1234...5678',
+                balance: '89.9',
+                value: '$89.75',
+                onPress: () => console.log('clicked'),
+              })}
+              renderItem={({ item }) => <TokenListItem {...item} />}
+            />
+          </Stack>
+        ),
       },
     ]}
   />
