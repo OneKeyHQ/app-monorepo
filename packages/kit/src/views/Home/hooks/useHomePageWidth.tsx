b1b6000bb5b43767dfd9e028520db8080f92228e
hublot <hublot@aliyun.com>
2024-02-29 19:47:01 +0800

    feat: fix flash when collapse is changed

---
+++ packages/kit/src/views/Home/hooks/useHomePageWidth.tsx
@@ -0,0 +1,19 @@
+import { useWindowDimensions } from 'react-native';
+import { useMedia } from 'tamagui';
+
+import useProviderSideBarValue from '@onekeyhq/components/src/hocs/Provider/hooks/useProviderSideBarValue';
+import { getTokens } from '@onekeyhq/components/src/hooks';
+
+export default function useHomePageWidth() {
+  const media = useMedia();
+  const screenWidth = useWindowDimensions().width;
+  const sideBarWidth = getTokens().size.sideBarWidth.val;
+  const { leftSidebarCollapsed } = useProviderSideBarValue();
+  return {
+    screenWidth,
+    pageWidth:
+      media.md || leftSidebarCollapsed
+        ? screenWidth
+        : screenWidth - sideBarWidth,
+  };
+}
