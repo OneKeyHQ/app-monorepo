e565efe5c60cf05705c180865b5aede0bb09542c
huhuanming <huanming@onekey.so>
2024-02-29 12:04:12 +0800

    feat: add number formatter functions (#4157)

--- packages/components/src/content/index.ts
+++ packages/components/src/content/index.ts
@@ -4,6 +4,7 @@ export * from './LinearGradient';
 export * from './BlurView';
 export * from './Empty';
 export * from './LottieView';
+export * from './NumberSizeableText';
 export * from './Progress';
 export * from './QRCode';
 export * from './Splash';
