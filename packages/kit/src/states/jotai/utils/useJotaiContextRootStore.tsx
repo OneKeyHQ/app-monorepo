133b83621b69a35797f10c793a098cff63503f17
morizon <sidmorizon@outlook.com>
2024-02-29 21:49:33 +0800

    fix: jotai context store remove by id (#4162)
    * fix: jotai context store remove by id
    
    * fix: lint

--- packages/kit/src/states/jotai/utils/useJotaiContextRootStore.tsx
+++ packages/kit/src/states/jotai/utils/useJotaiContextRootStore.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import type { IJotaiContextStoreData } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 
@@ -6,12 +6,16 @@ import { jotaiContextStore } from './jotaiContextStore';
 
 export function useJotaiContextRootStore(data: IJotaiContextStoreData) {
   const store = jotaiContextStore.getOrCreateStore(data);
+  const dataRef = useRef(data);
+  dataRef.current = data;
+
   useEffect(() => {
-    console.log('JotaiContextRootStore mount', data);
+    console.log('JotaiContextRootStore mount', dataRef.current);
     return () => {
-      console.log('JotaiContextRootStore unmount', data);
-      jotaiContextStore.removeStore(data);
+      console.log('JotaiContextRootStore unmount', dataRef.current);
+      jotaiContextStore.removeStore(dataRef.current);
     };
-  }, [data]);
+  }, []);
+
   return store;
 }
