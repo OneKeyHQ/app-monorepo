ae8c254951dce218e628ad4137848151d518140f
morizon <sidmorizon@outlook.com>
2024-02-29 18:23:19 +0800

    fix: lint

--- packages/kit/src/states/jotai/utils/useJotaiContextRootStore.tsx
+++ packages/kit/src/states/jotai/utils/useJotaiContextRootStore.tsx
@@ -2,25 +2,20 @@ import { useEffect, useRef } from 'react';
 
 import type { IJotaiContextStoreData } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 
-import {
-  buildJotaiContextStoreId,
-  jotaiContextStore,
-} from './jotaiContextStore';
+import { jotaiContextStore } from './jotaiContextStore';
 
 export function useJotaiContextRootStore(data: IJotaiContextStoreData) {
   const store = jotaiContextStore.getOrCreateStore(data);
   const dataRef = useRef(data);
   dataRef.current = data;
 
-  const id = buildJotaiContextStoreId(data);
   useEffect(() => {
     console.log('JotaiContextRootStore mount', dataRef.current);
     return () => {
       console.log('JotaiContextRootStore unmount', dataRef.current);
-      if (id) {
-        jotaiContextStore.removeStore(dataRef.current);
-      }
+      jotaiContextStore.removeStore(dataRef.current);
     };
-  }, [id]);
+  }, []);
+
   return store;
 }
