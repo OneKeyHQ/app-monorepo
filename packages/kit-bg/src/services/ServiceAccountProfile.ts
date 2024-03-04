272e5288e8d6c6d05a1e2329f8c50101a19f01cc
kwoktung <onlyif2009@gmail.com>
2024-03-04 14:44:41 +0800

    chore: fix lint

--- packages/kit-bg/src/services/ServiceAccountProfile.ts
+++ packages/kit-bg/src/services/ServiceAccountProfile.ts
@@ -70,52 +70,81 @@ class ServiceAccountProfile extends ServiceBase {
     enableWalletName,
   }: IQueryAddressArgs) {
     const result: IAddressQueryResult = { input: address };
-    if (networkId) {
-      result.isValid = await this.validateAddress({
+    if (!networkId) {
+      return result;
+    }
+    result.isValid = await this.validateAddress({
+      networkId,
+      address,
+    });
+    const isDomain = checkIsDomain(address);
+    if (isDomain && enableNameResolve) {
+      await this.handleNameSolve(networkId, address, result);
+    }
+    if (!result.isValid) {
+      return result;
+    }
+    const resolveAddress = result.resolveAddress ?? result.input;
+    if (enableAddressBook && resolveAddress) {
+      await this.handleAddressBookName(networkId, resolveAddress, result);
+    }
+    if (enableWalletName && resolveAddress) {
+      await this.handleWalletAccountName(networkId, resolveAddress, result);
+    }
+    return result;
+  }
+
+  private async handleNameSolve(
+    networkId: string,
+    address: string,
+    result: IAddressQueryResult,
+  ) {
+    const resolveNames =
+      await this.backgroundApi.serviceNameResolver.resolveName({
+        name: address,
         networkId,
-        address,
       });
-      const isDomain = checkIsDomain(address);
-      if (isDomain && enableNameResolve) {
-        const resolveNames =
-          await this.backgroundApi.serviceNameResolver.resolveName({
-            name: address,
-            networkId,
-          });
-        if (resolveNames && resolveNames.names?.length) {
-          result.resolveAddress = resolveNames.names?.[0].value;
-          result.resolveOptions = resolveNames.names?.map((o) => o.value);
-          if (!result.isValid) {
-            result.isValid = await this.validateAddress({
-              networkId,
-              address: result.resolveAddress,
-            });
-          }
-        }
+    if (resolveNames && resolveNames.names?.length) {
+      result.resolveAddress = resolveNames.names?.[0].value;
+      result.resolveOptions = resolveNames.names?.map((o) => o.value);
+      if (!result.isValid) {
+        result.isValid = await this.validateAddress({
+          networkId,
+          address: result.resolveAddress,
+        });
       }
-      if (result.isValid) {
-        const resolveAddress = result.resolveAddress ?? result.input;
-        if (enableAddressBook && resolveAddress) {
-          const addressBookItem =
-            await this.backgroundApi.serviceAddressBook.findItem({
-              networkId,
-              address: resolveAddress,
-            });
-          if (addressBookItem) {
-            result.addressBookName = addressBookItem.name;
-          }
-        }
-        if (enableWalletName && resolveAddress) {
-          const walletAccountItems =
-            await this.backgroundApi.serviceAccount.getAccountNameFromAddress({
-              networkId,
-              address: resolveAddress,
-            });
-          if (walletAccountItems && walletAccountItems.length) {
-            result.walletAccountName = walletAccountItems[0].accountName;
-          }
-        }
-      }
+    }
+    return result;
+  }
+
+  private async handleAddressBookName(
+    networkId: string,
+    resolveAddress: string,
+    result: IAddressQueryResult,
+  ) {
+    const addressBookItem =
+      await this.backgroundApi.serviceAddressBook.findItem({
+        networkId,
+        address: resolveAddress,
+      });
+    if (addressBookItem) {
+      result.addressBookName = addressBookItem.name;
+    }
+    return result;
+  }
+
+  private async handleWalletAccountName(
+    networkId: string,
+    resolveAddress: string,
+    result: IAddressQueryResult,
+  ) {
+    const walletAccountItems =
+      await this.backgroundApi.serviceAccount.getAccountNameFromAddress({
+        networkId,
+        address: resolveAddress,
+      });
+    if (walletAccountItems && walletAccountItems.length) {
+      result.walletAccountName = walletAccountItems[0].accountName;
     }
     return result;
   }
