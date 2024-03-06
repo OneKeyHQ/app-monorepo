4d1b2e9a1d52875ab6b4a41e077972a62868ea2f
huhuanming <huanming@onekey.so>
2024-03-06 19:52:18 +0800

    Update index.native.ts

--- packages/components/src/hocs/Provider/hooks/useAppearanceTheme/index.native.ts
+++ packages/components/src/hocs/Provider/hooks/useAppearanceTheme/index.native.ts
@@ -1,24 +1,39 @@
 import { useLayoutEffect } from 'react';
 
 import { StatusBar } from 'react-native';
+import { getTokenValue } from 'tamagui';
+
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import type { IUseAppearanceTheme } from './type';
 
 const setLightContent = (isAnimated = true) => {
-  StatusBar.pushStackEntry({
-    animated: isAnimated,
-    barStyle: 'light-content',
-  });
+  StatusBar.setBarStyle('light-content', isAnimated);
+
+  if (platformEnv.isNativeAndroid) {
+    StatusBar.setBackgroundColor(
+      getTokenValue('$bgAppLight', 'color'),
+      isAnimated,
+    );
+  }
 };
 
 const setDarkContent = (isAnimated = true) => {
-  StatusBar.pushStackEntry({
-    animated: isAnimated,
-    barStyle: 'dark-content',
-  });
+  StatusBar.setBarStyle('dark-content', isAnimated);
+  if (platformEnv.isNativeAndroid) {
+    StatusBar.setBackgroundColor(
+      getTokenValue('$bgAppDark', 'color'),
+      isAnimated,
+    );
+  }
 };
 
 export const useAppearanceTheme: IUseAppearanceTheme = (themeVariant) => {
+  console.log(
+    'useAppearanceTheme---',
+    getTokenValue('$bgAppLight', 'color'),
+    getTokenValue('$bgAppDark', 'color'),
+  );
   useLayoutEffect(() => {
     if (themeVariant === 'light') {
       setDarkContent();
