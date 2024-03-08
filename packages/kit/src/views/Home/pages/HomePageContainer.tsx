91b2fc961b47522b3c91ec8eaaec929de49cd0a2
huhuanming <huanming@onekey.so>
2024-03-08 16:38:47 +0800

    fix: remove black block on Android

--- packages/kit/src/views/Home/pages/HomePageContainer.tsx
+++ packages/kit/src/views/Home/pages/HomePageContainer.tsx
@@ -129,7 +129,11 @@ function HomePage({ onPressHide }: { onPressHide: () => void }) {
           />
           <Page.Body>
             {platformEnv.isNative && (
-              <XStack justifyContent="space-between" px="$4" pt="$20">
+              <XStack
+                justifyContent="space-between"
+                px="$4"
+                pt={platformEnv.isNativeIOS ? '$20' : 0}
+              >
                 <Stack flex={1}>{headerLeft()}</Stack>
                 <HeaderButtonGroup testID="Wallet-Page-Header-Right">
                   <HeaderIconButton title="Scan" icon="ScanOutline" />
