0ae276f6f41669f389ad3209327f42edea9d817a
hublot <hublot@aliyun.com>
2024-03-12 12:12:08 +0800

    feat: remove the AppContainer because of e2e touch problem

--- packages/kit/src/provider/Container/FullWindowOverlayContainer/OverlayContainer.ios.tsx
+++ packages/kit/src/provider/Container/FullWindowOverlayContainer/OverlayContainer.ios.tsx
@@ -2,18 +2,6 @@ import { FullWindowOverlay } from 'react-native-screens';
 
 import type { IOverlayContainerProps } from './type';
 
-function DevAppContainer({ children }: IOverlayContainerProps) {
-  if (!__DEV__) {
-    return <>{children}</>;
-  }
-  const AppContainer = require('react-native/Libraries/ReactNative/AppContainer');
-  return <AppContainer internal_excludeLogBox>{children}</AppContainer>;
-}
-
 export function OverlayContainer({ children }: IOverlayContainerProps) {
-  return (
-    <FullWindowOverlay>
-      <DevAppContainer>{children}</DevAppContainer>
-    </FullWindowOverlay>
-  );
+  return <FullWindowOverlay>{children}</FullWindowOverlay>;
 }
