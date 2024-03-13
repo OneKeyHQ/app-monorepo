cb9f4ced002ad855b041e6983e97e33739bc8453
weatherstar <weather@onekey.so>
2024-03-13 11:05:03 +0800

    fix: btc fork extends error

--- packages/kit-bg/src/vaults/impls/bch/Vault.ts
+++ packages/kit-bg/src/vaults/impls/bch/Vault.ts
@@ -1,3 +1,17 @@
+import { IDBWalletType } from '../../../dbs/local/types';
+import { KeyringBase } from '../../base/KeyringBase';
 import VaultBtc from '../btc/Vault';
+import { KeyringHardware } from './KeyringHardware';
+import { KeyringHd } from './KeyringHd';
+import { KeyringImported } from './KeyringImported';
+import { KeyringWatching } from './KeyringWatching';
 
-export default class Vault extends VaultBtc {}
+export default class Vault extends VaultBtc {
+  override keyringMap: Record<IDBWalletType, typeof KeyringBase> = {
+    hd: KeyringHd,
+    hw: KeyringHardware,
+    imported: KeyringImported,
+    watching: KeyringWatching,
+    external: KeyringWatching,
+  };
+}
