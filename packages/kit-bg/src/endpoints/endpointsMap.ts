63c5d0560075be565e3dbdacf9b126b05627ccae
王山栋 <jelly@onekey.so>
2024-03-13 11:02:05 +0800

    fix: fix endpoint
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/kit-bg/src/endpoints/endpointsMap.ts
+++ packages/kit-bg/src/endpoints/endpointsMap.ts
@@ -4,16 +4,11 @@ import type { IEndpoint, IEndpointEnv } from '@onekeyhq/shared/types/endpoint';
 const endpointsMap: Record<IEndpointEnv, IEndpoint> = {
   test: {
     http: 'https://rest.onekeytest.com',
-    // http: 'http://192.168.5.152:7001',
-    // http: ' http://10.44.23.238:7001',
-
     websocket: '',
   },
   prod: {
     // TODO: change to prod endpoint
     http: 'https://rest.onekeytest.com',
-    // http: 'http://192.168.5.152:7001',
-    // http: ' http://10.44.22.95:7001',
     websocket: '',
   },
 };
