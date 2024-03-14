6408df4a6f89354bddc1b059463a03d078dc88b9
huhuanming <huanming@onekey.so>
2024-03-14 18:03:28 +0800

    Update hooks.ts

--- packages/kit/src/views/Onboarding/components/hooks.ts
+++ packages/kit/src/views/Onboarding/components/hooks.ts
@@ -141,6 +141,7 @@ export const useSuggestion = (form: ReturnType<typeof useForm>) => {
       updateByPressLock.current = true;
       updateInputValue(word);
       resetSuggestions();
+      // the value of invalid word is undefined
       if (word && word.length > 0) {
         await focusNextInput();
         setTimeout(
