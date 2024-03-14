0d37b3ccb5d6c5f2a7b4cde93b7f6d752d838c1b
huhuanming <huanming@onekey.so>
2024-03-14 11:39:16 +0800

    Update useColor.ts

--- packages/components/src/hooks/useColor.ts
+++ packages/components/src/hooks/useColor.ts
@@ -1,11 +1,6 @@
-import platformEnv from '@onekeyhq/shared/src/platformEnv';
-
 import { useThemeValue } from './useStyle';
 
 export const useSelectionColor = () => {
-  // Android's transparent color is darker, so a lighter bgPrimary is used
-  const selectionColor = useThemeValue(
-    platformEnv.isNativeAndroid ? 'bgPrimaryActive' : 'bgPrimary',
-  );
+  const selectionColor = useThemeValue('bgPrimaryActive');
   return selectionColor;
 };
