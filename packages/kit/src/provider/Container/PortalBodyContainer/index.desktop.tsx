594fb1eaeb6b922474b2ff9a84e2bf0176160bb5
huhuanming <huanming@onekey.so>
2024-03-18 17:08:21 +0800

    chore: re-render web tabs on tab bar (#4255)

--- packages/kit/src/provider/Container/PortalBodyContainer/index.desktop.tsx
+++ packages/kit/src/provider/Container/PortalBodyContainer/index.desktop.tsx
@@ -1,12 +1,13 @@
 import { useMemo } from 'react';
 
-import { Portal } from '@onekeyhq/components';
+import { Portal, useMedia } from '@onekeyhq/components';
 
 import DesktopCustomTabBar from '../../../views/Discovery/pages/DesktopCustomTabBar';
 
 export function PortalBodyContainer() {
+  const { md } = useMedia();
   const memoDesktopCustomTabBar = useMemo(() => <DesktopCustomTabBar />, []);
-  return (
+  return md ? null : (
     <Portal.Body container={Portal.Constant.WEB_TAB_BAR}>
       {memoDesktopCustomTabBar}
     </Portal.Body>
