2d317567200a9c055515e762aad976e9eee29275
huhuanming <huanming@onekey.so>
2024-03-19 19:23:45 +0800

    feat: add numberFormat

--- packages/components/src/content/NumberSizeableText/index.tsx
+++ packages/components/src/content/NumberSizeableText/index.tsx
@@ -1,38 +1,17 @@
 import { useMemo } from 'react';
 
-import type { IFormatterOptions } from '@onekeyhq/shared/src/utils/numberUtils';
-import {
-  formatBalance,
-  formatDisplayNumber,
-  formatMarketCap,
-  formatPrice,
-  formatPriceChange,
-  formatValue,
-} from '@onekeyhq/shared/src/utils/numberUtils';
+import type { INumberFormatProps } from '@onekeyhq/shared/src/utils/numberUtils';
+import { numberFormat } from '@onekeyhq/shared/src/utils/numberUtils';
 
 import { SizableText } from '../../primitives';
 
 import type { ISizableTextProps } from '../../primitives';
 
-export const NUMBER_FORMATTER = {
-  /** Balance/Amount */
-  balance: formatBalance,
-  /** Price/USD */
-  price: formatPrice,
-  /** PriceChange */
-  priceChange: formatPriceChange,
-  /** DeFi */
-  value: formatValue,
-  /** FDV / MarketCap / Volume / Liquidty / TVL / TokenSupply */
-  marketCap: formatMarketCap,
-};
-
-export type INumberSizeableTextProps = Omit<ISizableTextProps, 'children'> & {
-  formatter: keyof typeof NUMBER_FORMATTER;
-  subTextStyle?: Omit<ISizableTextProps, 'children'>;
-  formatterOptions?: IFormatterOptions;
-  children: string | number | undefined;
-};
+export type INumberSizeableTextProps = Omit<ISizableTextProps, 'children'> &
+  Omit<INumberFormatProps, 'value'> & {
+    subTextStyle?: Omit<ISizableTextProps, 'children'>;
+    children: string | number | undefined;
+  };
 
 const subTextStyles = {
   sup: undefined,
@@ -52,12 +31,11 @@ export function NumberSizeableText({
   const result = useMemo(
     () =>
       ['string', 'number'].includes(typeof children)
-        ? formatDisplayNumber(
-            NUMBER_FORMATTER[formatter](String(children), formatterOptions),
-          )
+        ? numberFormat({ formatter, formatterOptions, value: children }, true)
         : '',
     [formatter, formatterOptions, children],
   );
+
   return typeof result === 'string' ? (
     <SizableText {...props}>{result}</SizableText>
   ) : (
--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/NumberSizeableTextGallery.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/NumberSizeableTextGallery.tsx
@@ -1,4 +1,5 @@
-import { NumberSizeableText, YStack } from '@onekeyhq/components';
+import { NumberSizeableText, SizableText, YStack } from '@onekeyhq/components';
+import { numberFormat } from '@onekeyhq/shared/src/utils/numberUtils';
 
 import { Layout } from './utils/Layout';
 
@@ -13,16 +14,43 @@ const NumberSizeableTextGallery = () => (
         element: (
           <YStack space="$3">
             <NumberSizeableText formatter="balance">1abcd1</NumberSizeableText>
+            <SizableText>
+              {numberFormat({ value: '1abcd1', formatter: 'balance' })}
+            </SizableText>
             <NumberSizeableText formatter="balance">
               564230002184512.1242
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '564230002184512.1242',
+                formatter: 'balance',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="balance">
               39477128561230002184512.1242
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '39477128561230002184512.1242',
+                formatter: 'balance',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="balance">0.0045</NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.0045',
+                formatter: 'balance',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="balance">
               0.00000002146
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.00000002146',
+                formatter: 'balance',
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="balance"
               formatterOptions={{
@@ -32,6 +60,12 @@ const NumberSizeableTextGallery = () => (
             >
               0.0000000214562
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.0000000214562',
+                formatter: 'balance',
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="balance"
               formatterOptions={{
@@ -41,6 +75,16 @@ const NumberSizeableTextGallery = () => (
             >
               -100.16798000000214562
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '-100.16798000000214562',
+                formatter: 'balance',
+                formatterOptions: {
+                  tokenSymbol: 'USDT',
+                  showPlusMinusSigns: true,
+                },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="balance"
               formatterOptions={{
@@ -50,12 +94,28 @@ const NumberSizeableTextGallery = () => (
             >
               202.16798000000214562
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '202.16798000000214562',
+                formatter: 'balance',
+                formatterOptions: {
+                  tokenSymbol: 'USDC',
+                  showPlusMinusSigns: true,
+                },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="balance"
               subTextStyle={{ color: 'red' }}
             >
               0.00000002146
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.00000002146',
+                formatter: 'balance',
+              })}
+            </SizableText>
           </YStack>
         ),
       },
@@ -69,24 +129,53 @@ const NumberSizeableTextGallery = () => (
             >
               1abcd1
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '1abcd1',
+                formatter: 'price',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="price"
               formatterOptions={{ currency: '$' }}
             >
               13557362245700035555161495398047413998367933131241010410691763880119784559016062844916472252762015173133555676356423519969743085158179152633859513576266605508375167501289296167138332859964556394542868213514778276007018586151530368896935403362153851120149886761999054463554127943866078939583808923520112330553910779375966862567701643361707370405490856611696753232661556874041759.1242
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value:
+                  '13557362245700035555161495398047413998367933131241010410691763880119784559016062844916472252762015173133555676356423519969743085158179152633859513576266605508375167501289296167138332859964556394542868213514778276007018586151530368896935403362153851120149886761999054463554127943866078939583808923520112330553910779375966862567701643361707370405490856611696753232661556874041759.1242',
+                formatter: 'price',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="price"
               formatterOptions={{ currency: '$' }}
             >
               0.0045
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.0045',
+                formatter: 'price',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="price"
               formatterOptions={{ currency: '$' }}
             >
               0.00000002146
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.00000002146',
+                formatter: 'price',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
           </YStack>
         ),
       },
@@ -97,15 +186,39 @@ const NumberSizeableTextGallery = () => (
             <NumberSizeableText formatter="priceChange">
               1abcd1
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '1abcd1',
+                formatter: 'priceChange',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="priceChange">
               12312381912937323374.7
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '12312381912937323374.7',
+                formatter: 'priceChange',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="priceChange">
               -0.02
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '-0.02',
+                formatter: 'priceChange',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="priceChange">
               -6218129
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '-6218129',
+                formatter: 'priceChange',
+              })}
+            </SizableText>
           </YStack>
         ),
       },
@@ -119,24 +232,52 @@ const NumberSizeableTextGallery = () => (
             >
               1abcd1
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '1abcd1',
+                formatter: 'value',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="value"
               formatterOptions={{ currency: '$' }}
             >
               0.009
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.009',
+                formatter: 'value',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="value"
               formatterOptions={{ currency: '$' }}
             >
               912312381912937323375
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '912312381912937323375',
+                formatter: 'value',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
             <NumberSizeableText
               formatter="value"
               formatterOptions={{ currency: '$' }}
             >
               12312381912937323374.7
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '12312381912937323374.7',
+                formatter: 'value',
+                formatterOptions: { currency: '$' },
+              })}
+            </SizableText>
           </YStack>
         ),
       },
@@ -147,21 +288,57 @@ const NumberSizeableTextGallery = () => (
             <NumberSizeableText formatter="marketCap">
               1abcd1
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '1abcd1',
+                formatter: 'marketCap',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="marketCap">
               0.125423
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '0.125423',
+                formatter: 'marketCap',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="marketCap">
               22.125423
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '22.125423',
+                formatter: 'marketCap',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="marketCap">
               882134512
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '882134512',
+                formatter: 'marketCap',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="marketCap">
               235002184512.1242
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '235002184512.1242',
+                formatter: 'marketCap',
+              })}
+            </SizableText>
             <NumberSizeableText formatter="marketCap">
               564200002184512.1242
             </NumberSizeableText>
+            <SizableText>
+              {numberFormat({
+                value: '564200002184512.1242',
+                formatter: 'marketCap',
+              })}
+            </SizableText>
           </YStack>
         ),
       },
