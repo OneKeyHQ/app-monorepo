e45b1700ded2dc7e150e0a172ab13b6ff0b03106
morizon <sidmorizon@outlook.com>
2024-03-20 15:10:24 +0800

    fix: lint

--- apps/desktop/src-electron/config.ts
+++ apps/desktop/src-electron/config.ts
@@ -1,15 +1,3 @@
-export const allowedDomains = [
-  'localhost',
-  '127.0.0.1',
-  'api.github.com',
-  'o554666.ingest.sentry.io',
-  'onekey.so',
-  'swap.onekey.so',
-  'portfolio.onekey.so',
-  'discover.onekey.so',
-  '243096.com',
-  'onekey-asset.com',
-];
 
 export const cspRules = [
   // Default to only own resources
