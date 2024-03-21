320e644466033d4e7de90205a7ea5c7c48896cfb
Leon <lixiao.dev@gmail.com>
2024-03-21 11:50:20 +0800

    feat: Add lightning core

---
+++ packages/core/src/chains/lightning/CoreChainSoftware.ts
@@ -0,0 +1,102 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import bufferUtils from '@onekeyhq/shared/src/utils/bufferUtils';
+
+import { CoreChainApiBase } from '../../base/CoreChainApiBase';
+
+import type {
+  ICoreApiGetAddressItem,
+  ICoreApiGetAddressQueryImported,
+  ICoreApiGetAddressQueryPublicKey,
+  ICoreApiGetAddressesQueryHd,
+  ICoreApiGetAddressesResult,
+  ICoreApiPrivateKeysMap,
+  ICoreApiSignBasePayload,
+  ICoreApiSignMsgPayload,
+  ICoreApiSignTxPayload,
+  ICurveName,
+  ISignedTxPro,
+} from '../../types';
+
+const curve: ICurveName = 'ed25519';
+
+export default class CoreChainSoftware extends CoreChainApiBase {
+  override async getPrivateKeys(
+    payload: ICoreApiSignBasePayload,
+  ): Promise<ICoreApiPrivateKeysMap> {
+    // throw new Error('Method not implemented.');
+    return this.baseGetPrivateKeys({
+      payload,
+      curve,
+    });
+  }
+
+  override async signTransaction(
+    payload: ICoreApiSignTxPayload,
+  ): Promise<ISignedTxPro> {
+    // throw new Error('Method not implemented.');
+    const { unsignedTx } = payload;
+    const signer = await this.baseGetSingleSigner({
+      payload,
+      curve,
+    });
+    // eslint-disable-next-line prefer-destructuring
+    const encodedTx = unsignedTx.encodedTx;
+    const txBytes = bufferUtils.toBuffer('');
+    const [signature] = await signer.sign(txBytes);
+    const txid = '';
+    const rawTx = '';
+    return {
+      encodedTx: unsignedTx.encodedTx,
+      txid,
+      rawTx,
+    };
+  }
+
+  override async signMessage(payload: ICoreApiSignMsgPayload): Promise<string> {
+    // throw new Error('Method not implemented.');
+    // eslint-disable-next-line prefer-destructuring
+    const unsignedMsg = payload.unsignedMsg;
+    const signer = await this.baseGetSingleSigner({
+      payload,
+      curve,
+    });
+    const msgBytes = bufferUtils.toBuffer('');
+    const [signature] = await signer.sign(msgBytes);
+    return '';
+  }
+
+  override async getAddressFromPrivate(
+    query: ICoreApiGetAddressQueryImported,
+  ): Promise<ICoreApiGetAddressItem> {
+    // throw new Error('Method not implemented.');
+    const { privateKeyRaw } = query;
+    const privateKey = bufferUtils.toBuffer(privateKeyRaw);
+    const pub = this.baseGetCurve(curve).publicFromPrivate(privateKey);
+    return this.getAddressFromPublic({
+      publicKey: bufferUtils.bytesToHex(pub),
+      networkInfo: query.networkInfo,
+    });
+  }
+
+  override async getAddressFromPublic(
+    query: ICoreApiGetAddressQueryPublicKey,
+  ): Promise<ICoreApiGetAddressItem> {
+    // throw new Error('Method not implemented.');
+    const { publicKey } = query;
+    const address = '';
+    return Promise.resolve({
+      address,
+      publicKey,
+    });
+  }
+
+  override async getAddressesFromHd(
+    query: ICoreApiGetAddressesQueryHd,
+  ): Promise<ICoreApiGetAddressesResult> {
+    // throw new Error('Method not implemented.');
+    return this.baseGetAddressesFromHd(query, {
+      curve,
+    });
+  }
+}
