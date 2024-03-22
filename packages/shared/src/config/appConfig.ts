896655bb4c154f099d3a5336d93ba4f53546119b
ByteZhang <quincysx@onekey.so>
2024-03-22 14:49:46 +0800

    chore: optimized pro check bridge

--- packages/shared/src/config/appConfig.ts
+++ packages/shared/src/config/appConfig.ts
@@ -14,7 +14,7 @@ export const HARDWARE_SDK_IFRAME_SRC_ONEKEYSO =
 export const HARDWARE_SDK_IFRAME_SRC_ONEKEYCN =
   process.env.HARDWARE_SDK_CONNECT_SRC_ONEKEYCN || 'https://jssdk.onekeycn.com';
 
-export const HARDWARE_SDK_VERSION = '0.3.41';
+export const HARDWARE_SDK_VERSION = '0.3.42';
 
 export const HARDWARE_BRIDGE_DOWNLOAD_URL =
   'https://onekey.so/download/?client=bridge';
