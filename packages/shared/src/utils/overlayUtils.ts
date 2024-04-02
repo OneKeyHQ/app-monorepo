332ce036eb541fed8424e4618fdd42e01a5b4d2f
huhuanming <huanming@onekey.so>
2024-04-02 23:16:47 +0800

    Update overlayUtils.ts

--- packages/shared/src/utils/overlayUtils.ts
+++ packages/shared/src/utils/overlayUtils.ts
@@ -2,6 +2,10 @@ export const APP_STATE_LOCK_Z_INDEX = 1e8;
 
 export const RESET_OVERLAY_Z_INDEX = APP_STATE_LOCK_Z_INDEX + 2;
 
-// in tamagui:
+// in Tamagui:
 //  the z-index of the Sheet is 1e5 - 1.
 //  the z-index of the Dialog is 1e5.
+//  the z-index of the Toast is 1e5.
+//  the z-index of the Popover 15e4.
+
+//  the z-index of burnt toast is 1e6.
