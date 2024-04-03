5a06dec74d053e3bdb3f78580e48dd52cec28efe
huhuanming <huanming@onekey.so>
2024-04-03 22:47:00 +0800

    Update ChunkedItemsView.tsx

--- packages/kit/src/views/Discovery/pages/Dashboard/SuggestAndExploreSection/ChunkedItemsView.tsx
+++ packages/kit/src/views/Discovery/pages/Dashboard/SuggestAndExploreSection/ChunkedItemsView.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from 'react';
 
+import { Dimensions } from 'react-native';
+
 import {
   Badge,
   Image,
@@ -14,6 +16,7 @@ import type {
   IScrollViewProps,
   IStackProps,
 } from '@onekeyhq/components';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import type { ICategory, IDApp } from '@onekeyhq/shared/types/discovery';
 
 import type { IMatchDAppItemType } from '../../../types';
@@ -31,6 +34,9 @@ type IChunkedItemsViewProps = {
   handleOpenWebSite: ({ dApp, webSite }: IMatchDAppItemType) => void;
   isExploreView: boolean;
 };
+
+const CARD_WIDTH_IN_MD = Dimensions.get('window').width - 48;
+const SPACING_FOR_CARD_INSET = 100;
 
 export function ItemsContainer({
   children,
@@ -51,6 +57,24 @@ export function ItemsContainer({
       pagingEnabled
       horizontal={horizontal}
       showsHorizontalScrollIndicator={false}
+      decelerationRate={0}
+      snapToInterval={CARD_WIDTH_IN_MD}
+      contentInset={
+        platformEnv.isNativeIOS
+          ? {
+              top: 0,
+              left: SPACING_FOR_CARD_INSET, // Left spacing for the very first card
+              bottom: 0,
+              right: SPACING_FOR_CARD_INSET, // Right spacing for the very last card
+            }
+          : undefined
+      }
+      contentContainerStyle={{
+        // contentInset alternative for Android
+        paddingHorizontal: platformEnv.isNativeAndroid
+          ? SPACING_FOR_CARD_INSET
+          : 0, // Horizontal spacing before and after the ScrollView
+      }}
       {...rest}
     >
       {children}
@@ -64,108 +88,110 @@ export function ChunkedItemsView({
   handleOpenWebSite,
 }: IChunkedItemsViewProps) {
   return (
-    <ItemsContainer mx="$-5" horizontal={!isExploreView}>
-      <XStack
-        px="$2"
-        $md={{
+    <ItemsContainer
+      mx="$-5"
+      horizontal={!isExploreView}
+      contentContainerStyle={{
+        px: '$2',
+        $md: {
           flexDirection: isExploreView ? 'column' : 'row',
-        }}
-        $gtMd={{
+        },
+        $gtMd: {
           flexDirection: 'column',
-        }}
-      >
-        {dataChunks.map((chunk, chunkIndex) => (
-          <Stack
-            key={chunkIndex}
-            $md={
-              isExploreView
-                ? {
-                    flexDirection: 'row',
-                    flexWrap: 'wrap',
-                  }
-                : {
-                    w: '$96',
-                  }
-            }
-            $gtMd={{
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-            }}
-          >
-            {chunk.map((item) => (
-              <XStack
-                key={item.dappId}
-                p="$3"
-                space="$3"
-                alignItems="center"
-                $md={
-                  isExploreView
-                    ? {
-                        flexBasis: '100%',
-                      }
-                    : undefined
+        },
+      }}
+    >
+      {dataChunks.map((chunk, chunkIndex) => (
+        <Stack
+          key={chunkIndex}
+          $md={
+            isExploreView
+              ? {
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
                 }
-                $gtMd={{
-                  flexBasis: '50%',
-                }}
-                $gtLg={{
-                  flexBasis: '33.3333%',
-                }}
-                onPress={() =>
-                  handleOpenWebSite({
-                    webSite: {
-                      url: item.url,
-                      title: item.name,
-                    },
-                  })
+              : {
+                  w: CARD_WIDTH_IN_MD,
                 }
-              >
-                <Image w="$14" h="$14" borderRadius="$3">
-                  <Image.Source
-                    source={{
-                      uri: item.logo,
-                    }}
-                  />
-                </Image>
-                <Stack flex={1}>
-                  <XStack alignItems="center">
-                    <SizableText
-                      size="$bodyLgMedium"
-                      $gtMd={{
-                        size: '$bodyMdMedium',
-                      }}
-                      numberOfLines={1}
-                    >
-                      {item.name}
-                    </SizableText>
-                    {Array.isArray(item.tags) && item.tags.length ? (
-                      <Badge
-                        badgeSize="sm"
-                        badgeType={item.tags[0].type as IBadgeType}
-                        ml="$2"
-                      >
-                        {item.tags[0].name}
-                      </Badge>
-                    ) : null}
-                  </XStack>
+          }
+          $gtMd={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+          }}
+        >
+          {chunk.map((item) => (
+            <XStack
+              key={item.dappId}
+              p="$3"
+              space="$3"
+              alignItems="center"
+              $md={
+                isExploreView
+                  ? {
+                      flexBasis: '100%',
+                    }
+                  : undefined
+              }
+              $gtMd={{
+                flexBasis: '50%',
+              }}
+              $gtLg={{
+                flexBasis: '33.3333%',
+              }}
+              onPress={() =>
+                handleOpenWebSite({
+                  webSite: {
+                    url: item.url,
+                    title: item.name,
+                  },
+                })
+              }
+            >
+              <Image w="$14" h="$14" borderRadius="$3">
+                <Image.Source
+                  source={{
+                    uri: item.logo,
+                  }}
+                />
+              </Image>
+              <Stack flex={1}>
+                <XStack alignItems="center">
                   <SizableText
-                    size="$bodyMd"
-                    color="$textSubdued"
-                    numberOfLines={1}
+                    size="$bodyLgMedium"
                     $gtMd={{
-                      size: '$bodySm',
-                      numberOfLines: 2,
-                      whiteSpace: 'break-spaces',
+                      size: '$bodyMdMedium',
                     }}
+                    numberOfLines={1}
                   >
-                    {item.description}
+                    {item.name}
                   </SizableText>
-                </Stack>
-              </XStack>
-            ))}
-          </Stack>
-        ))}
-      </XStack>
+                  {Array.isArray(item.tags) && item.tags.length ? (
+                    <Badge
+                      badgeSize="sm"
+                      badgeType={item.tags[0].type as IBadgeType}
+                      ml="$2"
+                    >
+                      {item.tags[0].name}
+                    </Badge>
+                  ) : null}
+                </XStack>
+                <SizableText
+                  size="$bodyMd"
+                  color="$textSubdued"
+                  numberOfLines={1}
+                  $gtMd={{
+                    size: '$bodySm',
+                    numberOfLines: 2,
+                    whiteSpace: 'break-spaces',
+                  }}
+                >
+                  {item.description}
+                </SizableText>
+              </Stack>
+            </XStack>
+          ))}
+        </Stack>
+      ))}
     </ItemsContainer>
   );
 }
