ca4516eabb638af909aea2483e9cd14da68a21da
morizon <sidmorizon@outlook.com>
2024-04-03 11:29:16 +0800

    fix: import address styles

--- packages/kit/src/views/Onboarding/pages/ImportWallet/ImportAddress.tsx
+++ packages/kit/src/views/Onboarding/pages/ImportWallet/ImportAddress.tsx
@@ -1,168 +1,77 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
-import { trim } from 'lodash';
-
-import {
-  Form,
-  Input,
-  Page,
-  SizableText,
-  useForm,
-  useFormWatch,
-} from '@onekeyhq/components';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
 import { AccountSelectorProviderMirror } from '@onekeyhq/kit/src/components/AccountSelector';
-import { DeriveTypeSelectorTriggerStaticInput } from '@onekeyhq/kit/src/components/AccountSelector/DeriveTypeSelectorTrigger';
-import { ControlledNetworkSelectorTrigger } from '@onekeyhq/kit/src/components/AccountSelector/NetworkSelectorTrigger';
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
-import { useDebounce } from '@onekeyhq/kit/src/hooks/useDebounce';
 import { useAccountSelectorActions } from '@onekeyhq/kit/src/states/jotai/contexts/accountSelector';
-import type { IAccountDeriveTypes } from '@onekeyhq/kit-bg/src/vaults/types';
-import { getNetworkIdsMap } from '@onekeyhq/shared/src/config/networkIds';
+import type { IValidateGeneralInputParams } from '@onekeyhq/kit-bg/src/vaults/types';
 import { WALLET_TYPE_WATCHING } from '@onekeyhq/shared/src/consts/dbConsts';
 import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
-import type { IGeneralInputValidation } from '@onekeyhq/shared/types/address';
 
 import { Tutorials } from '../../components';
 
-type IFormValues = {
-  networkId?: string;
-  input?: string;
-  deriveType?: IAccountDeriveTypes;
-};
+import {
+  ImportSingleChainBase,
+  fixInputImportSingleChain,
+} from './ImportSingleChainBase';
+
 function ImportAddress() {
-  const form = useForm<IFormValues>({
-    values: {
-      networkId: getNetworkIdsMap().btc,
+  const validationParams = useMemo<IValidateGeneralInputParams>(
+    () => ({
       input: '',
-      deriveType: undefined,
-    },
-  });
-
-  const { setValue, control, getValues } = form;
-  const [validateResult, setValidateResult] = useState<
-    IGeneralInputValidation | undefined
-  >();
-
-  const fixInput = useCallback((text: string) => trim(text), []);
-
-  const networkIdText = useFormWatch({ control, name: 'networkId' });
-  const inputText = useFormWatch({ control, name: 'input' });
-  const inputTextDebounced = useDebounce(inputText, 600);
-  const validateFn = useCallback(async () => {
-    setValue('deriveType', undefined);
-    if (inputTextDebounced && networkIdText) {
-      const input =
-        await backgroundApiProxy.servicePassword.encodeSensitiveText({
-          text: fixInput(inputTextDebounced),
-        });
-      const result =
-        await backgroundApiProxy.serviceAccount.validateGeneralInputOfImporting(
-          {
-            input,
-            networkId: networkIdText,
-            validateAddress: true,
-            validateXpub: true,
-          },
-        );
-      setValidateResult(result);
-      console.log('validateGeneralInputOfImporting result', result);
-    } else {
-      setValidateResult(undefined);
-    }
-  }, [inputTextDebounced, networkIdText, setValue, fixInput]);
-
-  useEffect(() => {
-    void validateFn();
-  }, [validateFn]);
-
-  useEffect(() => {
-    if (networkIdText) {
-      // setValue('input', '');
-    }
-  }, [networkIdText, setValue]);
+      validateAddress: true,
+      validateXpub: true,
+    }),
+    [],
+  );
 
   const actions = useAccountSelectorActions();
   const navigation = useAppNavigation();
 
   return (
-    <Page>
-      <Page.Header title="Add to Watchlist" />
-      <Page.Body px="$5">
-        <Form form={form}>
-          <Form.Field label="Network" name="networkId">
-            <ControlledNetworkSelectorTrigger />
-          </Form.Field>
-          <Form.Field label="Address" name="input">
-            <Input
-              placeholder="Address or domain name"
-              size="large"
-              addOns={[
-                {
-                  iconName: 'ScanOutline',
-                  onPress: () => console.log('scan'),
-                },
-              ]}
-              testID="address"
-            />
-          </Form.Field>
-          {validateResult?.deriveInfoItems ? (
-            <Form.Field label="Derivation Path" name="deriveType">
-              <DeriveTypeSelectorTriggerStaticInput
-                networkId={form.getValues().networkId || ''}
-                items={validateResult?.deriveInfoItems || []}
-              />
-            </Form.Field>
-          ) : null}
-        </Form>
+    <ImportSingleChainBase
+      title="Add to Watchlist"
+      inputLabel="Address"
+      inputPlaceholder="Address or domain name"
+      inputTestID="address"
+      invalidMessage="Invalid address, domain or xpub"
+      validationParams={validationParams}
+      onConfirm={async (form) => {
+        const values = form.getValues();
+        if (!values.input || !values.networkId) {
+          return;
+        }
+        const r = await backgroundApiProxy.serviceAccount.addWatchingAccount({
+          input: fixInputImportSingleChain(values.input),
+          networkId: values.networkId,
+          deriveType: values.deriveType,
+        });
+        console.log(r, values);
 
-        {!validateResult?.isValid && inputTextDebounced ? (
-          <SizableText color="$textCritical">
-            Invalid address, domain or xpub
-          </SizableText>
-        ) : null}
-
-        <Tutorials
-          list={[
-            {
-              title: 'What is a watch-only account?',
-              description:
-                "Watch-only account in OneKey allows monitoring of a specific address but cannot send or receive funds. It's useful for tracking transactions or monitoring holdings.",
-            },
-          ]}
-        />
-      </Page.Body>
-      <Page.Footer
-        confirmButtonProps={{
-          disabled: !validateResult?.isValid,
-        }}
-        onConfirm={async () => {
-          const values = form.getValues();
-          if (!values.input || !values.networkId) {
-            return;
-          }
-          const r = await backgroundApiProxy.serviceAccount.addWatchingAccount({
-            input: fixInput(values.input),
+        void actions.current.updateSelectedAccount({
+          num: 0,
+          builder: (v) => ({
+            ...v,
             networkId: values.networkId,
-            deriveType: values.deriveType,
-          });
-          console.log(r, values);
-
-          void actions.current.updateSelectedAccount({
-            num: 0,
-            builder: (v) => ({
-              ...v,
-              networkId: values.networkId,
-              focusedWallet: '$$others',
-              walletId: WALLET_TYPE_WATCHING,
-              othersWalletAccountId: r.accounts[0].id,
-              indexedAccountId: undefined,
-            }),
-          });
-          navigation.popStack();
-        }}
+            focusedWallet: '$$others',
+            walletId: WALLET_TYPE_WATCHING,
+            othersWalletAccountId: r.accounts[0].id,
+            indexedAccountId: undefined,
+          }),
+        });
+        navigation.popStack();
+      }}
+    >
+      <Tutorials
+        list={[
+          {
+            title: 'What is a watch-only account?',
+            description:
+              "Watch-only account in OneKey allows monitoring of a specific address but cannot send or receive funds. It's useful for tracking transactions or monitoring holdings.",
+          },
+        ]}
       />
-    </Page>
+    </ImportSingleChainBase>
   );
 }
 
