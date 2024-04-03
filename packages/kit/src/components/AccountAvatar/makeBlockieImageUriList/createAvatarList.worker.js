205686d1ddd5c347593f173364f37c22aa23e3d5
huhuanming <huanming@onekey.so>
2024-04-03 14:39:08 +0800

    Update createAvatarList.worker.js

--- packages/kit/src/components/AccountAvatar/makeBlockieImageUriList/createAvatarList.worker.js
+++ packages/kit/src/components/AccountAvatar/makeBlockieImageUriList/createAvatarList.worker.js
@@ -2,6 +2,10 @@ const createAvatar = require('ethereum-blockies-base64');
 
 const caches = {};
 
+// TODO: here is a better idea.
+//  Intercept network requests and generate avatar in web worker.
+//   In this way, you can use the browser's cache system to cache Avatar images, avoiding manual maintenance of memory cache.
+
 onmessage = (event) => {
   const id = event.data;
 
