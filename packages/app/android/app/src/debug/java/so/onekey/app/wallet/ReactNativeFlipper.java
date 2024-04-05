acadc1644db03f00204ea873a9ff73718d4a624e
ByteZhang <quincysx@onekey.so>
2024-04-05 22:28:49 +0800

    fix: android build error

--- packages/app/android/app/src/debug/java/so/onekey/app/wallet/ReactNativeFlipper.java
+++ packages/app/android/app/src/debug/java/so/onekey/app/wallet/ReactNativeFlipper.java
@@ -7,22 +7,22 @@
 package so.onekey.app.wallet;
 
 import android.content.Context;
-import com.facebook.flipper.android.AndroidFlipperClient;
-import com.facebook.flipper.android.utils.FlipperUtils;
-import com.facebook.flipper.core.FlipperClient;
-import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin;
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin;
-import com.facebook.flipper.plugins.fresco.FrescoFlipperPlugin;
-import com.facebook.flipper.plugins.inspector.DescriptorMapping;
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
-import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor;
-import com.facebook.flipper.plugins.network.NetworkFlipperPlugin;
-import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin;
+// import com.facebook.flipper.android.AndroidFlipperClient;
+// import com.facebook.flipper.android.utils.FlipperUtils;
+// import com.facebook.flipper.core.FlipperClient;
+// import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin;
+// import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin;
+// import com.facebook.flipper.plugins.fresco.FrescoFlipperPlugin;
+// import com.facebook.flipper.plugins.inspector.DescriptorMapping;
+// import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
+// import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor;
+// import com.facebook.flipper.plugins.network.NetworkFlipperPlugin;
+// import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin;
 import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.modules.network.NetworkingModule;
-import okhttp3.OkHttpClient;
-import tech.bam.rnperformance.flipper.RNPerfMonitorPlugin;
+// import com.facebook.react.bridge.ReactContext;
+// import com.facebook.react.modules.network.NetworkingModule;
+// import okhttp3.OkHttpClient;
+// import tech.bam.rnperformance.flipper.RNPerfMonitorPlugin;
 
 public class ReactNativeFlipper {
   public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
