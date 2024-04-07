8b8e43acb9889994fc54caac31aa8c39d91893ce
huhuanming <huanming@onekey.so>
2024-04-07 15:01:34 +0800

    feat: Improve local search in dapp (#4402)

---
+++ packages/shared/src/modules3rdParty/fuse/index.ts
@@ -0,0 +1,52 @@
+import { useMemo } from 'react';
+
+import Fuse from 'fuse.js';
+
+import type {
+  FuseIndex,
+  FuseResult,
+  FuseResultMatch,
+  IFuseOptions,
+} from 'fuse.js';
+
+const defaultFuseOptions: IFuseOptions<any> = {
+  location: 0,
+  isCaseSensitive: false,
+  includeMatches: true,
+  shouldSort: true,
+  findAllMatches: true,
+  minMatchCharLength: 2,
+  threshold: 0.5,
+  distance: 100,
+  useExtendedSearch: false,
+  ignoreLocation: false,
+  ignoreFieldNorm: true,
+  includeScore: true,
+};
+
+export type IFuseResult<T> = FuseResult<T>;
+export type IFuseResultMatch = FuseResultMatch;
+
+export function buildFuse<T>(
+  list: ReadonlyArray<T>,
+  options?: IFuseOptions<T>,
+  index?: FuseIndex<T>,
+) {
+  return new Fuse(
+    list,
+    {
+      ...defaultFuseOptions,
+      ...options,
+    },
+    index,
+  );
+}
+
+export function useFuse<T>(
+  list?: ReadonlyArray<T>,
+  options?: IFuseOptions<T>,
+  index?: FuseIndex<T>,
+) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(() => buildFuse(list || [], options, index), [list]);
+}
