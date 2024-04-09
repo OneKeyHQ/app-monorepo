893ada66f7cd67d7776c104e5c60c7e71c4688ef
huhuanming <huanming@onekey.so>
2024-04-09 21:09:04 +0800

    Update handle.desktop.ts

--- packages/shared/src/appUpdate/handle.desktop.ts
+++ packages/shared/src/appUpdate/handle.desktop.ts
@@ -9,6 +9,20 @@ export const handleReleaseInfo: IHandleReleaseInfo = (releaseInfo) => {
     releaseInfo.desktop,
     releaseInfo.changelog,
   ) as unknown as IAppUpdateInfo;
-  result.storeUrl = releaseInfo.desktop.mas.url;
+  if (platformEnv.isDesktopLinux) {
+    result.downloadUrl = releaseInfo.desktop.linux;
+  } else if (platformEnv.isDesktopLinuxSnap) {
+    result.storeUrl = releaseInfo.desktop.snapStore.url;
+  } else if (platformEnv.isDesktopMac) {
+    result.downloadUrl = releaseInfo.desktop.macX64;
+  } else if (platformEnv.isDesktopMacArm64) {
+    result.downloadUrl = releaseInfo.desktop.macARM;
+  } else if (platformEnv.isMas) {
+    result.storeUrl = releaseInfo.desktop.mas.url;
+  } else if (platformEnv.isDesktopWin) {
+    result.downloadUrl = releaseInfo.desktop.win;
+  } else if (platformEnv.isDesktopWinMsStore) {
+    result.storeUrl = releaseInfo.desktop.msStore.url;
+  }
   return result;
 };
