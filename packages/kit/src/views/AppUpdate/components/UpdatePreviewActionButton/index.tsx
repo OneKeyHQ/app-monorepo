7dea480c655463bc0534aefc27824e5e55dadc6b
huhuanming <huanming@onekey.so>
2024-04-09 21:06:13 +0800

    Update index.tsx

--- packages/kit/src/views/AppUpdate/components/UpdatePreviewActionButton/index.tsx
+++ packages/kit/src/views/AppUpdate/components/UpdatePreviewActionButton/index.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import { Page } from '@onekeyhq/components';
 import { useAppUpdateInfo } from '@onekeyhq/kit/src/components/UpdateReminder/hooks';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { openUrlExternal } from '@onekeyhq/shared/src/utils/openUrlUtils';
 
 import type { IUpdatePreviewActionButton } from './type';
@@ -13,7 +14,17 @@ export const UpdatePreviewActionButton: IUpdatePreviewActionButton = () => {
       if (appUpdateInfo.data.storeUrl) {
         openUrlExternal(appUpdateInfo.data.storeUrl);
       } else if (appUpdateInfo.data.downloadUrl) {
-        // download package in App
+        if (platformEnv.isDesktop) {
+          window.desktopApi?.on?.('update/checking', () => {
+            console.log('update/checking');
+          });
+          window.desktopApi?.on?.('update/available', ({ version }) => {
+            console.log('update/available, version: ', version);
+            window.desktopApi.downloadUpdate();
+          });
+          window.desktopApi.checkForUpdates();
+        } else if (platformEnv.isNativeAndroid) {
+        }
       }
     }
   }, [appUpdateInfo.data]);
