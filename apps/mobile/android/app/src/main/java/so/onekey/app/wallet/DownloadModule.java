83057511b4038ac150ef7dc3294de3b1375f66fe
huhuanming <huanming@onekey.so>
2024-04-10 09:02:30 +0800

    fix: fix app

---
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/DownloadModule.java
@@ -0,0 +1,65 @@
+package so.onekey.app.wallet;
+
+import android.os.Environment;
+import android.database.Cursor;
+// import android.widget.Toast;
+import android.webkit.MimeTypeMap;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.app.DownloadManager;
+
+import androidx.core.content.FileProvider;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Promise;
+
+public class DownloadModule extends ReactContextBaseJavaModule {
+
+    private ReactApplicationContext rContext;
+
+    DownloadModule(ReactApplicationContext context) {
+        super(context);
+        rContext = context;
+    }
+
+    public String getName() {
+        return "DownloadManager";
+    }
+
+
+    @ReactMethod
+    public void installApk(final String url, final Promise promise) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            File file;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                file = new File(url.replace("file:///", "/"));
+                Uri apkUri = FileProvider.getUriForFile(rContext, BuildConfig.APPLICATION_ID + '.provider', file);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+            }else{
+                file = new File(url);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+            }
+            promise.resolve("success");
+            rContext.startActivity(intent);
+        } catch (Exception e) {
+            promise.reject("Error", e.getMessage());
+        }
+    }
+}
--- packages/kit/src/views/AppUpdate/components/UpdatePreviewActionButton/index.tsx
+++ packages/kit/src/views/AppUpdate/components/UpdatePreviewActionButton/index.tsx
@@ -1,9 +1,13 @@
 import { useCallback } from 'react';
 
+import { NativeModules } from 'react-native';
+
+import type { IPageFooterProps } from '@onekeyhq/components';
 import { Page } from '@onekeyhq/components';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
 import { useAppUpdateInfo } from '@onekeyhq/kit/src/components/UpdateReminder/hooks';
 import { EAppUpdateStatus } from '@onekeyhq/shared/src/appUpdate';
+import RNFS from '@onekeyhq/shared/src/modules3rdParty/react-native-fs/index.native';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { openUrlExternal } from '@onekeyhq/shared/src/utils/openUrlUtils';
 
@@ -11,28 +15,41 @@ import type { IUpdatePreviewActionButton } from './type';
 
 export const UpdatePreviewActionButton: IUpdatePreviewActionButton = () => {
   const appUpdateInfo = useAppUpdateInfo();
-  const handlePress = useCallback(() => {
-    if (appUpdateInfo.data) {
-      if (appUpdateInfo.data.storeUrl) {
-        openUrlExternal(appUpdateInfo.data.storeUrl);
-      } else if (appUpdateInfo.data.downloadUrl) {
-        if (platformEnv.isDesktop) {
-          void backgroundApiProxy.ServiceAppUpdate.startDownloading();
-          window.desktopApi?.on?.('update/checking', () => {
-            console.log('update/checking');
-          });
-          window.desktopApi?.on?.('update/available', async ({ version }) => {
-            console.log('update/available, version: ', version);
-            window.desktopApi.downloadUpdate();
+  const handlePress: IPageFooterProps['onConfirm'] = useCallback(
+    async (close: () => void) => {
+      if (appUpdateInfo.data) {
+        if (appUpdateInfo.data.storeUrl) {
+          openUrlExternal(appUpdateInfo.data.storeUrl);
+        } else if (appUpdateInfo.data.downloadUrl) {
+          if (platformEnv.isDesktop) {
+            void backgroundApiProxy.ServiceAppUpdate.startDownloading();
+            window.desktopApi?.on?.('update/checking', () => {
+              console.log('update/checking');
+            });
+            window.desktopApi?.on?.('update/available', async ({ version }) => {
+              console.log('update/available, version: ', version);
+              window.desktopApi.downloadUpdate();
+            });
+            window.desktopApi.checkForUpdates();
             await backgroundApiProxy.ServiceAppUpdate.startDownloading();
-          });
-          window.desktopApi.checkForUpdates();
-        } else if (platformEnv.isNativeAndroid) {
-          // TODO: in another pr.
+          } else if (platformEnv.isNativeAndroid) {
+            await RNFS.mkdir(`file://${RNFS.DocumentDirectoryPath}/apk`);
+            await backgroundApiProxy.ServiceAppUpdate.startDownloading();
+            await RNFS.downloadFile({
+              fromUrl: appUpdateInfo.data.downloadUrl,
+              toFile: `${RNFS.DocumentDirectoryPath}/apk/${
+                appUpdateInfo.data.latestVersion || ''
+              }.apk`,
+              progress: console.log,
+            });
+            await backgroundApiProxy.ServiceAppUpdate.readToInstall();
+          }
+          close();
         }
       }
-    }
-  }, [appUpdateInfo.data]);
+    },
+    [appUpdateInfo.data],
+  );
   return appUpdateInfo.data?.status === EAppUpdateStatus.downloading ? null : (
     <Page.Footer onConfirmText="Update Now" onConfirm={handlePress} />
   );
