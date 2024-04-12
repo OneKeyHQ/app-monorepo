041e009b83f1bf9adbf539ddc8d54bd0a68f255b
morizon <sidmorizon@outlook.com>
2024-04-12 14:27:02 +0800

    fix: enable editable for swap account selector (#4440)

--- packages/kit/src/components/AccountSelector/AccountSelectorTrigger/AccountSelectorTriggerSwap.tsx
+++ packages/kit/src/components/AccountSelector/AccountSelectorTrigger/AccountSelectorTriggerSwap.tsx
@@ -1,5 +1,5 @@
 import { AccountSelectorTriggerBase } from './AccountSelectorTriggerBase';
 
 export function AccountSelectorTriggerSwap({ num }: { num: number }) {
-  return <AccountSelectorTriggerBase num={num} linkNetwork={false} />;
+  return <AccountSelectorTriggerBase num={num} linkNetwork={false} editable />;
 }
