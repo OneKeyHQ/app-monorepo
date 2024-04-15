d950a567f17aabe1019e6f9a32b1fc2c63c9772c
huhuanming <huanming@onekey.so>
2024-04-15 10:21:20 +0800

    feat: render modal content of WalletConnect to the topmost layer via FullWindowOverlay (#4445)

--- packages/kit/src/provider/Container/AppStateLockContainer/components/AppStateContainer/index.android.tsx
+++ packages/kit/src/provider/Container/AppStateLockContainer/components/AppStateContainer/index.android.tsx
@@ -1,8 +1,15 @@
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, useEffect } from 'react';
 
 import { Portal } from '@onekeyhq/components';
+import hooks from '@onekeyhq/kit/src/components/WalletConnect/WalletConnectModal';
 
+const { useModal } = hooks;
 export function AppStateContainer({ children }: PropsWithChildren) {
+  // The WalletConnect Modal is above the Android lock screen, it needs to be closed when the lock screen appears
+  const { closeModal } = useModal();
+  useEffect(() => {
+    closeModal();
+  }, [closeModal]);
   return (
     <>
       {children}
