6b5f523f9a48c01a6d4d459d8a2fd15b3af6c756
王山栋 <jelly@onekey.so>
2024-04-17 17:44:12 +0800

    fix: swap input amount decimal alignment
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/kit/src/views/Swap/utils/utils.ts
+++ packages/kit/src/views/Swap/utils/utils.ts
@@ -4,12 +4,28 @@ import { ESwapTxHistoryStatus } from '@onekeyhq/shared/types/swap/types';
 
 import type { ColorValue } from 'react-native';
 
-export function validateAmountInput(text: string) {
-  const regex = /^$|^0(\.\d{0,6})?$|^[1-9]\d*(\.\d{0,6})?$|^[1-9]\d*\.$|^0\.$/;
+export function validateAmountInput(text: string, decimal?: number) {
+  const regex = new RegExp(
+    `^$|^0(\\.\\d{0,${decimal ?? 6}})?$|^[1-9]\\d*(\\.\\d{0,${
+      decimal ?? 6
+    }})?$|^[1-9]\\d*\\.$|^0\\.$`,
+  );
   if (!regex.test(text)) {
     return false;
   }
   return true;
+}
+
+export function truncateDecimalPlaces(str?: string, decimal?: number) {
+  if (!str || Number.isNaN(str) || !decimal) {
+    return null;
+  }
+  const parts = str.split('.');
+  if (parts.length === 2 && parts[1].length > decimal) {
+    parts[1] = parts[1].substring(0, decimal);
+    return parts.join('.');
+  }
+  return str;
 }
 
 export function moveNetworkToFirst(arr: ISwapNetwork[], networkId: string) {
