ad5d6769aee55d7c8944d60a5f9b8999ea2849e0
huhuanming <huanming@onekey.so>
2024-04-18 23:06:26 +0800

    feat: remove auto update on Google Play channel

---
+++ apps/mobile/android/app/src/google/java/so/onekey/app/wallet/AutoUpdateModulePackage.java
@@ -0,0 +1,30 @@
+package so.onekey.app.wallet;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AutoUpdateModulePackage implements ReactPackage {
+
+    AutoUpdateModulePackage(ReactNativeHost mReactNativeHost) {
+        super();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        return modules;
+    }
+}
