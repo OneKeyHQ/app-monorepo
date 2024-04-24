3913e49c1b91ca3173c7fd20cd251503c1f4e956
huhuanming <huanming@onekey.so>
2024-04-24 21:11:20 +0800

    Update showMessage.native.tsx

--- packages/components/src/actions/Toast/showMessage.native.tsx
+++ packages/components/src/actions/Toast/showMessage.native.tsx
@@ -8,16 +8,10 @@ export function showMessage({ title, duration }: IToastMessageOptions) {
   toast('', {
     duration,
     customToast: ({ width }) => (
-      <XStack
-        alignItems="center"
-        justifyContent="center"
-        width={width}
-        px="$4"
-        py="$3"
-        borderRadius="$2"
-        bg="$bgApp"
-      >
-        {title}
+      <XStack alignItems="center" justifyContent="center" width={width}>
+        <View bg="$bgApp" px="$4" py="$3" borderRadius="$2" width="60%">
+          {title}
+        </View>
       </XStack>
     ),
   });
