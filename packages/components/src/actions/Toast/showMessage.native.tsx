662d154350923e3571ee008fdee7c64923f0e683
huhuanming <huanming@onekey.so>
2024-04-24 21:51:13 +0800

    Update showMessage.native.tsx

--- packages/components/src/actions/Toast/showMessage.native.tsx
+++ packages/components/src/actions/Toast/showMessage.native.tsx
@@ -7,9 +7,24 @@ import type { IToastMessageOptions } from './type';
 export function showMessage({ title, duration }: IToastMessageOptions) {
   toast('', {
     duration,
+    disableShadow: true,
     customToast: ({ width }) => (
       <XStack alignItems="center" justifyContent="center" width={width}>
-        <View bg="$bgApp" px="$4" py="$3" borderRadius="$2" width="60%">
+        <View
+          bg="$bgApp"
+          px="$4"
+          py="$3"
+          borderRadius="$2"
+          width="60%"
+          shadowColor="#181821"
+          shadowOffset={{
+            width: 0,
+            height: 3,
+          }}
+          shadowOpacity={0.15}
+          shadowRadius={4.65}
+          elevationAndroid={7}
+        >
           {title}
         </View>
       </XStack>
