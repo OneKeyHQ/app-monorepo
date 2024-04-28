7050d4cb982a36619a66822e9b36b748cfdf06e8
huhuanming <huanming@onekey.so>
2024-04-28 14:48:09 +0800

    feat: add release-desktop-snap

---
+++ apps/desktop/electron-builder-snap.config.js
@@ -0,0 +1,115 @@
+/* eslint-disable no-template-curly-in-string */
+require('../../development/env');
+
+module.exports = {
+  'extraMetadata': {
+    'main': 'dist/app.js',
+    'version': process.env.VERSION,
+  },
+  'appId': 'so.onekey.wallet.desktop',
+  'productName': 'OneKey',
+  'copyright': 'Copyright © ${author}',
+  'asar': true,
+  'buildVersion': process.env.BUILD_NUMBER,
+  'directories': {
+    'output': 'build-electron',
+  },
+  'files': [
+    'build/**/*',
+    '!build/static/bin/**/*',
+    'dist/**/*.js',
+    '!dist/__**',
+    'package.json',
+  ],
+  'protocols': {
+    'name': 'electron-deep-linking',
+    'schemes': ['onekey-wallet', 'wc', 'ethereum'],
+  },
+  'extraResources': [
+    {
+      'from': 'build/static/images/icons/512x512.png',
+      'to': 'static/images/icons/512x512.png',
+    },
+    {
+      'from': 'build/static/preload.js',
+      'to': 'static/preload.js',
+    },
+  ],
+  'publish': {
+    'provider': 'github',
+    'repo': 'app-monorepo',
+    'owner': 'OneKeyHQ',
+  },
+  'dmg': {
+    'sign': false,
+    'contents': [
+      {
+        'x': 410,
+        'y': 175,
+        'type': 'link',
+        'path': '/Applications',
+      },
+      {
+        'x': 130,
+        'y': 175,
+        'type': 'file',
+      },
+    ],
+    'background': 'build/static/images/icons/background.png',
+  },
+  'nsis': {
+    'oneClick': false,
+    'installerSidebar': 'build/static/images/icons/installerSidebar.bmp',
+  },
+  'mac': {
+    'extraResources': [
+      {
+        'from': 'build/static/bin/bridge/mac-${arch}',
+        'to': 'bin/bridge',
+      },
+    ],
+    'icon': 'build/static/images/icons/512x512.png',
+    'artifactName': 'OneKey-Wallet-${version}-mac-${arch}.${ext}',
+    'hardenedRuntime': true,
+    'gatekeeperAssess': false,
+    'darkModeSupport': false,
+    'category': 'productivity',
+    'target': [
+      { target: 'dmg', arch: ['x64', 'arm64'] },
+      { target: 'zip', arch: ['x64', 'arm64'] },
+    ],
+    'entitlements': 'entitlements.mac.plist',
+    'extendInfo': {
+      'NSCameraUsageDescription': 'Please allow OneKey to use your camera',
+    },
+  },
+  'win': {
+    'extraResources': [
+      {
+        'from': 'build/static/bin/bridge/win-${arch}',
+        'to': 'bin/bridge',
+      },
+    ],
+    'icon': 'build/static/images/icons/512x512.png',
+    'artifactName': 'OneKey-Wallet-${version}-win-${arch}.${ext}',
+    'verifyUpdateCodeSignature': false,
+    'target': ['nsis'],
+  },
+  'snap': {
+    'grade': 'stable',
+  },
+  'linux': {
+    'extraResources': [
+      {
+        'from': 'build/static/bin/bridge/linux-${arch}',
+        'to': 'bin/bridge',
+      },
+    ],
+    'icon': 'build/static/images/icons/512x512.png',
+    'artifactName': 'OneKey-Wallet-${version}-linux-${arch}.${ext}',
+    'executableName': 'onekey-wallet',
+    'category': 'Utility',
+    'target': ['AppImage', 'snap'],
+  },
+  'afterSign': 'scripts/notarize.js',
+};
