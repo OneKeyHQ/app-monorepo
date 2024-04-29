c89694afe6a0891f9676044bc849758c4281204d
huhuanming <huanming@onekey.so>
2024-04-29 21:55:53 +0800

    Update index.native.ts

--- packages/core/src/chains/ada/sdkAda/sdk/index.native.ts
+++ packages/core/src/chains/ada/sdkAda/sdk/index.native.ts
@@ -1,10 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import webembedApiProxy from '@onekeyhq/kit-bg/src/webembeds/instance/webembedApiProxy';
 
-import type { IAdaSdk, IEnsureSDKReady, IGetCardanoApi } from './types';
+import type { IAdaSdk, IAdaSdkApi, IEnsureSDKReady, IGetCardanoApi } from './types';
+
+const module = new Proxy({}, {
+  get: function (target, method: string) {
+    return function(...params: any[]) {
+      return webembedApiProxy.callRemoteApi({
+        module: 'chainAdaLegacy',
+        method,
+        params,
+      });
+    }
+  }
+}) as IAdaSdkApi;
 
 const getCardanoApi: IGetCardanoApi = async () =>
-  Promise.resolve(webembedApiProxy.chainAdaLegacy);
+  Promise.resolve(module);
 
 // auto check webembedApi ready by calling each method
 const ensureSDKReady: IEnsureSDKReady = async () => Promise.resolve(true);
