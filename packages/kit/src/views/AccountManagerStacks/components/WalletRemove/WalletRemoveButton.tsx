847140ad71a9862e25055b27c3be643ca5bbba07
huhuanming <huanming@onekey.so>
2024-04-30 09:39:22 +0800

    fix: fix ui wallet issues(OK-27335 OK-27336) (#4515)
    * fix: device Wallet don't show checkbox
    
    * Update WalletRemoveButton.tsx
    
    * Update ChunkedItemsView.tsx

--- packages/kit/src/views/AccountManagerStacks/components/WalletRemove/WalletRemoveButton.tsx
+++ packages/kit/src/views/AccountManagerStacks/components/WalletRemove/WalletRemoveButton.tsx
@@ -1,11 +1,23 @@
 import { IconButton } from '@onekeyhq/components';
 import { useAccountSelectorContextData } from '@onekeyhq/kit/src/states/jotai/contexts/accountSelector';
 import type { IDBWallet } from '@onekeyhq/kit-bg/src/dbs/local/types';
+import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 
 import { showWalletRemoveDialog } from './WalletRemoveDialog';
 
 function getTitleAndDescription({ wallet }: { wallet?: IDBWallet }) {
   if (wallet?.type === 'hw') {
+    if (
+      accountUtils.isHwHiddenWallet({
+        wallet,
+      })
+    ) {
+      return {
+        title: 'Remove Device',
+        description:
+          'This will permanently delete your data. Ensure you have saved the passphrase of this hidden wallet',
+      };
+    }
     return {
       title: 'Remove Device',
       description:
@@ -33,6 +45,7 @@ export function WalletRemoveButton({ wallet }: { wallet?: IDBWallet }) {
           config,
           title,
           description,
+          showCheckBox: wallet?.type !== 'hw',
           defaultChecked: false,
           wallet,
         });
