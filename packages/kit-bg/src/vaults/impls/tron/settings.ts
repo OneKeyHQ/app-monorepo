982245a6398ecae4630ebb4bd995104121ea5517
weatherstar <weather@onekey.so>
2024-05-01 23:04:13 +0800

    chore: init tron

---
+++ packages/kit-bg/src/vaults/impls/tron/settings.ts
@@ -0,0 +1,47 @@
+import {
+  COINTYPE_TRON,
+  IMPL_TRON,
+  INDEX_PLACEHOLDER,
+} from '@onekeyhq/shared/src/engine/engineConsts';
+
+import { EDBAccountType } from '../../../dbs/local/consts';
+
+import type { IAccountDeriveInfoMapBase, IVaultSettings } from '../../types';
+
+const accountDeriveInfo: IAccountDeriveInfoMapBase = {
+  default: {
+    namePrefix: 'TRON',
+    labelKey: 'form__bip44_standard',
+    template: `m/44'/${COINTYPE_TRON}'/0'/0/${INDEX_PLACEHOLDER}`,
+    coinType: COINTYPE_TRON,
+  },
+};
+
+const settings: IVaultSettings = {
+  impl: IMPL_TRON,
+  coinTypeDefault: COINTYPE_TRON,
+  accountType: EDBAccountType.SIMPLE,
+
+  importedAccountEnabled: true,
+  hardwareAccountEnabled: true,
+  externalAccountEnabled: true,
+  watchingAccountEnabled: true,
+
+  isUtxo: false,
+  isSingleToken: false,
+  NFTEnabled: false,
+  nonceRequired: false,
+  feeUTXORequired: false,
+  editFeeEnabled: false,
+  replaceTxEnabled: false,
+
+  accountDeriveInfo,
+  networkInfo: {
+    default: {
+      curve: 'secp256k1',
+      addressPrefix: '',
+    },
+  },
+};
+
+export default Object.freeze(settings);
