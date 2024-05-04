fdc3c6255d3049d6ca59d862a6874b1cf2e069e7
Leon <lixiao.dev@gmail.com>
2024-05-04 12:10:47 +0800

    feat: Sign transaction on hardware

--- packages/kit-bg/src/vaults/impls/ripple/KeyringHardware.ts
+++ packages/kit-bg/src/vaults/impls/ripple/KeyringHardware.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { hashes } from 'xrpl';
+
+import type { IEncodedTxRipple } from '@onekeyhq/core/src/chains/ripple/types';
 import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
 import type {
   ICoreApiGetAddressItem,
   ISignedMessagePro,
   ISignedTxPro,
 } from '@onekeyhq/core/src/types';
+import { UnknownHardwareError } from '@onekeyhq/shared/src/errors';
+import { convertDeviceError } from '@onekeyhq/shared/src/errors/utils/deviceErrorUtils';
+import { checkIsDefined } from '@onekeyhq/shared/src/utils/assertUtils';
 
 import { KeyringHardwareBase } from '../../base/KeyringHardwareBase';
 
@@ -58,10 +64,47 @@ export class KeyringHardware extends KeyringHardwareBase {
     });
   }
 
-  override signTransaction(
+  override async signTransaction(
     params: ISignTransactionParams,
   ): Promise<ISignedTxPro> {
-    throw new Error('Method not implemented.');
+    const sdk = await this.getHardwareSDKInstance();
+    const encodedTx = params.unsignedTx.encodedTx as IEncodedTxRipple;
+    const deviceParams = checkIsDefined(params.deviceParams);
+    const { connectId, deviceId } = deviceParams.dbDevice;
+    const dbAccount = await this.vault.getAccount();
+    const signTransactionParams = {
+      path: dbAccount.path,
+      transaction: {
+        fee: encodedTx.Fee,
+        flags: encodedTx.Flags,
+        sequence: encodedTx.Sequence,
+        maxLedgerVersion: encodedTx.LastLedgerSequence,
+        payment: {
+          amount: +encodedTx.Amount,
+          destination: encodedTx.Destination,
+          destinationTag: encodedTx.DestinationTag ?? undefined,
+        },
+      },
+    };
+
+    const response = await sdk.xrpSignTransaction(connectId, deviceId, {
+      ...params.deviceParams?.deviceCommonParams,
+      ...(signTransactionParams as unknown as any),
+    });
+
+    if (response.success) {
+      const { serializedTx } = response.payload;
+      if (!serializedTx) {
+        throw new UnknownHardwareError();
+      }
+      return {
+        txid: hashes.hashSignedTx(serializedTx),
+        rawTx: serializedTx,
+        encodedTx,
+      };
+    }
+
+    throw convertDeviceError(response.payload);
   }
 
   override signMessage(params: ISignMessageParams): Promise<ISignedMessagePro> {
