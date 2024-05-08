ebc1120d54e72ec49a439c95c071dc0f1036a9c7
morizon <sidmorizon@outlook.com>
2024-05-08 20:53:27 +0800

    fix: lint

--- packages/kit/src/routes/Modal/router.tsx
+++ packages/kit/src/routes/Modal/router.tsx
@@ -17,10 +17,10 @@ import { LiteCardPages } from '../../views/LiteCard/router';
 import { OnboardingRouter } from '../../views/Onboarding/router';
 import { ModalReceiveStack } from '../../views/Receive/router';
 import { ScanQrCodeModalRouter } from '../../views/ScanQrCode/router';
-import { ModalWebViewStack } from '../../views/WebView/router';
 import { ModalSendStack } from '../../views/Send/router';
 import { ModalSwapStack } from '../../views/Swap/router';
 import { TestModalRouter } from '../../views/TestModal/router';
+import { ModalWebViewStack } from '../../views/WebView/router';
 
 import { ModalMainStack } from './Main';
 
