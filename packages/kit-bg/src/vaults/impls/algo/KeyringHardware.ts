e2887fc8eb3df1014bdd65ef2cf4790372e4d1d1
weatherstar <weather@onekey.so>
2024-05-08 22:52:58 +0800

    feat: init alog chain

---
+++ packages/kit-bg/src/vaults/impls/algo/KeyringHardware.ts
@@ -0,0 +1,86 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
+import type {
+  ICoreApiGetAddressItem,
+  ISignedMessagePro,
+  ISignedTxPro,
+} from '@onekeyhq/core/src/types';
+
+import { KeyringHardwareBase } from '../../base/KeyringHardwareBase';
+
+import type { IDBAccount } from '../../../dbs/local/types';
+import type {
+  IPrepareAccountsParams,
+  IPrepareHardwareAccountsParams,
+  ISignMessageParams,
+  ISignTransactionParams,
+} from '../../types';
+
+export class KeyringHardware extends KeyringHardwareBase {
+  override coreApi = coreChainApi.algo.hd;
+
+  override async prepareAccounts(
+    params: IPrepareHardwareAccountsParams,
+  ): Promise<IDBAccount[]> {
+    const chainId = await this.getNetworkChainId();
+
+    return this.basePrepareHdNormalAccounts(params, {
+      buildAddressesInfo: async ({ usedIndexes }) => {
+        const addresses = await this.baseGetDeviceAccountAddresses({
+          params,
+          usedIndexes,
+          sdkGetAddressFn: async ({
+            connectId,
+            deviceId,
+            pathPrefix,
+            pathSuffix,
+            coinName,
+            showOnOnekeyFn,
+          }) => {
+            const sdk = await this.getHardwareSDKInstance();
+
+            const response = await sdk.algoGetAddress(connectId, deviceId, {
+              ...params.deviceParams.deviceCommonParams,
+              bundle: usedIndexes.map((index, arrIndex) => ({
+                path: `${pathPrefix}/${pathSuffix.replace(
+                  '{index}',
+                  `${index}`,
+                )}`,
+                showOnOneKey: showOnOnekeyFn(arrIndex),
+              })),
+            });
+            return response;
+          },
+        });
+
+        console.log('algo-buildAddressesInfo', addresses);
+
+        const ret: ICoreApiGetAddressItem[] = [];
+        for (let i = 0; i < addresses.length; i += 1) {
+          const item = addresses[i];
+          const { path, address } = item;
+          const { normalizedAddress } = await this.vault.validateAddress(
+            address ?? '',
+          );
+          const addressInfo: ICoreApiGetAddressItem = {
+            address: normalizedAddress || address || '',
+            path,
+            publicKey: '',
+          };
+          ret.push(addressInfo);
+        }
+        return ret;
+      },
+    });
+  }
+
+  override signTransaction(
+    params: ISignTransactionParams,
+  ): Promise<ISignedTxPro> {
+    throw new Error('Method not implemented.');
+  }
+
+  override signMessage(params: ISignMessageParams): Promise<ISignedMessagePro> {
+    throw new Error('Method not implemented.');
+  }
+}
---
+++ packages/kit-bg/src/vaults/impls/algo/Vault.ts
@@ -0,0 +1,165 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  decodeSensitiveText,
+  encodeSensitiveText,
+} from '@onekeyhq/core/src/secret';
+import type {
+  IEncodedTx,
+  ISignedTxPro,
+  IUnsignedTxPro,
+} from '@onekeyhq/core/src/types';
+import type {
+  IAddressValidation,
+  IGeneralInputValidation,
+  INetworkAccountAddressDetail,
+  IPrivateKeyValidation,
+  IXprvtValidation,
+  IXpubValidation,
+} from '@onekeyhq/shared/types/address';
+import type { IDecodedTx } from '@onekeyhq/shared/types/tx';
+
+import { VaultBase } from '../../base/VaultBase';
+
+import { KeyringExternal } from './KeyringExternal';
+import { KeyringHardware } from './KeyringHardware';
+import { KeyringHd } from './KeyringHd';
+import { KeyringImported } from './KeyringImported';
+import { KeyringWatching } from './KeyringWatching';
+import sdkAlgo from './sdkAlgo';
+
+import type { IDBWalletType } from '../../../dbs/local/types';
+import type { KeyringBase } from '../../base/KeyringBase';
+import type {
+  IBroadcastTransactionParams,
+  IBuildAccountAddressDetailParams,
+  IBuildDecodedTxParams,
+  IBuildEncodedTxParams,
+  IBuildUnsignedTxParams,
+  IGetPrivateKeyFromImportedParams,
+  IGetPrivateKeyFromImportedResult,
+  IUpdateUnsignedTxParams,
+  IValidateGeneralInputParams,
+  IVaultSettings,
+} from '../../types';
+
+export default class Vault extends VaultBase {
+  override keyringMap: Record<IDBWalletType, typeof KeyringBase> = {
+    hd: KeyringHd,
+    hw: KeyringHardware,
+    imported: KeyringImported,
+    watching: KeyringWatching,
+    external: KeyringExternal,
+  };
+
+  override async buildAccountAddressDetail(
+    params: IBuildAccountAddressDetailParams,
+  ): Promise<INetworkAccountAddressDetail> {
+    const { account, networkId } = params;
+
+    const address = account.address || '';
+
+    const { normalizedAddress, displayAddress, isValid } =
+      await this.validateAddress(address);
+    return {
+      networkId,
+      normalizedAddress,
+      displayAddress,
+      address: displayAddress,
+      baseAddress: normalizedAddress,
+      isValid,
+      allowEmptyAddress: false,
+    };
+  }
+
+  override buildEncodedTx(params: IBuildEncodedTxParams): Promise<IEncodedTx> {
+    throw new Error('Method not implemented.');
+  }
+
+  override buildDecodedTx(params: IBuildDecodedTxParams): Promise<IDecodedTx> {
+    throw new Error('Method not implemented.');
+  }
+
+  override buildUnsignedTx(
+    params: IBuildUnsignedTxParams,
+  ): Promise<IUnsignedTxPro> {
+    throw new Error('Method not implemented.');
+  }
+
+  override updateUnsignedTx(
+    params: IUpdateUnsignedTxParams,
+  ): Promise<IUnsignedTxPro> {
+    throw new Error('Method not implemented.');
+  }
+
+  override broadcastTransaction(
+    params: IBroadcastTransactionParams,
+  ): Promise<ISignedTxPro> {
+    throw new Error('Method not implemented.');
+  }
+
+  override validateAddress(address: string): Promise<IAddressValidation> {
+    if (sdkAlgo.isValidAddress(address)) {
+      return Promise.resolve({
+        isValid: true,
+        displayAddress: address,
+        normalizedAddress: address,
+      });
+    }
+    return Promise.resolve({
+      isValid: false,
+      displayAddress: '',
+      normalizedAddress: '',
+    });
+  }
+
+  override validateXpub(xpub: string): Promise<IXpubValidation> {
+    return Promise.resolve({
+      isValid: false,
+    });
+  }
+
+  override getPrivateKeyFromImported(
+    params: IGetPrivateKeyFromImportedParams,
+  ): Promise<IGetPrivateKeyFromImportedResult> {
+    const input = decodeSensitiveText({ encodedText: params.input });
+    let privateKey = Buffer.from(sdkAlgo.seedFromMnemonic(input)).toString(
+      'hex',
+    );
+    privateKey = encodeSensitiveText({ text: privateKey });
+    return Promise.resolve({
+      privateKey,
+    });
+  }
+
+  override validateXprvt(xprvt: string): Promise<IXprvtValidation> {
+    return Promise.resolve({
+      isValid: false,
+    });
+  }
+
+  override validatePrivateKey(
+    privateKey: string,
+  ): Promise<IPrivateKeyValidation> {
+    try {
+      const seed = sdkAlgo.seedFromMnemonic(privateKey);
+      if (seed) {
+        return Promise.resolve({
+          isValid: true,
+        });
+      }
+    } catch {
+      // pass
+    }
+
+    return Promise.resolve({
+      isValid: false,
+    });
+  }
+
+  override async validateGeneralInput(
+    params: IValidateGeneralInputParams,
+  ): Promise<IGeneralInputValidation> {
+    const { result } = await this.baseValidateGeneralInput(params);
+    return result;
+  }
+}
