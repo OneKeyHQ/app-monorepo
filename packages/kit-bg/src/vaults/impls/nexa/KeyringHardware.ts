02bb0d83712cef18dc9675eb6bb033e11662cc2d
Leon <lixiao.dev@gmail.com>
2024-05-11 00:02:34 +0800

    feat: Nexa hardware account

--- packages/kit-bg/src/vaults/impls/nexa/KeyringHardware.ts
+++ packages/kit-bg/src/vaults/impls/nexa/KeyringHardware.ts
@@ -1,23 +1,74 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { getNexaPrefix } from '@onekeyhq/core/src/chains/nexa/sdkNexa';
 import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
 import type { ISignedMessagePro, ISignedTxPro } from '@onekeyhq/core/src/types';
+import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 
 import { KeyringHardwareBase } from '../../base/KeyringHardwareBase';
 
 import type { IDBAccount } from '../../../dbs/local/types';
 import type {
-  IPrepareAccountsParams,
+  IPrepareHardwareAccountsParams,
   ISignMessageParams,
   ISignTransactionParams,
 } from '../../types';
+
+const SIGN_TYPE = 'Schnorr';
 
 export class KeyringHardware extends KeyringHardwareBase {
   override coreApi = coreChainApi.nexa.hd;
 
   override prepareAccounts(
-    params: IPrepareAccountsParams,
+    params: IPrepareHardwareAccountsParams,
   ): Promise<IDBAccount[]> {
-    throw new Error('Method not implemented.');
+    return this.basePrepareHdUtxoAccounts(params, {
+      checkIsAccountUsed: () => Promise.resolve({ isUsed: true }),
+      buildAddressesInfo: async ({ usedIndexes }) => {
+        const chainId = await this.getNetworkChainId();
+        const addressesInfo = await this.baseGetDeviceAccountAddresses({
+          params,
+          usedIndexes,
+          sdkGetAddressFn: async ({
+            connectId,
+            deviceId,
+            pathPrefix,
+            showOnOnekeyFn,
+          }) => {
+            const paths = usedIndexes.map(
+              (index) => `${pathPrefix}/${index}'/0/0`,
+            );
+            const bundle = paths.map((path, index) => ({
+              path,
+              showOnOneKey: showOnOnekeyFn(index),
+              prefix: getNexaPrefix(chainId),
+              scheme: SIGN_TYPE,
+            }));
+            const sdk = await this.getHardwareSDKInstance();
+            const response = await sdk.nexaGetAddress(connectId, deviceId, {
+              ...params.deviceParams.deviceCommonParams,
+              bundle,
+            });
+            return response;
+          },
+        });
+
+        const ret = [];
+        const firstAddressRelPath = '0/0';
+        for (const addressInfo of addressesInfo) {
+          const { address, path, pub } = addressInfo;
+          const formattedPath = accountUtils.formatUtxoPath(path);
+          ret.push({
+            address: pub,
+            publicKey: pub,
+            path: formattedPath,
+            relPath: firstAddressRelPath,
+            xpub: '',
+            addresses: { [this.networkId]: address },
+          });
+        }
+        return ret;
+      },
+    });
   }
 
   override signTransaction(
