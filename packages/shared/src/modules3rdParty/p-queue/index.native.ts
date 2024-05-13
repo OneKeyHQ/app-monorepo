ef5d6421cb19efb41cadadb4b769344fa6cc9728
huhuanming <huanming@onekey.so>
2024-05-13 10:01:49 +0800

    remove p-queue

--- packages/shared/src/modules3rdParty/p-queue/index.native.ts
+++
@@ -1,4 +0,0 @@
-import PQueue from 'p-queue/dist';
-
-export default PQueue;
-export * from 'p-queue/dist';
--- packages/shared/src/modules3rdParty/p-queue/index.ts
+++
@@ -1,4 +0,0 @@
-import PQueue from 'p-queue';
-
-export default PQueue;
-export * from 'p-queue';
