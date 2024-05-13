322598f435eed87c00341cbf72e40dbf8479e734
weatherstar <weather@onekey.so>
2024-05-13 18:34:13 +0800

    feat: decode tx

--- packages/kit-bg/src/vaults/impls/cfx/KeyringHardware.ts
+++ packages/kit-bg/src/vaults/impls/cfx/KeyringHardware.ts
@@ -1,12 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { web3Errors } from '@onekeyfe/cross-inpage-provider-errors';
+import BigNumber from 'bignumber.js';
+import { TypedDataUtils } from 'eth-sig-util';
+import { omitBy } from 'lodash';
+
+import type { IEncodedTxCfx } from '@onekeyhq/core/src/chains/cfx/types';
 import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
 import type {
   ICoreApiGetAddressItem,
   ISignedMessagePro,
   ISignedTxPro,
+  IUnsignedMessageCfx,
 } from '@onekeyhq/core/src/types';
+import {
+  convertDeviceError,
+  convertDeviceResponse,
+} from '@onekeyhq/shared/src/errors/utils/deviceErrorUtils';
+import { checkIsDefined } from '@onekeyhq/shared/src/utils/assertUtils';
+import hexUtils from '@onekeyhq/shared/src/utils/hexUtils';
+import { toBigIntHex } from '@onekeyhq/shared/src/utils/numberUtils';
+import type { IDeviceSharedCallParams } from '@onekeyhq/shared/types/device';
+import { EMessageTypesEth } from '@onekeyhq/shared/types/message';
 
 import { KeyringHardwareBase } from '../../base/KeyringHardwareBase';
+
+import { conflux as sdkCfx } from './sdkCfx';
 
 import type { IDBAccount } from '../../../dbs/local/types';
 import type {
@@ -14,6 +32,8 @@ import type {
   ISignMessageParams,
   ISignTransactionParams,
 } from '../../types';
+
+const { address: confluxAddress, Transaction } = sdkCfx;
 
 export class KeyringHardware extends KeyringHardwareBase {
   override coreApi = coreChainApi.cfx.hd;
@@ -72,13 +92,165 @@ export class KeyringHardware extends KeyringHardwareBase {
     });
   }
 
-  override signTransaction(
+  override async signTransaction(
     params: ISignTransactionParams,
   ): Promise<ISignedTxPro> {
-    throw new Error('Method not implemented.');
+    const { unsignedTx, deviceParams } = params;
+    const encodedTx = unsignedTx.encodedTx as IEncodedTxCfx;
+    const sdk = await this.getHardwareSDKInstance();
+    const path = await this.vault.getAccountPath();
+    const { deviceCommonParams, dbDevice } = checkIsDefined(deviceParams);
+    const { connectId, deviceId } = dbDevice;
+
+    const transaction = {
+      ...encodedTx,
+      epochHeight: toBigIntHex(new BigNumber(encodedTx.epochHeight ?? 0)),
+      to: encodedTx.to
+        ? `0x${confluxAddress
+            .decodeCfxAddress(encodedTx.to)
+            .hexAddress.toString('hex')}`
+        : '',
+      storageLimit: toBigIntHex(new BigNumber(encodedTx.storageLimit ?? 0)),
+      nonce: toBigIntHex(new BigNumber(encodedTx.nonce ?? 0)),
+      gasPrice: toBigIntHex(new BigNumber(encodedTx.gasPrice ?? 0)),
+      data: encodedTx.data ?? '0x',
+      value: toBigIntHex(new BigNumber(encodedTx.value ?? 0)),
+    };
+
+    const result = await convertDeviceResponse(async () =>
+      sdk.confluxSignTransaction(connectId, deviceId, {
+        path,
+        // @ts-ignore
+        transaction,
+        ...deviceCommonParams,
+      }),
+    );
+
+    const { r, s, v } = result;
+
+    const signedTransaction = new Transaction({
+      ...omitBy(transaction, (t) => !t),
+      r,
+      s,
+      v: new BigNumber(v).toNumber(),
+    });
+
+    return Promise.resolve({
+      txid: signedTransaction.hash,
+      rawTx: signedTransaction.serialize(),
+      encodedTx: unsignedTx.encodedTx,
+    });
   }
 
   override signMessage(params: ISignMessageParams): Promise<ISignedMessagePro> {
-    throw new Error('Method not implemented.');
+    const { messages, deviceParams } = params;
+    checkIsDefined(deviceParams);
+    return Promise.all(
+      messages.map(async (message) =>
+        this.handleSignMessage({
+          message: message as IUnsignedMessageCfx,
+          deviceParams: deviceParams as IDeviceSharedCallParams,
+        }),
+      ),
+    );
+  }
+
+  async handleSignMessage(params: {
+    message: IUnsignedMessageCfx;
+    deviceParams: IDeviceSharedCallParams;
+  }): Promise<string> {
+    const { message, deviceParams } = params;
+    const { dbDevice, deviceCommonParams } = deviceParams;
+    const { connectId, deviceId } = dbDevice;
+
+    const sdk = await this.getHardwareSDKInstance();
+    const path = await this.vault.getAccountPath();
+
+    if (message.type === EMessageTypesEth.TYPED_DATA_V1) {
+      throw web3Errors.provider.unsupportedMethod(
+        `Sign message method=${message.type} not supported for this device`,
+      );
+    }
+
+    if (
+      message.type === EMessageTypesEth.ETH_SIGN ||
+      message.type === EMessageTypesEth.PERSONAL_SIGN
+    ) {
+      let messageBuffer: Buffer;
+      try {
+        if (!hexUtils.isHexString(message.message))
+          throw new Error('not hex string');
+
+        messageBuffer = Buffer.from(
+          hexUtils.stripHexPrefix(message.message),
+          'hex',
+        );
+      } catch (error) {
+        messageBuffer = Buffer.from('');
+      }
+
+      let messageHex = message.message;
+      if (messageBuffer.length === 0) {
+        messageHex = Buffer.from(message.message, 'utf-8').toString('hex');
+      }
+
+      const response = await sdk.confluxSignMessage(connectId, deviceId, {
+        path,
+        messageHex,
+        ...deviceCommonParams,
+      });
+
+      if (!response.success) {
+        throw convertDeviceError(response.payload);
+      }
+
+      return hexUtils.addHexPrefix(response.payload.signature || '');
+    }
+
+    if (
+      message.type === EMessageTypesEth.TYPED_DATA_V3 ||
+      message.type === EMessageTypesEth.TYPED_DATA_V4
+    ) {
+      const useV4 = message.type === EMessageTypesEth.TYPED_DATA_V4;
+      const data = JSON.parse(message.message);
+      const typedData = TypedDataUtils.sanitizeData(data);
+
+      const domainType =
+        typedData?.types?.CIP23Domain?.length > 0
+          ? 'CIP23Domain'
+          : 'EIP712Domain';
+
+      const domainHash = TypedDataUtils.hashStruct(
+        domainType,
+        typedData.domain,
+        typedData.types,
+        useV4,
+      ).toString('hex');
+      const messageHash = TypedDataUtils.hashStruct(
+        // @ts-expect-error
+        typedData.primaryType,
+        typedData.message,
+        typedData.types,
+        useV4,
+      ).toString('hex');
+
+      const response = await sdk.confluxSignMessageCIP23(connectId, deviceId, {
+        path,
+        domainHash,
+        messageHash,
+        ...deviceCommonParams,
+      });
+
+      if (!response.success) {
+        throw convertDeviceError(response.payload);
+      }
+
+      return hexUtils.addHexPrefix(response?.payload?.signature || '');
+    }
+
+    throw web3Errors.rpc.methodNotFound(
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      `Sign message method=${message.type} not found`,
+    );
   }
 }
---
+++ packages/kit-bg/src/vaults/impls/cfx/sdkCfx/ClientCfx.ts
@@ -0,0 +1,106 @@
+import type { IBackgroundApi } from '@onekeyhq/kit-bg/src/apis/IBackgroundApi';
+
+import type { ISdkCfxContract } from '../types';
+
+class ClientCfx {
+  private backgroundApi: IBackgroundApi;
+
+  private networkId: string;
+
+  constructor({
+    backgroundApi,
+    networkId,
+  }: {
+    backgroundApi: any;
+    networkId: string;
+  }) {
+    this.networkId = networkId;
+    this.backgroundApi = backgroundApi;
+  }
+
+  async getStatus() {
+    const [result] =
+      await this.backgroundApi.serviceAccountProfile.sendProxyRequest<{
+        epochNumber: number;
+        chainId: number;
+      }>({
+        networkId: this.networkId,
+        body: [
+          {
+            route: 'conflux',
+            params: {
+              method: 'getStatus',
+              params: {},
+            },
+          },
+        ],
+      });
+
+    return result;
+  }
+
+  async estimateGasAndCollateral(params: {
+    from: string;
+    to: string;
+    value: string;
+    data: string;
+  }) {
+    const [result] =
+      await this.backgroundApi.serviceAccountProfile.sendProxyRequest<{
+        storageCollateralized: number;
+      }>({
+        networkId: this.networkId,
+        body: [
+          {
+            route: 'conflux',
+            params: {
+              method: 'estimateGasAndCollateral',
+              params,
+            },
+          },
+        ],
+      });
+
+    return result;
+  }
+
+  async getCode(address: string) {
+    const [result] =
+      await this.backgroundApi.serviceAccountProfile.sendProxyRequest<string>({
+        networkId: this.networkId,
+        body: [
+          {
+            route: 'conflux',
+            params: {
+              method: 'getCode',
+              params: [address],
+            },
+          },
+        ],
+      });
+
+    return result;
+  }
+
+  async CRC20(address: string) {
+    const [result] =
+      await this.backgroundApi.serviceAccountProfile.sendProxyRequest<ISdkCfxContract>(
+        {
+          networkId: this.networkId,
+          body: [
+            {
+              route: 'conflux',
+              params: {
+                method: 'CRC20',
+                params: [address],
+              },
+            },
+          ],
+        },
+      );
+
+    return result;
+  }
+}
+
+export default ClientCfx;
