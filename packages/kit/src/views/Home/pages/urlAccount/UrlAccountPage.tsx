725d211ec5040097110005a810b2e0a1788660b5
morizon <sidmorizon@outlook.com>
2024-05-13 14:12:57 +0800

    fix: desktop init route (#4575)
    * fix: desktop init route
    
    * fix: lint

--- packages/kit/src/views/Home/pages/urlAccount/UrlAccountPage.tsx
+++ packages/kit/src/views/Home/pages/urlAccount/UrlAccountPage.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { cloneDeep } from 'lodash';
+
 import {
   Button,
   Page,
@@ -14,6 +16,7 @@ import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { useAppRoute } from '@onekeyhq/kit/src/hooks/useAppRoute';
 import { useAccountSelectorActions } from '@onekeyhq/kit/src/states/jotai/contexts/accountSelector';
 import { WALLET_TYPE_WATCHING } from '@onekeyhq/shared/src/consts/dbConsts';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
 import type { IServerNetwork } from '@onekeyhq/shared/types';
 import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
@@ -47,6 +50,20 @@ function UrlAccountAutoCreate({ redirectMode }: { redirectMode?: boolean }) {
   >();
 
   useEffect(() => {
+    if (
+      !platformEnv.isDev &&
+      platformEnv.isDesktop &&
+      route.path === '/index.html' // production Desktop use `file:///index.html` not `file:///` as init route
+    ) {
+      const newRouteParams = cloneDeep(routeParams);
+      // 'file:///?networkId=index.html'
+      if (newRouteParams && newRouteParams?.networkId === 'index.html') {
+        delete newRouteParams.networkId;
+      }
+      urlAccountNavigation.replaceHomePage(navigation, newRouteParams);
+      return;
+    }
+
     setTimeout(async () => {
       let networkId = routeParams?.networkId;
       let networkCode = routeParams?.networkId;
@@ -147,6 +164,8 @@ function UrlAccountAutoCreate({ redirectMode }: { redirectMode?: boolean }) {
     actions,
     navigation,
     redirectMode,
+    route.params,
+    route.path,
     routeParams,
     routeParams?.address,
     routeParams?.networkId,
