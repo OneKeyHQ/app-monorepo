55f229720f717636ac192159041085912fc60627
Leon <lixiao.dev@gmail.com>
2024-05-14 10:03:22 +0800

    feat: Add api proxy for Cardano on mobile and extension side (#4579)
    * fix: cardano sdk
    
    * feat: Add API proxy for Cardano on mobile and extension side
    
    * chore: lint

--- packages/kit-bg/src/webembeds/instance/IWebembedApi.ts
+++ packages/kit-bg/src/webembeds/instance/IWebembedApi.ts
@@ -4,5 +4,6 @@ import type WebEmbedApiTest from '../WebEmbedApiTest';
 export type IWebembedApi = {
   chainAdaLegacy: WebEmbedApiChainAdaLegacy;
   test: WebEmbedApiTest;
+  isSDKReady(): Promise<boolean>;
 };
 export type IWebembedApiKeys = keyof IWebembedApi;
