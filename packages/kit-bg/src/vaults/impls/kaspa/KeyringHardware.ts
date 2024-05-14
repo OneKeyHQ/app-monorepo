9bf45cb50adfc1cc840ec2bd1d21ac9cd16a289a
Leon <lixiao.dev@gmail.com>
2024-05-14 23:52:06 +0800

    feat: Hardware sign transaction

--- packages/kit-bg/src/vaults/impls/kaspa/KeyringHardware.ts
+++ packages/kit-bg/src/vaults/impls/kaspa/KeyringHardware.ts
@@ -1,10 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { Transaction } from '@kaspa/core-lib';
+
+import {
+  SignType,
+  SignatureType,
+  SigningMethodType,
+  publicKeyFromX,
+  toTransaction,
+} from '@onekeyhq/core/src/chains/kaspa/sdkKaspa';
+import type { IEncodedTxKaspa } from '@onekeyhq/core/src/chains/kaspa/types';
 import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
 import type {
   ICoreApiGetAddressItem,
   ISignedMessagePro,
   ISignedTxPro,
 } from '@onekeyhq/core/src/types';
+import { convertDeviceError } from '@onekeyhq/shared/src/errors/utils/deviceErrorUtils';
+import { checkIsDefined } from '@onekeyhq/shared/src/utils/assertUtils';
+import bufferUtils from '@onekeyhq/shared/src/utils/bufferUtils';
 
 import { KeyringHardwareBase } from '../../base/KeyringHardwareBase';
 
@@ -14,6 +27,7 @@ import type {
   ISignMessageParams,
   ISignTransactionParams,
 } from '../../types';
+import type { KaspaSignTransactionParams } from '@onekeyfe/hd-core';
 
 export class KeyringHardware extends KeyringHardwareBase {
   override coreApi = coreChainApi.kaspa.hd;
@@ -58,10 +72,89 @@ export class KeyringHardware extends KeyringHardwareBase {
     });
   }
 
-  override signTransaction(
+  override async signTransaction(
     params: ISignTransactionParams,
   ): Promise<ISignedTxPro> {
-    throw new Error('Method not implemented.');
+    const sdk = await this.getHardwareSDKInstance();
+    const encodedTx = params.unsignedTx.encodedTx as IEncodedTxKaspa;
+    const deviceParams = checkIsDefined(params.deviceParams);
+    const { connectId, deviceId } = deviceParams.dbDevice;
+    const dbAccount = await this.vault.getAccount();
+    const chainId = await this.getNetworkChainId();
+
+    const txn = toTransaction(encodedTx);
+    const unSignTx: KaspaSignTransactionParams = {
+      version: txn.version,
+      inputs: txn.inputs.map((input) => ({
+        path: dbAccount.path,
+        prevTxId: input.prevTxId.toString('hex'),
+        outputIndex: input.outputIndex,
+        sequenceNumber: input.sequenceNumber.toString(),
+        output: {
+          satoshis: input?.output?.satoshis.toString() ?? '',
+          script: bufferUtils.bytesToHex(
+            input?.output?.script?.toBuffer() ?? Buffer.alloc(0),
+          ),
+        },
+        sigOpCount: input?.output?.script?.getSignatureOperationsCount() ?? 1,
+      })),
+      outputs: txn.outputs.map((output) => ({
+        satoshis: output.satoshis.toString(),
+        script: bufferUtils.bytesToHex(output.script.toBuffer()),
+        scriptVersion: 0,
+      })),
+      lockTime: txn.nLockTime.toString(),
+      sigHashType: SignatureType.SIGHASH_ALL,
+      sigOpCount: 1,
+      scheme: SignType.Schnorr,
+      prefix: chainId,
+    };
+
+    const response = await sdk.kaspaSignTransaction(connectId, deviceId, {
+      ...params.deviceParams?.deviceCommonParams,
+      ...unSignTx,
+    });
+
+    if (response.success) {
+      const signatures = response.payload;
+
+      for (const signature of signatures) {
+        const input = txn.inputs[signature.index];
+        // @ts-expect-error
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+        const publicKeyBuf = input?.output?.script?.getPublicKey();
+        const publicKey = publicKeyFromX(
+          true,
+          bufferUtils.bytesToHex(publicKeyBuf),
+        );
+
+        // @ts-expect-error
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        txn.inputs[signature.index].addSignature(
+          txn,
+          // @ts-expect-error
+          new Transaction.Signature({
+            publicKey,
+            prevTxId: input.prevTxId,
+            outputIndex: input.outputIndex,
+            inputIndex: signature.index,
+            signature: signature.signature,
+            sigtype: SignatureType.SIGHASH_ALL,
+          }),
+          SigningMethodType.Schnorr,
+        );
+      }
+
+      const rawTx = bufferUtils.bytesToHex(txn.toBuffer());
+
+      return {
+        // TODO: txid ??
+        txid: '',
+        rawTx,
+        encodedTx,
+      };
+    }
+    throw convertDeviceError(response.payload);
   }
 
   override signMessage(params: ISignMessageParams): Promise<ISignedMessagePro> {
