0fe246d632bf3724cec0148f199aa6f17e954c2a
Leon <lixiao.dev@gmail.com>
2024-05-14 14:08:59 +0800

    feat: Hardware account

--- packages/kit-bg/src/vaults/impls/kaspa/KeyringHardware.ts
+++ packages/kit-bg/src/vaults/impls/kaspa/KeyringHardware.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
-import type { ISignedMessagePro, ISignedTxPro } from '@onekeyhq/core/src/types';
+import type {
+  ICoreApiGetAddressItem,
+  ISignedMessagePro,
+  ISignedTxPro,
+} from '@onekeyhq/core/src/types';
 
 import { KeyringHardwareBase } from '../../base/KeyringHardwareBase';
 
@@ -17,7 +21,41 @@ export class KeyringHardware extends KeyringHardwareBase {
   override prepareAccounts(
     params: IPrepareHardwareAccountsParams,
   ): Promise<IDBAccount[]> {
-    throw new Error('Method not implemented.');
+    return this.basePrepareHdNormalAccounts(params, {
+      buildAddressesInfo: async ({ usedIndexes }) => {
+        const addressesInfo = await this.baseGetDeviceAccountAddresses({
+          params,
+          usedIndexes,
+          sdkGetAddressFn: async ({
+            connectId,
+            deviceId,
+            pathPrefix,
+            showOnOnekeyFn,
+          }) => {
+            const sdk = await this.getHardwareSDKInstance();
+            const response = await sdk.kaspaGetAddress(connectId, deviceId, {
+              ...params.deviceParams.deviceCommonParams,
+              bundle: usedIndexes.map((index, arrIndex) => ({
+                path: `${pathPrefix}/${index}`,
+                showOnOneKey: showOnOnekeyFn(arrIndex),
+              })),
+            });
+            return response;
+          },
+        });
+        const ret: ICoreApiGetAddressItem[] = [];
+        for (const addressInfo of addressesInfo) {
+          const { address, path } = addressInfo;
+          const item: ICoreApiGetAddressItem = {
+            address,
+            path,
+            publicKey: '',
+          };
+          ret.push(item);
+        }
+        return ret;
+      },
+    });
   }
 
   override signTransaction(
