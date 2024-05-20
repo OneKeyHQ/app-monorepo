5aaaf1e93fdc6717d278e2dc6abe7d58b9f612b6
huhuanming <huanming@onekey.so>
2024-05-20 17:49:58 +0800

    fix: fix list style

--- packages/kit/src/views/Market/components/MarketDetailPools.tsx
+++ packages/kit/src/views/Market/components/MarketDetailPools.tsx
@@ -1,13 +1,18 @@
+import type { PropsWithChildren } from 'react';
 import { useEffect, useState } from 'react';
 
+import type { ISizableTextProps } from '@onekeyhq/components';
 import {
   Button,
-  IconButton,
+  Icon,
   ListView,
   NumberSizeableText,
   SizableText,
+  Stack,
+  View,
   XStack,
   YStack,
+  useMedia,
 } from '@onekeyhq/components';
 import type {
   IMarketDetailPool,
@@ -16,13 +21,39 @@ import type {
 
 import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
 
-export function MarketDetailPools({
-  token: { id },
+function HeaderColumn({
+  children,
+  textAlign,
 }: {
-  token: IMarketTokenDetail;
+  textAlign: ISizableTextProps['textAlign'];
+  children: ISizableTextProps['children'];
 }) {
+  return (
+    <SizableText
+      flexGrow={1}
+      flexBasis={0}
+      size="$bodySmMedium"
+      color="$textSubdued"
+      textAlign={textAlign}
+    >
+      {children}
+    </SizableText>
+  );
+}
+
+function ItemColumn({ children }: PropsWithChildren) {
+  return (
+    <Stack flexGrow={1} flexBasis={0} jc="center">
+      {children}
+    </Stack>
+  );
+}
+
+export function MarketDetailPools({ token }: { token: IMarketTokenDetail }) {
   const [pools, setPools] = useState<IMarketDetailPool[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const { gtMd } = useMedia();
 
   useEffect(() => {
     setIsLoading(true);
@@ -37,22 +68,14 @@ export function MarketDetailPools({
     <ListView
       data={pools}
       ListHeaderComponent={
-        <XStack>
-          <SizableText size="$bodySmMedium" color="$textSubdued">
-            Pair
-          </SizableText>
-          <SizableText size="$bodySmMedium" color="$textSubdued">
-            price
-          </SizableText>
-          <SizableText size="$bodySmMedium" color="$textSubdued">
-            24H Txns
-          </SizableText>
-          <SizableText size="$bodySmMedium" color="$textSubdued">
-            24H Volume
-          </SizableText>
-          <SizableText size="$bodySmMedium" color="$textSubdued">
-            Liquidity
-          </SizableText>
+        <XStack py="$2.5">
+          <HeaderColumn textAlign="left">Pair</HeaderColumn>
+          {gtMd ? <HeaderColumn textAlign="right">Price</HeaderColumn> : null}
+          {gtMd ? (
+            <HeaderColumn textAlign="right">24H Txns</HeaderColumn>
+          ) : null}
+          <HeaderColumn textAlign="right">24H Volume</HeaderColumn>
+          <HeaderColumn textAlign="right">Liquidity</HeaderColumn>
         </XStack>
       }
       renderItem={({
@@ -60,28 +83,67 @@ export function MarketDetailPools({
       }: {
         item: IMarketDetailPool;
       }) => (
-        <XStack>
-          <YStack>
-            <SizableText size="$bodySmMedium" color="$textSubdued">
-              {attributes.name}
-            </SizableText>
+        <XStack py="$2">
+          <ItemColumn>
+            <XStack space="$2.5" ai="center">
+              <View>
+                <Icon name="TelegramBrand" size="$5" borderRadius="100%" />
+              </View>
+              <YStack flexShrink={1}>
+                <SizableText size="$bodySmMedium">
+                  {attributes.name}
+                </SizableText>
+                <SizableText size="$bodySm" color="$textSubdued">
+                  {relationships.dex.data.id}
+                </SizableText>
+              </YStack>
+            </XStack>
+          </ItemColumn>
 
-            <SizableText size="$bodySmMedium" color="$textSubdued">
-              {relationships.dex.data.id}
-            </SizableText>
-          </YStack>
-          <NumberSizeableText
-            formatter="price"
-            formatterOptions={{ currency: '$' }}
-          >
-            {attributes.base_token_price_usd}
-          </NumberSizeableText>
-          <NumberSizeableText formatter="marketCap">
-            {String(attributes.transactions.h24.buyers)}
-          </NumberSizeableText>
-          <NumberSizeableText formatter="marketCap">
-            {attributes.volume_usd.h24}
-          </NumberSizeableText>
+          {gtMd ? (
+            <ItemColumn>
+              <NumberSizeableText
+                size="$bodyMd"
+                formatter="price"
+                formatterOptions={{ currency: '$' }}
+                textAlign="right"
+              >
+                {attributes.base_token_price_usd}
+              </NumberSizeableText>
+            </ItemColumn>
+          ) : null}
+          {gtMd ? (
+            <ItemColumn>
+              <NumberSizeableText
+                size="$bodyMd"
+                formatter="marketCap"
+                textAlign="right"
+              >
+                {String(
+                  attributes.transactions.h24.buys +
+                    attributes.transactions.h24.sells,
+                )}
+              </NumberSizeableText>
+            </ItemColumn>
+          ) : null}
+          <ItemColumn>
+            <NumberSizeableText
+              size="$bodyMd"
+              formatter="marketCap"
+              textAlign="right"
+            >
+              {attributes.volume_usd.h24}
+            </NumberSizeableText>
+          </ItemColumn>
+          <ItemColumn>
+            <NumberSizeableText
+              size="$bodyMd"
+              formatter="marketCap"
+              textAlign="right"
+            >
+              {attributes.reserve_in_usd}
+            </NumberSizeableText>
+          </ItemColumn>
         </XStack>
       )}
     />
