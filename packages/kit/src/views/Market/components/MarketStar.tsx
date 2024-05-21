194dffa6618d9bb74fb7a8bcdb54a2b289721a5f
huhuanming <huanming@onekey.so>
2024-05-22 02:08:17 +0800

    Create MarketStar.tsx

---
+++ packages/kit/src/views/Market/components/MarketStar.tsx
@@ -0,0 +1,36 @@
+import { memo, useCallback, useMemo } from 'react';
+
+import { IconButton } from '@onekeyhq/components';
+import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+import { useMarketWatchListPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+
+function BasicMarketStar({ coingeckoId }: { coingeckoId: string }) {
+  const [{ items: coingeckoIds }] = useMarketWatchListPersistAtom();
+
+  const isChecked = useMemo(
+    () => !!coingeckoIds.find((i) => i.coingeckoId === coingeckoId),
+    [coingeckoId, coingeckoIds],
+  );
+  const handlePress = useCallback(() => {
+    const item = {
+      coingeckoId,
+    };
+    if (isChecked) {
+      void backgroundApiProxy.serviceMarket.removeFormWatchList(item);
+    } else {
+      void backgroundApiProxy.serviceMarket.addIntoWatchList(item);
+    }
+  }, [coingeckoId, isChecked]);
+
+  return (
+    <IconButton
+      icon={isChecked ? 'StarSolid' : 'StarOutline'}
+      color="red"
+      variant="tertiary"
+      iconSize="$5"
+      onPress={handlePress}
+    />
+  );
+}
+
+export const MarketStar = memo(BasicMarketStar);
