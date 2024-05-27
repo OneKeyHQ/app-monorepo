a7729d9f8f5f023095af4539d20f9ccebfe1e832
huhuanming <huanming@onekey.so>
2024-05-27 10:57:43 +0800

    feat: add Market module (#4623)

---
+++ packages/kit/src/views/Market/MarketWatchListProvider.tsx
@@ -0,0 +1,28 @@
+import type { PropsWithChildren } from 'react';
+import { memo, useMemo } from 'react';
+
+import { useJotaiContextRootStore } from '@onekeyhq/kit/src/states/jotai/utils/useJotaiContextRootStore';
+import { EJotaiContextStoreNames } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+
+import { ProviderJotaiContextMarketWatchList } from '../../states/jotai/contexts/market';
+
+export function useMarketWatchListContextStoreInitData(
+  storeName: EJotaiContextStoreNames,
+) {
+  const data = useMemo(
+    () => ({
+      storeName,
+    }),
+    [storeName],
+  );
+  return data;
+}
+
+export const MarketWatchListProvider = memo(() => {
+  const data = useMarketWatchListContextStoreInitData(
+    EJotaiContextStoreNames.marketWatchList,
+  );
+  const store = useJotaiContextRootStore(data);
+  return <ProviderJotaiContextMarketWatchList store={store} />;
+});
+MarketWatchListProvider.displayName = 'MarketWatchListProvider';
