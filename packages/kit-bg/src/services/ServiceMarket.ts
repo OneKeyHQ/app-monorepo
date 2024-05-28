9a00b6ba9f63ae1a33996434e9b52c9de307d2f0
huhuanming <huanming@onekey.so>
2024-05-28 20:43:31 +0800

    Update ServiceMarket.ts

--- packages/kit-bg/src/services/ServiceMarket.ts
+++ packages/kit-bg/src/services/ServiceMarket.ts
@@ -2,7 +2,6 @@ import {
   backgroundClass,
   backgroundMethod,
 } from '@onekeyhq/shared/src/background/backgroundDecorators';
-import { getRequestHeaders } from '@onekeyhq/shared/src/request/Interceptor';
 import type {
   IMarketCategory,
   IMarketDetailPool,
@@ -11,16 +10,9 @@ import type {
   IMarketTokenDetail,
 } from '@onekeyhq/shared/types/market';
 
-// import { marketWatchListPersistAtom } from '../states/jotai/atoms';
-
 import ServiceBase from './ServiceBase';
 
 const ONEKEY_SEARCH_TRANDING = 'onekey-search-trending';
-
-const getHeaders = async () => ({
-  'x-proxy': 'http://43.134.180.224',
-  ...(await getRequestHeaders()),
-});
 
 @backgroundClass()
 class ServiceMarket extends ServiceBase {
@@ -32,11 +24,8 @@ class ServiceMarket extends ServiceBase {
   async fetchCategories(filters = [ONEKEY_SEARCH_TRANDING]) {
     const client = await this.getClient();
     const response = await client.get<{
-      code: number;
       data: IMarketCategory[];
-    }>('/utility/v1/market/category/list', {
-      headers: await getHeaders(),
-    });
+    }>('/utility/v1/market/category/list');
     const { data } = response.data;
     data[0].name = 'Watchlist';
     return filters.length
@@ -77,7 +66,6 @@ class ServiceMarket extends ServiceBase {
     }
     const client = await this.getClient();
     const response = await client.get<{
-      code: number;
       data: IMarketToken[];
     }>('/utility/v1/market/tokens', {
       params: requestParams,
@@ -85,7 +73,6 @@ class ServiceMarket extends ServiceBase {
         const urlSearchParams = new URLSearchParams(params);
         return urlSearchParams.toString();
       },
-      headers: await getHeaders(),
     });
     const { data } = response.data;
     return data;
@@ -95,14 +82,12 @@ class ServiceMarket extends ServiceBase {
   async fetchTokenDetail(coingeckoId: string, explorerPlatforms = true) {
     const client = await this.getClient();
     const response = await client.get<{
-      code: number;
       data: IMarketTokenDetail;
     }>('/utility/v1/market/detail', {
       params: {
         id: coingeckoId,
         explorer_platforms: explorerPlatforms,
       },
-      headers: await getHeaders(),
     });
     const { data } = response.data;
     return data;
@@ -113,13 +98,11 @@ class ServiceMarket extends ServiceBase {
     const client = await this.getClient();
     try {
       const response = await client.get<{
-        code: number;
         data: IMarketDetailPool[];
       }>('/utility/v1/market/pools', {
         params: {
           query,
         },
-        headers: await getHeaders(),
       });
       const { data } = response.data;
       return data;
@@ -132,7 +115,6 @@ class ServiceMarket extends ServiceBase {
   async fetchTokenChart(coingeckoId: string, days: string, points: number) {
     const client = await this.getClient();
     const response = await client.get<{
-      code: number;
       data: IMarketTokenChart;
     }>('/utility/v1/market/token/chart', {
       params: {
@@ -140,7 +122,6 @@ class ServiceMarket extends ServiceBase {
         days,
         points,
       },
-      headers: await getHeaders(),
     });
     const { data } = response.data;
     return data;
@@ -150,13 +131,11 @@ class ServiceMarket extends ServiceBase {
   async searchToken(query: string) {
     const client = await this.getClient();
     const response = await client.get<{
-      code: number;
       data: string[];
     }>('/utility/v1/market/search', {
       params: {
         query,
       },
-      headers: await getHeaders(),
     });
     const { data } = response.data;
     if (data.length) {
