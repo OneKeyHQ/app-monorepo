03b25683d109bc6dbfb938850d13594fcfbacc8d
huhuanming <huanming@onekey.so>
2024-05-29 01:16:02 +0800

    support share in app

---
+++ packages/kit-bg/src/services/ServiceScanQRCode/utils/parseQRCode/handlers/marketDetail.ts
@@ -0,0 +1,36 @@
+import {
+  WEB_APP_URL,
+  WEB_APP_URL_DEV,
+} from '@onekeyhq/shared/src/config/appConfig';
+
+import { EQRCodeHandlerType } from '../type';
+
+import type { IMarketDetailValue, IQRCodeHandler } from '../type';
+
+/*
+https://wallet.onekeytest.com/btc/3EuKgMjxH8t3zEpMoobSeofXzQ64u2Sfpi
+*/
+export const marketDetail: IQRCodeHandler<IMarketDetailValue> = async (
+  value,
+  options,
+) => {
+  const urlValue = options?.urlResult;
+  if (urlValue?.data?.urlParamList) {
+    const origin = urlValue?.data?.origin;
+    if (
+      [WEB_APP_URL, WEB_APP_URL_DEV].includes(origin) &&
+      urlValue?.data?.pathname === '/market/market_detail' &&
+      urlValue?.data?.urlParamList
+    ) {
+      const { coinGeckoId } = urlValue.data.urlParamList;
+      return {
+        type: EQRCodeHandlerType.MARKET_DETAIL,
+        data: {
+          origin,
+          coinGeckoId,
+        },
+      };
+    }
+  }
+  return null;
+};
