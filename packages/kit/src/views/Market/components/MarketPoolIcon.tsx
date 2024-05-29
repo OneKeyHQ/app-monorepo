f59d14b1a8bb11bccb756aaec11aaebae58500ec
huhuanming <huanming@onekey.so>
2024-05-30 00:18:01 +0800

    fix: fix dex logo

--- packages/kit/src/views/Market/components/MarketPoolIcon.tsx
+++ packages/kit/src/views/Market/components/MarketPoolIcon.tsx
@@ -1,12 +1,15 @@
-import { Icon, Image } from '@onekeyhq/components';
+import { Icon, Image, Skeleton } from '@onekeyhq/components';
 
 export function MarketPoolIcon({ uri }: { uri: string }) {
   return (
     <Image size="$5" borderRadius="$full">
       <Image.Source src={uri} />
       <Image.Fallback>
-        <Icon size="$5" borderRadius="$full" name="SwitchHorOutline" />
+        <Icon borderRadius="$full" name="SwitchHorOutline" />
       </Image.Fallback>
+      <Image.Loading>
+        <Skeleton width="100%" height="100%" />
+      </Image.Loading>
     </Image>
   );
 }
