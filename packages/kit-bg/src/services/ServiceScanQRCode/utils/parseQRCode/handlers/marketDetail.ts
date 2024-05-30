534dc6a96f7a4c7d7fda048b83b31f03bf01da3d
Leon <lixiao.dev@gmail.com>
2024-05-30 10:28:11 +0800

    chore: Change github page cname (#4657)

--- packages/kit-bg/src/services/ServiceScanQRCode/utils/parseQRCode/handlers/marketDetail.ts
+++ packages/kit-bg/src/services/ServiceScanQRCode/utils/parseQRCode/handlers/marketDetail.ts
@@ -8,7 +8,7 @@ import { EQRCodeHandlerType } from '../type';
 import type { IMarketDetailValue, IQRCodeHandler } from '../type';
 
 /*
-https://wallet.onekeytest.com/market/market_detail?coinGeckoId=bitcoin
+https://app.onekeytest.com/market/market_detail?coinGeckoId=bitcoin
 */
 export const marketDetail: IQRCodeHandler<IMarketDetailValue> = async (
   value,
