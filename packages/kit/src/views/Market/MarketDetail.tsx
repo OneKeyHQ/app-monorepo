92f08aeac917b4d8dc7b2df212147cf697216106
huhuanming <huanming@onekey.so>
2024-05-30 18:30:16 +0800

    Update MarketDetail.tsx

--- packages/kit/src/views/Market/MarketDetail.tsx
+++ packages/kit/src/views/Market/MarketDetail.tsx
@@ -82,7 +82,7 @@ function TokenDetailHeader({
             {performance.priceChangePercentage24h}
           </PriceChangePercentage>
         </YStack>
-        <MarketStar coingeckoId={coinGeckoId} mx="$3" />
+        <MarketStar coingeckoId={coinGeckoId} />
       </XStack>
       {gtMd ? (
         <MarketDetailOverview token={token} pools={pools} />
