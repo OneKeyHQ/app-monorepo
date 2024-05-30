514cf5e9c62b7d5c11559e1628151976855088c2
huhuanming <huanming@onekey.so>
2024-05-30 18:00:19 +0800

    Update MarketPoolIcon.tsx

--- packages/kit/src/views/Market/components/MarketPoolIcon.tsx
+++ packages/kit/src/views/Market/components/MarketPoolIcon.tsx
@@ -1,11 +1,19 @@
-import { Icon, Image, Skeleton } from '@onekeyhq/components';
+import { Icon, Image, Skeleton, Stack } from '@onekeyhq/components';
 
 export function MarketPoolIcon({ uri }: { uri: string }) {
   return (
     <Image size="$5" borderRadius="$full">
       <Image.Source src={uri} />
       <Image.Fallback>
-        <Icon borderRadius="$full" name="SwitchHorOutline" />
+        <Stack
+          flex={1}
+          borderRadius="$full"
+          bg="$bgDisabled"
+          ai="center"
+          jc="center"
+        >
+          <Icon size="$3.5" color="$iconSubdued" name="SwitchHorOutline" />
+        </Stack>
       </Image.Fallback>
       <Image.Loading>
         <Skeleton width="100%" height="100%" />
