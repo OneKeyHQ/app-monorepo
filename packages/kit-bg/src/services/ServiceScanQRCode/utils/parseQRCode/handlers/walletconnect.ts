92f6e88dec92556b3fbbe9f823e404b3edcc93d9
hublot <hublot@aliyun.com>
2024-05-31 13:21:27 +0800

    feat: add parseScene to useScanQRCode & resolve the loop resetAnimationQrcodeScan (#4647)
    * feat: add parseScene to useScanQRCode & resolve the loop resetAnimationQrcodeScan

--- packages/kit-bg/src/services/ServiceScanQRCode/utils/parseQRCode/handlers/walletconnect.ts
+++ packages/kit-bg/src/services/ServiceScanQRCode/utils/parseQRCode/handlers/walletconnect.ts
@@ -20,7 +20,7 @@ wc:7a2eabf0-a5ab-4df5-805c-1bf50da956c7@1?bridge=https%3A%2F%2Fx.bridge.walletco
 
 // eslint-disable-next-line spellcheck/spell-checker
 // onekey-wallet://wc?uri=wc%3A6b18a69c27df54b4c228e0ff60218ba460a4994aa5775963f6f0ee354b629afe%402%3Frelay-protocol%3Dirn%26symKey%3D99f6e5fa2bda94c704be8d7adbc2643b861ef49dbe09e0af26d3713e219b4355
-export const walletConnect: IQRCodeHandler<IWalletConnectValue> = async (
+const walletConnect: IQRCodeHandler<IWalletConnectValue> = async (
   value,
   options,
 ) => {
@@ -30,7 +30,7 @@ export const walletConnect: IQRCodeHandler<IWalletConnectValue> = async (
     deeplinkValue &&
     deeplinkValue.data.urlPathList?.[0] === WALLET_CONNECT_DEEP_LINK_NAME
   ) {
-    urlValue = await urlHandler.url(
+    urlValue = await urlHandler.default(
       deeplinkValue.data.urlParamList?.uri,
       options,
     );
@@ -54,3 +54,5 @@ export const walletConnect: IQRCodeHandler<IWalletConnectValue> = async (
   }
   return null;
 };
+
+export default walletConnect;
