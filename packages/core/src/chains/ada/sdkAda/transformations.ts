3a01f386714aa82e52615411f6e6194628b19e54
morizon <sidmorizon@outlook.com>
2024-06-11 17:00:11 +0800

    fix: lint

--- packages/core/src/chains/ada/sdkAda/transformations.ts
+++ packages/core/src/chains/ada/sdkAda/transformations.ts
@@ -1,6 +1,9 @@
 import type { IAdaUTXO } from '../types';
 
-export enum ECardanoAddressType {
+export { CardanoAddressType as ECardanoAddressType } from '@onekeyfe/hd-transport';
+
+/*
+declare enum CardanoAddressType {
   BASE = 0,
   BASE_SCRIPT_KEY = 1,
   BASE_KEY_SCRIPT = 2,
@@ -13,6 +16,20 @@ export enum ECardanoAddressType {
   REWARD = 14,
   REWARD_SCRIPT = 15,
 }
+*/
+// export enum ECardanoAddressType {
+//   BASE = 0,
+//   BASE_SCRIPT_KEY = 1,
+//   BASE_KEY_SCRIPT = 2,
+//   BASE_SCRIPT_SCRIPT = 3,
+//   POINTER = 4,
+//   POINTER_SCRIPT = 5,
+//   ENTERPRISE = 6,
+//   ENTERPRISE_SCRIPT = 7,
+//   BYRON = 8,
+//   REWARD = 14,
+//   REWARD_SCRIPT = 15,
+// }
 
 interface IAdaUtxo {
   address: string;
