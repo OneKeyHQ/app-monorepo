37547a56e2a833ae1dc92f0d588a5eb0c1f81768
huhuanming <huanming@onekey.so>
2024-06-11 17:01:45 +0800

    chore: TS error filters i18n information (#4723)

--- packages/kit/src/views/Developer/router.tsx
+++ packages/kit/src/views/Developer/router.tsx
@@ -18,24 +18,20 @@ export const developerRouters: ITabSubNavigatorConfig<any, any>[] = [
     name: ETabDeveloperRoutes.TabDeveloper,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     component: TabDeveloper,
-    translationId: 'form__dev_mode',
     rewrite: '/',
   },
   ...galleryScreenList,
   {
     name: ETabDeveloperRoutes.DevHome,
     component: DevHome,
-    translationId: 'wallet__wallet',
   },
   {
     name: ETabDeveloperRoutes.DevHomeStack1,
     component: DevHomeStack1,
-    translationId: 'wallet__wallet',
   },
   {
     name: ETabDeveloperRoutes.DevHomeStack2,
     component: DevHomeStack2,
-    translationId: 'wallet__wallet',
   },
   {
     name: ETabDeveloperRoutes.SignatureRecord,
