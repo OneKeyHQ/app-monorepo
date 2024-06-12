6a3a8dc868d01b67a7757cfa38a78157baf35fb5
huhuanming <huanming@onekey.so>
2024-06-13 00:33:51 +0800

    feat: add description for dev settings & fix i18n  (#4746)

--- packages/shared/src/biologyAuth/index.desktop.ts
+++ packages/shared/src/biologyAuth/index.desktop.ts
@@ -2,6 +2,8 @@ import { AuthenticationType } from 'expo-local-authentication';
 
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
+import { ETranslations } from '../locale';
+import { appLocale } from '../locale/appLocale';
 import { memoizee } from '../utils/cacheUtils';
 
 import type { IBiologyAuth } from './types';
@@ -37,7 +39,9 @@ export const biologyAuthenticate: () => Promise<LocalAuthenticationResult> =
     }
 
     try {
-      const result = await window?.desktopApi?.promptTouchID('action__unlock');
+      const result = await window?.desktopApi?.promptTouchID(
+        appLocale.intl.formatMessage({ id: ETranslations.global_unlock }),
+      );
       return result.success
         ? { success: true }
         : {
