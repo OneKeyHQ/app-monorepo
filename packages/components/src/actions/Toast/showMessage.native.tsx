566c91c00ceb1320d7e2a65688eff51ae47df61b
huhuanming <huanming@onekey.so>
2024-06-14 19:18:38 +0800

    Update showMessage.native.tsx

--- packages/components/src/actions/Toast/showMessage.native.tsx
+++ packages/components/src/actions/Toast/showMessage.native.tsx
@@ -1,32 +1,44 @@
 import { toast } from '@backpackapp-io/react-native-toast';
+import { Dimensions } from 'react-native';
 
 import { View, XStack } from '../../primitives';
 
 import type { IToastMessageOptions } from './type';
 
+const GAP = 88;
 export function showMessage({ title, duration }: IToastMessageOptions) {
   toast('', {
     duration,
     disableShadow: true,
-    customToast: ({ width }) => (
-      <XStack alignItems="center" justifyContent="center" width={width}>
-        <View
-          bg="$bgApp"
-          px="$4"
-          py="$3"
-          borderRadius="$2"
-          shadowColor="#181821"
-          shadowOffset={{
-            width: 0,
-            height: 3,
-          }}
-          shadowOpacity={0.15}
-          shadowRadius={4.65}
-          elevationAndroid={7}
+    customToast: () => {
+      const windowWidth = Dimensions.get('window').width;
+      console.log('windowWidth', windowWidth);
+      return (
+        <XStack
+          alignItems="center"
+          justifyContent="center"
+          alignSelf="center"
+          width={windowWidth - GAP * 2}
+          left={GAP}
         >
-          {title}
-        </View>
-      </XStack>
-    ),
+          <View
+            bg="$bgApp"
+            px="$4"
+            py="$3"
+            borderRadius="$2"
+            shadowColor="#181821"
+            shadowOffset={{
+              width: 0,
+              height: 3,
+            }}
+            shadowOpacity={0.15}
+            shadowRadius={4.65}
+            elevationAndroid={7}
+          >
+            {title}
+          </View>
+        </XStack>
+      );
+    },
   });
 }
