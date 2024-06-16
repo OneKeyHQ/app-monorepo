3e84edd76ee32cfdb6d20a45f54a8c6bf2a95f28
JellyWang <38491708+ezailWang@users.noreply.github.com>
2024-06-16 17:40:09 +0800

    fix: support secureStorage check (#4781)

--- packages/shared/src/storage/secureStorage/types.ts
+++ packages/shared/src/storage/secureStorage/types.ts
@@ -2,4 +2,5 @@ export interface ISecureStorage {
   setSecureItem(key: string, data: string): Promise<void>;
   getSecureItem(key: string): Promise<string | null>;
   removeSecureItem(key: string): Promise<void>;
+  supportSecureStorage(): boolean;
 }
