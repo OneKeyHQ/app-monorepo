a97d3e0f84b81bdde52e9a6dbb8d9b27517d359a
huhuanming <huanming@onekey.so>
2024-06-19 20:55:47 +0800

    feat: replace native HeaderSearchBar(OK-28276 OK-27721)  (#4827)

--- packages/components/src/layouts/Page/PageHeader.tsx
+++ packages/components/src/layouts/Page/PageHeader.tsx
@@ -4,8 +4,10 @@ import { useNavigation } from '@react-navigation/native';
 import { useIntl } from 'react-intl';
 
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import { useThemeValue } from '../../hooks';
+import HeaderSearchBar from '../Navigation/Header/HeaderSearchBar';
 
 import type {
   IModalNavigationOptions,
@@ -18,34 +20,36 @@ export type IPageHeaderProps = IStackNavigationOptions &
 const usePageHeaderReloadOptions = () => {
   const intl = useIntl();
   const searchTextColor = useThemeValue('text');
-  const reload = useCallback(
-    (props: IPageHeaderProps) => {
-      if (!props) {
-        return props;
-      }
+  const reload = useCallback((props: IPageHeaderProps) => {
+    if (!props) {
+      return props;
+    }
 
-      const { headerSearchBarOptions, headerTransparent, headerStyle } = props;
-      return {
-        ...props,
-        ...(headerTransparent && {
-          headerStyle: [headerStyle ?? {}, { backgroundColor: 'transparent' }],
-        }),
-        ...(headerSearchBarOptions && {
-          headerSearchBarOptions: {
-            hideNavigationBar: false,
-            hideWhenScrolling: false,
-            cancelButtonText: intl.formatMessage({
-              id: ETranslations.global_cancel,
-            }),
-            textColor: searchTextColor,
-            tintColor: searchTextColor,
-            ...headerSearchBarOptions,
-          },
-        }),
-      };
-    },
-    [intl, searchTextColor],
-  );
+    const {
+      headerSearchBarOptions,
+      headerTransparent,
+      headerStyle,
+      ...restProps
+    } = props;
+    return {
+      ...restProps,
+      ...(headerTransparent && {
+        headerStyle: [headerStyle ?? {}, { backgroundColor: 'transparent' }],
+      }),
+      // ...(headerSearchBarOptions && {
+      //   headerSearchBarOptions: {
+      //     hideNavigationBar: false,
+      //     hideWhenScrolling: false,
+      //     cancelButtonText: intl.formatMessage({
+      //       id: ETranslations.global_cancel,
+      //     }),
+      //     textColor: searchTextColor,
+      //     tintColor: searchTextColor,
+      //     ...headerSearchBarOptions,
+      //   },
+      // }),
+    };
+  }, []);
   return useMemo(() => ({ reload }), [reload]);
 };
 
@@ -57,7 +61,18 @@ const PageHeader = (props: IPageHeaderProps) => {
     navigation.setOptions(reloadOptions);
   }, [navigation, reloadOptions]);
 
-  return null;
+  const { headerSearchBarOptions } = props;
+  // web HeaderSearchBar in packages/components/src/layouts/Navigation/Header/HeaderView.tsx
+  return platformEnv.isNative && headerSearchBarOptions ? (
+    <HeaderSearchBar
+      autoFocus={headerSearchBarOptions?.autoFocus}
+      placeholder={headerSearchBarOptions?.placeholder}
+      onChangeText={headerSearchBarOptions?.onChangeText}
+      onBlur={headerSearchBarOptions?.onBlur}
+      onFocus={headerSearchBarOptions?.onFocus}
+      onSearchButtonPress={headerSearchBarOptions?.onSearchButtonPress}
+    />
+  ) : null;
 };
 
 PageHeader.usePageHeaderReloadOptions = usePageHeaderReloadOptions;
