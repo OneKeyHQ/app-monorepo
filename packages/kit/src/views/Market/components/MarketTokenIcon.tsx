6062703027bf2746531f0fb22b5b191a7da14d94
huhuanming <huanming@onekey.so>
2024-06-20 14:46:53 +0800

    feat: add fallback image in Market & Discover module (#4833)

---
+++ packages/kit/src/views/Market/components/MarketTokenIcon.tsx
@@ -0,0 +1,37 @@
+import { memo } from 'react';
+
+import type { IImageProps } from '@onekeyhq/components';
+import { Icon, Image, Skeleton, Stack } from '@onekeyhq/components';
+
+function BasicMarketTokenIcon({
+  uri,
+  size,
+}: {
+  uri: string;
+  size: IImageProps['size'];
+}) {
+  if (!uri) {
+    return <Skeleton width={size} height={size} radius="round" />;
+  }
+  return (
+    <Image size={size} borderRadius="$full">
+      <Image.Source source={{ uri: decodeURIComponent(uri) }} />
+      <Image.Fallback>
+        <Stack
+          bg="$bgStrong"
+          ai="center"
+          jc="center"
+          width="100%"
+          height="100%"
+        >
+          <Icon name="CoinOutline" size="$6" color="$iconSubdued" />
+        </Stack>
+      </Image.Fallback>
+      <Image.Loading>
+        <Skeleton width="100%" height="100%" radius="round" />
+      </Image.Loading>
+    </Image>
+  );
+}
+
+export const MarketTokenIcon = memo(BasicMarketTokenIcon);
