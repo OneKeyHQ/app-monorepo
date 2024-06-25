2aebf4e09da2b62353b7f07e9404b77e009bea78
HuanXiang <huanxiangspace@qq.com>
2024-06-25 16:02:14 +0800

    fix: page auto reload

--- packages/kit/src/views/Discovery/hooks/useDAppNotifyChanges.ts
+++ packages/kit/src/views/Discovery/hooks/useDAppNotifyChanges.ts
@@ -1,9 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
 
-import { throttle } from 'lodash';
-
-import { useIsMounted } from '@onekeyhq/components/src/hocs/Provider/hooks/useIsMounted';
-import type { IElectronWebView } from '@onekeyhq/kit/src/components/WebView/types';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { ETabRoutes } from '@onekeyhq/shared/src/routes';
 import networkUtils from '@onekeyhq/shared/src/utils/networkUtils';
@@ -16,21 +12,9 @@ import backgroundApiProxy from '../../../background/instance/backgroundApiProxy'
 import useListenTabFocusState from '../../../hooks/useListenTabFocusState';
 import { getWebviewWrapperRef } from '../utils/explorerUtils';
 
-import { useWebTabDataById } from './useWebTabs';
-
 import type { IHandleAccountChangedParams } from '../../DAppConnection/hooks/useHandleAccountChanged';
 
-const notifyChanges = throttle((url: string, fromScene?: string) => {
-  console.log('webview notify changed events: ', url, fromScene);
-  const targetOrigin = new URL(url).origin;
-  void backgroundApiProxy.serviceDApp.notifyDAppAccountsChanged(targetOrigin);
-  void backgroundApiProxy.serviceDApp.notifyDAppChainChanged(targetOrigin);
-}, 800);
-
 export function useDAppNotifyChanges({ tabId }: { tabId: string | null }) {
-  const isMountedRef = useIsMounted();
-  const { tab } = useWebTabDataById(tabId ?? '');
-
   const webviewRef = getWebviewWrapperRef(tabId ?? '');
   const [isFocusedInDiscoveryTab, setIsFocusedInDiscoveryTab] = useState(false);
   useListenTabFocusState([ETabRoutes.MultiTabBrowser], (isFocus) => {
@@ -48,56 +32,6 @@ export function useDAppNotifyChanges({ tabId }: { tabId: string | null }) {
     }
     backgroundApiProxy.connectBridge(jsBridge);
   }, [webviewRef, isFocusedInDiscoveryTab]);
-
-  // sent accountChanged notification
-  useEffect(() => {
-    if (!platformEnv.isNative && !platformEnv.isDesktop) {
-      console.log('not native or not desktop');
-      return;
-    }
-
-    if (!isMountedRef.current) {
-      console.log('not mounted');
-      return;
-    }
-
-    if (!tab?.url || !isFocusedInDiscoveryTab) {
-      console.log('no url or not focused');
-      return;
-    }
-
-    if (!webviewRef) {
-      console.log('no webviewRef');
-      return;
-    }
-
-    console.log('webview isFocused and notifyChanges: ', tab.url);
-    if (platformEnv.isDesktop) {
-      const innerRef = webviewRef?.innerRef as IElectronWebView | undefined;
-
-      if (!innerRef) {
-        return;
-      }
-      // @ts-expect-error
-      if (innerRef.__dy) {
-        notifyChanges(tab.url, 'immediately');
-      } else {
-        const timer = setTimeout(() => {
-          notifyChanges(tab.url, 'setTimeout');
-        }, 1000);
-        const onDomReady = () => {
-          notifyChanges(tab.url, 'domReady');
-          clearTimeout(timer);
-        };
-        innerRef.addEventListener('dom-ready', onDomReady);
-
-        return () => {
-          clearTimeout(timer);
-          innerRef.removeEventListener('dom-ready', onDomReady);
-        };
-      }
-    }
-  }, [isFocusedInDiscoveryTab, tab?.url, webviewRef, isMountedRef]);
 }
 
 export function useShouldUpdateConnectedAccount() {
