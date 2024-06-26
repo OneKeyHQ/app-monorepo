0c2d04450ba3cdc82f5879dfb3834ef7e97709ec
huhuanming <huanming@onekey.so>
2024-06-26 17:00:09 +0800

    feat: sticky Swap page header (#4942)

--- packages/kit/src/views/Swap/pages/SwapPageContainer.tsx
+++ packages/kit/src/views/Swap/pages/SwapPageContainer.tsx
@@ -10,7 +10,7 @@ const SwapPageContainer = () => {
   useDebugComponentRemountLog({ name: 'SwapPageContainer' });
 
   return (
-    <Page scrollEnabled fullPage>
+    <Page fullPage>
       <TabPageHeader
         sceneName={EAccountSelectorSceneName.swap}
         showHeaderRight
