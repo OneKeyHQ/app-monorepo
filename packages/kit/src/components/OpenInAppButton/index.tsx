f598d23a464fde199e69a6426d5bbb9fac8be972
huhuanming <huanming@onekey.so>
2024-06-26 10:24:47 +0800

    Update index.tsx

--- packages/kit/src/components/OpenInAppButton/index.tsx
+++ packages/kit/src/components/OpenInAppButton/index.tsx
@@ -62,7 +62,7 @@ export function OpenInAppButton({
       renderContent: (
         <Stack>
           <Stack alignItems="center" justifyContent="center" overflow="hidden">
-            {deepLinkUrl && openByAppButtonLabel ? (
+            {/* {deepLinkUrl && openByAppButtonLabel ? (
               <Button
                 mb="$4"
                 onPress={() => {
@@ -72,7 +72,7 @@ export function OpenInAppButton({
               >
                 {openByAppButtonLabel}
               </Button>
-            ) : null}
+            ) : null} */}
             <Stack
               p="$4"
               borderRadius="$6"
