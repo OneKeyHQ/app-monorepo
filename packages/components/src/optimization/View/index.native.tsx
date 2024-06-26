06335f41b29253f86e663efe84627c65c9f77c88
蔡凯升 <kaisheng.cai@onkey.so>
2024-06-26 15:35:05 +0800

    test: :bug: E2E: fix tap & choose element in iOS & Android

--- packages/components/src/optimization/View/index.native.tsx
+++ packages/components/src/optimization/View/index.native.tsx
@@ -2,10 +2,18 @@
 import type { ElementType } from 'react';
 
 import type { IOptimizationViewType } from './type';
+import { Stack } from '../../primitives/Stack';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 const ViewNativeComponent: {
   default: ElementType<IOptimizationViewType>;
 } = require('react-native/Libraries/Components/View/ViewNativeComponent');
 
 export * from './type';
-export const OptimizationView = ViewNativeComponent.default;
+
+// E2E mode needs to be enabled screenshot in Android
+export const OptimizationView = platformEnv.isE2E
+  ? function (props: IOptimizationViewType) {
+      return <Stack {...props} />;
+    }
+  : ViewNativeComponent.default;
