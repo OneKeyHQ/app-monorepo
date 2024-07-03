708ca887615bbf0151a8c5dce2afbd271f2d6bbd
huhuanming <huanming@onekey.so>
2024-07-03 12:24:51 +0800

    Update app.ts

--- apps/desktop/src-electron/app.ts
+++ apps/desktop/src-electron/app.ts
@@ -265,7 +265,7 @@ function createMainWindow() {
     showMainWindow();
   });
 
-  browserWindow.setAspectRatio(ratio);
+  // browserWindow.setAspectRatio(ratio);
 
   if (isDev) {
     browserWindow.webContents.openDevTools();
