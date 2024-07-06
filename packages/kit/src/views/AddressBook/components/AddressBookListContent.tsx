8c3cfd9bde3d0bb3aca6314db76b962fca411e35
kwoktung <onlyif2009@gmail.com>
2024-07-06 14:19:42 +0800

    style: address book style (#5095)

--- packages/kit/src/views/AddressBook/components/AddressBookListContent.tsx
+++ packages/kit/src/views/AddressBook/components/AddressBookListContent.tsx
@@ -13,12 +13,14 @@ import {
   SizableText,
   Stack,
   useClipboard,
+  useMedia,
 } from '@onekeyhq/components';
 import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import type { IFuseResultMatch } from '@onekeyhq/shared/src/modules3rdParty/fuse';
 import { buildFuse } from '@onekeyhq/shared/src/modules3rdParty/fuse';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { EModalAddressBookRoutes } from '@onekeyhq/shared/src/routes';
 
 import type { IAddressItem, IAddressNetworkItem } from '../type';
@@ -313,6 +315,8 @@ export const AddressBookListContent = ({
     });
   }, [foldItems, items, searchKey]);
 
+  const media = useMedia();
+
   return (
     <Stack flex={1}>
       <Stack px="$5" pb="$2">
@@ -325,7 +329,7 @@ export const AddressBookListContent = ({
       <SectionList
         showsVerticalScrollIndicator={false}
         onContentSizeChange={onContentSizeChange}
-        estimatedItemSize={60}
+        estimatedItemSize={media.md ? 80 : 60}
         sections={memoSections}
         renderSectionHeader={renderSectionHeader}
         renderItem={renderItem}
