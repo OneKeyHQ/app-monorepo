060526fba6ce8d0708f44090a3c57f1dff23c148
huhuanming <huanming@onekey.so>
2024-07-08 23:01:12 +0800

    Update index.tsx

--- packages/kit/src/components/TokenListView/index.tsx
+++ packages/kit/src/components/TokenListView/index.tsx
@@ -1,4 +1,6 @@
-import { ListView, Stack } from '@onekeyhq/components';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+
+import { ListView, Spinner, Stack } from '@onekeyhq/components';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { getFilteredTokenBySearchKey } from '@onekeyhq/shared/src/utils/tokenUtils';
 import type { IAccountToken } from '@onekeyhq/shared/types/token';
@@ -31,7 +33,7 @@ type IProps = {
   isBuyTokenSupported?: boolean;
 };
 
-function TokenListView(props: IProps) {
+function BasicTokenListView(props: IProps) {
   const {
     onContentSizeChange,
     onPressToken,
@@ -51,7 +53,87 @@ function TokenListView(props: IProps) {
   const [searchKey] = useSearchKeyAtom();
   const { tokens } = tokenList;
 
-  const filteredTokens = getFilteredTokenBySearchKey({ tokens, searchKey });
+  const filteredTokens = useMemo(
+    () => getFilteredTokenBySearchKey({ tokens, searchKey }),
+    [searchKey, tokens],
+  );
+
+  const listHeaderComponent = useMemo(
+    () =>
+      withHeader && tokens.length > 0 ? (
+        <TokenListHeader
+          filteredTokens={filteredTokens}
+          tokens={tokens}
+          tableLayout={tableLayout}
+        />
+      ) : null,
+    [filteredTokens, tableLayout, tokens, withHeader],
+  );
+
+  const listEmptyComponent = useMemo(
+    () =>
+      searchKey ? (
+        EmptySearch
+      ) : (
+        <EmptyToken
+          withBuyAndReceive={withBuyAndReceive}
+          isBuyTokenSupported={isBuyTokenSupported}
+          onBuy={onBuyToken}
+          onReceive={onReceiveToken}
+        />
+      ),
+    [
+      isBuyTokenSupported,
+      onBuyToken,
+      onReceiveToken,
+      searchKey,
+      withBuyAndReceive,
+    ],
+  );
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const listFooterComponent = useMemo(
+    () =>
+      isLoading ? (
+        <Spinner size="small" />
+      ) : (
+        <Stack pb="$5">
+          {withFooter ? <TokenListFooter tableLayout={tableLayout} /> : null}
+        </Stack>
+      ),
+    [isLoading, tableLayout, withFooter],
+  );
+
+  const renderItem = useCallback(
+    ({ item, index }: { item: IAccountToken; index: number }) => (
+      <TokenListItem
+        token={item}
+        key={item.$key}
+        index={index}
+        onPress={onPressToken}
+        tableLayout={tableLayout}
+        withPrice={withPrice}
+      />
+    ),
+    [onPressToken, tableLayout, withPrice],
+  );
+
+  const [listLength, setListLength] = useState(
+    Math.min(20, filteredTokens.length),
+  );
+
+  useEffect(() => {
+    setListLength(Math.min(20, filteredTokens.length));
+  }, [filteredTokens.length]);
+
+  const handleEndReached = useCallback(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setListLength(Math.max(listLength + 20, filteredTokens.length));
+      setIsLoading(false);
+    }, 500);
+  }, [filteredTokens.length, listLength]);
 
   if (!tokenListState.initialized && tokenListState.isRefreshing) {
     return <ListLoading onContentSizeChange={onContentSizeChange} />;
@@ -63,46 +145,17 @@ function TokenListView(props: IProps) {
       estimatedItemSize={tableLayout ? 48 : 60}
       scrollEnabled={onContentSizeChange ? platformEnv.isWebTouchable : true}
       disableScrollViewPanResponder={!!onContentSizeChange}
-      data={filteredTokens}
-      ListHeaderComponent={
-        withHeader && tokens.length > 0 ? (
-          <TokenListHeader
-            filteredTokens={filteredTokens}
-            tokens={tokens}
-            tableLayout={tableLayout}
-          />
-        ) : null
-      }
+      data={filteredTokens.slice(0, listLength)}
+      ListHeaderComponent={listHeaderComponent}
       onContentSizeChange={onContentSizeChange}
-      ListEmptyComponent={
-        searchKey ? (
-          EmptySearch
-        ) : (
-          <EmptyToken
-            withBuyAndReceive={withBuyAndReceive}
-            isBuyTokenSupported={isBuyTokenSupported}
-            onBuy={onBuyToken}
-            onReceive={onReceiveToken}
-          />
-        )
-      }
-      renderItem={({ item, index }) => (
-        <TokenListItem
-          token={item}
-          key={item.$key}
-          index={index}
-          onPress={onPressToken}
-          tableLayout={tableLayout}
-          withPrice={withPrice}
-        />
-      )}
-      ListFooterComponent={
-        <Stack pb="$5">
-          {withFooter ? <TokenListFooter tableLayout={tableLayout} /> : null}
-        </Stack>
-      }
+      ListEmptyComponent={listEmptyComponent}
+      renderItem={renderItem}
+      ListFooterComponent={listFooterComponent}
+      onEndReached={handleEndReached}
+      onEndReachedThreshold={0.01}
     />
   );
 }
 
+const TokenListView = memo(BasicTokenListView);
 export { TokenListView };
