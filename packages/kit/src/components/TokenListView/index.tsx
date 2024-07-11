a4059e2c0d383e78df872525c6db84f6e110d68b
huhuanming <huanming@onekey.so>
2024-07-12 01:13:42 +0800

    Update index.tsx

--- packages/kit/src/components/TokenListView/index.tsx
+++ packages/kit/src/components/TokenListView/index.tsx
@@ -1,4 +1,7 @@
-import { ListView, Stack } from '@onekeyhq/components';
+import { useEffect, useRef } from 'react';
+
+import type { IListViewRef } from '@onekeyhq/components';
+import { ListView, Stack, useScrollViewRef } from '@onekeyhq/components';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { getFilteredTokenBySearchKey } from '@onekeyhq/shared/src/utils/tokenUtils';
 import type { IAccountToken } from '@onekeyhq/shared/types/token';
@@ -53,6 +56,49 @@ function TokenListView(props: IProps) {
 
   const filteredTokens = getFilteredTokenBySearchKey({ tokens, searchKey });
 
+  const scrollViewRef = useScrollViewRef();
+  const listViewRef = useRef<IListViewRef<unknown> | null>(null);
+
+  useEffect(() => {
+    if (!platformEnv.isNative) {
+      const onScroll = () => {
+        const { scrollTop, scrollHeight, clientHeight } = scrollViewRef.current;
+        const isNearBottom = scrollTop + clientHeight >= scrollHeight;
+        if (isNearBottom) {
+          listViewRef.current._listRef.getScrollRef().style.overflowY =
+            'scroll';
+        }
+      };
+
+      let lastScrollTop = 0;
+      const onListViewScroll = () => {
+        const { scrollTop } = listViewRef.current?._listRef._scrollRef;
+        if (lastScrollTop > scrollTop && scrollTop === 0) {
+          listViewRef.current._listRef.getScrollRef().style.overflowY =
+            'hidden';
+        }
+        console.log(lastScrollTop, scrollTop);
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+      };
+      const scrollView = scrollViewRef?.current as unknown as HTMLElement;
+      if (scrollView) {
+        scrollView?.addEventListener('scroll', onScroll);
+      }
+      console.log(listViewRef);
+      setTimeout(() => {
+        console.log(listViewRef.current?._listRef._scrollRef);
+        const listView = listViewRef.current?._listRef
+          ._scrollRef as unknown as HTMLElement;
+        if (listView) {
+          listView?.addEventListener('scroll', onListViewScroll);
+        }
+      }, 3500);
+      return () => {
+        scrollView?.removeEventListener('scroll', onScroll);
+      };
+    }
+  }, [scrollViewRef]);
+
   if (!tokenListState.initialized && tokenListState.isRefreshing) {
     return <ListLoading onContentSizeChange={onContentSizeChange} />;
   }
@@ -61,6 +107,7 @@ function TokenListView(props: IProps) {
     <ListView
       py={withPresetVerticalPadding ? '$3' : '$0'}
       estimatedItemSize={tableLayout ? 48 : 60}
+      ref={listViewRef}
       scrollEnabled={onContentSizeChange ? platformEnv.isWebTouchable : true}
       disableScrollViewPanResponder={!!onContentSizeChange}
       data={filteredTokens}
@@ -73,7 +120,6 @@ function TokenListView(props: IProps) {
           />
         ) : null
       }
-      onContentSizeChange={onContentSizeChange}
       ListEmptyComponent={
         searchKey ? (
           EmptySearch
