db5be897ef4c7b08f719c4fa42418a9acc0d9617
huhuanming <huanming@onekey.so>
2024-07-12 10:41:27 +0800

    Update FirmwareVerifyDialog.tsx

--- packages/kit/src/views/Onboarding/pages/ConnectHardwareWallet/FirmwareVerifyDialog.tsx
+++ packages/kit/src/views/Onboarding/pages/ConnectHardwareWallet/FirmwareVerifyDialog.tsx
@@ -93,7 +93,7 @@ function useFirmwareVerifyBase({
         );
       } else {
         setResult('unofficial');
-        setErrorObj({ code: authResult.result?.code || 0 });
+        setErrorObj({ code: authResult.result?.code || -99999 });
         setContentType(
           EFirmwareAuthenticationDialogContentType.unofficial_device_detected,
         );
