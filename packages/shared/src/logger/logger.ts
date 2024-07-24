1ee9217ec453f0e9004508447bbb75a78430a213
kwoktung <onlyif2009@gmail.com>
2024-07-24 20:53:51 +0800

    chore: staking mixpanel

--- packages/shared/src/logger/logger.ts
+++ packages/shared/src/logger/logger.ts
@@ -5,6 +5,7 @@ import { DemoScope } from './scopes/demo';
 import { DiscoveryScope } from './scopes/discovery';
 import { SettingScope } from './scopes/setting';
 import { SignatureRecordScope } from './scopes/signatureRecord';
+import { StakingScope } from './scopes/staking';
 import { TokenScope } from './scopes/token';
 import { UpdateScope } from './scopes/update';
 
@@ -26,6 +27,8 @@ class Logger {
   discovery = new DiscoveryScope();
 
   token = new TokenScope();
+
+  staking = new StakingScope();
 }
 
 export const defaultLogger = new Logger();
