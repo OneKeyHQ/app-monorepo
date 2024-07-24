26e51e50498d411bc46224ad1b0d4895ef2386f7
weatherstar <weather@onekey.so>
2024-07-24 23:46:17 +0800

    fix: all network history list

--- packages/kit/src/hooks/useReceiveToken.ts
+++ packages/kit/src/hooks/useReceiveToken.ts
@@ -6,9 +6,21 @@ import type {
   IAccountDeriveInfo,
   IAccountDeriveTypes,
 } from '@onekeyhq/kit-bg/src/vaults/types';
-import { EModalReceiveRoutes, EModalRoutes } from '@onekeyhq/shared/src/routes';
+import {
+  EAssetSelectorRoutes,
+  EModalReceiveRoutes,
+  EModalRoutes,
+} from '@onekeyhq/shared/src/routes';
 import type { IModalReceiveParamList } from '@onekeyhq/shared/src/routes';
 import networkUtils from '@onekeyhq/shared/src/utils/networkUtils';
+import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
+import type {
+  IAccountToken,
+  IToken,
+  ITokenData,
+} from '@onekeyhq/shared/types/token';
+
+import { useAccountData } from './useAccountData';
 
 function useReceiveToken({
   accountId,
@@ -16,13 +28,25 @@ function useReceiveToken({
   walletId,
   deriveInfo,
   deriveType,
+  token,
+  tokens,
+  tokenListState,
 }: {
   accountId: string;
   networkId: string;
   walletId: string;
   deriveInfo: IAccountDeriveInfo | undefined;
   deriveType: IAccountDeriveTypes;
+  token: IAccountToken | undefined;
+  isAllNetworks?: boolean;
+  tokens?: ITokenData;
+  tokenListState?: {
+    isRefreshing: boolean;
+    initialized: boolean;
+  };
 }) {
+  const { vaultSettings, network } = useAccountData({ networkId, accountId });
+
   const navigation =
     useAppNavigation<IPageNavigationProp<IModalReceiveParamList>>();
   const handleOnReceive = useCallback(() => {
@@ -39,17 +63,59 @@ function useReceiveToken({
 
     if (!deriveInfo) return;
 
-    navigation.pushModal(EModalRoutes.ReceiveModal, {
-      screen: EModalReceiveRoutes.ReceiveToken,
-      params: {
-        networkId,
-        accountId,
-        walletId,
-        deriveInfo,
-        deriveType,
-      },
-    });
-  }, [accountId, deriveInfo, deriveType, navigation, networkId, walletId]);
+    if (vaultSettings?.isSingleToken || token) {
+      navigation.pushModal(EModalRoutes.ReceiveModal, {
+        screen: EModalReceiveRoutes.ReceiveToken,
+        params: {
+          networkId,
+          accountId,
+          walletId,
+          deriveInfo,
+          deriveType,
+          token,
+        },
+      });
+    } else {
+      navigation.pushModal(EModalRoutes.AssetSelectorModal, {
+        screen: EAssetSelectorRoutes.TokenSelector,
+        params: {
+          networkId,
+          accountId,
+          networkName: network?.name,
+          tokens,
+          tokenListState,
+          onSelect: async (t: IToken) => {
+            await timerUtils.wait(600);
+            navigation.pushModal(EModalRoutes.ReceiveModal, {
+              screen: EModalReceiveRoutes.ReceiveToken,
+              params: {
+                networkId,
+                accountId,
+                walletId,
+                deriveInfo,
+                deriveType,
+                token: t,
+              },
+            });
+          },
+          isAllNetworks: network?.isAllNetworks,
+        },
+      });
+    }
+  }, [
+    accountId,
+    deriveInfo,
+    deriveType,
+    navigation,
+    network?.isAllNetworks,
+    network?.name,
+    networkId,
+    token,
+    tokenListState,
+    tokens,
+    vaultSettings?.isSingleToken,
+    walletId,
+  ]);
 
   return { handleOnReceive };
 }
