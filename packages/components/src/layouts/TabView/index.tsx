c866bd0378ee9f3fd707e662e79d7d05279cabfc
hublot <hublot@aliyun.com>
2024-07-24 09:26:15 +0800

    fix: renderNestedScrollView crash

--- packages/components/src/layouts/TabView/index.tsx
+++ packages/components/src/layouts/TabView/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, memo } from 'react';
+import { forwardRef } from 'react';
 import type { ReactElement } from 'react';
 
 import {
@@ -9,6 +9,8 @@ import {
 // @ts-expect-error
 import NestedScrollView from 'react-native-nested-scroll-view';
 import { withStaticProperties } from 'tamagui';
+
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import { Header } from './Header';
 import { Page } from './Page';
@@ -24,9 +26,9 @@ export const Tab = withStaticProperties(forwardRef(TabComponent), {
   SelectedLabel,
 });
 
-const renderNestedScrollView = NestedScrollView as (
-  props: ScrollViewProps,
-) => ReactElement;
+const renderNestedScrollView = platformEnv.isNativeAndroid
+  ? (NestedScrollView as (props: ScrollViewProps) => ReactElement)
+  : null;
 
 export { renderNestedScrollView, NestedScrollView };
 export { useTabIsRefreshingFocused } from './RefreshingFocused';
