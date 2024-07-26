e5ccb0a2f5e8466ce713f7af0bc1a71bd20d5a99
morizon <sidmorizon@outlook.com>
2024-07-26 10:11:22 +0800

    fix: batch create address catch

--- packages/shared/src/errors/utils/errorUtils.ts
+++ packages/shared/src/errors/utils/errorUtils.ts
@@ -237,6 +237,16 @@ async function withErrorAutoToast<T>(
   }
 }
 
+function isErrorByClassName({
+  error,
+  className,
+}: {
+  error: unknown;
+  className: EOneKeyErrorClassNames;
+}): boolean {
+  return Boolean((error as IOneKeyError)?.className === className && className);
+}
+
 export default {
   autoPrintErrorIgnore,
   normalizeErrorProps,
@@ -249,4 +259,5 @@ export default {
   toastIfErrorDisable,
   showToastOfError,
   withErrorAutoToast,
+  isErrorByClassName,
 };
