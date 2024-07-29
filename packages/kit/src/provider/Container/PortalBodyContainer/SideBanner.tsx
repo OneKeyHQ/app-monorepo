a5d82032431d8053ea3bf3be58ce340dc7e44bf0
huhuanming <huanming@onekey.so>
2024-07-29 16:55:15 +0800

    fix: shown by isCollapse

--- packages/kit/src/provider/Container/PortalBodyContainer/SideBanner.tsx
+++ packages/kit/src/provider/Container/PortalBodyContainer/SideBanner.tsx
@@ -12,6 +12,7 @@ import {
   Stack,
   useMedia,
 } from '@onekeyhq/components';
+import useProviderSideBarValue from '@onekeyhq/components/src/hocs/Provider/hooks/useProviderSideBarValue';
 import SidebarBannerImage from '@onekeyhq/kit/assets/sidebar-banner.png';
 import { useSpotlight } from '@onekeyhq/kit/src/components/Spotlight';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
@@ -22,6 +23,7 @@ import type { GestureResponderEvent } from 'react-native';
 
 function BasicSidebarBanner() {
   const intl = useIntl();
+  const { leftSidebarCollapsed: isCollapse } = useProviderSideBarValue();
   const { isFirstVisit, tourVisited } = useSpotlight(
     ESpotlightTour.oneKeyProBanner,
   );
@@ -38,52 +40,51 @@ function BasicSidebarBanner() {
     [tourVisited],
   );
 
-  return (
-    isFirstVisit && (
-      <Stack
-        borderRadius="$2"
-        borderCurve="continuous"
-        bg="$bgStrong"
-        overflow="hidden"
-        userSelect="none"
-        hoverStyle={{
-          bg: '$gray6',
-        }}
-        pressStyle={{
-          bg: '$gray7',
-        }}
-        onPress={openUrl}
-      >
-        <Stack>
-          <Image h={103} source={SidebarBannerImage} />
-          <Stack
-            position="absolute"
-            top="$2"
-            right="$2"
-            bg="$whiteA3"
-            borderRadius="$full"
-            hoverStyle={{
-              bg: '$whiteA4',
-            }}
-            pressStyle={{
-              bg: '$whiteA5',
-            }}
-            onPress={onTourVisited}
-          >
-            <Icon name="CrossedSmallOutline" size="$5" color="$whiteA7" />
-          </Stack>
-        </Stack>
-        <Stack px="$3" py="$2.5">
-          <Heading size="$bodySmMedium" pb="$0.5">
-            OneKey Pro
-          </Heading>
-          <SizableText size="$bodySm" color="$textSubdued">
-            {intl.formatMessage({ id: ETranslations.hw_banner_description })}
-          </SizableText>
+  const isShown = isFirstVisit && !isCollapse;
+  return isShown ? (
+    <Stack
+      borderRadius="$2"
+      borderCurve="continuous"
+      bg="$bgStrong"
+      overflow="hidden"
+      userSelect="none"
+      hoverStyle={{
+        bg: '$gray6',
+      }}
+      pressStyle={{
+        bg: '$gray7',
+      }}
+      onPress={openUrl}
+    >
+      <Stack>
+        <Image h={103} source={SidebarBannerImage} />
+        <Stack
+          position="absolute"
+          top="$2"
+          right="$2"
+          bg="$whiteA3"
+          borderRadius="$full"
+          hoverStyle={{
+            bg: '$whiteA4',
+          }}
+          pressStyle={{
+            bg: '$whiteA5',
+          }}
+          onPress={onTourVisited}
+        >
+          <Icon name="CrossedSmallOutline" size="$5" color="$whiteA7" />
         </Stack>
       </Stack>
-    )
-  );
+      <Stack px="$3" py="$2.5">
+        <Heading size="$bodySmMedium" pb="$0.5">
+          OneKey Pro
+        </Heading>
+        <SizableText size="$bodySm" color="$textSubdued">
+          {intl.formatMessage({ id: ETranslations.hw_banner_description })}
+        </SizableText>
+      </Stack>
+    </Stack>
+  ) : null;
 }
 
 export const SidebarBanner = () => {
