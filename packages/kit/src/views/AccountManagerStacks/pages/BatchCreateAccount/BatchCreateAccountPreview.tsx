c28b10a54177771f1818702c32dc0e978f7194af
morizon <sidmorizon@outlook.com>
2024-08-01 10:45:45 +0800

    fix: dnx path display

--- packages/kit/src/views/AccountManagerStacks/pages/BatchCreateAccount/BatchCreateAccountPreview.tsx
+++ packages/kit/src/views/AccountManagerStacks/pages/BatchCreateAccount/BatchCreateAccountPreview.tsx
@@ -17,18 +17,19 @@ import {
   XStack,
   useMedia,
 } from '@onekeyhq/components';
-import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
-import { AccountSelectorProviderMirror } from '@onekeyhq/kit/src/components/AccountSelector';
-import { DeriveTypeSelectorFormInput } from '@onekeyhq/kit/src/components/AccountSelector/DeriveTypeSelectorTrigger';
-import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
-import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
-import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
 import type { IDBUtxoAccount } from '@onekeyhq/kit-bg/src/dbs/local/types';
 import type {
   IBatchBuildAccountsAdvancedFlowParams,
   IBatchBuildAccountsNormalFlowParams,
 } from '@onekeyhq/kit-bg/src/services/ServiceBatchCreateAccount/ServiceBatchCreateAccount';
 import type { IAccountDeriveTypes } from '@onekeyhq/kit-bg/src/vaults/types';
+import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+import { AccountSelectorProviderMirror } from '@onekeyhq/kit/src/components/AccountSelector';
+import { DeriveTypeSelectorFormInput } from '@onekeyhq/kit/src/components/AccountSelector/DeriveTypeSelectorTrigger';
+import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
+import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
+import { getNetworkIdsMap } from '@onekeyhq/shared/src/config/networkIds';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import type {
   EAccountManagerStacksRoutes,
@@ -374,6 +375,19 @@ function BatchCreateAccountPreviewPage({
 
   const numWidth = '$20';
 
+  const buildRelPathSuffix = useCallback(
+    (account: INetworkAccount) => {
+      if (networkId === getNetworkIdsMap().dnx) {
+        return '';
+      }
+      if (account.relPath) {
+        return `/${account.relPath.replace(/^\/+/, '')}`;
+      }
+      return '';
+    },
+    [networkId],
+  );
+
   return (
     <Page scrollEnabled safeAreaEnabled>
       <Page.Header
@@ -469,9 +483,7 @@ function BatchCreateAccountPreviewPage({
                   </SizableText>
                   <SizableText size="$bodyMd" color="$textSubdued">
                     {account.path}
-                    {account.relPath
-                      ? `/${account.relPath.replace(/^\/+/, '')}`
-                      : ''}
+                    {buildRelPathSuffix(account)}
                   </SizableText>
                 </Stack>
                 <NumberSizeableText
