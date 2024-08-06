134ae0af20d9d8cc64f5c8b68e5c67f13cd99556
kwoktung <onlyif2009@gmail.com>
2024-08-06 09:01:59 +0800

    try catch reset data (#5425)
    Co-authored-by: loatheb <zhangzhao@onekey.so>

--- packages/kit-bg/src/services/ServiceApp.ts
+++ packages/kit-bg/src/services/ServiceApp.ts
@@ -58,11 +58,29 @@ class ServiceApp extends ServiceBase {
 
   private async resetData() {
     // clean app storage
-    await appStorage.clear();
-    await v4appStorage.clear();
+    try {
+      await appStorage.clear();
+    } catch {
+      console.error('appStorage.clear() error');
+    }
 
-    // clean local db
-    await localDb.reset();
+    await timerUtils.wait(100);
+
+    try {
+      await v4appStorage.clear();
+    } catch {
+      console.error('v4appStorage.clear() error');
+    }
+
+    await timerUtils.wait(100);
+
+    try {
+      // clean local db
+      await localDb.reset();
+    } catch {
+      console.error('localDb.reset() error');
+    }
+
     await timerUtils.wait(1500);
 
     if (platformEnv.isRuntimeBrowser) {
@@ -89,7 +107,11 @@ class ServiceApp extends ServiceBase {
 
     // logout from Google Drive
     if (platformEnv.isNativeAndroid && (await isAvailable())) {
-      await logoutFromGoogleDrive(true);
+      try {
+        await logoutFromGoogleDrive(true);
+      } catch {
+        console.error('logoutFromGoogleDrive error');
+      }
       await timerUtils.wait(1000);
     }
   }
