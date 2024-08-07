ed22bab8db251fd79aeadf563ff021703d08694c
huhuanming <huanming@onekey.so>
2024-08-07 11:49:51 +0800

    Update gallery.ts

--- packages/shared/src/routes/gallery.ts
+++ packages/shared/src/routes/gallery.ts
@@ -69,6 +69,5 @@ export enum EGalleryRoutes {
   ComponentDotMap = 'component-DotMap',
   ComponentUsePromiseResult = 'component-UsePromiseResult',
   ComponentImageCropGallery = 'component-ImageCrop',
-  ComponentUsePromiseResult = 'component-UsePromiseResultGallery',
   ComponentCurrency = 'component-Currency',
 }
