fae241cdb8588fa85b12d9f1b037d29cb8e56259
huhuanming <huanming@onekey.so>
2024-08-07 15:33:19 +0800

    Update index.native.tsx

--- packages/components/src/composite/ImageCrop/index.native.tsx
+++ packages/components/src/composite/ImageCrop/index.native.tsx
@@ -4,14 +4,17 @@ import { withStaticProperties } from 'tamagui';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import { appLocale } from '@onekeyhq/shared/src/locale/appLocale';
 
-
-import type { IOpenPickerFunc, IPickerImage } from './type';
+import {
+  type IOpenPickerFunc,
+  type IPickerImage,
+  RESULT_MINE_TYPE,
+} from './type';
 
 function BasicImageCrop() {
   return null;
 }
 
-const BASE64_PREFIX = 'data:image/jpeg;base64,';
+const BASE64_PREFIX = `data:${RESULT_MINE_TYPE};base64,`;
 const openPicker: IOpenPickerFunc = async (params) => {
   const response: IPickerImage = await nativeOpenPicker({
     mediaType: 'photo',
