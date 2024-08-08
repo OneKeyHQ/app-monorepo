06c83a5813830a0bb9dbf0f003eb9b5bdd1b1147
huhuanming <huanming@onekey.so>
2024-08-09 00:26:59 +0800

    Update index.tsx

--- packages/kit/src/components/TxHistoryListView/index.tsx
+++ packages/kit/src/components/TxHistoryListView/index.tsx
@@ -148,7 +148,7 @@ function TxHistoryListView(props: IProps) {
 
   return (
     <SectionList
-      {...(listViewProps as any)}
+      {...listViewProps}
       renderScrollComponent={renderNestedScrollView}
       ref={listViewRef}
       contentContainerStyle={{
