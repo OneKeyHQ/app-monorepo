d0aa4c9667e8107b25fbe7b1474d677567622824
huhuanming <huanming@onekey.so>
2024-08-10 00:23:03 +0800

    Revert "Update hooks.ts"
    This reverts commit 3efb600eb6dc4a010fbb13b0b6fd286874fc49e1.

--- packages/kit/src/components/UpdateReminder/hooks.ts
+++ packages/kit/src/components/UpdateReminder/hooks.ts
@@ -61,7 +61,7 @@ export const useDownloadPackage = () => {
   );
 };
 
-export const useAppUpdateInfo = (isFullModal = false) => {
+export const useAppUpdateInfo = (isFullModal = false, autoCheck = true) => {
   const intl = useIntl();
   const [appUpdateInfo] = useAppUpdatePersistAtom();
   const navigation = useAppNavigation();
@@ -118,6 +118,9 @@ export const useAppUpdateInfo = (isFullModal = false) => {
 
   // run only once
   useEffect(() => {
+    if (!autoCheck) {
+      return;
+    }
     if (isFirstLaunchAfterUpdated(appUpdateInfo)) {
       onViewReleaseInfo();
     }
