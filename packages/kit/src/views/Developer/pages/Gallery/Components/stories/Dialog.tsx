b3f72f7f3b3dffae11db7d4201a7988c71a5f003
huhuanming <huanming@onekey.so>
2024-08-14 00:30:26 +0800

    fix: fix sheet zindex

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/Dialog.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/Dialog.tsx
@@ -833,6 +833,67 @@ const DialogGallery = () => (
           </YStack>
         ),
       },
+      {
+        title: 'Dialogs',
+        element: (
+          <YStack gap="$4">
+            <Button
+              onPress={() => {
+                Dialog.show({
+                  title: 'A',
+                  description: 'AAAA',
+                  renderContent: <Stack h={200} />,
+                });
+                setTimeout(() => {
+                  Dialog.show({
+                    title: 'B',
+                    description: 'BBB',
+                    sheetProps: {
+                      zIndex: 1e5 + 2,
+                    },
+                  });
+                }, 10);
+              }}
+            >
+              Dialogs
+            </Button>
+            <Button
+              onPress={() => {
+                const SelectListItem = () => {
+                  const [val, setVal] = useState('Apple');
+                  return (
+                    <Select
+                      items={new Array(10).fill(undefined).map((_, index) => ({
+                        label: String(index),
+                        value: String(index),
+                      }))}
+                      sheetProps={{
+                        snapPointsMode: 'percent',
+                        snapPoints: [80],
+                      }}
+                      value={val}
+                      onChange={setVal}
+                      title="Demo Title"
+                      onOpenChange={console.log}
+                    />
+                  );
+                };
+                Dialog.show({
+                  title: 'A',
+                  description: 'AAAA',
+                  renderContent: (
+                    <Stack h={200}>
+                      <SelectListItem />
+                    </Stack>
+                  ),
+                });
+              }}
+            >
+              Select In Dialog
+            </Button>
+          </YStack>
+        ),
+      },
     ]}
   />
 );
