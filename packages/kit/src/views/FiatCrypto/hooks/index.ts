8ef84b2ca74a0526742bc333a79afb2a8cf28fa3
kwoktung <onlyif2009@gmail.com>
2024-08-13 20:11:53 +0800

    fix: buy/sell hooks

--- packages/kit/src/views/FiatCrypto/hooks/index.ts
+++ packages/kit/src/views/FiatCrypto/hooks/index.ts
@@ -1,3 +1,15 @@
+import { useCallback } from 'react';
+
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
+import {
+  useAllTokenListAtom,
+  useAllTokenListMapAtom,
+} from '@onekeyhq/kit/src/states/jotai/contexts/tokenList';
+import {
+  EModalFiatCryptoRoutes,
+  EModalRoutes,
+} from '@onekeyhq/shared/src/routes';
+import networkUtils from '@onekeyhq/shared/src/utils/networkUtils';
 import type {
   IFiatCryptoType,
   IGetTokensListParams,
@@ -13,6 +25,7 @@ export const useSupportNetworkId = (
   usePromiseResult(
     async () => {
       if (!networkId) return false;
+      if (networkUtils.isAllNetwork({ networkId })) return true;
       return backgroundApiProxy.serviceFiatCrypto.isNetworkSupported({
         networkId,
         type,
@@ -58,3 +71,35 @@ export const useGetTokensList = ({
     [networkId, type, accountId],
     { initResult: [], watchLoading: true },
   );
+
+export function useFiatCrypto({
+  accountId,
+  networkId,
+  fiatCryptoType,
+}: {
+  accountId: string;
+  networkId: string;
+  fiatCryptoType: IFiatCryptoType;
+}) {
+  const { result: isSupported } = useSupportNetworkId(
+    fiatCryptoType,
+    networkId,
+  );
+  const [allTokens] = useAllTokenListAtom();
+  const [map] = useAllTokenListMapAtom();
+  const navigation = useAppNavigation();
+  const handleFiatCrypto = useCallback(() => {
+    navigation.pushModal(EModalRoutes.FiatCryptoModal, {
+      screen:
+        fiatCryptoType === 'buy'
+          ? EModalFiatCryptoRoutes.BuyModal
+          : EModalFiatCryptoRoutes.SellModal,
+      params: { networkId, accountId, tokens: allTokens.tokens, map },
+    });
+  }, [accountId, navigation, networkId, allTokens, map, fiatCryptoType]);
+
+  return {
+    handleFiatCrypto,
+    isSupported: Boolean(networkId && accountId && isSupported),
+  };
+}
