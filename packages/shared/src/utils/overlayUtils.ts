6048ed2c726fc07ebb59139e83c79798a705c3f4
huhuanming <huanming@onekey.so>
2024-08-14 09:08:50 +0800

    fix: solve the problem of overlay layer when multiple sheets overlap. (#5508)

--- packages/shared/src/utils/overlayUtils.ts
+++ packages/shared/src/utils/overlayUtils.ts
@@ -4,6 +4,7 @@ export const RESET_OVERLAY_Z_INDEX = APP_STATE_LOCK_Z_INDEX + 2;
 
 // in Tamagui:
 //  the z-index of the Sheet is 1e5 - 1.
+export const SHEET_Z_INDEX = 1e5 - 1;
 //  the z-index of the Dialog is 1e5.
 //  the z-index of the Toast is 1e5.
 //  the z-index of the Popover 15e4.
