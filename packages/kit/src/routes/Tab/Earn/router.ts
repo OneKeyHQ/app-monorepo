b3c59709946f7c93ed57448c414152a3e7c4dee5
huhuanming <huanming@onekey.so>
2024-08-15 16:43:25 +0800

    feat: add earn router

---
+++ packages/kit/src/routes/Tab/Earn/router.ts
@@ -0,0 +1,19 @@
+import type { ITabSubNavigatorConfig } from '@onekeyhq/components';
+import { ETabEarnRoutes } from '@onekeyhq/shared/src/routes';
+
+import {
+  LazyLoadPage,
+  LazyLoadRootTabPage,
+} from '../../../components/LazyLoadPage';
+
+const EarnHome = LazyLoadRootTabPage(
+  () => import('../../../views/Earn/EarnHome'),
+);
+
+export const earnRouters: ITabSubNavigatorConfig<any, any>[] = [
+  {
+    rewrite: '/',
+    name: ETabEarnRoutes.EarnHome,
+    component: EarnHome,
+  },
+];
