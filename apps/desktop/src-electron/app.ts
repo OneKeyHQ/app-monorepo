d3755e117526f19b011e6968c799a200a01192c3
huhuanming <huanming@onekey.so>
2024-08-17 19:59:16 +0800

    fix: fix menu issues of Desktop on Mas (#5563)

--- apps/desktop/src-electron/app.ts
+++ apps/desktop/src-electron/app.ts
@@ -90,6 +90,7 @@ const initMenu = () => {
               !process.mas && {
                 label: i18nText(ETranslations.menu_check_for_updates),
                 click: () => {
+                  showMainWindow();
                   if (mainWindow) {
                     mainWindow.webContents.send(
                       ipcMessageKeys.CHECK_FOR_UPDATES,
@@ -102,10 +103,13 @@ const initMenu = () => {
                 label: i18nText(ETranslations.menu_preferences),
                 accelerator: 'CmdOrCtrl+,',
                 click: () => {
+                  const visible = !!mainWindow?.isVisible();
+                  logger.info('APP_OPEN_SETTINGS visible >>>> ', visible);
                   showMainWindow();
                   if (mainWindow) {
                     mainWindow.webContents.send(
                       ipcMessageKeys.APP_OPEN_SETTINGS,
+                      visible,
                     );
                   }
                 },
