511cefe21a2121a886783015b920d9b869cb7d2d
huhuanming <huanming@onekey.so>
2024-08-19 10:10:41 +0800

    Update index.native.ts

--- packages/shared/src/crashlytics/index.native.ts
+++ packages/shared/src/crashlytics/index.native.ts
@@ -1,7 +1,7 @@
 import platformEnv from '../platformEnv';
 
 const getInstance = async () => {
-  if (platformEnv.isDev || platformEnv.isE2E) {
+  if (platformEnv.isDev) {
     return null;
   }
   const Crashlytics = await import('@react-native-firebase/crashlytics');
