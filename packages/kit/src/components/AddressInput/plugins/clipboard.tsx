83e98f7c10f720118b5c644594f01fe02a4ce427
Leon <lixiao.dev@gmail.com>
2024-08-21 08:30:11 +0800

    chore: remove tabs permission (#5595)
    * chrome: change permission
    
    * chrome: activeTab
    
    * Update chrome_v3.js
    
    * Update clipboard.tsx
    
    * Update .env.version
    
    * chore: change version
    
    ---------
    
    Co-authored-by: huhuanming <huanming@onekey.so>

--- packages/kit/src/components/AddressInput/plugins/clipboard.tsx
+++ packages/kit/src/components/AddressInput/plugins/clipboard.tsx
@@ -4,6 +4,7 @@ import { useIntl } from 'react-intl';
 
 import { IconButton, useClipboard } from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { EInputAddressChangeType } from '@onekeyhq/shared/types/address';
 
 import type { IAddressPluginProps } from '../types';
@@ -20,7 +21,8 @@ export const ClipboardPlugin: FC<IAddressPluginProps> = ({
     onChange?.(text);
     onInputTypeChange?.(EInputAddressChangeType.Paste);
   }, [getClipboard, onChange, onInputTypeChange]);
-  return (
+  return platformEnv.isExtensionUiPopup ||
+    platformEnv.isExtensionUiSidePanel ? null : (
     <IconButton
       title={intl.formatMessage({ id: ETranslations.send_to_paste_tooltip })}
       variant="tertiary"
