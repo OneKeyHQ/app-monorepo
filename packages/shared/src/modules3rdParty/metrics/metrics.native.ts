6b58c3666eda96bd021aee68a213fd936d91b2a8
huhuanming <huanming@onekey.so>
2024-08-27 01:05:43 +0800

    fix: fix error

--- packages/shared/src/modules3rdParty/metrics/metrics.native.ts
+++ packages/shared/src/modules3rdParty/metrics/metrics.native.ts
@@ -1,8 +1,23 @@
-// Replace by performance.reactNativeStartupTiming.initializeRuntimeStart;
-//  in React Native 0.73
-export {
-  getTimeSinceStartup,
-  onUpdate,
-  start,
-  stop,
-} from 'react-native-metrix';
+// // Replace by performance.reactNativeStartupTiming.initializeRuntimeStart;
+// //  in React Native 0.73
+// export {
+//   getTimeSinceStartup,
+//   onUpdate,
+//   start,
+//   stop,
+// } from 'react-native-metrix';
+import type {
+  IGetTimeSinceStartupFunc,
+  IOnUpdateFunc,
+  IStartFunc,
+  IStopFunc,
+} from './type';
+
+export const getTimeSinceStartup: IGetTimeSinceStartupFunc = () =>
+  Date.now() - performance.timeOrigin;
+
+export const onUpdate: IOnUpdateFunc = () => () => {};
+
+export const start: IStartFunc = () => undefined;
+
+export const stop: IStopFunc = () => undefined;
