be3e3ba44790f40cf7513435d1af5607dcc883e8
kwoktung <kwoktung@onekey.so>
2024-09-02 17:00:41 +0800

    delete useless code

--- packages/kit-bg/src/services/ServiceStaking.ts
+++ packages/kit-bg/src/services/ServiceStaking.ts
@@ -3,7 +3,6 @@ import {
   backgroundClass,
   backgroundMethod,
 } from '@onekeyhq/shared/src/background/backgroundDecorators';
-import { getNetworkIdsMap } from '@onekeyhq/shared/src/config/networkIds';
 import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 import { memoizee } from '@onekeyhq/shared/src/utils/cacheUtils';
 import earnUtils from '@onekeyhq/shared/src/utils/earnUtils';
@@ -17,17 +16,11 @@ import type {
 import { EServiceEndpointEnum } from '@onekeyhq/shared/types/endpoint';
 import type {
   IAllowanceOverview,
-  IAprItem,
-  IAprToken,
   IAvailableAsset,
   IClaimableListResponse,
   IEarnAccountResponse,
   IGetPortfolioParams,
-  ILidoEthOverview,
-  ILidoHistoryItem,
-  ILidoMaticOverview,
   IPortfolioItem,
-  IServerEvmTransaction,
   IStakeBaseParams,
   IStakeClaimBaseParams,
   IStakeHistoriesResponse,
@@ -48,238 +41,6 @@ import ServiceBase from './ServiceBase';
 class ServiceStaking extends ServiceBase {
   constructor({ backgroundApi }: { backgroundApi: any }) {
     super({ backgroundApi });
-  }
-
-  private baseGetApr = memoizee(
-    async (token: IAprToken) => {
-      const client = await this.getClient(EServiceEndpointEnum.Earn);
-      const resp = await client.get<{
-        data: IAprItem[];
-      }>(`/earn/v1/apr/index`, { params: { token } });
-      return resp.data.data;
-    },
-    {
-      promise: true,
-      maxAge: timerUtils.getTimeDurationMs({ minute: 5 }),
-    },
-  );
-
-  @backgroundMethod()
-  async getApr(token: IAprToken): Promise<IAprItem[]> {
-    return this.baseGetApr(token);
-  }
-
-  @backgroundMethod()
-  public async fetchLidoEthOverview({
-    accountId,
-    networkId,
-  }: {
-    accountId: string;
-    networkId: string;
-  }): Promise<ILidoEthOverview> {
-    const accountAddress =
-      await this.backgroundApi.serviceAccount.getAccountAddressForApi({
-        networkId,
-        accountId,
-      });
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.get<{
-      data: ILidoEthOverview;
-    }>(`/earn/v1/lido-eth/overview`, { params: { accountAddress, networkId } });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async getLidoEthHistory({
-    accountId,
-    networkId,
-  }: {
-    accountId: string;
-    networkId: string;
-  }) {
-    const accountAddress =
-      await this.backgroundApi.serviceAccount.getAccountAddressForApi({
-        networkId,
-        accountId,
-      });
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.get<{
-      data: ILidoHistoryItem[];
-    }>(`/earn/v1/lido-eth/history`, { params: { accountAddress, networkId } });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoEthStakingTransaction({
-    amount,
-    networkId,
-  }: {
-    amount: string;
-    networkId: string;
-  }) {
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.post<{
-      data: IServerEvmTransaction;
-    }>(`/earn/v1/lido-eth/tx/stake`, { amount, networkId });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoEthPermitMessage({
-    amount,
-    accountId,
-    networkId,
-  }: {
-    amount: string;
-    accountId: string;
-    networkId: string;
-  }) {
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const accountAddress =
-      await this.backgroundApi.serviceAccount.getAccountAddressForApi({
-        networkId,
-        accountId,
-      });
-    const resp = await client.post<{
-      data: { message: string; deadline: number };
-    }>(`/earn/v1/lido-eth/tx/permit`, { amount, accountAddress, networkId });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoEthWithdrawalTransaction({
-    amount,
-    deadline,
-    signature,
-    networkId,
-    accountId,
-  }: {
-    amount: string;
-    deadline: number;
-    signature: string;
-    accountId: string;
-    networkId: string;
-  }) {
-    const accountAddress =
-      await this.backgroundApi.serviceAccount.getAccountAddressForApi({
-        networkId,
-        accountId,
-      });
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.post<{
-      data: IServerEvmTransaction;
-    }>(`/earn/v1/lido-eth/tx/withdrawal`, {
-      amount,
-      deadline,
-      signature,
-      accountAddress,
-      networkId,
-    });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoEthClaimTransaction({
-    requestIds,
-    networkId,
-  }: {
-    requestIds: number[];
-    networkId: string;
-  }) {
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.post<{
-      data: IServerEvmTransaction;
-    }>(`/earn/v1/lido-eth/tx/claim`, { requestIds, networkId });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async fetchLidoMaticOverview({
-    accountId,
-    networkId,
-  }: {
-    accountId: string;
-    networkId: string;
-  }): Promise<ILidoMaticOverview> {
-    const accountAddress =
-      await this.backgroundApi.serviceAccount.getAccountAddressForApi({
-        networkId,
-        accountId,
-      });
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.get<{
-      data: ILidoMaticOverview;
-    }>(`/earn/v1/lido-matic/overview`, {
-      params: { accountAddress, networkId },
-    });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async getLidoMaticHistory({
-    accountId,
-    networkId,
-  }: {
-    accountId: string;
-    networkId: string;
-  }) {
-    const accountAddress =
-      await this.backgroundApi.serviceAccount.getAccountAddressForApi({
-        networkId,
-        accountId,
-      });
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.get<{
-      data: ILidoHistoryItem[];
-    }>(`/earn/v1/lido-matic/history`, {
-      params: { accountAddress, networkId },
-    });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoMaticStakingTransaction({
-    amount,
-    networkId,
-  }: {
-    amount: string;
-    networkId: string;
-  }) {
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.post<{
-      data: IServerEvmTransaction;
-    }>(`/earn/v1/lido-matic/tx/stake`, { amount, networkId });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoMaticWithdrawalTransaction({
-    amount,
-    networkId,
-  }: {
-    amount: string;
-    networkId: string;
-  }) {
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.post<{
-      data: IServerEvmTransaction;
-    }>(`/earn/v1/lido-matic/tx/unstake`, { amount, networkId });
-    return resp.data.data;
-  }
-
-  @backgroundMethod()
-  public async buildLidoMaticClaimTransaction({
-    tokenId,
-    networkId,
-  }: {
-    tokenId: number;
-    networkId: string;
-  }) {
-    const client = await this.getClient(EServiceEndpointEnum.Earn);
-    const resp = await client.post<{
-      data: IServerEvmTransaction;
-    }>(`/earn/v1/lido-matic/tx/claim`, { tokenId, networkId });
-    return resp.data.data;
   }
 
   @backgroundMethod()
@@ -609,13 +370,7 @@ class ServiceStaking extends ServiceBase {
         networkId,
         accountAddress: acc.address,
         symbol: symbol.toUpperCase(),
-        publicKey: [
-          getNetworkIdsMap().btc,
-          getNetworkIdsMap().sbtc,
-          getNetworkIdsMap().tbtc,
-        ].includes(networkId)
-          ? acc.pub
-          : undefined,
+        publicKey: networkUtils.isBTCNetwork(networkId) ? acc.pub : undefined,
         ...rest,
       },
     });
--- packages/shared/types/staking.ts
+++ packages/shared/types/staking.ts
@@ -1,58 +1,9 @@
 import type { IToken } from './token';
 
-export type IServerEvmTransaction = {
-  data: string;
-  value: string;
-  to: string;
-};
-
-export type ILidoEthRequest = {
-  id: number;
-  amountOfStETH: string;
-  isFinalized: boolean;
-  isClaimed: boolean;
-};
-
-export type ILidoTokenItem = {
-  price: string;
-  balanceParsed: string;
-  info: IToken;
-};
-
-export type ILidoEthOverview = {
-  requests: ILidoEthRequest[];
-  eth: ILidoTokenItem;
-  stETH: ILidoTokenItem;
-  minWithdrawAmount: string;
-  minTransactionFee?: string;
-};
-
-export type ILidoMaticRequest = {
-  id: number;
-  claimable: boolean;
-  amount: string;
-};
-
-export type ILidoMaticOverview = {
-  matic: ILidoTokenItem;
-  stMatic: ILidoTokenItem;
-  matic2StMatic: string;
-  requests: ILidoMaticRequest[];
-};
-
 export type IAllowanceOverview = {
   allowance: string;
   allowanceParsed: string;
 };
-
-export type IAprItem = {
-  protocol: string;
-  apr: number;
-  logoUrl: string;
-  displayName: string;
-};
-
-export type IAprToken = 'eth' | 'matic';
 
 // export type IStakeTag = 'lido-eth' | 'lido-matic';
 export type IStakeTag = string;
