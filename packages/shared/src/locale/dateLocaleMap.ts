d571d9b9dc413530a31c5ea9b19b2be71dd81519
JellyWang <38491708+ezailWang@users.noreply.github.com>
2024-09-03 12:16:23 +0800

    fix: swap add popular tokens (#5696)

--- packages/shared/src/locale/dateLocaleMap.ts
+++ packages/shared/src/locale/dateLocaleMap.ts
@@ -25,6 +25,7 @@ import {
 import type { ILocaleSymbol } from './type';
 
 const DateLocaleMap: Record<Exclude<ILocaleSymbol, 'system'>, Locale> = {
+  en: enUS,
   'en-US': enUS,
   'zh-CN': zhCN,
   'zh-HK': zhHK,
