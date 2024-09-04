35cdc48f8b705c3bc330c88ffbfdaad7947593d3
Leon <lixiao.dev@gmail.com>
2024-09-04 10:37:28 +0800

    feat: dApp connection trigger

--- packages/kit/src/views/DAppConnection/components/DAppConnectExtensionFloatingTrigger/index.ext.tsx
+++ packages/kit/src/views/DAppConnection/components/DAppConnectExtensionFloatingTrigger/index.ext.tsx
@@ -1,20 +1,35 @@
 import { useCallback, useEffect, useState } from 'react';
 
-import { Icon, Image, Skeleton, Stack } from '@onekeyhq/components';
+import { useIntl } from 'react-intl';
+
+import {
+  Icon,
+  IconButton,
+  Image,
+  SizableText,
+  Skeleton,
+  Stack,
+  XStack,
+  YStack,
+} from '@onekeyhq/components';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+import { Token } from '@onekeyhq/kit/src/components/Token';
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
 import {
   EAppEventBusNames,
   appEventBus,
 } from '@onekeyhq/shared/src/eventBus/appEventBus';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
 import {
   EDAppConnectionModal,
   EModalRoutes,
 } from '@onekeyhq/shared/src/routes';
+import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 import type { IConnectionAccountInfoWithNum } from '@onekeyhq/shared/types/dappConnection';
 
 export default function DAppConnectExtensionFloatingTrigger() {
+  const intl = useIntl();
   const [activeTabId, setActiveTabId] = useState<number | null>(null);
   const { result, run } = usePromiseResult(
     () =>
@@ -25,6 +40,9 @@ export default function DAppConnectExtensionFloatingTrigger() {
         connectedAccountsInfo: IConnectionAccountInfoWithNum[] | null;
         faviconUrl: string | undefined;
         originFaviconUrl: string | undefined;
+        connectLabel: string;
+        networkIcons: string[];
+        addressLabel: string;
       } | null>((resolve) => {
         chrome.tabs.query(
           { active: true, currentWindow: true },
@@ -32,10 +50,34 @@ export default function DAppConnectExtensionFloatingTrigger() {
             if (tabs[0]) {
               try {
                 const currentOrigin = new URL(tabs[0]?.url ?? '').origin;
+                const hostName = new URL(currentOrigin).hostname;
+                const connectLabel = `Connected to ${hostName}`;
                 const connectedAccountsInfo =
-                  await backgroundApiProxy.serviceDApp.findInjectedAccountByOrigin(
+                  (await backgroundApiProxy.serviceDApp.findInjectedAccountByOrigin(
                     currentOrigin,
-                  );
+                  )) ?? [];
+                const networkIcons = await Promise.all(
+                  connectedAccountsInfo.map(async (accountInfo) => {
+                    const network =
+                      await backgroundApiProxy.serviceNetwork.getNetwork({
+                        networkId: accountInfo.networkId,
+                      });
+                    return network.logoURI ?? '';
+                  }),
+                );
+                let addressLabel = '';
+                if (connectedAccountsInfo.length > 0) {
+                  if (connectedAccountsInfo.length === 1) {
+                    addressLabel = accountUtils.shortenAddress({
+                      address: connectedAccountsInfo[0].address,
+                    });
+                  } else {
+                    addressLabel = intl.formatMessage(
+                      { id: ETranslations.global_count_addresses },
+                      { count: connectedAccountsInfo.length },
+                    );
+                  }
+                }
                 const faviconUrl =
                   await backgroundApiProxy.serviceDiscovery.buildWebsiteIconUrl(
                     currentOrigin,
@@ -48,6 +90,9 @@ export default function DAppConnectExtensionFloatingTrigger() {
                   connectedAccountsInfo,
                   faviconUrl,
                   originFaviconUrl: tabs[0].favIconUrl,
+                  connectLabel,
+                  networkIcons,
+                  addressLabel,
                 });
                 return;
               } catch (error) {
@@ -122,50 +167,102 @@ export default function DAppConnectExtensionFloatingTrigger() {
   return (
     <Stack
       position="absolute"
-      bottom="$2"
-      right="$2"
-      h="$14"
-      w="$14"
-      gap="$2"
+      bottom="0"
+      right="0"
+      left="0"
+      h="$16"
+      w="100%"
+      py="$3"
+      px="$5"
+      flexDirection="row"
       alignItems="center"
-      justifyContent="center"
+      justifyContent="space-between"
       bg="$bgApp"
-      borderRadius="$3"
-      shadowOffset={{
-        width: 0,
-        height: 12,
-      }}
-      shadowRadius={24}
-      shadowColor="rgba(0, 0, 0, 0.09)"
+      borderTopWidth="$px"
+      borderBottomWidth="0"
+      borderLeftWidth="0"
+      borderRightWidth="0"
       borderColor="$border"
-      borderWidth="$px"
       onPress={handlePressFloatingButton}
     >
-      <Stack position="relative">
-        <Image size="$10" borderRadius="$2">
-          <Image.Source src={result?.faviconUrl || result?.originFaviconUrl} />
-          <Image.Fallback>
-            <Icon size="$10" name="GlobusOutline" />
-          </Image.Fallback>
-          <Image.Loading>
-            <Skeleton width="100%" height="100%" />
-          </Image.Loading>
-        </Image>
-        <Stack
-          position="absolute"
-          bottom={-2}
-          right={-2}
-          justifyContent="center"
-          alignItems="center"
-          w="$3"
-          h="$3"
-          borderRadius="$full"
-          bg="$bg"
-          zIndex="$1"
-        >
-          <Stack w="$2" h="$2" bg="$iconSuccess" borderRadius="$full" />
+      <XStack alignItems="center" gap="$3">
+        <Stack position="relative">
+          <Image size="$9" borderRadius="$2">
+            <Image.Source
+              src={result?.faviconUrl || result?.originFaviconUrl}
+            />
+            <Image.Fallback>
+              <Icon size="$10" name="GlobusOutline" />
+            </Image.Fallback>
+            <Image.Loading>
+              <Skeleton width="100%" height="100%" />
+            </Image.Loading>
+          </Image>
+          <Stack
+            position="absolute"
+            bottom={-2}
+            right={-2}
+            justifyContent="center"
+            alignItems="center"
+            w="$3"
+            h="$3"
+            borderRadius="$full"
+            bg="$bg"
+            zIndex="$1"
+          >
+            <Stack w="$2" h="$2" bg="$iconSuccess" borderRadius="$full" />
+          </Stack>
         </Stack>
-      </Stack>
+        <YStack flex={1} maxWidth={276}>
+          <SizableText size="$bodyLgMedium" numberOfLines={1}>
+            {result?.connectLabel}
+          </SizableText>
+          <XStack
+            alignItems="center"
+            p="$1.5"
+            m="-$1.5"
+            borderRadius="$2"
+            hoverStyle={{
+              bg: '$bgHover',
+            }}
+            pressStyle={{
+              bg: '$bgActive',
+            }}
+            focusable
+            focusVisibleStyle={{
+              outlineWidth: 2,
+              outlineColor: '$focusRing',
+              outlineStyle: 'solid',
+            }}
+            onPress={() => {
+              console.log('open account selector');
+            }}
+          >
+            {result?.networkIcons.slice(0, 2).map((icon, index) => (
+              <Token
+                key={icon}
+                size="xs"
+                tokenImageUri={icon}
+                ml={index === 1 ? '$-2' : undefined}
+                borderColor={index === 1 ? '$bgApp' : undefined}
+                borderWidth={index === 1 ? 2 : undefined}
+                borderStyle={index === 1 ? 'solid' : undefined}
+                // @ts-expect-error
+                style={index === 1 ? { boxSizing: 'content-box' } : undefined}
+              />
+            ))}
+            <SizableText pl="$1" size="$bodySm" numberOfLines={1}>
+              {result?.addressLabel}
+            </SizableText>
+            <Icon
+              size="$4"
+              color="$iconSubdued"
+              name="ChevronRightSmallOutline"
+            />
+          </XStack>
+        </YStack>
+      </XStack>
+      <IconButton icon="BrokenLinkOutline" size="medium" variant="tertiary" />
     </Stack>
   );
 }
