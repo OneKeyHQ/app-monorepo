28b191f23c5f3288656f30f2ef99b6ddd229bfb3
Franco <franco@onekey.so>
2024-09-05 22:26:06 +0800

    Swap quote result collapse panel (#5727)
    * Update @tamagui+accordion+1.108.0.patch
    
    * collapse panel
    
    ---------
    
    Co-authored-by: huhuanming <huanming@onekey.so>

--- packages/kit/src/views/Swap/components/SwapQuoteResultRate.tsx
+++ packages/kit/src/views/Swap/components/SwapQuoteResultRate.tsx
@@ -1,20 +1,16 @@
 import { useMemo, useState } from 'react';
 
 import { BigNumber } from 'bignumber.js';
-import { MotiView } from 'moti';
 import { useIntl } from 'react-intl';
 
 import {
-  Divider,
-  IconButton,
+  Icon,
   Image,
   NumberSizeableText,
   SizableText,
   Skeleton,
   Stack,
   XStack,
-  YStack,
-  useMedia,
 } from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import type { ISwapToken } from '@onekeyhq/shared/types/swap/types';
@@ -40,7 +36,6 @@ const SwapQuoteResultRate = ({
   openResult,
 }: ISwapQuoteResultRateProps) => {
   const intl = useIntl();
-  const { md } = useMedia();
   const [isReverse, setIsReverse] = useState(false);
   const rateIsExit = useMemo(() => {
     const rateBN = new BigNumber(rate ?? 0);
@@ -73,15 +68,18 @@ const SwapQuoteResultRate = ({
         hoverStyle={{
           opacity: 0.5,
         }}
-        onPress={() => {
+        onPress={(event) => {
+          event.stopPropagation();
           setIsReverse(!isReverse);
         }}
         cursor="pointer"
       >
         <SizableText
           size="$bodyMdMedium"
-          pl="$1"
-          maxWidth={md ? 240 : 360}
+          maxWidth={240}
+          $gtMd={{
+            maxWidth: 240,
+          }}
           textAlign="right"
         >
           {`1 ${
@@ -100,87 +98,49 @@ const SwapQuoteResultRate = ({
         </SizableText>
       </XStack>
     );
-  }, [fromToken, intl, isReverse, md, onOpenResult, rate, rateIsExit, toToken]);
+  }, [fromToken, intl, isReverse, onOpenResult, rate, rateIsExit, toToken]);
   return (
-    <YStack>
-      <XStack justifyContent="space-between" alignItems="center">
-        {isLoading ? (
-          <Stack py="$1">
-            <Skeleton h="$3" w="$24" />
-          </Stack>
-        ) : (
-          rateContent
-        )}
+    <XStack justifyContent="space-between" alignItems="center">
+      {isLoading ? (
+        <Stack py="$0.5">
+          <Skeleton h="$4" w="$32" />
+        </Stack>
+      ) : (
+        rateContent
+      )}
 
-        <XStack alignItems="center" userSelect="none" gap="$1">
-          {!providerIcon ||
-          !fromToken ||
-          !toToken ||
-          openResult ||
-          !onOpenResult ? null : (
-            <>
-              <Image
-                source={{ uri: providerIcon }}
-                w="$5"
-                h="$5"
-                borderRadius="$1"
-              />
-              <SizableText
-                numberOfLines={1}
-                size="$bodyMdMedium"
-                ml="$1"
-                maxWidth="$20"
-              >
-                {providerName ?? ''}
-              </SizableText>
-            </>
-          )}
-          {onOpenResult ? (
-            <MotiView
-              {...(openResult
-                ? {
-                    from: {
-                      rotate: '0deg',
-                    },
-                    animate: {
-                      rotate: '180deg',
-                    },
-                  }
-                : {
-                    from: {
-                      rotate: '180deg',
-                    },
-                    animate: {
-                      rotate: '0deg',
-                    },
-                  })}
-              transition={{
-                type: 'timing',
-                duration: 150,
-              }}
+      <XStack alignItems="center" userSelect="none" gap="$1">
+        {!providerIcon || !fromToken || !toToken || !onOpenResult ? null : (
+          <XStack
+            animation="quick"
+            y={openResult ? '$1' : '$0'}
+            opacity={openResult ? 0 : 1}
+          >
+            <Image
+              source={{ uri: providerIcon }}
+              w="$5"
+              h="$5"
+              borderRadius="$1"
+            />
+            <SizableText
+              numberOfLines={1}
+              size="$bodyMdMedium"
+              ml="$1"
+              maxWidth="$20"
             >
-              <IconButton
-                size="small"
-                icon="ChevronDownSmallSolid"
-                variant="tertiary"
-                onPress={onOpenResult}
-                hoverStyle={{
-                  bg: '$background',
-                }}
-                pressStyle={{
-                  bg: '$background',
-                }}
-                focusStyle={{
-                  bg: '$background',
-                }}
-                cursor="pointer"
-              />
-            </MotiView>
-          ) : null}
-        </XStack>
+              {providerName ?? ''}
+            </SizableText>
+          </XStack>
+        )}
+        <Stack animation="quick" rotate={openResult ? '180deg' : '0deg'}>
+          <Icon
+            name="ChevronDownSmallOutline"
+            color={openResult ? '$iconActive' : '$iconSubdued'}
+            size="$5"
+          />
+        </Stack>
       </XStack>
-      {openResult && onOpenResult ? <Divider mt="$2" /> : null}
-    </YStack>
+    </XStack>
   );
 };
 export default SwapQuoteResultRate;
