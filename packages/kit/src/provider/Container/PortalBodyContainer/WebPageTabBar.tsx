fcf48ba4cdbc87fef2a08112409d096bf3608b20
huhuanming <huanming@onekey.so>
2024-09-05 21:49:37 +0800

    Update WebPageTabBar.tsx

--- packages/kit/src/provider/Container/PortalBodyContainer/WebPageTabBar.tsx
+++ packages/kit/src/provider/Container/PortalBodyContainer/WebPageTabBar.tsx
@@ -1,15 +1,16 @@
 import { useMemo } from 'react';
 
 import { Portal, useMedia } from '@onekeyhq/components';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import DesktopCustomTabBar from '../../../views/Discovery/pages/DesktopCustomTabBar';
 
 export function WebPageTabBar() {
-  const { md } = useMedia();
+  const { gtMd } = useMedia();
   const memoDesktopCustomTabBar = useMemo(() => <DesktopCustomTabBar />, []);
-  return md ? null : (
+  return platformEnv.isDesktop || (platformEnv.isNative && gtMd) ? (
     <Portal.Body container={Portal.Constant.WEB_TAB_BAR}>
       {memoDesktopCustomTabBar}
     </Portal.Body>
-  );
+  ) : null;
 }
