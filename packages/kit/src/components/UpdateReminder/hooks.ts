84c036f7618ed6f99fdf98f176b9b502f12ae841
huhuanming <huanming@onekey.so>
2024-09-10 01:47:56 +0800

    Update hooks.ts

--- packages/kit/src/components/UpdateReminder/hooks.ts
+++ packages/kit/src/components/UpdateReminder/hooks.ts
@@ -42,7 +42,13 @@ export const useDownloadPackage = () => {
         const result = await NativeDownloadPackage(params);
         await backgroundApiProxy.serviceAppUpdate.verifyPackage(result);
         // The UI verification must display for at least 3 seconds.
-        await Promise.all([verifyPackage(result), timerUtils.wait(3000)]);
+        await Promise.all([
+          verifyPackage({
+            ...params,
+            ...result,
+          }),
+          timerUtils.wait(3000),
+        ]);
         await backgroundApiProxy.serviceAppUpdate.readyToInstall();
       } catch (e) {
         Toast.error({
