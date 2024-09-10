d449b60c888304b97c7db388cc85597166b6853b
huhuanming <huanming@onekey.so>
2024-09-10 09:33:30 +0800

    chore: update verification implementation to use GPG verification on Android (#5768)

--- packages/kit-bg/src/states/jotai/atoms/appUpdate.ts
+++ packages/kit-bg/src/states/jotai/atoms/appUpdate.ts
@@ -14,7 +14,6 @@ export const { target: appUpdatePersistAtom, use: useAppUpdatePersistAtom } =
       latestVersion: '0.0.0',
       isForceUpdate: false,
       updateAt: 0,
-      sha256: '',
       status: EAppUpdateStatus.done,
     },
   });
