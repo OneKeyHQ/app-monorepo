5dc0442c62fc23d5d902f7ae2721a474aad5d497
huhuanming <huanming@onekey.so>
2024-09-13 09:05:34 +0800

    fix: fix all i18n error (#5808)

--- packages/kit/src/views/Home/components/DefiListView/DefiListHeader.tsx
+++ packages/kit/src/views/Home/components/DefiListView/DefiListHeader.tsx
@@ -1,13 +1,14 @@
 import { useIntl } from 'react-intl';
 
 import { SizableText, XStack } from '@onekeyhq/components';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 function DefiListHeader() {
   const intl = useIntl();
   return (
     <XStack px="$2">
       <SizableText size="$headingLg">
-        {intl.formatMessage({ id: 'title__assets' })}
+        {intl.formatMessage({ id: ETranslations.global_asset })}
       </SizableText>
     </XStack>
   );
