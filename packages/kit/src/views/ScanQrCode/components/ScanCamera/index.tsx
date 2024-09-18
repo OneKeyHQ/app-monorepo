27cd14864cd05cb77c61f302e471eef22e504dd5
huhuanming <huanming@onekey.so>
2024-09-18 14:31:23 +0800

    fix: initial camera orientation is portrait even when device's orientation is landscape (OK-32219) (#5841)
    * upgrade expo-camera
    
    * Update index.tsx
    
    * upgrade react-native-camera-kit

--- packages/kit/src/views/ScanQrCode/components/ScanCamera/index.tsx
+++ packages/kit/src/views/ScanQrCode/components/ScanCamera/index.tsx
@@ -2,6 +2,7 @@ import { BarCodeScanner } from 'expo-barcode-scanner';
 import { Camera } from 'expo-camera';
 
 import type { IScanCameraProps } from './types';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 export type { IScanCameraProps };
 
@@ -27,6 +28,7 @@ export function ScanCamera({
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
       }}
+      responsiveOrientationWhenOrientationLocked={platformEnv.isNativeIOSPad}
       {...rest}
     >
       {children}
