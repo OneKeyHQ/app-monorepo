e7d99b961c189daef7ef68a6871715eb35f8f2d4
morizon <sidmorizon@outlook.com>
2024-09-23 16:37:10 +0800

    fix: replace classic mini homescreen image (#5854)
    * fix: image utils
    
    * fix: classic homescreen
    
    * fix: lint
    
    * fix: classic upload to monochrome image
    
    * fix: lint
    
    * fix: monochrome image
    
    * fix: webembed api for imageUtils
    
    * fix: lint
    
    * fix: lint

--- packages/kit/src/views/AccountManagerStacks/pages/HardwareHomeScreen/hardwareHomeScreenData.ts
+++ packages/kit/src/views/AccountManagerStacks/pages/HardwareHomeScreen/hardwareHomeScreenData.ts
@@ -1,21 +1,49 @@
 import type { IImageSourcePropType } from '@onekeyhq/components/src/primitives/Image/type';
 
+export type IHardwareHomeScreenName =
+  | 'blank'
+  | 'original'
+  | 'bitcoin_shade'
+  | 'bitcoin_full'
+  | 'ethereum'
+  | 'bitcoin_b'
+  | 'doge'
+  | 'coffee'
+  | 'carlos'
+  | 'einstein'
+  | 'anonymous'
+  | 'piggy'
+  | 'nyancat'
+  | 'dogs'
+  | 'tetris'
+  | 'pacman'
+  | 'tothemoon'
+  | 'xrc'
+  | 'wallpaper-1'
+  | 'wallpaper-2'
+  | 'wallpaper-3'
+  | 'wallpaper-4'
+  | 'wallpaper-5'
+  | 'wallpaper-6'
+  | 'wallpaper-7';
+
 export type IHardwareHomeScreenData = {
-  name: string;
-  source?: IImageSourcePropType;
-  uri?: string;
-  hex?: string;
-  thumbnailHex?: string;
+  name: IHardwareHomeScreenName;
+  source?: IImageSourcePropType; // image source by require('')
+  uri?: string; // image base64 by upload & crop
+  hex?: string; // image hex by resize
+  thumbnailHex?: string; // thumb image hex by resize
   isUserUpload?: boolean;
 };
 
-export type IHardwareHomeScreenDataMap = {
-  [name: string]: IHardwareHomeScreenData;
-};
+export type IHardwareHomeScreenDataMap = Record<
+  IHardwareHomeScreenName,
+  IHardwareHomeScreenData
+>;
 
 const classicMini: IHardwareHomeScreenData[] = [
   {
-    'name': 'default',
+    'name': 'blank',
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/blank.png'),
   },
   {
@@ -23,204 +51,64 @@ const classicMini: IHardwareHomeScreenData[] = [
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/original.png'),
   },
   {
-    'name': 'circleweb',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/circleweb.png'),
-  },
-  {
-    'name': 'circuit',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/circuit.png'),
-  },
-  {
-    'name': 'starweb',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/starweb.png'),
-  },
-  {
-    'name': 'stars',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/stars.png'),
-  },
-  {
-    'name': 'bitcoin_b2',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_b2.png'),
-  },
-  {
     'name': 'bitcoin_shade',
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_shade.png'),
-  },
-  {
-    'name': 'bitcoin_b',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_b.png'),
   },
   {
     'name': 'bitcoin_full',
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_full.png'),
   },
   {
-    'name': 'bitcat',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcat.png'),
-  },
-  {
-    'name': 'nyancat',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/nyancat.png'),
-  },
-  {
-    'name': 'coffee',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/coffee.png'),
-  },
-  {
-    'name': 'flower',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/flower.png'),
-  },
-  {
-    'name': 'saturn',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/saturn.png'),
-  },
-  {
-    'name': 'jupiter',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/jupiter.png'),
-  },
-  {
-    'name': 'einstein',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/einstein.png'),
-  },
-  {
-    'name': 'piggy',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/piggy.png'),
-  },
-  {
-    'name': 'honeybadger',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/honeybadger.png'),
-  },
-  {
-    'name': 'dragon',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/dragon.png'),
-  },
-  {
-    'name': 'narwal',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/narwal.png'),
-  },
-  {
-    'name': 'rabbit',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/rabbit.png'),
-  },
-  {
-    'name': 'bunny',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bunny.png'),
-  },
-  {
-    'name': 'rooster',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/rooster.png'),
-  },
-  // {
-  //   'name': 'genesis',
-  //   'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/genesis.png'),
-  // },
-  {
-    'name': 'my_bank',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/my_bank.png'),
-  },
-  {
-    'name': 'candle',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/candle.png'),
-  },
-  {
-    'name': 'ancap',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/ancap.png'),
-  },
-  {
-    'name': 'anonymous',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/anonymous.png'),
-  },
-  {
-    'name': 'mushroom',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/mushroom.png'),
-  },
-  {
-    'name': 'invader',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/invader.png'),
-  },
-  {
-    'name': 'mtgox',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/mtgox.png'),
-  },
-  {
-    'name': 'electrum',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/electrum.png'),
-  },
-  {
-    'name': 'mycelium',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/mycelium.png'),
-  },
-  {
     'name': 'ethereum',
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/ethereum.png'),
   },
   {
-    'name': 'litecoin',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/litecoin.png'),
-  },
-  {
-    'name': 'myetherwallet',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/myetherwallet.png'),
-  },
-  {
-    'name': 'zcash',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/zcash.png'),
-  },
-  {
-    'name': 'dash',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/dash.png'),
-  },
-  {
-    'name': 'bitcoin_cash',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_cash.png'),
-  },
-  {
-    'name': 'bitcoin_gold',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_gold.png'),
-  },
-  {
-    'name': 'vertcoin',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/vertcoin.png'),
-  },
-  {
-    'name': 'namecoin',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/namecoin.png'),
-  },
-  {
-    'name': 'monacoin',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/monacoin.png'),
+    'name': 'bitcoin_b',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/bitcoin_b.png'),
   },
   {
     'name': 'doge',
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/doge.png'),
   },
   {
-    'name': 'digibyte',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/digibyte.png'),
-  },
-  {
-    'name': 'decred',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/decred.png'),
-  },
-  {
-    'name': 'multibit',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/multibit.png'),
-  },
-  {
-    'name': 'reddit',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/reddit.png'),
-  },
-  {
-    'name': 'hacker',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/hacker.png'),
-  },
-  {
-    'name': 'polis',
-    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/polis.png'),
+    'name': 'coffee',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/coffee.png'),
   },
   {
     'name': 'carlos',
     'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/carlos.png'),
+  },
+  {
+    'name': 'einstein',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/einstein.png'),
+  },
+  {
+    'name': 'anonymous',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/anonymous.png'),
+  },
+  {
+    'name': 'piggy',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/piggy.png'),
+  },
+  {
+    'name': 'nyancat',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/nyancat.png'),
+  },
+  {
+    'name': 'dogs',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/dogs.png'),
+  },
+  {
+    'name': 'tetris',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/tetris.png'),
+  },
+  {
+    'name': 'pacman',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/pacman.png'),
+  },
+  {
+    'name': 'tothemoon',
+    'source': require('@onekeyhq/shared/src/assets/hardware/homescreens/t1/tothemoon.png'),
   },
   {
     'name': 'xrc',
