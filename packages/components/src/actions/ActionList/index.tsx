b6ae12dec29d8dfb5b0c8026752317ddd442fb2d
huhuanming <huanming@onekey.so>
2024-09-24 13:51:13 +0800

    feat: support shortcuts text on ActionList(OK-30711) (#5887)

--- packages/components/src/actions/ActionList/index.tsx
+++ packages/components/src/actions/ActionList/index.tsx
@@ -17,9 +17,11 @@ import {
   Heading,
   Icon,
   SizableText,
+  XStack,
   YStack,
 } from '../../primitives';
 import { Popover } from '../Popover';
+import { Shortcut } from '../Shortcut';
 import { Trigger } from '../Trigger';
 
 import type { IIconProps, IKeyOfIcons } from '../../primitives';
@@ -33,6 +35,7 @@ export interface IActionListItemProps {
   onPress?: (close: () => void) => void | Promise<boolean | void>;
   disabled?: boolean;
   testID?: string;
+  shortcutKeys?: string[];
 }
 
 export function ActionListItem({
@@ -44,6 +47,7 @@ export function ActionListItem({
   disabled,
   onClose,
   testID,
+  shortcutKeys,
 }: IActionListItemProps & {
   onClose: () => void;
 }) {
@@ -86,24 +90,38 @@ export function ActionListItem({
       onPress={handlePress}
       testID={testID}
     >
-      {icon ? (
-        <Icon
-          name={icon}
-          size="$5"
-          mr="$3"
-          $md={{ size: '$6' }}
-          color={destructive ? '$iconCritical' : '$icon'}
-          {...iconProps}
-        />
-      ) : null}
-      <SizableText
-        textAlign="left"
-        size="$bodyMd"
-        $md={{ size: '$bodyLg' }}
-        color={destructive ? '$textCritical' : '$text'}
-      >
-        {label}
-      </SizableText>
+      <XStack jc="space-between" flex={1}>
+        <XStack>
+          {icon ? (
+            <Icon
+              name={icon}
+              size="$5"
+              mr="$3"
+              $md={{ size: '$6' }}
+              color={destructive ? '$iconCritical' : '$icon'}
+              {...iconProps}
+            />
+          ) : null}
+          <SizableText
+            textAlign="left"
+            size="$bodyMd"
+            $md={{ size: '$bodyLg' }}
+            color={destructive ? '$textCritical' : '$text'}
+          >
+            {label}
+          </SizableText>
+        </XStack>
+        {(platformEnv.isDesktop || platformEnv.isNativeIOSPad) &&
+        shortcutKeys?.length ? (
+          <XStack>
+            <Shortcut>
+              {shortcutKeys.map((key) => (
+                <Shortcut.Key key={key}>{key}</Shortcut.Key>
+              ))}
+            </Shortcut>
+          </XStack>
+        ) : null}
+      </XStack>
     </ButtonFrame>
   );
 }
