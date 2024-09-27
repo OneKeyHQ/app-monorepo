b1c4bb5140005beb35bdc9b92cee0cf5abfcaf16
Leon <lixiao.dev@gmail.com>
2024-09-27 16:17:52 +0800

    chore: fix type

--- packages/kit-bg/src/vaults/impls/evm/sdkEvm/utils.ts
+++ packages/kit-bg/src/vaults/impls/evm/sdkEvm/utils.ts
@@ -3,13 +3,6 @@ import B from 'bignumber.js';
 import type { IServerFiatTokenInfo } from '@onekeyhq/shared/types/serverToken';
 import type { IToken } from '@onekeyhq/shared/types/token';
 
-// export interface IJsonRpcResponse<T = unknown> {
-//   id?: string | number;
-//   jsonrpc?: '2.0';
-//   result: T;
-//   error?: { code?: number; message?: string };
-// }
-
 export function safeNumberString(n: string | B | number, fallback?: string) {
   const res = new B(n);
 
@@ -21,13 +14,21 @@ export function safeNumberString(n: string | B | number, fallback?: string) {
 }
 
 export const nativeTokenUniqueKey = 'native';
+
+function isServerFiatTokenInfo(
+  token: IServerFiatTokenInfo | IToken,
+): token is IServerFiatTokenInfo {
+  return 'balance' in token;
+}
 export function parseTokenItem(token: IServerFiatTokenInfo | IToken) {
   const res = {
-    balance: token.balance,
-    balanceParsed: safeNumberString(token?.balanceParsed ?? ''),
-    fiatValue: token.fiatValue,
-    price: token.price,
-    price24h: token.price24h,
+    balance: isServerFiatTokenInfo(token) ? token.balance : undefined,
+    balanceParsed: isServerFiatTokenInfo(token)
+      ? safeNumberString(token.balanceParsed ?? '')
+      : undefined,
+    fiatValue: isServerFiatTokenInfo(token) ? token.fiatValue : undefined,
+    price: isServerFiatTokenInfo(token) ? token.price : undefined,
+    price24h: isServerFiatTokenInfo(token) ? token.price24h : undefined,
     info: {
       uniqueKey: (token.uniqueKey ?? token.address) || nativeTokenUniqueKey,
       address: token.address,
@@ -36,9 +37,9 @@ export function parseTokenItem(token: IServerFiatTokenInfo | IToken) {
       logoURI: token.logoURI,
       name: token.name,
       symbol: token.symbol,
-      totalSupply: token.totalSupply,
+      totalSupply: isServerFiatTokenInfo(token) ? token.totalSupply : undefined,
       riskLevel: token.riskLevel,
-      adaName: token.adaName,
+      adaName: isServerFiatTokenInfo(token) ? token.adaName : undefined,
       sendAddress: token.sendAddress,
       networkId: token.networkId,
     },
