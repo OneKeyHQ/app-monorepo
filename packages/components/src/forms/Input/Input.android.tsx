8bea2bc0907a662f169c42b0648eb0d315bc154d
huhuanming <huanming@onekey.so>
2024-09-30 00:19:41 +0800

    Update Input.android.tsx

--- packages/components/src/forms/Input/Input.android.tsx
+++ packages/components/src/forms/Input/Input.android.tsx
@@ -1,12 +1,11 @@
 import { useRef } from 'react';
 
 import { styled, useComposedRefs } from '@tamagui/core';
+import TextInput from 'react-native-text-input';
 import { defaultStyles, useInputProps } from 'tamagui';
 import { inputSizeVariant } from 'tamagui/src/helpers/inputHelpers';
 
 import type { InputExtraProps } from 'tamagui';
-
-export const TextInput = require('./AndroidInput');
 
 export const InputFrame = styled(
   TextInput,
