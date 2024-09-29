c621ef6d77d0d89d9afaf84863acb8e7d97af59f
huhuanming <huanming@onekey.so>
2024-09-29 23:11:34 +0800

    Update MainApplication.java

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainApplication.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainApplication.java
@@ -32,6 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
 
       @Override
       protected List<ReactPackage> getPackages() {
+        @SuppressWarnings("UnnecessaryLocalVariable")
+
         List<ReactPackage> packages = new PackageList(this).getPackages();
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
