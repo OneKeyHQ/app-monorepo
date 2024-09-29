94fdbb7fffbb26ec15701e86730f300a13e96973
huhuanming <huanming@onekey.so>
2024-09-30 00:12:45 +0800

    fix: fix type

--- packages/components/src/forms/Input/index.tsx
+++ packages/components/src/forms/Input/index.tsx
@@ -10,13 +10,7 @@ import {
 } from 'react';
 
 import { InteractionManager } from 'react-native';
-import {
-  Group,
-  Input as TMInput,
-  getFontSize,
-  useProps,
-  useThemeName,
-} from 'tamagui';
+import { Group, getFontSize, useProps, useThemeName } from 'tamagui';
 
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
@@ -24,6 +18,7 @@ import { useSelectionColor } from '../../hooks';
 import { useScrollToLocation } from '../../layouts/ScrollView';
 import { Icon } from '../../primitives';
 
+import { Input as TMInput } from './Input';
 import { type IInputAddOnProps, InputAddOnItem } from './InputAddOnItem';
 import { getSharedInputStyles } from './sharedStyles';
 
