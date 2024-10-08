8fb163a2fa8c640873a1c1524b1e4c1f8db97e03
huhuanming <huanming@onekey.so>
2024-10-08 18:49:27 +0800

    Update index.tsx

--- packages/kit/src/components/LetterAvatar/index.tsx
+++ packages/kit/src/components/LetterAvatar/index.tsx
@@ -1,5 +1,8 @@
+import memoizee from 'memoizee';
+
 import type { IStackProps } from '@onekeyhq/components';
 import { SizableText, Stack, getTokenValue } from '@onekeyhq/components';
+import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
 
 import type { Token } from 'tamagui';
 
@@ -8,15 +11,35 @@ type ILetterAvatarProps = {
   size?: IStackProps['width'];
 } & IStackProps;
 
+const a = -19.706_600_493_789_4;
+const b = 4.223_740_874_034_56;
+const c = -0.118_638_455_363_439;
+const d = 0.001_386_577_584_423_69;
+
+// uses a cubic polynomial (ax³ + bx² + cx + d) to calculate the lineHeight based on the input size
+//  where data from packages/components/tamagui.config.ts.
+const getLineHeight = (x: number) =>
+  Math.round(a + b * x + c * x * x + d * x * x * x);
+
+const getFontStyle = memoizee(
+  (size: ILetterAvatarProps['size']) => {
+    const x: number =
+      typeof size === 'number' ? size : getTokenValue(size as Token, 'size');
+    const y = Math.round(x / 1.6667);
+    const z = getLineHeight(y);
+    return [x, y, z];
+  },
+  {
+    maxAge: timerUtils.getTimeDurationMs({ minute: 10 }),
+  },
+);
+
 export function LetterAvatar({
   letter,
   size = '$8',
   ...props
 }: ILetterAvatarProps) {
-  const frameSize: number =
-    typeof size === 'number' ? size : getTokenValue(size as Token, 'size');
-  const fontSize = Math.round(frameSize / 1.6667);
-  const lineHeight = Math.round(fontSize * 1.3333);
+  const [frameSize, fontSize, lineHeight] = getFontStyle(size);
   return (
     <Stack
       borderRadius="$full"
