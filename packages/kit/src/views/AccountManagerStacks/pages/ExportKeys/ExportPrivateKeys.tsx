3a50789d956c73a60b32892379669453ba428560
huhuanming <huanming@onekey.so>
2024-10-10 23:25:33 +0800

    Update ExportPrivateKeys.tsx

--- packages/kit/src/views/AccountManagerStacks/pages/ExportKeys/ExportPrivateKeys.tsx
+++ packages/kit/src/views/AccountManagerStacks/pages/ExportKeys/ExportPrivateKeys.tsx
@@ -227,6 +227,8 @@ function ExportPrivateKeysPage({
         onPress: async () => {
           if (!rawKeyValue) {
             await refreshKey({ noDebouncedCall: true });
+          } else {
+            reset();
           }
           setSecureEntry(!secureEntry);
         },
@@ -256,7 +258,16 @@ function ExportPrivateKeysPage({
         },
       },
     ],
-    [clipboard, exportType, form, intl, rawKeyValue, refreshKey, secureEntry],
+    [
+      clipboard,
+      exportType,
+      form,
+      intl,
+      rawKeyValue,
+      refreshKey,
+      reset,
+      secureEntry,
+    ],
   );
 
   useEffect(() => {
