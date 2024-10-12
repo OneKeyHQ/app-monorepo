5e03f96e6adb3abf4bd432f7e260d49547b6c979
huhuanming <huanming@onekey.so>
2024-10-12 15:47:05 +0800

    fix: fix title & icon of BiometricAuth(OK-32960)

---
+++ packages/kit/src/hooks/useBiometricAuthInfo.ts
@@ -0,0 +1,47 @@
+import { useMemo } from 'react';
+
+import { AuthenticationType } from 'expo-local-authentication';
+import { useIntl } from 'react-intl';
+
+import type { IKeyOfIcons } from '@onekeyhq/components';
+import { usePasswordBiologyAuthInfoAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
+export const useBiometricAuthInfo = () => {
+  const [{ isSupport: biologyAuthIsSupport, authType }] =
+    usePasswordBiologyAuthInfoAtom();
+  const intl = useIntl();
+  return useMemo(() => {
+    let icon: IKeyOfIcons = 'TouchIdSolid';
+    let titleId =
+      platformEnv.isNativeIOS || platformEnv.isDesktopMac
+        ? ETranslations.global_touch_id
+        : ETranslations.auth_with_biometric;
+
+    if (platformEnv.isNative) {
+      if (
+        biologyAuthIsSupport &&
+        (authType.includes(AuthenticationType.FACIAL_RECOGNITION) ||
+          authType.includes(AuthenticationType.IRIS))
+      ) {
+        titleId =
+          authType.length > 1
+            ? ETranslations.global_biometric
+            : ETranslations.global_face_id;
+        icon = 'FaceIdSolid';
+      }
+    } else if (platformEnv.isDesktopWin) {
+      titleId = ETranslations.global_windows_hello;
+      icon = 'WindowsHelloSolid';
+    } else if (platformEnv.isExtension) {
+      titleId = ETranslations.settings_passkey;
+      icon = 'PassKeySolid';
+    }
+
+    return {
+      title: intl.formatMessage({ id: titleId }),
+      icon,
+    };
+  }, [authType, biologyAuthIsSupport, intl]);
+};
