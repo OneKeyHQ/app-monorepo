9256ec90b65a9027bfe61fa9ba0dac204a04e568
huhuanming <huanming@onekey.so>
2024-10-14 14:09:22 +0800

    Update index.ts

--- packages/shared/src/modules3rdParty/react-native-image-colors/index.ts
+++ packages/shared/src/modules3rdParty/react-native-image-colors/index.ts
@@ -5,9 +5,9 @@ import platformEnv from '../../platformEnv';
 import type { ImageColorsResult } from 'react-native-image-colors';
 
 const parseHexColor = (hexColor: string) => {
-  const r = parseInt(hexColor.slice(1, 3), 16);
-  const g = parseInt(hexColor.slice(3, 5), 16);
-  const b = parseInt(hexColor.slice(5, 7), 16);
+  const r = Number.parseInt(hexColor.slice(1, 3), 16);
+  const g = Number.parseInt(hexColor.slice(3, 5), 16);
+  const b = Number.parseInt(hexColor.slice(5, 7), 16);
   return `rgba(${r}, ${g}, ${b}, 0.075)`;
 };
 
