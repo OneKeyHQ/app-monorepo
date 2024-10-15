a4c24c4a1df9fb030ddcf0e742ef5e6981f7e02a
huhuanming <huanming@onekey.so>
2024-10-15 18:21:22 +0800

    remove openUrl

--- packages/shared/src/utils/openUrlUtils.ts
+++ packages/shared/src/utils/openUrlUtils.ts
@@ -4,19 +4,51 @@ import {
   openURL as linkingOpenURL,
 } from 'expo-linking';
 
-import platformEnv from '../platformEnv';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+import {
+  EModalRoutes,
+  EModalWebViewRoutes,
+  ERootRoutes,
+} from '@onekeyhq/shared/src/routes';
 
 import type { IPrefType } from '../../types/desktop';
 
-export const openUrlExternal = (url: string) => {
-  if (platformEnv.isNative) {
-    void linkingOpenURL(url);
-  } else if (platformEnv.isExtension) {
+const openUrlByWebview = (url: string, title?: string) => {
+  global.$navigationRef.current?.navigate(ERootRoutes.Modal, {
+    screen: EModalRoutes.WebViewModal,
+    params: {
+      screen: EModalWebViewRoutes.WebView,
+      params: {
+        url,
+        title,
+      },
+    },
+  });
+};
+
+const openUrlOutsideNative = (url: string): void => {
+  if (platformEnv.isExtension) {
     void chrome.tabs.create({
       url,
     });
   } else {
     window.open(url, '_blank');
+  }
+};
+
+export const openUrlInApp = (url: string, title?: string) => {
+  if (platformEnv.isNative) {
+    openUrlByWebview(url, title);
+  } else {
+    openUrlOutsideNative(url);
+  }
+};
+
+export const openUrlExternal = (url: string) => {
+  if (platformEnv.isNative) {
+    void linkingOpenURL(url);
+  } else {
+    openUrlOutsideNative(url);
   }
 };
 
@@ -31,6 +63,7 @@ export const openSettings = (prefType: IPrefType) => {
 };
 
 export default {
+  openUrlInApp,
   openUrlExternal,
   openSettings,
   linkingCanOpenURL,
