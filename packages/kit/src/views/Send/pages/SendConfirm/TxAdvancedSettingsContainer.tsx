6159c505093055f86b3b0ec446591118bd68f051
weatherstar <weather@onekey.so>
2024-10-18 18:10:11 +0800

    feat: add nonce&data faq info

--- packages/kit/src/views/Send/pages/SendConfirm/TxAdvancedSettingsContainer.tsx
+++ packages/kit/src/views/Send/pages/SendConfirm/TxAdvancedSettingsContainer.tsx
@@ -6,18 +6,21 @@ import { useIntl } from 'react-intl';
 
 import {
   Button,
+  Dialog,
   Divider,
   Form,
   Input,
   TextAreaInput,
   useForm,
 } from '@onekeyhq/components';
+import type { IEncodedTxEvm } from '@onekeyhq/core/src/chains/evm/types';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
 import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
 import { useUnsignedTxsAtom } from '@onekeyhq/kit/src/states/jotai/contexts/sendConfirm';
 import { useSettingsPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
-import type { IEncodedTxEvm } from '@onekeyhq/core/src/chains/evm/types';
+import { appLocale } from '@onekeyhq/shared/src/locale/appLocale';
+
 import {
   InfoItem,
   InfoItemGroup,
@@ -26,6 +29,38 @@ import {
 type IProps = {
   accountId: string;
   networkId: string;
+};
+
+const showNonceFaq = () => {
+  Dialog.show({
+    title: appLocale.intl.formatMessage({
+      id: ETranslations.global_nonce,
+    }),
+    icon: 'LabOutline',
+    description: appLocale.intl.formatMessage({
+      id: ETranslations.global_nonce_faq_desc,
+    }),
+    showCancelButton: false,
+    onConfirmText: appLocale.intl.formatMessage({
+      id: ETranslations.global_done,
+    }),
+  });
+};
+
+const showHexDataFaq = () => {
+  Dialog.show({
+    title: appLocale.intl.formatMessage({
+      id: ETranslations.global_hex_data,
+    }),
+    icon: 'ConsoleOutline',
+    description: appLocale.intl.formatMessage({
+      id: ETranslations.global_hex_data_faq_desc,
+    }),
+    showCancelButton: false,
+    onConfirmText: appLocale.intl.formatMessage({
+      id: ETranslations.global_done,
+    }),
+  });
 };
 
 function TxAdvancedSettingsContainer(props: IProps) {
@@ -109,7 +144,12 @@ function TxAdvancedSettingsContainer(props: IProps) {
             },
           )}
           labelAddon={
-            <Button size="small" variant="tertiary" icon="WalletOutline">
+            <Button
+              size="small"
+              variant="tertiary"
+              icon="WalletOutline"
+              onPress={() => showNonceFaq()}
+            >
               {intl.formatMessage({
                 id: ETranslations.global_nonce_faq,
               })}
@@ -127,7 +167,12 @@ function TxAdvancedSettingsContainer(props: IProps) {
             validate: handleValidateData,
           }}
           labelAddon={
-            <Button size="small" variant="tertiary" icon="WalletOutline">
+            <Button
+              size="small"
+              variant="tertiary"
+              icon="WalletOutline"
+              onPress={() => showHexDataFaq()}
+            >
               {intl.formatMessage({
                 id: ETranslations.global_hex_data_faq,
               })}
