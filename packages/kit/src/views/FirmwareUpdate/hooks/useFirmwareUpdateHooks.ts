560ace2792177d3b1c406ad2154ae83b22438edf
huhuanming <huanming@onekey.so>
2024-10-22 17:30:05 +0800

    Update useFirmwareUpdateHooks.ts

--- packages/kit/src/views/FirmwareUpdate/hooks/useFirmwareUpdateHooks.ts
+++ packages/kit/src/views/FirmwareUpdate/hooks/useFirmwareUpdateHooks.ts
@@ -1,7 +1,6 @@
 import { useCallback, useEffect } from 'react';
 
 import { useIntl } from 'react-intl';
-import { Alert, BackHandler } from 'react-native';
 
 import { Dialog, usePreventRemove } from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
@@ -71,7 +70,6 @@ export function useAppExitPrevent({
   message: string;
   title: string;
 }) {
-  const intl = useIntl();
   // Prevents web page refresh/exit
   useEffect(() => {
     if (platformEnv.isRuntimeBrowser && !platformEnv.isExtensionUiPopup) {
@@ -86,43 +84,6 @@ export function useAppExitPrevent({
       };
     }
   }, [message]);
-
-  // Prevent Android exit
-  // https://reactnavigation.org/docs/7.x/preventing-going-back
-  useEffect(() => {
-    const onBackPress = () => {
-      Alert.alert(
-        title,
-        message,
-        [
-          {
-            text: intl.formatMessage({ id: ETranslations.global_cancel }),
-            onPress: () => {
-              // Do nothing
-            },
-            style: 'cancel',
-          },
-          {
-            text: intl.formatMessage({ id: ETranslations.global_quit }),
-            onPress: () => BackHandler.exitApp(),
-          },
-        ],
-        { cancelable: false },
-      );
-
-      return true;
-    };
-
-    const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
-      onBackPress,
-    );
-
-    return () => backHandler.remove();
-  }, [message, title, intl]);
-
-  // Prevent Desktop exit
-  // TODO
 }
 
 export function useExtensionUpdatingFromExpandTab() {
