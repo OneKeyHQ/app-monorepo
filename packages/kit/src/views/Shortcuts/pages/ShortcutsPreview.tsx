8f75aeb0f9a62a039edbe4f95277304326fb818c
huhuanming <huanming@onekey.so>
2024-10-23 18:03:35 +0800

    add Shortcuts page

---
+++ packages/kit/src/views/Shortcuts/pages/ShortcutsPreview.tsx
@@ -0,0 +1,11 @@
+import { Page } from '@onekeyhq/components';
+
+function ShortcutsPreview() {
+  return (
+    <Page>
+      <Page.Header headerTitle="Shortcuts" />
+    </Page>
+  );
+}
+
+export default ShortcutsPreview;
