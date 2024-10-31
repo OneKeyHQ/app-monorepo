8c70eab8bd5c85f3fb2d40291c5cd404107f7e40
huhuanming <huanming@onekey.so>
2024-10-31 18:21:38 +0800

    Update index.ts

--- apps/desktop/src-electron/service/index.ts
+++ apps/desktop/src-electron/service/index.ts
@@ -1,33 +1,45 @@
 import path from 'path';
 
-import { MessageChannelMain, utilityProcess } from 'electron/main';
+import { utilityProcess } from 'electron/main';
 import Logger from 'electron-log/main';
 
 import type { UtilityProcess } from 'electron/main';
 
 let child: UtilityProcess | null = null;
+let onMessageCallbacks: { type: string; callback: (e: any) => void }[] = [];
 export const startServices = () => {
   child = utilityProcess.fork(
     // After build, the directory is 'dist' and WindowsHello file is located in 'dist/service'
     path.join(__dirname, './service/windowsHello.js'),
   );
+  child.on('message', (e: { data: { type: string; result: boolean } }) => {
+    Logger.info('checkAvailabilityAsync', e);
+    if (e.data.type === 'checkAvailabilityAsync') {
+      const callbacks = onMessageCallbacks.filter(
+        (callbackItem) => callbackItem.type === 'checkAvailabilityAsync',
+      );
+      if (callbacks.length) {
+        callbacks.forEach((callbackItem) => {
+          callbackItem.callback(e.data.result);
+        });
+        onMessageCallbacks = onMessageCallbacks.filter(
+          (callbackItem) => !callbacks.includes(callbackItem),
+        );
+      }
+    }
+  });
 };
 
 let isSupport = true;
+
 export const checkAvailabilityAsync = () =>
   isSupport
     ? Promise.race([
         new Promise((resolve) => {
-          child?.on(
-            'message',
-            (e: { data: { type: string; result: boolean } }) => {
-              Logger.info('checkAvailabilityAsync', e);
-              if (e.data.type === 'checkAvailabilityAsync') {
-                resolve(e.data.result);
-              }
-            },
-          );
-          child?.removeAllListeners('message');
+          onMessageCallbacks.push({
+            type: 'checkAvailabilityAsync',
+            callback: resolve,
+          });
           child?.postMessage({ type: 'checkAvailabilityAsync' });
         }),
         new Promise((resolve) =>
