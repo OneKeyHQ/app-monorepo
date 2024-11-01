0a72ff9196622b1a720bb4aa1ace3edb0039aabe
huhuanming <huanming@onekey.so>
2024-11-01 18:16:08 +0800

    fix: green indicator of camera don't close(OK-33336) (#6075)

--- packages/kit/src/views/ScanQrCode/components/ScanCamera/index.native.tsx
+++ packages/kit/src/views/ScanQrCode/components/ScanCamera/index.native.tsx
@@ -1,6 +1,9 @@
+import { useCallback, useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
 import { Camera } from 'react-native-camera-kit/src';
 
-import { defaultLogger } from '@onekeyhq/shared/src/logger/logger';
+import { Button, usePreventRemove } from '@onekeyhq/components';
 
 import type { IScanCameraProps } from './types';
 
@@ -8,31 +11,50 @@ export type { IScanCameraProps };
 
 export function ScanCamera({
   style,
-  isActive,
   children,
   handleScanResult,
   ...rest
 }: IScanCameraProps) {
-  if (!isActive) {
-    return null;
-  }
+  const [isFocus, setIsFocus] = useState(true);
+  const navigation = useNavigation();
+  const onUsePreventRemove = useCallback(
+    ({
+      data,
+    }: {
+      data: {
+        action: Readonly<{
+          type: string;
+          payload?: object | undefined;
+          source?: string | undefined;
+          target?: string | undefined;
+        }>;
+      };
+    }) => {
+      setIsFocus(false);
+      setTimeout(() => {
+        navigation.dispatch(data.action);
+      }, 350);
+    },
+    [navigation],
+  );
+  usePreventRemove(true, onUsePreventRemove);
+
   return (
     <>
-      <Camera
-        ref={(ref) =>
-          ref === null && defaultLogger.scanQrCode.readQrCode.releaseCamera()
-        }
-        style={{ flex: 1 }}
-        resizeMode="cover"
-        scanBarcode
-        onReadCode={({ nativeEvent: { codeStringValue } }) => {
-          if (typeof codeStringValue !== 'string') {
-            return;
-          }
-          handleScanResult?.(codeStringValue);
-        }}
-        {...rest}
-      />
+      {isFocus ? (
+        <Camera
+          style={{ flex: 1 }}
+          resizeMode="cover"
+          scanBarcode
+          onReadCode={({ nativeEvent: { codeStringValue } }) => {
+            if (typeof codeStringValue !== 'string') {
+              return;
+            }
+            handleScanResult?.(codeStringValue);
+          }}
+          {...rest}
+        />
+      ) : null}
       {children}
     </>
   );
