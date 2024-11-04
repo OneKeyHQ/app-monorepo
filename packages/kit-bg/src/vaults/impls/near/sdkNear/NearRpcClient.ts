688cbbb734ed23e73c458113c6c2cb141b2cc9a3
Leon <lixiao.dev@gmail.com>
2024-11-04 15:45:07 +0800

    feat: Near custom rpc

---
+++ packages/kit-bg/src/vaults/impls/near/sdkNear/NearRpcClient.ts
@@ -0,0 +1,32 @@
+import Axios from 'axios';
+
+import { JsonRPCRequest } from '@onekeyhq/shared/src/request/JsonRPCRequest';
+import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
+
+import type { AxiosInstance } from 'axios';
+
+export class NearRpcClient {
+  readonly rpc: JsonRPCRequest;
+
+  constructor({ url }: { url: string }) {
+    this.rpc = new JsonRPCRequest(`${url}/json_rpc`);
+  }
+
+  async getBestBlock(): Promise<{ blockNumber: number; blockHash: string }> {
+    const resp = await this.rpc.call<{
+      sync_info: { latest_block_height: string; latest_block_hash: string };
+    }>('status', []);
+    return {
+      blockNumber: Number(resp.sync_info.latest_block_height),
+      blockHash: resp.sync_info.latest_block_hash,
+    };
+  }
+
+  async broadcastTransaction(rawTx: string): Promise<string> {
+    const tx = await this.rpc.call<{ transaction: { hash: string } }>(
+      'broadcast_tx_commit',
+      [rawTx],
+    );
+    return tx.transaction.hash;
+  }
+}
