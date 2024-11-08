62ef233dad87c230e93b6cb0e2b015a6a9682a8a
huhuanming <huanming@onekey.so>
2024-11-08 20:38:48 +0800

    Update PairDetailDialog.tsx

--- packages/kit/src/views/Market/components/PairDetailDialog.tsx
+++ packages/kit/src/views/Market/components/PairDetailDialog.tsx
@@ -1,13 +1,21 @@
+import { useCallback } from 'react';
+
 import BigNumber from 'bignumber.js';
-import { differenceInDays } from 'date-fns';
 import { useIntl } from 'react-intl';
 
-import { SizableText, XStack, YStack } from '@onekeyhq/components';
+import {
+  IconButton,
+  SizableText,
+  XStack,
+  YStack,
+  useDialogInstance,
+} from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+import { openUrlInApp } from '@onekeyhq/shared/src/utils/openUrlUtils';
 import type { IMarketDetailTicker } from '@onekeyhq/shared/types/market';
 
 import { MarketPoolIcon } from './MarketPoolIcon';
-import { MarketTokenAddress } from './MarketTokenAddress';
 import { PoolDetailsItem } from './PoolDetailDialog';
 
 export function PairDetailDialog({
@@ -20,18 +28,28 @@ export function PairDetailDialog({
     volume,
     bid_ask_spread_percentage: spread,
     trust_score: trustScore,
+    trade_url: tradeUrl,
   },
 }: {
   item: IMarketDetailTicker;
 }) {
   const intl = useIntl();
+  const pairName = `${base}/${target}`;
+
+  const dialog = useDialogInstance();
+  const handleOpenUrl = useCallback(async () => {
+    if (platformEnv.isNative) {
+      await dialog.close();
+    }
+    openUrlInApp(tradeUrl);
+  }, [dialog, tradeUrl]);
   return (
-    <YStack gap="$3">
+    <YStack gap="$3" pb="$5">
       <XStack gap="$4">
         <PoolDetailsItem
           title={intl.formatMessage({ id: ETranslations.global_pair })}
         >
-          {`${base} / ${target}`}
+          {pairName}
         </PoolDetailsItem>
         <PoolDetailsItem
           title={intl.formatMessage({ id: ETranslations.global_dex })}
@@ -90,6 +108,22 @@ export function PairDetailDialog({
         >
           {trustScore === 'green' ? '🟢' : ''}
         </PoolDetailsItem>
+      </XStack>
+      <XStack gap="$1.5" ai="center" pt="$2">
+        <XStack gap="$2">
+          <SizableText size="$bodyMdMedium">
+            {intl.formatMessage({ id: ETranslations.market_pair_link })}
+          </SizableText>
+          <SizableText size="$bodyMd">{pairName}</SizableText>
+        </XStack>
+        <IconButton
+          variant="tertiary"
+          color="$iconSubdued"
+          icon="OpenOutline"
+          size="small"
+          iconSize="$4"
+          onPress={handleOpenUrl}
+        />
       </XStack>
     </YStack>
   );
