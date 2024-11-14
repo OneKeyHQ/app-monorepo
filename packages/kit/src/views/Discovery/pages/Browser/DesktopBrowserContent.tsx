8f0901c86e971db215375f7f21cca5d7d9814d98
huhuanming <huanming@onekey.so>
2024-11-14 23:24:14 +0800

    Update DesktopBrowserContent.tsx

--- packages/kit/src/views/Discovery/pages/Browser/DesktopBrowserContent.tsx
+++ packages/kit/src/views/Discovery/pages/Browser/DesktopBrowserContent.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 
 import { Freeze } from 'react-freeze';
 
@@ -14,14 +14,99 @@ import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import WebContent from '../../components/WebContent/WebContent';
 import { useWebTabDataById } from '../../hooks/useWebTabs';
+import { webviewRefs } from '../../utils/explorerUtils';
+import { useDebouncedCallback } from 'use-debounce';
+
+interface IElectronWebView {
+  stopFindInPage: (text: string) => void;
+  findInPage: (
+    text: string,
+    params?: { findNext: boolean; forward: boolean },
+  ) => void;
+  addEventListener: (
+    eventName: string,
+    callback: (result: any) => void,
+  ) => void;
+}
 
 function BasicFind({ id }: { id: string }) {
+  const [matches, setMatches] = useState(0)
+  const [activeMatchOrdinal, setActiveMatchOrdinal] = useState(0)
+  const prevSearchText = useRef('');
   const handleFindPrev = useCallback(() => {
-    console.log('handleFindPrev');
-  }, []);
-  const handleFindNext = useCallback(() => {}, []);
+    const webView = webviewRefs[id]?.innerRef as unknown as IElectronWebView;
+    webView.findInPage(prevSearchText.current, {
+      findNext: false,
+      forward: false,
+    });  }, []);
+  const handleFindNext = useCallback(() => {
+    const webView = webviewRefs[id]?.innerRef as unknown as IElectronWebView;
+    if (activeMatchOrdinal === matches) {
 
-  const handleClose = useCallback(() => {}, []);
+    } else {
+      webView.findInPage(prevSearchText.current, {
+        findNext: false,
+        forward: true,
+      });
+    }
+  }, [id, activeMatchOrdinal, matches]);
+
+  const handleClose = useCallback(() => {
+  }, []);
+
+  const bindEvent = useCallback(() => {
+    const webView = webviewRefs[id]?.innerRef as unknown as IElectronWebView;
+    webView.addEventListener('found-in-page', ({ result }: { result: {
+        requestId: number,
+        matches: number,
+        selectionArea: {
+            x: number,
+            y: number,
+            width: number,
+            height: number
+        },
+        activeMatchOrdinal: number,
+        finalUpdate: boolean
+    }}) => {
+      console.log(result);
+      // webView.stopFindInPage('activateSelection');
+      setMatches(result.matches)
+      setActiveMatchOrdinal(result.activeMatchOrdinal)
+    });
+    console.log('event init!!')
+  }, [id]);
+
+  const repeatBindEvent = useCallback(() => {
+    const webView = webviewRefs[id]?.innerRef as unknown as IElectronWebView;
+    if (webView) {
+      bindEvent();
+    } else {
+      setTimeout(() => {
+        repeatBindEvent();
+      }, 3000);
+    }
+  }, [bindEvent, id]);
+
+  useEffect(() => {
+    repeatBindEvent();
+  }, [bindEvent, id, repeatBindEvent]);
+
+  const handleTextChange = useDebouncedCallback(
+    (text: string) => {
+      const webView = webviewRefs[id]?.innerRef as unknown as IElectronWebView;
+      if (!webView) {
+        return;
+      }
+      if (text.length === 0) {
+        webView.stopFindInPage('clearSelection');
+      } else {
+        webView.findInPage(text, { findNext: true, forward: false });
+      }
+      prevSearchText.current = text;
+    },
+    100,
+  );
+
   return (
     <XStack position="absolute" left="50%" top={0} zIndex={100_000}>
       <XStack
@@ -33,8 +118,10 @@ function BasicFind({ id }: { id: string }) {
         borderRadius="$3"
         borderWidth="$px"
         borderColor="$border"
+        gap="$4"
       >
         <Input
+          onChangeText={handleTextChange}
           containerProps={{
             borderWidth: 0,
             px: 0,
@@ -43,8 +130,8 @@ function BasicFind({ id }: { id: string }) {
             px: 0,
           }}
         />
-        <SizableText>0/0</SizableText>
-        <Stack width="$px" height="100%" bg="$borderStrong" mx="$4" />
+        <SizableText>{activeMatchOrdinal}/{matches}</SizableText>
+        <Stack width="$px" height="100%" bg="$borderStrong" />
         <XStack gap="$2">
           <IconButton
             variant="tertiary"
