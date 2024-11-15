835a929d1ccc350850460dea0c6991a2b89c3a46
王山栋 <jelly@onekey.so>
2024-11-15 17:01:25 +0800

    fix: approve manualSelect provider
    Signed-off-by: 王山栋 <jelly@onekey.so>

--- packages/kit/src/views/Swap/hooks/useSwapBuiltTx.ts
+++ packages/kit/src/views/Swap/hooks/useSwapBuiltTx.ts
@@ -39,6 +39,7 @@ import { useSendConfirm } from '../../../hooks/useSendConfirm';
 import {
   useSwapBuildTxFetchingAtom,
   useSwapFromTokenAmountAtom,
+  useSwapManualSelectQuoteProvidersAtom,
   useSwapQuoteCurrentSelectAtom,
   useSwapQuoteEventTotalCountAtom,
   useSwapQuoteListAtom,
@@ -67,6 +68,8 @@ export function useSwapBuildTx() {
   const [, setSwapShouldRefreshQuote] = useSwapShouldRefreshQuoteAtom();
   const swapFromAddressInfo = useSwapAddressInfo(ESwapDirectionType.FROM);
   const swapToAddressInfo = useSwapAddressInfo(ESwapDirectionType.TO);
+  const [, setSwapManualSelectQuoteProviders] =
+    useSwapManualSelectQuoteProvidersAtom();
   const { generateSwapHistoryItem } = useSwapTxHistoryActions();
   const [{ isFirstTimeSwap }, setSettings] = useSettingsPersistAtom();
   const { navigationToSendConfirm } = useSendConfirm({
@@ -149,6 +152,15 @@ export function useSwapBuildTx() {
             tx: txId,
           });
         }
+        console.log(
+          'swap__handleApproveTxSuccess',
+          data[0].approveInfo?.swapApproveRes,
+        );
+        if (data[0].approveInfo?.swapApproveRes) {
+          setSwapManualSelectQuoteProviders(
+            data[0].approveInfo?.swapApproveRes,
+          );
+        }
         setInAppNotificationAtom((prev) => {
           if (prev.swapApprovingTransaction) {
             return {
@@ -163,7 +175,11 @@ export function useSwapBuildTx() {
         });
       }
     },
-    [inAppNotificationAtom.swapApprovingTransaction, setInAppNotificationAtom],
+    [
+      inAppNotificationAtom.swapApprovingTransaction,
+      setInAppNotificationAtom,
+      setSwapManualSelectQuoteProviders,
+    ],
   );
 
   const handleTxFail = useCallback(() => {
@@ -555,6 +571,7 @@ export function useSwapBuildTx() {
                 address: fromToken.contractAddress,
                 name: fromToken.name ?? fromToken.symbol,
               },
+              swapApproveRes: selectQuote,
             };
             approvesInfo = [approveInfo];
             if (resetApproveValue && amount === swapApproveResetValue) {
@@ -569,6 +586,7 @@ export function useSwapBuildTx() {
                   address: fromToken.contractAddress,
                   name: fromToken.name ?? fromToken.symbol,
                 },
+                swapApproveRes: selectQuote,
               };
               approvesInfo = [...approvesInfo, approveResetInfo];
             }
@@ -675,6 +693,7 @@ export function useSwapBuildTx() {
                 address: fromToken.contractAddress,
                 name: fromToken.name ?? fromToken.symbol,
               },
+              swapApproveRes: selectQuote,
             };
             setInAppNotificationAtom((pre) => ({
               ...pre,
@@ -702,11 +721,7 @@ export function useSwapBuildTx() {
       }
     },
     [
-      selectQuote?.allowanceResult,
-      selectQuote?.info.provider,
-      selectQuote?.info.providerName,
-      selectQuote?.fee?.percentageFee,
-      selectQuote?.routesData,
+      selectQuote,
       fromToken,
       toToken,
       swapFromAddressInfo.networkId,
@@ -715,14 +730,14 @@ export function useSwapBuildTx() {
       settingsPersistAtom.swapBatchApproveAndSwap,
       setSwapBuildTxFetching,
       createBuildTx,
-      navigationToSendConfirm,
-      handleBuildTxSuccess,
-      cancelBuildTx,
       syncRecentTokenPairs,
       slippageItem.value,
       isFirstTimeSwap,
       pageType,
       setSettings,
+      navigationToSendConfirm,
+      cancelBuildTx,
+      handleBuildTxSuccess,
       setSwapShouldRefreshQuote,
       setInAppNotificationAtom,
       handleApproveTxSuccess,
