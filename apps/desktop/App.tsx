49edd4fd74ef6679ab05f618103e7ab91cbb0121
huhuanming <huanming@onekey.so>
2024-11-19 00:22:10 +0800

    add electron sentry

--- apps/desktop/App.tsx
+++ apps/desktop/App.tsx
@@ -4,4 +4,10 @@ import '@onekeyhq/shared/src/web/index.css';
 
 import { KitProvider } from '@onekeyhq/kit';
 
+import * as Sentry from '@sentry/electron';
+
+Sentry.init({
+  dsn: 'https://05ed77019985fb3c81f0bcbdbe1774cd@o4508208799809536.ingest.de.sentry.io/4508320047890512',
+});
+
 export default KitProvider;
