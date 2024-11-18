b5343154a80c04935ed137ec939b0a8580833f28
huhuanming <huanming@onekey.so>
2024-11-18 20:18:43 +0800

    Update index.tsx

--- packages/kit/src/views/Setting/pages/List/index.tsx
+++ packages/kit/src/views/Setting/pages/List/index.tsx
@@ -113,7 +113,7 @@ const SocialButtonGroup = () => {
       </XStack>
       <YStack
         jc="center"
-        py="$4"
+        p="$4"
         ai="center"
         userSelect="none"
         testID="setting-version"
@@ -121,8 +121,8 @@ const SocialButtonGroup = () => {
         <SizableText color="$textSubdued" onPress={handlePress}>
           {versionString}
         </SizableText>
-        {appUpdateInfo.status === EAppUpdateStatus.done ? (
-          <SizableText color="$textSubdued">
+        {appUpdateInfo.status !== EAppUpdateStatus.done ? (
+          <SizableText color="$textSubdued" textAlign="center">
             {intl.formatMessage({ id: ETranslations.update_app_up_to_date })}
           </SizableText>
         ) : null}
