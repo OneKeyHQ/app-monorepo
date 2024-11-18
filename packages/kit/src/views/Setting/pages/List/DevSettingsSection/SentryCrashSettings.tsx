4512d1f7c3f9cf03b49512a4103318327a0bc93d
huhuanming <huanming@onekey.so>
2024-11-19 00:06:35 +0800

    Update SentryCrashSettings.tsx

--- packages/kit/src/views/Setting/pages/List/DevSettingsSection/SentryCrashSettings.tsx
+++ packages/kit/src/views/Setting/pages/List/DevSettingsSection/SentryCrashSettings.tsx
@@ -1,4 +1,4 @@
-import { captureException } from '@sentry/react-native';
+import { captureException, nativeCrash } from '@sentry/react-native';
 
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
@@ -7,12 +7,21 @@ import { SectionPressItem } from './SectionPressItem';
 export function SentryCrashSettings() {
   if (platformEnv.isNative) {
     return (
-      <SectionPressItem
-        title="Sentry Crash Test"
-        onPress={() => {
-          captureException(new Error('First error'));
-        }}
-      />
+      <>
+        <SectionPressItem
+          title="Sentry Crash Test"
+          onPress={() => {
+            captureException(new Error('First error'));
+          }}
+        />
+
+        <SectionPressItem
+          title="Sentry Native Crash"
+          onPress={() => {
+            nativeCrash();
+          }}
+        />
+      </>
     );
   }
   return null;
