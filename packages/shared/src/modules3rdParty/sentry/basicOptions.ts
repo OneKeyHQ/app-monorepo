50946d5654242d9157f3d91a538123de9a827333
huhuanming <huanming@onekey.so>
2024-11-20 01:47:05 +0800

    Update basicOptions.ts

--- packages/shared/src/modules3rdParty/sentry/basicOptions.ts
+++ packages/shared/src/modules3rdParty/sentry/basicOptions.ts
@@ -3,6 +3,32 @@ import type { BrowserOptions } from '@sentry/browser';
 export const basicOptions: BrowserOptions = {
   enabled: process.env.NODE_ENV === 'production',
   maxBreadcrumbs: 100,
+  beforeSend: (event, hint) => {
+    const error = hint.originalException as { message?: string };
+    if (error && error.message) {
+      const words = error.message.split(' ');
+      if (words.length < 13) {
+        return event;
+      }
+      for (let index = 0; index < words.length; index += 1) {
+        const word = words[index];
+        if (word.length < 20) {
+          return event;
+        }
+      }
+      if (event.exception?.values) {
+        for (let index = 0; index < event.exception.values.length; index += 1) {
+          event.exception.values[index].value = event.exception.values[
+            index
+          ].value?.slice(
+            0,
+            (event.exception.values[index].value?.length || 0) / 2,
+          );
+        }
+      }
+    }
+    return event;
+  },
 };
 
 export const buildOptions = (Sentry: typeof import('@sentry/react')) => ({
