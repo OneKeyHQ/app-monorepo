7cf047640780c4eb27d7af8261ca71228c4da0b7
huhuanming <huanming@onekey.so>
2024-11-22 14:17:10 +0800

    fix: fix the issue where the tab route does not switch when opening the market deeplink. (#6259)

--- packages/kit/src/views/Market/marketUtils.ts
+++ packages/kit/src/views/Market/marketUtils.ts
@@ -1,6 +1,7 @@
 import { WEB_APP_URL } from '@onekeyhq/shared/src/config/appConfig';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { ETabMarketRoutes, ETabRoutes } from '@onekeyhq/shared/src/routes';
+import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
 
 import type { IAppNavigation } from '../../hooks/useAppNavigation';
 
@@ -22,11 +23,11 @@ export const marketNavigation = {
       coinGeckoId: string;
     },
   ) {
-    navigation.navigate(ETabRoutes.Market, {
-      screen: ETabMarketRoutes.MarketDetail,
-      params: {
-        token: coinGeckoId,
-      },
+    await timerUtils.wait(80);
+    navigation.switchTab(ETabRoutes.Market);
+    await timerUtils.wait(80);
+    navigation.push(ETabMarketRoutes.MarketDetail, {
+      token: coinGeckoId,
     });
   },
 };
