750cfdfffca5a5919cbf8e58e84b264f23b8962e
huhuanming <huanming@onekey.so>
2024-11-29 16:09:22 +0800

    Update WebView.native.tsx

--- packages/kit/src/components/TradingView/WebView.native.tsx
+++ packages/kit/src/components/TradingView/WebView.native.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import { WebView as NativeWebView } from 'react-native-webview';
 
 import { Stack } from '@onekeyhq/components';
@@ -7,17 +9,25 @@ import type { ViewStyle } from 'react-native';
 export function WebView({
   uri,
   style,
+  onLoadEnd,
   ...props
 }: {
   uri: string;
   style: ViewStyle;
+  onLoadEnd: () => void;
 }) {
+  const handleLoadedEnd = useCallback(() => {
+    setTimeout(() => {
+      onLoadEnd();
+    }, 800);
+  }, [onLoadEnd]);
   return (
     <Stack style={style as any}>
       <NativeWebView
         source={{
           uri,
         }}
+        onLoadEnd={handleLoadedEnd}
         {...props}
       />
     </Stack>
