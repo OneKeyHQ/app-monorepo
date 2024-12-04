51ae703d7db7bfc5cef77c2f61c2b7c0eefa9bd4
huhuanming <huanming@onekey.so>
2024-12-04 15:13:32 +0800

    Update Image.tsx

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/Image.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/Image.tsx
@@ -165,7 +165,7 @@ const ImageGallery = () => (
         element: (
           <YStack gap="$4">
             <Image height="$10" width="$10">
-              <Image.Source src="https://onekey-asset.com/assets/btc/bt" />
+              <Image.Source src="https://uni.onekey-asset.com/static/chain/btc.pn" />
               <Image.Skeleton />
             </Image>
             <Image
@@ -178,7 +178,7 @@ const ImageGallery = () => (
             >
               <Image.Source
                 source={{
-                  uri: 'https://onekey-asset.com/assets/btc/bt',
+                  uri: 'https://uni.onekey-asset.com/static/chain/btc.pn',
                 }}
               />
               <Image.Fallback>
@@ -202,7 +202,7 @@ const ImageGallery = () => (
               <Image.Source
                 delayMs={10 * 1000}
                 source={{
-                  uri: 'https://onekey-asset.com/assets/btc/bt',
+                  uri: 'https://uni.onekey-asset.com/static/chain/btc.pn',
                 }}
               />
               <Image.Fallback>
