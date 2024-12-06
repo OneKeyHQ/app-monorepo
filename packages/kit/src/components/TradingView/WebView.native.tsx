85bc467a965389ded87cdb2d39479f5d98ab7c21
huhuanming <huanming@onekey.so>
2024-12-06 18:52:08 +0800

    Update WebView.native.tsx

--- packages/kit/src/components/TradingView/WebView.native.tsx
+++ packages/kit/src/components/TradingView/WebView.native.tsx
@@ -1,10 +1,12 @@
 import { useCallback, useMemo } from 'react';
 
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { WebView as NativeWebView } from 'react-native-webview';
 
 import { Stack } from '@onekeyhq/components';
 
 import type { ViewStyle } from 'react-native';
+import { ShouldStartLoadRequestEvent } from 'react-native-webview/lib/RNCWebViewNativeComponent';
 
 export function WebView({
   tradingViewProps: { uri, injectedJavaScript },
@@ -24,14 +26,21 @@ export function WebView({
       onLoadEnd();
     }, 500);
   }, [onLoadEnd]);
+
+  const onMessage = useCallback(() => {
+  }, []);
+  const onShouldStartLoadWithRequest = useCallback((event: ShouldStartLoadRequestEvent) => {
+    return !!event.mainDocumentURL?.startsWith('https://www.tradingview-widget.com/embed-widget/advanced-chart');
+  }, [])
   return uri ? (
     <Stack style={style as any}>
       <NativeWebView
         javaScriptEnabled
         domStorageEnabled
-        onMessage={(event) => {}}
-        webviewDebuggingEnabled
+        onMessage={onMessage}
+        webviewDebuggingEnabled={platformEnv.isDev}
         injectedJavaScript={injectedJavaScript}
+        onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
         source={{
           uri,
         }}
