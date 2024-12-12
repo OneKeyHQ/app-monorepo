aff1c424102861e00aa8c4af93c967ccda35afbd
huhuanming <huanming@onekey.so>
2024-12-12 23:33:54 +0800

    Update MarketTokenPrice.tsx

--- packages/kit/src/views/Market/components/MarketTokenPrice.tsx
+++ packages/kit/src/views/Market/components/MarketTokenPrice.tsx
@@ -55,7 +55,7 @@ class MarketTokenPriceEvent {
   ) {
     const cacheKey = this.buildKey(tokenName, tokenSymbol);
     const listeners = this.priceChangedListenerMap.get(cacheKey) || [];
-    const throttleCallback = throttle(callback, 50);
+    const throttleCallback = throttle(callback, 200);
     listeners.push(throttleCallback);
     this.priceChangedListenerMap.set(cacheKey, listeners);
     return () => {
