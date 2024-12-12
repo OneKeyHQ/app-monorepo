18b905693c29468d171db85d746fa8d23d8a8761
huhuanming <huanming@onekey.so>
2024-12-12 15:25:22 +0800

    Update marketProvider.constants.ts

--- packages/shared/types/market/marketProvider.constants.ts
+++ packages/shared/types/market/marketProvider.constants.ts
@@ -1,13 +1,11 @@
 import { memoizee } from '@onekeyhq/shared/src/utils/cacheUtils';
 
 import { getNetworkIdsMap } from '../../src/config/networkIds';
-import { ESwapTabSwitchType } from '../swap/types';
 
 const getSwapTokenMap = memoizee(
   (): Record<
     string,
     {
-      switchType: ESwapTabSwitchType;
       contractAddress: string;
       default: {
         contractAddress: string;
@@ -18,22 +16,11 @@ const getSwapTokenMap = memoizee(
         isNative: boolean;
         networkLogoURI?: string;
       };
-      target: {
-        networkId: string;
-        contractAddress: string;
-        name: string;
-        symbol: string;
-        decimals: number;
-        isNative: boolean;
-        networkLogoURI: string;
-        isPopular?: boolean;
-      };
     }
   > => {
     const networkIdsMap = getNetworkIdsMap();
     return {
       [networkIdsMap.btc]: {
-        switchType: ESwapTabSwitchType.BRIDGE,
         contractAddress: '',
         default: {
           'contractAddress': '',
@@ -43,18 +30,8 @@ const getSwapTokenMap = memoizee(
           'decimals': 8,
           'isNative': true,
         },
-        target: {
-          'networkId': 'evm--1',
-          'contractAddress': '',
-          'name': 'Ethereum',
-          'symbol': 'ETH',
-          'decimals': 18,
-          'isNative': true,
-          'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/eth.png',
-        },
       },
       [networkIdsMap.eth]: {
-        switchType: ESwapTabSwitchType.SWAP,
         contractAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
         default: {
           'networkId': 'evm--1',
@@ -65,19 +42,8 @@ const getSwapTokenMap = memoizee(
           'isNative': true,
           'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/eth.png',
         },
-        target: {
-          'networkId': 'evm--1',
-          'contractAddress': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-          'name': 'USD Coin',
-          'symbol': 'USDC',
-          'decimals': 6,
-          'isNative': false,
-          'isPopular': true,
-          'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/eth.png',
-        },
       },
       [networkIdsMap.sol]: {
-        switchType: ESwapTabSwitchType.SWAP,
         contractAddress: 'So11111111111111111111111111111111111111112',
         default: {
           'networkId': 'sol--101',
@@ -87,18 +53,8 @@ const getSwapTokenMap = memoizee(
           'decimals': 9,
           'isNative': true,
         },
-        target: {
-          'networkId': 'sol--101',
-          'contractAddress': 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
-          'name': 'USDC',
-          'symbol': 'USDC',
-          'decimals': 6,
-          'isNative': false,
-          'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/sol.png',
-        },
       },
       [networkIdsMap.bsc]: {
-        switchType: ESwapTabSwitchType.SWAP,
         contractAddress: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
         default: {
           'networkId': 'evm--56',
@@ -108,18 +64,8 @@ const getSwapTokenMap = memoizee(
           'decimals': 18,
           'isNative': true,
         },
-        target: {
-          'networkId': 'evm--56',
-          'contractAddress': '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
-          'name': 'USD Coin',
-          'symbol': 'USDC',
-          'decimals': 18,
-          'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/bsc.png',
-          'isNative': false,
-        },
       },
       [networkIdsMap.polygon]: {
-        switchType: ESwapTabSwitchType.SWAP,
         contractAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
         default: {
           'contractAddress': '',
@@ -129,19 +75,8 @@ const getSwapTokenMap = memoizee(
           'decimals': 18,
           'isNative': true,
         },
-        target: {
-          'contractAddress': '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359',
-          'networkId': 'evm--137',
-          'name': 'USD Coin',
-          'symbol': 'USDC',
-          'decimals': 6,
-          'networkLogoURI':
-            'https://uni.onekey-asset.com/static/chain/polygon.png',
-          'isNative': false,
-        },
       },
       [networkIdsMap.apt]: {
-        switchType: ESwapTabSwitchType.BRIDGE,
         contractAddress: '0x1::aptos_coin::AptosCoin',
         default: {
           'contractAddress': '',
@@ -150,15 +85,6 @@ const getSwapTokenMap = memoizee(
           'symbol': 'APT',
           'decimals': 8,
           'isNative': true,
-        },
-        target: {
-          'networkId': 'evm--1',
-          'contractAddress': '',
-          'name': 'Ethereum',
-          'symbol': 'ETH',
-          'decimals': 18,
-          'isNative': true,
-          'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/eth.png',
         },
       },
     };
@@ -193,7 +119,6 @@ export function getImportFromToken({
     return {
       isNative,
       importFromToken: isNative ? item.target : item.default,
-      swapTabSwitchType: item.switchType,
     };
   }
   return undefined;
