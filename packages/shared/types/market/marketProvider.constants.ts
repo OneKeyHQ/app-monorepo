4b0fa9d4ea7508a5be00249e2c6b5f3f727871ac
huhuanming <huanming@onekey.so>
2024-12-12 16:25:13 +0800

    Update marketProvider.constants.ts

--- packages/shared/types/market/marketProvider.constants.ts
+++ packages/shared/types/market/marketProvider.constants.ts
@@ -7,85 +7,42 @@ const getSwapTokenMap = memoizee(
     string,
     {
       contractAddress: string;
-      default: {
-        contractAddress: string;
-        networkId: string;
-        name: string;
-        symbol: string;
-        decimals: number;
-        isNative: boolean;
-        networkLogoURI?: string;
-      };
+      symbol: string;
     }
   > => {
     const networkIdsMap = getNetworkIdsMap();
     return {
       [networkIdsMap.btc]: {
         contractAddress: '',
-        default: {
-          'contractAddress': '',
-          'networkId': 'btc--0',
-          'name': 'Bitcoin',
-          'symbol': 'BTC',
-          'decimals': 8,
-          'isNative': true,
-        },
+        symbol: 'BTC',
       },
       [networkIdsMap.eth]: {
         contractAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-        default: {
-          'networkId': 'evm--1',
-          'contractAddress': '',
-          'name': 'Ethereum',
-          'symbol': 'ETH',
-          'decimals': 18,
-          'isNative': true,
-          'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/eth.png',
-        },
+        symbol: 'ETH',
       },
       [networkIdsMap.sol]: {
         contractAddress: 'So11111111111111111111111111111111111111112',
-        default: {
-          'networkId': 'sol--101',
-          'contractAddress': '',
-          'name': 'Solana',
-          'symbol': 'SOL',
-          'decimals': 9,
-          'isNative': true,
-        },
+        symbol: 'SOL',
       },
       [networkIdsMap.bsc]: {
         contractAddress: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
-        default: {
-          'networkId': 'evm--56',
-          'contractAddress': '',
-          'name': 'BNB',
-          'symbol': 'BNB',
-          'decimals': 18,
-          'isNative': true,
-        },
+        symbol: 'BNB',
       },
       [networkIdsMap.polygon]: {
         contractAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
-        default: {
-          'contractAddress': '',
-          'networkId': 'evm--137',
-          'name': 'Polygon',
-          'symbol': 'POL',
-          'decimals': 18,
-          'isNative': true,
-        },
+        symbol: 'POL',
+      },
+      [networkIdsMap.avalanche]: {
+        contractAddress: '0x0000000000000000000000000000000000000000',
+        symbol: 'AVAX',
       },
       [networkIdsMap.apt]: {
         contractAddress: '0x1::aptos_coin::AptosCoin',
-        default: {
-          'contractAddress': '',
-          'networkId': 'aptos--1',
-          'name': 'Aptos Coin',
-          'symbol': 'APT',
-          'decimals': 8,
-          'isNative': true,
-        },
+        symbol: 'APT',
+      },
+      [networkIdsMap.kaspa]: {
+        contractAddress: '',
+        symbol: 'APT',
       },
     };
   },
@@ -114,11 +71,10 @@ export function getImportFromToken({
   const item = map[networkId];
   if (item) {
     const isNative =
-      tokenSymbol.toUpperCase() === item.default.symbol &&
+      tokenSymbol.toUpperCase() === item.symbol &&
       item.contractAddress === contractAddress;
     return {
       isNative,
-      importFromToken: isNative ? item.target : item.default,
     };
   }
   return undefined;
