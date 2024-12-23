4b27bb33697c6ccc3cfca1e9fc9cc93d2d1330a5
huhuanming <huanming@onekey.so>
2024-12-24 00:50:32 +0800

    Update index.tsx

--- packages/components/src/composite/Banner/index.tsx
+++ packages/components/src/composite/Banner/index.tsx
@@ -43,9 +43,7 @@ function BannerItem<T extends IBannerData>({
   const item = useProps(rawItem, {
     resolveValues: 'value',
   }) as T;
-  const textStyle = useStyle(item.titleTextProps || {}, {
-    resolveValues: 'auto',
-  });
+  console.log('item.titleTextProps', item.titleTextProps);
   const onItemPress = useCallback(() => {
     onPress(item);
   }, [item, onPress]);
@@ -79,7 +77,7 @@ function BannerItem<T extends IBannerData>({
               key={index}
               color={item.theme === 'dark' ? '$textDark' : '$textLight'}
               size="$headingLg"
-              {...textStyle}
+              {...item.titleTextProps}
             >
               {text}
             </SizableText>
