31099458f1b030b85154f02acdc39d2edbdcf8b8
huhuanming <huanming@onekey.so>
2024-12-26 17:23:00 +0800

    fix: fix the issue where the account selector does not work in the market module.

--- packages/kit/src/views/Market/components/tradeHook.tsx
+++ packages/kit/src/views/Market/components/tradeHook.tsx
@@ -3,7 +3,8 @@ import { useCallback, useMemo, useRef, useState } from 'react';
 import { useIntl } from 'react-intl';
 
 import type { IPageNavigationProp } from '@onekeyhq/components';
-import { Dialog, SizableText } from '@onekeyhq/components';
+import { Button, Dialog, SizableText, Toast } from '@onekeyhq/components';
+import { useAccountSelectorTrigger } from '@onekeyhq/kit/src/components/AccountSelector/hooks/useAccountSelectorTrigger';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import { defaultLogger } from '@onekeyhq/shared/src/logger/logger';
 import {
@@ -12,8 +13,11 @@ import {
 } from '@onekeyhq/shared/src/routes';
 import { EModalRoutes } from '@onekeyhq/shared/src/routes/modal';
 import { EModalSwapRoutes } from '@onekeyhq/shared/src/routes/swap';
+import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
+import networkUtils from '@onekeyhq/shared/src/utils/networkUtils';
 import { openUrlExternal } from '@onekeyhq/shared/src/utils/openUrlUtils';
 import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
+import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
 import { isSupportStaking } from '@onekeyhq/shared/types/earn/earnProvider.constants';
 import type { IFiatCryptoType } from '@onekeyhq/shared/types/fiatCrypto';
 import type {
@@ -24,6 +28,8 @@ import { getNetworkIdBySymbol } from '@onekeyhq/shared/types/market/marketProvid
 import { ESwapTabSwitchType } from '@onekeyhq/shared/types/swap/types';
 
 import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
+import { AccountSelectorProviderMirror } from '../../../components/AccountSelector/AccountSelectorProvider';
+import { useAccountSelectorCreateAddress } from '../../../components/AccountSelector/hooks/useAccountSelectorCreateAddress';
 import useAppNavigation from '../../../hooks/useAppNavigation';
 import { useActiveAccount } from '../../../states/jotai/contexts/accountSelector';
 
@@ -56,6 +62,77 @@ export const useMarketTradeNetworkId = (
     return onekeyNetworkId ?? getNetworkIdBySymbol(symbol);
   }, [network, symbol]);
 
+function BasicCreateAddressDialogContent({
+  onCreate,
+  networkId,
+  indexedAccountId,
+}: {
+  onCreate: () => void;
+  networkId: string;
+  indexedAccountId?: string;
+}) {
+  const intl = useIntl();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    activeAccount: { wallet },
+  } = useActiveAccount({ num: 0 });
+  const { createAddress } = useAccountSelectorCreateAddress();
+  const handleCreateAddress = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const deriveType =
+        await backgroundApiProxy.serviceNetwork.getGlobalDeriveTypeOfNetwork({
+          networkId,
+        });
+      await createAddress({
+        num: 0,
+        account: {
+          walletId: wallet?.id,
+          networkId,
+          indexedAccountId,
+          deriveType: networkUtils.isBTCNetwork(networkId)
+            ? 'BIP86'
+            : deriveType,
+        },
+        selectAfterCreate: false,
+      });
+      onCreate();
+    } finally {
+      setIsLoading(false);
+    }
+  }, [networkId, createAddress, wallet?.id, indexedAccountId, onCreate]);
+  return (
+    <Button onPress={handleCreateAddress} loading={isLoading}>
+      {intl.formatMessage({ id: ETranslations.global_create })}
+    </Button>
+  );
+}
+function CreateAddressDialogContent({
+  onCreate,
+  networkId,
+  indexedAccountId,
+}: {
+  onCreate: () => void;
+  networkId: string;
+  indexedAccountId?: string;
+}) {
+  return (
+    <AccountSelectorProviderMirror
+      config={{
+        sceneName: EAccountSelectorSceneName.home,
+        sceneUrl: '',
+      }}
+      enabledNum={[0]}
+    >
+      <BasicCreateAddressDialogContent
+        onCreate={onCreate}
+        networkId={networkId}
+        indexedAccountId={indexedAccountId}
+      />
+    </AccountSelectorProviderMirror>
+  );
+}
+
 export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
   const { symbol = '', name } = token || {};
   const intl = useIntl();
@@ -66,6 +143,11 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
     useAppNavigation<IPageNavigationProp<IModalSwapParamList>>();
 
   const { activeAccount } = useActiveAccount({ num: 0 });
+
+  const { showAccountSelector } = useAccountSelectorTrigger({
+    num: 0,
+    linkNetwork: true,
+  });
 
   const contractAddress = useMemo(
     () => network?.contract_address ?? '',
@@ -79,7 +161,7 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
     (action: 'buy' | 'sell' | 'trade', showDialog = true) => {
       defaultLogger.market.token.unsupportedToken({ name: symbol, action });
       if (showDialog) {
-        Dialog.confirm({
+        Dialog.show({
           title: intl.formatMessage({
             id: ETranslations.earn_unsupported_token,
           }),
@@ -101,15 +183,125 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
     [intl, symbol],
   );
 
+  const showSwitchAccountSelector = useCallback(() => {
+    Dialog.confirm({
+      icon: 'ErrorOutline',
+      tone: 'warning',
+      title: intl.formatMessage(
+        {
+          id: ETranslations.wallet_unsupported_network_title,
+        },
+        {
+          network: symbol.toUpperCase(),
+        },
+      ),
+      description: intl.formatMessage({
+        id: ETranslations.global_switch_supported_accounts_wallets,
+      }),
+      onConfirm: showAccountSelector,
+      onConfirmText: intl.formatMessage({
+        id: ETranslations.global_switch,
+      }),
+    });
+  }, [intl, showAccountSelector, symbol]);
+
+  const createAccountIfNotExists = useCallback(async () => {
+    if (!networkId) {
+      throw new Error(`cannot find NetworkId ${networkId || ''}`);
+    }
+    try {
+      if (
+        accountUtils.isWatchingAccount({
+          accountId: activeAccount.account?.id ?? '',
+        })
+      ) {
+        showSwitchAccountSelector();
+        return undefined;
+      }
+      if (activeAccount.account?.indexedAccountId) {
+        const deriveType =
+          await backgroundApiProxy.serviceNetwork.getGlobalDeriveTypeOfNetwork({
+            networkId,
+          });
+        await backgroundApiProxy.serviceAccount.getNetworkAccount({
+          accountId: undefined,
+          indexedAccountId: activeAccount.account?.indexedAccountId,
+          networkId,
+          deriveType,
+        });
+      } else if (
+        activeAccount.network?.id &&
+        activeAccount.network?.id !== networkId
+      ) {
+        showSwitchAccountSelector();
+        return undefined;
+      }
+    } catch (error) {
+      const isCreated = await new Promise<boolean>((resolve) => {
+        const dialog = Dialog.show({
+          title: intl.formatMessage({
+            id: ETranslations.wallet_no_address,
+          }),
+          icon: 'WalletCryptoOutline',
+          description: intl.formatMessage(
+            {
+              id: ETranslations.global_private_key_error,
+            },
+            {
+              network: symbol.toUpperCase(),
+              path: networkUtils.isBTCNetwork(networkId) ? '(Taproot)' : '',
+            },
+          ),
+          showFooter: false,
+          onClose: (extra) => {
+            if (extra?.flag !== 'created') {
+              resolve(false);
+            }
+          },
+          renderContent: (
+            <CreateAddressDialogContent
+              onCreate={async () => {
+                resolve(true);
+                await dialog.close({ flag: 'created' });
+                Toast.success({
+                  title: intl.formatMessage({
+                    id: ETranslations.swap_page_toast_address_generated,
+                  }),
+                });
+              }}
+              networkId={networkId}
+              indexedAccountId={activeAccount.account?.indexedAccountId}
+            />
+          ),
+        });
+      });
+      if (!isCreated) {
+        return undefined;
+      }
+    }
+
+    return {
+      activeAccount,
+      networkId,
+    };
+  }, [activeAccount, intl, networkId, showSwitchAccountSelector, symbol]);
+
   const handleBuyOrSell = useCallback(
     async (type: IFiatCryptoType) => {
-      if (!activeAccount.account || !networkId) {
+      const checkResult = await createAccountIfNotExists();
+      if (!checkResult) {
+        return;
+      }
+      const { activeAccount: currentAccount, networkId: currentNetworkId } =
+        checkResult;
+
+      if (!currentNetworkId) {
         return;
       }
 
       const isSupported =
         await backgroundApiProxy.serviceFiatCrypto.isTokenSupported({
-          networkId,
+          networkId: currentNetworkId,
           tokenAddress: realContractAddress,
           type,
         });
@@ -119,22 +311,11 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
         return;
       }
 
-      const deriveType =
-        await backgroundApiProxy.serviceNetwork.getGlobalDeriveTypeOfNetwork({
-          networkId,
-        });
-      const dbAccount =
-        await backgroundApiProxy.serviceAccount.getNetworkAccount({
-          accountId: undefined,
-          indexedAccountId: activeAccount.account.indexedAccountId,
-          networkId,
-          deriveType,
-        });
       const { url, build } =
         await backgroundApiProxy.serviceFiatCrypto.generateWidgetUrl({
-          networkId,
+          networkId: currentNetworkId,
           tokenAddress: realContractAddress,
-          accountId: dbAccount.id,
+          accountId: currentAccount.dbAccount?.id,
           type,
         });
       if (!url || !build) {
@@ -143,16 +324,19 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
       }
       openUrlExternal(url);
     },
-    [
-      activeAccount.account,
-      networkId,
-      realContractAddress,
-      remindUnsupportedToken,
-    ],
+    [createAccountIfNotExists, realContractAddress, remindUnsupportedToken],
   );
 
   const handleSwap = useCallback(
     async (mode?: 'modal' | 'button') => {
+      const checkResult = await createAccountIfNotExists();
+      if (!checkResult) {
+        return;
+      }
+      const { networkId: currentNetworkId } = checkResult;
+      if (!currentNetworkId) {
+        return;
+      }
       const navigateToSwapPage = (
         params: IModalSwapParamList[EModalSwapRoutes.SwapMainLand],
       ) => {
@@ -165,7 +349,7 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
           });
         }
       };
-      if (!networkId) {
+      if (!currentNetworkId) {
         remindUnsupportedToken('trade', false);
         navigateToSwapPage({
           importNetworkId: 'unknown',
@@ -174,26 +358,26 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
       }
       const { isSupportSwap, isSupportCrossChain } =
         await backgroundApiProxy.serviceSwap.checkSupportSwap({
-          networkId,
+          networkId: currentNetworkId,
           contractAddress: isNative ? realContractAddress : contractAddress,
         });
 
       if (!isSupportSwap && !isSupportCrossChain) {
         remindUnsupportedToken('trade', false);
         navigateToSwapPage({
-          importNetworkId: networkId,
+          importNetworkId: currentNetworkId,
         });
         return;
       }
       const onekeyNetwork = await backgroundApiProxy.serviceNetwork.getNetwork({
-        networkId,
+        networkId: currentNetworkId,
       });
       navigateToSwapPage({
         importFromToken: {
           ...onekeyNetwork,
           logoURI: isNative ? onekeyNetwork.logoURI : undefined,
           contractAddress: realContractAddress,
-          networkId,
+          networkId: currentNetworkId,
           isNative,
           networkLogoURI: onekeyNetwork.logoURI,
           symbol: symbol.toUpperCase(),
@@ -206,35 +390,35 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
     },
     [
       contractAddress,
+      createAccountIfNotExists,
       isNative,
       name,
       navigation,
-      networkId,
       realContractAddress,
       remindUnsupportedToken,
       symbol,
     ],
   );
 
-  const handleStaking = useCallback(() => {
-    if (networkId && activeAccount.account) {
+  const handleStaking = useCallback(async () => {
+    const checkResult = await createAccountIfNotExists();
+    if (!checkResult) {
+      return;
+    }
+    const { activeAccount: currentAccount, networkId: currentNetworkId } =
+      checkResult;
+    if (currentNetworkId && currentAccount.account) {
       navigation.pushModal(EModalRoutes.StakingModal, {
         screen: EModalStakingRoutes.AssetProtocolList,
         params: {
-          networkId,
-          accountId: activeAccount.account?.id,
-          indexedAccountId: activeAccount.indexedAccount?.id,
+          networkId: currentNetworkId,
+          accountId: currentAccount.account?.id,
+          indexedAccountId: currentAccount.indexedAccount?.id,
           symbol,
         },
       });
     }
-  }, [
-    activeAccount.account,
-    activeAccount.indexedAccount,
-    navigation,
-    networkId,
-    symbol,
-  ]);
+  }, [createAccountIfNotExists, navigation, symbol]);
   const canStaking = useMemo(() => isSupportStaking(symbol), [symbol]);
 
   return useMemo(
@@ -243,9 +427,16 @@ export const useMarketTradeActions = (token: IMarketTokenDetail | null) => {
       onStaking: handleStaking,
       onBuy: () => handleBuyOrSell('buy'),
       onSell: () => handleBuyOrSell('sell'),
+      createAccountIfNotExists,
       canStaking,
     }),
-    [canStaking, handleBuyOrSell, handleStaking, handleSwap],
+    [
+      canStaking,
+      createAccountIfNotExists,
+      handleBuyOrSell,
+      handleStaking,
+      handleSwap,
+    ],
   );
 };
 
@@ -258,24 +449,32 @@ export const useLazyMarketTradeActions = (coinGeckoId: string) => {
         coinGeckoId,
       );
     setToken(response);
+    return response;
   }, [coinGeckoId]);
 
   const actions = useMarketTradeActions(token);
   const actionsRef = useRef(actions);
   actionsRef.current = actions;
+  const navigation =
+    useAppNavigation<IPageNavigationProp<IModalSwapParamList>>();
   const compose = useCallback(
     async (actionName: IActionName) => {
       await fetchMarketTokenDetail();
       // wait for token detail loaded and actionsRef updated
       await timerUtils.wait(80);
+      const result = await actionsRef.current.createAccountIfNotExists();
+      if (!result) {
+        if (actionName === 'onSwap') {
+          navigation.pop();
+        }
+        return;
+      }
       await actionsRef.current[actionName]('modal');
     },
-    [fetchMarketTokenDetail],
+    [fetchMarketTokenDetail, navigation],
   );
 
-  const navigation =
-    useAppNavigation<IPageNavigationProp<IModalSwapParamList>>();
-  const handleSwapLazyModal = useCallback(() => {
+  const handleSwapLazyModal = useCallback(async () => {
     navigation.pushModal(EModalRoutes.SwapModal, {
       screen: EModalSwapRoutes.SwapLazyMarketModal,
       params: {
