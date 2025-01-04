fd61b586d6fb028ef6304e3f540cf8740408cf47
morizon <sidmorizon@outlook.com>
2025-01-04 10:55:17 +0800

    fix: import

--- packages/kit-bg/src/vaults/impls/kaspa/KeyringHd.ts
+++ packages/kit-bg/src/vaults/impls/kaspa/KeyringHd.ts
@@ -1,6 +1,4 @@
-import { privateKeyFromBuffer } from '@onekeyhq/core/src/chains/kaspa/sdkKaspa';
 import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
-import { decrypt } from '@onekeyhq/core/src/secret';
 import type { ISignedTxPro } from '@onekeyhq/core/src/types';
 
 import { KeyringHdBase } from '../../base/KeyringHdBase';
