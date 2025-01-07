74639e28cd8c19bcfcf15981622b3fa9424f5b88
ByteZhang <ByteZhang@protonmail.com>
2025-01-07 09:45:20 +0800

    chore: fix lint

--- packages/core/src/chains/sol/sdkSol/OffchainMessage.ts
+++ packages/core/src/chains/sol/sdkSol/OffchainMessage.ts
@@ -13,31 +13,31 @@ function isValidUTF8(data: Uint8Array): boolean {
         if (data[i] < 0x80) {
             /* 0xxxxxxx */
             ++i;
-        } else if ((data[i] & 0xe0) == 0xc0) {
+        } else if ((data[i] & 0xe0) === 0xc0) {
             /* 110XXXXx 10xxxxxx */
-            if (i + 1 >= length || (data[i + 1] & 0xc0) != 0x80 ||
-                (data[i] & 0xfe) == 0xc0) /* overlong? */ {
+            if (i + 1 >= length || (data[i + 1] & 0xc0) !== 0x80 ||
+                (data[i] & 0xfe) === 0xc0) /* overlong? */ {
                 return false;
             } else {
                 i += 2;
             }
-        } else if ((data[i] & 0xf0) == 0xe0) {
+        } else if ((data[i] & 0xf0) === 0xe0) {
             /* 1110XXXX 10Xxxxxx 10xxxxxx */
-            if (i + 2 >= length || (data[i + 1] & 0xc0) != 0x80 || (data[i + 2] & 0xc0) != 0x80 ||
-                (data[i] == 0xe0 && (data[i + 1] & 0xe0) == 0x80) || /* overlong? */
-                (data[i] == 0xed && (data[i + 1] & 0xe0) == 0xa0) || /* surrogate? */
-                (data[i] == 0xef && data[i + 1] == 0xbf &&
-                 (data[i + 2] & 0xfe) == 0xbe)) /* U+FFFE or U+FFFF? */ {
+            if (i + 2 >= length || (data[i + 1] & 0xc0) !== 0x80 || (data[i + 2] & 0xc0) !== 0x80 ||
+                (data[i] === 0xe0 && (data[i + 1] & 0xe0) === 0x80) || /* overlong? */
+                (data[i] === 0xed && (data[i + 1] & 0xe0) === 0xa0) || /* surrogate? */
+                (data[i] === 0xef && data[i + 1] === 0xbf &&
+                 (data[i + 2] & 0xfe) === 0xbe)) /* U+FFFE or U+FFFF? */ {
                 return false;
             } else {
                 i += 3;
             }
         } else if ((data[i] & 0xf8) == 0xf0) {
             /* 11110XXX 10XXxxxx 10xxxxxx 10xxxxxx */
-            if (i + 3 >= length || (data[i + 1] & 0xc0) != 0x80 || (data[i + 2] & 0xc0) != 0x80 ||
-                (data[i + 3] & 0xc0) != 0x80 ||
-                (data[i] == 0xf0 && (data[i + 1] & 0xf0) == 0x80) || /* overlong? */
-                (data[i] == 0xf4 && data[i + 1] > 0x8f) || data[i] > 0xf4) /* > U+10FFFF? */ {
+            if (i + 3 >= length || (data[i + 1] & 0xc0) !== 0x80 || (data[i + 2] & 0xc0) !== 0x80 ||
+                (data[i + 3] & 0xc0) !== 0x80 ||
+                (data[i] === 0xf0 && (data[i + 1] & 0xf0) === 0x80) || /* overlong? */
+                (data[i] === 0xf4 && data[i + 1] > 0x8f) || data[i] > 0xf4) /* > U+10FFFF? */ {
                 return false;
             } else {
                 i += 4;
