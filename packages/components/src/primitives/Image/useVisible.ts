7645512ed7b54a45e252813fe2ccb12baa4692d8
huhuanming <huanming@onekey.so>
2025-01-13 12:17:56 +0800

    Update useVisible.ts

--- packages/components/src/primitives/Image/useVisible.ts
+++ packages/components/src/primitives/Image/useVisible.ts
@@ -3,15 +3,14 @@ import { useLayoutEffect, useState } from 'react';
 export const useVisible = (delayMs: number) => {
   const [visible, setVisible] = useState(!(delayMs > 0));
   useLayoutEffect(() => {
-    let timerId: ReturnType<typeof setTimeout>;
     if (delayMs > 0) {
-      timerId = setTimeout(() => {
+      const timerId = setTimeout(() => {
         setVisible(true);
       }, delayMs);
+      return () => {
+        clearTimeout(timerId);
+      };
     }
-    return () => {
-      clearTimeout(timerId);
-    };
   }, [delayMs]);
   return visible;
 };
