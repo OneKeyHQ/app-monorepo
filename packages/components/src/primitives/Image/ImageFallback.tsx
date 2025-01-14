226fe5581a6abc7a8ee831c9b5de0a9235bb554d
huhuanming <huanming@onekey.so>
2025-01-14 11:35:41 +0800

    fix: fixed tab drag-and-drop issues and mobile tab close return problem. (OK-34436 OK-34820) (#6495)

--- packages/components/src/primitives/Image/ImageFallback.tsx
+++ packages/components/src/primitives/Image/ImageFallback.tsx
@@ -1,35 +1,22 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 import { Skeleton } from '../Skeleton';
 import { Stack } from '../Stack';
 
 import { ImageContext } from './context';
+import { useVisible } from './useVisible';
 
 import type { IImageFallbackProps, IImageSkeletonProps } from './type';
 
-const useVisible = (delayMs: number) => {
-  const [visible, setVisible] = useState(!(delayMs > 0));
-  useEffect(() => {
-    let timerId: ReturnType<typeof setTimeout>;
-    if (delayMs > 0) {
-      timerId = setTimeout(() => {
-        setVisible(true);
-      }, delayMs);
-    }
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [delayMs]);
-  const { loadedSuccessfully } = useContext(ImageContext);
-  return !loadedSuccessfully && visible;
-};
 export function ImageFallback({
   delayMs = 80,
   children,
   ...props
 }: IImageFallbackProps) {
+  const { loadedSuccessfully } = useContext(ImageContext);
+
   const visible = useVisible(delayMs);
-  return visible ? (
+  return !loadedSuccessfully && visible ? (
     <Stack
       position="absolute"
       bg="$bgApp"
