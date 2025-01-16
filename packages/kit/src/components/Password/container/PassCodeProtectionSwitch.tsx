1f8e5c1b265853d17aade41b3c472bac6420e308
limichange <limichange@hotmail.com>
2025-01-16 23:52:46 +0800

    feat: add doc & move props

--- packages/kit/src/components/Password/container/PassCodeProtectionSwitch.tsx
+++ packages/kit/src/components/Password/container/PassCodeProtectionSwitch.tsx
@@ -2,13 +2,16 @@ import type { ISwitchProps } from '@onekeyhq/components';
 import { Switch } from '@onekeyhq/components';
 import { usePasswordPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 
+/**
+ * PassCodeProtectionSwitch component
+ * @param {ISwitchProps} switchProps - Props to be passed to the underlying Switch component
+ */
 const PassCodeProtectionSwitch = (switchProps: ISwitchProps) => {
   const [{ enablePasswordErrorProtection }, setPasswordPersist] =
     usePasswordPersistAtom();
 
   return (
     <Switch
-      {...switchProps}
       value={enablePasswordErrorProtection}
       onChange={(value: boolean) => {
         setPasswordPersist((v) => ({
@@ -16,6 +19,7 @@ const PassCodeProtectionSwitch = (switchProps: ISwitchProps) => {
           enablePasswordErrorProtection: value,
         }));
       }}
+      {...switchProps}
     />
   );
 };
