790ee40c71ef924114b4aedb20a8aa5a3ae55bf3
limichange <limichange@hotmail.com>
2025-01-16 23:09:42 +0800

    feat: import switch props

--- packages/kit/src/components/Password/container/PassCodeProtectionSwitch.tsx
+++ packages/kit/src/components/Password/container/PassCodeProtectionSwitch.tsx
@@ -1,13 +1,13 @@
+import type { SwitchProps } from '@onekeyhq/components';
 import { Switch } from '@onekeyhq/components';
 import { usePasswordPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 
-const PassCodeProtectionSwitch = (props: { size: string }) => {
-  const { size } = props;
+const PassCodeProtectionSwitch: React.FC<SwitchProps> = (switchProps) => {
   const [{ enablePasswordErrorProtection }, setPasswordPersist] =
     usePasswordPersistAtom();
   return (
     <Switch
-      size={size}
+      {...switchProps}
       value={enablePasswordErrorProtection}
       onChange={(value: boolean) => {
         setPasswordPersist((v) => ({
