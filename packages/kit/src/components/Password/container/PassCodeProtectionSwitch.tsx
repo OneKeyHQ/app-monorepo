7b2262be3378566d8c7bc5a153c4eef9454ef328
limichange <limichange@hotmail.com>
2025-01-16 17:15:17 +0800

    fix: PassCodeProtectionSwitch component size

--- packages/kit/src/components/Password/container/PassCodeProtectionSwitch.tsx
+++ packages/kit/src/components/Password/container/PassCodeProtectionSwitch.tsx
@@ -1,11 +1,13 @@
 import { Switch } from '@onekeyhq/components';
 import { usePasswordPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 
-const PassCodeProtectionSwitch = () => {
+const PassCodeProtectionSwitch = (props: { size: string }) => {
+  const { size } = props;
   const [{ enablePasswordErrorProtection }, setPasswordPersist] =
     usePasswordPersistAtom();
   return (
     <Switch
+      size={size}
       value={enablePasswordErrorProtection}
       onChange={(value: boolean) => {
         setPasswordPersist((v) => ({
