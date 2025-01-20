cd597787ffcc49d400f27b89eb2ee0f249d0bb21
limichange <limichange@hotmail.com>
2025-01-20 13:54:44 +0800

    feat: Enhance AccordionGallery with single selection option
    - Updated AccordionGallery to include a new 'Single' type for displaying content.
    - Changed the title of the existing 'Full' option to 'Multiple' for clarity.
    - Added interactive content for the 'Single' option, including a new item with a description about cold showers.

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/AccordionGallery.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/AccordionGallery.tsx
@@ -13,7 +13,7 @@ const AccordionGallery = () => (
     description="A vertically stacked set of interactive headings that each reveal an associated section of content"
     elements={[
       {
-        title: 'Full',
+        title: 'Multiple',
         element: (
           <YStack gap="$2" alignItems="center">
             <Accordion overflow="hidden" width="100%" type="multiple">
@@ -84,6 +84,50 @@ const AccordionGallery = () => (
                       Eggs have been a dietary staple since time immemorial and
                       there’s good reason for their continued presence in our
                       menus and meals.
+                    </SizableText>
+                  </Accordion.Content>
+                </Accordion.HeightAnimator>
+              </Accordion.Item>
+            </Accordion>
+          </YStack>
+        ),
+      },
+      {
+        title: 'Single',
+        element: (
+          <YStack gap="$2" alignItems="center">
+            <Accordion overflow="hidden" width="100%" type="single" collapsible>
+              <Accordion.Item value="a1">
+                <Accordion.Trigger
+                  flexDirection="row"
+                  justifyContent="space-between"
+                >
+                  {({ open }: { open: boolean }) => (
+                    <>
+                      <SizableText>1. Take a cold shower</SizableText>
+                      <View
+                        animation="quick"
+                        rotate={open ? '180deg' : '0deg'}
+                        transformOrigin="center"
+                      >
+                        <Icon
+                          name="ChevronDownSmallOutline"
+                          color="$iconSubdued"
+                          size="$6"
+                        />
+                      </View>
+                    </>
+                  )}
+                </Accordion.Trigger>
+                <Accordion.HeightAnimator animation="quick">
+                  <Accordion.Content
+                    animation="quick"
+                    exitStyle={{ opacity: 0 }}
+                  >
+                    <SizableText>
+                      Cold showers can help reduce inflammation, relieve pain,
+                      improve circulation, lower stress levels, and reduce
+                      muscle soreness and fatigue.
                     </SizableText>
                   </Accordion.Content>
                 </Accordion.HeightAnimator>
