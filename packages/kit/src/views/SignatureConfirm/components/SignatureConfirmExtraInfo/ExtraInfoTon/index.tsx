322f15e9ff3ac4f319117ddba49d5db302b9701e
weatherstar <weather@onekey.so>
2025-01-20 17:24:33 +0800

    Fix/issues OK-35245 OK-35242 OK-35094 (#6561)
    * fix: swap info issues
    
    * fix: extra info label
    
    * chore: udpate locale
    
    * fix: validate address
    
    * fix: fee info alert

--- packages/kit/src/views/SignatureConfirm/components/SignatureConfirmExtraInfo/ExtraInfoTon/index.tsx
+++ packages/kit/src/views/SignatureConfirm/components/SignatureConfirmExtraInfo/ExtraInfoTon/index.tsx
@@ -1,16 +1,12 @@
 import { memo } from 'react';
 
-import { useIntl } from 'react-intl';
-
 import type { IStackProps } from '@onekeyhq/components';
 import type { IDecodedTxExtraTon } from '@onekeyhq/core/src/chains/ton/types';
 import { useDecodedTxsAtom } from '@onekeyhq/kit/src/states/jotai/contexts/signatureConfirm';
-import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 import { SignatureConfirmItem } from '../../SignatureConfirmItem';
 
 function TxExtraInfoTon({ style }: { style?: IStackProps }) {
-  const intl = useIntl();
   const [{ decodedTxs }] = useDecodedTxsAtom();
 
   const decodedTx = decodedTxs?.[0];
@@ -21,11 +17,8 @@ function TxExtraInfoTon({ style }: { style?: IStackProps }) {
 
   return (
     <SignatureConfirmItem {...style}>
-      <SignatureConfirmItem.Label>
-        {intl.formatMessage({
-          id: ETranslations.send_tag,
-        })}
-      </SignatureConfirmItem.Label>
+      <SignatureConfirmItem.Label>Memo/Tag/Note</SignatureConfirmItem.Label>
+
       <SignatureConfirmItem.Value>{extraInfo.memo}</SignatureConfirmItem.Value>
     </SignatureConfirmItem>
   );
--- packages/kit/src/views/SignatureConfirm/components/SignatureConfirmExtraInfo/ExtraInfoXrp/index.tsx
+++ packages/kit/src/views/SignatureConfirm/components/SignatureConfirmExtraInfo/ExtraInfoXrp/index.tsx
@@ -1,16 +1,12 @@
 import { memo } from 'react';
 
-import { useIntl } from 'react-intl';
-
 import type { IStackProps } from '@onekeyhq/components';
 import type { IDecodedTxExtraXrp } from '@onekeyhq/core/src/chains/xrp/types';
 import { useDecodedTxsAtom } from '@onekeyhq/kit/src/states/jotai/contexts/signatureConfirm';
-import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 import { SignatureConfirmItem } from '../../SignatureConfirmItem';
 
 function TxExtraInfoXrp({ style }: { style?: IStackProps }) {
-  const intl = useIntl();
   const [{ decodedTxs }] = useDecodedTxsAtom();
 
   const decodedTx = decodedTxs?.[0];
@@ -21,11 +17,7 @@ function TxExtraInfoXrp({ style }: { style?: IStackProps }) {
 
   return (
     <SignatureConfirmItem {...style}>
-      <SignatureConfirmItem.Label>
-        {intl.formatMessage({
-          id: ETranslations.send_tag,
-        })}
-      </SignatureConfirmItem.Label>
+      <SignatureConfirmItem.Label>Memo/Tag/Note</SignatureConfirmItem.Label>
       <SignatureConfirmItem.Value>
         {extraInfo.destinationTag}
       </SignatureConfirmItem.Value>
