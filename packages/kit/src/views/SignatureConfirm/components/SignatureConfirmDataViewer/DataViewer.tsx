e4e495c277731137a9921601c0786fa60abffe70
huhuanming <huanming@onekey.so>
2025-01-21 18:38:45 +0800

    Update DataViewer.tsx

--- packages/kit/src/views/SignatureConfirm/components/SignatureConfirmDataViewer/DataViewer.tsx
+++ packages/kit/src/views/SignatureConfirm/components/SignatureConfirmDataViewer/DataViewer.tsx
@@ -3,11 +3,12 @@ import { TextAreaInput } from '@onekeyhq/components';
 function DataViewer({ data }: { data: string }) {
   return (
     <TextAreaInput
+      className="break-all"
       containerProps={{
         borderWidth: 0,
       }}
+      textBreakStrategy="simple"
       fontSize={12}
-      whiteSpace="pre"
       lineHeight={16}
       bg="$bg"
       h="$60"
