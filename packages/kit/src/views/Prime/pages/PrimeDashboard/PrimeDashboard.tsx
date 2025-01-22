03bd9c695977d3a7827e52bbbbe783b54b4b982a
limichange <limichange@hotmail.com>
2025-01-22 14:41:17 +0800

    feat: add PrimeBenefitsList

--- packages/kit/src/views/Prime/pages/PrimeDashboard/PrimeDashboard.tsx
+++ packages/kit/src/views/Prime/pages/PrimeDashboard/PrimeDashboard.tsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { StyleSheet } from 'react-native';
 
-import type { IKeyOfIcons } from '@onekeyhq/components';
 import {
   ActionList,
   Button,
@@ -14,13 +13,11 @@ import {
   SizableText,
   Stack,
   Theme,
-  Toast,
   XStack,
   YStack,
   useSafeAreaInsets,
 } from '@onekeyhq/components';
 import PrimeBannerBgDark from '@onekeyhq/kit/assets/animations/prime-banner-bg-dark.json';
-import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
 import { EWebEmbedRoutePath } from '@onekeyhq/shared/src/consts/webEmbedConsts';
@@ -32,6 +29,7 @@ import { useFetchPrimeUserInfo } from '../../hooks/useFetchPrimeUserInfo';
 import { usePrimeAuth } from '../../hooks/usePrimeAuth';
 import { usePrimePayment } from '../../hooks/usePrimePayment';
 
+import { PrimeBenefitsList } from './PrimeBenefitsList';
 import { PrimeSubscriptionPlans } from './PrimeSubscriptionPlans';
 import { PrimeUserInfo } from './PrimeUserInfo';
 
@@ -60,89 +58,6 @@ function PrimeBanner() {
         experience.
       </SizableText>
     </YStack>
-  );
-}
-
-function PrimeBenefitsItem({
-  icon,
-  title,
-  subtitle,
-  onPress,
-}: {
-  icon: IKeyOfIcons;
-  title: string;
-  subtitle: string;
-  onPress: () => void;
-}) {
-  return (
-    <ListItem drillIn onPress={onPress}>
-      <YStack borderRadius="$3" borderCurve="continuous" bg="$brand4" p="$2">
-        <Icon name={icon} size="$6" color="$brand9" />
-      </YStack>
-      <ListItem.Text
-        userSelect="none"
-        flex={1}
-        primary={title}
-        secondary={subtitle}
-      />
-    </ListItem>
-  );
-}
-
-function PrimeBenefitsList() {
-  return (
-    <Stack py="$2">
-      <PrimeBenefitsItem
-        icon="RepeatOutline"
-        title="Sync"
-        subtitle="Automatically back up app usage data, sync across devices."
-        onPress={() => {
-          Toast.success({
-            title: 'Sync',
-          });
-        }}
-      />
-      <PrimeBenefitsItem
-        icon="BezierNodesOutline"
-        title="Premium RPC"
-        subtitle="Enjoy rapid and secure blockchain access."
-        onPress={() => {
-          Toast.success({
-            title: 'Premium RPC',
-          });
-        }}
-      />
-      <PrimeBenefitsItem
-        icon="BellOutline"
-        title="Account Activity"
-        subtitle="Subscribe to activities from up to 100 accounts."
-        onPress={() => {
-          Toast.success({
-            title: 'Account Activity',
-          });
-        }}
-      />
-      <PrimeBenefitsItem
-        icon="FileTextOutline"
-        title="Analytics"
-        subtitle="sint occaecat cupidatat non proident"
-        onPress={() => {
-          Toast.success({
-            title: 'Analytics',
-          });
-        }}
-      />
-      <PrimeBenefitsItem
-        icon="PhoneOutline"
-        title="Device management"
-        subtitle="Access Prime on up to 5 devices."
-        onPress={() => {
-          Toast.success({
-            title: 'Device management',
-          });
-        }}
-      />
-    </Stack>
   );
 }
 
@@ -333,6 +248,7 @@ export default function PrimeDashboard() {
               ) : null}
               {subscriptionPlans}
             </Stack>
+
             <PrimeBenefitsList />
             <XStack flexWrap="wrap">
               <Button
