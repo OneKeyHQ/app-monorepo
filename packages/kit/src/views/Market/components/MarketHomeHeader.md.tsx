8ebacfb9813ccd0faa8d7c3f0e01bd137d48eecd
huhuanming <huanming@onekey.so>
2025-01-27 18:07:21 +0800

    Update MarketHomeHeader.md.tsx

--- packages/kit/src/views/Market/components/MarketHomeHeader.md.tsx
+++ packages/kit/src/views/Market/components/MarketHomeHeader.md.tsx
@@ -1,4 +1,7 @@
+import { useMemo } from 'react';
+
 import { Stack } from '@onekeyhq/components';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
 
 import { TabPageHeader } from '../../../components/TabPageHeader';
@@ -6,12 +9,26 @@ import { TabPageHeader } from '../../../components/TabPageHeader';
 import { MarketHomeHeaderSearchBar } from './MarketHomeHeaderSearchBar';
 
 export function MarketHomeHeader() {
-  return (
-    <>
-      <TabPageHeader sceneName={EAccountSelectorSceneName.home} />
-      <Stack px="$5" py="$3">
-        <MarketHomeHeaderSearchBar />
-      </Stack>
-    </>
+  const content = useMemo(
+    () => (
+      <>
+        <TabPageHeader sceneName={EAccountSelectorSceneName.home} />
+        <Stack px="$5" pt={platformEnv.isNativeIOSPad ? '$6' : '$3'} pb="$3">
+          <MarketHomeHeaderSearchBar />
+        </Stack>
+      </>
+    ),
+    [],
+  );
+  return platformEnv.isNativeIOSPad ? (
+    <Stack
+      flexDirection={platformEnv.isNativeIOSPad ? 'row' : 'column'}
+      ai="center"
+      jc="space-between"
+    >
+      {content}
+    </Stack>
+  ) : (
+    content
   );
 }
