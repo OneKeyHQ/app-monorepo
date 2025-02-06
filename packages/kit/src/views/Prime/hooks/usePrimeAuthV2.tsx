a2a6bfd592e628eb403e1f4ba02d6348c914a7dc
limichange <limichange@hotmail.com>
2025-02-06 16:03:29 +0800

    refactor: simplify Prime authentication hooks and remove unused code

--- packages/kit/src/views/Prime/hooks/usePrimeAuthV2.tsx
+++ packages/kit/src/views/Prime/hooks/usePrimeAuthV2.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useRef } from 'react';
 
-import { useIntl } from 'react-intl';
-
 import type { IDialogInstance } from '@onekeyhq/components';
-import { Dialog, Toast } from '@onekeyhq/components';
+import { Dialog } from '@onekeyhq/components';
 import { usePrimePersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
-import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 import { PrimeLoginEmailCodeDialogV2 } from '../components/PrimeLoginEmailCodeDialogV2';
 import { PrimeLoginEmailDialogV2 } from '../components/PrimeLoginEmailDialogV2';
@@ -16,7 +13,6 @@ export function usePrimeAuthV2() {
   const [primePersistAtom] = usePrimePersistAtom();
   const emailDialogRef = useRef<IDialogInstance | undefined>(undefined);
   const emailCodeDialogRef = useRef<IDialogInstance | undefined>(undefined);
-  const intl = useIntl();
 
   const { useLoginWithEmail, logout, getAccessToken, isReady, authenticated } =
     usePrivyUniversalV2();
@@ -30,42 +26,11 @@ export function usePrimeAuthV2() {
   });
 
   useEffect(() => {
-    console.log('state.status', state.status);
-    if (state.status === 'sending-code') {
-      // Toast.success({
-      //   title: 'send code',
-      // });
-    } else if (state.status === 'awaiting-code-input') {
-      // Toast.success({
-      //   title: 'awaiting code input',
-      // });
-    } else if (state.status === 'submitting-code') {
-      // Toast.success({
-      //   title: 'submitting code',
-      // });
-    }
-
     if (state.status === 'done') {
-      // Toast.success({
-      //   title: intl.formatMessage({
-      //     id: ETranslations.login_welcome_message,
-      //   }),
-      // });
-
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      emailDialogRef.current?.close();
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      emailCodeDialogRef.current?.close();
+      void emailDialogRef.current?.close();
+      void emailCodeDialogRef.current?.close();
     }
-
-    if (state.status === 'error') {
-      Toast.error({
-        title: intl.formatMessage({
-          id: ETranslations.auth_error_passcode_incorrect,
-        }),
-      });
-    }
-  }, [intl, state.status]);
+  }, [state.status]);
 
   const loginWithEmail = () => {
     Dialog.show({
