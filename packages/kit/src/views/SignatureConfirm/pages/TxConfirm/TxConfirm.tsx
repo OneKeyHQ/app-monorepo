b436e42b1d3a33b18e797868a70bbaf5dcd95ce3
weatherstar <weather@onekey.so>
2025-02-10 20:53:08 +0800

    fix: lint

--- packages/kit/src/views/SignatureConfirm/pages/TxConfirm/TxConfirm.tsx
+++ packages/kit/src/views/SignatureConfirm/pages/TxConfirm/TxConfirm.tsx
@@ -24,6 +24,7 @@ import type {
   EModalSignatureConfirmRoutes,
   IModalSignatureConfirmParamList,
 } from '@onekeyhq/shared/src/routes';
+import { EDAppModalPageStatus } from '@onekeyhq/shared/types/dappConnection';
 import { ESendFeeStatus } from '@onekeyhq/shared/types/fee';
 import { ESendPreCheckTimingEnum } from '@onekeyhq/shared/types/send';
 
@@ -41,7 +42,6 @@ import SwapInfo from '../../components/SwapInfo';
 import { usePreCheckNativeBalance } from '../../hooks/usePreCheckNativeBalance';
 
 import type { RouteProp } from '@react-navigation/core';
-import { EDAppModalPageStatus } from '@onekeyhq/shared/types/dappConnection';
 
 function TxConfirm() {
   const route =
