d17b0a302676b9999640e0d0650840a388ca9e97
huhuanming <huanming@onekey.so>
2025-02-12 13:34:43 +0800

    Update PasswordVerifyContainer.tsx

--- packages/kit/src/components/Password/container/PasswordVerifyContainer.tsx
+++ packages/kit/src/components/Password/container/PasswordVerifyContainer.tsx
@@ -17,10 +17,8 @@ import {
   usePasswordModeAtom,
   usePasswordPersistAtom,
 } from '@onekeyhq/kit-bg/src/states/jotai/atoms/password';
-import biologyAuth from '@onekeyhq/shared/src/biologyAuth';
 import { dismissKeyboard } from '@onekeyhq/shared/src/keyboard';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
-import { checkBiometricAuthChanged } from '@onekeyhq/shared/src/modules3rdParty/check-biometric-auth-changed';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import resetUtils from '@onekeyhq/shared/src/utils/resetUtils';
 import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
@@ -79,21 +77,6 @@ const PasswordVerifyContainer = ({
     void backgroundApiProxy.servicePassword.resetPasswordStatus();
   }, []);
 
-  const checkAuthChanged = useCallback(async () => {
-    const isSupport = await biologyAuth.isSupportBiologyAuth();
-    if (!isSupport) {
-      return false;
-    }
-    const changed = await checkBiometricAuthChanged();
-    if (changed) {
-      void backgroundApiProxy.servicePassword.setBiologyAuthEnable(false);
-      alert(
-        'Biometric authentication has changed. Please manually re-enable it.',
-      );
-    }
-    return changed;
-  }, []);
-
   useEffect(() => {
     if (webAuthCredentialId && isBiologyAuthSwitchOn) {
       void (async () => {
@@ -108,33 +91,14 @@ const PasswordVerifyContainer = ({
     if (isEnable && isBiologyAuthSwitchOn) {
       void (async () => {
         try {
-          let changed = false;
-          if (platformEnv.isNativeIOS || platformEnv.isDesktopMac) {
-            changed = await checkAuthChanged();
-          }
-          if (changed) {
-            return;
-          }
           const securePassword = await biologyAuthUtils.getPassword();
           setHasSecurePassword(!!securePassword);
         } catch (e) {
           setHasSecurePassword(false);
         }
       })();
-
-      if (platformEnv.isNativeIOS || platformEnv.isDesktopMac) {
-        const handleVisibilityStateChange = (visible: boolean) => {
-          if (visible) {
-            void checkAuthChanged();
-          }
-        };
-        const removeSubscription = onVisibilityStateChange(
-          handleVisibilityStateChange,
-        );
-        return removeSubscription;
-      }
     }
-  }, [isEnable, isBiologyAuthSwitchOn, checkAuthChanged]);
+  }, [isEnable, isBiologyAuthSwitchOn]);
 
   useEffect(() => {
     setPasswordAtom((v) => ({
