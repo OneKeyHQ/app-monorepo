c016d434b1a976a9f039650fa36130d93fe04da7
weatherstar <weather@onekey.so>
2025-02-19 14:00:16 +0800

    feat: filter scam history entry

--- packages/kit/src/components/TxHistoryListView/TxHistoryListHeader.tsx
+++ packages/kit/src/components/TxHistoryListView/TxHistoryListHeader.tsx
@@ -1,16 +1,28 @@
-import { debounce } from 'lodash';
+import { useCallback, useMemo } from 'react';
 
-import { Stack } from '@onekeyhq/components';
+import { debounce } from 'lodash';
+import { useIntl } from 'react-intl';
+
+import {
+  ESwitchSize,
+  IconButton,
+  Popover,
+  Stack,
+  Switch,
+} from '@onekeyhq/components';
 import {
   SEARCH_DEBOUNCE_INTERVAL,
   SEARCH_KEY_MIN_LENGTH,
 } from '@onekeyhq/shared/src/consts/walletConsts';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
 import type { IAccountHistoryTx } from '@onekeyhq/shared/types/history';
 
+import { useActiveAccount } from '../../states/jotai/contexts/accountSelector';
 import {
   useHistoryListActions,
   useSearchKeyAtom,
 } from '../../states/jotai/contexts/historyList';
+import { ListItem } from '../ListItem';
 import { ListToolToolBar } from '../ListToolBar';
 
 type IProps = {
@@ -18,8 +30,22 @@ type IProps = {
 };
 
 function TxHistoryListHeader({ filteredHistory }: IProps) {
+  const intl = useIntl();
   const [searchKey] = useSearchKeyAtom();
   const { updateSearchKey } = useHistoryListActions().current;
+
+  const handleFilterScamHistoryOnChange = useCallback(() => {
+    console.log('handleFilterScamHistoryOnChange');
+  }, []);
+
+  const {
+    activeAccount: { network },
+  } = useActiveAccount({ num: 0 });
+
+  const filterScamHistorySupported = useMemo(
+    () => network?.isAllNetworks || network?.backendIndex,
+    [network],
+  );
 
   return (
     <Stack>
@@ -34,24 +60,43 @@ function TxHistoryListHeader({ filteredHistory }: IProps) {
               ? filteredHistory.length
               : 0,
         }}
-        // headerRight={
-        //   <Popover
-        //     title="Settings"
-        //     renderTrigger={
-        //       <IconButton variant="tertiary" icon="SliderVerOutline" />
-        //     }
-        //     renderContent={
-        //       <Stack py="$2">
-        //         <ListItem
-        //           title="Hide Zero-Value Transfers"
-        //           subtitle="Avoid scam attempts by hiding transfers of zero-value tokens."
-        //         >
-        //           <Switch size={ESwitchSize.small} value={val} onChange={setVal} />
-        //         </ListItem>
-        //       </Stack>
-        //     }
-        //   />
-        // }
+        headerRight={
+          <Popover
+            title={intl.formatMessage({ id: ETranslations.global_settings })}
+            renderTrigger={
+              <IconButton variant="tertiary" icon="SliderVerOutline" />
+            }
+            renderContent={
+              <Stack py="$2">
+                <ListItem
+                  title={intl.formatMessage({
+                    id: ETranslations.wallet_history_settings_hide_risk_transaction_title,
+                  })}
+                  subtitle={
+                    filterScamHistorySupported
+                      ? intl.formatMessage({
+                          id: ETranslations.wallet_history_settings_hide_risk_transaction_desc,
+                        })
+                      : intl.formatMessage(
+                          {
+                            id: ETranslations.wallet_history_settings_hide_risk_transaction_desc_unsupprted,
+                          },
+                          {
+                            networkName: network?.name ?? '',
+                          },
+                        )
+                  }
+                >
+                  <Switch
+                    disabled={!filterScamHistorySupported}
+                    size={ESwitchSize.large}
+                    onChange={handleFilterScamHistoryOnChange}
+                  />
+                </ListItem>
+              </Stack>
+            }
+          />
+        }
       />
     </Stack>
   );
