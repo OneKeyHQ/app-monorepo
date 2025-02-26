7f80a67d3d14f151c4b56890f3ae99e120772226
limichange <limichange@hotmail.com>
2025-02-26 20:11:30 +0800

    refactor: Improve Welcome page component with dynamic logo rendering and configuration

--- packages/kit/src/views/Discovery/pages/Dashboard/Welcome/Welcome.tsx
+++ packages/kit/src/views/Discovery/pages/Dashboard/Welcome/Welcome.tsx
@@ -8,40 +8,45 @@ import { WelcomeItem } from './WelcomeItem';
 export function Welcome({ banner }: { banner: React.ReactNode }) {
   const logos = Object.values(browserWelcomeLogos);
 
+  // Configuration for left side items
+  const leftSideItems = [
+    { position: { top: '25%', right: '$28' }, logoIndex: 0, size: '$14' },
+    { position: { bottom: '25%', right: '$12' }, logoIndex: 1, size: '$12' },
+    { position: { top: '30%', right: '$0' }, logoIndex: 2, size: '$9' },
+  ];
+
+  // Configuration for right side items
+  const rightSideItems = [
+    { position: { top: '22%', left: '$28' }, logoIndex: 3, size: '$12' },
+    { position: { bottom: '22%', left: '$11' }, logoIndex: 4, size: '$10' },
+    { position: { top: '40%', left: '$2' }, logoIndex: 5, size: '$8' },
+  ];
+
+  // Shared stack props for the side containers
+  const sideStackProps = {
+    $sm: { display: 'none' as const },
+    flex: 1,
+    width: '$50',
+    height: '100%',
+  };
+
   return (
     <XStack width="100%" $gtSm={{ justifyContent: 'center' }}>
-      <Stack $sm={{ display: 'none' }} flex={1} width="$50" height="100%">
-        <WelcomeItem
-          position="absolute"
-          top="25%"
-          right="$28"
-          key={logos[0].name}
-          logo={logos[0].icon}
-          url={logos[0].url}
-          size="$14"
-        />
-
-        <WelcomeItem
-          position="absolute"
-          bottom="25%"
-          right="$12"
-          key={logos[1].name}
-          logo={logos[1].icon}
-          url={logos[1].url}
-          size="$12"
-        />
-
-        <WelcomeItem
-          position="absolute"
-          top="30%"
-          right="$0"
-          key={logos[2].name}
-          logo={logos[2].icon}
-          url={logos[2].url}
-          size="$9"
-        />
+      {/* Left side with logo items */}
+      <Stack {...sideStackProps}>
+        {leftSideItems.map((item, index) => (
+          <WelcomeItem
+            position="absolute"
+            {...item.position}
+            key={logos[item.logoIndex]?.name || `left-item-${index}`}
+            logo={logos[item.logoIndex]?.icon}
+            url={logos[item.logoIndex]?.url}
+            size={item.size}
+          />
+        ))}
       </Stack>
 
+      {/* Center content */}
       <Stack
         alignItems="center"
         justifyContent="center"
@@ -56,40 +61,21 @@ export function Welcome({ banner }: { banner: React.ReactNode }) {
         }}
       >
         {banner || <DefaultTitle />}
-
         <SearchInput />
       </Stack>
 
-      <Stack $sm={{ display: 'none' }} flex={1} width="$50" height="100%">
-        <WelcomeItem
-          position="absolute"
-          top="22%"
-          left="$28"
-          key={logos[3].name}
-          logo={logos[3].icon}
-          url={logos[3].url}
-          size="$12"
-        />
-
-        <WelcomeItem
-          position="absolute"
-          bottom="22%"
-          left="$11"
-          key={logos[4].name}
-          logo={logos[4].icon}
-          url={logos[4].url}
-          size="$10"
-        />
-
-        <WelcomeItem
-          position="absolute"
-          top="40%"
-          left="$2"
-          key={logos[5].name}
-          logo={logos[5].icon}
-          url={logos[5].url}
-          size="$8"
-        />
+      {/* Right side with logo items */}
+      <Stack {...sideStackProps}>
+        {rightSideItems.map((item, index) => (
+          <WelcomeItem
+            position="absolute"
+            {...item.position}
+            key={logos[item.logoIndex]?.name || `right-item-${index}`}
+            logo={logos[item.logoIndex]?.icon}
+            url={logos[item.logoIndex]?.url}
+            size={item.size}
+          />
+        ))}
       </Stack>
     </XStack>
   );
