23a96680bce6b2a32b85e8d0c769f4a9519fc13b
Leon <lixiao.dev@gmail.com>
2025-02-26 15:57:03 +0800

    feat: Build transaction

--- packages/kit-bg/src/vaults/impls/neo/Vault.ts
+++ packages/kit-bg/src/vaults/impls/neo/Vault.ts
@@ -1,6 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { sc, tx, wallet } from '@cityofzion/neon-core';
+import BigNumber from 'bignumber.js';
+import { isEmpty } from 'lodash';
+
+import type { IEncodedTxNeoN3 } from '@onekeyhq/core/src/chains/neo/types';
 import type { IEncodedTx, IUnsignedTxPro } from '@onekeyhq/core/src/types';
-import { NotImplemented } from '@onekeyhq/shared/src/errors';
+import {
+  InvalidAddress,
+  NotImplemented,
+  OneKeyInternalError,
+} from '@onekeyhq/shared/src/errors';
 import type {
   IAddressValidation,
   IGeneralInputValidation,
@@ -9,7 +18,12 @@ import type {
   IXprvtValidation,
   IXpubValidation,
 } from '@onekeyhq/shared/types/address';
-import type { IDecodedTx } from '@onekeyhq/shared/types/tx';
+import {
+  EDecodedTxActionType,
+  EDecodedTxStatus,
+  type IDecodedTx,
+  type IDecodedTxAction,
+} from '@onekeyhq/shared/types/tx';
 
 import { VaultBase } from '../../base/VaultBase';
 
@@ -28,6 +42,7 @@ import type {
   IBuildUnsignedTxParams,
   IGetPrivateKeyFromImportedParams,
   IGetPrivateKeyFromImportedResult,
+  ITransferInfo,
   IUpdateUnsignedTxParams,
   IValidateGeneralInputParams,
 } from '../../types';
@@ -58,18 +73,177 @@ export default class Vault extends VaultBase {
     });
   }
 
-  override buildEncodedTx(params: IBuildEncodedTxParams): Promise<IEncodedTx> {
-    throw new NotImplemented();
+  private async buildTransferInputs({
+    transferInfo,
+  }: {
+    transferInfo: ITransferInfo;
+  }) {
+    const { to, amount, tokenInfo } = transferInfo;
+
+    if (
+      !tokenInfo ||
+      typeof tokenInfo.decimals !== 'number' ||
+      tokenInfo.decimals < 0
+    ) {
+      throw new OneKeyInternalError('Token decimals is required');
+    }
+    const dbAccount = await this.getAccount();
+    const scriptHash = wallet.getScriptHashFromAddress(dbAccount.address);
+    const amountValue = new BigNumber(amount)
+      .shiftedBy(tokenInfo.decimals)
+      .toFixed();
+
+    return {
+      scriptHash,
+      fromAccountAddress: dbAccount.address,
+      toAccountAddress: to,
+      tokenScriptHash: tokenInfo.address,
+      amountToTransfer: amountValue,
+      systemFee: new BigNumber(0).toFixed(),
+      networkFee: new BigNumber(0).toFixed(),
+    };
   }
 
-  override buildDecodedTx(params: IBuildDecodedTxParams): Promise<IDecodedTx> {
-    throw new NotImplemented();
+  override async buildEncodedTx(
+    params: IBuildEncodedTxParams,
+  ): Promise<IEncodedTxNeoN3> {
+    const { transfersInfo } = params;
+    if (!transfersInfo || isEmpty(transfersInfo)) {
+      throw new OneKeyInternalError('transfersInfo is required');
+    }
+    if (transfersInfo.length > 1) {
+      throw new OneKeyInternalError('Batch transfer is not supported');
+    }
+    const transferInfo = transfersInfo[0];
+    if (!transferInfo.to) {
+      throw new Error('buildEncodedTx ERROR: transferInfo.to is missing');
+    }
+
+    const inputs = await this.buildTransferInputs({
+      transferInfo,
+    });
+
+    const script = sc.createScript({
+      scriptHash: inputs.tokenScriptHash,
+      operation: 'transfer',
+      args: [
+        sc.ContractParam.hash160(inputs.fromAccountAddress),
+        sc.ContractParam.hash160(inputs.toAccountAddress),
+        sc.ContractParam.integer(inputs.amountToTransfer),
+        sc.ContractParam.any(null),
+      ],
+    });
+
+    const currentHeight = await this.getBlockCount();
+
+    const transaction = new tx.Transaction({
+      signers: [
+        {
+          account: inputs.scriptHash,
+          scopes: tx.WitnessScope.CalledByEntry,
+        },
+      ],
+      validUntilBlock: currentHeight + 100,
+      systemFee: inputs.systemFee,
+      script,
+    });
+
+    return transaction.toJson();
   }
 
-  override buildUnsignedTx(
+  override async buildDecodedTx(
+    params: IBuildDecodedTxParams,
+  ): Promise<IDecodedTx> {
+    const { unsignedTx } = params;
+    const encodedTx = unsignedTx?.encodedTx as IEncodedTxNeoN3;
+    console.log('encodedTx: ', encodedTx);
+
+    const network = await this.getNetwork();
+    const account = await this.getAccount();
+
+    const actions: IDecodedTxAction[] = [];
+
+    if (unsignedTx.transfersInfo?.[0]) {
+      const { tokenInfo } = unsignedTx.transfersInfo[0];
+      const inputs = await this.buildTransferInputs({
+        transferInfo: unsignedTx.transfersInfo[0],
+      });
+
+      const token = await this.backgroundApi.serviceToken.getToken({
+        networkId: this.networkId,
+        accountId: this.accountId,
+        tokenIdOnNetwork: tokenInfo?.address ?? '',
+      });
+
+      if (
+        token?.decimals === undefined ||
+        token?.decimals === null ||
+        Number.isNaN(token?.decimals)
+      ) {
+        throw new OneKeyInternalError('Token decimals is required');
+      }
+
+      const action = await this.buildTxTransferAssetAction({
+        from: inputs.fromAccountAddress,
+        to: inputs.toAccountAddress,
+        transfers: [
+          {
+            from: inputs.fromAccountAddress,
+            to: inputs.toAccountAddress,
+            amount: new BigNumber(inputs.amountToTransfer)
+              .shiftedBy(-token.decimals)
+              .toFixed(),
+            icon: token?.logoURI ?? '',
+            symbol: token?.symbol ?? '',
+            name: token?.name ?? '',
+            tokenIdOnNetwork: token?.address ?? '',
+            isNative: token?.isNative,
+          },
+        ],
+      });
+      actions.push(action);
+    } else {
+      // TODO: contract interaction
+    }
+
+    if (actions.length === 0) {
+      actions.push({
+        type: EDecodedTxActionType.UNKNOWN,
+        unknownAction: {
+          from: account.address,
+          to: '',
+          icon: network.logoURI ?? '',
+        },
+      });
+    }
+
+    const result: IDecodedTx = {
+      txid: '',
+      owner: account.address,
+      signer: account.address,
+      nonce: 0,
+      actions,
+      status: EDecodedTxStatus.Pending,
+      networkId: this.networkId,
+      accountId: this.accountId,
+      extraInfo: null,
+      encodedTx,
+    };
+
+    return Promise.resolve(result);
+  }
+
+  override async buildUnsignedTx(
     params: IBuildUnsignedTxParams,
   ): Promise<IUnsignedTxPro> {
-    throw new NotImplemented();
+    const encodedTx = params.encodedTx ?? (await this.buildEncodedTx(params));
+    if (encodedTx) {
+      return {
+        encodedTx,
+        transfersInfo: params.transfersInfo,
+      };
+    }
+    throw new OneKeyInternalError();
   }
 
   override updateUnsignedTx(
@@ -79,7 +253,14 @@ export default class Vault extends VaultBase {
   }
 
   override validateAddress(address: string): Promise<IAddressValidation> {
-    throw new NotImplemented();
+    if (wallet.isAddress(address)) {
+      return Promise.resolve({
+        isValid: true,
+        normalizedAddress: address,
+        displayAddress: address,
+      });
+    }
+    return Promise.reject(new InvalidAddress());
   }
 
   override validateXpub(xpub: string): Promise<IXpubValidation> {
@@ -106,5 +287,44 @@ export default class Vault extends VaultBase {
     params: IValidateGeneralInputParams,
   ): Promise<IGeneralInputValidation> {
     throw new NotImplemented();
+  }
+
+  async getBlockCount() {
+    try {
+      const [blockCount] =
+        await this.backgroundApi.serviceAccountProfile.sendProxyRequest<{
+          result: number;
+        }>({
+          networkId: this.networkId,
+          body: [
+            {
+              route: 'rpc',
+              params: {
+                method: 'getblockcount',
+                params: [],
+              },
+            },
+          ],
+          returnRawData: true,
+        });
+      if (blockCount.result === undefined || blockCount.result === null) {
+        throw new OneKeyInternalError(
+          'Invalid block count: result is null or undefined',
+        );
+      }
+      const blockCountBN = new BigNumber(blockCount.result);
+      if (blockCountBN.isNaN() || blockCountBN.isNegative()) {
+        throw new OneKeyInternalError(
+          'Invalid block count: expected a non-negative number',
+        );
+      }
+      return blockCountBN.toNumber();
+    } catch (error) {
+      throw new OneKeyInternalError(
+        `Failed to get block count: ${
+          error instanceof Error ? error.message : 'Unknown error'
+        }`,
+      );
+    }
   }
 }
