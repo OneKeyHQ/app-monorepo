5697366d23da9e4c9d8bb1ef700751f40866b5e6
huhuanming <huanming@onekey.so>
2025-02-28 19:03:52 +0800

    Update fileOperation.js

--- apps/desktop/scripts/fileOperation.js
+++ apps/desktop/scripts/fileOperation.js
@@ -42,15 +42,7 @@ exports.default = async function fileOperation(context) {
   if (electronPlatformName === 'mas' || electronPlatformName === 'darwin') {
     await context.packager.addElectronFuses(context, {
       version: FuseVersion.V1,
-      strictlyRequireAllFuses: true,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.GrantFileProtocolExtraPrivileges]: false,
-      [FuseV1Options.LoadBrowserProcessSpecificV8Snapshot]: false,
-      [FuseV1Options.OnlyLoadAppFromAsar]: false,
     });
   }
 };
