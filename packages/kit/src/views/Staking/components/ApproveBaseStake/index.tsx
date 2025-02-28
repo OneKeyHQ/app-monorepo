12f2315defa02b0244e5f22f1b26e0ce41789572
huhuanming <huanming@onekey.so>
2025-02-28 10:32:25 +0800

    Update index.tsx

--- packages/kit/src/views/Staking/components/ApproveBaseStake/index.tsx
+++ packages/kit/src/views/Staking/components/ApproveBaseStake/index.tsx
@@ -481,7 +481,6 @@ export function ApproveBaseStake({
       approvesInfo,
 
       onSuccess(data) {
-        trackAllowance(data[0].decodedTx.txid);
         setApproving(false);
       },
       onFail() {
