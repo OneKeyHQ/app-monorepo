b229ca8bcd12af97e3d4ab3f98a6219374c22c71
limichange <limichange@hotmail.com>
2025-03-03 16:40:19 +0800

    feat: Enhance BrowserTitle with responsive text rendering

--- packages/kit/src/views/Discovery/pages/components/BrowserTitle.tsx
+++ packages/kit/src/views/Discovery/pages/components/BrowserTitle.tsx
@@ -1,18 +1,27 @@
+import { useMemo } from 'react';
+
 import { useIntl } from 'react-intl';
 
-import { SizableText } from '@onekeyhq/components';
+import { SizableText, useMedia } from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 export function BrowserTitle() {
   const intl = useIntl();
+  const media = useMedia();
+
+  const memoizedText = useMemo(
+    () =>
+      intl.formatMessage({
+        id: media.gtSm
+          ? ETranslations.global_browser
+          : ETranslations.browser_dive_in,
+      }),
+    [intl, media.gtSm],
+  );
 
   return (
     <SizableText size="$headingLg" color="$text">
-      {intl.formatMessage({
-        id: ETranslations.global_browser,
-      })}
+      {memoizedText}
     </SizableText>
   );
 }
-
-export default BrowserTitle;
