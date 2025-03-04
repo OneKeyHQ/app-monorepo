bb738d1d864fb43cd2866f9a95c3de1108f64ed9
huhuanming <huanming@onekey.so>
2025-03-04 12:51:16 +0800

    Update index.js

--- apps/desktop/index.js
+++ apps/desktop/index.js
@@ -1,9 +1,8 @@
-// import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo';
 
-// import App from './App';
+import App from './App';
 
-// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// // It also ensures that whether you load the app in Expo Go or in a native build,
-// // the environment is set up appropriately
-// registerRootComponent(App);
-console.log('111');
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
