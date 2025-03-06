67513906a76c0c361c731ab7dd1b65ce1bdce675
limichange <limichange@hotmail.com>
2025-03-07 00:46:30 +0800

    feat: Conditionally render BrowserTitle based on banner status
    - Remove BrowserTitle from Page.Header
    - Add logic to hide BrowserTitle when active banners exist
    - Simplify iOS header layout using XStack
    - Fetch and check banner data before rendering title

--- packages/kit/src/views/Discovery/pages/components/BrowserTitle.tsx
+++ packages/kit/src/views/Discovery/pages/components/BrowserTitle.tsx
@@ -3,11 +3,32 @@ import { useMemo } from 'react';
 import { useIntl } from 'react-intl';
 
 import { SizableText, useMedia } from '@onekeyhq/components';
+import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+
+import { useBannerData } from '../../hooks/useBannerData';
 
 export function BrowserTitle() {
   const intl = useIntl();
   const media = useMedia();
+
+  const { result: homePageData } = usePromiseResult(
+    async () => {
+      const data =
+        await backgroundApiProxy.serviceDiscovery.fetchDiscoveryHomePageData();
+      return data;
+    },
+    [],
+    {
+      watchLoading: true,
+    },
+  );
+
+  const { data: bannerData } = useBannerData(homePageData?.banners || []);
+
+  // Check if there are any active banners (not closed by user)
+  const hasActiveBanners = bannerData && bannerData.length > 0;
 
   const memoizedText = useMemo(
     () =>
@@ -18,6 +39,10 @@ export function BrowserTitle() {
       }),
     [intl, media.gtSm],
   );
+
+  if (hasActiveBanners) {
+    return null;
+  }
 
   return (
     <SizableText size="$headingLg" color="$text">
