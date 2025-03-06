82ae49852a83fe76abf300e9510a554cd440d057
huhuanming <huanming@onekey.so>
2025-03-06 22:29:20 +0800

    Update index.tsx

--- packages/kit/src/views/AppUpdate/components/UpdatePreviewActionButton/index.tsx
+++ packages/kit/src/views/AppUpdate/components/UpdatePreviewActionButton/index.tsx
@@ -3,101 +3,42 @@ import { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 
 import type { IPageFooterProps } from '@onekeyhq/components';
-import { Page, Toast, YStack } from '@onekeyhq/components';
-import {
-  useAppUpdateInfo,
-  useDownloadPackage,
-} from '@onekeyhq/kit/src/components/UpdateReminder/hooks';
-import { EAppUpdateStatus } from '@onekeyhq/shared/src/appUpdate';
+import { Page, YStack } from '@onekeyhq/components';
+import { useAppUpdateInfo } from '@onekeyhq/kit/src/components/UpdateReminder/hooks';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
-import {
-  installPackage,
-  useDownloadProgress,
-} from '@onekeyhq/shared/src/modules3rdParty/auto-update';
+import { downloadPackage } from '@onekeyhq/shared/src/modules3rdParty/auto-update';
+import { EAppUpdateRoutes } from '@onekeyhq/shared/src/routes/appUpdate';
 import { openUrlExternal } from '@onekeyhq/shared/src/utils/openUrlUtils';
 
 import type { IUpdatePreviewActionButton } from './type';
 
-export const UpdatePreviewActionButton: IUpdatePreviewActionButton = ({
-  autoClose,
-}: {
-  autoClose: boolean;
-}) => {
+export const UpdatePreviewActionButton: IUpdatePreviewActionButton = () => {
   const intl = useIntl();
   const appUpdateInfo = useAppUpdateInfo();
-  const downloadPackage = useDownloadPackage();
-  const downloadSuccess = useCallback(() => {}, []);
-  const downloadFailed = useCallback(() => {}, []);
-  const progress = useDownloadProgress(downloadSuccess, downloadFailed);
-  const handleToUpdate: IPageFooterProps['onConfirm'] = useCallback(
-    (close: () => void) => {
-      if (appUpdateInfo.data) {
-        if (appUpdateInfo.data.storeUrl) {
-          openUrlExternal(appUpdateInfo.data.storeUrl);
-        } else if (appUpdateInfo.data.downloadUrl) {
-          void downloadPackage(appUpdateInfo.data);
-          if (autoClose) {
-            close();
-          }
-        }
-      }
-    },
-    [appUpdateInfo.data, autoClose, downloadPackage],
-  );
 
-  const handleToInstall = useCallback(async () => {
-    try {
-      await installPackage(appUpdateInfo.data);
-    } catch (error) {
-      const { message } = error as { message: string };
-      if (message) {
-        Toast.error({ title: message });
+  const navigation = useAppNavigation();
+
+  const handleToUpdate: IPageFooterProps['onConfirm'] = useCallback(() => {
+    if (appUpdateInfo.data) {
+      if (appUpdateInfo.data.storeUrl) {
+        openUrlExternal(appUpdateInfo.data.storeUrl);
+      } else if (appUpdateInfo.data.downloadUrl) {
+        void downloadPackage(appUpdateInfo.data);
+        navigation.push(EAppUpdateRoutes.DownloadVerify);
       }
     }
-  }, [appUpdateInfo.data]);
-
-  const isDisabledButton = [
-    EAppUpdateStatus.downloading,
-    EAppUpdateStatus.verifying,
-  ].includes(appUpdateInfo.data?.status);
-
-  const isReadyToInstall =
-    EAppUpdateStatus.ready === appUpdateInfo.data?.status;
-
-  const renderButtonText = useCallback(() => {
-    switch (appUpdateInfo.data?.status) {
-      case EAppUpdateStatus.downloading: {
-        return intl.formatMessage(
-          {
-            id: ETranslations.update_progress_downloading,
-          },
-          {
-            progress,
-          },
-        );
-      }
-      case EAppUpdateStatus.verifying: {
-        return intl.formatMessage({
-          id: ETranslations.update_verifying,
-        });
-      }
-      default: {
-        return intl.formatMessage({
-          id: ETranslations.update_update_now,
-        });
-      }
-    }
-  }, [appUpdateInfo.data?.status, intl, progress]);
+  }, [appUpdateInfo.data, navigation]);
   return (
     <Page.Footer>
       <YStack>
         <Page.FooterActions
-          confirmButtonProps={{
-            disabled: isDisabledButton,
-            loading: isDisabledButton,
-          }}
-          onConfirmText={renderButtonText()}
-          onConfirm={isReadyToInstall ? handleToInstall : handleToUpdate}
+          onConfirmText={intl.formatMessage({
+            id: appUpdateInfo.data.storeUrl
+              ? ETranslations.update_update_now
+              : ETranslations.update_download_and_verify_text,
+          })}
+          onConfirm={handleToUpdate}
         />
       </YStack>
     </Page.Footer>
