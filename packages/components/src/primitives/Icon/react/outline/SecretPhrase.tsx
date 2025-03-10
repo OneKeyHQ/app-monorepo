f9f360adc7073e61cd8841c32e12839f78022c0c
limichange <limichange@hotmail.com>
2025-03-10 16:51:03 +0800

    chore: Remove fillOpacity from Key2 and SecretPhrase icons OK-36263 (#6845)
    * Remove fillOpacity from Key2 and SecretPhrase icons
    
    * feat: Re-enable PrimeDebugPanel in development environment

--- packages/components/src/primitives/Icon/react/outline/SecretPhrase.tsx
+++ packages/components/src/primitives/Icon/react/outline/SecretPhrase.tsx
@@ -4,14 +4,12 @@ const SvgSecretPhrase = (props: SvgProps) => (
     <Path
       d="M6 9a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1ZM14 8a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3ZM6 12a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1ZM14 11a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3ZM6 15a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1ZM14 14a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2h-3Z"
       fill="currentColor"
-      fillOpacity={0.447}
     />
     <Path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M2 7a3 3 0 0 1 3-3h14a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V7Zm3-1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5Z"
       fill="currentColor"
-      fillOpacity={0.447}
     />
   </Svg>
 );
