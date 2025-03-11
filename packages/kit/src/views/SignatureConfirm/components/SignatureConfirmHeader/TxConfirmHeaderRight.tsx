e378808e954950c1b7974c3e9f2323ffcd175fa7
weatherstar <weather@onekey.so>
2025-03-11 18:14:38 +0800

    fix: mev fallback

--- packages/kit/src/views/SignatureConfirm/components/SignatureConfirmHeader/TxConfirmHeaderRight.tsx
+++ packages/kit/src/views/SignatureConfirm/components/SignatureConfirmHeader/TxConfirmHeaderRight.tsx
@@ -1,5 +1,6 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
+import { find } from 'lodash';
 import { useIntl } from 'react-intl';
 
 import {
@@ -12,17 +13,51 @@ import {
   YStack,
   useMedia,
 } from '@onekeyhq/components';
+import type { IUnsignedTxPro } from '@onekeyhq/core/src/types';
+import { getNetworksSupportMevProtection } from '@onekeyhq/shared/src/config/presetNetworks';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import type { IDecodedTx } from '@onekeyhq/shared/types/tx';
 
-function TxConfirmHeaderRight(props: { decodedTxs: IDecodedTx[] | undefined }) {
-  const { decodedTxs } = props;
+const mevProtectionProviders = getNetworksSupportMevProtection();
+
+function TxConfirmHeaderRight(props: {
+  decodedTxs: IDecodedTx[] | undefined;
+  unsignedTxs: IUnsignedTxPro[] | undefined;
+}) {
+  const { decodedTxs, unsignedTxs } = props;
   const intl = useIntl();
   const { gtMd } = useMedia();
 
   const decodedTx = decodedTxs?.[0];
 
-  const mevProtectionProvider = decodedTx?.txDisplay?.mevProtectionProvider;
+  const mevProtectionProvider = useMemo(() => {
+    if (!unsignedTxs) return null;
+
+    const swapTx = find(unsignedTxs, 'swapInfo');
+
+    if (swapTx && swapTx.swapInfo) {
+      let isBridge = false;
+
+      try {
+        isBridge =
+          swapTx.swapInfo.sender.accountInfo.networkId !==
+          swapTx.swapInfo.receiver.accountInfo.networkId;
+      } catch (e) {
+        isBridge = false;
+      }
+
+      if (
+        !isBridge &&
+        mevProtectionProviders[swapTx.swapInfo.receiver.accountInfo.networkId]
+      ) {
+        return mevProtectionProviders[
+          swapTx.swapInfo.receiver.accountInfo.networkId
+        ];
+      }
+    }
+
+    return decodedTx?.txDisplay?.mevProtectionProvider;
+  }, [unsignedTxs, decodedTx?.txDisplay?.mevProtectionProvider]);
 
   if (!mevProtectionProvider) return null;
 
@@ -32,28 +67,44 @@ function TxConfirmHeaderRight(props: { decodedTxs: IDecodedTx[] | undefined }) {
         title={intl.formatMessage({ id: ETranslations.mev_protection_label })}
         renderTrigger={<HeaderIconButton icon="ShieldCheckDoneOutline" />}
         renderContent={
-          <YStack p="$5" pt={gtMd ? '$5' : '$0'} gap="$5">
-            <SizableText size="$bodyLg">
-              {intl.formatMessage({ id: ETranslations.mev_protection_desc })}
-            </SizableText>
-            <YStack gap="$2">
-              <SizableText size="$bodyLg">
-                {intl.formatMessage({ id: ETranslations.global_power_by })}
+          <YStack p="$5" pt={gtMd ? '$5' : '$0'} gap="$2">
+            {gtMd ? (
+              <SizableText size="$headingMd">
+                {intl.formatMessage({ id: ETranslations.mev_protection_label })}
               </SizableText>
-              <Image width={160} height={40}>
-                <Image.Source
-                  source={{
-                    uri: mevProtectionProvider.logoURI,
-                  }}
-                />
-                <Image.Loading>
-                  <Skeleton width="100%" height="100%" />
-                </Image.Loading>
-              </Image>
+            ) : null}
+            <YStack gap="$5">
+              <SizableText size={gtMd ? '$bodyMd' : '$bodyLg'}>
+                {intl.formatMessage({ id: ETranslations.mev_protection_desc })}
+              </SizableText>
+              <YStack gap="$2">
+                <SizableText size={gtMd ? '$bodyMd' : '$bodyLg'}>
+                  Power by
+                </SizableText>
+                <Image width={160} height={40}>
+                  <Image.Source
+                    source={{
+                      uri: mevProtectionProvider.logoURI,
+                    }}
+                  />
+                  <Image.Loading>
+                    <Skeleton width="100%" height="100%" />
+                  </Image.Loading>
+                </Image>
+              </YStack>
+              <SizableText
+                size="$bodyMd"
+                fontStyle="italic"
+                color="$textSubdued"
+                style={{
+                  fontStyle: 'italic',
+                }}
+              >
+                {`*${intl.formatMessage({
+                  id: ETranslations.mev_protection_note,
+                })}`}
+              </SizableText>
             </YStack>
-            <SizableText size="$bodyMd" fontStyle="italic" color="$textSubdued">
-              {intl.formatMessage({ id: ETranslations.mev_protection_note })}
-            </SizableText>
           </YStack>
         }
       />
