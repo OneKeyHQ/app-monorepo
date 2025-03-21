b9f0cd7d3db2cac09ebbe802a78996793504f7c1
huhuanming <huanming@onekey.so>
2025-03-21 16:09:41 +0800

    fix: fix optinput

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/OTPInputGallery.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/OTPInputGallery.tsx
@@ -31,7 +31,6 @@ function OTPInputGallery() {
 
               <Button
                 onPress={() => {
-                  setText('');
                   setIsCompleted(false);
                 }}
               >
