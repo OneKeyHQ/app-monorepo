475b51839f17b425f2b75a42de2dc6b9dae84d81
huhuanming <huanming@onekey.so>
2025-03-26 00:51:40 +0800

    Update useReferFriends.tsx

--- packages/kit/src/hooks/useReferFriends.tsx
+++ packages/kit/src/hooks/useReferFriends.tsx
@@ -174,28 +174,30 @@ export const useReferFriends = () => {
                     iconColor="$iconSubdued"
                     onPress={() => copyText(myReferralCode)}
                   />
-                  <IconButton
-                    title={intl.formatMessage({
-                      id: ETranslations.global_copy,
-                    })}
-                    variant="tertiary"
-                    icon="ShareOutline"
-                    size="large"
-                    iconColor="$iconSubdued"
-                    onPress={() => {
-                      setTimeout(() => {
-                        void Share.share(
-                          platformEnv.isNativeIOS
-                            ? {
-                                url: sharedUrl,
-                              }
-                            : {
-                                message: sharedUrl,
-                              },
-                        );
-                      }, 300);
-                    }}
-                  />
+                  {platformEnv.isNative ? (
+                    <IconButton
+                      title={intl.formatMessage({
+                        id: ETranslations.global_copy,
+                      })}
+                      variant="tertiary"
+                      icon="ShareOutline"
+                      size="large"
+                      iconColor="$iconSubdued"
+                      onPress={() => {
+                        setTimeout(() => {
+                          void Share.share(
+                            platformEnv.isNativeIOS
+                              ? {
+                                  url: sharedUrl,
+                                }
+                              : {
+                                  message: sharedUrl,
+                                },
+                          );
+                        }, 300);
+                      }}
+                    />
+                  ) : null}
                 </XStack>
               </XStack>
             </YStack>
