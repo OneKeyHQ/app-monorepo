60e47f1fb10d26c3674d9c5b536cbb17f9e6127e
limichange <limichange@hotmail.com>
2025-03-25 16:48:23 +0800

    feat: enhance HeaderLeftToolBarInput with shortcut handling and search result display
    - Added keyboard shortcut handling for refreshing the input field on desktop platforms, improving accessibility.
    - Updated HeaderLeftToolBarInput to manage internal state for search input and integrated a new prop for displaying search results, enhancing user interaction.

--- packages/kit/src/views/Discovery/pages/Dashboard/Welcome/SearchInput.tsx
+++ packages/kit/src/views/Discovery/pages/Dashboard/Welcome/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import { useIntl } from 'react-intl';
 
