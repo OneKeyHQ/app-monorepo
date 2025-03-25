1320fa5ce3aad0d5a6f749029207a53f5045bf74
huhuanming <huanming@onekey.so>
2025-03-25 22:13:16 +0800

    Update index.tsx

--- packages/kit/src/views/ReferFriends/pages/YourReferred/index.tsx
+++ packages/kit/src/views/ReferFriends/pages/YourReferred/index.tsx
@@ -3,23 +3,28 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { Animated, Easing, Keyboard } from 'react-native';
 
-import {
-  AnimatePresence,
-  Empty,
-  Icon,
-  Image,
-  Page,
-  SizableText,
-  Tab,
-  XStack,
-  YStack,
-} from '@onekeyhq/components';
+import { Empty, Page, SizableText, Tab, YStack } from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import useHomePageWidth from '../../../Home/hooks/useHomePageWidth';
 
 let CONTENT_ITEM_WIDTH: Animated.Value | undefined;
+
+function EmptyData() {
+  const intl = useIntl();
+  return (
+    <Empty
+      icon="PeopleOutline"
+      title={intl.formatMessage({
+        id: ETranslations.referral_referred_empty,
+      })}
+      description={intl.formatMessage({
+        id: ETranslations.referral_reward_empty_desc,
+      })}
+    />
+  );
+}
 
 function HardwareSales() {
   return (
@@ -29,11 +34,7 @@ function HardwareSales() {
         <SizableText size="$bodyMd" color="$textSubdued">
           2025-01-26 21:46
         </SizableText>
-        <Empty
-          icon="PeopleOutline"
-          title="Start Earning Today"
-          description="Share your referral code to start earning rewards."
-        />
+        <EmptyData />
       </YStack>
     </YStack>
   );
@@ -53,15 +54,19 @@ export default function YourReferred() {
         page: HardwareSales,
       },
       {
-        title: 'Earn',
+        title: intl.formatMessage({
+          id: ETranslations.referral_referred_type_2,
+        }),
         page: HardwareSales,
       },
       {
-        title: 'Hardware Sales',
+        title: intl.formatMessage({
+          id: ETranslations.referral_referred_type_3,
+        }),
         page: HardwareSales,
       },
     ],
-    [],
+    [intl],
   );
 
   const onRefresh = useCallback(() => {}, []);
@@ -78,14 +83,20 @@ export default function YourReferred() {
   }, [pageWidth]);
   return (
     <Page scrollEnabled>
-      <Page.Header title="Your referred" />
+      <Page.Header
+        title={intl.formatMessage({ id: ETranslations.referral_your_referred })}
+      />
       <Page.Body>
         <Tab
           disableRefresh={!platformEnv.isNative}
           data={tabs}
           ListHeaderComponent={
             <YStack px="$5">
-              <SizableText size="$bodyLg">Total</SizableText>
+              <SizableText size="$bodyLg">
+                {intl.formatMessage({
+                  id: ETranslations.referral_referred_total,
+                })}
+              </SizableText>
               <SizableText size="$heading5xl">245</SizableText>
             </YStack>
           }
