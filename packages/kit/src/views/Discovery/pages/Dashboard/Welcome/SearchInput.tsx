2a3fcb950adf08872b9120749646f310858c4049
limichange <limichange@hotmail.com>
2025-03-28 12:54:58 +0800

    fix: clear search input on item click in SearchInput component
    - Added functionality to reset the search value when an item is clicked in the SearchInput component, enhancing user experience by providing immediate feedback.

--- packages/kit/src/views/Discovery/pages/Dashboard/Welcome/SearchInput.tsx
+++ packages/kit/src/views/Discovery/pages/Dashboard/Welcome/SearchInput.tsx
@@ -202,6 +202,7 @@ export function SearchInput() {
                 selectedIndex={selectedIndex}
                 innerRef={searchResultRef}
                 onItemClick={() => {
+                  setSearchValue('');
                   setIsPopoverOpen(false);
                 }}
               />
