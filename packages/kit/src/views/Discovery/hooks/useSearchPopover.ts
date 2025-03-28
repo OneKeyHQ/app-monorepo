8ee8f861fde754b21593434bfa41c7fb8e9ec12e
limichange <limichange@hotmail.com>
2025-03-28 15:02:41 +0800

    refactor: disable animation on scroll in useSearchPopover hook
    - Updated the scrollTo method in the useSearchPopover hook to set animated to false, improving the user experience by providing a more immediate response when resetting the scroll position.

--- packages/kit/src/views/Discovery/hooks/useSearchPopover.ts
+++ packages/kit/src/views/Discovery/hooks/useSearchPopover.ts
@@ -56,6 +56,7 @@ export function useSearchPopover({
     if (scrollViewRef.current) {
       scrollViewRef.current.scrollTo({
         y: 0,
+        animated: false,
       });
     }
   }, [searchValue, scrollViewRef]);
