2aea015e46ceef1206c111946b68a2af964d37a5
huhuanming <huanming@onekey.so>
2025-04-03 10:23:02 +0800

    Update MoreActionButton.tsx

--- packages/kit/src/components/TabPageHeader/MoreActionButton.tsx
+++ packages/kit/src/components/TabPageHeader/MoreActionButton.tsx
@@ -184,25 +184,27 @@ export function MoreActionButton() {
             ...popupMenu,
           ].filter(Boolean),
         },
-        devSettings.settings?.showOneKeyId && {
-          items: [
-            {
-              label: 'OneKey ID',
-              icon: 'PeopleOutline',
-              onPress: async () => {
-                await loginOneKeyId({ toOneKeyIdPageOnLoginSuccess: true });
-              },
-              testID: 'onekey_id',
-            },
-            {
-              label: intl.formatMessage({
-                id: ETranslations.id_refer_a_friend,
-              }),
-              icon: 'GiftOutline',
-              onPress: toReferFriendsPage,
-              testID: 'refer-a-friend',
-            },
-          ],
+        {
+          items: devSettings.settings?.showOneKeyId
+            ? [
+                {
+                  label: 'OneKey ID',
+                  icon: 'PeopleOutline',
+                  onPress: async () => {
+                    await loginOneKeyId({ toOneKeyIdPageOnLoginSuccess: true });
+                  },
+                  testID: 'onekey_id',
+                },
+                {
+                  label: intl.formatMessage({
+                    id: ETranslations.id_refer_a_friend,
+                  }),
+                  icon: 'GiftOutline',
+                  onPress: toReferFriendsPage,
+                  testID: 'refer-a-friend',
+                },
+              ]
+            : [],
         },
         {
           items: [
@@ -233,7 +235,7 @@ export function MoreActionButton() {
             },
           ],
         },
-      ].filter(Boolean)}
+      ]}
     />
   );
 }
