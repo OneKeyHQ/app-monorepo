6e8efd7a49d30e3d259c838fbed50a5c3cc8e706
weatherstar <weather@onekey.so>
2025-04-07 18:34:11 +0800

    fix: wrong token list in selector

--- packages/kit/src/views/AssetSelector/pages/TokenSelector.tsx
+++ packages/kit/src/views/AssetSelector/pages/TokenSelector.tsx
@@ -28,12 +28,19 @@ import { HomeTokenListProviderMirrorWrapper } from '../../Home/components/HomeTo
 
 import type { RouteProp } from '@react-navigation/core';
 import type { TextInputFocusEventData } from 'react-native';
+import { usePromiseResult } from '../../../hooks/usePromiseResult';
+import networkUtils from '@onekeyhq/shared/src/utils/networkUtils';
 
 const num = 0;
 
 function TokenSelector() {
   const intl = useIntl();
-  const { updateCreateAccountState } = useTokenListActions().current;
+  const {
+    updateCreateAccountState,
+    refreshActiveAccountTokenList,
+    refreshTokenListMap,
+    updateActiveAccountTokenListState,
+  } = useTokenListActions().current;
 
   const route =
     useRoute<
@@ -54,6 +61,8 @@ function TokenSelector() {
     isAllNetworks,
     searchPlaceholder,
     footerTipText,
+    activeAccountId,
+    activeNetworkId,
   } = route.params;
 
   const { network, account } = useAccountData({ networkId, accountId });
@@ -242,6 +251,54 @@ function TokenSelector() {
     [accountId, networkId],
   );
 
+  const showActiveAccountTokenList = useMemo(() => {
+    return !!(
+      activeAccountId &&
+      activeNetworkId &&
+      activeAccountId !== accountId &&
+      activeNetworkId !== networkId
+    );
+  }, [activeAccountId, activeNetworkId, accountId, networkId]);
+
+  usePromiseResult(async () => {
+    if (activeAccountId && activeNetworkId && showActiveAccountTokenList) {
+      updateActiveAccountTokenListState({
+        initialized: false,
+        isRefreshing: true,
+      });
+      const r = await backgroundApiProxy.serviceToken.fetchAccountTokens({
+        accountId,
+        networkId,
+        flag: 'token-selector',
+      });
+
+      refreshActiveAccountTokenList({
+        tokens: [...r.tokens.data, ...r.smallBalanceTokens.data],
+        keys: `${r.tokens.keys}_${r.smallBalanceTokens.keys}`,
+      });
+      refreshTokenListMap({
+        tokens: {
+          ...r.tokens.map,
+          ...r.smallBalanceTokens.map,
+        },
+        merge: true,
+      });
+      updateActiveAccountTokenListState({
+        isRefreshing: false,
+        initialized: true,
+      });
+    }
+  }, [
+    accountId,
+    activeAccountId,
+    activeNetworkId,
+    networkId,
+    refreshActiveAccountTokenList,
+    refreshTokenListMap,
+    showActiveAccountTokenList,
+    updateActiveAccountTokenListState,
+  ]);
+
   useEffect(() => {
     if (searchAll && searchKey && searchKey.length >= SEARCH_KEY_MIN_LENGTH) {
       void searchTokensBySearchKey(searchKey);
@@ -269,6 +326,7 @@ function TokenSelector() {
       />
       <Page.Body>
         <TokenListView
+          showActiveAccountTokenList={showActiveAccountTokenList}
           withPresetVerticalPadding={false}
           onPressToken={handleTokenOnPress}
           isAllNetworks={isAllNetworks ?? network?.isAllNetworks}
