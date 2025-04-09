a04f8486717805c32df6db2b8aa851149249563c
huhuanming <huanming@onekey.so>
2025-04-09 23:29:46 +0800

    Update index.tsx

--- packages/kit/src/views/ReferFriends/pages/InviteReward/index.tsx
+++ packages/kit/src/views/ReferFriends/pages/InviteReward/index.tsx
@@ -497,6 +497,8 @@ export default function InviteReward() {
     [],
     {
       initResult: undefined,
+      revalidateOnFocus: true,
+      revalidateOnReconnect: true,
     },
   );
   return (
