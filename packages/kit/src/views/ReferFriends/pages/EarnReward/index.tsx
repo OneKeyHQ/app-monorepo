3e8fefa209ffadaeb38f543daecc118324824f9a
huhuanming <huanming@onekey.so>
2025-04-11 01:59:44 +0800

    Update index.tsx

--- packages/kit/src/views/ReferFriends/pages/EarnReward/index.tsx
+++ packages/kit/src/views/ReferFriends/pages/EarnReward/index.tsx
@@ -19,11 +19,10 @@ import {
 } from '@onekeyhq/components';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
 import { useSpotlight } from '@onekeyhq/kit/src/components/Spotlight';
+import { Token } from '@onekeyhq/kit/src/components/Token';
 import { useSettingsPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import { ESpotlightTour } from '@onekeyhq/shared/src/spotlight';
-
-import { Token } from '../../../../components/Token';
 
 function EmptyData() {
   const intl = useIntl();
@@ -207,8 +206,144 @@ function UndistributedList() {
 }
 
 function TotalList() {
-  return null;
-}
+  const intl = useIntl();
+  const [settings] = useSettingsPersistAtom();
+  const sectionData = [
+    {
+      title: '0x1234...1234',
+      amount: '10.25',
+      data: [
+        {
+          name: 'Vault name',
+          action: '10 ETH deposited',
+          token: {
+            uri: 'https://uni.onekey-asset.com/static/chain/btc.png',
+            symbol: 'USDC',
+            amount: 10.25,
+            fiatAmount: 0.1,
+          },
+        },
+      ],
+    },
+  ];
+  return (
+    <YStack px="$5" py="$2">
+      <ListHeader />
+      {sectionData.map(({ title, amount, data }, index) => (
+        <YStack key={index}>
+          <Accordion type="multiple" gap="$2">
+            <Accordion.Item value={String(index)}>
+              <Accordion.Trigger
+                unstyled
+                flexDirection="row"
+                alignItems="center"
+                borderWidth={0}
+                bg="$transparent"
+                px="$2"
+                py="$1"
+                mx="$-2"
+                my="$-1"
+                hoverStyle={{
+                  bg: '$bgHover',
+                }}
+                pressStyle={{
+                  bg: '$bgActive',
+                }}
+                borderRadius="$2"
+              >
+                {({ open }: { open: boolean }) => (
+                  <XStack my="$3" jc="space-between" flex={1}>
+                    <SizableText
+                      textAlign="left"
+                      flex={1}
+                      size="$bodyLgMedium"
+                      color={open ? '$text' : '$textSubdued'}
+                    >
+                      {title}
+                    </SizableText>
+                    <XStack ai="center" gap="$2">
+                      <NumberSizeableText
+                        color="$textSuccess"
+                        formatter="balance"
+                        size="$bodyLgMedium"
+                        formatterOptions={{
+                          currency: settings.currencyInfo.symbol,
+                          showPlusMinusSigns: true,
+                        }}
+                      >
+                        {amount}
+                      </NumberSizeableText>
+                      <Stack
+                        animation="quick"
+                        rotate={open ? '180deg' : '0deg'}
+                      >
+                        <Icon
+                          name="ChevronDownSmallOutline"
+                          color={open ? '$iconActive' : '$iconSubdued'}
+                          size="$5"
+                        />
+                      </Stack>
+                    </XStack>
+                  </XStack>
+                )}
+              </Accordion.Trigger>
+              <Accordion.HeightAnimator animation="quick">
+                <Accordion.Content
+                  unstyled
+                  pt="$2"
+                  pb="$5"
+                  animation="100ms"
+                  enterStyle={{ opacity: 0 }}
+                  exitStyle={{ opacity: 0 }}
+                >
+                  {data.map((item, itemIndex) => (
+                    <XStack
+                      ai="center"
+                      jc="space-between"
+                      key={itemIndex}
+                      py="$2"
+                    >
+                     <SizableText size="$bodyMd">{item.name}</SizableText>
+                      <XStack ai="center">
+                        <Token size="xs" networkId="evm--1" mr="$2" />
+                        <NumberSizeableText
+                          mr="$1"
+                          formatter="balance"
+                          size="$bodyMd"
+                          formatterOptions={{
+                            tokenSymbol: item.token.symbol,
+                          }}
+                        >
+                          {item.token.amount}
+                        </NumberSizeableText>
+                        <XStack ai="center">
+                          <SizableText size="$bodyMd" color="$textSubdued">
+                            (
+                          </SizableText>
+                          <NumberSizeableText
+                            formatter="balance"
+                            size="$bodyMd"
+                            formatterOptions={{
+                              currency: settings.currencyInfo.symbol,
+                            }}
+                          >
+                            {item.token.fiatAmount}
+                          </NumberSizeableText>
+                          <SizableText size="$bodyMd" color="$textSubdued">
+                            )
+                          </SizableText>
+                        </XStack>
+                      </XStack>
+                    </XStack>
+                  ))}
+                </Accordion.Content>
+              </Accordion.HeightAnimator>
+            </Accordion.Item>
+          </Accordion>
+        </YStack>
+      ))}
+    </YStack>
+  );
 
 export default function EarnReward() {
   const intl = useIntl();
