be999645223ad0d1397d41306e6eb007ada1b375
huhuanming <huanming@onekey.so>
2025-04-11 11:22:04 +0800

    Update ServiceReferralCode.ts

--- packages/kit-bg/src/services/ServiceReferralCode.ts
+++ packages/kit-bg/src/services/ServiceReferralCode.ts
@@ -34,12 +34,11 @@ class ServiceReferralCode extends ServiceBase {
   }
 
   @backgroundMethod()
-  async bindAddress(networkId: string, address: string, emailOTP: string) {
+  async bindAddress(networkId: string, address: string) {
     const client = await this.getOneKeyIdClient(EServiceEndpointEnum.Rebate);
     await client.post('/rebate/v1/address', {
       address,
       networkId,
-      emailOTP,
     });
   }
 
