945c40742d04bc166993c2070cb72b03ed27061b
huhuanming <huanming@onekey.so>
2025-04-18 19:33:21 +0800

    Update FaqSection.tsx

--- packages/kit/src/views/Staking/components/ProtocolDetails/FaqSection.tsx
+++ packages/kit/src/views/Staking/components/ProtocolDetails/FaqSection.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import { useIntl } from 'react-intl';
 
 import {
@@ -9,17 +11,85 @@ import {
 } from '@onekeyhq/components';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
 import { HyperlinkText } from '@onekeyhq/kit/src/components/HyperlinkText';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
+import { useUserWalletProfile } from '@onekeyhq/kit/src/hooks/useUserWalletProfile';
+import { useActiveAccount } from '@onekeyhq/kit/src/states/jotai/contexts/accountSelector';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import { defaultLogger } from '@onekeyhq/shared/src/logger/logger';
+import { EModalRoutes } from '@onekeyhq/shared/src/routes/modal';
+import { EModalSwapRoutes } from '@onekeyhq/shared/src/routes/swap';
+import { getImportFromToken } from '@onekeyhq/shared/types/earn/earnProvider.constants';
 import type { IStakeProtocolDetails } from '@onekeyhq/shared/types/staking';
+import {
+  ESwapSource,
+  ESwapTabSwitchType,
+} from '@onekeyhq/shared/types/swap/types';
 
 type ISolution = {
   question: string;
   answer: string;
 };
 
-function FaqInfo({ solutions }: { solutions: ISolution[] }) {
+function FaqInfo({
+  solutions,
+  token: tokenInfo,
+}: {
+  solutions: ISolution[];
+  token?: IStakeProtocolDetails['token'];
+}) {
   const intl = useIntl();
+  const navigation = useAppNavigation();
+  const {
+    activeAccount: { wallet },
+  } = useActiveAccount({ num: 0 });
+  const { isSoftwareWalletOnlyUser } = useUserWalletProfile();
+  const networkId = tokenInfo?.info.networkId ?? '';
+  const token = tokenInfo?.info;
+
+  const handleAction = useCallback(
+    async (actionId: string) => {
+      const id = actionId.trim();
+      if (id === 'trade_usdf') {
+        const { isSupportSwap } =
+          await backgroundApiProxy.serviceSwap.checkSupportSwap({
+            networkId,
+          });
+        const network = await backgroundApiProxy.serviceNetwork.getNetwork({
+          networkId,
+        });
+        const { importFromToken, swapTabSwitchType } = getImportFromToken({
+          networkId,
+          isSupportSwap,
+          tokenAddress: token?.address ?? '',
+        });
+        defaultLogger.wallet.walletActions.actionTrade({
+          walletType: wallet?.type ?? '',
+          networkId,
+          source: 'earn',
+          tradeType: ESwapTabSwitchType.SWAP,
+          isSoftwareWalletOnlyUser,
+        });
+        navigation.pushModal(EModalRoutes.SwapModal, {
+          screen: EModalSwapRoutes.SwapMainLand,
+          params: {
+            importToToken: {
+              ...(token ?? {}),
+              contractAddress: token?.address ?? '',
+              networkId,
+              networkLogoURI: network.logoURI,
+              decimals: token?.decimals ?? 0,
+              symbol: token?.symbol ?? '',
+            },
+            importFromToken,
+            swapTabSwitchType,
+            swapSource: ESwapSource.EARN,
+          },
+        });
+      }
+    },
+    [isSoftwareWalletOnlyUser, navigation, networkId, token, wallet?.type],
+  );
   return (
     <YStack gap="$6">
       <SizableText size="$headingLg">
@@ -80,11 +150,9 @@ function FaqInfo({ solutions }: { solutions: ISolution[] }) {
                     size="$bodyMd"
                     translationId={answer as ETranslations}
                     defaultMessage={answer}
-                    onAction={(actionId) => {
-                      const id = actionId.trim();
-                      if (id === 'trade_usdf') {
-                        console.log('111');
-                      }
+                    onAction={handleAction}
+                    underlineTextProps={{
+                      color: '$textInfo',
                     }}
                   />
                 </Accordion.Content>
@@ -116,5 +184,5 @@ export const FaqSection = ({
     },
   );
   if (solutions.length === 0) return null;
-  return <FaqInfo solutions={solutions} />;
+  return <FaqInfo solutions={solutions} token={details?.token} />;
 };
