a41828ebee65384fadd04499cd0fbc4e84b0581e
yuanx <yuanx.me@gmail.com>
2025-04-19 23:11:11 +0800

    feat: handling edge case

--- packages/kit-bg/src/vaults/impls/aptos/sdkAptos/AptosClient.ts
+++ packages/kit-bg/src/vaults/impls/aptos/sdkAptos/AptosClient.ts
@@ -57,10 +57,16 @@ export class AptosClient {
         typeof message === 'string' &&
         message.includes('account_not_found')
       ) {
-        const { message: errorMessage } = JSON.parse(message);
-        throw new InvalidAccount({
-          message: errorMessage,
-        });
+        try {
+          const { message: errorMessage } = JSON.parse(message);
+          throw new InvalidAccount({
+            message: errorMessage,
+          });
+        } catch (_) {
+          throw new InvalidAccount({
+            message,
+          });
+        }
       }
       throw error;
     }
