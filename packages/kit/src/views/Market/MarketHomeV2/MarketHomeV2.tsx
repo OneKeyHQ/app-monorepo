8b0faf84c88b21696e0dfe414d98a57eb7941fe5
limichange <limichange@hotmail.com>
2025-04-27 21:20:22 +0800

    refactor: simplify MarketHomeV2 component by removing unused imports and optimizing dependencies
    - Removed unused imports including various React hooks and components to streamline the code.
    - Retained essential imports for functionality while enhancing readability and maintainability of the MarketHomeV2 component.

--- packages/kit/src/views/Market/MarketHomeV2/MarketHomeV2.tsx
+++ packages/kit/src/views/Market/MarketHomeV2/MarketHomeV2.tsx
@@ -1,41 +1,16 @@
-import type { ForwardedRef } from 'react';
-import {
-  forwardRef,
-  useCallback,
-  useEffect,
-  useImperativeHandle,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import { useEffect } from 'react';
 
 import { Animated, Easing } from 'react-native';
 
-import {
-  Icon,
-  Page,
-  Spinner,
-  Stack,
-  Tab,
-  useMedia,
-} from '@onekeyhq/components';
-import type { IColorTokens } from '@onekeyhq/components';
+import { Page, Stack, useMedia } from '@onekeyhq/components';
 import { EJotaiContextStoreNames } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
-import {
-  EAppEventBusNames,
-  appEventBus,
-} from '@onekeyhq/shared/src/eventBus/appEventBus';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
 
-import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
 import { AccountSelectorProviderMirror } from '../../../components/AccountSelector';
-import { usePromiseResult } from '../../../hooks/usePromiseResult';
 import useHomePageWidth from '../../Home/hooks/useHomePageWidth';
 import { MarketHomeHeader } from '../components/MarketHomeHeader';
 import { MarketHomeHeader as MDMarketHomeHeader } from '../components/MarketHomeHeader.md';
-import { MarketHomeList } from '../components/MarketHomeList';
-import { MarketWatchList } from '../components/MarketWatchList';
 import { MarketWatchListProviderMirror } from '../MarketWatchListProviderMirror';
 
 import { MarketFilterBar } from './components/MarketFilterBar';
