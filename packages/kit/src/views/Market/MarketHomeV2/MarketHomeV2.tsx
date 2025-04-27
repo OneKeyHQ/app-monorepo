2d789d04daeaf2f31317df0563716ad8bf7e1aea
limichange <limichange@hotmail.com>
2025-04-27 13:04:36 +0800

    refactor: simplify MarketHome component by removing unused animated icon and tab configuration logic

--- packages/kit/src/views/Market/MarketHomeV2/MarketHomeV2.tsx
+++ packages/kit/src/views/Market/MarketHomeV2/MarketHomeV2.tsx
@@ -43,43 +43,10 @@ import { MarketTokenList } from './components/MarketTokenList';
 import MarketTokenListNetworkSelector from './components/MarketTokenListNetworkSelector/MarketTokenListNetworkSelector';
 import { RiskIndicatorCardDemo } from './components/RiskIndicatorCard/RiskIndicatorCardDemo';
 
-type IAnimatedIconRef = { setIsSelected: (isSelected: boolean) => void };
-function BasicAnimatedIcon(
-  {
-    normalColor,
-    selectedColor,
-  }: {
-    normalColor: IColorTokens;
-    selectedColor: IColorTokens;
-  },
-  ref: ForwardedRef<IAnimatedIconRef>,
-) {
-  const [color, setColor] = useState(selectedColor);
-  const isSelectedValue = useRef(false);
-  useImperativeHandle(
-    ref,
-    () => ({
-      setIsSelected: (isSelected: boolean) => {
-        isSelectedValue.current = isSelected;
-        setColor(isSelected ? selectedColor : normalColor);
-      },
-    }),
-    [normalColor, selectedColor],
-  );
-  useEffect(() => {
-    if (color !== normalColor && color !== selectedColor) {
-      setColor(isSelectedValue.current ? selectedColor : normalColor);
-    }
-  }, [selectedColor, normalColor, color]);
-  return <Icon name="StarOutline" color={color} size="$4.5" px="$1" />;
-}
-
-const AnimatedIcon = forwardRef(BasicAnimatedIcon);
-
 let CONTENT_ITEM_WIDTH: Animated.Value | undefined;
 
 function MarketHome() {
-  const { screenWidth, pageWidth } = useHomePageWidth();
+  const { pageWidth } = useHomePageWidth();
   if (CONTENT_ITEM_WIDTH == null) {
     CONTENT_ITEM_WIDTH = new Animated.Value(pageWidth);
   }
@@ -95,88 +62,8 @@ function MarketHome() {
     }).start();
   }, [pageWidth]);
 
-  const { result: categories } = usePromiseResult(
-    () => backgroundApiProxy.serviceMarket.fetchCategories(),
-    [],
-    {
-      revalidateOnReconnect: true,
-    },
-  );
-
   const { gtMd } = useMedia();
 
-  const tabConfig = useMemo(
-    () =>
-      categories?.map((category, index) => ({
-        title: category.name,
-        // eslint-disable-next-line react/no-unstable-nested-components
-        page: () =>
-          index === 0 ? (
-            <MarketWatchList category={category} />
-          ) : (
-            <MarketHomeList category={category} tabIndex={index} />
-          ),
-      })) || [],
-    [categories],
-  );
-
-  const ref = useRef<IAnimatedIconRef>(null);
-  const headerProps = useMemo(
-    () => ({
-      showHorizontalScrollButton: !gtMd && platformEnv.isRuntimeBrowser,
-      renderItem: (item: any, index: any, titleStyle: any) =>
-        index === 0 && !gtMd ? (
-          <AnimatedIcon
-            ref={ref}
-            normalColor={
-              (titleStyle as { normalColor: IColorTokens })?.normalColor
-            }
-            selectedColor={
-              (titleStyle as { selectedColor: IColorTokens })?.selectedColor
-            }
-          />
-        ) : (
-          <Tab.SelectedLabel {...titleStyle} />
-        ),
-    }),
-    [gtMd],
-  );
-
-  const handleSelectedPageIndex = useCallback((index: number) => {
-    ref?.current?.setIsSelected?.(index === 0);
-    appEventBus.emit(EAppEventBusNames.SwitchMarketHomeTab, {
-      tabIndex: index,
-    });
-    console.log('选中', index, index === 0 ? 1 : 0);
-  }, []);
-  const renderTabContainer = useCallback(() => {
-    if (!tabConfig.length) {
-      return (
-        <Stack flex={1} ai="center" jc="center">
-          <Spinner size="large" />
-        </Stack>
-      );
-    }
-    if (platformEnv.isNativeAndroid) {
-      return (
-        <Tab
-          disableRefresh
-          data={tabConfig}
-          onSelectedPageIndex={handleSelectedPageIndex}
-        />
-      );
-    }
-    return (
-      <Tab.Page
-        data={tabConfig}
-        contentItemWidth={CONTENT_ITEM_WIDTH}
-        contentWidth={screenWidth}
-        headerProps={headerProps}
-        onSelectedPageIndex={handleSelectedPageIndex}
-        windowSize={3}
-      />
-    );
-  }, [handleSelectedPageIndex, headerProps, tabConfig, screenWidth]);
   return (
     <Page>
       {gtMd && !platformEnv.isNativeIOSPad ? (
@@ -189,7 +76,6 @@ function MarketHome() {
         <RiskIndicatorCardDemo />
         <MarketFilterBar />
         <MarketTokenList />
-        {renderTabContainer()}
       </Page.Body>
     </Page>
   );
