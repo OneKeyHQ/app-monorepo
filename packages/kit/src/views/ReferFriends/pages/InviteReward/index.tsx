5adaf1f1a2d6c3948da59a3832554b4afb464a20
huhuanming <huanming@onekey.so>
2025-05-09 17:12:43 +0800

    Update index.tsx

--- packages/kit/src/views/ReferFriends/pages/InviteReward/index.tsx
+++ packages/kit/src/views/ReferFriends/pages/InviteReward/index.tsx
@@ -565,25 +565,13 @@ function Dashboard({
         borderWidth={StyleSheet.hairlineWidth}
         borderColor="$borderSubdued"
         borderRadius="$3"
-        // onPress={toEarnRewardPage}
+        onPress={toEarnRewardPage}
       >
         <XStack ai="center" jc="space-between">
-          <XStack ai="flex-end">
-            <SizableText size="$headingMd">
-              {intl.formatMessage({ id: ETranslations.referral_earn_reward })}
-            </SizableText>
-            <SizableText
-              size="$bodySm"
-              color="$textSubdued"
-              position="relative"
-              top={-2}
-            >
-              {`  (${intl.formatMessage({
-                id: ETranslations.coming_soon,
-              })})`}
-            </SizableText>
-          </XStack>
-          {/* <Icon size="$4.5" color="$iconSubdued" name="ChevronRightOutline" /> */}
+          <SizableText size="$headingMd">
+            {intl.formatMessage({ id: ETranslations.referral_earn_reward })}
+          </SizableText>
+          <Icon size="$4.5" color="$iconSubdued" name="ChevronRightOutline" />
         </XStack>
         <SizableText mt="$0.5" size="$bodyMd" color="$textSubdued">
           {intl.formatMessage({ id: ETranslations.referral_earn_reward_desc })}
