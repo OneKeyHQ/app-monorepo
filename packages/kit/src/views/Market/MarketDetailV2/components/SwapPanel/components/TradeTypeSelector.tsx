0627c54d71850d0ab6b0fe106f9532d30aa578f9
limichange <limichange@hotmail.com>
2025-05-15 21:55:00 +0800

    feat: enhance TradeTypeSelector with button components and internationalization
    - Replaced static labels in the `TradeTypeSelector` with dynamic buttons that utilize the `useIntl` hook for internationalization.
    - Introduced common button style properties for consistent styling across buy and sell buttons.
    - Updated the options for the `SegmentControl` to improve user interaction and visual feedback based on the selected trade type.

--- packages/kit/src/views/Market/MarketDetailV2/components/SwapPanel/components/TradeTypeSelector.tsx
+++ packages/kit/src/views/Market/MarketDetailV2/components/SwapPanel/components/TradeTypeSelector.tsx
@@ -1,6 +1,26 @@
-import { SegmentControl } from '@onekeyhq/components';
+import { useIntl } from 'react-intl';
+
+import {
+  Button,
+  type IButtonProps,
+  SegmentControl,
+} from '@onekeyhq/components';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 import type { ITradeType } from '../useSwapPanel';
+
+const commonButtonStyleProps: IButtonProps = {
+  flex: 1,
+  borderRadius: '$2',
+  borderWidth: 0,
+  height: '$8',
+  hoverStyle: {
+    opacity: 0.9,
+  },
+  pressStyle: {
+    opacity: 0.7,
+  },
+};
 
 export function TradeTypeSelector({
   value,
@@ -9,15 +29,53 @@ export function TradeTypeSelector({
   value: ITradeType;
   onChange: (value: ITradeType) => void;
 }) {
+  const intl = useIntl();
+  const isBuyActive = value === 'buy';
+  const isSellActive = value === 'sell';
+
+  const options = [
+    {
+      value: 'buy' as ITradeType,
+      label: (
+        <Button
+          {...commonButtonStyleProps}
+          bg={isBuyActive ? '$iconSuccess' : '$transparent'}
+          color={isBuyActive ? '$textOnColor' : '$textSubdued'}
+        >
+          {intl.formatMessage({ id: ETranslations.global_buy })}
+        </Button>
+      ),
+    },
+    {
+      value: 'sell' as ITradeType,
+      label: (
+        <Button
+          bg={isSellActive ? '$iconCritical' : '$transparent'}
+          color={isSellActive ? '$textOnColor' : '$textSubdued'}
+          {...commonButtonStyleProps}
+        >
+          {intl.formatMessage({ id: ETranslations.global_sell })}
+        </Button>
+      ),
+    },
+  ];
+
   return (
     <SegmentControl
       value={value}
-      options={[
-        { label: 'Buy', value: 'buy' },
-        { label: 'Sell', value: 'sell' },
-      ]}
-      onChange={(v) => {
-        onChange(v as ITradeType);
+      onChange={(newValue) => {
+        if (newValue === 'buy' || newValue === 'sell') {
+          onChange(newValue as ITradeType);
+        }
+      }}
+      options={options}
+      backgroundColor="$neutral5"
+      borderRadius="$2.5"
+      p="$0.5"
+      fullWidth
+      segmentControlItemStyleProps={{
+        bg: '$transparent',
+        p: 0,
       }}
     />
   );
