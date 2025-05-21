981f84bfb9bfca44cfa17f53dfd69fbcb9e782d0
huhuanming <huanming@onekey.so>
2025-05-21 12:13:39 +0800

    Update useLoadCustomFonts.ts

--- packages/components/src/hocs/Provider/hooks/useLoadCustomFonts.ts
+++ packages/components/src/hocs/Provider/hooks/useLoadCustomFonts.ts
@@ -1,15 +1,15 @@
 import { useFonts } from 'expo-font';
 
 const customFont = {
-  'GeistMono-Black': require('../fonts/GeistMono-Black.ttf'),
-  'GeistMono-Bold': require('../fonts/GeistMono-Bold.ttf'),
-  'GeistMono-Light': require('../fonts/GeistMono-Light.ttf'),
+  // 'GeistMono-Black': require('../fonts/GeistMono-Black.ttf'),
+  // 'GeistMono-Bold': require('../fonts/GeistMono-Bold.ttf'),
+  // 'GeistMono-Light': require('../fonts/GeistMono-Light.ttf'),
   'GeistMono-Medium': require('../fonts/GeistMono-Medium.ttf'),
   'GeistMono-Regular': require('../fonts/GeistMono-Regular.ttf'),
-  'GeistMono-SemiBold': require('../fonts/GeistMono-SemiBold.ttf'),
-  'GeistMono-Thin': require('../fonts/GeistMono-Thin.ttf'),
-  'GeistMono-UltraBlack': require('../fonts/GeistMono-UltraBlack.ttf'),
-  'GeistMono-UltraLight': require('../fonts/GeistMono-UltraLight.ttf'),
+  // 'GeistMono-SemiBold': require('../fonts/GeistMono-SemiBold.ttf'),
+  // 'GeistMono-Thin': require('../fonts/GeistMono-Thin.ttf'),
+  // 'GeistMono-UltraBlack': require('../fonts/GeistMono-UltraBlack.ttf'),
+  // 'GeistMono-UltraLight': require('../fonts/GeistMono-UltraLight.ttf'),
 };
 
 export default function useLoadCustomFonts() {
