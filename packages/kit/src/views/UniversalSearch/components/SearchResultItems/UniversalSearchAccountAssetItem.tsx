77ac0dd46a49864c0c5a7e0a7764b5cf53886c08
Leon <lixiao.dev@gmail.com>
2025-05-23 09:55:38 +0800

    OK-38189: Support search account on universal search page (#7383)
    * fix: Add footer components for UniversalSearchList
    
    * feat: Enhance UniversalSearch with section footer and data slicing functionality
    
    * feat: Add Dapp search functionality to UniversalSearch, enhancing search capabilities with new result type and rendering logic
    
    * feat: Integrate DiscoveryBrowserProviderMirror into UniversalSearch for enhanced website handling
    
    * feat: Refactor search modal data handling by centralizing constants and improving Google search item logic
    
    * refactor: Remove console logs and clean up renderSectionFooter in UniversalSearch for improved code clarity
    
    * feat: Add "Show More" functionality to section footer in UniversalSearch for improved user interaction
    
    * OK-38195, OK-38191, OK-38159: Add dApp results to universal search (#7372)
    
    * fix: Add footer components for UniversalSearchList
    
    * feat: Enhance UniversalSearch with section footer and data slicing functionality
    
    * feat: Add UniversalSearch shortcut to ShortcutsPreview for improved accessibility
    
    * feat: Add Dapp search functionality to UniversalSearch, enhancing search capabilities with new result type and rendering logic
    
    * feat: Integrate DiscoveryBrowserProviderMirror into UniversalSearch for enhanced website handling
    
    * feat: Refactor search modal data handling by centralizing constants and improving Google search item logic
    
    * refactor: Remove console logs and clean up renderSectionFooter in UniversalSearch for improved code clarity
    
    * fix: Add footer components for UniversalSearchList
    
    * feat: Enhance UniversalSearch with section footer and data slicing functionality
    
    * feat: Add Dapp search functionality to UniversalSearch, enhancing search capabilities with new result type and rendering logic
    
    * feat: Refactor search modal data handling by centralizing constants and improving Google search item logic
    
    * feat: Enhance UniversalSearch with account avatar rendering and improved address validation logic
    
    * feat: switch to select account
    
    * feat: Add accountsValue
    
    * feat: Introduce UniversalSearchDappItem component for improved Dapp rendering in UniversalSearch
    
    * feat: Refactor UniversalSearchDappItem to use getSearchInput and streamline website handling logic
    
    * feat: Add UniversalSearchAccountAssetItem component for rendering account asset details in UniversalSearch
    
    * feat: Add UniversalSearchMarketTokenItem component for rendering market token details in UniversalSearch
    
    * feat: Add UniversalSearchAddressItem component for rendering address details in UniversalSearch
    
    * feat: Refactor UniversalSearchAddressItem to improve account value rendering and address shortening logic

---
+++ packages/kit/src/views/UniversalSearch/components/SearchResultItems/UniversalSearchAccountAssetItem.tsx
@@ -0,0 +1,123 @@
+import { useCallback } from 'react';
+
+import BigNumber from 'bignumber.js';
+
+import { NumberSizeableText, Stack } from '@onekeyhq/components';
+import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
+import NumberSizeableTextWrapper from '@onekeyhq/kit/src/components/NumberSizeableTextWrapper';
+import { Token, TokenName } from '@onekeyhq/kit/src/components/Token';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
+import { useActiveAccount } from '@onekeyhq/kit/src/states/jotai/contexts/accountSelector';
+import { useSettingsPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+import {
+  EModalAssetDetailRoutes,
+  EModalRoutes,
+} from '@onekeyhq/shared/src/routes';
+import networkUtils from '@onekeyhq/shared/src/utils/networkUtils';
+import { getTokenPriceChangeStyle } from '@onekeyhq/shared/src/utils/tokenUtils';
+import type { IUniversalSearchAccountAssets } from '@onekeyhq/shared/types/search';
+
+interface IUniversalSearchAccountAssetItemProps {
+  item: IUniversalSearchAccountAssets;
+}
+
+export function UniversalSearchAccountAssetItem({
+  item,
+}: IUniversalSearchAccountAssetItemProps) {
+  const navigation = useAppNavigation();
+  const { activeAccount } = useActiveAccount({ num: 0 });
+  const [settings] = useSettingsPersistAtom();
+  const { token, tokenFiat } = item.payload;
+  const priceChange = tokenFiat?.price24h ?? 0;
+  const { changeColor, showPlusMinusSigns } = getTokenPriceChangeStyle({
+    priceChange,
+  });
+  const fiatValue = new BigNumber(tokenFiat?.fiatValue ?? 0);
+
+  const handlePress = useCallback(() => {
+    navigation.pop();
+    setTimeout(async () => {
+      if (
+        !activeAccount ||
+        !activeAccount.account ||
+        !activeAccount.network ||
+        !activeAccount.wallet ||
+        !activeAccount.deriveInfo ||
+        !activeAccount.deriveType ||
+        !activeAccount.indexedAccount
+      )
+        return;
+
+      navigation.pushModal(EModalRoutes.MainModal, {
+        screen: EModalAssetDetailRoutes.TokenDetails,
+        params: {
+          accountId: token.accountId ?? activeAccount.account?.id ?? '',
+          networkId: token.networkId ?? activeAccount.network?.id,
+          walletId: activeAccount.wallet?.id,
+          deriveInfo: activeAccount.deriveInfo,
+          deriveType: activeAccount.deriveType,
+          tokenInfo: token,
+          isAllNetworks: activeAccount.network?.isAllNetworks,
+          indexedAccountId: activeAccount.indexedAccount?.id ?? '',
+        },
+      });
+    }, 80);
+  }, [activeAccount, navigation, token]);
+
+  return (
+    <ListItem
+      key={token?.$key || token?.name}
+      userSelect="none"
+      onPress={handlePress}
+    >
+      <Token
+        size="lg"
+        tokenImageUri={token?.logoURI}
+        networkId={token?.networkId}
+        showNetworkIcon
+      />
+      <Stack flexGrow={1} flexBasis={0} minWidth={96} flexDirection="column">
+        <TokenName
+          name={token?.name}
+          networkId={token?.networkId}
+          isNative={token?.isNative}
+          isAllNetworks={networkUtils.isAllNetwork({
+            networkId: activeAccount?.network?.id,
+          })}
+          withNetwork={networkUtils.isAllNetwork({
+            networkId: activeAccount?.network?.id,
+          })}
+          textProps={{
+            size: '$bodyLgMedium',
+            flexShrink: 0,
+          }}
+        />
+        <NumberSizeableTextWrapper
+          formatter="balance"
+          formatterOptions={{ tokenSymbol: token?.symbol }}
+          size="$bodyMd"
+          color="$textSubdued"
+        >
+          {tokenFiat?.balanceParsed ?? '0'}
+        </NumberSizeableTextWrapper>
+      </Stack>
+      <Stack flexDirection="column" alignItems="flex-end" flexShrink={1}>
+        <NumberSizeableTextWrapper
+          formatter="value"
+          formatterOptions={{ currency: settings.currencyInfo.symbol }}
+          size="$bodyLgMedium"
+        >
+          {fiatValue.isNaN() ? 0 : fiatValue.toFixed()}
+        </NumberSizeableTextWrapper>
+        <NumberSizeableText
+          formatter="priceChange"
+          formatterOptions={{ showPlusMinusSigns }}
+          color={changeColor}
+          size="$bodyMd"
+        >
+          {priceChange}
+        </NumberSizeableText>
+      </Stack>
+    </ListItem>
+  );
+}
