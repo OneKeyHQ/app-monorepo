a2b873d030ac58a56042e86a8610fd9439960902
limichange <limichange@hotmail.com>
2025-05-27 08:34:12 +0800

    feat: Update switch animation to use spring easing for improved performance
    - Replaced the existing 'easeInOut' animation with a new 'switch' animation using spring dynamics in the Tamagui configuration.
    - Updated the Switch component to utilize the new animation for a more responsive user experience.

--- packages/components/src/forms/Switch/index.tsx
+++ packages/components/src/forms/Switch/index.tsx
@@ -78,10 +78,7 @@ export function Switch({
         h={size === 'small' ? '$5' : '$7'}
         borderRadius="$full"
         bg="$bg"
-        // Native platforms use quicker animations due to different user experience expectations.
-        // Please don't set the animation too fast.
-        // ref: https://github.com/tamagui/tamagui/commit/0586079faec69d044a5b1d45f84ae9f2e4e6e463
-        animation={platformEnv.isNative ? 'quick' : 'easeInOut'}
+        animation="switch"
         {...thumbProps}
       />
     </TMSwitch>
