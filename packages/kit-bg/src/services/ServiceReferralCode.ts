c17f5bd0a726361960643c043dbe073d0767cad4
huhuanming <huanming@onekey.so>
2025-05-29 16:15:45 +0800

    Update ServiceReferralCode.ts

--- packages/kit-bg/src/services/ServiceReferralCode.ts
+++ packages/kit-bg/src/services/ServiceReferralCode.ts
@@ -235,7 +235,7 @@ class ServiceReferralCode extends ServiceBase {
   async getPostConfig() {
     const postConfig =
       await this.backgroundApi.simpleDb.referralCode.getPostConfig();
-    if (postConfig) {
+    if (postConfig?.locales) {
       setTimeout(() => {
         void this.fetchPostConfig();
       });
