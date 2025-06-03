cb61a222eafe1ad566dee5f294450783f7e7da6e
huhuanming <huanming@onekey.so>
2025-06-03 22:49:57 +0800

    fiatValue

--- packages/kit/src/views/ReferFriends/pages/HardwareSalesReward/index.tsx
+++ packages/kit/src/views/ReferFriends/pages/HardwareSalesReward/index.tsx
@@ -100,8 +100,8 @@ export default function HardwareSalesReward() {
         if (summaryResult.status === 'fulfilled') {
           const data = summaryResult.value;
           setAmount({
-            available: data.HardwareSales.available?.[0]?.amount || '0',
-            pending: data.HardwareSales.pending?.[0]?.amount || '0',
+            available: data.HardwareSales.available?.[0]?.fiatValue || '0',
+            pending: data.HardwareSales.pending?.[0]?.fiatValue || '0',
           });
         }
         setIsLoading(false);
