9789608ca578f3a1c2800285c51985d436d375fd
huhuanming <huanming@onekey.so>
2025-06-06 14:31:17 +0800

    Update ServiceApp.ts

--- packages/kit-bg/src/services/ServiceApp.ts
+++ packages/kit-bg/src/services/ServiceApp.ts
@@ -108,72 +108,67 @@ class ServiceApp extends ServiceBase {
     defaultLogger.setting.page.clearDataStep('localDb-reset');
     await timerUtils.wait(100);
 
-    try {
-      const storageBuckets = (globalThis.navigator as INavigator)
-        .storageBuckets;
-      const names = await storageBuckets?.keys();
-      if (names) {
-        for (const name of names) {
-          try {
-            await storageBuckets?.delete(name);
-          } catch (error) {
-            console.error('storageBuckets.delete() error', error);
+    if (!platformEnv.isNative && platformEnv.isRuntimeBrowser) {
+      try {
+        const storageBuckets = (globalThis.navigator as INavigator)
+          .storageBuckets;
+        const names = await storageBuckets?.keys();
+        if (names) {
+          for (const name of names) {
+            try {
+              await storageBuckets?.delete(name);
+            } catch (error) {
+              console.error('storageBuckets.delete() error', error);
+            }
           }
         }
+      } catch {
+        console.error('storageBuckets.delete() error');
       }
-    } catch {
-      console.error('storageBuckets.delete() error');
     }
     defaultLogger.setting.page.clearDataStep('storageBuckets-delete');
     await timerUtils.wait(100);
 
-    const shouldDeleteAllOtherIndexedDBs = true;
+    if (!platformEnv.isNative && platformEnv.isRuntimeBrowser) {
+      const shouldDeleteAllOtherIndexedDBs = true;
+      try {
+        if (globalThis?.indexedDB && shouldDeleteAllOtherIndexedDBs) {
+          const indexedDB = globalThis?.indexedDB;
+          const deleteAllIndexedDBs = async () => {
+            const dbNames: IDBDatabaseInfo[] =
+              (await indexedDB?.databases?.()) || [];
+            for (const { name } of dbNames) {
+              if (name) {
+                try {
+                  await new Promise<void>((resolve, reject) => {
+                    const timer = setTimeout(() => {
+                      reject(new Error(`deleteIndexedDB timeout: ${name}`));
+                    }, 1000);
 
-    try {
-      if (globalThis?.indexedDB && shouldDeleteAllOtherIndexedDBs) {
-        const indexedDB = globalThis?.indexedDB;
-        const deleteAllIndexedDBs = async () => {
-          const dbNames: IDBDatabaseInfo[] =
-            (await indexedDB?.databases?.()) || [];
-          for (const { name } of dbNames) {
-            if (name) {
-              try {
-                await new Promise<void>((resolve, reject) => {
-                  const timer = setTimeout(() => {
-                    reject(new Error(`deleteIndexedDB timeout: ${name}`));
-                  }, 1000);
-
-                  const deleteRequest = indexedDB?.deleteDatabase(name);
-                  deleteRequest.onsuccess = () => {
-                    clearTimeout(timer);
-                    resolve();
-                  };
-                  deleteRequest.onerror = () => {
-                    clearTimeout(timer);
-                    reject(new Error(`deleteIndexedDB error: ${name}`));
-                  };
-                });
-              } catch (error) {
-                console.error('deleteIndexedDB error', error);
+                    const deleteRequest = indexedDB?.deleteDatabase(name);
+                    deleteRequest.onsuccess = () => {
+                      clearTimeout(timer);
+                      resolve();
+                    };
+                    deleteRequest.onerror = () => {
+                      clearTimeout(timer);
+                      reject(new Error(`deleteIndexedDB error: ${name}`));
+                    };
+                  });
+                } catch (error) {
+                  console.error('deleteIndexedDB error', error);
+                }
               }
             }
-          }
-        };
-
-        await deleteAllIndexedDBs();
+          };
+          await deleteAllIndexedDBs();
+        }
+      } catch (error) {
+        console.error('deleteAllIndexedDBs error', error);
       }
-    } catch (error) {
-      console.error('deleteAllIndexedDBs error', error);
     }
-
     defaultLogger.setting.page.clearDataStep('shouldDeleteAllOtherIndexedDBs');
     await timerUtils.wait(100);
-
-    // await this.backgroundApi.serviceV4Migration.saveAppStorageV4migrationAutoStartDisabled(
-    //   {
-    //     v4migrationAutoStartDisabled,
-    //   },
-    // );
 
     try {
       const isV4DbExist: boolean =
