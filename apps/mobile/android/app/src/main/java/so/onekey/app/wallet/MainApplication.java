7201f5126f9b2ff19bb6421343b961362e09ffbf
huhuanming <huanming@onekey.so>
2025-06-08 12:23:53 +0800

    remove GeckoView

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainApplication.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainApplication.java
@@ -39,7 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
         List<ReactPackage> packages = new PackageList(this).getPackages();
         packages.add(new AutoUpdateModulePackage(mReactNativeHost));
         packages.add(new RootViewBackgroundPackage());
-        packages.add(new GeckoViewPackage());
+        // packages.add(new GeckoViewPackage());
         packages.add(new ExitPackage());
         return packages;
       }
