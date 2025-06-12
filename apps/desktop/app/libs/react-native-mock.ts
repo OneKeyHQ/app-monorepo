17fae1efde98793bef2813502b0ed6ff43d0ac1e
huhuanming <huanming@onekey.so>
2025-06-12 15:58:57 +0800

    Update react-native-mock.ts

--- apps/desktop/app/libs/react-native-mock.ts
+++ apps/desktop/app/libs/react-native-mock.ts
@@ -10,17 +10,45 @@ export const InteractionManager = {};
 
 export const NativeEventEmitter = {};
 
-if (
-  typeof globalThis !== 'undefined' &&
-  typeof (
-    globalThis as unknown as {
-      __DEV__: boolean;
+const _globalThis = globalThis as unknown as {
+  __DEV__: boolean;
+  desktopApi: {
+    systemVersion: string;
+    platform: string;
+    arch: string;
+    channel?: string;
+    isMas: boolean;
+    isDev: boolean;
+  };
+};
+
+const getChannel = () => {
+  let channel;
+  try {
+    if (process.platform === 'linux' && process.env.APPIMAGE) {
+      channel = 'appImage';
+    } else if (process.platform === 'linux' && process.env.SNAP) {
+      channel = 'snap';
     }
-  ).__DEV__ === 'undefined'
-) {
-  (
-    globalThis as unknown as {
-      __DEV__: boolean;
-    }
-  ).__DEV__ = isDev;
+  } catch (e) {
+    // ignore
+  }
+  return channel;
+};
+
+if (typeof globalThis !== 'undefined') {
+  if (typeof _globalThis.__DEV__ === 'undefined') {
+    _globalThis.__DEV__ = isDev;
+  }
+
+  if (typeof _globalThis.desktopApi === 'undefined') {
+    _globalThis.desktopApi = {
+      platform: process.platform,
+      systemVersion: process.getSystemVersion(),
+      arch: process.arch,
+      channel: getChannel(),
+      isMas: process.mas,
+      isDev,
+    };
+  }
 }
