987e8de6b33bcbf5fd87d69f74a6fc02e3f55c72
huhuanming <huanming@onekey.so>
2025-06-16 04:33:24 +0800

    Update SubSettings.tsx

--- packages/kit/src/views/Setting/pages/Tab/SubSettings.tsx
+++ packages/kit/src/views/Setting/pages/Tab/SubSettings.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { useIntl } from 'react-intl';
 import { StyleSheet } from 'react-native';
@@ -6,13 +6,41 @@ import { StyleSheet } from 'react-native';
 import type { IKeyOfIcons } from '@onekeyhq/components';
 import { Divider, Page, YStack } from '@onekeyhq/components';
 import { TabSubStackNavigator } from '@onekeyhq/components/src/layouts/Navigation/Navigator';
+import { ListItem } from '@onekeyhq/kit/src/components/ListItem';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import type { ETranslations } from '@onekeyhq/shared/src/locale';
-
-import { ListItem } from '../../../../components/ListItem';
 
 import { SettingsConfig } from './config';
 
 type ISettingName = (typeof SettingsConfig)[number]['translationId'];
+
+function Grid({
+  item,
+}: {
+  item: (typeof SettingsConfig)[number]['configs'][number][number];
+}) {
+  const intl = useIntl();
+  const appNavigation = useAppNavigation();
+  const onPress = useCallback(() => {
+    if (item && 'navigateTo' in item && item.navigateTo) {
+      appNavigation.push(item.navigateTo);
+    }
+  }, [item, appNavigation]);
+  return (
+    <ListItem
+      py="$3"
+      mx={0}
+      borderRadius={0}
+      onPress={onPress}
+      key={item?.icon ?? item?.translationId}
+      icon={item?.icon as IKeyOfIcons}
+      title={intl.formatMessage({
+        id: (item?.translationId as ETranslations) ?? '',
+      })}
+      drillIn
+    />
+  );
+}
 
 export function SubSettingsPage({ name }: { name: ISettingName }) {
   const intl = useIntl();
@@ -36,6 +64,7 @@ export function SubSettingsPage({ name }: { name: ISettingName }) {
               <YStack
                 key={index}
                 bg="$bgSubdued"
+                overflow="hidden"
                 borderRadius="$2.5"
                 borderWidth={StyleSheet.hairlineWidth}
                 borderColor="$borderSubdued"
@@ -43,15 +72,7 @@ export function SubSettingsPage({ name }: { name: ISettingName }) {
                 {list.map((i, idx) => {
                   return i ? (
                     <>
-                      <ListItem
-                        py="$3"
-                        key={i?.icon ?? idx}
-                        icon={i?.icon as IKeyOfIcons}
-                        title={intl.formatMessage({
-                          id: (i?.translationId as ETranslations) ?? '',
-                        })}
-                        drillIn
-                      />
+                      <Grid item={i} />
                       {idx !== list.length - 1 ? <Divider mx="$5" /> : null}
                     </>
                   ) : null;
