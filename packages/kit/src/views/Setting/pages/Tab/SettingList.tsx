dd54162bdc0903979c8c16aba040c1d0710c96c8
huhuanming <huanming@onekey.so>
2025-06-16 06:21:26 +0800

    Create SettingList.tsx

---
+++ packages/kit/src/views/Setting/pages/Tab/SettingList.tsx
@@ -0,0 +1,59 @@
+import { useCallback } from 'react';
+
+import { useIntl } from 'react-intl';
+
+import type { IKeyOfIcons } from '@onekeyhq/components';
+import { Divider, Page, ScrollView, SearchBar } from '@onekeyhq/components';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
+
+import { useOnLock } from '../List/DefaultSection';
+
+import { useSettingsConfig } from './config';
+import { TabSettingsListItem } from './ListItem';
+
+export function SettingList() {
+  const intl = useIntl();
+  const onLock = useOnLock();
+  const handleLock = useCallback(async () => {
+    await onLock();
+  }, [onLock]);
+  const navigation = useAppNavigation();
+  const settingsConfig = useSettingsConfig();
+  return (
+    <Page>
+      <Page.Header
+        title={intl.formatMessage({ id: ETranslations.global_settings })}
+        headerShown={true}
+      />
+      <Page.Body>
+        <SearchBar />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          contentContainerStyle={{ pb: '$5' }}
+        >
+          <TabSettingsListItem
+            drillIn
+            title={intl.formatMessage({ id: ETranslations.settings_lock_now })}
+            icon="LockOutline"
+            onPress={async () => {
+              await handleLock();
+            }}
+          />
+          <Divider />
+          {settingsConfig.map(({ icon, translationId, ...options }) => (
+            <TabSettingsListItem
+              drillIn
+              key={translationId}
+              icon={icon as IKeyOfIcons}
+              title={intl.formatMessage({ id: translationId })}
+              onPress={() => {
+                navigation.push(translationId);
+              }}
+            />
+          ))}
+        </ScrollView>
+      </Page.Body>
+    </Page>
+  );
+}
