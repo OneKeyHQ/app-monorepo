c0231ef15789ffea3412e209e522ba629f01aec4
Franco <franco@onekey.so>
2025-06-17 15:47:04 +0800

    ui

--- packages/kit/src/views/AccountManagerStacks/components/WalletEdit/WalletEditButton.tsx
+++ packages/kit/src/views/AccountManagerStacks/components/WalletEdit/WalletEditButton.tsx
@@ -177,16 +177,16 @@ function WalletEditButtonView({
             </>
           ) : null}
 
-          {showRemoveDeviceButton ? (
+          {showRemoveWalletButton ? (
             <WalletRemoveButton
+              isRemoveToMocked
               wallet={wallet}
               onClose={handleActionListClose}
             />
           ) : null}
 
-          {showRemoveWalletButton ? (
+          {showRemoveDeviceButton ? (
             <WalletRemoveButton
-              isRemoveToMocked
               wallet={wallet}
               onClose={handleActionListClose}
             />
