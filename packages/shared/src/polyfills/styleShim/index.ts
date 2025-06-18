6c0698eddb20bd52edee1289de8b31279b4cf1ef
huhuanming <huanming@onekey.so>
2025-06-18 16:11:03 +0800

    Update index.ts

--- packages/shared/src/polyfills/styleShim/index.ts
+++ packages/shared/src/polyfills/styleShim/index.ts
@@ -1,15 +1,20 @@
 import { StyleSheet } from 'react-native';
 
-// Create a custom hairlineWidth since StyleSheet.hairlineWidth is read-only
-// Browser minimum support is 0.5px for hairline width
-const hairlineWidth = 0.5;
-
-// Override the StyleSheet object with our custom hairlineWidth
-if (StyleSheet && StyleSheet.hairlineWidth) {
-  Object.defineProperty(StyleSheet, 'hairlineWidth', {
-    value: hairlineWidth,
-    writable: false,
-    enumerable: true,
-    configurable: true,
-  });
+if (
+  globalThis &&
+  'devicePixelRatio' in globalThis &&
+  globalThis.devicePixelRatio > 1
+) {
+  // Create a custom hairlineWidth since StyleSheet.hairlineWidth is read-only
+  // Browser minimum support is 0.5px for hairline width
+  const hairlineWidth = 0.5;
+  // Override the StyleSheet object with our custom hairlineWidth
+  if (StyleSheet && StyleSheet.hairlineWidth) {
+    Object.defineProperty(StyleSheet, 'hairlineWidth', {
+      value: hairlineWidth,
+      writable: false,
+      enumerable: true,
+      configurable: true,
+    });
+  }
 }
