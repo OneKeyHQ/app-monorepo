0960903bd2cd5bac977f9c2eafd2df5c1be8e5e4
huhuanming <huanming@onekey.so>
2025-06-18 15:53:32 +0800

    Update index.ts

--- packages/shared/src/polyfills/styleShim/index.ts
+++ packages/shared/src/polyfills/styleShim/index.ts
@@ -1,13 +1,8 @@
 import { StyleSheet } from 'react-native';
 
 // Create a custom hairlineWidth since StyleSheet.hairlineWidth is read-only
-const MIN_WIDTH = 0.333;
-const hairlineWidth =
-  globalThis &&
-  'devicePixelRatio' in globalThis &&
-  typeof globalThis.devicePixelRatio === 'number'
-    ? Math.min(MIN_WIDTH, 1 / globalThis.devicePixelRatio)
-    : MIN_WIDTH;
+// Browser minimum support is 0.5px for hairline width
+const hairlineWidth = 0.5;
 
 // Override the StyleSheet object with our custom hairlineWidth
 if (StyleSheet && StyleSheet.hairlineWidth) {
