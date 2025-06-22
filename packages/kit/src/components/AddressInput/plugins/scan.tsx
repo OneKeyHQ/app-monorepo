56281a158f1214455523b187827cac97eaaaac44
huhuanming <huanming@onekey.so>
2025-06-23 00:48:17 +0800

    Enhance ScanPluginContent with token fetching logic
    Updated the ScanPluginContent to fetch token information after scanning a QR code. The component now retrieves either a specific token or the native token based on the scan result, using the active account and network context.

--- packages/kit/src/components/AddressInput/plugins/scan.tsx
+++ packages/kit/src/components/AddressInput/plugins/scan.tsx
@@ -12,7 +12,13 @@ import type {
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 import type { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
 import { EInputAddressChangeType } from '@onekeyhq/shared/types/address';
-import { EQRCodeHandlerNames } from '@onekeyhq/shared/types/qrCode';
+import {
+  EQRCodeHandlerNames,
+  EQRCodeHandlerType,
+} from '@onekeyhq/shared/types/qrCode';
+
+import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
+import { useActiveAccount } from '../../../states/jotai/contexts/accountSelector';
 
 import type { IAddressPluginProps } from '../types';
 
@@ -24,6 +30,9 @@ const ScanPluginContent: FC<IAddressPluginProps> = ({
 }) => {
   const { start } = useScanQrCode();
   const intl = useIntl();
+  const {
+    activeAccount: { account, network },
+  } = useActiveAccount({ num: 0 });
   const onPress = useCallback(async () => {
     const result = (await start({
       handlers: [
@@ -36,9 +45,29 @@ const ScanPluginContent: FC<IAddressPluginProps> = ({
       autoHandleResult: false,
     })) as IQRCodeHandlerParseResult<IChainValue>;
     console.log('scaned result', result);
-    onChange?.(result?.data?.address);
+    onChange?.(
+      result.type === EQRCodeHandlerType.UNKNOWN
+        ? result.raw
+        : result?.data?.address,
+    );
     onInputTypeChange?.(EInputAddressChangeType.Scan);
-  }, [onChange, onInputTypeChange, start]);
+    const tokenAddress = result?.data?.tokenAddress;
+    const accountId = account?.id;
+    const networkId = result?.data?.network?.id || network?.id || '';
+    if (accountId) {
+      const token = tokenAddress
+        ? await backgroundApiProxy.serviceToken.getToken({
+            networkId,
+            accountId,
+            tokenIdOnNetwork: tokenAddress,
+          })
+        : await backgroundApiProxy.serviceToken.getNativeToken({
+            networkId,
+            accountId: account.id,
+          });
+      console.log('token result', accountId, networkId, token);
+    }
+  }, [account?.id, network?.id, onChange, onInputTypeChange, start]);
   return (
     <IconButton
       title={intl.formatMessage({ id: ETranslations.send_to_scan_tooltip })}
