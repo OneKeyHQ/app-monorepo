adbe62dde1948cdd3a4ff13c0251508b4a292c3d
huhuanming <huanming@onekey.so>
2025-06-24 13:03:30 +0800

    Update appEventBusNames.ts

--- packages/shared/src/eventBus/appEventBusNames.ts
+++ packages/shared/src/eventBus/appEventBusNames.ts
@@ -99,5 +99,4 @@ export enum EAppEventBusNames {
   ClearStorageOnExtension = 'ClearStorageOnExtension',
   SettingsSearchResult = 'SettingsSearchResult',
   CheckEndpointPrefix = 'CheckEndpointPrefix',
-  UpdateSendAmountInputValues = 'UpdateSendAmountInputValues',
 }
