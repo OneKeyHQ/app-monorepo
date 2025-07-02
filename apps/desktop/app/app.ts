a8e42745cd30023d15fb4b6039e9becac8bb9584
Leon <lixiao.dev@gmail.com>
2025-07-02 10:33:56 +0800

    chore: update hd-sdk packages to version 1.0.38-alpha.0 and refactor BLE initialization in desktop app

--- apps/desktop/app/app.ts
+++ apps/desktop/app/app.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from 'events';
 import * as path from 'path';
 import { format as formatUrl } from 'url';
 
-import { initElectronBleBridge } from '@onekeyfe/hd-transport-electron';
+import { initNobleBleSupport } from '@onekeyfe/hd-transport-electron';
 import {
   attachTitlebarToWindow,
   setupTitlebar,
@@ -778,7 +778,7 @@ function createMainWindow() {
     }
   });
 
-  initElectronBleBridge(browserWindow.webContents);
+  void initNobleBleSupport(browserWindow.webContents);
 
   return browserWindow;
 }
