1557e87d168793ab7c783846151abef194c36546
huhuanming <huanming@onekey.so>
2025-07-02 15:14:51 +0800

    Fix parseOnChainAmount to handle undefined amounts
    Updated parseOnChainAmount to return undefined if data.amount is not present, preventing unintended string conversion of undefined values.

--- packages/kit/src/views/ScanQrCode/hooks/useParseQRCode.tsx
+++ packages/kit/src/views/ScanQrCode/hooks/useParseQRCode.tsx
@@ -73,7 +73,7 @@ export const parseOnChainAmount = async (
       });
     }
   }
-  return String(data.amount);
+  return data.amount ? String(data.amount) : undefined;
 };
 
 export const getAccountIdOnNetwork = async ({
