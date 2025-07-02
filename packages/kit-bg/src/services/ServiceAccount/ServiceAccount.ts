395255c0f18fb6c7b7bb554c49d009ab8f56350e
limichange <limichange@hotmail.com>
2025-07-02 12:02:36 +0800

    refactor: Remove unused variables and streamline parameter destructuring across multiple services

--- packages/kit-bg/src/services/ServiceAccount/ServiceAccount.ts
+++ packages/kit-bg/src/services/ServiceAccount/ServiceAccount.ts
@@ -740,7 +740,7 @@ class ServiceAccount extends ServiceBase {
     networkId: string;
     account: IBatchCreateAccount;
   }) {
-    const { addressDetail, existsInDb, displayAddress, ...dbAccount } = account;
+    const { ...dbAccount } = account;
     if (isNil(dbAccount.pathIndex)) {
       throw new OneKeyLocalError(
         'addBatchCreatedHdOrHwAccount ERROR: pathIndex is required',
@@ -1125,7 +1125,7 @@ class ServiceAccount extends ServiceBase {
           networkId,
           account,
         });
-      const { prepareParams, deviceParams } =
+      const { prepareParams, deviceParams: _deviceParams } =
         await this.getPrepareHDOrHWAccountsParams({
           walletId,
           networkId,
@@ -1133,10 +1133,6 @@ class ServiceAccount extends ServiceBase {
           deriveType,
           confirmOnDevice: EConfirmOnDeviceType.EveryItem,
         });
-      const vault = await vaultFactory.getWalletOnlyVault({
-        networkId,
-        walletId,
-      });
 
       // const accounts = await vault.keyring.prepareAccounts(prepareParams);
       const { accountsForCreate } =
