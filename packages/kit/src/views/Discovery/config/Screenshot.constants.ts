9c719e06f8f1320fa366f378545a139b3e22b00f
huhuanming <huanming@onekey.so>
2025-07-04 13:32:00 +0800

    Update Screenshot.constants.ts

--- packages/kit/src/views/Discovery/config/Screenshot.constants.ts
+++ packages/kit/src/views/Discovery/config/Screenshot.constants.ts
@@ -1,4 +1,3 @@
 import RNFS from '@onekeyhq/shared/src/modules3rdParty/react-native-fs/index.native';
 
-console.log('RNFS', RNFS, RNFS.DocumentDirectoryPath);
 export const SCREENSHOT_FOLDER = `${RNFS.DocumentDirectoryPath}/discovery/screenshot`;
