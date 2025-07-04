bd283039eb91f03147b10a448bbb6c267c077450
limichange <limichange@hotmail.com>
2025-07-04 18:39:46 +0800

    refactor: comment out unused imports in ApproveBaseStake component
    - Commented out unused imports in the ApproveBaseStake component to improve code clarity and maintainability.
    - This change helps streamline the codebase by removing unnecessary dependencies while keeping the option to reintegrate them if needed.

--- packages/kit/src/views/Staking/pages/ApproveBaseStake/index.tsx
+++ packages/kit/src/views/Staking/pages/ApproveBaseStake/index.tsx
@@ -1,31 +1,31 @@
-import { useCallback, useMemo } from 'react';
+// import { useCallback, useMemo } from 'react';
 
-import { useIntl } from 'react-intl';
+// import { useIntl } from 'react-intl';
 
-import { Page } from '@onekeyhq/components';
-import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
-import { AccountSelectorProviderMirror } from '@onekeyhq/kit/src/components/AccountSelector';
-import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
-import { useAppRoute } from '@onekeyhq/kit/src/hooks/useAppRoute';
-import { useEarnActions } from '@onekeyhq/kit/src/states/jotai/contexts/earn/actions';
-import { EarnProviderMirror } from '@onekeyhq/kit/src/views/Earn/EarnProviderMirror';
-import { EJotaiContextStoreNames } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
-import { ETranslations } from '@onekeyhq/shared/src/locale';
-import { defaultLogger } from '@onekeyhq/shared/src/logger/logger';
-import type {
-  EModalStakingRoutes,
-  IModalStakingParamList,
-} from '@onekeyhq/shared/src/routes';
-import earnUtils from '@onekeyhq/shared/src/utils/earnUtils';
-import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
-import type { IApproveConfirmFnParams } from '@onekeyhq/shared/types/staking';
-import { EApproveType, EEarnLabels } from '@onekeyhq/shared/types/staking';
-import type { IToken } from '@onekeyhq/shared/types/token';
+// import { Page } from '@onekeyhq/components';
+// import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+// import { AccountSelectorProviderMirror } from '@onekeyhq/kit/src/components/AccountSelector';
+// import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
+// import { useAppRoute } from '@onekeyhq/kit/src/hooks/useAppRoute';
+// import { useEarnActions } from '@onekeyhq/kit/src/states/jotai/contexts/earn/actions';
+// import { EarnProviderMirror } from '@onekeyhq/kit/src/views/Earn/EarnProviderMirror';
+// import { EJotaiContextStoreNames } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
+// import { ETranslations } from '@onekeyhq/shared/src/locale';
+// import { defaultLogger } from '@onekeyhq/shared/src/logger/logger';
+// import type {
+//   EModalStakingRoutes,
+//   IModalStakingParamList,
+// } from '@onekeyhq/shared/src/routes';
+// import earnUtils from '@onekeyhq/shared/src/utils/earnUtils';
+// import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
+// import type { IApproveConfirmFnParams } from '@onekeyhq/shared/types/staking';
+// import { EApproveType, EEarnLabels } from '@onekeyhq/shared/types/staking';
+// import type { IToken } from '@onekeyhq/shared/types/token';
 
-import { ApproveBaseStake } from '../../components/ApproveBaseStake';
-import { useProviderLabel } from '../../hooks/useProviderLabel';
-import { useUniversalStake } from '../../hooks/useUniversalHooks';
-import { buildLocalTxStatusSyncId } from '../../utils/utils';
+// import { ApproveBaseStake } from '../../components/ApproveBaseStake';
+// import { useProviderLabel } from '../../hooks/useProviderLabel';
+// import { useUniversalStake } from '../../hooks/useUniversalHooks';
+// import { buildLocalTxStatusSyncId } from '../../utils/utils';
 
 // const BasicApproveBaseStakePage = () => {
 //   const route = useAppRoute<
