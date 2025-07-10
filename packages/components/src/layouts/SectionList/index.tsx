ccf06e982abc01f663233652a57e9aefda74028d
huhuanming <huanming@onekey.so>
2025-07-10 12:57:42 +0800

    fix: fix types

--- packages/components/src/layouts/SectionList/index.tsx
+++ packages/components/src/layouts/SectionList/index.tsx
@@ -14,7 +14,11 @@ import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { SizableText, Stack, XStack } from '../../primitives';
 import { ListView } from '../ListView/list';
 
-import type { ISizableTextProps, IStackProps } from '../../primitives';
+import type {
+  ISizableTextProps,
+  IStackProps,
+  IXStackProps,
+} from '../../primitives';
 import type { IListViewProps, IListViewRef } from '../ListView/list';
 import type { Tokens } from '@tamagui/web';
 import type { ListRenderItem } from 'react-native';
@@ -303,7 +307,13 @@ function SectionHeader({
   titleProps?: ISizableTextProps;
 }) {
   return (
-    <XStack h="$9" px="$5" alignItems="center" bg="$bgApp" {...restProps}>
+    <XStack
+      h="$9"
+      px="$5"
+      alignItems="center"
+      bg="$bgApp"
+      {...(restProps as IXStackProps)}
+    >
       <SizableText
         numberOfLines={1}
         size="$headingSm"
