5c8ec5be43b48ba6e4e6e26e1d536e2c99273e10
huhuanming <huanming@onekey.so>
2025-07-15 10:02:33 +0800

    Update Input.android.tsx

--- packages/components/src/forms/Input/Input.android.tsx
+++ packages/components/src/forms/Input/Input.android.tsx
@@ -9,10 +9,9 @@ import {
 import { getButtonSized } from '@tamagui/get-button-sized';
 import { getFontSized } from '@tamagui/get-font-sized';
 import { getSpace } from '@tamagui/get-token';
-import { TextInput } from 'react-native';
 import { defaultStyles, useInputProps } from 'tamagui';
 
-// import TextInput from './TextInput';
+import TextInput from './TextInput';
 
 import type { SizeVariantSpreadFunction } from '@tamagui/core';
 import type { InputExtraProps } from 'tamagui';
@@ -84,11 +83,6 @@ export const InputFrame = styled(
 
       size: {
         '...size': inputSizeVariant,
-      },
-      textAlignVertical: {
-        center: {
-          textAlignVertical: 'center',
-        },
       },
 
       disabled: {
