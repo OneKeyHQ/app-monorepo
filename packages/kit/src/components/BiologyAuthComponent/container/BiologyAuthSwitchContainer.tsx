704153d25962613431c7332395474b6879fe55ef
huhuanming <huanming@onekey.so>
2025-07-19 00:47:10 +0800

    fix: fix code

--- packages/kit/src/components/BiologyAuthComponent/container/BiologyAuthSwitchContainer.tsx
+++ packages/kit/src/components/BiologyAuthComponent/container/BiologyAuthSwitchContainer.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 
+import { flushSync } from 'react-dom';
 import { useIntl } from 'react-intl';
 
 import { Toast } from '@onekeyhq/components';
@@ -27,10 +28,12 @@ const BiologyAuthSwitchContainer = ({
   const onChange = useCallback(
     async (checked: boolean) => {
       try {
-        await backgroundApiProxy.servicePassword.setBiologyAuthEnable(
-          checked,
-          skipAuth,
-        );
+        void flushSync(async () => {
+          await backgroundApiProxy.servicePassword.setBiologyAuthEnable(
+            checked,
+            skipAuth,
+          );
+        });
       } catch (e) {
         const error = e as { message?: string; name?: string };
         if (error?.name === BIOLOGY_AUTH_CANCEL_ERROR) {
--- packages/kit/src/provider/Container/NavigationContainer.tsx
+++ packages/kit/src/provider/Container/NavigationContainer.tsx
@@ -13,6 +13,7 @@ import { TabFreezeOnBlurContainer } from './TabFreezeOnBlurContainer';
 
 function BasicNavigation({ children }: PropsWithChildren) {
   const { containerProps, routerConfig } = useRouterConfig();
+  console.log('BasicNavigation----', containerProps, routerConfig);
   return (
     <NavigationContainerComponent {...containerProps}>
       <TabFreezeOnBlurContainer>
