13c03da60fe4189d9071b30f93d891f8233f1fcb
weatherstar <weather@onekey.so>
2025-07-21 16:30:18 +0800

    feat: fetch wallet banner request

--- packages/kit-bg/src/services/ServiceWalletBanner.ts
+++ packages/kit-bg/src/services/ServiceWalletBanner.ts
@@ -2,6 +2,9 @@ import {
   backgroundClass,
   backgroundMethod,
 } from '@onekeyhq/shared/src/background/backgroundDecorators';
+import { EServiceEndpointEnum } from '@onekeyhq/shared/types/endpoint';
+import type { IFetchResponse } from '@onekeyhq/shared/types/swap/types';
+import type { IWalletBannerResponse } from '@onekeyhq/shared/types/walletBanner';
 
 import ServiceBase from './ServiceBase';
 
@@ -9,6 +12,32 @@ import ServiceBase from './ServiceBase';
 class ServiceWalletBanner extends ServiceBase {
   constructor({ backgroundApi }: { backgroundApi: any }) {
     super({ backgroundApi });
+  }
+
+  @backgroundMethod()
+  async fetchWalletBanner({ accountId }: { accountId?: string }) {
+    const client = await this.backgroundApi.serviceGas.getClient(
+      EServiceEndpointEnum.Wallet,
+    );
+    const resp = await client.get<IFetchResponse<IWalletBannerResponse>>(
+      '/utility/v1/wallet-banner/list',
+      {
+        params: {},
+        headers:
+          await this.backgroundApi.serviceAccountProfile._getWalletTypeHeader({
+            accountId,
+          }),
+      },
+    );
+
+    if (resp.data.code !== 0) {
+      return {
+        banners: [],
+        categories: [],
+      };
+    }
+
+    return resp.data.data;
   }
 }
 
