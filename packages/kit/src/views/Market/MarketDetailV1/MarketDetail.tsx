dfe5b6df6f16a93bd500912d82ad72ee66ea090d
limichange <limichange@hotmail.com>
2025-07-21 20:16:13 +0800

    refactor: clean up MarketDetail component by removing unused imports and components
    - Removed unused imports and components from MarketDetail to streamline the codebase.
    - Simplified the header rendering logic by eliminating unnecessary dependencies and props.
    - Enhanced overall readability and maintainability of the MarketDetail component.

--- packages/kit/src/views/Market/MarketDetailV1/MarketDetail.tsx
+++ packages/kit/src/views/Market/MarketDetailV1/MarketDetail.tsx
@@ -5,7 +5,6 @@ import { CommonActions, StackActions } from '@react-navigation/native';
 import type { IPageScreenProps } from '@onekeyhq/components';
 import {
   EPageType,
-  HeaderIconButton,
   NavBackButton,
   Page,
   ScrollView,
@@ -17,35 +16,27 @@ import {
   useDeferredPromise,
   useMedia,
   usePageType,
-  useShare,
 } from '@onekeyhq/components';
 import { EJotaiContextStoreNames } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
-import { EOneKeyDeepLinkPath } from '@onekeyhq/shared/src/consts/deeplinkConsts';
-import { EWatchlistFrom } from '@onekeyhq/shared/src/logger/scopes/market/scenes/token';
-import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { ETabMarketRoutes } from '@onekeyhq/shared/src/routes';
 import type { ITabMarketParamList } from '@onekeyhq/shared/src/routes';
 import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
-import uriUtils from '@onekeyhq/shared/src/utils/uriUtils';
 import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
 import type { IMarketTokenDetail } from '@onekeyhq/shared/types/market';
 
 import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
 import { AccountSelectorProviderMirror } from '../../../components/AccountSelector';
-import { OpenInAppButton } from '../../../components/OpenInAppButton';
 import useAppNavigation from '../../../hooks/useAppNavigation';
 import { usePromiseResult } from '../../../hooks/usePromiseResult';
 import { useActiveAccount } from '../../../states/jotai/contexts/accountSelector';
 import { MarketDetailOverview } from '../components/MarketDetailOverview';
 import { MarketHomeHeaderSearchBar } from '../components/MarketHomeHeaderSearchBar';
-import { MarketStar } from '../components/MarketStar';
 import { MarketTokenIcon } from '../components/MarketTokenIcon';
 import { MarketTokenPrice } from '../components/MarketTokenPrice';
 import { MarketTradeButton } from '../components/MarketTradeButton';
 import { PriceChangePercentage } from '../components/PriceChangePercentage';
 import { TokenDetailTabs } from '../components/TokenDetailTabs';
 import { TokenPriceChart } from '../components/TokenPriceChart';
-import { buildMarketFullUrl } from '../marketUtils';
 import { MarketWatchListProviderMirror } from '../MarketWatchListProviderMirror';
 
 function TokenDetailHeader({
@@ -97,12 +88,6 @@ function TokenDetailHeader({
             tokenName={name}
             tokenSymbol={symbol}
             lastUpdated={lastUpdated}
-          />
-          <MarketStar
-            coingeckoId={coinGeckoId}
-            mr="$-2"
-            size="medium"
-            from={EWatchlistFrom.details}
           />
         </XStack>
         <PriceChangePercentage pt="$0.5" width="100%">
@@ -183,44 +168,14 @@ function MarketDetail({
     ),
     [tokenDetail?.image, tokenDetail?.symbol],
   );
-  const { shareText } = useShare();
-
-  const buildDeepLinkUrl = useCallback(
-    () =>
-      uriUtils.buildDeepLinkUrl({
-        path: EOneKeyDeepLinkPath.market_detail,
-        query: {
-          coinGeckoId,
-        },
-      }),
-    [coinGeckoId],
-  );
-
-  const buildFullUrl = useCallback(
-    async () => buildMarketFullUrl({ coinGeckoId }),
-    [coinGeckoId],
-  );
 
   const renderHeaderRight = useCallback(
     () => (
       <XStack gap="$6" ai="center">
-        {!platformEnv.isExtensionUiPopup && !platformEnv.isNative ? (
-          <OpenInAppButton
-            buildDeepLinkUrl={buildDeepLinkUrl}
-            buildFullUrl={buildFullUrl}
-          />
-        ) : null}
-        <HeaderIconButton
-          icon="ShareOutline"
-          onPress={async () => {
-            const url = buildMarketFullUrl({ coinGeckoId });
-            await shareText(url);
-          }}
-        />
         {gtMd ? <MarketHomeHeaderSearchBar /> : null}
       </XStack>
     ),
-    [buildDeepLinkUrl, buildFullUrl, coinGeckoId, gtMd, shareText],
+    [gtMd],
   );
 
   const navigation = useAppNavigation();
