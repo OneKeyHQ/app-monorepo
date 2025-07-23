57a82d1eba2b1f1c6858dd509a558b41df021b2f
limichange <limichange@hotmail.com>
2025-07-23 13:36:01 +0800

    feat: enhance web tab navigation by adding new parameters for window behavior and improving URL validation logic

--- packages/kit/src/states/jotai/contexts/discovery/actions.ts
+++ packages/kit/src/states/jotai/contexts/discovery/actions.ts
@@ -848,6 +848,8 @@ class ContextJotaiActionsDiscovery extends ContextJotaiActionsBase {
       set,
       {
         url,
+        isNewWindow,
+        isInPlace,
         title,
         favicon,
         canGoBack,
@@ -857,10 +859,12 @@ class ContextJotaiActionsDiscovery extends ContextJotaiActionsBase {
         handlePhishingUrl,
       }: IOnWebviewNavigationFnParams,
     ) => {
+      const now = Date.now();
       const tab = this.getWebTabById.call(set, id ?? '');
       if (!tab) {
         return;
       }
+      const isValidNewUrl = typeof url === 'string' && url !== tab.url;
 
       if (url) {
         const cache = get(phishingLruCacheAtom());
@@ -877,6 +881,29 @@ class ContextJotaiActionsDiscovery extends ContextJotaiActionsBase {
           handleDeepLinkUrl({ url });
           return;
         }
+      }
+
+      if (isValidNewUrl) {
+        if (tab.timestamp && now - tab.timestamp < 500) {
+          // ignore url change if it's too fast to avoid back & forth loop
+          return;
+        }
+        if (
+          homeResettingFlags[tab.id] &&
+          url !== homeTab.url &&
+          now - homeResettingFlags[tab.id] < 1000
+        ) {
+          return;
+        }
+
+        void this.gotoSite.call(set, {
+          url,
+          title,
+          favicon,
+          isNewWindow,
+          isInPlace,
+          id: tab.id,
+        });
       }
 
       this.setWebTabData.call(set, {
