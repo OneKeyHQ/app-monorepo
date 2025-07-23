cf4ce8e234ac98d233aaece10f08c7c43e3ede99
huhuanming <huanming@onekey.so>
2025-07-23 16:37:25 +0800

    Update index.native.tsx

--- packages/components/src/content/Keyboard/index.native.tsx
+++ packages/components/src/content/Keyboard/index.native.tsx
@@ -6,10 +6,17 @@ import {
   KeyboardToolbar,
 } from 'react-native-keyboard-controller';
 
+import {
+  dismissKeyboard,
+  dismissKeyboardWithDelay,
+} from '@onekeyhq/shared/src/keyboard';
+
 export const Keyboard = {
   AvoidingView: KeyboardAvoidingView,
   AwareScrollView: KeyboardAwareScrollView,
   StickyView: KeyboardStickyView,
   Toolbar: KeyboardToolbar,
   ControllerView: KeyboardControllerView,
+  dismiss: dismissKeyboard,
+  dismissWithDelay: dismissKeyboardWithDelay,
 };
