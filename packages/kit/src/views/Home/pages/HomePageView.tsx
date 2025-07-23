f067b3eb7c071ecf861d022f637d526e1ee472ea
huhuanming <huanming@onekey.so>
2025-07-23 21:04:23 +0800

    Update HomePageView.tsx

--- packages/kit/src/views/Home/pages/HomePageView.tsx
+++ packages/kit/src/views/Home/pages/HomePageView.tsx
@@ -160,7 +160,7 @@ export function HomePageView({
         elevation: 0,
       },
       pagerProps: {
-        scrollEnabled: false,
+        scrollEnabled: platformEnv.isNativeAndroid,
       },
       renderHeader,
       renderTabBar: (props: any) => (
