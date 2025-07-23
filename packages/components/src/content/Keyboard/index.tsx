61cb815d3b83cfaa803c6bee89c535fbfbdf31d9
huhuanming <huanming@onekey.so>
2025-07-23 16:37:26 +0800

    Update index.tsx

--- packages/components/src/content/Keyboard/index.tsx
+++ packages/components/src/content/Keyboard/index.tsx
@@ -1,3 +1,7 @@
+import {
+  dismissKeyboard,
+  dismissKeyboardWithDelay,
+} from '@onekeyhq/shared/src/keyboard';
 
 export const Keyboard = {
   AvoidingView: (children: React.ReactNode) => children,
@@ -5,4 +9,6 @@ export const Keyboard = {
   StickyView: (children: React.ReactNode) => children,
   Toolbar: (children: React.ReactNode) => children,
   ControllerView: (children: React.ReactNode) => children,
+  dismiss: dismissKeyboard,
+  dismissWithDelay: dismissKeyboardWithDelay,
 };
