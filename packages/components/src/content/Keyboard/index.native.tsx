ab5a4dd20a5c0a2d87572a390ae851f8e2f2b7de
huhuanming <huanming@onekey.so>
2025-07-23 16:35:32 +0800

    add Keyboard Component

---
+++ packages/components/src/content/Keyboard/index.native.tsx
@@ -0,0 +1,15 @@
+import {
+  KeyboardAvoidingView,
+  KeyboardAwareScrollView,
+  KeyboardControllerView,
+  KeyboardStickyView,
+  KeyboardToolbar,
+} from 'react-native-keyboard-controller';
+
+export const Keyboard = {
+  AvoidingView: KeyboardAvoidingView,
+  AwareScrollView: KeyboardAwareScrollView,
+  StickyView: KeyboardStickyView,
+  Toolbar: KeyboardToolbar,
+  ControllerView: KeyboardControllerView,
+};
---
+++ packages/components/src/content/Keyboard/index.tsx
@@ -0,0 +1,8 @@
+
+export const Keyboard = {
+  AvoidingView: (children: React.ReactNode) => children,
+  AwareScrollView: (children: React.ReactNode) => children,
+  StickyView: (children: React.ReactNode) => children,
+  Toolbar: (children: React.ReactNode) => children,
+  ControllerView: (children: React.ReactNode) => children,
+};
