20cec75323ff1a73e63e6aa6fd6406385449e505
huhuanming <huanming@onekey.so>
2025-07-25 00:27:26 +0800

    Update ScrollView.tsx

--- packages/components/src/composite/Tabs/ScrollView.tsx
+++ packages/components/src/composite/Tabs/ScrollView.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useRef } from 'react';
+
 import { YStack } from '../../primitives';
 
-import { useTabsScrollContext } from './context';
+import { useTabsContext, useTabsScrollContext } from './context';
+import { useTabNameContext } from './TabNameContext';
+import { useConvertAnimatedToValue } from './useFocusedTab';
 
 export function ScrollView({ children }: { children: React.ReactNode }) {
-  const { width } = useTabsScrollContext();
+  const { width, registerChild } = useTabsScrollContext();
+  const ref = useRef<Element>(null);
+  const scrollTabElementsRef = useTabsContext().scrollTabElementsRef;
+  const currentTabName = useTabNameContext();
+  const { focusedTab } = useTabsContext();
+
+  const focusedTabValue = useConvertAnimatedToValue(focusedTab, '');
+
+  useEffect(() => {
+    if (focusedTabValue === currentTabName) {
+      if (
+        scrollTabElementsRef?.current &&
+        !scrollTabElementsRef?.current[currentTabName]
+      ) {
+        scrollTabElementsRef.current[currentTabName] = {} as any;
+      }
+      scrollTabElementsRef.current[currentTabName].element =
+        ref.current as HTMLElement;
+      registerChild(ref.current);
+    }
+  }, [focusedTabValue, currentTabName, registerChild, scrollTabElementsRef]);
+
   return (
-    <YStack flex={1} width={width}>
+    <YStack flex={1} ref={ref as any} width={width}>
       {children}
     </YStack>
   );
