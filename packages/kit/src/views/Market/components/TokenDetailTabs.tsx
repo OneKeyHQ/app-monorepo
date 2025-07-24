e650b3ab51f721d996759c974ee9ac0e0200120d
huhuanming <huanming@onekey.so>
2025-07-25 00:12:14 +0800

    Update TokenDetailTabs.tsx

--- packages/kit/src/views/Market/components/TokenDetailTabs.tsx
+++ packages/kit/src/views/Market/components/TokenDetailTabs.tsx
@@ -7,8 +7,10 @@ import {
   RefreshControl,
   Stack,
   Tab,
+  Tabs,
   useIsModalPage,
   useMedia,
+  YStack,
 } from '@onekeyhq/components';
 import type { IDeferredPromise, ITabPageProps } from '@onekeyhq/components';
 import type { ITabInstance } from '@onekeyhq/components/src/layouts/TabView/StickyTabComponent/types';
@@ -39,7 +41,7 @@ function BasicTokenDetailTabs({
 }) {
   const intl = useIntl();
   const isModalPage = useIsModalPage();
-  const { md: mdMedia } = useMedia();
+  const { md: mdMedia, gtMd: gtMdMedia } = useMedia();
   const md = isModalPage ? true : mdMedia;
 
   useEffect(() => {
@@ -48,7 +50,7 @@ function BasicTokenDetailTabs({
     }, 100);
   }, [defer]);
 
-  const tabConfig = useMemo(
+  const tabConfigs = useMemo(
     () =>
       [
         md && token
@@ -152,33 +154,36 @@ function BasicTokenDetailTabs({
   );
 
   return (
-    <Tab
-      ref={tabRef}
-      refreshControl={
-        <RefreshControl refreshing={!!isRefreshing} onRefresh={onRefresh} />
-      }
-      $gtMd={{ pr: isModalPage ? 0 : '$5' }}
-      $md={{ mt: '$5' }}
-      {...(isModalPage ? { mt: '$5' } : null)}
-      data={tabConfig}
-      disableRefresh
-      ListHeaderComponent={
-        <Stack
-          mb="$5"
+    <Tabs.Container
+      containerStyle={{
+        ...(gtMdMedia ? { paddingRight: isModalPage ? 0 : 20 } : undefined),
+        ...(md ? { marginTop: 20 } : undefined),
+        ...(isModalPage ? { marginTop: 20 } : undefined),
+      }}
+      onIndexChange={onSelectedPageIndex}
+      renderHeader={() => (
+        <YStack
+          bg="$bgApp"
+          pb="$5"
           onLayout={handleMount}
-          h={150}
+          h={170}
           $gtMd={{
             ...(isModalPage ? null : { h: 450 }),
           }}
         >
           {listHeaderComponent}
-          {/* {pools ? null : (
-            <YStack $gtMd={{ px: '$5' }}>{renderPoolSkeleton}</YStack>
-          )} */}
-        </Stack>
-      }
-      onSelectedPageIndex={onSelectedPageIndex}
-    />
+        </YStack>
+      )}
+      renderTabBar={(props) => <Tabs.TabBar {...props} />}
+    >
+      {tabConfigs.map((tab) => (
+        <Tabs.Tab key={tab.title} name={tab.title}>
+          <Tabs.ScrollView>
+            {tab.page({ showWalletActions: false })}
+          </Tabs.ScrollView>
+        </Tabs.Tab>
+      ))}
+    </Tabs.Container>
   );
 }
 
