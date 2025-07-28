fd4c8b67f295f36ff1d3035c561b6c8e402bb979
huhuanming <huanming@onekey.so>
2025-07-28 18:51:06 +0800

    Update ImageV2.native.tsx

--- packages/components/src/primitives/Image/ImageV2.native.tsx
+++ packages/components/src/primitives/Image/ImageV2.native.tsx
@@ -8,7 +8,7 @@ import { Skeleton } from '../Skeleton';
 
 import { AnimatedExpoImage } from './AnimatedImage';
 import { useImage } from './useImage';
-import { useResetError } from './utils';
+import { isEmptyResolvedSource, useResetError } from './utils';
 
 import type { IImageV2Props } from './type';
 import type { ImageErrorEventData, ImageSource, ImageStyle } from 'expo-image';
@@ -90,7 +90,7 @@ export function ImageV2({
   );
 
   if (!image) {
-    if (hasError) {
+    if (hasError || isEmptyResolvedSource(source as ImageSource | null)) {
       return fallback;
     }
     return skeleton || <Skeleton width={style.width} height={style.height} />;
