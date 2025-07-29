448a77a2368549b01d8590d774e4857cf3fff2f7
limichange <limichange@hotmail.com>
2025-07-29 15:29:49 +0800

    fix: enhance ImageV2 component error handling and layout OK-41068 (#7924)
    * fix: enhance ImageV2 component error handling and layout
    
    - Added Stack component to wrap fallback content for better layout management.
    - Improved error handling logic to conditionally render fallback or skeleton based on image loading state.
    - Adjusted styles for YStack to ensure proper alignment and overflow handling.
    
    * refactor: simplify fallback rendering in BasicDiscoveryIcon component
    
    - Removed Stack component from fallback rendering to streamline the layout.
    - Updated fallback to directly use Icon for improved clarity and efficiency.

--- packages/kit/src/views/Discovery/components/DiscoveryIcon.tsx
+++ packages/kit/src/views/Discovery/components/DiscoveryIcon.tsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 import { StyleSheet } from 'react-native';
 
 import type { IImageProps } from '@onekeyhq/components';
-import { Icon, Image, Skeleton, Stack } from '@onekeyhq/components';
+import { Icon, Image, Skeleton } from '@onekeyhq/components';
 
 function BasicDiscoveryIcon({
   uri,
@@ -27,9 +27,7 @@ function BasicDiscoveryIcon({
       source={{ uri: decodeURIComponent(uri) }}
       fallback={
         <Image.Fallback>
-          <Stack bg="$bgStrong" alignItems="center" justifyContent="center">
-            <Icon name="GlobusOutline" w={size} h={size} />
-          </Stack>
+          <Icon color="$iconSubdued" name="GlobusOutline" w={size} h={size} />
         </Image.Fallback>
       }
     />
