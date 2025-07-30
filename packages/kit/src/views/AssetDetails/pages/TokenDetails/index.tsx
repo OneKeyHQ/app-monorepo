fe6db0e76a66b36d5b982297aa01a6726840b33e
huhuanming <huanming@onekey.so>
2025-07-31 00:14:43 +0800

    Update index.tsx

--- packages/kit/src/views/AssetDetails/pages/TokenDetails/index.tsx
+++ packages/kit/src/views/AssetDetails/pages/TokenDetails/index.tsx
@@ -260,7 +260,7 @@ function TokenDetailsView() {
                 scrollSensitivity: 4,
               } as any
             }
-            renderTabBar={(props) => <Tabs.TabBar {...props} />}
+            renderTabBar={(props) => <Tabs.TabBar {...props} scrollable />}
           >
             {tabs}
           </Tabs.Container>
