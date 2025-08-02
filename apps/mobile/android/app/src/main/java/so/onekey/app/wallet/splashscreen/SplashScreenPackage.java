4eef6b0fc88a2161d72ea053075bbb82c54bf592
huhuanming <huanming@onekey.so>
2025-08-02 23:26:06 +0800

    Update SplashScreenPackage.java

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/splashscreen/SplashScreenPackage.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/splashscreen/SplashScreenPackage.java
@@ -23,7 +23,7 @@ public class SplashScreenPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-        modules.add(new SplashScreenModule());
+        modules.add(new SplashScreenModule(reactContext));
         return modules;
     }
 
