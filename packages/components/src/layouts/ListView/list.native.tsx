ec61af10121b70577009316d9da847884868031e
huhuanming <huanming@onekey.so>
2025-08-04 15:53:28 +0800

    Remove estimatedItemSize prop and update types
    Eliminates the estimatedItemSize prop from List and ListView components, simplifying their usage and internal logic. Also updates type definitions for IListViewRef and keyValuePairs in ExtensionStorage to improve type safety and compatibility.

--- packages/components/src/layouts/ListView/list.native.tsx
+++ packages/components/src/layouts/ListView/list.native.tsx
@@ -11,7 +11,7 @@ import { OptimizationView } from '../../optimization';
 import type { FlashListProps, ListRenderItem } from '@shopify/flash-list';
 import type { StackStyle, Tokens } from '@tamagui/web';
 
-type IListViewRef<T> = FlashList<T>;
+type IListViewRef<T> = typeof FlashList<T>;
 
 type IListViewProps<T> = Omit<
   FlashListProps<T>,
@@ -75,14 +75,7 @@ function BaseListView<T>(
       resolveValues: 'auto',
     },
   );
-  const itemSize = useMemo<number | undefined>(() => {
-    if (typeof estimatedItemSize === 'undefined') {
-      return undefined;
-    }
-    return typeof estimatedItemSize === 'number'
-      ? estimatedItemSize
-      : (getTokenValue(estimatedItemSize, 'size') as number);
-  }, [estimatedItemSize]);
+
   return (
     // FlashList doesn't support the style, so we have to wrap it,
     // and we set default flex = 1 just like FlatList
@@ -90,14 +83,12 @@ function BaseListView<T>(
       style={[{ flex: 1, minHeight: 2 }, style as StyleProp<ViewStyle>]}
     >
       <FlashList<T>
-        ref={ref}
+        ref={ref as any}
         ListHeaderComponentStyle={listHeaderStyle}
         ListFooterComponentStyle={listFooterStyle}
         contentContainerStyle={contentStyle}
         data={data}
         renderItem={renderItem}
-        estimatedItemSize={itemSize}
-        disableAutoLayout={I18nManager.isRTL}
         keyboardDismissMode="on-drag"
         keyboardShouldPersistTaps="handled"
         {...restProps}
