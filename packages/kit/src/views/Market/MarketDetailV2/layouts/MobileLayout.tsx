92444193cee3116f3306d3b31bcb7bc894952360
huhuanming <huanming@onekey.so>
2025-08-13 17:06:14 +0800

    Update MobileLayout.tsx

--- packages/kit/src/views/Market/MarketDetailV2/layouts/MobileLayout.tsx
+++ packages/kit/src/views/Market/MarketDetailV2/layouts/MobileLayout.tsx
@@ -1,9 +1,18 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 
 import { useIntl } from 'react-intl';
+import { useSharedValue } from 'react-native-reanimated';
 
-import { Stack, Tabs } from '@onekeyhq/components';
+import type { ICarouselInstance } from '@onekeyhq/components';
+import {
+  Carousel,
+  ScrollView,
+  Stack,
+  Tabs,
+  YStack,
+} from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import {
   InformationPanel,
@@ -19,7 +28,69 @@ export function MobileLayout() {
   const { tokenAddress, networkId, tokenDetail } = useTokenDetail();
   const intl = useIntl();
   const [panesCount, setPanesCount] = useState(1);
+  const tabNames = useMemo(
+    () => [
+      intl.formatMessage({ id: ETranslations.market_chart }),
+      intl.formatMessage({ id: ETranslations.global_overview }),
+    ],
+    [intl],
+  );
 
+  const carouselRef = useRef<ICarouselInstance>(null);
+  const focusedTab = useSharedValue(tabNames[0]);
+
+  const handleTabChange = useCallback(
+    (tabName: string) => {
+      focusedTab.value = tabName;
+      carouselRef.current?.scrollTo({ index: tabNames.indexOf(tabName) });
+    },
+    [focusedTab, tabNames],
+  );
+
+  const height = useMemo(() => {
+    return platformEnv.isNative ? undefined : 'calc(100vh - 96px)';
+  }, []);
+
+  const renderItem = useCallback(
+    ({ index }: { index: number }) => {
+      if (index === 0) {
+        return (
+          <YStack flex={1} height={height}>
+            <InformationPanel />
+          </YStack>
+        );
+      }
+      return (
+        <ScrollView  flex={1}>
+          <TokenOverview />
+          <TokenActivityOverview />
+        </ScrollView>
+      );
+    },
+    [height],
+  );
+
+  return (
+    <YStack>
+      <Tabs.TabBar
+        divider={false}
+        onTabPress={handleTabChange}
+        tabNames={tabNames}
+        focusedTab={focusedTab}
+      />
+      <Carousel
+        containerStyle={{ height }}
+        ref={carouselRef as any}
+        pagerProps={{
+          scrollSensitivity: 4,
+        }}
+        loop={false}
+        showPagination={false}
+        data={tabNames}
+        renderItem={renderItem}
+      />
+    </YStack>
+  );
   return (
     <>
       {/* Header */}
