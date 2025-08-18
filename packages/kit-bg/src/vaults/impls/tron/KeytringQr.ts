908df7a6dcbef2951a15e3f224c74bfce5b32a39
weatherstar <weather@onekey.so>
2025-08-18 14:41:11 +0800

    feat: create tron qr wallet account

--- packages/kit-bg/src/vaults/impls/tron/KeytringQr.ts
+++ packages/kit-bg/src/vaults/impls/tron/KeytringQr.ts
@@ -1,19 +1,36 @@
 import type { CoreChainApiBase } from '@onekeyhq/core/src/base/CoreChainApiBase';
-import type { ISignedMessagePro, ISignedTxPro } from '@onekeyhq/core/src/types';
-import { NotImplemented } from '@onekeyhq/shared/src/errors';
+import coreChainApi from '@onekeyhq/core/src/instance/coreChainApi';
 
+import HDKey from 'hdkey';
+
+import type {
+  ICoreApiGetAddressItem,
+  ISignedMessagePro,
+  ISignedTxPro,
+} from '@onekeyhq/core/src/types';
+import {
+  NotImplemented,
+  OneKeyErrorAirGapAccountNotFound,
+  OneKeyLocalError,
+} from '@onekeyhq/shared/src/errors';
+
+import localDb from '../../../dbs/local/localDb';
 import { KeyringQrBase } from '../../base/KeyringQrBase';
 
 import type { IDBAccount } from '../../../dbs/local/types';
 import type {
+  IGetChildPathTemplatesParams,
+  IGetChildPathTemplatesResult,
   INormalizeGetMultiAccountsPathParams,
   IPrepareQrAccountsParams,
   ISignMessageParams,
   ISignTransactionParams,
 } from '../../types';
+import { getAirGapSdk } from '@onekeyhq/qr-wallet-sdk';
+import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 
 export class KeyringQr extends KeyringQrBase {
-  override coreApi: CoreChainApiBase | undefined = undefined;
+  override coreApi: CoreChainApiBase = coreChainApi.tron.hd;
 
   override verifySignedTxMatched(..._args: any[]): Promise<void> {
     throw new NotImplemented();
@@ -32,15 +49,88 @@ export class KeyringQr extends KeyringQrBase {
   }
 
   override async prepareAccounts(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     params: IPrepareQrAccountsParams,
   ): Promise<IDBAccount[]> {
-    throw new NotImplemented();
+    const wallet = await localDb.getWallet({ walletId: this.walletId });
+    const networkInfo = await this.getCoreApiNetworkInfo();
+
+    return this.basePrepareHdNormalAccounts(params, {
+      buildAddressesInfo: async ({ usedIndexes }) => {
+        const ret: ICoreApiGetAddressItem[] = [];
+        for (const index of usedIndexes) {
+          if (params?.isVerifyAddressAction) {
+            return this.verifyQrWalletAddressByTwoWayScan(params, {
+              indexes: usedIndexes,
+            });
+          }
+          const { fullPath, airGapAccount, childPathTemplate } =
+            await this.findAirGapAccountInPrepareAccounts(params, {
+              index,
+              wallet,
+            });
+
+          if (!airGapAccount) {
+            throw new OneKeyErrorAirGapAccountNotFound();
+          }
+
+          let publicKey = airGapAccount?.publicKey;
+
+          if (childPathTemplate) {
+            const xpub = airGapAccount?.extendedPublicKey;
+            if (!xpub) {
+              throw new OneKeyLocalError('xpub not found');
+            }
+            let hdk = HDKey.fromExtendedKey(xpub);
+            const childPath = accountUtils.buildPathFromTemplate({
+              template: childPathTemplate,
+              index,
+            });
+            hdk = hdk.derive(`m/${childPath}`);
+            publicKey = hdk.publicKey.toString('hex');
+          }
+
+          if (!publicKey) {
+            throw new OneKeyLocalError('publicKey not found');
+          }
+
+          const addressInfo = await this.coreApi.getAddressFromPublic({
+            publicKey,
+            networkInfo,
+          });
+          if (!addressInfo) {
+            throw new OneKeyLocalError('addressInfo not found');
+          }
+          const { normalizedAddress } = await this.vault.validateAddress(
+            addressInfo.address,
+          );
+          addressInfo.address = normalizedAddress || addressInfo.address;
+          addressInfo.path = fullPath;
+          ret.push(addressInfo);
+          console.log('KeyringQr prepareAccounts', {
+            params,
+            wallet,
+            fullPath,
+            airGapAccount,
+            addressInfo,
+          });
+        }
+        return ret;
+      },
+    });
+  }
+
+  override getChildPathTemplates(
+    _params: IGetChildPathTemplatesParams,
+  ): IGetChildPathTemplatesResult {
+    return {
+      childPathTemplates: ['0/*'],
+    };
   }
 
   override async normalizeGetMultiAccountsPath(
     _params: INormalizeGetMultiAccountsPathParams,
   ): Promise<string> {
-    throw new NotImplemented();
+    const sdk = getAirGapSdk();
+    return sdk.tron.normalizeGetMultiAccountsPath(_params.path);
   }
 }
