5f6a1dbfc798c6c10fd5016078481bdb2386cedc
huhuanming <huanming@onekey.so>
2025-08-26 12:35:12 +0800

    fix: fix versions

--- packages/components/src/content/SecureView/type.ts
+++
@@ -1,5 +0,0 @@
-import type { PropsWithChildren } from 'react';
-
-export type ISecureViewProps = PropsWithChildren<{
-  onScreenCapture: () => void;
-}>;
--- packages/kit/src/views/Onboarding/components/PhaseInputArea.tsx
+++ packages/kit/src/views/Onboarding/components/PhaseInputArea.tsx
@@ -35,7 +35,6 @@ import {
   Page,
   Popover,
   ScrollView,
-  SecureView,
   Select,
   SizableText,
   Stack,
@@ -601,42 +600,40 @@ export function PhaseInputArea({
             ) : null}
           </XStack>
         ) : null}
-        <SecureView onScreenCapture={handleScreenCapture}>
-          <Form form={form}>
-            <XStack bg="red" px="$4" flexWrap="wrap">
-              {Array.from({ length: phraseLengthNumber }).map((_, index) => (
-                <Stack
-                  key={index}
-                  $md={{
-                    flexBasis: '50%',
-                  }}
-                  flexBasis="33.33%"
-                  p="$1"
-                >
-                  <Form.Field name={`phrase${index + 1}`}>
-                    <PhaseInput
-                      index={index}
-                      isShowError={isShowErrors[index]}
-                      onInputBlur={onInputBlur}
-                      phraseLength={phraseLengthNumber}
-                      onInputChange={onInputChange}
-                      onInputFocus={onInputFocus}
-                      onPasteMnemonic={onPasteMnemonic}
-                      suggestionsRef={suggestionsRef}
-                      updateInputValue={updateInputValue}
-                      openStatusRef={openStatusRef}
-                      selectInputIndex={selectInputIndex}
-                      closePopover={closePopover}
-                      onReturnKeyPressed={handleReturnKeyPressed}
-                      getReturnKeyLabel={getReturnKeyLabel}
-                      testID={`phrase-input-index${index}`}
-                    />
-                  </Form.Field>
-                </Stack>
-              ))}
-            </XStack>
-          </Form>
-        </SecureView>
+        <Form form={form}>
+          <XStack bg="red" px="$4" flexWrap="wrap">
+            {Array.from({ length: phraseLengthNumber }).map((_, index) => (
+              <Stack
+                key={index}
+                $md={{
+                  flexBasis: '50%',
+                }}
+                flexBasis="33.33%"
+                p="$1"
+              >
+                <Form.Field name={`phrase${index + 1}`}>
+                  <PhaseInput
+                    index={index}
+                    isShowError={isShowErrors[index]}
+                    onInputBlur={onInputBlur}
+                    phraseLength={phraseLengthNumber}
+                    onInputChange={onInputChange}
+                    onInputFocus={onInputFocus}
+                    onPasteMnemonic={onPasteMnemonic}
+                    suggestionsRef={suggestionsRef}
+                    updateInputValue={updateInputValue}
+                    openStatusRef={openStatusRef}
+                    selectInputIndex={selectInputIndex}
+                    closePopover={closePopover}
+                    onReturnKeyPressed={handleReturnKeyPressed}
+                    getReturnKeyLabel={getReturnKeyLabel}
+                    testID={`phrase-input-index${index}`}
+                  />
+                </Form.Field>
+              </Stack>
+            ))}
+          </XStack>
+        </Form>
 
         <HeightTransition>
           {invalidWordsLength > 0 ? (
