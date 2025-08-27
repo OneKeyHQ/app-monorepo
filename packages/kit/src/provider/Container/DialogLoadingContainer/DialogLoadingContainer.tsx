1fb520098a3392b21bf4fb236ff40846ce5746a8
huhuanming <huanming@onekey.so>
2025-08-27 18:25:03 +0800

    Revert "Update DialogLoadingContainer.tsx"
    This reverts commit 607afe562c006fa2da1da629ee05c30d0468539b.

--- packages/kit/src/provider/Container/DialogLoadingContainer/DialogLoadingContainer.tsx
+++ packages/kit/src/provider/Container/DialogLoadingContainer/DialogLoadingContainer.tsx
@@ -54,7 +54,7 @@ export function DialogLoadingContainer() {
   return (
     <Portal.Body container={Portal.Constant.FULL_WINDOW_OVERLAY_PORTAL}>
       <DialogContainer
-        key={key}
+        key={undefined}
         open={visible}
         // ref={dialogRef}
         // onClose={buildForwardOnClose({ onClose })}
