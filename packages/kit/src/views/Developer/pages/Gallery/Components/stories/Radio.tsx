485cda49ba65952cd8eaf855d1dfb7cbf354790a
Leon <lixiao.dev@gmail.com>
2025-09-01 10:54:25 +0800

    feat: sign and verify message (#8221)
    * feat: add Sign and Verify functionality to wallet actions
    
    * feat: implement segment control for sign and verify actions in SignAndVerifyMessage component
    
    * feat: add orientation and gap props to Radio component for flexible layout options
    
    * feat: refactor SignAndVerifyMessage component to use form handling with useForm hook
    
    * feat: enhance InputAddOnItem to support custom renderContent prop and update SignAndVerifyMessage to utilize NetworkAvatar in form input
    
    * feat: integrate serviceInternalSignAndVerify into SignAndVerifyMessage for enhanced signing functionality
    
    * feat: enhance WalletActionSignAndVerify and SignAndVerifyMessage components to support detailed signing parameters and improve state management
    
    * feat: generate select options
    
    * feat: update SignAndVerifyMessage to utilize useRef for sign accounts and improve account selection logic
    
    * feat: implement default account selection logic in SignAndVerifyMessage for improved user experience
    
    * feat: add disabled option support to Radio component and update examples in stories
    
    * feat: update SignAndVerifyMessage to prevent unnecessary updates on account info change and disable BIP137 option
    
    * feat: Add form rules
    
    * feat: update package versions for @onekeyfe dependencies and enhance SignAndVerifyMessage with improved signing logic and state management
    
    * feat: add clipboard functionality to SignAndVerifyMessage for copying signed messages
    
    * feat: enhance SignAndVerifyMessage to clear signature on message or account changes and improve state management
    
    * feat: generate sol unsigned message
    
    * feat: BTC sign message
    
    * feat: update SignAndVerifyMessage to enhance radio options logic and account handling
    
    * feat: Support bip137 on hd wallet
    
    * refactor: add SignForm
    
    * feat: add VerifyForm
    
    * feat: verify ethereum signed message
    
    * feat: Solana verify message
    
    * feat: Bitcoin signature
    
    * feat: add format field
    
    * fix: optimize render
    
    * fix: optimize render
    
    * refactor: update VerifyForm to remove unused prop and set default format
    
    * chore: upgrade sdk
    
    * chore: lint

--- packages/kit/src/views/Developer/pages/Gallery/Components/stories/Radio.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/stories/Radio.tsx
@@ -19,6 +19,38 @@ const RadioExample = () => {
   );
 };
 
+const HorizontalRadioExample = () => {
+  const [radioValue, setRadioValue] = useState<string>('medium');
+  const options = [
+    { label: 'Small', value: 'small' },
+    { label: 'Medium', value: 'medium' },
+    { label: 'Large', value: 'large' },
+    { label: 'Extra Large', value: 'xl' },
+  ];
+
+  return (
+    <Radio
+      value={radioValue}
+      onChange={setRadioValue}
+      orientation="horizontal"
+      gap="$6"
+      options={options}
+    />
+  );
+};
+
+const RadioItemDisabledExample = () => {
+  const [radioValue, setRadioValue] = useState<string>();
+  const options = [
+    { label: 'Option 4', value: '4' },
+    { label: 'Option 5', value: '5' },
+    { label: 'Option 6', value: '6', disabled: true },
+  ];
+  return (
+    <Radio value={radioValue} onChange={setRadioValue} options={options} />
+  );
+};
+
 const RadioGallery = () => (
   <Layout
     filePath={__CURRENT_FILE_PATH__}
@@ -27,6 +59,14 @@ const RadioGallery = () => (
       {
         title: 'Default',
         element: <RadioExample />,
+      },
+      {
+        title: 'Horizontal Layout',
+        element: <HorizontalRadioExample />,
+      },
+      {
+        title: 'Radio Item Disabled',
+        element: <RadioItemDisabledExample />,
       },
     ]}
   />
