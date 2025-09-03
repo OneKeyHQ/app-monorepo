24914fd8000e38f26e46b14ffb725f57113552fc
huhuanming <huanming@onekey.so>
2025-09-04 02:00:37 +0800

    Update PerpMobileLayout.tsx

--- packages/kit/src/views/Perp/layouts/PerpMobileLayout.tsx
+++ packages/kit/src/views/Perp/layouts/PerpMobileLayout.tsx
@@ -1,5 +1,41 @@
-import { YStack } from '@onekeyhq/components';
+import { useCallback } from 'react';
+
+import { SizableText, Tabs, XStack, YStack } from '@onekeyhq/components';
 
 export function PerpMobileLayout() {
-  return <YStack />;
+  const renderTabHeader = useCallback(() => {
+    return (
+      <XStack gap="$3">
+        <YStack>
+          <SizableText>Long Or Short</SizableText>
+        </YStack>
+        <YStack>
+          <SizableText>PriceBook</SizableText>
+        </YStack>
+      </XStack>
+    );
+  }, []);
+  return (
+    <YStack>
+      <XStack>
+        <SizableText>BTC</SizableText>
+      </XStack>
+      <Tabs.Container
+        initialTabName="Orders"
+        renderHeader={renderTabHeader}
+        renderTabBar={(props) => <Tabs.TabBar {...props} />}
+      >
+        <Tabs.Tab name="Orders">
+          <Tabs.ScrollView>
+            <SizableText>Orders</SizableText>
+          </Tabs.ScrollView>
+        </Tabs.Tab>
+        <Tabs.Tab name="Positions">
+          <Tabs.ScrollView>
+            <SizableText>Positions</SizableText>
+          </Tabs.ScrollView>
+        </Tabs.Tab>
+      </Tabs.Container>
+    </YStack>
+  );
 }
