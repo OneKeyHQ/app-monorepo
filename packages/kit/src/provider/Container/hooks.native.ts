6cf917be359b1f20095e3773f32f0bcdcc8e591a
huhuanming <huanming@onekey.so>
2025-09-09 12:58:31 +0800

    Update hooks.native.ts

--- packages/kit/src/provider/Container/hooks.native.ts
+++ packages/kit/src/provider/Container/hooks.native.ts
@@ -6,8 +6,8 @@ export const useInitialNotification = () => {
   const coldStartRef = useRef(true);
   useEffect(() => {
     setTimeout(async () => {
-      coldStartRef.current = false;
       if (coldStartRef.current) {
+        coldStartRef.current = false;
         const notifeeNotification = await notifee.getInitialNotification();
         alert(`notifeeNotification: ${JSON.stringify(notifeeNotification)}`);
       }
