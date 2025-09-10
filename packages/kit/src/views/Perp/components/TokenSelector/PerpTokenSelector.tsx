944ee31f5fd5bc853cbc3da5dbaf39f94a999da7
ezailWang <jelly@onekey.so>
2025-09-10 14:14:36 +0800

    fix: perp token selector list opt
    Signed-off-by: ezailWang <jelly@onekey.so>

--- packages/kit/src/views/Perp/components/TokenSelector/PerpTokenSelector.tsx
+++ packages/kit/src/views/Perp/components/TokenSelector/PerpTokenSelector.tsx
@@ -2,26 +2,21 @@ import { useState } from 'react';
 
 import {
   Badge,
-  Button,
   Icon,
-  NumberSizeableText,
+  ListView,
   Popover,
-  ScrollView,
   SearchBar,
   SizableText,
   Spinner,
   XStack,
   YStack,
 } from '@onekeyhq/components';
-import {
-  NUMBER_FORMATTER,
-  formatDisplayNumber,
-  numberFormat,
-} from '@onekeyhq/shared/src/utils/numberUtils';
 
 // eslint-disable-next-line import-path/parent-depth
 import { Token } from '../../../../components/Token';
 import { usePerpTokenSelector } from '../../hooks';
+
+import { PerpTokenSelectorRow } from './PerpTokenSelectorRow';
 
 function PerpTokenSelector() {
   const [isOpen, setIsOpen] = useState(false);
@@ -93,48 +88,58 @@ function PerpTokenSelector() {
               onChangeText={setSearchQuery}
             />
           </XStack>
-          <XStack
-            px="$5"
-            py="$3"
-            borderBottomWidth="$px"
-            borderBottomColor="$borderSubdued"
-          >
-            <XStack width={140} justifyContent="flex-start">
-              <SizableText size="$bodySm" color="$textSubdued">
-                Asset
-              </SizableText>
-            </XStack>
-            <XStack width={80} justifyContent="flex-start">
-              <SizableText size="$bodySm" color="$textSubdued">
-                Last Price
-              </SizableText>
-            </XStack>
-            <XStack width={120} justifyContent="flex-start">
-              <SizableText size="$bodySm" color="$textSubdued">
-                24h Change
-              </SizableText>
-            </XStack>
-            <XStack width={100} justifyContent="flex-start">
-              <SizableText size="$bodySm" color="$textSubdued">
-                8h Funding
-              </SizableText>
-            </XStack>
-            <XStack width={100} justifyContent="flex-start">
-              <SizableText size="$bodySm" color="$textSubdued">
-                24h Volume
-              </SizableText>
-            </XStack>
-            <XStack flex={1} justifyContent="flex-end">
-              <SizableText size="$bodySm" color="$textSubdued">
-                Open Interest
-              </SizableText>
-            </XStack>
-          </XStack>
 
           {/* Token List */}
-          <ScrollView flex={1} maxHeight={300}>
-            <YStack>
-              {filteredTokens.length === 0 ? (
+          <YStack flex={1} maxHeight={300}>
+            <ListView
+              data={filteredTokens.filter((token) => !token.isDelisted)}
+              renderItem={({ item: token }) => (
+                <PerpTokenSelectorRow
+                  token={token}
+                  onPress={() => handleSelectToken(token.name)}
+                />
+              )}
+              estimatedItemSize={60}
+              ListHeaderComponent={
+                <XStack
+                  px="$5"
+                  py="$3"
+                  borderBottomWidth="$px"
+                  borderBottomColor="$borderSubdued"
+                >
+                  <XStack width={140} justifyContent="flex-start">
+                    <SizableText size="$bodySm" color="$textSubdued">
+                      Asset
+                    </SizableText>
+                  </XStack>
+                  <XStack width={80} justifyContent="flex-start">
+                    <SizableText size="$bodySm" color="$textSubdued">
+                      Last Price
+                    </SizableText>
+                  </XStack>
+                  <XStack width={120} justifyContent="flex-start">
+                    <SizableText size="$bodySm" color="$textSubdued">
+                      24h Change
+                    </SizableText>
+                  </XStack>
+                  <XStack width={100} justifyContent="flex-start">
+                    <SizableText size="$bodySm" color="$textSubdued">
+                      8h Funding
+                    </SizableText>
+                  </XStack>
+                  <XStack width={100} justifyContent="flex-start">
+                    <SizableText size="$bodySm" color="$textSubdued">
+                      24h Volume
+                    </SizableText>
+                  </XStack>
+                  <XStack flex={1} justifyContent="flex-end">
+                    <SizableText size="$bodySm" color="$textSubdued">
+                      Open Interest
+                    </SizableText>
+                  </XStack>
+                </XStack>
+              }
+              ListEmptyComponent={
                 <XStack p="$5" justifyContent="center">
                   <SizableText size="$bodySm" color="$textSubdued">
                     {searchQuery
@@ -142,107 +147,9 @@ function PerpTokenSelector() {
                       : 'Loading tokens...'}
                   </SizableText>
                 </XStack>
-              ) : (
-                filteredTokens.map((token) =>
-                  token.isDelisted ? null : (
-                    <XStack
-                      key={token.name}
-                      onPress={() => handleSelectToken(token.name)}
-                      borderRadius="$0"
-                      justifyContent="flex-start"
-                      hoverStyle={{ bg: '$bgHover' }}
-                      px="$5"
-                      py="$3"
-                      cursor="pointer"
-                    >
-                      <XStack flex={1} alignItems="center">
-                        {/* Token Info */}
-                        <XStack
-                          width={140}
-                          justifyContent="flex-start"
-                          gap="$2"
-                          alignItems="center"
-                        >
-                          <Token
-                            size="xs"
-                            tokenImageUri={`https://app.hyperliquid.xyz/coins/${token.name}.svg`}
-                            fallbackIcon="CryptoCoinOutline"
-                          />
-                          <SizableText size="$bodySmMedium">
-                            {token.name}
-                          </SizableText>
-                          <Badge radius="$2" bg="$bgInfo">
-                            <SizableText color="$textInfo" size="$bodySm">
-                              {token.maxLeverage}x
-                            </SizableText>
-                          </Badge>
-                        </XStack>
-
-                        <XStack width={80} justifyContent="flex-start">
-                          <NumberSizeableText
-                            formatter="price"
-                            size="$bodySmMedium"
-                            color="$text"
-                          >
-                            {token.markPrice}
-                          </NumberSizeableText>
-                        </XStack>
-
-                        <XStack width={120} justifyContent="flex-start">
-                          <SizableText
-                            size="$bodySm"
-                            color={
-                              token.change24hPercent > 0 ? '$green11' : '$red11'
-                            }
-                          >
-                            {numberFormat(token.change24h, {
-                              formatter: 'price',
-                            })}{' '}
-                            /{' '}
-                            <NumberSizeableText
-                              size="$bodySm"
-                              color={
-                                token.change24hPercent > 0
-                                  ? '$green11'
-                                  : '$red11'
-                              }
-                              formatter="priceChange"
-                              formatterOptions={{ showPlusMinusSigns: true }}
-                            >
-                              {token.change24hPercent.toString()}
-                            </NumberSizeableText>
-                          </SizableText>
-                        </XStack>
-                        <XStack width={100} justifyContent="flex-start">
-                          <SizableText size="$bodySm" color="$text">
-                            {(Number(token.fundingRate) * 100).toFixed(4)}%
-                          </SizableText>
-                        </XStack>
-
-                        <XStack width={100} justifyContent="flex-start">
-                          <SizableText size="$bodySm" color="$text">
-                            $
-                            {formatDisplayNumber(
-                              NUMBER_FORMATTER.marketCap(token.volume24h),
-                            )}
-                          </SizableText>
-                        </XStack>
-
-                        <XStack flex={1} justifyContent="flex-end">
-                          <SizableText size="$bodySm" color="$text">
-                            $
-                            {formatDisplayNumber(
-                              NUMBER_FORMATTER.marketCap(token.openInterest),
-                            )}
-                          </SizableText>
-                        </XStack>
-                      </XStack>
-                    </XStack>
-                  ),
-                )
-              )}
-            </YStack>
-          </ScrollView>
+              }
+            />
+          </YStack>
         </YStack>
       }
     />
