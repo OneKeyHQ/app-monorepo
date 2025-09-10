284a5bd7018e0e6e0e6895469c397b4945cc6fad
Leon <lixiao.dev@gmail.com>
2025-09-10 17:37:20 +0800

    refactor: remove unused round function and keep only necessary formatting functions in OrderBook utils

--- packages/kit/src/views/Perp/components/OrderBook/utils.ts
+++ packages/kit/src/views/Perp/components/OrderBook/utils.ts
@@ -1,17 +1,10 @@
-export function round(value: number, decimals = 0) {
-  return Number(`${Math.round(Number(`${value}e${decimals}`))}e-${decimals}`);
-}
-
-export function floor(value: number, decimals = 0) {
+// Only keep the functions that are actually used
+function floor(value: number, decimals = 0) {
   return Number(`${Math.floor(Number(`${value}e${decimals}`))}e-${decimals}`);
 }
 
-export function ceil(value: number, decimals = 0) {
+function ceil(value: number, decimals = 0) {
   return Number(`${Math.ceil(Number(`${value}e${decimals}`))}e-${decimals}`);
-}
-
-export function roundToTick(n: number, tickSize: number) {
-  return round(n * (1 / tickSize)) / (1 / tickSize);
 }
 
 export function floorToTick(n: number, tickSize: number) {
