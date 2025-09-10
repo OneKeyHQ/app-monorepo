62e636f85b046ec9ac79528772f1f00386c8af5f
huhuanming <huanming@onekey.so>
2025-09-10 21:31:03 +0800

    Refactor PerpTokenSelector to use popover context
    Extracted the token selector content into a separate component and integrated usePopoverContext for closing the popover after token selection. Improved performance by wrapping the main component with useMemo and updated the popover's renderContent to use the new content component.

--- packages/kit/src/views/Perp/components/TokenSelector/PerpTokenSelector.tsx
+++ packages/kit/src/views/Perp/components/TokenSelector/PerpTokenSelector.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import {
   Badge,
@@ -10,6 +10,7 @@ import {
   Spinner,
   XStack,
   YStack,
+  usePopoverContext,
 } from '@onekeyhq/components';
 
 // eslint-disable-next-line import-path/parent-depth
@@ -18,141 +19,147 @@ import { usePerpTokenSelector } from '../../hooks';
 
 import { PerpTokenSelectorRow } from './PerpTokenSelectorRow';
 
-function PerpTokenSelector() {
-  const [isOpen, setIsOpen] = useState(false);
-  const {
-    currentToken,
-    searchQuery,
-    setSearchQuery,
-    filteredTokens,
-    selectToken,
-    isLoading,
-  } = usePerpTokenSelector();
+function PerpTokenSelectorContent({ isOpen }: { isOpen: boolean }) {
+  const { closePopover } = usePopoverContext();
+  const { searchQuery, setSearchQuery, filteredTokens, selectToken } =
+    usePerpTokenSelector();
 
   const handleSelectToken = async (symbol: string) => {
     try {
       await selectToken(symbol);
-      setIsOpen(false);
+      await closePopover?.();
     } catch (error) {
       console.error('Failed to switch token:', error);
     }
   };
-
+  if (!isOpen) {
+    return null;
+  }
   return (
-    <Popover
-      title="Select Token"
-      floatingPanelProps={{
-        width: 700,
-      }}
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      placement="bottom-start"
-      renderTrigger={
-        <Badge
-          gap="$3"
-          bg="$bgApp"
-          cursor="pointer"
-          hoverStyle={{
-            p: '$2',
-            borderRadius: '$full',
-            bg: '$bgHover',
+    <YStack>
+      <XStack px="$5" pt="$5">
+        <SearchBar
+          containerProps={{
+            borderRadius: '$2',
           }}
-          pressStyle={{
-            p: '$2',
-            borderRadius: '$full',
-            bg: '$bgActive',
-          }}
-        >
-          <Token
-            size="md"
-            tokenImageUri={`https://app.hyperliquid.xyz/coins/${currentToken}.svg`}
-            fallbackIcon="CryptoCoinOutline"
-          />
+          autoFocus
+          placeholder="Search"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+      </XStack>
 
-          {/* Token Name */}
-          <SizableText size="$heading2xl">{currentToken}</SizableText>
-          <Icon name="ChevronBottomOutline" size="$4" />
-          {isLoading ? <Spinner size="small" /> : null}
-        </Badge>
-      }
-      renderContent={
-        <YStack>
-          <XStack px="$5" pt="$5">
-            <SearchBar
-              containerProps={{
-                borderRadius: '$2',
-              }}
-              autoFocus
-              placeholder="Search"
-              value={searchQuery}
-              onChangeText={setSearchQuery}
+      {/* Token List */}
+      <YStack flex={1} maxHeight={300}>
+        <ListView
+          useFlashList
+          data={filteredTokens.filter((token) => !token.isDelisted)}
+          renderItem={({ item: token }) => (
+            <PerpTokenSelectorRow
+              token={token}
+              onPress={() => handleSelectToken(token.name)}
             />
-          </XStack>
+          )}
+          ListHeaderComponent={
+            <XStack
+              px="$5"
+              py="$3"
+              borderBottomWidth="$px"
+              borderBottomColor="$borderSubdued"
+            >
+              <XStack width={140} justifyContent="flex-start">
+                <SizableText size="$bodySm" color="$textSubdued">
+                  Asset
+                </SizableText>
+              </XStack>
+              <XStack width={80} justifyContent="flex-start">
+                <SizableText size="$bodySm" color="$textSubdued">
+                  Last Price
+                </SizableText>
+              </XStack>
+              <XStack width={120} justifyContent="flex-start">
+                <SizableText size="$bodySm" color="$textSubdued">
+                  24h Change
+                </SizableText>
+              </XStack>
+              <XStack width={100} justifyContent="flex-start">
+                <SizableText size="$bodySm" color="$textSubdued">
+                  8h Funding
+                </SizableText>
+              </XStack>
+              <XStack width={100} justifyContent="flex-start">
+                <SizableText size="$bodySm" color="$textSubdued">
+                  24h Volume
+                </SizableText>
+              </XStack>
+              <XStack flex={1} justifyContent="flex-end">
+                <SizableText size="$bodySm" color="$textSubdued">
+                  Open Interest
+                </SizableText>
+              </XStack>
+            </XStack>
+          }
+          ListEmptyComponent={
+            <XStack p="$5" justifyContent="center">
+              <SizableText size="$bodySm" color="$textSubdued">
+                {searchQuery ? 'No matching tokens found' : 'Loading tokens...'}
+              </SizableText>
+            </XStack>
+          }
+        />
+      </YStack>
+    </YStack>
+  );
+}
 
-          {/* Token List */}
-          <YStack flex={1} maxHeight={300}>
-            <ListView
-              data={filteredTokens.filter((token) => !token.isDelisted)}
-              renderItem={({ item: token }) => (
-                <PerpTokenSelectorRow
-                  token={token}
-                  onPress={() => handleSelectToken(token.name)}
-                />
-              )}
-              estimatedItemSize={60}
-              ListHeaderComponent={
-                <XStack
-                  px="$5"
-                  py="$3"
-                  borderBottomWidth="$px"
-                  borderBottomColor="$borderSubdued"
-                >
-                  <XStack width={140} justifyContent="flex-start">
-                    <SizableText size="$bodySm" color="$textSubdued">
-                      Asset
-                    </SizableText>
-                  </XStack>
-                  <XStack width={80} justifyContent="flex-start">
-                    <SizableText size="$bodySm" color="$textSubdued">
-                      Last Price
-                    </SizableText>
-                  </XStack>
-                  <XStack width={120} justifyContent="flex-start">
-                    <SizableText size="$bodySm" color="$textSubdued">
-                      24h Change
-                    </SizableText>
-                  </XStack>
-                  <XStack width={100} justifyContent="flex-start">
-                    <SizableText size="$bodySm" color="$textSubdued">
-                      8h Funding
-                    </SizableText>
-                  </XStack>
-                  <XStack width={100} justifyContent="flex-start">
-                    <SizableText size="$bodySm" color="$textSubdued">
-                      24h Volume
-                    </SizableText>
-                  </XStack>
-                  <XStack flex={1} justifyContent="flex-end">
-                    <SizableText size="$bodySm" color="$textSubdued">
-                      Open Interest
-                    </SizableText>
-                  </XStack>
-                </XStack>
-              }
-              ListEmptyComponent={
-                <XStack p="$5" justifyContent="center">
-                  <SizableText size="$bodySm" color="$textSubdued">
-                    {searchQuery
-                      ? 'No matching tokens found'
-                      : 'Loading tokens...'}
-                  </SizableText>
-                </XStack>
-              }
+function PerpTokenSelector() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { currentToken, isLoading } = usePerpTokenSelector();
+
+  return useMemo(
+    () => (
+      <Popover
+        title="Select Token"
+        floatingPanelProps={{
+          width: 700,
+        }}
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        placement="bottom-start"
+        renderTrigger={
+          <Badge
+            gap="$3"
+            bg="$bgApp"
+            cursor="pointer"
+            hoverStyle={{
+              p: '$2',
+              borderRadius: '$full',
+              bg: '$bgHover',
+            }}
+            pressStyle={{
+              p: '$2',
+              borderRadius: '$full',
+              bg: '$bgActive',
+            }}
+          >
+            <Token
+              size="md"
+              tokenImageUri={`https://app.hyperliquid.xyz/coins/${currentToken}.svg`}
+              fallbackIcon="CryptoCoinOutline"
             />
-          </YStack>
-        </YStack>
-      }
-    />
+
+            {/* Token Name */}
+            <SizableText size="$heading2xl">{currentToken}</SizableText>
+            <Icon name="ChevronBottomOutline" size="$4" />
+            {isLoading ? <Spinner size="small" /> : null}
+          </Badge>
+        }
+        renderContent={({ isOpen: isOpenProp }) =>
+          PerpTokenSelectorContent({ isOpen: isOpenProp ?? false })
+        }
+      />
+    ),
+    [isOpen, currentToken, isLoading],
   );
 }
 
