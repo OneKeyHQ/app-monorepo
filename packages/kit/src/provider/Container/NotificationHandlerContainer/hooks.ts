d59032deca71f2685a15eba922ef1443a84dc5fe
huhuanming <huanming@onekey.so>
2025-09-11 18:46:43 +0800

    Update hooks.ts

--- packages/kit/src/provider/Container/NotificationHandlerContainer/hooks.ts
+++ packages/kit/src/provider/Container/NotificationHandlerContainer/hooks.ts
@@ -1,4 +1,5 @@
-import { RefObject } from 'react';
+import type { RefObject } from 'react';
+
 import type { IAccountSelectorActiveAccountInfo } from '../../../states/jotai/contexts/accountSelector';
 
 export const useInitialNotification = (
