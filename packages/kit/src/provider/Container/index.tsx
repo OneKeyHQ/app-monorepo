78922910b57c6076df6b04ca05f316f294c4a54d
huhuanming <huanming@onekey.so>
2025-09-11 19:05:34 +0800

    Update index.tsx

--- packages/kit/src/provider/Container/index.tsx
+++ packages/kit/src/provider/Container/index.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 import { RootSiblingParent } from 'react-native-root-siblings';
 
-import { Dialog } from '@onekeyhq/components';
 import appGlobals from '@onekeyhq/shared/src/appGlobals';
-import {
-  EAppEventBusNames,
-  appEventBus,
-} from '@onekeyhq/shared/src/eventBus/appEventBus';
+
 import LazyLoad from '@onekeyhq/shared/src/lazyLoad';
-import { navigateToNotificationDetailByLocalParams } from '@onekeyhq/shared/src/utils/notificationsUtils';
-import {
-  openUrlExternal,
-  openUrlInApp,
-} from '@onekeyhq/shared/src/utils/openUrlUtils';
-import {
-  ENotificationViewDialogActionType,
-  type IJPushRemotePushMessageInfo,
-  type INotificationViewDialogPayload,
+
+import type {
+   IJPushRemotePushMessageInfo,
 } from '@onekeyhq/shared/types/notification';
 
 import backgroundApiProxy from '../../background/instance/backgroundApiProxy';
@@ -106,68 +96,6 @@ export function ColdStartByNotification() {
         },
       );
     }
-    const handleShowFallbackUpdateDialog = ({
-      version,
-    }: {
-      version: string | null | undefined;
-    }) => {
-      showFallbackUpdateDialog(version);
-    };
-    appEventBus.on(
-      EAppEventBusNames.ShowFallbackUpdateDialog,
-      handleShowFallbackUpdateDialog,
-    );
-    const handleShowNotificationViewDialog = ({
-      payload: payloadObj,
-      localParams,
-    }: {
-      payload: INotificationViewDialogPayload;
-      localParams: Record<string, string | undefined>;
-    }) => {
-      const { onConfirm, ...rest } = payloadObj;
-      Dialog.show({
-        ...rest,
-        onConfirm: async () => {
-          const { actionType, payload } = onConfirm;
-          switch (actionType) {
-            case ENotificationViewDialogActionType.navigate:
-              try {
-                await navigateToNotificationDetailByLocalParams({
-                  payload: payload as any,
-                  localParams,
-                  getEarnAccount: (props) =>
-                    backgroundApiProxy.serviceStaking.getEarnAccount(props),
-                });
-              } catch (error) {
-                showFallbackUpdateDialog(null);
-              }
-              break;
-            case ENotificationViewDialogActionType.openInApp:
-              openUrlInApp(payload as string);
-              break;
-            case ENotificationViewDialogActionType.openInBrowser:
-              openUrlExternal(payload as string);
-              break;
-            default:
-              break;
-          }
-        },
-      });
-    };
-    appEventBus.on(
-      EAppEventBusNames.ShowNotificationViewDialog,
-      handleShowNotificationViewDialog,
-    );
-    return () => {
-      appEventBus.off(
-        EAppEventBusNames.ShowFallbackUpdateDialog,
-        handleShowFallbackUpdateDialog,
-      );
-      appEventBus.off(
-        EAppEventBusNames.ShowNotificationViewDialog,
-        handleShowNotificationViewDialog,
-      );
-    };
   }, [isVersionCompatible, showFallbackUpdateDialog]);
 
   return null;
