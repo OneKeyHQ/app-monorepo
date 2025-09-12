b8542dbfcb34c8545a1484a912bd130b4c01bac6
huhuanming <huanming@onekey.so>
2025-09-12 10:21:51 +0800

    Delete LaunchOptionsManager.desktop.ts

--- packages/shared/src/modules/LaunchOptionsManager/LaunchOptionsManager.desktop.ts
+++
@@ -1,6 +0,0 @@
-import type { ILaunchOptionsManagerInterface } from './type';
-
-export default {
-  getLaunchOptions: () => Promise.resolve(null),
-  clearLaunchOptions: () => Promise.resolve(true),
-} as ILaunchOptionsManagerInterface;
