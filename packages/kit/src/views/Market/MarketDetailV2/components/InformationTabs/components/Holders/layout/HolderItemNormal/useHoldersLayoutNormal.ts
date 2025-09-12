5ee058798eea3ecc6b76268e42a5967adc0a321e
limichange <limichange@hotmail.com>
2025-09-12 17:47:43 +0800

    refactor: remove unused styles from useHoldersLayoutNormal for cleaner code

--- packages/kit/src/views/Market/MarketDetailV2/components/InformationTabs/components/Holders/layout/HolderItemNormal/useHoldersLayoutNormal.ts
+++ packages/kit/src/views/Market/MarketDetailV2/components/InformationTabs/components/Holders/layout/HolderItemNormal/useHoldersLayoutNormal.ts
@@ -22,26 +22,5 @@ export const useHoldersLayoutNormal = () => {
     },
   };
 
-  // const styles = {
-  //   rank: {
-  //     width: '$10',
-  //   },
-  //   address: {
-  //     width: 200,
-  //   },
-  //   percentage: {
-  //     width: '$40',
-  //     textAlign: 'right' as const,
-  //   },
-  //   amount: {
-  //     width: '$40',
-  //     textAlign: 'right' as const,
-  //   },
-  //   value: {
-  //     width: '$40',
-  //     textAlign: 'right' as const,
-  //   },
-  // };
-
   return { styles };
 };
