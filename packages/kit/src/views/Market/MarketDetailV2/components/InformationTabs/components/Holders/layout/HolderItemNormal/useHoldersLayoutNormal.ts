74bf9a4608ffaa9fc707af9e0c90716d4e42a056
limichange <limichange@hotmail.com>
2025-09-12 17:45:33 +0800

    update

--- packages/kit/src/views/Market/MarketDetailV2/components/InformationTabs/components/Holders/layout/HolderItemNormal/useHoldersLayoutNormal.ts
+++ packages/kit/src/views/Market/MarketDetailV2/components/InformationTabs/components/Holders/layout/HolderItemNormal/useHoldersLayoutNormal.ts
@@ -1,24 +1,47 @@
 export const useHoldersLayoutNormal = () => {
   const styles = {
     rank: {
-      width: '$10',
+      width: '5%',
+      minWidth: 40,
     },
     address: {
-      width: 200,
+      width: '20%',
+      minWidth: 200,
     },
     percentage: {
-      width: '$40',
+      width: '25%',
       textAlign: 'right' as const,
     },
     amount: {
-      width: '$40',
+      width: '25%',
       textAlign: 'right' as const,
     },
     value: {
-      width: '$40',
+      width: '25%',
       textAlign: 'right' as const,
     },
   };
+
+  // const styles = {
+  //   rank: {
+  //     width: '$10',
+  //   },
+  //   address: {
+  //     width: 200,
+  //   },
+  //   percentage: {
+  //     width: '$40',
+  //     textAlign: 'right' as const,
+  //   },
+  //   amount: {
+  //     width: '$40',
+  //     textAlign: 'right' as const,
+  //   },
+  //   value: {
+  //     width: '$40',
+  //     textAlign: 'right' as const,
+  //   },
+  // };
 
   return { styles };
 };
