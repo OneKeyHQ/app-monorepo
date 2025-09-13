ea55078e688ffe60720ccb89ad40e030cd1f4432
huhuanming <huanming@onekey.so>
2025-09-14 02:44:27 +0800

    Update electronUpdateListeners.desktop.ts

--- packages/shared/src/modules3rdParty/auto-update/electronUpdateListeners.desktop.ts
+++ packages/shared/src/modules3rdParty/auto-update/electronUpdateListeners.desktop.ts
@@ -5,24 +5,21 @@ import type { IElectronUpdateListeners, IUpdateDownloadedEvent } from './type';
 
 export const electronUpdateListeners: IElectronUpdateListeners = {
   onProgressUpdate: (callback: (params: IUpdateProgressUpdate) => void) => {
-    return globalThis.desktopApi?.on?.(
+    return globalThis.desktopApi.on(
       ipcMessageKeys.UPDATE_DOWNLOADING,
       callback,
     );
   },
   onDownloaded: (callback: (params: IUpdateDownloadedEvent) => void) => {
-    return globalThis.desktopApi?.on?.(
-      ipcMessageKeys.UPDATE_DOWNLOADED,
-      callback,
-    );
+    return globalThis.desktopApi.on(ipcMessageKeys.UPDATE_DOWNLOADED, callback);
   },
   onDownloadedFileEvent: (callback: (fileUrl: string) => void) => {
-    return globalThis.desktopApi?.on?.(
+    return globalThis.desktopApi.on(
       ipcMessageKeys.UPDATE_DOWNLOAD_FILE_INFO,
       callback,
     );
   },
   onUpdateError: (callback: (params: { message: string }) => void) => {
-    return globalThis.desktopApi?.on?.(ipcMessageKeys.UPDATE_ERROR, callback);
+    return globalThis.desktopApi.on(ipcMessageKeys.UPDATE_ERROR, callback);
   },
 };
