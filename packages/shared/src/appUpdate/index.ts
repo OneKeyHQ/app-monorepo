c89ac91b4a86e38182da25ac96e0504f6498431a
huhuanming <huanming@onekey.so>
2025-09-16 01:13:42 +0800

    Update index.ts

--- packages/shared/src/appUpdate/index.ts
+++ packages/shared/src/appUpdate/index.ts
@@ -10,7 +10,7 @@ export * from './utils';
 export * from './type';
 
 const APP_VERSION = platformEnv.version ?? '1.0.0';
-const APP_BUNDLE_VERSION = platformEnv.bundleVersion ?? '1.0.0';
+const APP_BUNDLE_VERSION = platformEnv.bundleVersion ?? 1;
 
 interface IIsNeedUpdateParams {
   latestVersion?: string;
