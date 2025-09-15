96f45a2d73b1f9957601e7164005eba5839b6cea
huhuanming <huanming@onekey.so>
2025-09-15 21:52:58 +0800

    Update index.ts

--- packages/shared/src/appUpdate/index.ts
+++ packages/shared/src/appUpdate/index.ts
@@ -22,26 +22,46 @@ export const isNeedUpdate: (params: IIsNeedUpdateParams) => {
   shouldUpdate: boolean;
   fileType: EUpdateFileType;
 } = ({ latestVersion, jsBundleVersion, status }: IIsNeedUpdateParams) => {
-  if (jsBundleVersion) {
+  // App shell version is equal to the latest version, check js bundle version
+  if (
+    jsBundleVersion &&
+    latestVersion &&
+    semver.eq(latestVersion, APP_VERSION)
+  ) {
     return {
-      isNeedUpdate:
+      shouldUpdate: !!(
         latestVersion &&
-        semver.eq(latestVersion, APP_VERSION) &&
         Number(jsBundleVersion) > Number(APP_BUNDLE_VERSION || 0) &&
-        status !== EAppUpdateStatus.done,
+        status !== EAppUpdateStatus.done
+      ),
       fileType: EUpdateFileType.jsBundle,
     };
   }
   return {
-    shouldUpdate:
+    shouldUpdate: !!(
       latestVersion &&
       semver.gt(latestVersion, APP_VERSION) &&
-      status !== EAppUpdateStatus.done,
+      status !== EAppUpdateStatus.done
+    ),
     fileType: EUpdateFileType.appShell,
   };
 };
 
-export const isFirstLaunchAfterUpdated = (appUpdateInfo: IAppUpdateInfo) =>
-  appUpdateInfo.status !== EAppUpdateStatus.done &&
-  appUpdateInfo.latestVersion &&
-  semver.eq(APP_VERSION, appUpdateInfo.latestVersion);
+export const isFirstLaunchAfterUpdated = (appUpdateInfo: IAppUpdateInfo) => {
+  // App shell version is equal to the latest version, check js bundle version
+  if (
+    appUpdateInfo.jsBundleVersion &&
+    appUpdateInfo.latestVersion &&
+    semver.eq(appUpdateInfo.latestVersion, APP_VERSION)
+  ) {
+    return (
+      appUpdateInfo.status !== EAppUpdateStatus.done &&
+      appUpdateInfo.jsBundleVersion === APP_BUNDLE_VERSION
+    );
+  }
+  return (
+    appUpdateInfo.status !== EAppUpdateStatus.done &&
+    appUpdateInfo.latestVersion &&
+    semver.eq(APP_VERSION, appUpdateInfo.latestVersion)
+  );
+};
