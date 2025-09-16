ed6c5447bdb96eb90563c057ebb778a70c697a55
huhuanming <huanming@onekey.so>
2025-09-16 10:36:20 +0800

    Update resoucePath.ts

--- apps/desktop/app/resoucePath.ts
+++ apps/desktop/app/resoucePath.ts
@@ -1,9 +1,33 @@
+import fs from 'fs';
 import * as path from 'path';
 
 import isDev from 'electron-is-dev';
 
-export const getResourcesPath = () =>
-  isDev ? path.join(__dirname, '../../public/static') : process.resourcesPath;
+const getJsBundleStaticPath = () => {
+  const indexHtmlPath =
+    globalThis.$desktopMainAppFunctions?.getBundleIndexHtmlPath?.();
+  if (indexHtmlPath) {
+    const dir = path.dirname(indexHtmlPath);
+    const staticPath = path.join(dir, 'static');
+    if (fs.existsSync(staticPath)) {
+      return staticPath;
+    }
+    fs.mkdirSync(staticPath, { recursive: true });
+  }
+  return undefined;
+};
+
+export const getResourcesPath = () => {
+  const staticPath = getJsBundleStaticPath();
+  if (staticPath) {
+    const dir = path.dirname(staticPath);
+    return dir;
+  }
+
+  return isDev
+    ? path.join(__dirname, '../../public/static')
+    : process.resourcesPath;
+};
 
 export const getStaticPath = () => {
   const resourcesPath = getResourcesPath();
