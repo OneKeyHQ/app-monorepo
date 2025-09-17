e3f9288b58a33b9393f0dfa57138f584a320f6db
huhuanming <huanming@onekey.so>
2025-09-17 23:44:30 +0800

    Update BundleUpdateModule.h

--- apps/mobile/ios/OneKeyWallet/BundleUpdateModule.h
+++ apps/mobile/ios/OneKeyWallet/BundleUpdateModule.h
@@ -11,9 +11,10 @@
 @interface BundleUpdateModule : RCTEventEmitter <RCTBridgeModule>
 
 - (BOOL)verifyBundleSHA256:(NSString *)bundlePath sha256:(NSString *)sha256;
-- (NSString *)calculateSHA256:(NSString *)filePath;
 - (NSString *)extractSHA256FromASCFile:(NSString *)ascPath;
 + (NSString *)downloadBundleDir;
 + (NSString *)bundleDir;
++ (BOOL)valiateAllFilesInDir:(NSString *)DirPath metadata:(NSDictionary *)metadata;
++ (NSString *)calculateSHA256:(NSString *)filePath;
 
 @end
