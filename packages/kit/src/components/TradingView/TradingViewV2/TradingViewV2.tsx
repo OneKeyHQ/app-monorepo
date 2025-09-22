e93937f1306646b1d2cae5867a75ec26a0d9dde2
limichange <limichange@hotmail.com>
2025-09-22 17:11:01 +0800

    refactor: update enabled property in TradingViewV2 component
    - Simplified the enabled property to directly reflect the isVisible state, enhancing clarity and removing unnecessary dependencies.

--- packages/kit/src/components/TradingView/TradingViewV2/TradingViewV2.tsx
+++ packages/kit/src/components/TradingView/TradingViewV2/TradingViewV2.tsx
@@ -83,7 +83,7 @@ export function TradingViewV2(props: ITradingViewV2Props & WebViewProps) {
     tokenAddress,
     networkId,
     webRef,
-    enabled: isVisible && isNative,
+    enabled: isVisible,
   });
 
   useAutoTokenDetailUpdate({
