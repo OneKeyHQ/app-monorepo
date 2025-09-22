7fb780053a139f035fc5074359a0af51525cfc18
huhuanming <huanming@onekey.so>
2025-09-22 23:00:14 +0800

    fix: fix passkey window

---
+++ apps/ext/src/closePasskeyWIndow.ts
@@ -0,0 +1,16 @@
+import { EPassKeyWindowFrom } from '@onekeyhq/shared/src/utils/extUtils';
+
+const params = new URLSearchParams(globalThis.location.href.split('?').pop());
+const from = params.get('from') as EPassKeyWindowFrom;
+
+export const closeWindow = () => {
+  console.log('closeWindow');
+  if (
+    from === EPassKeyWindowFrom.popup ||
+    from === EPassKeyWindowFrom.sidebar
+  ) {
+    setTimeout(() => {
+      window.close();
+    }, 50);
+  }
+};
