ef681fbe7145541a0c380fac7da63a4e491cb68d
huhuanming <huanming@onekey.so>
2025-10-04 03:02:48 +0800

    Update index.native.ts

--- packages/shared/src/modules3rdParty/auto-update/index.native.ts
+++ packages/shared/src/modules3rdParty/auto-update/index.native.ts
@@ -16,6 +16,7 @@ import type {
   IDownloadASC,
   IDownloadPackage,
   IInstallPackage,
+  IJSBundle,
   IManualInstallPackage,
   IUseDownloadProgress,
   IVerifyASC,
@@ -221,6 +222,8 @@ interface INativeBundleUpdateModule {
   verifyBundleASC: (params: any) => Promise<void>;
   downloadBundleASC: (params: any) => Promise<void>;
   installBundle: (params: any) => Promise<void>;
+  getFallbackUpdateBundleData: () => Promise<IJSBundle[]>;
+  setCurrentUpdateBundleData: (params: IJSBundle) => Promise<void>;
   clearBundle: () => Promise<void>;
   clearAllJSBundleData: () => Promise<void>;
   getWebEmbedPath: () => Promise<string>;
@@ -304,4 +307,11 @@ export const BundleUpdate: IBundleUpdate = {
     BundleUpdateModule && BundleUpdateModule.getWebEmbedPath
       ? BundleUpdateModule.getWebEmbedPath()
       : Promise.resolve(''),
+  getFallbackBundles: () => BundleUpdateModule.getFallbackUpdateBundleData(),
+  switchBundle: async (params) => {
+    await BundleUpdateModule.setCurrentUpdateBundleData(params);
+    setTimeout(() => {
+      RNRestart.restart();
+    }, 2500);
+  },
 };
