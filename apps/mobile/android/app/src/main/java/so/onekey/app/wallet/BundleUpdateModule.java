3aac26bdc76df3949eee75f482c95fca8df9f39e
huhuanming <huanming@onekey.so>
2025-10-04 02:07:15 +0800

    Update BundleUpdateModule.java

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/BundleUpdateModule.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/BundleUpdateModule.java
@@ -171,15 +171,15 @@ public class BundleUpdateModule extends ReactContextBaseJavaModule {
     }
 
     public static boolean validateMetadataFileSha256(Context context, String currentBundleVersion, String signature) throws IOException {
-        String metadataFilePath = getMetadataFilePath(context, currentBundleVersion);
-        if (metadataFilePath == null) {
-            staticLog(TAG, "metadataFilePath is null");
-            return false;
-        }
-        String extractedSha256 = readMetadataFileSha256(context, signature);
-        if (extractedSha256 == null || extractedSha256.isEmpty()) {
-            return false;
-        }
+        // String metadataFilePath = getMetadataFilePath(context, currentBundleVersion);
+        // if (metadataFilePath == null) {
+        //     staticLog(TAG, "metadataFilePath is null");
+        //     return false;
+        // }
+        // String extractedSha256 = readMetadataFileSha256(context, signature);
+        // if (extractedSha256 == null || extractedSha256.isEmpty()) {
+        //     return false;
+        // }
         // return calculateSHA256(metadataFilePath).equals(extractedSha256);
         return true;
     }
