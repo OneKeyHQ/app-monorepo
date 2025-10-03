87430aac19281215ab32b3aefe1e3bde3b10ccbe
huhuanming <huanming@onekey.so>
2025-10-03 21:10:23 +0800

    Update useJsBundle.native.ts

--- packages/shared/src/modules3rdParty/auto-update/useJsBundle.native.ts
+++ packages/shared/src/modules3rdParty/auto-update/useJsBundle.native.ts
@@ -3,9 +3,10 @@ import { NativeModules } from 'react-native';
 const BundleUpdateModule = NativeModules.BundleUpdateModule as {
   jsBundlePath: () => string;
 };
-const jsBundlePath = BundleUpdateModule
-  ? BundleUpdateModule.jsBundlePath()
-  : '';
+const jsBundlePath =
+  BundleUpdateModule && BundleUpdateModule.jsBundlePath
+    ? BundleUpdateModule.jsBundlePath()
+    : '';
 
 export const getJsBundlePath = () => {
   return jsBundlePath;
