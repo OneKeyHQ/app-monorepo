bbc0d91041c2cedf30c526bc17555f6fc58614a5
huhuanming <huanming@onekey.so>
2025-10-04 02:05:23 +0800

    Update BundleUpdateModule.m

--- apps/mobile/ios/OneKeyWallet/BundleUpdateModule.m
+++ apps/mobile/ios/OneKeyWallet/BundleUpdateModule.m
@@ -375,20 +375,20 @@ RCT_EXPORT_MODULE();
 
 
 + (BOOL)validateMetadataFileSha256:(NSString *)currentBundleVersion signature:(NSString *)signature {
-    NSString *metadataFilePath = [self getMetadataFilePath:currentBundleVersion];
-    if (!metadataFilePath) {
-        DDLogDebug(@"metadataFilePath is null");
-        return NO;
-    }
+    // NSString *metadataFilePath = [self getMetadataFilePath:currentBundleVersion];
+    // if (!metadataFilePath) {
+    //     DDLogDebug(@"metadataFilePath is null");
+    //     return NO;
+    // }
     
-    NSString *extractedSha256 = [self readMetadataFileSha256:signature];
-    if (!extractedSha256 || extractedSha256.length == 0) {
-        return NO;
-    }
+    // NSString *extractedSha256 = [self readMetadataFileSha256:signature];
+    // if (!extractedSha256 || extractedSha256.length == 0) {
+    //     return NO;
+    // }
     
-    NSString *calculatedSha256 = [self calculateSHA256:metadataFilePath];
-    return YES;
+    // NSString *calculatedSha256 = [self calculateSHA256:metadataFilePath];
     // return [calculatedSha256 isEqualToString:extractedSha256];
+    return YES;
 }
 
 - (dispatch_queue_t)methodQueue {
