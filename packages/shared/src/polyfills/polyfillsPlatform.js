6e95f556e6ac2249664a1ca4e5611ecae84769a1
huhuanming <huanming@onekey.so>
2025-10-04 15:17:55 +0800

    Update polyfillsPlatform.js

--- packages/shared/src/polyfills/polyfillsPlatform.js
+++ packages/shared/src/polyfills/polyfillsPlatform.js
@@ -114,7 +114,9 @@ if (platformEnv.isNative) {
           defaultLogger.app.error.log(`iOSAsset end`);
           if (useJsBundle) {
             defaultLogger.app.error.log(`useJsBundle start`, assetsPath);
-            iOSAsset.uri = iOSAsset.uri.replace(this.jsbundleUrl, assetsPath);
+            iOSAsset.uri = iOSAsset.uri
+              .replace(this.jsbundleUrl, assetsPath)
+              .replace('__packages', 'assets/packages');
             defaultLogger.app.error.log(`useJsBundle end`, iOSAsset.uri);
           }
           return iOSAsset;
