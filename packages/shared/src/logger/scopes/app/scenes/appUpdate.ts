b119469c9af517a81ece478058ba2b75a552ce51
huhuanming <huanming@onekey.so>
2025-10-04 12:21:50 +0800

    Update appUpdate.ts

--- packages/shared/src/logger/scopes/app/scenes/appUpdate.ts
+++ packages/shared/src/logger/scopes/app/scenes/appUpdate.ts
@@ -1,6 +1,7 @@
 import type {
   EUpdateFileType,
   EUpdateStrategy,
+  IResponseAppUpdateInfo,
 } from '@onekeyhq/shared/src/appUpdate';
 import type {
   IDownloadPackageParams,
@@ -20,6 +21,15 @@ export class AppUpdateScene extends BaseScene {
       fileType,
       updateStrategy,
     };
+  }
+
+  @LogToLocal({ level: 'info' })
+  public fetchConfig(updateInfo: IResponseAppUpdateInfo | undefined) {
+    return updateInfo;
+  }
+
+  isNeedSyncAppUpdateInfo(isNeedSync: boolean) {
+    return isNeedSync;
   }
 
   @LogToLocal({ level: 'info' })
