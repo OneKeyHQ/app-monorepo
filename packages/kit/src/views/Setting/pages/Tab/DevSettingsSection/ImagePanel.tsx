a8a0c392f5a0d8cdea6195a30dd972523860321d
huhuanming <huanming@onekey.so>
2025-10-04 23:29:59 +0800

    Revert "Update ImagePanel.tsx"
    This reverts commit a5f760a572780e60fbc54cbd641635f93730ae3a.

--- packages/kit/src/views/Setting/pages/Tab/DevSettingsSection/ImagePanel.tsx
+++ packages/kit/src/views/Setting/pages/Tab/DevSettingsSection/ImagePanel.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-import { Image as NativeImage } from 'react-native';
-
 import { Image, Input, YStack } from '@onekeyhq/components';
 
 export function ImagePanel() {
@@ -10,10 +8,6 @@ export function ImagePanel() {
     <YStack gap="$4">
       <Input value={imageUrl} onChangeText={setImage} placeholder="Image URL" />
       <Image source={{ uri: imageUrl }} size="$10" />
-      <NativeImage
-        source={{ uri: imageUrl }}
-        style={{ width: 100, height: 100 }}
-      />
     </YStack>
   );
 }
