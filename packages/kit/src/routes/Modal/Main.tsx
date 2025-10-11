b874f3b2d70b777115bfea67d19261571aeaf39a
huhuanming <huanming@onekey.so>
2025-10-11 19:17:06 +0800

    Update Main.tsx

--- packages/kit/src/routes/Modal/Main.tsx
+++ packages/kit/src/routes/Modal/Main.tsx
@@ -1,11 +1,11 @@
-import { ModalAssetDetailsStack } from '../../views/AssetDetails/router';
-import { ModalAssetListStack } from '../../views/AssetList/router';
-import { urlAccountRoutes } from '../../views/Home/router';
-import { RewardCenterStack } from '../../views/RewardCenter/router';
+// import { ModalAssetDetailsStack } from '../../views/AssetDetails/router';
+// import { ModalAssetListStack } from '../../views/AssetList/router';
+// import { urlAccountRoutes } from '../../views/Home/router';
+// import { RewardCenterStack } from '../../views/RewardCenter/router';
 
 export const ModalMainStack = [
-  ...ModalAssetListStack,
-  ...ModalAssetDetailsStack,
-  ...urlAccountRoutes,
-  ...RewardCenterStack,
+  // ...ModalAssetListStack,
+  // ...ModalAssetDetailsStack,
+  // ...urlAccountRoutes,
+  // ...RewardCenterStack,
 ];
