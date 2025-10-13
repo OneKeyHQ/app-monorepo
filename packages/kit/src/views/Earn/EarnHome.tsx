93f2942919235358385c3481eeaf5a2d7cb71513
Charon-Fan <charon.dian@gmail.com>
2025-10-13 16:31:04 +0800

    feat: update EarnBlockedOverview component layout with padding and margin adjustments

--- packages/kit/src/views/Earn/EarnHome.tsx
+++ packages/kit/src/views/Earn/EarnHome.tsx
@@ -664,7 +664,7 @@ function EarnBlockedOverview(props: {
         sceneName={EAccountSelectorSceneName.home}
         tabRoute={ETabRoutes.Earn}
       />
-      <Page.Body>
+      <Page.Body px="$16" mt="$10">
         <Empty
           icon={icon}
           title={title}
