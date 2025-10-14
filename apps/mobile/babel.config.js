44d5bd9b89913159a2aa785cedba28f177b62f83
huhuanming <huanming@onekey.so>
2025-10-14 20:02:29 +0800

    Update babel.config.js

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -4,7 +4,10 @@ const babelTools = require('../../development/babelTools');
 console.log('process.env.TAMAGUI_TARGET: ', process.env.TAMAGUI_TARGET);
 if (process.env.TAMAGUI_TARGET !== 'native') {
   process.env.TAMAGUI_TARGET = 'native';
-  console.log('process.env.TAMAGUI_TARGET: ', process.env.TAMAGUI_TARGET);
+  console.log(
+    'fixed: process.env.TAMAGUI_TARGET: ',
+    process.env.TAMAGUI_TARGET,
+  );
 }
 
 module.exports = function (api) {
