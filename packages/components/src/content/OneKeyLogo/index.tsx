01261a591f502d657a7e85dd608045c3d4fc3ba5
limichange <limichange@hotmail.com>
2025-10-18 17:31:22 +0800

    refactor: simplify OneKeyLogo component by removing unnecessary padding
    - Removed the horizontal padding from the XStack in the OneKeyLogo component to streamline the layout.
    - This change enhances the component's appearance and maintains a cleaner design.

--- packages/components/src/content/OneKeyLogo/index.tsx
+++ packages/components/src/content/OneKeyLogo/index.tsx
@@ -2,7 +2,7 @@ import { Icon, XStack } from '../../primitives';
 
 export function OneKeyLogo() {
   return (
-    <XStack px="$4" py="$3">
+    <XStack py="$3">
       <Icon name="OnekeyTextIllus" width={101} height={28} color="$text" />
     </XStack>
   );
