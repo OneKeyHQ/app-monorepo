af8a51128cc06c61ba9d802f43ea5bbb7784225b
Leon <lixiao.dev@gmail.com>
2025-10-20 16:39:48 +0800

    feat: implement migration for BTCFreshAddress setting

--- packages/kit-bg/src/dbs/simple/entity/SimpleDbEntityAppStatus.ts
+++ packages/kit-bg/src/dbs/simple/entity/SimpleDbEntityAppStatus.ts
@@ -17,6 +17,7 @@ export interface ISimpleDBAppStatus {
   lastDBBackupTime?: number;
   filterScamHistorySettingMigrated?: boolean;
   fixHardwareLtcXPubMigrated?: boolean;
+  btcFreshAddressSettingMigrated?: boolean;
 }
 
 export class SimpleDbEntityAppStatus extends SimpleDbEntityBase<ISimpleDBAppStatus> {
