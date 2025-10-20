e9ebca5d781d7b2283db0c4800f29c969f3881ff
huhuanming <huanming@onekey.so>
2025-10-21 00:16:59 +0800

    Update Navigator.tsx

--- packages/kit/src/routes/Tab/Navigator.tsx
+++ packages/kit/src/routes/Tab/Navigator.tsx
@@ -91,20 +91,21 @@ const preloadTabs = (navigation: NavigationProp<any>) => {
   );
 };
 
-const usePreloadTabs = platformEnv.isNative
-  ? () => {}
-  : () => {
-      const navigation = useNavigation();
-      useEffect(() => {
-        setTimeout(async () => {
-          await Promise.race([
-            new Promise<void>((resolve) => setTimeout(resolve, 1200)),
-            whenAppUnlocked(),
-          ]);
-          preloadTabs(navigation as NavigationProp<any>);
-        });
-      }, [navigation]);
-    };
+const usePreloadTabs =
+  platformEnv.isDev || platformEnv.isNative
+    ? () => {}
+    : () => {
+        const navigation = useNavigation();
+        useEffect(() => {
+          setTimeout(async () => {
+            await Promise.race([
+              new Promise<void>((resolve) => setTimeout(resolve, 1200)),
+              whenAppUnlocked(),
+            ]);
+            preloadTabs(navigation as NavigationProp<any>);
+          });
+        }, [navigation]);
+      };
 
 // When using navigation.preload, the web layer will re-render the interface with sidebar,
 // which may cause duplicate Portal rendering. Use isRendered to prevent duplicate Portal rendering.
