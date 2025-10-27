acd6fcd60b5758fc87c1148608db177f85fdc68a
huhuanming <huanming@onekey.so>
2025-10-27 22:04:09 +0800

    Update bundle.ts

--- apps/desktop/app/bundle.ts
+++ apps/desktop/app/bundle.ts
@@ -286,6 +286,7 @@ export const checkFileHash = ({
   // Handle Windows path separators
   if (isWin) {
     key = key
+      .replace(/\\/g, '/')
       .replace(bundleDirPath, '')
       .replace(driveLetter, '')
       .replace('C:/', '')
