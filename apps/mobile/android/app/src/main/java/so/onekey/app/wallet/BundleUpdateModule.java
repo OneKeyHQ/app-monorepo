1acd323d78ead5bb068e0ff25fc69db85ffe6eb9
huhuanming <huanming@onekey.so>
2025-10-29 02:32:00 +0800

    Update BundleUpdateModule.java

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/BundleUpdateModule.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/BundleUpdateModule.java
@@ -145,7 +145,7 @@ public class BundleUpdateModule extends ReactContextBaseJavaModule {
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         String version = getCurrentBundleVersion(context);
         if (version != null) {
-            prefs.edit().remove(version).remove(CURRENT_BUNDLE_VERSION_KEY).apply();
+            prefs.edit().clear().apply();
         }
     }
 
