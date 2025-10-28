008c83479f76fed402ba155c188aa94737def84e
limichange <limichange@hotmail.com>
2025-10-28 22:32:11 +0800

    refactor: rename hidden property to hiddenIcon in tab navigation components
    - Updated DesktopLeftSideBar and MobileBottomTabBar to use hiddenIcon instead of hidden for better clarity.
    - Adjusted router configuration to reflect the property change for consistency.

--- packages/kit/src/routes/Tab/router.ts
+++ packages/kit/src/routes/Tab/router.ts
@@ -170,7 +170,7 @@ export const useTabRouterConfig = (params?: IGetTabRouterParams) => {
           exact: true,
           children: homeRouters,
           trackId: 'global-wallet',
-          hidden: isWebDappMode,
+          hiddenIcon: isWebDappMode,
         },
         shouldShowMarketTab
           ? {
