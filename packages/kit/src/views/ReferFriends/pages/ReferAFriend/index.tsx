178f8ca6e83eaf9d5914c511f3be20f3f350fa82
limichange <limichange@hotmail.com>
2025-11-01 12:25:20 +0800

    feat: update refer friends router

--- packages/kit/src/views/ReferFriends/pages/ReferAFriend/index.tsx
+++ packages/kit/src/views/ReferFriends/pages/ReferAFriend/index.tsx
@@ -13,6 +13,8 @@ import {
   YStack,
 } from '@onekeyhq/components';
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+import { AccountSelectorProviderMirror } from '@onekeyhq/kit/src/components/AccountSelector';
+import { TabPageHeader } from '@onekeyhq/kit/src/components/TabPageHeader';
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { useReferFriends } from '@onekeyhq/kit/src/hooks/useReferFriends';
 import { useThemeVariant } from '@onekeyhq/kit/src/hooks/useThemeVariant';
@@ -22,13 +24,11 @@ import { ETranslations } from '@onekeyhq/shared/src/locale';
 import { defaultLogger } from '@onekeyhq/shared/src/logger/logger';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import type { IInvitePostConfig } from '@onekeyhq/shared/src/referralCode/type';
-import {
-  EModalReferFriendsRoutes,
-  EModalRoutes,
-} from '@onekeyhq/shared/src/routes';
+import { ETabRoutes } from '@onekeyhq/shared/src/routes';
 import { ESpotlightTour } from '@onekeyhq/shared/src/spotlight';
 import { openUrlExternal } from '@onekeyhq/shared/src/utils/openUrlUtils';
 import uriUtils from '@onekeyhq/shared/src/utils/uriUtils';
+import { EAccountSelectorSceneName } from '@onekeyhq/shared/types';
 
 enum EPhaseState {
   next = 'next',
@@ -281,7 +281,7 @@ function ReferAFriendPage({ postConfig }: { postConfig: IInvitePostConfig }) {
   );
 }
 
-export default function ReferAFriend() {
+function ReferAFriendPageWrapper() {
   const intl = useIntl();
   const navigation = useAppNavigation();
   const [postConfig, setPostConfig] = useState<IInvitePostConfig | undefined>(
@@ -298,10 +298,7 @@ export default function ReferAFriend() {
       });
     void backgroundApiProxy.servicePrime.isLoggedIn().then((isLogin) => {
       if (isLogin) {
-        navigation.pop();
-        navigation.pushModal(EModalRoutes.ReferFriendsModal, {
-          screen: EModalReferFriendsRoutes.InviteReward,
-        });
+        navigation.push('TabInviteReward');
         return;
       }
 
@@ -327,14 +324,27 @@ export default function ReferAFriend() {
   }, [navigation]);
   return (
     <Page scrollEnabled>
-      <Page.Header
-        title={intl.formatMessage({
-          id: ETranslations.id_refer_a_friend,
-        })}
+      <TabPageHeader
+        sceneName={EAccountSelectorSceneName.home}
+        tabRoute={ETabRoutes.ReferFriends}
       />
       <Page.Body>
         {postConfig ? <ReferAFriendPage postConfig={postConfig} /> : null}
       </Page.Body>
     </Page>
+  );
+}
+
+export default function ReferAFriend() {
+  return (
+    <AccountSelectorProviderMirror
+      config={{
+        sceneName: EAccountSelectorSceneName.home,
+        sceneUrl: '',
+      }}
+      enabledNum={[0]}
+    >
+      <ReferAFriendPageWrapper />
+    </AccountSelectorProviderMirror>
   );
 }
