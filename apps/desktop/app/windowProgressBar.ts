e3b40bb69f09beda7b5b10cc39a60487db1a897c
huhuanming <huanming@onekey.so>
2025-11-05 03:33:19 +0800

    window ProgressBar

---
+++ apps/desktop/app/windowProgressBar.ts
@@ -0,0 +1,29 @@
+import type { BrowserWindow } from 'electron';
+
+const checkWindowProgressBar = (window: BrowserWindow | undefined) => {
+  if (!window || window.setProgressBar === undefined) {
+    return false;
+  }
+  return true;
+};
+
+export const updateWindowProgressBar = (
+  window: BrowserWindow | undefined,
+  progress: number,
+) => {
+  if (!checkWindowProgressBar(window)) {
+    return;
+  }
+  if (progress > 0 && progress < 1) {
+    window?.setProgressBar(progress);
+  } else {
+    window?.setProgressBar(-1);
+  }
+};
+
+export const clearWindowProgressBar = (window: BrowserWindow | undefined) => {
+  if (!checkWindowProgressBar(window)) {
+    return;
+  }
+  window?.setProgressBar(-1);
+};
