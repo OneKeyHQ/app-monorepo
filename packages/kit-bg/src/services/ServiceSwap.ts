4f2f25794120e1f76bd68b564cb5f562e22002d8
ezailWang <jelly@onekey.so>
2025-11-06 11:58:21 +0800

    fix: coding
    Signed-off-by: ezailWang <jelly@onekey.so>

--- packages/kit-bg/src/services/ServiceSwap.ts
+++ packages/kit-bg/src/services/ServiceSwap.ts
@@ -13,6 +13,7 @@ import {
   toastIfError,
 } from '@onekeyhq/shared/src/background/backgroundDecorators';
 import { getNetworkIdsMap } from '@onekeyhq/shared/src/config/networkIds';
+import { PERPS_NETWORK_ID } from '@onekeyhq/shared/src/consts/perp';
 import {
   EAppEventBusNames,
   appEventBus,
@@ -33,6 +34,7 @@ import { equalsIgnoreCase } from '@onekeyhq/shared/src/utils/stringUtils';
 import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
 import { equalTokenNoCaseSensitive } from '@onekeyhq/shared/src/utils/tokenUtils';
 import { EServiceEndpointEnum } from '@onekeyhq/shared/types/endpoint';
+import { HYPERLIQUID_DEPOSIT_ADDRESS } from '@onekeyhq/shared/types/hyperliquid/perp.constants';
 import type { ESigningScheme } from '@onekeyhq/shared/types/message';
 import type {
   ISwapProviderManager,
@@ -87,7 +89,10 @@ import {
   ESwapTxHistoryStatus,
 } from '@onekeyhq/shared/types/swap/types';
 
-import { inAppNotificationAtom } from '../states/jotai/atoms';
+import {
+  inAppNotificationAtom,
+  perpsDepositOrderAtom,
+} from '../states/jotai/atoms';
 import { vaultFactory } from '../vaults/factory';
 
 import ServiceBase from './ServiceBase';
@@ -124,6 +129,8 @@ export default class ServiceSwap extends ServiceBase {
   private perpDepositOrderFetchLoopInterval: ReturnType<
     typeof setTimeout
   > | null = null;
+
+  private perpDepositOrderFetchLoopIntervalTimeout = 2000;
 
   private historyCurrentStateIntervalIds: string[] = [];
 
@@ -2366,18 +2373,102 @@ export default class ServiceSwap extends ServiceBase {
     toPerpDepositTokenAddress?: string;
     receivingAddress: string;
   }) {
-    // todo
-    return '';
+    try {
+      const client = await this.getClient(EServiceEndpointEnum.Swap);
+
+      const { data } = await client.post<
+        IFetchResponse<IFetchSwapTxHistoryStatusResponse>
+      >('/swap/v1/perp-deposit-order-status', params);
+      if (data?.data) {
+        const perpDepositOrder = await perpsDepositOrderAtom.get();
+        const findTxidOrder = perpDepositOrder.find(
+          (item) => item.fromTxId === params.txId,
+        );
+        if (findTxidOrder) {
+          const filteredPerpDepositOrder = perpDepositOrder.filter(
+            (item) => item.fromTxId !== params.txId,
+          );
+          if (data?.data.state === ESwapTxHistoryStatus.SUCCESS) {
+            findTxidOrder.status = ESwapTxHistoryStatus.SUCCESS;
+            if (!params.isArbUSDCToken) {
+              findTxidOrder.toTxId = data?.data.swapOrderHash?.toTxHash;
+            }
+            void this.backgroundApi.serviceApp.showToast({
+              method: 'success',
+              title: appLocale.intl.formatMessage({
+                id: ETranslations.perp_toast_deposit_success_msg,
+              }),
+            });
+            await perpsDepositOrderAtom.set([
+              ...filteredPerpDepositOrder,
+              findTxidOrder,
+            ]);
+          } else if (
+            data?.data.state === ESwapTxHistoryStatus.FAILED ||
+            data?.data.state === ESwapTxHistoryStatus.CANCELED ||
+            data?.data.state === ESwapTxHistoryStatus.CANCELING ||
+            data?.data.state === ESwapTxHistoryStatus.PARTIALLY_FILLED
+          ) {
+            findTxidOrder.status = ESwapTxHistoryStatus.FAILED;
+            void this.backgroundApi.serviceApp.showToast({
+              method: 'error',
+              title: appLocale.intl.formatMessage({
+                id: ETranslations.perp_toast_deposit_success_msg,
+              }),
+            });
+            await perpsDepositOrderAtom.set([
+              ...filteredPerpDepositOrder,
+              findTxidOrder,
+            ]);
+          }
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   @backgroundMethod()
   async perpDepositOrderFetchLoop(params: {
     accountId?: string | null;
-    indexedAccoundId?: string | null;
+    indexedAccountId?: string | null;
   }) {
     if (this.perpDepositOrderFetchLoopInterval) {
       clearTimeout(this.perpDepositOrderFetchLoopInterval);
       this.perpDepositOrderFetchLoopInterval = null;
+    }
+    const { accountId, indexedAccountId } = params;
+    const perpDepositOrder = await perpsDepositOrderAtom.get();
+    const filteredPerpDepositOrder = perpDepositOrder.filter((item) => {
+      return (
+        ((!item.accountId && !accountId) || item.accountId === accountId) &&
+        ((!item.indexedAccountId && !indexedAccountId) ||
+          item.indexedAccountId === indexedAccountId) &&
+        item.status === ESwapTxHistoryStatus.PENDING
+      );
+    });
+    if (filteredPerpDepositOrder.length > 0) {
+      const receivingAddressInfo =
+        await this.backgroundApi.serviceAccount.getNetworkAccount({
+          accountId: indexedAccountId ? undefined : accountId ?? '',
+          indexedAccountId: indexedAccountId ?? '',
+          networkId: PERPS_NETWORK_ID,
+          deriveType: 'default',
+        });
+      await Promise.all(
+        filteredPerpDepositOrder.map((item) => {
+          return this.fetchPerpDepositOrderStatus({
+            networkId: item.token.networkId,
+            txId: item.fromTxId,
+            isArbUSDCToken: item.isArbUSDCOrder,
+            toPerpDepositTokenAddress: HYPERLIQUID_DEPOSIT_ADDRESS,
+            receivingAddress: receivingAddressInfo.addressDetail.address,
+          });
+        }),
+      );
+      this.perpDepositOrderFetchLoopInterval = setTimeout(() => {
+        void this.perpDepositOrderFetchLoop(params);
+      }, this.perpDepositOrderFetchLoopIntervalTimeout);
     }
   }
 }
