d66f43642fc7db4b2aab5c40bb0651972f260340
huhuanming <huanming@onekey.so>
2025-11-07 22:05:58 +0800

    Update VerifyRecoveryPhrase.tsx

--- packages/kit/src/views/Onboardingv2/pages/VerifyRecoveryPhrase.tsx
+++ packages/kit/src/views/Onboardingv2/pages/VerifyRecoveryPhrase.tsx
@@ -1,69 +1,157 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+
+import { useRoute } from '@react-navigation/core';
+import bip39Wordlists from 'bip39/src/wordlists/english.json';
+import { useIntl } from 'react-intl';
 
 import {
   Button,
   Page,
   SizableText,
+  Toast,
   XStack,
   YStack,
 } from '@onekeyhq/components';
+import {
+  ensureSensitiveTextEncoded,
+  generateMnemonic,
+} from '@onekeyhq/core/src/secret';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
+import type {
+  EOnboardingPagesV2,
+  IOnboardingParamListV2,
+} from '@onekeyhq/shared/src/routes/onboardingv2';
 
+import backgroundApiProxy from '../../../background/instance/backgroundApiProxy';
+import useAppNavigation from '../../../hooks/useAppNavigation';
+import { usePromiseResult } from '../../../hooks/usePromiseResult';
 import { OnboardingLayout } from '../components/OnboardingLayout';
 
+import type { RouteProp } from '@react-navigation/core';
+
 export default function VerifyRecoveryPhrase() {
-  // Track selected word index for each question (0-2 for 3 options)
+  const intl = useIntl();
+  const navigation = useAppNavigation();
+  const route =
+    useRoute<
+      RouteProp<IOnboardingParamListV2, EOnboardingPagesV2.VerifyRecoveryPhrase>
+    >();
+
+  const { result: mnemonic = '' } = usePromiseResult(async () => {
+    console.log('RecoveryPhrase generateMnemonic');
+    const routeMnemonic = route.params?.mnemonic;
+    if (routeMnemonic) {
+      ensureSensitiveTextEncoded(routeMnemonic);
+      return backgroundApiProxy.servicePassword.decodeSensitiveText({
+        encodedText: routeMnemonic,
+      });
+    }
+    return generateMnemonic();
+  }, [route.params.mnemonic]);
+  const recoveryPhrase = useMemo(
+    () => mnemonic.split(' ').filter(Boolean),
+    [mnemonic],
+  );
+
+  const answerIndices = useMemo(() => {
+    if (recoveryPhrase.length === 0) return [];
+
+    const shuffledIndices = Array.from(
+      { length: recoveryPhrase.length },
+      (_, i) => i,
+    )
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 3);
+
+    return shuffledIndices;
+  }, [recoveryPhrase]);
   const [selectedWords, setSelectedWords] = useState<{
-    [key: number]: number | null;
+    [key: number]: string | null;
   }>({
     0: null,
     1: null,
     2: null,
   });
 
-  const handleWordSelect = (questionIndex: number, wordIndex: number) => {
-    setSelectedWords((prev) => ({
-      ...prev,
-      [questionIndex]: wordIndex,
-    }));
-  };
+  const shuffleWords = useMemo(() => {
+    const answerWords = answerIndices.map((index) => recoveryPhrase[index]);
+    const confuseWords: string[] = [];
+    const getRandomWord = (): string => {
+      const randomIndex = Math.floor(Math.random() * bip39Wordlists.length);
+      const word = bip39Wordlists[randomIndex];
 
-  const questions = [
-    {
-      position: 9,
-      options: ['word1', 'word2', 'word3'],
+      if (answerWords.includes(word) || confuseWords.includes(word)) {
+        return getRandomWord();
+      }
+
+      return word;
+    };
+
+    for (let i = 0; i < 6; i += 1) {
+      confuseWords.push(getRandomWord());
+    }
+    return answerWords.map((word, index) => {
+      return {
+        words: [word, ...confuseWords.slice(index * 2, index * 2 + 2)].sort(
+          () => Math.random() - 0.5,
+        ),
+        index: answerIndices[index],
+      };
+    });
+  }, [answerIndices, recoveryPhrase]);
+
+  const handleWordSelect = useCallback(
+    (questionIndex: number, word: string) => {
+      setSelectedWords((prev) => ({
+        ...prev,
+        [questionIndex]: word,
+      }));
+
+      setTimeout(() => {
+        const verifyResult = answerIndices.every(
+          (recoveryPhraseIndex, index) => {
+            if (selectedWords[index] === null) {
+              return false;
+            }
+            return selectedWords[index] === recoveryPhrase[recoveryPhraseIndex];
+          },
+        );
+
+        if (verifyResult) {
+          Toast.success({
+            title: intl.formatMessage({
+              id: ETranslations.backup_recovery_phrase_backed_up,
+            }),
+          });
+          navigation.popStack();
+        }
+      });
     },
-    {
-      position: 12,
-      options: ['word4', 'word5', 'word6'],
-    },
-    {
-      position: 3,
-      options: ['word7', 'word8', 'word9'],
-    },
-  ];
+    [answerIndices, intl, navigation, recoveryPhrase, selectedWords],
+  );
 
   return (
     <Page>
       <OnboardingLayout>
         <OnboardingLayout.Header title="Verify your recovery phrase" />
         <OnboardingLayout.Body>
-          {questions.map((question, questionIndex) => (
+          {shuffleWords.map((question, questionIndex) => (
             <YStack key={questionIndex} gap="$2">
               <SizableText size="$bodyMd">
-                Word #{question.position}
+                Word #{question.index + 1}
               </SizableText>
               <XStack gap="$2">
-                {question.options.map((word, wordIndex) => (
+                {question.words.map((word, wordIndex) => (
                   <Button
                     key={wordIndex}
                     size="large"
                     flex={1}
                     variant={
-                      selectedWords[questionIndex] === wordIndex
+                      selectedWords[questionIndex] === word
                         ? 'primary'
                         : 'secondary'
                     }
-                    onPress={() => handleWordSelect(questionIndex, wordIndex)}
+                    onPress={() => handleWordSelect(questionIndex, word)}
                   >
                     {word}
                   </Button>
