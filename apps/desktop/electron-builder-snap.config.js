87d3b5e312263658c24348415cdf5ccf50aa93c2
wabicai <kaisheng.cai@onekey.so>
2025-11-20 00:29:08 +1300

    Revert "fix: Add raw-usb plugs in linux snap for web-usb connect (#8887)"
    This reverts commit e3646d4c28117c8bfcc6235f233d575e4fa0c5ba.

--- apps/desktop/electron-builder-snap.config.js
+++ apps/desktop/electron-builder-snap.config.js
@@ -17,8 +17,4 @@ module.exports = {
     'category': 'Utility',
     'target': ['snap'],
   },
-  // Refer：https://canonical-snap.readthedocs-hosted.com/reference/development/interfaces/raw-usb-interface/
-  'snap': {
-    'plugs': ['raw-usb'],
-  },
 };
