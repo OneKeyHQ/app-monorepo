4eab99b98ec59fe44e566d9c7597baa2677282b9
limichange <limichange@hotmail.com>
2025-11-20 10:27:44 +0800

    refactor: streamline navigation logic in useNavigateToRewardHistory hook
    - Removed platform environment checks for responsive navigation, simplifying the navigation logic to exclusively use modal navigation for accessing the Reward Distribution History.
    - Enhanced clarity and maintainability of the navigation callback function.

--- packages/kit/src/views/ReferFriends/pages/RewardDistributionHistory/hooks/useNavigateToRewardHistory.ts
+++ packages/kit/src/views/ReferFriends/pages/RewardDistributionHistory/hooks/useNavigateToRewardHistory.ts
@@ -1,25 +1,18 @@
 import { useCallback } from 'react';
 
 import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
-import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import {
   EModalReferFriendsRoutes,
   EModalRoutes,
-  ETabReferFriendsRoutes,
 } from '@onekeyhq/shared/src/routes';
 
 export function useNavigateToRewardHistory() {
   const navigation = useAppNavigation();
 
   return useCallback(() => {
-    if (platformEnv.isNative) {
-      // Native or medium+ screens: use Modal navigation
-      navigation.pushModal(EModalRoutes.ReferFriendsModal, {
-        screen: EModalReferFriendsRoutes.RewardDistributionHistory,
-      });
-    } else {
-      // Small screens: use Tab navigation
-      navigation.push(ETabReferFriendsRoutes.TabRewardDistributionHistory);
-    }
+    // Native or medium+ screens: use Modal navigation
+    navigation.pushModal(EModalRoutes.ReferFriendsModal, {
+      screen: EModalReferFriendsRoutes.RewardDistributionHistory,
+    });
   }, [navigation]);
 }
