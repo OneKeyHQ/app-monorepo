1a68f6d740c02a8cf66ed5baa0fb6d9953d11072
huhuanming <huanming@onekey.so>
2025-11-22 01:31:40 +0800

    fix: fix render version

--- packages/shared/src/request/axiosInterceptor.ts
+++ packages/shared/src/request/axiosInterceptor.ts
@@ -1,233 +1,233 @@
-/* eslint-disable @typescript-eslint/no-restricted-imports */
-/* eslint-disable @typescript-eslint/unbound-method */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+// /* eslint-disable @typescript-eslint/no-restricted-imports */
+// /* eslint-disable @typescript-eslint/unbound-method */
+// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import axios, { AxiosError } from 'axios';
-import { debounce, forEach } from 'lodash';
+// import axios, { AxiosError } from 'axios';
+// import { debounce, forEach } from 'lodash';
 
-import { OneKeyError, OneKeyServerApiError } from '@onekeyhq/shared/src/errors';
-import {
-  EAppEventBusNames,
-  appEventBus,
-} from '@onekeyhq/shared/src/eventBus/appEventBus';
-import type { IOneKeyAPIBaseResponse } from '@onekeyhq/shared/types/request';
+// import { OneKeyError, OneKeyServerApiError } from '@onekeyhq/shared/src/errors';
+// import {
+//   EAppEventBusNames,
+//   appEventBus,
+// } from '@onekeyhq/shared/src/eventBus/appEventBus';
+// import type { IOneKeyAPIBaseResponse } from '@onekeyhq/shared/types/request';
 
-import { EOneKeyErrorClassNames } from '../errors/types/errorTypes';
-import { ETranslations } from '../locale';
-import { appLocale } from '../locale/appLocale';
-import { defaultLogger } from '../logger/logger';
-import { isEnableLogNetwork } from '../logger/scopes/app/scenes/network';
-import platformEnv from '../platformEnv';
-import systemTimeUtils from '../utils/systemTimeUtils';
+// import { EOneKeyErrorClassNames } from '../errors/types/errorTypes';
+// import { ETranslations } from '../locale';
+// import { appLocale } from '../locale/appLocale';
+// import { defaultLogger } from '../logger/logger';
+// import { isEnableLogNetwork } from '../logger/scopes/app/scenes/network';
+// import platformEnv from '../platformEnv';
+// import systemTimeUtils from '../utils/systemTimeUtils';
 
-import {
-  HEADER_REQUEST_ID_KEY,
-  checkRequestIsOneKeyDomain,
-  getRequestHeaders,
-} from './Interceptor';
-import { REQUEST_TIMEOUT } from './requestConst';
+// import {
+//   HEADER_REQUEST_ID_KEY,
+//   checkRequestIsOneKeyDomain,
+//   getRequestHeaders,
+// } from './Interceptor';
+// import { REQUEST_TIMEOUT } from './requestConst';
 
-import type { IAxiosResponse } from '../appApiClient/appApiClient';
-import type { AxiosInstance, AxiosRequestConfig } from 'axios';
+// import type { IAxiosResponse } from '../appApiClient/appApiClient';
+// import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-const refreshNetInfo = debounce(() => {
-  appEventBus.emit(EAppEventBusNames.RefreshNetInfo, undefined);
-}, 2500);
+// const refreshNetInfo = debounce(() => {
+//   appEventBus.emit(EAppEventBusNames.RefreshNetInfo, undefined);
+// }, 2500);
 
-axios.interceptors.request.use(async (config) => {
-  if (config.timeout === undefined) {
-    config.timeout = 30_000;
-  }
-  try {
-    const isOneKeyDomain = await checkRequestIsOneKeyDomain({ config });
+// axios.interceptors.request.use(async (config) => {
+//   if (config.timeout === undefined) {
+//     config.timeout = 30_000;
+//   }
+//   try {
+//     const isOneKeyDomain = await checkRequestIsOneKeyDomain({ config });
 
-    if (!isOneKeyDomain) {
-      if (isEnableLogNetwork(config.url)) {
-        defaultLogger.app.network.start('axios', config.method, config.url);
-      }
-      return config;
-    }
-  } catch (e) {
-    return config;
-  }
+//     if (!isOneKeyDomain) {
+//       if (isEnableLogNetwork(config.url)) {
+//         defaultLogger.app.network.start('axios', config.method, config.url);
+//       }
+//       return config;
+//     }
+//   } catch (e) {
+//     return config;
+//   }
 
-  const headers = await getRequestHeaders();
-  forEach(headers, (val, key) => {
-    config.headers[key] = val;
-  });
+//   const headers = await getRequestHeaders();
+//   forEach(headers, (val, key) => {
+//     config.headers[key] = val;
+//   });
 
-  if (isEnableLogNetwork(config.url)) {
-    defaultLogger.app.network.start(
-      'axios',
-      config.method,
-      config.url,
-      headers[HEADER_REQUEST_ID_KEY],
-    );
-  }
-  return config;
-});
+//   if (isEnableLogNetwork(config.url)) {
+//     defaultLogger.app.network.start(
+//       'axios',
+//       config.method,
+//       config.url,
+//       headers[HEADER_REQUEST_ID_KEY],
+//     );
+//   }
+//   return config;
+// });
 
-axios.interceptors.response.use(
-  async (response) => {
-    const { config } = response;
-    const url =
-      response?.request?.responseURL || config?.baseURL || config?.url || '';
-    void systemTimeUtils.handleServerResponseDate({
-      source: 'axios',
-      headerDate: response?.headers?.date || '',
-      url,
-    });
+// axios.interceptors.response.use(
+//   async (response) => {
+//     const { config } = response;
+//     const url =
+//       response?.request?.responseURL || config?.baseURL || config?.url || '';
+//     void systemTimeUtils.handleServerResponseDate({
+//       source: 'axios',
+//       headerDate: response?.headers?.date || '',
+//       url,
+//     });
 
-    try {
-      const isOneKeyDomain = await checkRequestIsOneKeyDomain({ config });
-      if (!isOneKeyDomain) {
-        if (isEnableLogNetwork(config.url)) {
-          defaultLogger.app.network.end({
-            requestType: 'axios',
-            method: config.method as string,
-            path: config.url as string,
-            statusCode: response.status,
-            requestId: config.headers[HEADER_REQUEST_ID_KEY],
-            responseCode: response.data.code,
-          });
-        }
-        return response;
-      }
-    } catch (e) {
-      return response;
-    }
+//     try {
+//       const isOneKeyDomain = await checkRequestIsOneKeyDomain({ config });
+//       if (!isOneKeyDomain) {
+//         if (isEnableLogNetwork(config.url)) {
+//           defaultLogger.app.network.end({
+//             requestType: 'axios',
+//             method: config.method as string,
+//             path: config.url as string,
+//             statusCode: response.status,
+//             requestId: config.headers[HEADER_REQUEST_ID_KEY],
+//             responseCode: response.data.code,
+//           });
+//         }
+//         return response;
+//       }
+//     } catch (e) {
+//       return response;
+//     }
 
-    const data = response.data as IOneKeyAPIBaseResponse;
+//     const data = response.data as IOneKeyAPIBaseResponse;
 
-    if ((config as any).autoHandleError !== false && data.code !== 0) {
-      const requestIdKey = HEADER_REQUEST_ID_KEY;
-      if (platformEnv.isDev) {
-        console.error(requestIdKey, config.headers[requestIdKey]);
-      }
+//     if ((config as any).autoHandleError !== false && data.code !== 0) {
+//       const requestIdKey = HEADER_REQUEST_ID_KEY;
+//       if (platformEnv.isDev) {
+//         console.error(requestIdKey, config.headers[requestIdKey]);
+//       }
 
-      let autoToast = !!data?.message;
-      if (data.disableAutoToast) {
-        autoToast = false;
-      }
+//       let autoToast = !!data?.message;
+//       if (data.disableAutoToast) {
+//         autoToast = false;
+//       }
 
-      throw new OneKeyServerApiError({
-        autoToast,
-        disableFallbackMessage: true,
-        message:
-          data?.translatedMessage ||
-          data?.message ||
-          'OneKeyServer Unknown Error',
-        code: data.code,
-        data: {
-          ...data,
-          requestUrl: url,
-        },
-        requestId: config.headers[requestIdKey] as string,
-      });
-    }
-    if (isEnableLogNetwork(config.url)) {
-      defaultLogger.app.network.end({
-        requestType: 'axios',
-        method: config.method as string,
-        path: config.url as string,
-        statusCode: response.status,
-        requestId: config.headers[HEADER_REQUEST_ID_KEY],
-        responseCode: data.code,
-        responseErrorMessage: data.code !== 0 ? data.message : '',
-      });
-      (response as IAxiosResponse<any>).$requestId =
-        config.headers[HEADER_REQUEST_ID_KEY];
-    }
-    return response;
-  },
-  async (error) => {
-    const { response } = error;
+//       throw new OneKeyServerApiError({
+//         autoToast,
+//         disableFallbackMessage: true,
+//         message:
+//           data?.translatedMessage ||
+//           data?.message ||
+//           'OneKeyServer Unknown Error',
+//         code: data.code,
+//         data: {
+//           ...data,
+//           requestUrl: url,
+//         },
+//         requestId: config.headers[requestIdKey] as string,
+//       });
+//     }
+//     if (isEnableLogNetwork(config.url)) {
+//       defaultLogger.app.network.end({
+//         requestType: 'axios',
+//         method: config.method as string,
+//         path: config.url as string,
+//         statusCode: response.status,
+//         requestId: config.headers[HEADER_REQUEST_ID_KEY],
+//         responseCode: data.code,
+//         responseErrorMessage: data.code !== 0 ? data.message : '',
+//       });
+//       (response as IAxiosResponse<any>).$requestId =
+//         config.headers[HEADER_REQUEST_ID_KEY];
+//     }
+//     return response;
+//   },
+//   async (error) => {
+//     const { response } = error;
 
-    if (response?.status && response?.config) {
-      const config = response.config;
-      const isOneKeyDomain = await checkRequestIsOneKeyDomain({
-        config,
-      });
-      defaultLogger.app.network.error({
-        requestType: 'axios',
-        method: config.method as string,
-        path: config.url as string,
-        statusCode: response?.status,
-        requestId: config.headers[HEADER_REQUEST_ID_KEY],
-        responseCode: response?.data?.code,
-        errorMessage: response?.data?.message,
-      });
-      if (isOneKeyDomain && Number(response.status) === 403) {
-        const title = appLocale.intl.formatMessage({
-          id: ETranslations.title_403,
-        });
-        const description = appLocale.intl.formatMessage({
-          id: ETranslations.description_403,
-        });
-        throw new OneKeyServerApiError({
-          autoToast: true,
-          message: title,
-          code: 403,
-          requestId: description,
-        });
-      } else if (
-        isOneKeyDomain &&
-        Number(response.status) >= 500 &&
-        Number(response.status) < 600
-      ) {
-        const title = appLocale.intl.formatMessage({
-          id: ETranslations.global_server_error,
-        });
-        throw new OneKeyServerApiError({
-          autoToast: true,
-          message: title,
-          code: Number(response.status),
-          requestId: config.headers[HEADER_REQUEST_ID_KEY],
-        });
-      }
-    }
-    if (
-      error &&
-      error instanceof AxiosError &&
-      error.message === 'Network Error' &&
-      error.code === AxiosError.ERR_NETWORK &&
-      error.name === 'AxiosError'
-    ) {
-      refreshNetInfo();
-      const title = appLocale.intl.formatMessage({
-        id: ETranslations.global_network_error,
-      });
-      // if (process.env.NODE_ENV !== 'production') {
-      //   title += error?.config?.url || '';
-      // }
-      throw new OneKeyError({
-        name: error.name,
-        message: title,
-        className: EOneKeyErrorClassNames.AxiosNetworkError,
-        key: ETranslations.global_network_error,
-      });
-    }
-    throw error;
-  },
-);
+//     if (response?.status && response?.config) {
+//       const config = response.config;
+//       const isOneKeyDomain = await checkRequestIsOneKeyDomain({
+//         config,
+//       });
+//       defaultLogger.app.network.error({
+//         requestType: 'axios',
+//         method: config.method as string,
+//         path: config.url as string,
+//         statusCode: response?.status,
+//         requestId: config.headers[HEADER_REQUEST_ID_KEY],
+//         responseCode: response?.data?.code,
+//         errorMessage: response?.data?.message,
+//       });
+//       if (isOneKeyDomain && Number(response.status) === 403) {
+//         const title = appLocale.intl.formatMessage({
+//           id: ETranslations.title_403,
+//         });
+//         const description = appLocale.intl.formatMessage({
+//           id: ETranslations.description_403,
+//         });
+//         throw new OneKeyServerApiError({
+//           autoToast: true,
+//           message: title,
+//           code: 403,
+//           requestId: description,
+//         });
+//       } else if (
+//         isOneKeyDomain &&
+//         Number(response.status) >= 500 &&
+//         Number(response.status) < 600
+//       ) {
+//         const title = appLocale.intl.formatMessage({
+//           id: ETranslations.global_server_error,
+//         });
+//         throw new OneKeyServerApiError({
+//           autoToast: true,
+//           message: title,
+//           code: Number(response.status),
+//           requestId: config.headers[HEADER_REQUEST_ID_KEY],
+//         });
+//       }
+//     }
+//     if (
+//       error &&
+//       error instanceof AxiosError &&
+//       error.message === 'Network Error' &&
+//       error.code === AxiosError.ERR_NETWORK &&
+//       error.name === 'AxiosError'
+//     ) {
+//       refreshNetInfo();
+//       const title = appLocale.intl.formatMessage({
+//         id: ETranslations.global_network_error,
+//       });
+//       // if (process.env.NODE_ENV !== 'production') {
+//       //   title += error?.config?.url || '';
+//       // }
+//       throw new OneKeyError({
+//         name: error.name,
+//         message: title,
+//         className: EOneKeyErrorClassNames.AxiosNetworkError,
+//         key: ETranslations.global_network_error,
+//       });
+//     }
+//     throw error;
+//   },
+// );
 
-const orgCreate = axios.create;
-axios.create = function (config?: AxiosRequestConfig): AxiosInstance {
-  const defaultConfig: AxiosRequestConfig = {
-    timeout: REQUEST_TIMEOUT,
-  };
-  const mergedConfig = {
-    ...defaultConfig,
-    ...config,
-    timeout:
-      config?.timeout !== undefined ? config.timeout : defaultConfig.timeout,
-  };
-  const result = orgCreate.call(this, mergedConfig);
-  forEach((axios.interceptors.request as any).handlers, (handler) => {
-    result.interceptors.request.use(handler.fulfilled, handler.rejected);
-  });
-  forEach((axios.interceptors.response as any).handlers, (handler) => {
-    result.interceptors.response.use(handler.fulfilled, handler.rejected);
-  });
-  return result;
-};
+// const orgCreate = axios.create;
+// axios.create = function (config?: AxiosRequestConfig): AxiosInstance {
+//   const defaultConfig: AxiosRequestConfig = {
+//     timeout: REQUEST_TIMEOUT,
+//   };
+//   const mergedConfig = {
+//     ...defaultConfig,
+//     ...config,
+//     timeout:
+//       config?.timeout !== undefined ? config.timeout : defaultConfig.timeout,
+//   };
+//   const result = orgCreate.call(this, mergedConfig);
+//   forEach((axios.interceptors.request as any).handlers, (handler) => {
+//     result.interceptors.request.use(handler.fulfilled, handler.rejected);
+//   });
+//   forEach((axios.interceptors.response as any).handlers, (handler) => {
+//     result.interceptors.response.use(handler.fulfilled, handler.rejected);
+//   });
+//   return result;
+// };
