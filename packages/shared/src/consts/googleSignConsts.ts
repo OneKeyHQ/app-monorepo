92a5bd7620aaac85f3fe37aa552d1ca0485776c2
morizon <sidmorizon@outlook.com>
2025-11-26 11:43:48 +0800

    Fix/update dev settings (#9108)
    * fix: update dev settings
    
    * fix: lint
    
    * fix: lint

--- packages/shared/src/consts/googleSignConsts.ts
+++ packages/shared/src/consts/googleSignConsts.ts
@@ -1,10 +1,12 @@
+/* eslint-disable spellcheck/spell-checker */
 import platformEnv from '../platformEnv';
 
 export const GoogleSignInConfigure = {
   scopes: ['https://www.googleapis.com/auth/drive.file'],
-  webClientId: platformEnv.isDev
-    ? // DEVELOPER_ERROR: On local development this can happen due to incorrect app signing. Please test with a production-signed build.
-      '117481276073-fs7omuqsmvgtg6bci3ja1gvo03g0d984.apps.googleusercontent.com' // Dev
-    : '94391474021-ffaspa4ikjqpqvn5ndplqobvuvhnj8v3.apps.googleusercontent.com', // Pro
-  offlineAccess: true,
+  // webClientId: platformEnv.isDev
+  //   ? // DEVELOPER_ERROR: On local development this can happen due to incorrect app signing. Please test with a production-signed build.
+  //     '117481276073-fs7omuqsmvgtg6bci3ja1gvo03g0d984.apps.googleusercontent.com' // Dev
+  //   : '94391474021-ffaspa4ikjqpqvn5ndplqobvuvhnj8v3.apps.googleusercontent.com', // Pro
+  // offlineAccess: true,
+  offlineAccess: false,
 };
