efbec519e44db98ac585c81a8d08e29dbe531656
wabicai <kaisheng.cai@onekey.so>
2025-11-27 17:28:09 +0800

    feat: support desktop-webusb

--- packages/kit-bg/src/services/ServiceAccount/ServiceAccount.ts
+++ packages/kit-bg/src/services/ServiceAccount/ServiceAccount.ts
@@ -2756,7 +2756,6 @@ class ServiceAccount extends ServiceBase {
       deviceCommonParams: {
         passphraseState: wallet?.passphraseState,
         useEmptyPassphrase: !wallet.passphraseState,
-        skipWebDevicePrompt: platformEnv.isDesktop,
       },
     };
   }
