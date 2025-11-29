7b858c01d7676b3d494d870aaf88ffae1020251a
Charon <charon.dian@gmail.com>
2025-11-29 13:05:33 +0800

    fix: refactor fetchEarnHomePageBannerList to remove memoization and simplify implementation

--- packages/kit-bg/src/services/ServiceStaking.ts
+++ packages/kit-bg/src/services/ServiceStaking.ts
@@ -1451,23 +1451,13 @@ class ServiceStaking extends ServiceBase {
   }
 
   @backgroundMethod()
-  fetchEarnHomePageBannerList() {
-    return this._fetchEarnHomePageBannerList();
+  async fetchEarnHomePageBannerList() {
+    const client = await this.getClient(EServiceEndpointEnum.Utility);
+    const res = await client.get<{ data: IDiscoveryBanner[] }>(
+      '/utility/v1/earn-banner/list',
+    );
+    return res.data.data;
   }
-
-  _fetchEarnHomePageBannerList = memoizee(
-    async () => {
-      const client = await this.getClient(EServiceEndpointEnum.Utility);
-      const res = await client.get<{ data: IDiscoveryBanner[] }>(
-        '/utility/v1/earn-banner/list',
-      );
-      return res.data.data;
-    },
-    {
-      promise: true,
-      maxAge: timerUtils.getTimeDurationMs({ seconds: 60 }),
-    },
-  );
 
   @backgroundMethod()
   async getEarnAvailableAccounts(params: {
