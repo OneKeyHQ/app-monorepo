444589cc6bcec43cb1b9955da433f3323bffb4a1
morizon <sidmorizon@outlook.com>
2025-12-05 10:58:29 +0800

    chore: update Supabase project URL and public API key in consts.ts

--- packages/kit/src/components/OneKeyAuth/supabase/consts.ts
+++ packages/kit/src/components/OneKeyAuth/supabase/consts.ts
@@ -1,8 +1,9 @@
 // Project URL at https://supabase.com/dashboard/project/xxxxxx/settings/api
-export const SUPABASE_PROJECT_URL = 'https://wtspqckturkzhstyjabx.supabase.co';
+
+export const SUPABASE_PROJECT_URL = 'https://zvxscjkvkjepbrjncvzt.supabase.co';
 
 // Publishable key at https://supabase.com/dashboard/project/xxxxxx/settings/api-keys/new
 export const SUPABASE_PUBLIC_API_KEY =
-  'sb_publishable_So24RIupCcXUHaKo1gM4VA_uOBbgjoN';
+  'sb_publishable_ryfw0-h47JC2lHFRB2yrjw_iS_1KPgW';
 
 export const SUPABASE_STORAGE_KEY_PREFIX = 'OneKeySupabaseAuth__';
