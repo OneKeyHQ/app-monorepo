bed418f6c9c4a1c6d2eb225030a5c58f893c554d
morizon <sidmorizon@outlook.com>
2025-12-06 09:51:10 +0800

    refactor: extend cache duration in SupabaseStorage for improved performance
    - Increased the maxAge for cached items from 10 seconds to 30 seconds in the SupabaseStorage class, enhancing data retrieval efficiency.

--- packages/shared/src/storage/SupabaseStorage/SupabaseStorage.ts
+++ packages/shared/src/storage/SupabaseStorage/SupabaseStorage.ts
@@ -34,7 +34,7 @@ export class SupabaseStorage {
     {
       promise: true,
       primitive: true,
-      maxAge: timerUtils.getTimeDurationMs({ seconds: 10 }),
+      maxAge: timerUtils.getTimeDurationMs({ seconds: 30 }),
     },
   );
 
