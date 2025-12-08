30a057108c24f33d04183ca65a124f10abf987b7
huhuanming <huanming@onekey.so>
2025-12-08 22:10:34 +0800

    Update BackgroundRunnerModule.m

--- apps/mobile/ios/OneKeyWallet/BackgroundRunnerModule.m
+++ apps/mobile/ios/OneKeyWallet/BackgroundRunnerModule.m
@@ -35,7 +35,7 @@ RCT_EXPORT_MODULE();
         return;
     }
     
-    NSString *urlString = @"http://localhost:8081/apps/mobile/background.bundle?platform=ios&dev=true&lazy=false&minify=false&inlineSourceMap=false&modulesOnly=false&runModule=true&excludeSource=true&sourcePaths=url-server&app=so.onekey.wallet&transform.routerRoot=app&transform.engine=hermes&transform.bytecode=1&unstable_transformProfile=hermes-stable";
+    NSString *urlString = @"http://localhost:8082/apps/mobile/background.bundle?platform=ios&dev=true&lazy=false&minify=false&inlineSourceMap=false&modulesOnly=false&runModule=true&excludeSource=true&sourcePaths=url-server&app=so.onekey.wallet&transform.routerRoot=app&transform.engine=hermes&transform.bytecode=1&unstable_transformProfile=hermes-stable";
     NSURL *jsCodeLocation = [NSURL URLWithString:urlString];
     backgroundRootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"background"
@@ -43,10 +43,16 @@ RCT_EXPORT_MODULE();
                                                    launchOptions:nil];
 }
 
-RCT_EXPORT_METHOD(sendMessage:(NSDictionary *)msg)
+RCT_EXPORT_METHOD(sendToUI:(NSDictionary *)msg)
 {
     NSLog(@"[BackgroundRunnerModule] Received message: %@", msg);
     [self sendEventWithName:@"toUI" body:msg];
+}
+
+RCT_EXPORT_METHOD(sendToBackground:(NSDictionary *)msg)
+{
+    NSLog(@"[BackgroundRunnerModule] Received message: %@", msg);
+    [self sendEventWithName:@"toBackground" body:msg];
 }
 
 @end
