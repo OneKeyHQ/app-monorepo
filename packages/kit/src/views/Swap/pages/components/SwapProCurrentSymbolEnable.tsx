623ede3449e9c1918565b549d05496f57e0b7d72
ezailWang <jelly@onekey.so>
2025-12-08 18:21:24 +0800

    fix: coding
    Signed-off-by: ezailWang <jelly@onekey.so>

--- packages/kit/src/views/Swap/pages/components/SwapProCurrentSymbolEnable.tsx
+++ packages/kit/src/views/Swap/pages/components/SwapProCurrentSymbolEnable.tsx
@@ -1,9 +1,13 @@
+import { useIntl } from 'react-intl';
+
 import { Checkbox, SizableText, XStack } from '@onekeyhq/components';
 import { useSwapProEnableCurrentSymbolAtom } from '@onekeyhq/kit/src/states/jotai/contexts/swap';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
 
 const SwapProCurrentSymbolEnable = () => {
   const [swapProEnableCurrentSymbol, setSwapProEnableCurrentSymbol] =
     useSwapProEnableCurrentSymbolAtom();
+  const intl = useIntl();
   return (
     <XStack gap="$2" alignItems="center" mt="$2">
       <Checkbox
@@ -12,7 +16,9 @@ const SwapProCurrentSymbolEnable = () => {
           setSwapProEnableCurrentSymbol(!!value);
         }}
       />
-      <SizableText>Current symbol</SizableText>
+      <SizableText>
+        {intl.formatMessage({ id: ETranslations.dexmarket_pro_current_symbol })}
+      </SizableText>
     </XStack>
   );
 };
