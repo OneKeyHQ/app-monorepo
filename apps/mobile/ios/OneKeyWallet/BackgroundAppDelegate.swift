275346f84ba6491d75d5d9c499a89cd637b7c8c2
huhuanming <huanming@onekey.so>
2025-12-08 23:51:13 +0800

    fix: fix compile

--- apps/mobile/ios/OneKeyWallet/BackgroundAppDelegate.swift
+++ apps/mobile/ios/OneKeyWallet/BackgroundAppDelegate.swift
@@ -17,7 +17,7 @@ class BackgroundAppDelegate: ExpoAppDelegate {
       
       self.reactNativeDelegate = delegate
       self.reactNativeFactory = factory
-      bindReactNativeFactory(factory)
+      self.bindReactNativeFactory(factory)
 
       let window = UIWindow(frame: UIScreen.main.bounds)
       self.window = window
@@ -32,7 +32,7 @@ class BackgroundAppDelegate: ExpoAppDelegate {
 
 class BackgroundReactNativeDelegate: ExpoReactNativeFactoryDelegate {
   static let BACKGROUND_BUNDLE_NAME = "background"
-  static let backgroundBundleDebugURL = "http://localhost:8082/apps/mobile/background.bundle?platform=ios&dev=true&lazy=false&minify=false&inlineSourceMap=false&modulesOnly=false&runModule=true&excludeSource=true&sourcePaths=url-server&app=so.onekey.wallet&transform.routerRoot=app&transform.engine=hermes&transform.bytecode=1&unstable_transformProfile=hermes-stable"
+  static let BACKGROUND_BUNDLE_DEBUG_URL = "http://localhost:8082/apps/mobile/background.bundle?platform=ios&dev=true&lazy=false&minify=false&inlineSourceMap=false&modulesOnly=false&runModule=true&excludeSource=true&sourcePaths=url-server&app=so.onekey.wallet&transform.routerRoot=app&transform.engine=hermes&transform.bytecode=1&unstable_transformProfile=hermes-stable"
   
   // Override to provide the JS bundle URL for the background runner
   override func sourceURL(for bridge: RCTBridge) -> URL? {
@@ -43,7 +43,7 @@ class BackgroundReactNativeDelegate: ExpoReactNativeFactoryDelegate {
   override func bundleURL() -> URL? {
 #if DEBUG
     // Load Metro bundle for background runner (change root as needed)
-    return RCTBundleURLProvider.sharedSettings().jsBundleURL(BackgroundReactNativeDelegate.backgroundBundleDebugURL)
+    return URL(string: BackgroundReactNativeDelegate.BACKGROUND_BUNDLE_DEBUG_URL)
 #else
     // Look for updated background bundle in Documents first
     // if let bundlePath = BundleUpdateModule.currentBundleBackgroundJSBundle() {
