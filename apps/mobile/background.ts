1eae0713bfe32bc8b291517064f25f2e4f282fc8
huhuanming <huanming@onekey.so>
2025-12-09 23:55:49 +0800

    Update background.ts

--- apps/mobile/background.ts
+++ apps/mobile/background.ts
@@ -1,1 +1,14 @@
+import { NativeModules } from 'react-native';
+import { createMMKV } from 'react-native-mmkv';
+import { FileLogger } from 'react-native-file-logger'
+
 console.log('background.ts');
+console.log('background222.ts');
+
+globalThis._reactNativeModules = require('react-native');
+
+globalThis.BackgroundRunnerModule = NativeModules.BackgroundRunnerModule;
+
+globalThis.RootViewBackground = NativeModules.RootViewBackground;
+console.log('createMMKV', createMMKV);
+console.log('FileLogger', FileLogger);
