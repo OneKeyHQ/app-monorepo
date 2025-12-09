2fd9d77f07318a91297000f3cf754bcee721814e
huhuanming <huanming@onekey.so>
2025-12-10 01:27:03 +0800

    Update background.ts

--- apps/mobile/background.ts
+++ apps/mobile/background.ts
@@ -1,9 +1,11 @@
+import * as ExpoDevice from 'expo-device';
+import * as Sharing from 'expo-sharing';
 import { NativeModules } from 'react-native';
+import { FileLogger } from 'react-native-file-logger';
 import { createMMKV } from 'react-native-mmkv';
-import { FileLogger } from 'react-native-file-logger'
 
 console.log('background.ts');
-console.log('background222.ts');
+console.log('background22212.ts');
 
 globalThis._reactNativeModules = require('react-native');
 
@@ -12,3 +14,8 @@ globalThis.BackgroundRunnerModule = NativeModules.BackgroundRunnerModule;
 globalThis.RootViewBackground = NativeModules.RootViewBackground;
 console.log('createMMKV', createMMKV);
 console.log('FileLogger', FileLogger);
+console.log('ExpoDevice', ExpoDevice);
+
+globalThis.Sharing = async () => {
+  return Sharing.shareAsync('12313213');
+};
