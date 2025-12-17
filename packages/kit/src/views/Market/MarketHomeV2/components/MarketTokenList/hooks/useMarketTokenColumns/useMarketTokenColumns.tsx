46cfed2387e221f297cea804c2c94f361e259245
huhuanming <huanming@onekey.so>
2025-12-17 21:54:38 +0800

    Update useMarketTokenColumns.tsx

--- packages/kit/src/views/Market/MarketHomeV2/components/MarketTokenList/hooks/useMarketTokenColumns/useMarketTokenColumns.tsx
+++ packages/kit/src/views/Market/MarketHomeV2/components/MarketTokenList/hooks/useMarketTokenColumns/useMarketTokenColumns.tsx
@@ -20,10 +20,12 @@ export const useMarketTokenColumns = (
   );
   const mobileColumns = useColumnsMobile();
 
-  const { md } = useMedia();
+  const media = useMedia();
+
+  console.log('md', media);
 
   return useMemo(
-    () => (md ? mobileColumns : desktopColumns),
-    [md, mobileColumns, desktopColumns],
+    () => (media.gtMd ? desktopColumns : mobileColumns),
+    [media.gtMd, desktopColumns, mobileColumns],
   );
 };
