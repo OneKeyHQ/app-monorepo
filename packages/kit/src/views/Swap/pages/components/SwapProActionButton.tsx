19130596609b02cda5323c281034f7f06efd83b3
ezailWang <jelly@onekey.so>
2025-12-19 12:13:10 +0800

    fix: swappro some issue fix
    Signed-off-by: ezailWang <jelly@onekey.so>

--- packages/kit/src/views/Swap/pages/components/SwapProActionButton.tsx
+++ packages/kit/src/views/Swap/pages/components/SwapProActionButton.tsx
@@ -1,22 +1,29 @@
 import { useMemo } from 'react';
 
+import BigNumber from 'bignumber.js';
 import { useIntl } from 'react-intl';
 
 import { Button } from '@onekeyhq/components';
+import { useCurrency } from '@onekeyhq/kit/src/components/Currency';
 import { useDebouncedCallback } from '@onekeyhq/kit/src/hooks/useDebounce';
 import {
+  useSwapFromTokenAmountAtom,
   useSwapProDirectionAtom,
-  useSwapProSelectTokenAtom,
+  useSwapProInputAmountAtom,
   useSwapProTradeTypeAtom,
   useSwapQuoteCurrentSelectAtom,
   useSwapSpeedQuoteFetchingAtom,
   useSwapSpeedQuoteResultAtom,
 } from '@onekeyhq/kit/src/states/jotai/contexts/swap';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import { numberFormat } from '@onekeyhq/shared/src/utils/numberUtils';
 import { ESwapProTradeType } from '@onekeyhq/shared/types/swap/types';
 
 import { ESwapDirection } from '../../../Market/MarketDetailV2/components/SwapPanel/hooks/useTradeType';
-import { useSwapProAccount } from '../../hooks/useSwapPro';
+import {
+  useSwapProAccount,
+  useSwapProInputToken,
+} from '../../hooks/useSwapPro';
 import { useSwapQuoteLoading } from '../../hooks/useSwapState';
 
 interface ISwapProActionButtonProps {
@@ -36,10 +43,86 @@ const SwapProActionButton = ({
   const [swapQuoteResult] = useSwapQuoteCurrentSelectAtom();
   const [swapProQuoteResult] = useSwapSpeedQuoteResultAtom();
   const swapProAccount = useSwapProAccount();
-  const [swapProSelectToken] = useSwapProSelectTokenAtom();
   const quoteLoading = useSwapQuoteLoading();
+  const currencyInfo = useCurrency();
   const [quoteFetching] = useSwapSpeedQuoteFetchingAtom();
+  const [swapProInputAmount] = useSwapProInputAmountAtom();
+  const [swapFromInputAmount] = useSwapFromTokenAmountAtom();
+  const inputToken = useSwapProInputToken();
+  const inputAmount = useMemo(() => {
+    if (swapProTradeType === ESwapProTradeType.MARKET) {
+      return swapProInputAmount;
+    }
+    return swapFromInputAmount.value;
+  }, [swapProTradeType, swapProInputAmount, swapFromInputAmount.value]);
 
+  const inputTokenValue = useMemo(() => {
+    const inputPrice = new BigNumber(inputToken?.price || '0');
+    if (swapProTradeType === ESwapProTradeType.MARKET) {
+      const inputAmountBN = new BigNumber(inputAmount || '0');
+      if (
+        inputPrice.isNaN() ||
+        inputAmountBN.isNaN() ||
+        inputPrice.isZero() ||
+        inputAmountBN.isZero()
+      ) {
+        return '';
+      }
+      return `(${numberFormat(
+        inputPrice.multipliedBy(inputAmountBN).toFixed(),
+        {
+          formatter: 'value',
+          formatterOptions: {
+            currency: currencyInfo.symbol,
+          },
+        },
+      )})`;
+    }
+    const inputAmountBN = new BigNumber(swapFromInputAmount.value || '0');
+    if (
+      inputPrice.isNaN() ||
+      inputAmountBN.isNaN() ||
+      inputPrice.isZero() ||
+      inputAmountBN.isZero()
+    ) {
+      return '';
+    }
+    return `(${numberFormat(inputPrice.multipliedBy(inputAmountBN).toFixed(), {
+      formatter: 'value',
+      formatterOptions: {
+        tokenSymbol: currencyInfo.symbol,
+      },
+    })})`;
+  }, [
+    inputToken?.price,
+    swapProTradeType,
+    swapFromInputAmount.value,
+    inputAmount,
+    currencyInfo.symbol,
+  ]);
+  const inputTokenAmountValue = useMemo(() => {
+    if (swapProTradeType === ESwapProTradeType.MARKET) {
+      const swapProInputAmountValue = new BigNumber(swapProInputAmount || '0');
+      return `${
+        swapProInputAmountValue.isZero() || swapProInputAmountValue.isNaN()
+          ? ''
+          : swapProInputAmountValue.toFixed()
+      } ${inputToken?.symbol ?? '-'}`;
+    }
+    const swapFromInputAmountValue = new BigNumber(
+      swapFromInputAmount.value || '0',
+    );
+    return `${
+      swapFromInputAmountValue.isZero() || swapFromInputAmountValue.isNaN()
+        ? ''
+        : swapFromInputAmountValue.toFixed()
+    } ${inputToken?.symbol ?? '-'}`;
+  }, [
+    swapProTradeType,
+    swapFromInputAmount.value,
+    swapProInputAmount,
+    inputToken?.symbol,
+  ]);
   const debouncedOnSwapProActionClick = useDebouncedCallback(
     onSwapProActionClick,
     500,
@@ -84,13 +167,14 @@ const SwapProActionButton = ({
         swapProDirection === ESwapDirection.BUY
           ? ETranslations.global_buy
           : ETranslations.global_sell,
-    })} ${swapProSelectToken?.symbol ?? ''}`;
+    })} ${inputTokenAmountValue} ${inputTokenValue}`;
   }, [
     hasEnoughBalance,
+    inputTokenAmountValue,
+    inputTokenValue,
     intl,
     swapProAccount?.result?.addressDetail.address,
     swapProDirection,
-    swapProSelectToken?.symbol,
   ]);
 
   return (
