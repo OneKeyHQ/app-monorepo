2cb22e3e57ab88476300882e803d72beee663b98
morizon <sidmorizon@outlook.com>
2025-12-19 20:51:40 +0800

    fix: update development extension ID handling in manifest
    - Simplified the assignment of the `devExtensionIdKey` in the manifest by removing the conditional check for the production environment.
    - Ensured that the development extension ID is always set, improving consistency for local development setups.

--- apps/ext/src/manifest/shared.js
+++ apps/ext/src/manifest/shared.js
@@ -8,9 +8,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 // Keep it DEV-only and read from env so we don't hardcode a team-wide ID.
 const devExtensionIdKey =
   // DEV extensionId:   goodgebcoklfekldbhjmckehehjdfipe
-  isDev
-    ? 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArQxWBwYxmyGIm461gHgZKcoiu9uzjxBvnIkzIsv5woytRUbDt3ZyF1wnSceoSxcLiTN3q/Ml0X/EAIjV+eLzzKbeYsbmYNbTcdq71I6vxIQ4X4YbafVx8bURz13hFYl8YxFVNlNP2GxcNATNeF3CIMDL+VhYBV/eJ6Oaj1YbZhDESyYPBDtNjyT3wc7eD4kE9ICEX3EC/cksRbuZ80/Rzsyn66hH+EJxfcJT9sO4f2ol6PvBDurDcEUB3fIt1NglCPBF8Kn5NRLhsr+c8r0bBvAObyVu5oow73iaAi8BS4nMmaBybc9Ec/n6A3U7DfOL1p7zE66X7lRRNcfr6lHTvQIDAQAB'
-    : undefined;
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArQxWBwYxmyGIm461gHgZKcoiu9uzjxBvnIkzIsv5woytRUbDt3ZyF1wnSceoSxcLiTN3q/Ml0X/EAIjV+eLzzKbeYsbmYNbTcdq71I6vxIQ4X4YbafVx8bURz13hFYl8YxFVNlNP2GxcNATNeF3CIMDL+VhYBV/eJ6Oaj1YbZhDESyYPBDtNjyT3wc7eD4kE9ICEX3EC/cksRbuZ80/Rzsyn66hH+EJxfcJT9sO4f2ol6PvBDurDcEUB3fIt1NglCPBF8Kn5NRLhsr+c8r0bBvAObyVu5oow73iaAi8BS4nMmaBybc9Ec/n6A3U7DfOL1p7zE66X7lRRNcfr6lHTvQIDAQAB';
 
 // TODO:
 // Required value 'version' is missing or invalid. It must be between 1-4 dot-separated integers each between 0 and 65536.
@@ -20,7 +18,7 @@ const version = process.env.VERSION;
 
 module.exports = {
   // generate extensionId in local
-  'key': process.env.NODE_ENV !== 'production' ? devExtensionIdKey : undefined,
+  'key': devExtensionIdKey,
   version,
   'name': 'OneKey: Secure Crypto Wallet',
   'description':
