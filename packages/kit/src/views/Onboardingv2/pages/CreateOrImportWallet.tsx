b49c665179d058949e460a0a2b4697c980892a54
Franco <franco@onekey.so>
2025-12-22 17:33:42 +0800

    ui

--- packages/kit/src/views/Onboardingv2/pages/CreateOrImportWallet.tsx
+++ packages/kit/src/views/Onboardingv2/pages/CreateOrImportWallet.tsx
@@ -247,6 +247,55 @@ function CreateOrImportWallet() {
                       color="$iconSubdued"
                     />
                   </Card.Header>
+                  <Card.Body>
+                    <XStack gap="$2" flexWrap="wrap">
+                      {[
+                        {
+                          title: intl.formatMessage({
+                            id: ETranslations.highest_security,
+                          }),
+                          badge: 'success' as const,
+                        },
+                        {
+                          title: intl.formatMessage({
+                            id: ETranslations.for_large_assets,
+                          }),
+                        },
+                        {
+                          title: intl.formatMessage({
+                            id: ETranslations.private_key_staty_on_device,
+                          }),
+                        },
+                        {
+                          title: intl.formatMessage({
+                            id: ETranslations.ideal_for_long_term_storage,
+                          }),
+                        },
+                        {
+                          title: intl.formatMessage({
+                            id: ETranslations.protects_against_malware,
+                          }),
+                        },
+                      ].map((item, index) => (
+                        <Badge
+                          key={index}
+                          {...(item.badge && { badgeType: item.badge })}
+                        >
+                          <Badge.Text size="$bodySm">{item.title}</Badge.Text>
+                        </Badge>
+                      ))}
+                      <Badge>
+                        <Badge.Text size="$bodySm">
+                          {intl.formatMessage({
+                            id: ETranslations.global_supports,
+                          })}
+                        </Badge.Text>
+                        <XStack gap="$1" ml="$1">
+                          <Icon name="OnekeyLogoIllus" size="$3" />
+                        </XStack>
+                      </Badge>
+                    </XStack>
+                  </Card.Body>
                 </Card>
               </>
             ) : null}
