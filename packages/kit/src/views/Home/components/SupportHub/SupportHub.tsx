c586a15e27089518b9cc498ecbfb89fd833e1b74
weatherstar <weather@onekey.so>
2025-12-24 17:06:59 +0800

    fix: support hub style

--- packages/kit/src/views/Home/components/SupportHub/SupportHub.tsx
+++ packages/kit/src/views/Home/components/SupportHub/SupportHub.tsx
@@ -84,7 +84,11 @@ function SupportHub() {
         <RichBlock
           blockContainerProps={{
             flex: 1,
-            flexBasis: 0,
+          }}
+          $gtMd={{
+            blockContainerProps: {
+              flexBasis: 0,
+            },
           }}
           content={
             <YStack
@@ -133,7 +137,12 @@ function SupportHub() {
             outlineColor: '$neutral2',
           }}
         />
-        <Stack flexDirection="column" gap="$3" flex={1} flexBasis={0}>
+        <Stack
+          flexDirection="column"
+          gap="$3"
+          flex={1}
+          $gtMd={{ flexBasis: 0 }}
+        >
           <RichBlock
             blockContainerProps={{
               flex: 1,
