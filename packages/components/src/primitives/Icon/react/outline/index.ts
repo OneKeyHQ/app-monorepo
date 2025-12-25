8b5b978701f5c08386ee4eb349f3eb08300f5654
limichange <limichange@hotmail.com>
2025-12-25 16:24:03 +0800

    feat: add AtomOutline icon and add-icon claude command

--- packages/components/src/primitives/Icon/react/outline/index.ts
+++ packages/components/src/primitives/Icon/react/outline/index.ts
@@ -65,6 +65,7 @@ export { default as ArrowTriangleLeft } from './ArrowTriangleLeft';
 export { default as ArrowTriangleRight } from './ArrowTriangleRight';
 export { default as ArrowTriangleTop } from './ArrowTriangleTop';
 export { default as AtSign } from './AtSign';
+export { default as Atom } from './Atom';
 export { default as Attachment } from './Attachment';
 export { default as AutoFlash } from './AutoFlash';
 export { default as AutoPageSize } from './AutoPageSize';
