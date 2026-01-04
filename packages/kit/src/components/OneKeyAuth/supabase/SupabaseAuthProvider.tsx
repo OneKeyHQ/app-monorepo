ac00289bb0d92f22e3b4df58586b20e86e216415
morizon <sidmorizon@outlook.com>
2026-01-04 13:16:00 +0800

    fix: lint

--- packages/kit/src/components/OneKeyAuth/supabase/SupabaseAuthProvider.tsx
+++ packages/kit/src/components/OneKeyAuth/supabase/SupabaseAuthProvider.tsx
@@ -1,17 +1,10 @@
 import type { PropsWithChildren } from 'react';
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-import { getSupabaseClient } from './getSupabaseClient';
-import { SupabaseAuthContext } from './SupabaseAuthContext';
-
+import { getSupabaseClient } from '@onekeyhq/shared/src/utils/supabaseClientUtils';
 import type { Session } from '@supabase/supabase-js';
+
+import { SupabaseAuthContext } from './SupabaseAuthContext';
 
 export default function SupabaseAuthProvider({ children }: PropsWithChildren) {
   const [authSession, setSession] = useState<Session | undefined | null>();
@@ -61,23 +54,7 @@ export default function SupabaseAuthProvider({ children }: PropsWithChildren) {
       "message": "Could not find the table 'public.profiles' in the schema cache"
     }
   */
-  useEffect(() => {
-    const fetchProfile = async () => {
-      setIsLoading(true);
-      // if (authSession) {
-      //   const { data } = await getSupabaseClient()
-      //     .client.from('profiles')
-      //     .select('*')
-      //     .eq('id', authSession.user.id)
-      //     .single();
-      //   setProfile(data);
-      // } else {
-      //   setProfile(null);
-      // }
-      setIsLoading(false);
-    };
-    // void fetchProfile();
-  }, [authSession]);
+
 
   return (
     <SupabaseAuthContext.Provider
