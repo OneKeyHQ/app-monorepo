f5d75b8b4372cc3601ebf7054e3304bbf4aa8c21
Zen <mingzhen.fang@onekey.so>
2026-01-04 22:20:45 +0800

    fix(perp): resume subscriptions on app foreground to prevent stale data

--- packages/kit/src/views/Perp/components/PerpMobileNetworkAlert.tsx
+++ packages/kit/src/views/Perp/components/PerpMobileNetworkAlert.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 
 import { useIntl } from 'react-intl';
 
@@ -6,11 +6,34 @@ import { Alert, SizableText } from '@onekeyhq/components';
 import { usePerpsNetworkStatusAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
 
+const ALERT_SHOW_DELAY_MS = 1500;
+
 function PerpMobileNetworkAlertComponent() {
   const intl = useIntl();
   const [networkStatus] = usePerpsNetworkStatusAtom();
+  const [shouldShowAlert, setShouldShowAlert] = useState(false);
 
-  if (networkStatus?.connected !== false) {
+  const isDisconnected = networkStatus?.connected === false;
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout> | undefined;
+
+    if (isDisconnected) {
+      timer = setTimeout(() => {
+        setShouldShowAlert(true);
+      }, ALERT_SHOW_DELAY_MS);
+    } else {
+      setShouldShowAlert(false);
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [isDisconnected]);
+
+  if (!shouldShowAlert) {
     return null;
   }
 
