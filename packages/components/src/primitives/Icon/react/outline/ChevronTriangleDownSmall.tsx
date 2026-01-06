1eb8ee87970bc93b2aca36ccc027c3fb69947f27
Charon <charon.dian@gmail.com>
2026-01-06 09:30:40 +0800

    fix: Update fillOpacity for SvgChevronTriangleDownSmall component

--- packages/components/src/primitives/Icon/react/outline/ChevronTriangleDownSmall.tsx
+++ packages/components/src/primitives/Icon/react/outline/ChevronTriangleDownSmall.tsx
@@ -4,6 +4,7 @@ const SvgChevronTriangleDownSmall = (props: SvgProps) => (
   <Svg fill="none" viewBox="0 0 18 18" accessibilityRole="image" {...props}>
     <Path
       fill="currentColor"
+      fillOpacity={0.447}
       d="M7.142 7.313a1.125 1.125 0 0 0-.888 1.815l1.858 2.39c.45.579 1.326.579 1.776 0l1.858-2.39a1.125 1.125 0 0 0-.888-1.815z"
     />
   </Svg>
