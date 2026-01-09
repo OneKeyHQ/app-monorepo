5c88d758f94a7945c6b94d5eafe1a2749226262b
huhuanming <huanming@onekey.so>
2026-01-09 10:32:28 +0800

    Revert "Reapply "Update index.js""
    This reverts commit b876e8d470e443cc77e6b47434318350f26ff848.

--- apps/desktop/index.js
+++ apps/desktop/index.js
@@ -1,1 +1,14 @@
-console.log('This is the desktop app entry point');
+/* eslint-disable import/first */
+/* eslint-disable unicorn/prefer-global-this */
+if (typeof window !== 'undefined') {
+  window.$$onekeyJsReadyAt = Date.now();
+}
+
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
