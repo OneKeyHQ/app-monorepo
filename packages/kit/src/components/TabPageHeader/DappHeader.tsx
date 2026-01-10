2bec9ef8779a587378febe778f8d540503eff4d7
huhuanming <huanming@onekey.so>
2026-01-11 00:10:40 +0800

    Update DappHeader.tsx

--- packages/kit/src/components/TabPageHeader/DappHeader.tsx
+++ packages/kit/src/components/TabPageHeader/DappHeader.tsx
@@ -1,6 +1,18 @@
 import { useCallback } from 'react';
 
-import { Page, XStack, useTheme } from '@onekeyhq/components';
+import { useIntl } from 'react-intl';
+
+import {
+  HeaderIconButton,
+  Page,
+  Popover,
+  Tooltip,
+  XStack,
+  YStack,
+  useTheme,
+} from '@onekeyhq/components';
+import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import type { ETabRoutes } from '@onekeyhq/shared/src/routes';
 
 import { useAccountSelectorContextData } from '../../states/jotai/contexts/accountSelector';
@@ -15,18 +27,59 @@ import {
   WalletConnectionForWeb,
   WebHeaderNavigation,
 } from './components';
-import { MoreAction } from './HeaderRight';
 import { HeaderTitle } from './HeaderTitle';
 
 import type { ITabPageHeaderProp } from './type';
+
+function MoreDappAction() {
+  const intl = useIntl();
+  return (
+    <Popover
+      title={intl.formatMessage({ id: ETranslations.address_book_menu_title })}
+      showHeader={false}
+      keepChildrenMounted
+      floatingPanelProps={{
+        maxWidth: 288,
+        width: 288,
+        p: 0,
+        overflow: 'hidden',
+        style: { transformOrigin: 'bottom left' },
+      }}
+      placement="bottom-end"
+      renderTrigger={
+        <Tooltip
+          placement="bottom"
+          renderTrigger={
+            <HeaderIconButton
+              testID="moreActions"
+              title={intl.formatMessage({ id: ETranslations.explore_options })}
+              icon="DotGridOutline"
+            />
+          }
+          renderContent={intl.formatMessage({
+            id: ETranslations.address_book_menu_title,
+          })}
+        />
+      }
+      renderContent={<YStack>123</YStack>}
+    />
+  );
+}
 
 function RightActions({ tabRoute }: { tabRoute: ETabRoutes }) {
   return (
     <XStack ai="center" gap="$2">
       <WalletConnectionForWeb tabRoute={tabRoute} />
-      <XStack ai="center" gap="$2.5" px="$1.5" borderRadius="$2" bg="$bgStrong">
+      <XStack
+        ai="center"
+        gap="$2.5"
+        px="$1.5"
+        py="$1"
+        borderRadius="$2"
+        bg="$bgStrong"
+      >
         <HeaderNotificationIconButton testID="header-right-notification" />
-        <MoreAction />
+        <MoreDappAction />
         <DownloadButton />
         <LanguageButton />
         <ThemeButton />
