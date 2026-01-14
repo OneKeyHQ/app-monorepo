f085cf26b17a4cc047b6007bdc237958dfb2724a
Charon <charon.dian@gmail.com>
2026-01-14 14:59:48 +0800

    fix: OK-49015

--- packages/kit/src/components/TabPageHeader/index.tsx
+++ packages/kit/src/components/TabPageHeader/index.tsx
@@ -166,6 +166,8 @@ export function TabPageHeader({
         sceneName={sceneName}
         hideSearch={hideSearch}
         selectedHeaderTab={selectedHeaderTab}
+        customHeaderLeftItems={customHeaderLeftItems}
+        customHeaderRightItems={customHeaderRightItems}
         renderCustomHeaderRightItems={renderCustomHeaderRightItems}
         headerPx={headerPx}
       />
