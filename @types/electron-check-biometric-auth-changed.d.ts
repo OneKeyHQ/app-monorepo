4d855c8f8e79a21054a92abd2375cc3450541755
huhuanming <huanming@onekey.so>
2026-01-15 22:48:19 +0800

    Revert "fix: fix eslint error"
    This reverts commit 5c5ed6226e8c1ede5beb8e34be1e64d7076b7404.

--- @types/electron-check-biometric-auth-changed.d.ts
+++
@@ -1,7 +0,0 @@
-// TODO: This is a stub declaration for the 'electron-check-biometric-auth-changed' module.
-// Once the actual types are known, this file should be updated accordingly.
-// For now, it provides a basic type definition to avoid TypeScript errors.
-
-declare module 'electron-check-biometric-auth-changed' {
-  export function checkBiometricAuthChanged(): boolean;
-}
