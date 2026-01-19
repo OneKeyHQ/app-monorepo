8c534b9e81d4d6ef9024fa54d2d964749381fc50
huhuanming <huanming@onekey.so>
2026-01-19 11:38:14 +0800

    Update index.tsx

--- packages/components/src/content/Badge/index.tsx
+++ packages/components/src/content/Badge/index.tsx
@@ -7,8 +7,7 @@ import {
 } from '@onekeyhq/components/src/shared/tamagui';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
-// oxlint-disable-next-line import/no-cycle
-import { XStack } from '../../primitives';
+import { XStack } from '../../primitives/Stack';
 
 import type { IXStackProps } from '../../primitives';
 
