b53a454351adfb132261f429d90510f2e52d6da3
huhuanming <huanming@onekey.so>
2026-01-21 01:34:33 +0800

    OK-49258

--- packages/components/src/hooks/useShare.ts
+++ packages/components/src/hooks/useShare.ts
@@ -1,10 +1,8 @@
 import { useCallback, useMemo } from 'react';
 
-import * as ExpoSharing from 'expo-sharing';
 import { Share } from 'react-native';
 
 import { ETranslations } from '@onekeyhq/shared/src/locale';
-import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 // oxlint-disable-next-line import/no-cycle
 import { useClipboard } from './useClipboard';
@@ -13,19 +11,9 @@ export function useShare() {
   const { copyText } = useClipboard();
   const shareText = useCallback(
     async (text: string) => {
-      if (platformEnv.isNativeIOS) {
-        if (await ExpoSharing.isAvailableAsync()) {
-          // https://docs.expo.dev/versions/latest/sdk/sharing/
-          await ExpoSharing.shareAsync(text);
-          return;
-        }
-      }
-      if (platformEnv.isNativeAndroid) {
-        await Share.share({
-          message: text,
-        });
-        return;
-      }
+      await Share.share({
+        message: text,
+      });
       copyText(text, ETranslations.global_link_copied);
     },
     [copyText],
