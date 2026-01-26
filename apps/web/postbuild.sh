f0c0f63ce5585bbf7bc4834c0a2d7fb7f655190d
luo jiyin <luojiyin@hotmail.com>
2026-01-26 14:48:15 +0800

    fix: add shebang and strict error handling to postbuild scripts (#9864)
    * fix: add shebang and strict error handling to web/postbuild.sh
    
    * fix: add strict error handling to web-embed/postbuild.sh
    
    ---------
    
    Co-authored-by: huhuanming <huanming@onekey.so>

--- apps/web/postbuild.sh
+++ apps/web/postbuild.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail -x
+
 mkdir -p ./web-build/.well-known
 cp ./validation/deeplink.android.json ./web-build/.well-known/assetlinks.json
 cp ./validation/deeplink.ios.json ./web-build/.well-known/apple-app-site-association
