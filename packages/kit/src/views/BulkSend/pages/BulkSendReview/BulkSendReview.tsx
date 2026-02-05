436b710cd3c789ce996c8c1a9659ee8e20ce115c
weatherstar <weather@onekey.so>
2026-02-05 11:16:06 +0800

    fix: douplicate success toast

--- packages/kit/src/views/BulkSend/pages/BulkSendReview/BulkSendReview.tsx
+++ packages/kit/src/views/BulkSend/pages/BulkSendReview/BulkSendReview.tsx
@@ -86,8 +86,8 @@ function BaseBulkSendReview({
   const confirmButtonText =
     approvesInfo.length > 0
       ? intl.formatMessage({
-          id: ETranslations.wallet_bulk_send_btn_approve_and_confirm,
-        })
+        id: ETranslations.wallet_bulk_send_btn_approve_and_confirm,
+      })
       : intl.formatMessage({ id: ETranslations.wallet_bulk_send_btn_confirm });
 
   // Handle editing approval amount
@@ -341,13 +341,6 @@ function BaseBulkSendReview({
           newUnsignedTxs,
           feeState.feeInfos,
         );
-
-        // Show success toast
-        Toast.success({
-          title: intl.formatMessage({
-            id: ETranslations.feedback_transaction_submitted,
-          }),
-        });
 
         setIsSubmitting(false);
         onSuccess?.(results);
