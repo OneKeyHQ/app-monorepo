f44912e82f741d430f7d82ee2ee8014c47104f3a
huhuanming <huanming@onekey.so>
2026-02-06 22:02:01 +0800

    fix: fix tsc-staged type and lint errors

--- packages/shared/src/config/appConfig.ts
+++ packages/shared/src/config/appConfig.ts
@@ -123,8 +123,7 @@ export const ONEKEY_HEALTH_CHECK_URL = '/wallet/v1/health';
 
 export const SUPPORT_URL = 'https://help.onekey.so/hc/requests/new';
 
-export const SWAP_FAQ_HELP_URL =
-  'https://help.onekey.so/articles/13608266';
+export const SWAP_FAQ_HELP_URL = 'https://help.onekey.so/articles/13608266';
 
 export const HYPERLIQUID_EXPLORER_URL = 'https://hypurrscan.io/address/';
 
