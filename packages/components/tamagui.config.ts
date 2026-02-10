c5f3d903753a0b9342986d26f66dc8b6caf5d4cd
huhuanming <huanming@onekey.so>
2026-02-10 15:49:25 +0800

    Update tamagui.config.ts

--- packages/components/tamagui.config.ts
+++ packages/components/tamagui.config.ts
@@ -153,14 +153,16 @@ const tamaguiWebFontFamily = webFontFamily;
 
 const font = createFont({
   family: isTamaguiNative ? 'Roobert-Regular' : tamaguiWebFontFamily,
-  face: isTamaguiNative
+  ...(isTamaguiNative
     ? {
-        400: { normal: 'Roobert-Regular' },
-        500: { normal: 'Roobert-Medium' },
-        600: { normal: 'Roobert-SemiBold' },
-        700: { normal: 'Roobert-Bold' },
+        face: {
+          400: { normal: 'Roobert-Regular' },
+          500: { normal: 'Roobert-Medium' },
+          600: { normal: 'Roobert-SemiBold' },
+          700: { normal: 'Roobert-Bold' },
+        },
       }
-    : undefined,
+    : {}),
   ...basicFontVariants,
 });
 
