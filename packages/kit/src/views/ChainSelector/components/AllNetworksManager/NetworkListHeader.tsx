20e1eb07edf2f29a0ffe66ebdc9c898202dc3550
weatherstar <weather@onekey.so>
2026-02-12 01:33:47 +0800

    fix: constrain ChainSelectorTooltip trigger area to text width

--- packages/kit/src/views/ChainSelector/components/AllNetworksManager/NetworkListHeader.tsx
+++ packages/kit/src/views/ChainSelector/components/AllNetworksManager/NetworkListHeader.tsx
@@ -83,13 +83,13 @@ function NetworkListHeader() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Stack flex={1} mr="$2">
+          <Stack flex={1} mr="$2" alignItems="flex-start">
             <ChainSelectorTooltip
               renderContent={intl.formatMessage({
                 id: ETranslations.network_selection_performance_tip,
               })}
               renderTrigger={
-                <Stack alignSelf="flex-start">
+                <Stack>
                   <SizableText size="$bodyLgMedium">
                     {intl.formatMessage(
                       {
