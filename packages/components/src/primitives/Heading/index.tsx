8a0e203cfee76de655cf9f0b8eb2fa0fd2b97614
huhuanming <huanming@onekey.so>
2026-02-12 22:43:26 +0800

    fix: format Heading component JSX

--- packages/components/src/primitives/Heading/index.tsx
+++ packages/components/src/primitives/Heading/index.tsx
@@ -4,7 +4,11 @@ import { type HeadingProps } from '@onekeyhq/components/src/shared/tamagui';
 
 export function Heading(props: HeadingProps) {
   return (
-    <TamaguiHeading {...props} allowFontScaling={false} maxFontSizeMultiplier={1} />
+    <TamaguiHeading
+      {...props}
+      allowFontScaling={false}
+      maxFontSizeMultiplier={1}
+    />
   );
 }
 
