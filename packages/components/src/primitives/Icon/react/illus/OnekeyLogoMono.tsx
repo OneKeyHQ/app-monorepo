49f3d94bd84992527f19287489d1a49d46689904
yikZero <i@yikzero.com>
2026-02-12 19:16:02 +0800

    feat: update tablet split view empty state with mono logo and subdued background (#10205)
    Co-authored-by: Leon <lixiao.dev@gmail.com>

---
+++ packages/components/src/primitives/Icon/react/illus/OnekeyLogoMono.tsx
@@ -0,0 +1,17 @@
+import Svg, { Path } from 'react-native-svg';
+import type { SvgProps } from 'react-native-svg';
+const SvgOnekeyLogoMono = (props: SvgProps) => (
+  <Svg fill="none" viewBox="0 0 280 280" accessibilityRole="image" {...props}>
+    <Path
+      fill="currentColor"
+      d="M140.001 151.965c13.394 0 24.253 10.858 24.253 24.253s-10.859 24.252-24.253 24.252-24.253-10.858-24.253-24.252c0-13.395 10.859-24.253 24.253-24.253"
+    />
+    <Path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M140 0c96.65 0 140 43.35 140 140s-43.35 140-140 140S0 236.65 0 140 43.35 0 140 0m.001 131.8c-24.531 0-44.417 19.887-44.417 44.418s19.886 44.417 44.417 44.417 44.418-19.886 44.418-44.417-19.887-44.418-44.418-44.418m-26.299-72.435-6.833 20.661h21.633v43.52h24.147v-64.18z"
+      clipRule="evenodd"
+    />
+  </Svg>
+);
+export default SvgOnekeyLogoMono;
