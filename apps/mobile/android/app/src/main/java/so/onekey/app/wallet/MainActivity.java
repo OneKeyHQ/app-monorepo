c3b2aed77e077f984572d7e0ba9ed8352b3eeb85
huhuanming <huanming@onekey.so>
2026-03-03 17:56:58 +0800

    refactor: replace react-native-file-logger with @onekeyfe/react-native-native-logger

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainActivity.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainActivity.java
@@ -8,12 +8,10 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.betomorrow.rnfilelogger.FileLoggerModule;
+import com.margelo.nitro.nativelogger.OneKeyLog;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
@@ -22,8 +20,6 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import expo.modules.ReactActivityDelegateWrapper;
@@ -35,8 +31,6 @@ import so.onekey.app.wallet.splashscreen.SplashScreenViewController;
 import so.onekey.app.wallet.splashscreen.singletons.SplashScreen;
 
 public class MainActivity extends ReactActivity {
-    private FileLoggerModule fileLogger;
-    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
     private SplashScreenImageResizeMode getResizeMode(Context context) {
     String resizeModeString = context.getString(R.string.expo_splash_screen_resize_mode).toLowerCase();
     SplashScreenImageResizeMode mode = SplashScreenImageResizeMode.fromString(resizeModeString);
@@ -101,7 +95,6 @@ public class MainActivity extends ReactActivity {
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
     sharedI18nUtilInstance.allowRTL(getApplicationContext(), true);
     EventBus.getDefault().register(this);
-    fileLogger = new FileLoggerModule((ReactApplicationContext) getReactHost().getCurrentReactContext());
   }
 
   @Override
@@ -122,8 +115,7 @@ public class MainActivity extends ReactActivity {
     public void onLogEvent(Object event)
     {
         List<String> messages = (List<String>) event;
-        String currentTime = sdf.format(new Date());
-        fileLogger.write(1, currentTime + " | INFO : app => native => " + messages.get(0) + ": " + messages.get(1));
+        OneKeyLog.info("App", "native => " + messages.get(0) + ": " + messages.get(1));
     };
 
 
