1153d8a2d9f2ad83cc46574ff6141d021448cb16
huhuanming <huanming@onekey.so>
2026-03-04 17:58:27 +0800

    fix: fix lint

--- development/webpack/webpack.base.config.js
+++ development/webpack/webpack.base.config.js
@@ -110,10 +110,10 @@ const basePlugins = [
         PERF_FUNCTION_WARN_MS: JSON.stringify(
           process.env.PERF_FUNCTION_WARN_MS || '',
         ),
-        ONEKEY_ALLOW_SKIP_GPG_VERIFICATION:
-          process.env.ONEKEY_ALLOW_SKIP_GPG_VERIFICATION
-            ? process.env.ONEKEY_ALLOW_SKIP_GPG_VERIFICATION === 'true'
-            : process.env.NODE_ENV !== 'production',
+        ONEKEY_ALLOW_SKIP_GPG_VERIFICATION: process.env
+          .ONEKEY_ALLOW_SKIP_GPG_VERIFICATION
+          ? process.env.ONEKEY_ALLOW_SKIP_GPG_VERIFICATION === 'true'
+          : process.env.NODE_ENV !== 'production',
       },
     },
   }),
