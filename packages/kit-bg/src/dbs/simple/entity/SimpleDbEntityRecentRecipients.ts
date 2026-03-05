f91969122b8b384b99d90ce9bba2e42e599f961b
Patrick Choo <patrick.choo@outlook.com>
2026-03-05 23:02:32 +0800

    fix: prettier formatting for SimpleDbEntityRecentRecipients

--- packages/kit-bg/src/dbs/simple/entity/SimpleDbEntityRecentRecipients.ts
+++ packages/kit-bg/src/dbs/simple/entity/SimpleDbEntityRecentRecipients.ts
@@ -65,9 +65,9 @@ export class SimpleDbEntityRecentRecipients extends SimpleDbEntityBase<IRecentRe
       const migratedRecipients: Record<
         string,
         Record<string, IRecentRecipientData>
-      > = { ...(currentData?.recentRecipients ?? {}) };
+      > = { ...currentData?.recentRecipients };
       const evmRecipients: Record<string, IRecentRecipientData> = {
-        ...(migratedRecipients.evm ?? {}),
+        ...migratedRecipients.evm,
       };
 
       for (const [storageKey, recipients] of Object.entries(
