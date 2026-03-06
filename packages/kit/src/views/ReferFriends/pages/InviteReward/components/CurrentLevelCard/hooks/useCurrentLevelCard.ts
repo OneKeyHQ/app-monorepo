14579b89c1072390ecb3fc8d77f4d1ef9f21a4a0
0xchloe996 <ziying@onekey.so>
2026-03-06 15:55:38 +0800

    fix: resolve CI lint prettier formatting error

--- packages/kit/src/views/ReferFriends/pages/InviteReward/components/CurrentLevelCard/hooks/useCurrentLevelCard.ts
+++ packages/kit/src/views/ReferFriends/pages/InviteReward/components/CurrentLevelCard/hooks/useCurrentLevelCard.ts
@@ -92,10 +92,7 @@ export function useCurrentLevelCard(
             rate: {
               you: rate.rebate,
               invitee: rate.discount,
-              label:
-                rate.commissionRatesLabel ||
-                rate.label ||
-                `Rate ${index + 1}`,
+              label: rate.commissionRatesLabel || rate.label || `Rate ${index + 1}`,
             },
           }));
       } else {
