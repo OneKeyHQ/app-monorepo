d3be03af7dab075082c462cb7e8622f7cfee4fbb
huhuanming <huanming@onekey.so>
2026-03-12 18:28:40 +0800

    Update env.js

--- development/env.js
+++ development/env.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   process.env.BUILD_NUMBER =
     process.env.BUILD_NUMBER || `${dateFns.format(Date.now(), 'MMddHHmm')}-dev`;
-  process.env.BUNDLE_VERSION = process.env.BUNDLE_VERSION || '100000';
+  process.env.BUNDLE_VERSION = process.env.BUNDLE_VERSION || '1000000';
 }
 
 process.env.BUILD_TIME = Date.now();
