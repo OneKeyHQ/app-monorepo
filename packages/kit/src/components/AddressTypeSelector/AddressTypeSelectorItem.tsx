cab84ecc2293f35e8edebe0b5df5c5edfda9a688
Franco <franco@onekey.so>
2026-03-14 22:06:12 +0800

    feat: add tooltip for BTC address type descriptions(OK-42825)
    Add per-item tooltip to BTC address type selector showing user-friendly
    descriptions for Taproot, Native SegWit, Nested SegWit, and Legacy formats.
    Uses Tooltip on web/desktop and Popover on native for platform adaptation.

--- packages/kit/src/components/AddressTypeSelector/AddressTypeSelectorItem.tsx
+++ packages/kit/src/components/AddressTypeSelector/AddressTypeSelectorItem.tsx
@@ -1,13 +1,24 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
 import { useIntl } from 'react-intl';
 
+import {
+  Icon,
+  Popover,
+  SizableText,
+  Stack,
+  Tooltip,
+  XStack,
+  YStack,
+} from '@onekeyhq/components';
+import { EAddressEncodings } from '@onekeyhq/core/src/types';
 import type { IDBUtxoAccount } from '@onekeyhq/kit-bg/src/dbs/local/types';
 import type {
   IAccountDeriveInfo,
   IAccountDeriveTypes,
 } from '@onekeyhq/kit-bg/src/vaults/types';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import accountUtils from '@onekeyhq/shared/src/utils/accountUtils';
 import type { INetworkAccount } from '@onekeyhq/shared/types/account';
 
@@ -16,6 +27,44 @@ import { ListItem } from '../ListItem';
 import AddressTypeCheckMark from './AddressTypeCheckMark';
 import AddressTypeFiat from './AddressTypeFiat';
 import { useAddressTypeSelectorDynamicContext } from './AddressTypeSelectorContext';
+
+const addressTypeTooltipMap: Partial<Record<EAddressEncodings, ETranslations>> =
+  {
+    [EAddressEncodings.P2TR]: ETranslations.address_type_tooltip_taproot__desc,
+    [EAddressEncodings.P2WPKH]:
+      ETranslations.address_type_tooltip_native_segwit__desc,
+    [EAddressEncodings.P2SH_P2WPKH]:
+      ETranslations.address_type_tooltip_nested_segwit__desc,
+    [EAddressEncodings.P2PKH]: ETranslations.address_type_tooltip_legacy__desc,
+  };
+
+function AddressTypeTooltip({ tooltipText }: { tooltipText: string }) {
+  const icon = <Icon name="InfoCircleOutline" size="$4" color="$iconSubdued" />;
+
+  if (platformEnv.isNative) {
+    return (
+      <Popover
+        title=""
+        showHeader={false}
+        placement="top"
+        renderTrigger={<Stack p="$1">{icon}</Stack>}
+        renderContent={
+          <YStack p="$5">
+            <SizableText size="$bodyMd">{tooltipText}</SizableText>
+          </YStack>
+        }
+      />
+    );
+  }
+
+  return (
+    <Tooltip
+      placement="top"
+      renderTrigger={<Stack px="$1">{icon}</Stack>}
+      renderContent={tooltipText}
+    />
+  );
+}
 
 type IProps = {
   data: {
@@ -36,6 +85,54 @@ function AddressTypeSelectorItem(props: IProps) {
   const intl = useIntl();
   const { isCreatingAddress } = useAddressTypeSelectorDynamicContext();
 
+  const tooltipI18nKey = deriveInfo.addressEncoding
+    ? addressTypeTooltipMap[deriveInfo.addressEncoding]
+    : undefined;
+
+  const tooltipText = tooltipI18nKey
+    ? intl.formatMessage({ id: tooltipI18nKey })
+    : undefined;
+
+  const titleText = deriveInfo.labelKey
+    ? intl.formatMessage({ id: deriveInfo.labelKey })
+    : deriveInfo.label;
+
+  const subtitleText = account
+    ? accountUtils.shortenAddress({
+        address: account.addressDetail.displayAddress,
+      })
+    : intl.formatMessage({ id: ETranslations.global_create_address });
+
+  const renderItemText = useMemo(
+    () => (
+      <YStack flex={1} userSelect="none">
+        <XStack alignItems="center" pb="$0.5">
+          <SizableText
+            size="$bodyMdMedium"
+            $gtMd={{
+              size: '$bodySmMedium',
+            }}
+          >
+            {titleText}
+          </SizableText>
+          {tooltipText ? (
+            <AddressTypeTooltip tooltipText={tooltipText} />
+          ) : null}
+        </XStack>
+        <SizableText
+          size="$bodyMd"
+          color="$textSubdued"
+          $gtMd={{
+            size: '$bodySm',
+          }}
+        >
+          {subtitleText}
+        </SizableText>
+      </YStack>
+    ),
+    [titleText, tooltipText, subtitleText],
+  );
+
   return (
     <ListItem
       disabled={isCreatingAddress}
@@ -44,31 +141,7 @@ function AddressTypeSelectorItem(props: IProps) {
       mx="$0"
       px="$2"
       py="$1"
-      title={
-        deriveInfo.labelKey
-          ? intl.formatMessage({ id: deriveInfo.labelKey })
-          : deriveInfo.label
-      }
-      titleProps={{
-        size: '$bodyMdMedium',
-        $gtMd: {
-          size: '$bodySmMedium',
-        },
-        pb: '$0.5',
-      }}
-      subtitle={
-        account
-          ? accountUtils.shortenAddress({
-              address: account.addressDetail.displayAddress,
-            })
-          : intl.formatMessage({ id: ETranslations.global_create_address })
-      }
-      subtitleProps={{
-        size: '$bodyMd',
-        $gtMd: {
-          size: '$bodySm',
-        },
-      }}
+      renderItemText={renderItemText}
       childrenBefore={
         <AddressTypeCheckMark accountId={account?.id} deriveType={deriveType} />
       }
