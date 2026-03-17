2e28c2558d12be8202fee6a01ceb92ab0cf7d1c3
Zen <mingzhen.fang@onekey.so>
2026-03-17 10:41:01 +0800

    fix: prettier formatting for PerpTokenSelectorRow

--- packages/kit/src/views/Perp/components/TokenSelector/PerpTokenSelectorRow.tsx
+++ packages/kit/src/views/Perp/components/TokenSelector/PerpTokenSelectorRow.tsx
@@ -226,40 +226,40 @@ const TokenInfoCellDesktop = memo(() => {
             flex={1}
             minWidth={0}
           >
-          <Token
-            size="xs"
-            borderRadius="$full"
-            tokenImageUri={getHyperliquidTokenImageUrl(token.displayName)}
-            fallbackIcon="CryptoCoinOutline"
-          />
-          <SizableText size="$bodySmMedium" numberOfLines={1} flexShrink={1}>
-            {token.displayName}
-          </SizableText>
-          <XStack gap="$1" minWidth={0}>
-            <XStack
-              borderRadius="$1"
-              bg="$bgStrong"
-              justifyContent="center"
-              alignItems="center"
-              px="$1.5"
-            >
-              <SizableText
-                fontSize={10}
-                alignSelf="center"
-                color="$textSubdued"
-                lineHeight={16}
+            <Token
+              size="xs"
+              borderRadius="$full"
+              tokenImageUri={getHyperliquidTokenImageUrl(token.displayName)}
+              fallbackIcon="CryptoCoinOutline"
+            />
+            <SizableText size="$bodySmMedium" numberOfLines={1} flexShrink={1}>
+              {token.displayName}
+            </SizableText>
+            <XStack gap="$1" minWidth={0}>
+              <XStack
+                borderRadius="$1"
+                bg="$bgStrong"
+                justifyContent="center"
+                alignItems="center"
+                px="$1.5"
               >
-                {token.maxLeverage}x
-              </SizableText>
+                <SizableText
+                  fontSize={10}
+                  alignSelf="center"
+                  color="$textSubdued"
+                  lineHeight={16}
+                >
+                  {token.maxLeverage}x
+                </SizableText>
+              </XStack>
+              {token.subtitle && gtLg ? (
+                <SubtitleBadge
+                  subtitle={token.subtitle}
+                  maxWidth={DESKTOP_SUBTITLE_MAX_WIDTH}
+                  withTooltip
+                />
+              ) : null}
             </XStack>
-            {token.subtitle && gtLg ? (
-              <SubtitleBadge
-                subtitle={token.subtitle}
-                maxWidth={DESKTOP_SUBTITLE_MAX_WIDTH}
-                withTooltip
-              />
-            ) : null}
-          </XStack>
           </XStack>
         </XStack>
       </DebugRenderTracker>
