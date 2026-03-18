952675e44bd94d16a9f803d27e49882d2b64f4ee
huhuanming <huanming@onekey.so>
2026-03-18 17:46:18 +0800

    Update MainActivity.java

--- apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainActivity.java
+++ apps/mobile/android/app/src/main/java/so/onekey/app/wallet/MainActivity.java
@@ -3,6 +3,7 @@ package so.onekey.app.wallet;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
