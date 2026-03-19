27ffc16cf2978a811a167f72a4432d4910dadf2e
Leon <lixiao.dev@gmail.com>
2026-03-19 23:27:06 +0800

    chore: revert background color

--- packages/kit/src/views/Receive/pages/ReceiveToken.tsx
+++ packages/kit/src/views/Receive/pages/ReceiveToken.tsx
@@ -874,7 +874,7 @@ function ReceiveToken() {
       <Page.Header
         title={intl.formatMessage({ id: ETranslations.global_receive })}
       />
-      <Page.Body flex={1} pb="$5" px="$5" bg="$bgCriticalStrong">
+      <Page.Body flex={1} pb="$5" px="$5">
         {renderReceiveQrCode()}
         <YStack gap="$2">
           {banner && shouldShowQRCode && !isBtcUsedAddressVerifyMode ? (
