02b702bff6560b21583966f4bf8ad74946c2cc16
morizon <sidmorizon@outlook.com>
2026-03-23 22:49:36 +0800

    fix(Dialog): remove TypeScript error suppression for trapFocus prop
    - Removed the TypeScript error suppression comment for the trapFocus prop in the DialogFrame component, ensuring better type safety and code clarity.

--- packages/components/src/composite/Dialog/index.tsx
+++ packages/components/src/composite/Dialog/index.tsx
@@ -362,7 +362,6 @@ function DialogFrame({
             <TMDialog.Title display="none" />
             <TMDialog.Content
               elevate
-              // @ts-expect-error
               trapFocus={effectiveTrapFocus}
               onEscapeKeyDown={handleEscapeKeyDown as any}
               key="content"
