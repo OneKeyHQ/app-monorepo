8046308e4b1821a8e0936b71cee3782f664968c4
Patrick Choo <patrick.choo@outlook.com>
2026-03-26 15:08:53 +0800

    fix: add missing useMemo dependencies for lint
    Add saveDeselectionToCache and restoreDeselectionFromCache to
    headerRight useMemo dependency array (both have stable [] deps).

--- packages/kit/src/views/AccountManagerStacks/pages/BatchCreateAccount/BatchCreateAccountPreview.tsx
+++ packages/kit/src/views/AccountManagerStacks/pages/BatchCreateAccount/BatchCreateAccountPreview.tsx
@@ -624,6 +624,8 @@ function BatchCreateAccountPreviewPage({
       networkId,
       showPopoverDeriveTypeInfo,
       networkIdsCompatibleAccount,
+      saveDeselectionToCache,
+      restoreDeselectionFromCache,
     ],
   );
 
