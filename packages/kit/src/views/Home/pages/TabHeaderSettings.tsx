271ddb9012b954709957740782b3ff891a246707
weatherstar <weather@onekey.so>
2026-03-31 16:24:44 +0800

    feat(home): handle export history action

--- packages/kit/src/views/Home/pages/TabHeaderSettings.tsx
+++ packages/kit/src/views/Home/pages/TabHeaderSettings.tsx
@@ -1,3 +1,4 @@
+import type { ReactNode } from 'react';
 import { memo, useCallback, useMemo } from 'react';
 
 import { useIntl } from 'react-intl';
@@ -12,8 +13,10 @@ import {
   Switch,
   XStack,
   useMedia,
+  usePopoverContext,
 } from '@onekeyhq/components';
 import { useOneKeyAuth } from '@onekeyhq/kit/src/components/OneKeyAuth/useOneKeyAuth';
+import useAppNavigation from '@onekeyhq/kit/src/hooks/useAppNavigation';
 import { useSettingsPersistAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 import { getNetworksSupportFilterScamHistory } from '@onekeyhq/shared/src/config/presetNetworks';
 import {
@@ -21,6 +24,9 @@ import {
   appEventBus,
 } from '@onekeyhq/shared/src/eventBus/appEventBus';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import { EModalRoutes } from '@onekeyhq/shared/src/routes';
+import { EPrimeFeatures, EPrimePages } from '@onekeyhq/shared/src/routes/prime';
+import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
 
 import { ListItem } from '../../../components/ListItem';
 import { useManageToken } from '../../../hooks/useManageToken';
@@ -69,11 +75,112 @@ const filterScamHistorySupportedNetworkIds = new Set(
   filterScamHistorySupportedNetworks.map((n) => n.id),
 );
 
+function TxHistorySettingsContent({
+  filterScamHistorySupported,
+  networkName,
+  isFilterScamHistoryEnabled,
+  isFilterLowValueHistoryEnabled,
+  onFilterScamHistoryChange,
+  onFilterLowValueHistoryChange,
+  exportHistoryText,
+  exportHistoryTitle,
+  isPrimeUser,
+}: {
+  filterScamHistorySupported: boolean;
+  networkName: string;
+  isFilterScamHistoryEnabled: boolean;
+  isFilterLowValueHistoryEnabled: boolean;
+  onFilterScamHistoryChange: (value: boolean) => void;
+  onFilterLowValueHistoryChange: (value: boolean) => void;
+  exportHistoryText: string;
+  exportHistoryTitle: ReactNode;
+  isPrimeUser: boolean;
+}) {
+  const intl = useIntl();
+  const navigation = useAppNavigation();
+  const { closePopover } = usePopoverContext();
+
+  const handleExportHistoryPress = useCallback(async () => {
+    await closePopover?.();
+
+    if (isPrimeUser) {
+      return;
+    }
+
+    await timerUtils.wait(150);
+
+    navigation.pushModal(EModalRoutes.PrimeModal, {
+      screen: EPrimePages.PrimeDashboard,
+      params: {
+        fromFeature: EPrimeFeatures.HistoryExport,
+      },
+    });
+  }, [closePopover, isPrimeUser, navigation]);
+
+  return (
+    <Stack py="$2">
+      <ListItem
+        title={intl.formatMessage({
+          id: ETranslations.wallet_history_settings_hide_risk_transaction_title,
+        })}
+        subtitle={
+          filterScamHistorySupported
+            ? intl.formatMessage({
+                id: ETranslations.wallet_history_settings_hide_risk_transaction_desc,
+              })
+            : intl.formatMessage(
+                {
+                  id: ETranslations.wallet_history_settings_hide_risk_transaction_desc_unsupported,
+                },
+                {
+                  networkName,
+                },
+              )
+        }
+      >
+        <Switch
+          isUncontrolled
+          disabled={!filterScamHistorySupported}
+          size={ESwitchSize.small}
+          onChange={onFilterScamHistoryChange}
+          defaultChecked={
+            filterScamHistorySupported ? isFilterScamHistoryEnabled : false
+          }
+        />
+      </ListItem>
+      <ListItem
+        title={intl.formatMessage({
+          id: ETranslations.wallet_history_settings_hide_small_transaction_title,
+        })}
+        subtitle={intl.formatMessage({
+          id: ETranslations.wallet_history_settings_hide_small_transaction_desc,
+        })}
+      >
+        <Switch
+          isUncontrolled
+          size={ESwitchSize.small}
+          onChange={onFilterLowValueHistoryChange}
+          defaultChecked={isFilterLowValueHistoryEnabled}
+        />
+      </ListItem>
+      <ListItem onPress={handleExportHistoryPress} drillIn>
+        <ListItem.Text
+          flex={1}
+          primary={exportHistoryTitle}
+          secondary={exportHistoryText}
+        />
+      </ListItem>
+    </Stack>
+  );
+}
+
 function TxHistorySettings() {
   const intl = useIntl();
   const [settings, setSettings] = useSettingsPersistAtom();
   const { user } = useOneKeyAuth();
-  const isPrimeUser = user?.primeSubscription?.isActive && user?.onekeyUserId;
+  const isPrimeUser = !!(
+    user?.primeSubscription?.isActive && user?.onekeyUserId
+  );
   const exportHistoryText = useMemo(
     () =>
       intl.formatMessage({
@@ -121,8 +228,6 @@ function TxHistorySettings() {
     [setSettings],
   );
 
-  const handleExportHistoryPress = useCallback(() => undefined, []);
-
   const {
     activeAccount: { network },
   } = useActiveAccount({ num: 0 });
@@ -148,61 +253,19 @@ function TxHistorySettings() {
           />
         }
         renderContent={
-          <Stack py="$2">
-            <ListItem
-              title={intl.formatMessage({
-                id: ETranslations.wallet_history_settings_hide_risk_transaction_title,
-              })}
-              subtitle={
-                filterScamHistorySupported
-                  ? intl.formatMessage({
-                      id: ETranslations.wallet_history_settings_hide_risk_transaction_desc,
-                    })
-                  : intl.formatMessage(
-                      {
-                        id: ETranslations.wallet_history_settings_hide_risk_transaction_desc_unsupported,
-                      },
-                      {
-                        networkName: network?.name ?? '',
-                      },
-                    )
-              }
-            >
-              <Switch
-                isUncontrolled
-                disabled={!filterScamHistorySupported}
-                size={ESwitchSize.small}
-                onChange={handleFilterScamHistoryOnChange}
-                defaultChecked={
-                  filterScamHistorySupported
-                    ? settings.isFilterScamHistoryEnabled
-                    : false
-                }
-              />
-            </ListItem>
-            <ListItem
-              title={intl.formatMessage({
-                id: ETranslations.wallet_history_settings_hide_small_transaction_title,
-              })}
-              subtitle={intl.formatMessage({
-                id: ETranslations.wallet_history_settings_hide_small_transaction_desc,
-              })}
-            >
-              <Switch
-                isUncontrolled
-                size={ESwitchSize.small}
-                onChange={handleFilterLowValueHistoryOnChange}
-                defaultChecked={settings.isFilterLowValueHistoryEnabled}
-              />
-            </ListItem>
-            <ListItem onPress={handleExportHistoryPress} drillIn>
-              <ListItem.Text
-                flex={1}
-                primary={exportHistoryTitle}
-                secondary={exportHistoryText}
-              />
-            </ListItem>
-          </Stack>
+          <TxHistorySettingsContent
+            filterScamHistorySupported={!!filterScamHistorySupported}
+            networkName={network?.name ?? ''}
+            isFilterScamHistoryEnabled={!!settings.isFilterScamHistoryEnabled}
+            isFilterLowValueHistoryEnabled={
+              !!settings.isFilterLowValueHistoryEnabled
+            }
+            onFilterScamHistoryChange={handleFilterScamHistoryOnChange}
+            onFilterLowValueHistoryChange={handleFilterLowValueHistoryOnChange}
+            exportHistoryText={exportHistoryText}
+            exportHistoryTitle={exportHistoryTitle}
+            isPrimeUser={isPrimeUser}
+          />
         }
       />
     </Stack>
