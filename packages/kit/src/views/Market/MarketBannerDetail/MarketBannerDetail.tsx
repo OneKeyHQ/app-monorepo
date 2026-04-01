11447b2c07fa9e80ddcf3ad36f21538e2658509e
haicongliao <haicongliao@gmail.com>
2026-04-01 14:57:19 +0800

    fix(market): add missing tabBarHeight and intl to useMemo deps

--- packages/kit/src/views/Market/MarketBannerDetail/MarketBannerDetail.tsx
+++ packages/kit/src/views/Market/MarketBannerDetail/MarketBannerDetail.tsx
@@ -299,6 +299,8 @@ function MarketBannerDetailContent({ title }: { title: string }) {
     transformedData,
     renderBannerItem,
     bannerKeyExtractor,
+    tabBarHeight,
+    intl,
   ]);
 
   return (
