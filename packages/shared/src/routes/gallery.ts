9fbab18616a4e3280276e96c37ce5176a7275046
huhuanming <huanming@onekey.so>
2026-04-02 21:14:20 +0800

    Update gallery.ts

--- packages/shared/src/routes/gallery.ts
+++ packages/shared/src/routes/gallery.ts
@@ -71,7 +71,6 @@ export enum EGalleryRoutes {
   ComponentSecureQRToast = 'component-SecureQrToast',
   ComponentSegmentControl = 'component-SegmentControl',
   ComponentSelect = 'component-Select',
-  ComponentSplitBundle = 'component-SplitBundle',
   ComponentPagination = 'component-Pagination',
   ComponentSend = 'component-Send',
   ComponentShortcut = 'component-Shortcut',
