a543f8bc2ac99289161d1754e27cbe684427d662
weatherstar <weather@onekey.so>
2026-04-09 17:35:01 +0800

    fix: disable lnurl pay continue when amount is empty

--- packages/kit/src/views/LightningNetwork/pages/Send/LnurlPayRequestModal.tsx
+++ packages/kit/src/views/LightningNetwork/pages/Send/LnurlPayRequestModal.tsx
@@ -93,6 +93,8 @@ function LnurlPayRequestModal() {
       comment: '',
     },
   });
+  const amountValue = useFormReturn.watch('amount');
+  const isAmountEmpty = useMemo(() => !`${amountValue ?? ''}`.trim(), [amountValue]);
 
   const commentAllowedLength = useMemo(() => {
     if (
@@ -281,7 +283,7 @@ function LnurlPayRequestModal() {
             }}
             confirmButtonProps={{
               loading: isLoading,
-              disabled: !continueOperate,
+              disabled: !continueOperate || isAmountEmpty,
             }}
             showContinueOperateCheckbox={showContinueOperate}
             riskLevel={riskLevel}
