17ec4a1f036c027fa2a790928cc15bbad7455b4d
huhuanming <huanming@onekey.so>
2026-04-09 22:12:30 +0800

    Update jotaiStorage.test.ts

--- packages/kit-bg/src/states/jotai/jotaiStorage.test.ts
+++ packages/kit-bg/src/states/jotai/jotaiStorage.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return, onekey/no-raw-error */
 /**
  * Unit tests for JotaiStorageNativeMMKV migration-aware read/write logic.
  */
