9bf7577420c59560183f1f179e3962e67a77d0eb
limichange <limichange@hotmail.com>
2026-04-24 17:51:39 +0800

    fix: avoid duplicate category token fetch

--- packages/kit/src/views/Home/components/PopularTrading/useHomeMarketCategoryTokens.ts
+++ packages/kit/src/views/Home/components/PopularTrading/useHomeMarketCategoryTokens.ts
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-
 import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
 import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
 import timerUtils from '@onekeyhq/shared/src/utils/timerUtils';
@@ -9,6 +7,10 @@ import { EMPTY_DISPLAY_TOKENS, mapMarketTokenToDisplay } from './utils';
 
 import type { IFavoriteTokenDisplay } from './types';
 
+const HOME_MARKET_CATEGORY_POLLING_INTERVAL = timerUtils.getTimeDurationMs({
+  seconds: 30,
+});
+
 function useHomeMarketCategoryTokens({
   minLiquidity,
   selectedMarketCategoryId,
@@ -16,51 +18,40 @@ function useHomeMarketCategoryTokens({
   minLiquidity: number;
   selectedMarketCategoryId?: string;
 }) {
-  const {
-    result: categoryTokensResult,
-    isLoading: isCategoryLoading,
-    run: refreshCategoryTokens,
-  } = usePromiseResult(
-    async () => {
-      if (!selectedMarketCategoryId) {
-        return [];
-      }
+  const { result: categoryTokensResult, isLoading: isCategoryLoading } =
+    usePromiseResult(
+      async () => {
+        if (!selectedMarketCategoryId) {
+          return [];
+        }
 
-      const response =
-        await backgroundApiProxy.serviceMarketV2.fetchMarketTokenList({
-          networkId: '',
-          sortBy: 'v24hUSD',
-          sortType: 'desc',
-          page: 1,
-          limit: HOME_MARKET_CATEGORY_REQUEST_LIMIT,
-          minLiquidity,
-          type: selectedMarketCategoryId,
-          timeFrame: '2',
-        });
+        const response =
+          await backgroundApiProxy.serviceMarketV2.fetchMarketTokenList({
+            networkId: '',
+            sortBy: 'v24hUSD',
+            sortType: 'desc',
+            page: 1,
+            limit: HOME_MARKET_CATEGORY_REQUEST_LIMIT,
+            minLiquidity,
+            type: selectedMarketCategoryId,
+            timeFrame: '2',
+          });
 
-      return response.list
-        .map(mapMarketTokenToDisplay)
-        .filter((item): item is IFavoriteTokenDisplay => item !== null)
-        .slice(0, HOME_MARKET_CATEGORY_REQUEST_LIMIT);
-    },
-    [minLiquidity, selectedMarketCategoryId],
-    {
-      initResult: [],
-      watchLoading: true,
-      pollingInterval: selectedMarketCategoryId
-        ? timerUtils.getTimeDurationMs({ seconds: 30 })
-        : undefined,
-      revalidateOnFocus: true,
-      revalidateOnReconnect: true,
-      undefinedResultIfReRun: true,
-    },
-  );
-
-  useEffect(() => {
-    if (selectedMarketCategoryId) {
-      void refreshCategoryTokens();
-    }
-  }, [refreshCategoryTokens, selectedMarketCategoryId]);
+        return response.list
+          .map(mapMarketTokenToDisplay)
+          .filter((item): item is IFavoriteTokenDisplay => item !== null)
+          .slice(0, HOME_MARKET_CATEGORY_REQUEST_LIMIT);
+      },
+      [minLiquidity, selectedMarketCategoryId],
+      {
+        initResult: [],
+        watchLoading: true,
+        pollingInterval: HOME_MARKET_CATEGORY_POLLING_INTERVAL,
+        revalidateOnFocus: true,
+        revalidateOnReconnect: true,
+        undefinedResultIfReRun: true,
+      },
+    );
 
   return {
     categoryTokens: categoryTokensResult ?? EMPTY_DISPLAY_TOKENS,
