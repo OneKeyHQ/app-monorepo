7f9e668db62af1e2776ff9008f9cecd055655cf3
Zhao <Charon.Dian@gmail.com>
2026-04-29 10:42:54 +0800

    fix: update provider fee content i18n (#11402)
    Co-authored-by: morizon <sidmorizon@outlook.com>

--- packages/kit/src/views/Swap/components/SwapServiceFeeOverview.tsx
+++ packages/kit/src/views/Swap/components/SwapServiceFeeOverview.tsx
@@ -1,23 +1,13 @@
-import BigNumber from 'bignumber.js';
 import { useIntl } from 'react-intl';
 
 import { Icon, Popover, SizableText, Stack } from '@onekeyhq/components';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
-import { swapServiceFeeDefault } from '@onekeyhq/shared/types/swap/SwapProvider.constants';
 
-export function SwapServiceFeeOverview({
-  percentageFee,
-  percentOriginFee,
-}: {
+export function SwapServiceFeeOverview(_props: {
   percentageFee?: number;
   percentOriginFee?: number;
 }) {
   const intl = useIntl();
-  const displayFee =
-    typeof percentageFee === 'number' &&
-    new BigNumber(percentageFee).lt(percentOriginFee ?? swapServiceFeeDefault)
-      ? (percentOriginFee ?? swapServiceFeeDefault)
-      : (percentageFee ?? swapServiceFeeDefault);
   return (
     <Popover
       title={intl.formatMessage({
@@ -34,14 +24,9 @@ export function SwapServiceFeeOverview({
       renderContent={
         <Stack gap="$1" p="$4">
           <SizableText size="$bodyMd" color="$textSubdued">
-            {intl.formatMessage(
-              {
-                id: ETranslations.provider_popover_onekey_fee_content,
-              },
-              {
-                number: `${displayFee}%`,
-              },
-            )}
+            {intl.formatMessage({
+              id: ETranslations.provider_popover_onekey_fee_content_nofee,
+            })}
           </SizableText>
           <SizableText size="$bodyMd" color="$textSubdued">
             {intl.formatMessage({
