4705ec224b99e61367a4989e8b97a2ce78b53bea
zy2000-star <2000z1013y@gmail.com>
2026-07-13 12:39:08 +0800

    feat: add OKX-style TradingView settings gallery

---
+++ packages/kit/src/components/TradingView/TradingViewV2/components/chartSettings/index.ts
@@ -0,0 +1,24 @@
+export {
+  TradingViewChartSettings,
+  TradingViewIndicatorSettings,
+} from './TradingViewSettingsMockGallery';
+export type {
+  ITradingViewChartSettingsProps,
+  ITradingViewIndicatorSettingsProps,
+} from './TradingViewSettingsMockGallery';
+export {
+  TRADING_VIEW_SETTINGS_SCHEMA_VERSION,
+  createTradingViewChartSettingsValue,
+  createTradingViewIndicatorSettingsValue,
+} from './TradingViewSettingsMockState';
+export type {
+  ITradingViewSettingsMockAppearanceItem as ITradingViewChartSettingsAppearanceItem,
+  ITradingViewSettingsMockAppearanceSection as ITradingViewChartSettingsAppearanceSection,
+  ITradingViewChartSettingsValue,
+  ITradingViewIndicatorSettingsValue,
+  ITradingViewSettingsMockIndicator as ITradingViewIndicatorSettingsItem,
+  ITradingViewSettingsMockIndicatorScope as ITradingViewIndicatorSettingsScope,
+  ITradingViewSettingsMockLine as ITradingViewIndicatorSettingsLine,
+  ITradingViewSettingsMockLineStyle as ITradingViewIndicatorSettingsLineStyle,
+  ITradingViewSettingsMockNumberParam as ITradingViewIndicatorSettingsNumberParam,
+} from './TradingViewSettingsMockState';
