6fd4c7a282edb4b016de9763e1cd9a805dd28d35
Charon <charon.dian@gmail.com>
2026-07-13 18:20:23 +0800

    revert: restore extension Perps home navigation OK-57350

--- packages/kit/src/views/Perp/pages/ExtPerp.tsx
+++ packages/kit/src/views/Perp/pages/ExtPerp.tsx
@@ -19,7 +19,7 @@ export function ExtPerp() {
       if (shouldOpenExpandExtPerp) {
         void backgroundApiProxy.serviceWebviewPerp.openExtPerpTab();
         setTimeout(() => {
-          switchTab(ETabRoutes.Swap);
+          switchTab(ETabRoutes.Home);
         }, 300);
       }
     }, []),
