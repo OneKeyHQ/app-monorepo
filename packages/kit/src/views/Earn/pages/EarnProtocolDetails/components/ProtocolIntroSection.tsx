14eb721c03f49ec27f21d2fea5eb230494d4e264
Charon <charon.dian@gmail.com>
2026-07-15 09:35:19 +0800

    fix: keep protocol dialogs visible in extension popup (OK-56215)

--- packages/kit/src/views/Earn/pages/EarnProtocolDetails/components/ProtocolIntroSection.tsx
+++ packages/kit/src/views/Earn/pages/EarnProtocolDetails/components/ProtocolIntroSection.tsx
@@ -32,6 +32,7 @@ import { EarnTestIDs } from '@onekeyhq/kit/src/views/Earn/testIDs';
 import { EarnIcon } from '@onekeyhq/kit/src/views/Staking/components/ProtocolDetails/EarnIcon';
 import { EarnText } from '@onekeyhq/kit/src/views/Staking/components/ProtocolDetails/EarnText';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 import { openUrlExternal } from '@onekeyhq/shared/src/utils/openUrlUtils';
 import type {
   IEarnProtocolIntroAudit,
@@ -345,10 +346,21 @@ function hasProtocolIntroItemContent(item: IEarnProtocolIntroItem) {
 }
 
 const DIALOG_CONTENT_MAX_HEIGHT = 512;
+const EXTENSION_POPUP_DIALOG_CONTENT_HEIGHT = 260;
 
 function DialogContent({ children }: { children: React.ReactNode }) {
   return (
-    <ScrollView maxHeight={DIALOG_CONTENT_MAX_HEIGHT} nestedScrollEnabled>
+    <ScrollView
+      height={
+        platformEnv.isExtensionUiPopup
+          ? EXTENSION_POPUP_DIALOG_CONTENT_HEIGHT
+          : undefined
+      }
+      maxHeight={
+        platformEnv.isExtensionUiPopup ? undefined : DIALOG_CONTENT_MAX_HEIGHT
+      }
+      nestedScrollEnabled
+    >
       <YStack px="$5" pb="$5">
         {children}
       </YStack>
@@ -1561,6 +1573,7 @@ function ProtocolIntroSectionComponent({
           variant: 'secondary',
         },
         showCancelButton: false,
+        disableDrag: platformEnv.isExtensionUiPopup,
       });
     },
     [intl],
