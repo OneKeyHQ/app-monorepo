4eea652ea710b6af6b59b6b25d804c1ec4dcf784
wabicai <kaisheng.cai@onekey.so>
2026-08-04 20:33:24 +0900

    chore: upgrade hardware SDK to alpha.53

--- packages/kit/src/views/Developer/pages/Gallery/Components/index.tsx
+++ packages/kit/src/views/Developer/pages/Gallery/Components/index.tsx
@@ -71,7 +71,6 @@ const Index = () => {
             <ListItem
               style={{ width: '90%', maxWidth: 640, alignSelf: 'center' }}
               key={item.replace('component-', '')}
-              testID={`developer-gallery-${item}-item`}
               drillIn
               onPress={() => {
                 // @ts-expect-error
