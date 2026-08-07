8a91f31c41c520c0801d318c4e3aaa2fe57fc6b5
JellyWang <38491708+ezailWang@users.noreply.github.com>
2026-08-07 15:50:39 +0800

    Feat/protocol tips OK-58972 (#12715)
    (cherry picked from commit fa9228f7627c1c975be56ebece7ff52b40d4def2)

--- packages/kit/src/views/Earn/pages/EarnProtocolDetails/index.tsx
+++ packages/kit/src/views/Earn/pages/EarnProtocolDetails/index.tsx
@@ -87,6 +87,7 @@ import { EarnNavigation, EarnNetworkUtils } from '../../earnUtils';
 
 import { ApyChart } from './components/ApyChart';
 import { ProtocolIntroSection } from './components/ProtocolIntroSection';
+import { ProtocolTipsSection } from './components/ProtocolTipsSection';
 import { useProtocolDetailBreadcrumb } from './hooks/useProtocolDetailBreadcrumb';
 import { useProtocolDetailData } from './hooks/useProtocolDetailData';
 
@@ -636,6 +637,8 @@ const DetailsPartComponent = ({
                 provider={provider}
                 vault={vault}
               />
+              {/* Protocol Tips (OK-58972)：图表下方浅灰卡片，dashboard 配置 */}
+              <ProtocolTipsSection protocolTips={detailInfo.protocolTips} />
             </YStack>
             <EarnPlatformBonusSection
               appearance="alert"
