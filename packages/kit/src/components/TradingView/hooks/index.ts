dff362df0107e3e8f743745c98efd73867c8e7a4
huhuanming <huanming@onekey.so>
2026-08-19 13:11:49 +0800

    fix: sync sidebar theme switching (#12887)
    * fix: sync sidebar theme switching
    
    * fix: stabilize theme switching
    
    * fix: sync embedded themes after reload OK-54061
    
    (cherry picked from commit 929f05a52973c63445b36a606772c1ee2f775f12)

--- packages/kit/src/components/TradingView/hooks/index.ts
+++ packages/kit/src/components/TradingView/hooks/index.ts
@@ -1,3 +1,3 @@
-export { useTradingViewUrl } from './useTradingViewUrl';
+export { syncTradingViewTheme, useTradingViewUrl } from './useTradingViewUrl';
 export type { ITradingViewDisabledFeature } from '../constants';
 export { useNavigationHandler } from './useNavigationHandler';
