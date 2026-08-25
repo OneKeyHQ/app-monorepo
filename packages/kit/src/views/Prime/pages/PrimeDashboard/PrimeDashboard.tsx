f119c1b640bd4cb8edb45d96e49ce94c496b4cf4
yikZero <i@yikzero.com>
2026-08-25 08:21:17 +0800

    style: format Prime dashboard

--- packages/kit/src/views/Prime/pages/PrimeDashboard/PrimeDashboard.tsx
+++ packages/kit/src/views/Prime/pages/PrimeDashboard/PrimeDashboard.tsx
@@ -503,7 +503,6 @@ export default function PrimeDashboard({
                   />
                   {renderLoginPrompt}
                 </YStack>
-
               </Stack>
             </Page.Footer>
           ) : null}
