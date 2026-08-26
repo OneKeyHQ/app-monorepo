fdfc64074c5d3777ab9f98d7314e8aedd6f3e8fe
Leon <lixiao.dev@gmail.com>
2026-08-26 11:23:53 +0800

    fix: name the device on the connecting capsule from the scan result (OK-59934)

--- packages/kit/src/views/Onboardingv2/pages/ConnectYourDevice.tsx
+++ packages/kit/src/views/Onboardingv2/pages/ConnectYourDevice.tsx
@@ -1104,6 +1104,8 @@ function ConnectYourDevicePage({
           const showCheckingDeviceDialog = () =>
             backgroundApiProxy.serviceHardwareUI.showCheckingDeviceDialog({
               connectId,
+              deviceType: item.device?.deviceType ?? undefined,
+              deviceName: item.device?.name ?? undefined,
             });
           checkingDialogOpened = true;
           if (platformEnv.isNativeIOS) {
