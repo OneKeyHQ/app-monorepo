1d9036541cb5c04e455808d23bfff00cc0a4b3f9
huhuanming <huanming@onekey.so>
2026-09-04 09:36:36 +0800

    test: align dev vendor package version

--- apps/mobile/plugins/__tests__/devVendor.test.js
+++ apps/mobile/plugins/__tests__/devVendor.test.js
@@ -1072,8 +1072,8 @@ describe('devVendor', () => {
         ({ name }) => name === '@react-native-async-storage/async-storage',
       ),
     ).toMatchObject({
-      resolution: '@onekeyfe/react-native-async-storage@npm:3.0.95',
-      version: '3.0.95',
+      resolution: '@onekeyfe/react-native-async-storage@npm:3.0.103',
+      version: '3.0.103',
     });
     expect(
       androidDescriptor.dependencies.find(
