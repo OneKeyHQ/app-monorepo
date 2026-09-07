63e689971d1ec2b5fc2e9e8387f74168fb9c6d86
morizon <sidmorizon@outlook.com>
2026-09-07 10:21:00 +0800

    style: apply runtime network adapter formatting

--- packages/shared/src/travelMode/runtimeNetworkAdapter.native.ts
+++ packages/shared/src/travelMode/runtimeNetworkAdapter.native.ts
@@ -6,7 +6,5 @@ export function createRuntimeNetworkAdapter(
   adapter: AxiosAdapter,
 ): AxiosAdapter {
   return (config) =>
-    runRuntimeWalletEffect(() =>
-      Promise.resolve().then(() => adapter(config)),
-    );
+    runRuntimeWalletEffect(() => Promise.resolve().then(() => adapter(config)));
 }
