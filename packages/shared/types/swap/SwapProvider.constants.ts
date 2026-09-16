7c55055625b826edc2df87dcfb4c3cbc8da2a504
Charon <charon.dian@gmail.com>
2026-09-17 07:49:34 +0800

    fix: handle Arc swap entries and defaults (OK-63560)

--- packages/shared/types/swap/SwapProvider.constants.ts
+++ packages/shared/types/swap/SwapProvider.constants.ts
@@ -78,6 +78,10 @@ export const maxRecentTokenPairs = 10;
 
 export const swapProviderRecommendApprovedWeights = 1.1;
 
+export const ARC_NETWORK_ID = 'evm--5042';
+export const ARC_ERC20_USDC_CONTRACT_ADDRESS =
+  '0x3600000000000000000000000000000000000000';
+
 export const limitOrderEstimationFeePercent = 1.05;
 
 export const defaultSupportUrl = 'https://help.onekey.so/articles/11536900';
@@ -214,6 +218,28 @@ export const swapDefaultSetTokens: Record<
     limitToToken?: ISwapToken;
   }
 > = {
+  [ARC_NETWORK_ID]: {
+    fromToken: {
+      'networkId': ARC_NETWORK_ID,
+      'contractAddress': ARC_ERC20_USDC_CONTRACT_ADDRESS,
+      'name': 'USD Coin',
+      'symbol': 'USDC',
+      'decimals': 6,
+      'logoURI': `https://uni.onekey-asset.com/server-service-onchain/${ARC_NETWORK_ID}/tokens/${ARC_ERC20_USDC_CONTRACT_ADDRESS}.png`,
+      'isNative': false,
+    },
+    toToken: {
+      'networkId': 'evm--1',
+      'contractAddress': '',
+      'name': 'Ethereum',
+      'symbol': 'ETH',
+      'decimals': 18,
+      'logoURI':
+        'https://uni.onekey-asset.com/server-service-indexer/evm--1/tokens/address--1721282106924.png',
+      'isNative': true,
+      'networkLogoURI': 'https://uni.onekey-asset.com/static/chain/eth.png',
+    },
+  },
   'onekeyall--0': {
     fromToken: {
       'networkId': 'evm--1',
