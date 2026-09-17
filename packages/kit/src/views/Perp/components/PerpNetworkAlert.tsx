6cdc476ec5e97d230df33bba6904a1b888515ca6
Kahn <0xkahnchan@gmail.com>
2026-09-17 22:24:34 +0800

    fix: refine perps banners on web and desktop

--- packages/kit/src/views/Perp/components/PerpNetworkAlert.tsx
+++ packages/kit/src/views/Perp/components/PerpNetworkAlert.tsx
@@ -1,10 +1,12 @@
 import { memo } from 'react';
 
 import { useIntl } from 'react-intl';
+import { Platform, StyleSheet } from 'react-native';
 
 import { Alert, YStack } from '@onekeyhq/components';
 import { usePerpsNetworkStatusAtom } from '@onekeyhq/kit-bg/src/states/jotai/atoms';
 import { ETranslations } from '@onekeyhq/shared/src/locale';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 function PerpNetworkAlertComponent() {
   const intl = useIntl();
@@ -15,15 +17,28 @@ function PerpNetworkAlertComponent() {
   }
 
   return (
-    <YStack height={0} overflow="visible" zIndex={10}>
+    <YStack
+      height={Platform.select<number | 'auto'>({ web: 'auto', default: 0 })}
+      overflow="visible"
+      zIndex={10}
+    >
       <Alert
         type="default"
         fullBleed
         title={intl.formatMessage({
-          id: ETranslations.perps_offline_moblie,
+          id: Platform.select({
+            web: ETranslations.perps_offline_desktop__msg,
+            default: ETranslations.perps_offline_moblie,
+          }),
         })}
         closable={false}
         px="$4"
+        {...Platform.select({
+          web: {
+            borderTopWidth: 0,
+            borderBottomWidth: platformEnv.isWeb ? StyleSheet.hairlineWidth : 0,
+          },
+        })}
       />
     </YStack>
   );
